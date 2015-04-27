56939ea165469bdccecb48095278ac168e367917
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2015-04-27 18:29:08 +0000

    3.9: - WPDB: Sanity check that any strings being stored in the DB are not too long to store correctly. - When upgrading, remove any suspicious comments.
    git-svn-id: https://develop.svn.wordpress.org/branches/3.9@32316 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/db.php
+++ tests/phpunit/tests/db.php
@@ -613,6 +613,7 @@ class Tests_DB extends WP_UnitTestCase {
 				'format' => '%s',
 				'charset' => $expected_charset,
 				'ascii' => false,
+				'length' => $wpdb->get_col_length( $wpdb->posts, 'post_content' ),
 			)
 		);
 
