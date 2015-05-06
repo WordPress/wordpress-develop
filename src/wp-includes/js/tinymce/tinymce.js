7731b9f9e2da8d42fc39d65b195e2408d7543e89
Andrew Ozz <azaozz@git.wordpress.org>
2015-05-06 05:11:28 +0000

    Add some of the TinyMCE 4.1.10 fixes to 4.2. See [32366], 32262.
    git-svn-id: https://develop.svn.wordpress.org/branches/4.2@32369 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/js/tinymce/tinymce.js
+++ src/wp-includes/js/tinymce/tinymce.js
@@ -1,4 +1,4 @@
-// 4.1.9 (2015-04-05)
+// 4.1.9 (2015-05-05)
 
 /**
  * Compiled inline version. (Library mode)
@@ -6209,7 +6209,7 @@ define("tinymce/html/Entities", [
 		attrsCharsRegExp = /[&<>\"\u0060\u007E-\uD7FF\uE000-\uFFEF]|[\uD800-\uDBFF][\uDC00-\uDFFF]/g,
 		textCharsRegExp = /[<>&\u007E-\uD7FF\uE000-\uFFEF]|[\uD800-\uDBFF][\uDC00-\uDFFF]/g,
 		rawCharsRegExp = /[<>&\"\']/g,
-		entityRegExp = /&(#x|#)?([\w]+);/g,
+		entityRegExp = /&#([a-z0-9]+);?|&([a-z0-9]+);/gi,
 		asciiMap = {
 			128: "\u20AC", 130: "\u201A", 131: "\u0192", 132: "\u201E", 133: "\u2026", 134: "\u2020",
 			135: "\u2021", 136: "\u02C6", 137: "\u2030", 138: "\u0160", 139: "\u2039", 140: "\u0152",
@@ -6423,17 +6423,21 @@ define("tinymce/html/Entities", [
 		 * @return {String} Entity decoded string.
 		 */
 		decode: function(text) {
-			return text.replace(entityRegExp, function(all, numeric, value) {
+			return text.replace(entityRegExp, function(all, numeric) {
 				if (numeric) {
-					value = parseInt(value, numeric.length === 2 ? 16 : 10);
+					if (numeric.charAt(0).toLowerCase() === 'x') {
+						numeric = parseInt(numeric.substr(1), 16);
+					} else {
+						numeric = parseInt(numeric, 10);
+					}
 
 					// Support upper UTF
-					if (value > 0xFFFF) {
-						value -= 0x10000;
+					if (numeric > 0xFFFF) {
+						numeric -= 0x10000;
 
-						return String.fromCharCode(0xD800 + (value >> 10), 0xDC00 + (value & 0x3FF));
+						return String.fromCharCode(0xD800 + (numeric >> 10), 0xDC00 + (numeric & 0x3FF));
 					} else {
-						return asciiMap[value] || String.fromCharCode(value);
+						return asciiMap[numeric] || String.fromCharCode(numeric);
 					}
 				}
 
@@ -12648,7 +12652,7 @@ define("tinymce/html/Writer", [
 			 */
 			pi: function(name, text) {
 				if (text) {
-					html.push('<?', name, ' ', text, '?>');
+					html.push('<?', name, ' ', encode(text), '?>');
 				} else {
 					html.push('<?', name, '?>');
 				}
@@ -15585,7 +15589,8 @@ define("tinymce/dom/Selection", [
 		},
 
 		/**
-		 * Executes callback of the current selection matches the specified selector or not and passes the state and args to the callback.
+		 * Executes callback when the current selection starts/stops matching the specified selector. The current
+		 * state will be passed to the callback as it's first argument.
 		 *
 		 * @method selectorChanged
 		 * @param {String} selector CSS selector to check for.
@@ -28020,7 +28025,7 @@ define("tinymce/util/Quirks", [
 				editor.on('keydown', function(e) {
 					if (VK.metaKeyPressed(e) && (e.keyCode == 37 || e.keyCode == 39)) {
 						e.preventDefault();
-						editor.selection.getSel().modify('move', e.keyCode == 37 ? 'backward' : 'forward', 'word');
+						editor.selection.getSel().modify('move', e.keyCode == 37 ? 'backward' : 'forward', 'lineboundary');
 					}
 				});
 			}
@@ -31322,7 +31327,7 @@ define("tinymce/EditorManager", [
 		 * @property releaseDate
 		 * @type String
 		 */
-		releaseDate: '2015-04-05',
+		releaseDate: '2015-05-05',
 
 		/**
 		 * Collection of editor instances.
