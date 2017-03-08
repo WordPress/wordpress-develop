6e9b29e13ad80fc643da6237b4cadb45ec9d95a1
John Blackbourn <johnbillion@git.wordpress.org>
2017-03-08 00:47:48 +0000

    Build/Test tools: In Travis, skip some tests when not on trunk.
    This skips time sensitive tests (copyright year and PHP/MySQL version requirements) when tests are run on branches on Travis.
    
    Props netweb, jorbin
    
    Fixes #39486
    
    Merges [40241] to the 3.9 branch.
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/3.9@40248 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/includes/testcase.php
+++ tests/phpunit/includes/testcase.php
@@ -49,6 +49,27 @@ class WP_UnitTestCase extends PHPUnit_Framework_TestCase {
 		$this->flush_cache();
 	}
 
+	/**
+	 * Allow tests to be skipped on some automated runs
+	 *
+	 * For test runs on Travis for something other than trunk/master 
+	 * we want to skip tests that only need to run for master.
+	 */
+	public function skipOnAutomatedBranches() {
+		// gentenv can be disabled
+		if ( ! function_exists( 'getenv' ) ) {
+			return false;
+		}
+
+		// https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
+		$travis_branch       = getenv( 'TRAVIS_BRANCH' );
+		$travis_pull_request = getenv( 'TRAVIS_PULL_REQUEST' );
+
+		if ( false !== $travis_pull_request && 'master' !== $travis_branch ) {
+			$this->markTestSkipped( 'For automated test runs, this test is only run on trunk/master' );
+		}
+	}
+
 	function flush_cache() {
 		global $wp_object_cache;
 		$wp_object_cache->group_ops = array();
