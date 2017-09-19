d96d2366464ca203229f55693378802a3abf35f5
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2017-09-19 19:41:01 +0000

    Bump 4.8 branch to version 4.8.2.
    git-svn-id: https://develop.svn.wordpress.org/branches/4.8@41509 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8.2-alpha-41210-src';
+$wp_version = '4.8.2-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
