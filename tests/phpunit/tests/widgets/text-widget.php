b6ab792ca4a75b0f0a50044200e027f54ca9e683
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2017-09-19 07:43:34 +0000

    Widgets: Prevent visual Text widget from decoding encoded HTML.
    Also apply `the_editor_content` filters on widget `text` with `format_for_editor()` as is done for the post editor.
    
    Merge of [41260] to the 4.8 branch.
    
    Amends [40631].
    Props westonruter, azaozz.
    See #35243.
    Fixes #41596.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/4.8@41392 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/widgets/text-widget.php
+++ tests/phpunit/tests/widgets/text-widget.php
@@ -446,7 +446,9 @@ class Test_WP_Widget_Text extends WP_UnitTestCase {
 	 * @covers WP_Widget_Text::form
 	 */
 	function test_form() {
+		add_filter( 'user_can_richedit', '__return_true' );
 		$widget = new WP_Widget_Text();
+		$widget->_set( 2 );
 		$instance = array(
 			'title' => 'Title',
 			'text' => 'Text',
@@ -458,7 +460,7 @@ class Test_WP_Widget_Text extends WP_UnitTestCase {
 		$widget->form( $instance );
 		$form = ob_get_clean();
 		$this->assertContains( 'class="visual" type="hidden" value=""', $form );
-		$this->assertNotContains( 'class="visual" type="hidden" value="on"', $form );
+		$this->assertNotContains( 'class="visual sync-input" type="hidden" value="on"', $form );
 
 		$instance = array(
 			'title' => 'Title',
@@ -469,8 +471,8 @@ class Test_WP_Widget_Text extends WP_UnitTestCase {
 		ob_start();
 		$widget->form( $instance );
 		$form = ob_get_clean();
-		$this->assertContains( 'class="visual" type="hidden" value="on"', $form );
-		$this->assertNotContains( 'class="visual" type="hidden" value=""', $form );
+		$this->assertContains( 'class="visual sync-input" type="hidden" value="on"', $form );
+		$this->assertNotContains( 'class="visual sync-input" type="hidden" value=""', $form );
 
 		$instance = array(
 			'title' => 'Title',
@@ -481,12 +483,12 @@ class Test_WP_Widget_Text extends WP_UnitTestCase {
 		ob_start();
 		$widget->form( $instance );
 		$form = ob_get_clean();
-		$this->assertContains( 'class="visual" type="hidden" value="on"', $form );
-		$this->assertNotContains( 'class="visual" type="hidden" value=""', $form );
+		$this->assertContains( 'class="visual sync-input" type="hidden" value="on"', $form );
+		$this->assertNotContains( 'class="visual sync-input" type="hidden" value=""', $form );
 
 		$instance = array(
 			'title' => 'Title',
-			'text' => 'Text',
+			'text' => 'This is some HTML Code: <code>&lt;strong&gt;BOLD!&lt;/strong&gt;</code>',
 			'filter' => true,
 			'visual' => true,
 		);
@@ -494,8 +496,24 @@ class Test_WP_Widget_Text extends WP_UnitTestCase {
 		ob_start();
 		$widget->form( $instance );
 		$form = ob_get_clean();
-		$this->assertContains( 'class="visual" type="hidden" value="on"', $form );
-		$this->assertNotContains( 'class="visual" type="hidden" value=""', $form );
+		$this->assertContains( 'class="visual sync-input" type="hidden" value="on"', $form );
+		$this->assertContains( '&lt;code&gt;&amp;lt;strong&amp;gt;BOLD!', $form );
+		$this->assertNotContains( 'class="visual sync-input" type="hidden" value=""', $form );
+
+		remove_filter( 'user_can_richedit', '__return_true' );
+		add_filter( 'user_can_richedit', '__return_false' );
+		$instance = array(
+			'title' => 'Title',
+			'text' => 'Evil:</textarea><script>alert("XSS")</script>',
+			'filter' => true,
+			'visual' => true,
+		);
+		$this->assertFalse( $widget->is_legacy_instance( $instance ) );
+		ob_start();
+		$widget->form( $instance );
+		$form = ob_get_clean();
+		$this->assertNotContains( 'Evil:</textarea>', $form );
+		$this->assertContains( 'Evil:&lt;/textarea>', $form );
 	}
 
 	/**
