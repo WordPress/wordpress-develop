e4ffde619329b7260d2444ae98e11e5044fda6e2
Ian Dunn <iandunn@git.wordpress.org>
2018-05-03 17:24:35 +0000

    Privacy: Limit Privacy Settings screen to Super Admins in Multisite.
    In many common Multisite use cases, the network administrator will want to set a network-wide privacy policy -- via the `privacy_policy_url` filter -- for consistency and convenience. When that's done, the Privacy Settings screen on individual sites becomes unnecessary, and may confuse administrators of those sites when they see that their changes don't have any effect on the policy link in the footer.
    
    Since we can't programatically determine which behavior the network admins would like, the safest default setting is to restrict the ability to super admins, and let them delegate it to individual site owners via a plugin, if they'd like to.
    
    Fixes #43935.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@43147 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/user/capabilities.php
+++ tests/phpunit/tests/user/capabilities.php
@@ -239,6 +239,7 @@ class Tests_User_Capabilities extends WP_UnitTestCase {
 			'upgrade_php'            => array( 'administrator' ),
 			'export_others_personal_data' => array( 'administrator' ),
 			'erase_others_personal_data'  => array( 'administrator' ),
+			'manage_privacy_policy'       => array( 'administrator' ),
 
 			'edit_categories'        => array( 'administrator', 'editor' ),
 			'delete_categories'      => array( 'administrator', 'editor' ),
@@ -273,6 +274,7 @@ class Tests_User_Capabilities extends WP_UnitTestCase {
 			'upgrade_php'            => array(),
 			'export_others_personal_data' => array( '' ),
 			'erase_others_personal_data'  => array( '' ),
+			'manage_privacy_policy'       => array(),
 
 			'customize'              => array( 'administrator' ),
 			'delete_site'            => array( 'administrator' ),
