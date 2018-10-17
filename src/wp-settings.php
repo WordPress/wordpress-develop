238c743071ce6bb0a2f4d68bdade6734c25fdd19
Daniel Bachhuber <daniel@bachhuber.co>
2018-10-17 04:34:16 -0700

    Initial `45097.diff` patch

--- src/wp-settings.php
+++ src/wp-settings.php
@@ -240,6 +240,8 @@ require( ABSPATH . WPINC . '/rest-api/fields/class-wp-rest-comment-meta-fields.p
 require( ABSPATH . WPINC . '/rest-api/fields/class-wp-rest-post-meta-fields.php' );
 require( ABSPATH . WPINC . '/rest-api/fields/class-wp-rest-term-meta-fields.php' );
 require( ABSPATH . WPINC . '/rest-api/fields/class-wp-rest-user-meta-fields.php' );
+require( ABSPATH . WPINC . '/class-wp-block-type.php' );
+require( ABSPATH . WPINC . '/class-wp-block-type-registry.php' );
 
 $GLOBALS['wp_embed'] = new WP_Embed();
 
