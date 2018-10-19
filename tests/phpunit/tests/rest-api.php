88fbc8f35db248414570cddf5829838d86a16a62
Gary Pendergast <pento@git.wordpress.org>
2018-10-19 08:56:58 +0000

    REST API: Introduce the `rest_preload_api_request()` function.
    This function helps perform multiple REST API requests, for the purpose of preloading data into a page.
    
    See #45110.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.0@43763 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/rest-api.php
+++ tests/phpunit/tests/rest-api.php
@@ -593,4 +593,13 @@ class Tests_REST_API extends WP_UnitTestCase {
 		$routes = $GLOBALS['wp_rest_server']->get_routes();
 		$this->assertEquals( $routes['/test-ns/test'][0]['methods'], array( 'GET' => true ) );
 	}
+
+	/**
+	 * Ensure rest_preload_api_request() works without notices in PHP 5.2.
+	 *
+	 * The array_reduce() function only accepts mixed variables starting with PHP 5.3.
+	 */
+	function test_rest_preload_api_request_no_notices_php_52() {
+		$this->assertTrue( is_array( rest_preload_api_request( 0, '/' ) ) );
+	}
 }
