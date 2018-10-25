5d9480bd6725a728243ed3aa1ebfd28de06180d8
Aaron Jorbin <aaron@jorb.in>
2018-10-24 21:47:44 -0400

    Actually, include groupby so it's easier to see everything that can be filtered

--- src/wp-includes/class-wp-network-query.php
+++ src/wp-includes/class-wp-network-query.php
@@ -393,7 +393,9 @@ class WP_Network_Query {
 
 		$where = implode( ' AND ', $this->sql_clauses['where'] );
 
-		$pieces = array( 'fields', 'join', 'where', 'orderby', 'limits' );
+		$groupby = '';
+
+		$pieces = array( 'fields', 'join', 'where', 'orderby', 'limits', 'groupby' );
 
 		/**
 		 * Filters the network query clauses.
