b8abc8a02ab4a6e5a1c96a9135af69751556869e
Jonathan Desrosiers <desrosj@gmail.com>
2018-11-15 15:40:07 -0500

    Update new test method.

--- tests/phpunit/tests/menu/nav-menu.php
+++ tests/phpunit/tests/menu/nav-menu.php
@@ -206,22 +206,23 @@ class Tests_Nav_Menu_Theme_Change extends WP_UnitTestCase {
 	 *
 	 * @covers wp_map_nav_menu_locations()
 	 */
-	function test_numerical_old_locations() {
+	public function test_numerical_old_locations() {
 		$this->register_nav_menu_locations( array( 'primary', 1 ) );
 
 		$old_nav_menu_locations = array(
-			'primary'   => 1,
-			1           => 2,
-			'tertiary'  => 3,
+			'primary'  => 1,
+			'tertiary' => 2,
+			0          => 3,
 		);
 
-		$old_next_theme_nav_menu_locations = array();
-		$new_next_theme_nav_menu_locations = wp_map_nav_menu_locations( $old_next_theme_nav_menu_locations, $old_nav_menu_locations );
+		$next_theme_nav_menu_locations     = array();
+		$new_next_theme_nav_menu_locations = wp_map_nav_menu_locations( $next_theme_nav_menu_locations, $old_nav_menu_locations );
 
 		$expected_nav_menu_locations = array(
 			'primary' => 1,
-			1         => 2,
+			0         => 3,
 		);
+
 		$this->assertEqualSets( $expected_nav_menu_locations, $new_next_theme_nav_menu_locations );
 	}
 }
