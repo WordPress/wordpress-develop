de91abb11f634b474cddf64e1bbb8fc39b120069
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-03-28 21:16:48 +0000

    Build/Test Tools: Exclude `twentynineteen/node_modules` from `jsvalidate:build` task.
    Fixes #46706.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@45060 602fd350-edb4-49c9-b593-d223f7449a82

--- Gruntfile.js
+++ Gruntfile.js
@@ -967,6 +967,7 @@ module.exports = function(grunt) {
 					src: [
 						WORKING_DIR + 'wp-{admin,includes}/**/*.js',
 						WORKING_DIR + 'wp-content/themes/twenty*/**/*.js',
+						'!' + WORKING_DIR + 'wp-content/themes/twenty*/node_modules/**/*.js',
 						'!' + WORKING_DIR + 'wp-includes/js/dist/vendor/*.js',
 					]
 				}
