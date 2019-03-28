a66e27efc41b8efb0108b03618e1195a834129f6
Aaron Jorbin <jorbin@git.wordpress.org>
2019-03-28 21:11:29 +0000

    PHP: Finish updating PHP Minimum version
    Previous [44982] and [44950].
    
    This bumps the minimum version down slightly based on some research into the usage of php 5.6. This also brings the recommended version in line with https://wordpress.org/about/requirements/ . 
    
    The travis matrix is also being reduced in order to speed up builds. This is a hard break. PHP 5.2 - 5.5 we are never ever, ever, getting back together. You go talk to your friends, talk to my friends, talk to me, But we are never ever, ever, ever getting back together.
    
    Props DD32, afergen for research.
    Fixes #46594.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@45058 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -34,7 +34,7 @@ $tinymce_version = '4920-20181217';
  *
  * @global string $required_php_version
  */
-$required_php_version = '5.6.30';
+$required_php_version = '5.6.20';
 
 /**
  * Holds the required MySQL version
