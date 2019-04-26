a5a9960f8412ea7fdc216769f6a12e2d32255b90
Gary Pendergast <pento@git.wordpress.org>
2019-04-26 01:14:16 +0000

    WordPress 5.2 RC1
    git-svn-id: https://develop.svn.wordpress.org/trunk@45272 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-beta3-45183-src';
+$wp_version = '5.2-RC1-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
