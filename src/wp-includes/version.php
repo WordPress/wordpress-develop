5640763da953c28033e9f285c7c2a1da4c613aa3
Aaron Jorbin <jorbin@git.wordpress.org>
2019-05-02 16:27:59 +0000

    Trunk is now 5.3 alpha
    git-svn-id: https://develop.svn.wordpress.org/trunk@45282 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-RC2-45280-src';
+$wp_version = '5.3-alpha-45282-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
