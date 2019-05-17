996c9ddd02c1f25b3a2d29769ffc5d1861a23a86
Jonathan Desrosiers <desrosj@git.wordpress.org>
2019-05-17 19:07:11 +0000

    WordPress 5.2.1-RC1.
    git-svn-id: https://develop.svn.wordpress.org/branches/5.2@45360 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.1-alpha-45295-src';
+$wp_version = '5.2.1-RC1-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
