c70201ac74bd07be3eeb072ace44c880a08ae18a
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-05-17 12:11:06 +0000

    Formatting: Adjust `wp_targeted_link_rel()` to ensure JSON format is preserved and correct quotes are used when adding the missing `rel` attribute.
    Props birgire, TobiasBg, fierevere, audrasjb, SergeyBiryukov.
    Merges [45348] to the 5.2 branch.
    Fixes #46316, #47244.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.2@45349 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/formatting/WPTargetedLinkRel.php
+++ tests/phpunit/tests/formatting/WPTargetedLinkRel.php
@@ -101,4 +101,30 @@ class Tests_Targeted_Link_Rel extends WP_UnitTestCase {
 
 		$this->assertEquals( $expected, $post->post_content );
 	}
+
+	/**
+	 * Ensure JSON format is preserved when relation attribute (rel) is missing.
+	 *
+	 * @ticket 46316
+	 */
+	public function test_wp_targeted_link_rel_should_preserve_json() {
+		$content  = '<p>Links: <a href=\"\/\" target=\"_blank\">No rel<\/a><\/p>';
+		$expected = '<p>Links: <a href=\"\/\" target=\"_blank\" rel=\"noopener noreferrer\">No rel<\/a><\/p>';
+		$this->assertEquals( $expected, wp_targeted_link_rel( $content ) );
+	}
+
+	/**
+	 * Ensure correct quotes are used when relation attribute (rel) is missing.
+	 *
+	 * @ticket 47244
+	 */
+	public function test_wp_targeted_link_rel_should_use_correct_quotes() {
+		$content  = '<p>Links: <a href=\'\/\' target=\'_blank\'>No rel<\/a><\/p>';
+		$expected = '<p>Links: <a href=\'\/\' target=\'_blank\' rel=\'noopener noreferrer\'>No rel<\/a><\/p>';
+		$this->assertEquals( $expected, wp_targeted_link_rel( $content ) );
+
+		$content  = '<p>Links: <a href=\'\/\' target=_blank>No rel<\/a><\/p>';
+		$expected = '<p>Links: <a href=\'\/\' target=_blank rel=\'noopener noreferrer\'>No rel<\/a><\/p>';
+		$this->assertEquals( $expected, wp_targeted_link_rel( $content ) );
+	}
 }
