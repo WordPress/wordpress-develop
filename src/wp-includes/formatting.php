b078f0d8f27627fe10b4faebb0e52d4938e1aa7b
Adam Silverstein <adam@10up.com>
2019-07-09 11:31:57 -0600

    impreove regexes, remove escaping

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -610,11 +610,11 @@ function wpautop( $pee, $br = true ) {
 
 	// No <p> around HTML comment
 	if ( strpos( $pee, '<!--' ) !== false ) {
-		$pee = preg_replace( '|<p>\s*<\!--|', '<!--', $pee );
+		$pee = preg_replace( '|<p>\s*<!--|', '<!--', $pee );
 
 		// Remove the tailing paragraph if this paragraph starts as a comment.
 		if ( 0 === strpos( $pee, '<!--' ) ) {
-			$pee = preg_replace( '|\-->\s*</p>|', '-->', $pee );
+			$pee = preg_replace( '|-->\s*</p>|', '-->', $pee );
 		}
 	}
 
