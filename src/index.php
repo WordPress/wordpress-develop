8df86efc2b248e05ad54655a7857243e586e2ece
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-07-16 22:27:55 +0000

    Site Health: Use consistent verbiage for "Learn more" links.
    Props ronakganatra, garrett-eclipse.
    Fixes #47302.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@45649 602fd350-edb4-49c9-b593-d223f7449a82

--- src/index.php
+++ src/index.php
@@ -49,7 +49,7 @@ $die .= '</ul>';
 
 $die .= '<p>' . sprintf(
 	/* translators: 1: NPM URL, 2: Handbook URL */
-	__( 'This requires <a href="%1$s">NPM</a>. <a href="%2$s">Read more about setting up your local development environment</a>.' ),
+	__( 'This requires <a href="%1$s">NPM</a>. <a href="%2$s">Learn more about setting up your local development environment</a>.' ),
 	'https://www.npmjs.com/get-npm',
 	__( 'https://make.wordpress.org/core/handbook/tutorials/installing-wordpress-locally/' )
 ) . '</p>';
