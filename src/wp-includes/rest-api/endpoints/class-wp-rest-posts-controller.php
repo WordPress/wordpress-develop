faa59413f16d92a9b7259036cc40652014c5b1dc
Adam Silverstein <adam@10up.com>
2019-08-13 14:15:00 -0600

    doc cleanup

--- src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php
@@ -1036,7 +1036,8 @@ class WP_REST_Posts_Controller extends WP_REST_Controller {
 			}
 		}
 
-		// Sending a blank date or date_gmt value resets date and date_gmt to their default values (`0000-00-00 00:00:00`).
+		// Sending a blank (null or empty string) date or date_gmt value resets date and date_gmt to their
+		// default values (`0000-00-00 00:00:00`).
 		if (
 			( isset( $request['date_gmt'] ) && '' === $request['date_gmt'] ) ||
 			( isset( $request['date'] ) && '' === $request['date'] )
