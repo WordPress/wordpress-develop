91f60afbe51feb939144aa06d37017a4b6e1a516
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-08-19 21:05:42 +0000

    Date/Time: Use PHP `DateTime` class API in `current_time()`.
    Only use the legacy WP timestamp approach (a sum of timestamp and timezone offset) for `timestamp` and `U` formats without the `$gmt` flag.
    
    Otherwise, make sure the function returns correct local time for any format.
    
    Props Rarst, jdgrimes.
    Fixes #40653.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@45856 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/date/currentTime.php
+++ tests/phpunit/tests/date/currentTime.php
@@ -6,8 +6,10 @@
  */
 class Tests_Date_CurrentTime extends WP_UnitTestCase {
 
+	/**
+	 * @ticket 37440
+	 */
 	public function test_should_work_with_changed_timezone() {
-
 		$format          = 'Y-m-d H:i:s';
 		$timezone_string = 'America/Regina';
 		update_option( 'timezone_string', $timezone_string );
@@ -20,5 +22,37 @@ class Tests_Date_CurrentTime extends WP_UnitTestCase {
 		date_default_timezone_set( 'UTC' );
 		$this->assertEquals( gmdate( $format ), current_time( $format, true ) );
 		$this->assertEquals( $datetime->format( $format ), current_time( $format ) );
+	}
+
+	/**
+	 * @ticket 40653
+	 */
+	public function test_should_return_wp_timestamp() {
+		update_option( 'timezone_string', 'Europe/Kiev' );
+		$timestamp = time();
+		$datetime  = new DateTime( '@' . $timestamp );
+		$datetime->setTimezone( wp_timezone() );
+		$wp_timestamp = $timestamp + $datetime->getOffset();
+
+		$this->assertEquals( $timestamp, current_time( 'timestamp', true ), '', 2 );
+		$this->assertEquals( $timestamp, current_time( 'U', true ), '', 2 );
+		$this->assertEquals( $wp_timestamp, current_time( 'timestamp' ), '', 2 );
+		$this->assertEquals( $wp_timestamp, current_time( 'U' ), '', 2 );
+		$this->assertInternalType( 'int', current_time( 'timestamp' ) );
+	}
+
+	/**
+	 * @ticket 40653
+	 */
+	public function test_should_return_correct_local_time() {
+		update_option( 'timezone_string', 'Europe/Kiev' );
+		$timestamp      = time();
+		$datetime_local = new DateTime( '@' . $timestamp );
+		$datetime_local->setTimezone( wp_timezone() );
+		$datetime_utc = new DateTime( '@' . $timestamp );
+		$datetime_utc->setTimezone( new DateTimeZone( 'UTC' ) );
+
+		$this->assertEquals( $datetime_local->format( DATE_W3C ), current_time( DATE_W3C ), '', 2 );
+		$this->assertEquals( $datetime_utc->format( DATE_W3C ), current_time( DATE_W3C, true ), '', 2 );
 	}
 }
