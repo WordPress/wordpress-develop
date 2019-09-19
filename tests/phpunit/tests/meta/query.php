6eabc83b81100cffd74d201a06ea0bc299bd1f5e
Boone Gorges <boonebgorges@git.wordpress.org>
2019-09-19 15:02:20 +0000

    Query: Expand the list of operators available to `compare_key` in `WP_Meta_Query`.
    `compare_key`, introduced in #42409, previously supported only `=` and `LIKE`
    operators. This changeset introduces a number of other operators: `!=`, `IN`,
    `NOT IN`, `NOT LIKE`, `RLIKE`, `REGEXP`, `NOT REGEXP`, `EXISTS`, and `NOT EXISTS`
    (the latter two aliased to `=` and `!=`, respectively). To support case-sensitive
    regular expression key comparisons, the new `type_key` parameter will force
    a MySQL `CAST` when 'BINARY' is passed.
    
    Props soulseekah.
    Fixes #43346.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@46188 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/meta/query.php
+++ tests/phpunit/tests/meta/query.php
@@ -738,6 +738,42 @@ class Tests_Meta_Query extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Verifies only that meta_type_key is passed. See query/metaQuery.php for more complete tests.
+	 *
+	 * @ticket 43446
+	 */
+	public function test_meta_type_key_should_be_passed_to_meta_query() {
+		$posts = self::factory()->post->create_many( 3 );
+
+		add_post_meta( $posts[0], 'AAA_FOO_AAA', 'abc' );
+		add_post_meta( $posts[1], 'aaa_bar_aaa', 'abc' );
+		add_post_meta( $posts[2], 'aaa_foo_bbb', 'abc' );
+		add_post_meta( $posts[2], 'aaa_foo_aaa', 'abc' );
+
+		$q = new WP_Query(
+			array(
+				'meta_key'         => 'AAA_foo_.*',
+				'meta_compare_key' => 'REGEXP',
+				'fields'           => 'ids',
+			)
+		);
+
+		$this->assertEqualSets( array( $posts[0], $posts[2] ), $q->posts );
+
+		$q = new WP_Query(
+			array(
+				'meta_key'         => 'AAA_FOO_.*',
+				'meta_compare_key' => 'REGEXP',
+				'meta_type_key'    => 'BINARY',
+				'fields'           => 'ids',
+				'fields'           => 'ids',
+			)
+		);
+
+		$this->assertEqualSets( array( $posts[0] ), $q->posts );
+	}
+
+	/**
 	 * This is the clause that ensures that empty arrays are not valid queries.
 	 */
 	public function test_get_sql_null_value_and_empty_key_should_not_have_table_join() {
