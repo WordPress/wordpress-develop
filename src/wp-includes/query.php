a3fa06f74a481f093b4dbf51fd572a9bb82d0577
Micah Wood <micah@wpscholar.com>
2019-10-01 14:34:33 -0400

    Initial implementation of wp_loop() function.

--- src/wp-includes/query.php
+++ src/wp-includes/query.php
@@ -1161,3 +1161,50 @@ function generate_postdata( $post ) {
 
 	return false;
 }
+
+/**
+ * Simplifies the WordPress loop.
+ *
+ * @param WP_Query|WP_Post[] $iterable
+ *
+ * @return Generator
+ */
+function wp_loop( $iterable = null ) {
+
+	if ( null === $iterable ) {
+		$iterable = $GLOBALS['wp_query'];
+	}
+
+	$posts = $iterable;
+
+	if ( is_object( $iterable ) && property_exists( $iterable, 'posts' ) ) {
+		$posts = $iterable->posts;
+	}
+
+	if ( ! is_array( $posts ) ) {
+		/* translators: Data type */
+		_doing_it_wrong( __FUNCTION__, sprintf( __( 'Expected an array, received %s instead.' ), gettype( $posts ) ), '3.1.0' );
+
+		return false;
+	}
+
+	global $post;
+
+	// Save the global post object so we can restore it later
+	$save_post = $post;
+
+	try {
+		foreach ( $posts as $post ) {
+
+			setup_postdata( $post );
+			yield $post;
+
+		}
+	} finally {
+
+		wp_reset_postdata();
+		// Restore the global post object
+		$post = $save_post;
+
+	}
+}
