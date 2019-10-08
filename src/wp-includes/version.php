1e1ef432b4f683f772c6be4290cf32f26ff33240
Adam Silverstein <adamsilverstein@git.wordpress.org>
2019-10-08 19:57:09 +0000

    WordPress 5.3 Beta 3.
    git-svn-id: https://develop.svn.wordpress.org/trunk@46448 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta2-46366-src';
+$wp_version = '5.3-beta3-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
