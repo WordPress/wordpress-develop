040304def7fc6b5eb73878a8e5d37d872163b8aa
K. Adam White <kadamwhite@git.wordpress.org>
2019-10-08 04:43:10 +0000

    REST API: Permit embedding of the 'self' link relation in the /search endpoint.
    Removes a special-case prohibition against embedding 'self' which prevented ?_embed from being used with the /wp/v2/search endpoint.
    
    Props TimothyBlynJacobs, chrisvanpatten, kadamwhite.
    Fixes #47684.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@46434 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/rest-api/rest-posts-controller.php
+++ tests/phpunit/tests/rest-api/rest-posts-controller.php
@@ -1363,6 +1363,7 @@ class WP_Test_REST_Posts_Controller extends WP_Test_REST_Post_Type_Controller_Te
 
 		$this->assertEquals( rest_url( '/wp/v2/posts/' . self::$post_id ), $links['self'][0]['href'] );
 		$this->assertEquals( rest_url( '/wp/v2/posts' ), $links['collection'][0]['href'] );
+		$this->assertArrayNotHasKey( 'embeddable', $links['self'][0]['attributes'] );
 
 		$this->assertEquals( rest_url( '/wp/v2/types/' . get_post_type( self::$post_id ) ), $links['about'][0]['href'] );
 
