3f12403dc8edb5621e7ccbbc3e1ce32c792cc66e
Jake Spurlock <whyisjake@git.wordpress.org>
2019-10-14 19:08:52 +0000

    Backporting several bug fixes.
    - Query: Remove the static query property.
    - HTTP API: Protect against hex interpretation.
    - Filesystem API: Prevent directory travelersals when creating new folders.
    - Administration: Ensure that admin referer nonce is valid.
    - REST API: Send a Vary: Origin header on GET requests.
    - Customizer: Properly sanitize background images.
    
    Backports [46474], [46475], [46476], [46477], [46478], [46483], [46485] to the 4.4 branch.
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/4.4@46498 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/auth.php
+++ tests/phpunit/tests/auth.php
@@ -141,6 +141,15 @@ class Tests_Auth extends WP_UnitTestCase {
 		$this->assertEquals( ( $count + 1 ), did_action( $this->nonce_failure_hook ) );
 	}
 
+	public function test_check_admin_referer_with_default_action_as_string_not_doing_it_wrong() {
+		// A valid nonce needs to be set so the check doesn't die()
+		$_REQUEST['_wpnonce'] = wp_create_nonce( '-1' );
+		$result               = check_admin_referer( '-1' );
+		$this->assertSame( 1, $result );
+
+		unset( $_REQUEST['_wpnonce'] );
+	}
+
 	/**
 	 * @ticket 24030
 	 */
@@ -151,6 +160,34 @@ class Tests_Auth extends WP_UnitTestCase {
 		wp_verify_nonce( $nonce, 'nonce_test_action' );
 
 		$this->assertEquals( $count, did_action( $this->nonce_failure_hook ) );
+	}
+
+	/**
+	 * @ticket 36361
+	 */
+	public function test_check_admin_referer_with_no_action_triggers_doing_it_wrong() {
+		$this->setExpectedIncorrectUsage( 'check_admin_referer' );
+
+		// A valid nonce needs to be set so the check doesn't die()
+		$_REQUEST['_wpnonce'] = wp_create_nonce( -1 );
+		$result = check_admin_referer();
+		$this->assertSame( 1, $result );
+
+		unset( $_REQUEST['_wpnonce'] );
+	}
+
+	/**
+	 * @ticket 36361
+	 */
+	public function test_check_ajax_referer_with_no_action_triggers_doing_it_wrong() {
+		$this->setExpectedIncorrectUsage( 'check_ajax_referer' );
+
+		// A valid nonce needs to be set so the check doesn't die()
+		$_REQUEST['_wpnonce'] = wp_create_nonce( -1 );
+		$result = check_ajax_referer();
+		$this->assertSame( 1, $result );
+
+		unset( $_REQUEST['_wpnonce'] );
 	}
 
 	function test_password_length_limit() {
