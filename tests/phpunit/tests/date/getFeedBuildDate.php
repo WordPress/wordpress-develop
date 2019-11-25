a8a6a1fc00f24555d8e0214e34a8fc09b498089e
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-11-25 10:10:31 +0000

    Date/Time: Remove `mysql2date()` usage in `get_feed_build_date()` to ensure the output includes correct timezone offset.
    With the changes in [45908], `mysql2date()` works correctly for all local time inputs, but should not be used for UTC time inputs.
    
    Add a unit test.
    
    Props Rarst, lisota.
    Merges [46756] and [46757] to the 5.3 branch.
    Fixes #48675.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.3@46774 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/date/getFeedBuildDate.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @group date
+ * @group datetime
+ */
+class Tests_Date_Get_Feed_Build_Date extends WP_UnitTestCase {
+
+	function tearDown() {
+		global $wp_query;
+
+		update_option( 'timezone_string', 'UTC' );
+
+		unset( $wp_query );
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @ticket 48675
+	 */
+	public function test_should_return_correct_feed_build_date() {
+		global $wp_query;
+
+		$timezone = 'America/Chicago';
+		update_option( 'timezone_string', $timezone );
+
+		$post_id = self::factory()->post->create(
+			array(
+				'post_date'     => '2018-07-22 21:13:23',
+				'post_date_gmt' => '2018-07-23 03:13:23',
+			)
+		);
+
+		$wp_query = new WP_Query( array( 'p' => $post_id ) );
+
+		$this->assertEquals( '2018-07-23T03:13:23+00:00', get_feed_build_date( DATE_RFC3339 ) );
+	}
+}
