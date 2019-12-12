e9e95c33cd03830d4ef430b5e2ba9048cab770ec
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-12-12 20:29:37 +0000

    WordPress 4.5.20
    git-svn-id: https://develop.svn.wordpress.org/branches/4.5@46928 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5.19-src';
+$wp_version = '4.5.20-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
