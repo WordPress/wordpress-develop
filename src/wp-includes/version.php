77630d2f49df7ca83bb92bbd0e40272e12d7bdf5
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-12-17 22:09:19 +0000

    WordPress 5.3.2 RC1
    git-svn-id: https://develop.svn.wordpress.org/branches/5.3@46983 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.2-alpha-46956-src';
+$wp_version = '5.3.2-RC1-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
