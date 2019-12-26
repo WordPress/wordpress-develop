091c577662c9315048e5d14ad67a9c3ddc46abb6
Adam Silverstein <adamjs@google.com>
2019-12-26 16:05:00 -0700

    Media: ensire `item` is available before setting skipHistory on load.

--- src/js/media/routers/manage.js
+++ src/js/media/routers/manage.js
@@ -43,12 +43,13 @@ var Router = Backbone.Router.extend(/** @lends wp.media.view.MediaFrame.Manage.R
 
 		// Trigger the media frame to open the correct item
 		item = library.findWhere( { id: parseInt( query, 10 ) } );
-		item.set( 'skipHistory', true );
 
 		if ( item ) {
+			item.set( 'skipHistory', true );
 			frame.trigger( 'edit:attachment', item );
 		} else {
 			item = media.attachment( query );
+			item.set( 'skipHistory', true );
 			frame.listenTo( item, 'change', function( model ) {
 				frame.stopListening( item );
 				frame.trigger( 'edit:attachment', model );
