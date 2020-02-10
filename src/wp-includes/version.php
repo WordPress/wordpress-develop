c80af9030d68c909f34795c29ead09b5698f22ac
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2020-02-10 03:28:20 +0000

    Docs: Add descriptions for some globals:
    * `$wp_version`
    * `$wp_local_package`
    * `$required_php_version`
    * `$required_mysql_version`
    
    See #48303.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@47230 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -9,7 +9,7 @@
  */
 
 /**
- * The WordPress version string
+ * The WordPress version string.
  *
  * @global string $wp_version
  */
@@ -23,21 +23,21 @@ $wp_version = '5.4-alpha-46582-src';
 $wp_db_version = 47018;
 
 /**
- * Holds the TinyMCE version
+ * Holds the TinyMCE version.
  *
  * @global string $tinymce_version
  */
 $tinymce_version = '4960-20190918';
 
 /**
- * Holds the required PHP version
+ * Holds the required PHP version.
  *
  * @global string $required_php_version
  */
 $required_php_version = '5.6.20';
 
 /**
- * Holds the required MySQL version
+ * Holds the required MySQL version.
  *
  * @global string $required_mysql_version
  */
