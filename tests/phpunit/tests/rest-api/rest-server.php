d3cb4635a8ef9ba418f7c38049c0a1a86ca20db3
Timothy Jacobs <timothy@ironbounddesigns.com>
2020-02-19 16:56:06 -0500

    REST API: Fix routing when one namespaces is the prefix of another.
    While not recommended, a plugin may register routes in both the "test" prefix and the "test/v1" prefix. After r47260 routes registered in the latter prefix would not be discoverable.
    
    The REST API now includes routes from all possible namespaces instead of stopping at the first match.
    
    Fixes #48530.
    Props david.binda.

--- tests/phpunit/tests/rest-api/rest-server.php
+++ tests/phpunit/tests/rest-api/rest-server.php
@@ -1442,6 +1442,37 @@ class Tests_REST_Server extends WP_Test_REST_TestCase {
 		}
 	}
 
+	/**
+	 * @ticket 48530
+	 */
+	public function test_get_routes_no_namespace_overriding() {
+		register_rest_route(
+			'test-ns',
+			'/test',
+			array(
+				'methods'  => array( 'GET' ),
+				'callback' => function() {
+					return new WP_REST_Response( 'data', 204 );
+				},
+			)
+		);
+		register_rest_route(
+			'test-ns/v1',
+			'/test',
+			array(
+				'methods'  => array( 'GET' ),
+				'callback' => function() {
+					return new WP_REST_Response( 'data', 204 );
+				},
+			)
+		);
+
+		$request  = new WP_REST_Request( 'GET', '/test-ns/v1/test' );
+		$response = rest_get_server()->dispatch( $request );
+
+		$this->assertEquals( 204, $response->get_status(), '/test-ns/v1/test' );
+	}
+
 	public function _validate_as_integer_123( $value, $request, $key ) {
 		if ( ! is_int( $value ) ) {
 			return new WP_Error( 'some-error', 'This is not valid!' );
