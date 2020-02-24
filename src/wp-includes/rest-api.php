eacf918fc195371142bdf529eb8ccb5b85ce9e37
Dominik Schilling <dominikschilling+git@gmail.com>
2020-02-24 17:31:41 +0100

    Ensure rest_ensure_response() upgrades WP_HTTP_Response to WP_REST_Response

--- src/wp-includes/rest-api.php
+++ src/wp-includes/rest-api.php
@@ -506,15 +506,15 @@ function rest_ensure_request( $request ) {
 /**
  * Ensures a REST response is a response object (for consistency).
  *
- * This implements WP_HTTP_Response, allowing usage of `set_status`/`header`/etc
+ * This implements WP_REST_Response, allowing usage of `set_status`/`header`/etc
  * without needing to double-check the object. Will also allow WP_Error to indicate error
  * responses, so users should immediately check for this value.
  *
  * @since 4.4.0
  *
- * @param WP_HTTP_Response|WP_Error|mixed $response Response to check.
+ * @param WP_REST_Response|WP_Error|WP_HTTP_Response|mixed $response Response to check.
  * @return WP_REST_Response|mixed If response generated an error, WP_Error, if response
- *                                is already an instance, WP_HTTP_Response, otherwise
+ *                                is already an instance, WP_REST_Response, otherwise
  *                                returns a new WP_REST_Response instance.
  */
 function rest_ensure_response( $response ) {
@@ -522,8 +522,16 @@ function rest_ensure_response( $response ) {
 		return $response;
 	}
 
-	if ( $response instanceof WP_HTTP_Response ) {
+	if ( $response instanceof WP_REST_Response ) {
 		return $response;
+	}
+
+	if ( $response instanceof WP_HTTP_Response ) {
+		return new WP_REST_Response(
+			$response->get_data(),
+			$response->get_status(),
+			$response->get_headers()
+		);
 	}
 
 	return new WP_REST_Response( $response );
