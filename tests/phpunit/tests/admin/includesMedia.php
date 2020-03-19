5cc9c05577bb3eeca1f3b0227f17fa4e5e3c8db6
Luigi Guevara <luigi.fox15@gmail.com>
2020-03-19 23:05:24 +0100

    Improving the way of getting external files and mocking the process

--- tests/phpunit/tests/admin/includesMedia.php
+++ tests/phpunit/tests/admin/includesMedia.php
@@ -2,6 +2,7 @@
 
 /**
  * @group media
+ * @group k99
  * @group admin
  */
 class Tests_Admin_includesMedia extends WP_UnitTestCase {
@@ -15,6 +16,45 @@ class Tests_Admin_includesMedia extends WP_UnitTestCase {
 		self::$different_post = $factory->post->create_and_get();
 	}
 
+	public function wpTearDownAfterClass() {
+		self::$post           = null;
+		self::$different_post = null;
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		add_filter( 'pre_http_request', [ $this, 'internal_fake_download_url' ], 10, 3 );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+
+		$this->remove_added_uploads();
+		remove_filter( 'pre_http_request', [ $this, 'internal_fake_download_url' ] );
+	}
+
+	/**
+	 * Filters whether to preempt an HTTP request's return value. Mocking the
+	 * response. Since ticket 49631
+	 *
+	 * @param false  $preempt Whether to preempt an HTTP request's return value. Default false.
+	 * @param array  $parsed_args HTTP request arguments.
+	 * @param string $url The request URL.
+	 *
+	 * @return array
+	 */
+	public function internal_fake_download_url( $preempt, array $parsed_args, $url = '' ) {
+		// Just need an image content string to fill the new image, keeping the same mime type.
+		file_put_contents( $parsed_args['filename'], file_get_contents( DIR_TESTDATA . '/images/canola.jpg' ) );
+
+		return [
+			'response' => [
+				'code' => 200,
+			],
+		];
+	}
+
 	/**
 	 * @ticket 49631
 	 */
@@ -22,8 +62,8 @@ class Tests_Admin_includesMedia extends WP_UnitTestCase {
 		$year  = gmdate( 'Y' );
 		$month = gmdate( 'm' );
 
-		$image_source = 'https://raw.githubusercontent.com/WordPress/wordpress-develop/master/tests/phpunit/data/images/canola.jpg';
-		$image        = 'http://' . WP_TESTS_DOMAIN . "/wp-content/uploads/{$year}/{$month}/canola.jpg";
+		$image_source = 'http://' . WP_TESTS_DOMAIN . '/external/source/image1.jpg';
+		$image        = 'http://' . WP_TESTS_DOMAIN . "/wp-content/uploads/{$year}/{$month}/image1.jpg";
 		$image_html   = addslashes( $image );
 
 		$media_html = media_sideload_image( $image_source, self::$post->ID );
@@ -31,14 +71,14 @@ class Tests_Admin_includesMedia extends WP_UnitTestCase {
 		$this->assertNotWPError( $media_html );
 		$this->assertRegExp( "~<img src='$image_html' alt='' \/>~", $media_html );
 
-		$image_source = 'https://raw.githubusercontent.com/WordPress/wordpress-develop/master/tests/phpunit/data/images/test-image.jpg';
-		$image        = 'http://' . WP_TESTS_DOMAIN . "/wp-content/uploads/{$year}/{$month}/test-image.jpg";
+		$image_source = 'http://' . WP_TESTS_DOMAIN . '/external/source/image2.jpg';
+		$image        = 'http://' . WP_TESTS_DOMAIN . "/wp-content/uploads/{$year}/{$month}/image2.jpg";
 
 		$media_src = media_sideload_image( $image_source, self::$different_post->ID, null, 'src' );
 
 		$this->assertEquals( $image, $media_src );
 
-		$image_source = 'https://raw.githubusercontent.com/WordPress/wordpress-develop/master/tests/phpunit/data/images/test-image-large.png';
+		$image_source = 'http://' . WP_TESTS_DOMAIN . '/external/source/image3.jpg';
 
 		$media_id                   = media_sideload_image( $image_source, self::$different_post->ID, null, 'id' );
 		$attachment_meta_source_url = get_post_meta( $media_id, '_source_url', true );
