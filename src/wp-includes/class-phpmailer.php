936502dd8c3fb8d791ced4fff6654919f931c857
Jonathan Desrosiers <desrosj@gmail.com>
2020-04-28 16:58:50 -0400

    Use class aliasing instead of a full class.

--- src/wp-includes/class-phpmailer.php
+++ src/wp-includes/class-phpmailer.php
@@ -6,27 +6,4 @@
 _deprecated_file( basename( __FILE__ ), '5.5.0', WPINC . '/PHPMailer/PHPMailer.php', __( 'The PHPMailer class has been moved to wp-includes/PHPMailer subdirectory and now uses the PHPMailer\PHPMailer namespace.' ) );
 include __DIR__ .'/PHPMailer/PHPMailer.php';
 
-/**
- * Class PHPMailer.
- *
- * Use PHPMailer\PHPMailer\PHPMailer instead.
- *
- * @since 2.2.0
- * @deprecated 5.5.0
- */
-class PHPMailer {
-	/**
-	 * Constructor.
-	 *
-	 * @deprecated 5.5.0
-	 *
-	 * @param boolean $exceptions Should we throw external exceptions?
-	 * @return PHPMailer\PHPMailer\PHPMailer An instance of the new, correctly namespaced PHPMailer.
-	 */
-	public function __construct( $exceptions = null )
-	{
-		_deprecated_function( __METHOD__, '5.5', 'PHPMailer\PHPMailer\PHPMailer::construct()' );
-
-		return new PHPMailer\PHPMailer\PHPMailer( $exceptions );
-	}
-}
+class_alias( 'PHPMailer\PHPMailer\PHPMailer', 'PHPMailer' );
