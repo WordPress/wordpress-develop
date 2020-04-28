c1c293a27739ee537f998480d70e63fb8ac0b532
Ryan Kienstra <kienstraryan@gmail.com>
2020-04-27 22:35:39 -0500

    Move the add_header() call, in an effort to fix failed test

--- tests/phpunit/tests/rest-api/rest-block-renderer-controller.php
+++ tests/phpunit/tests/rest-api/rest-block-renderer-controller.php
@@ -465,8 +465,8 @@ class REST_Block_Renderer_Controller_Test extends WP_Test_REST_Controller_Testca
 		$string_attribute = 'Lorem ipsum dolor';
 		$attributes       = array( 'some_string' => $string_attribute );
 		$request          = new WP_REST_Request( 'POST', self::$rest_api_route . self::$block_name );
-		$request->set_header( 'content-type', 'application/json' );
 		$request->set_param( 'context', 'edit' );
+		$request->set_header( 'content-type', 'application/json' );
 		$request->set_body( wp_json_encode( compact( 'attributes' ) ) );
 		$response = rest_get_server()->dispatch( $request );
 
