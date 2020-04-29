46ad68acedf559e6adaaa9fdf6d92b1a0945838f
Jonathan Desrosiers <desrosj@git.wordpress.org>
2020-04-29 17:57:58 +0000

    WordPress 3.7.33
    git-svn-id: https://develop.svn.wordpress.org/branches/3.7@47683 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.7.32-src';
+$wp_version = '3.7.33-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
