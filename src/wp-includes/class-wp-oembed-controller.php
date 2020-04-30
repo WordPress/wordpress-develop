c5e004e35d3077ccf9d3e451cc760474f98911ea
Dion Hulse <wordpress@dd32.id.au>
2020-04-30 11:17:57 +1000

    Update definition as per TimothyBlynJacobs

--- src/wp-includes/class-wp-oembed-controller.php
+++ src/wp-includes/class-wp-oembed-controller.php
@@ -40,10 +40,10 @@ final class WP_oEmbed_Controller {
 					'callback' => array( $this, 'get_item' ),
 					'args'     => array(
 						'url'      => array(
-							'required'          => true,
-							'type'              => 'string',
-							'validate_callback' => 'rest_validate_request_arg',
-							'sanitize_callback' => 'esc_url_raw',
+							'description' => __( 'The URL of the resource for which to fetch oEmbed data.' ),
+							'required'    => true,
+							'type'        => 'string',
+							'format'      => 'uri',
 						),
 						'format'   => array(
 							'default'           => 'json',
@@ -68,10 +68,10 @@ final class WP_oEmbed_Controller {
 					'permission_callback' => array( $this, 'get_proxy_item_permissions_check' ),
 					'args'                => array(
 						'url'       => array(
-							'description'       => __( 'The URL of the resource for which to fetch oEmbed data.' ),
-							'type'              => 'string',
-							'required'          => true,
-							'sanitize_callback' => 'esc_url_raw',
+							'description' => __( 'The URL of the resource for which to fetch oEmbed data.' ),
+							'required'    => true,
+							'type'        => 'string',
+							'format'      => 'uri',
 						),
 						'format'    => array(
 							'description' => __( 'The oEmbed format to use.' ),
