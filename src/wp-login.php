6b933aa8459bfed2824e111b26017d2623e7ad2f
John Blackbourn <johnbillion@gmail.com>
2020-05-03 21:10:45 +0100

    Add the language parameter to the URL in the password reset email.

--- src/wp-login.php
+++ src/wp-login.php
@@ -477,6 +477,16 @@ function retrieve_password() {
 		$site_name = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
 	}
 
+	$reset_url = add_query_arg(
+		array(
+			'action'  => 'rp',
+			'key'     => $key,
+			'login'   => rawurlencode( $user_login ),
+			'wp_lang' => $user_locale,
+		),
+		network_site_url( 'wp-login.php', 'login' )
+	);
+
 	$message = __( 'Someone has requested a password reset for the following account:' ) . "\r\n\r\n";
 	/* translators: %s: Site name. */
 	$message .= sprintf( __( 'Site Name: %s' ), $site_name ) . "\r\n\r\n";
@@ -484,7 +494,7 @@ function retrieve_password() {
 	$message .= sprintf( __( 'Username: %s' ), $user_login ) . "\r\n\r\n";
 	$message .= __( 'If this was a mistake, just ignore this email and nothing will happen.' ) . "\r\n\r\n";
 	$message .= __( 'To reset your password, visit the following address:' ) . "\r\n\r\n";
-	$message .= network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' ) . "\r\n";
+	$message .= $reset_url . "\r\n";
 
 	/* translators: Password reset notification email subject. %s: Site title. */
 	$title = sprintf( __( '[%s] Password Reset' ), $site_name );
