b1eb541ce84377e5d57534f1aaaf1c1f70e92372
Dominik Schilling <ocean90@git.wordpress.org>
2020-06-01 19:41:50 +0000

    Build/Test Tools: Let `WP_UnitTestCase_Base` extend the namespaced version of PHPUnit's test case class.
    With the minimum PHP version requirement set to 5.6 we're now able to use namespaces by default. This replaces `PHPUnit_Framework_TestCase` with `PHPUnit\Framework\TestCase` for `WP_UnitTestCase_Base` to solve autocompletion issues with code editors when using more recent PHPUnit versions. `PHPUnit\Framework\TestCase` is available since PHPUnit 5.4 and now the minimum required PHPUnit version.
    
    Fixes #50236.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@47880 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/includes/functions.php
+++ tests/phpunit/includes/functions.php
@@ -10,8 +10,7 @@ function tests_get_phpunit_version() {
 	if ( class_exists( 'PHPUnit_Runner_Version' ) ) {
 		$version = PHPUnit_Runner_Version::id();
 	} elseif ( class_exists( 'PHPUnit\Runner\Version' ) ) {
-		// Must be parsable by PHP 5.2.x.
-		$version = call_user_func( 'PHPUnit\Runner\Version::id' );
+		$version = PHPUnit\Runner\Version::id();
 	} else {
 		$version = 0;
 	}
