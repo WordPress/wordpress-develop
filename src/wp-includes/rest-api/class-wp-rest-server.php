d974ba7fc52efd463630d450c6f444cf9bfb1961
jrfnl <jrfnl@users.noreply.github.com>
2020-06-02 05:14:59 +0200

    49810: proposed changes related to removed $HTTP_RAW_POST_DATA variable

--- src/wp-includes/rest-api/class-wp-rest-server.php
+++ src/wp-includes/rest-api/class-wp-rest-server.php
@@ -1366,22 +1366,10 @@ class WP_REST_Server {
 	 *
 	 * @since 4.4.0
 	 *
-	 * @global string $HTTP_RAW_POST_DATA Raw post data.
-	 *
 	 * @return string Raw request data.
 	 */
 	public static function get_raw_data() {
-		global $HTTP_RAW_POST_DATA;
-
-		/*
-		 * A bug in PHP < 5.2.2 makes $HTTP_RAW_POST_DATA not set by default,
-		 * but we can do it ourself.
-		 */
-		if ( ! isset( $HTTP_RAW_POST_DATA ) ) {
-			$HTTP_RAW_POST_DATA = file_get_contents( 'php://input' );
-		}
-
-		return $HTTP_RAW_POST_DATA;
+		return file_get_contents( 'php://input' );
 	}
 
 	/**
