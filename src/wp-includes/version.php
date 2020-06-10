b7efcf9751409b07c7de64b061a06fb9af208818
Jonathan Desrosiers <desrosj@git.wordpress.org>
2020-06-10 22:48:37 +0000

    Post WordPress 5.4.2 version bump.
    git-svn-id: https://develop.svn.wordpress.org/branches/5.4@48025 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4.2-src';
+$wp_version = '5.4.3-alpha-48025-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
