275ee33352a7e1a72c015ffe0e64c5af930c8b90
Adam Silverstein <adamsilverstein@git.wordpress.org>
2020-06-18 20:59:43 +0000

    Formatting: new filter `safecss_filter_attr_allow_css` on css parts.
    Enables developers to determine whether a section of CSS should be allowed or discarded. By default, the value will be false if the part contains \ ( & } = or comments. Returning true allows the CSS part to be included in the output.
    
    Replaces the `safe_style_disallowed_chars` filter introduced in r47891.
    
    Props azaozz.
    Fixes #37134.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@48086 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/kses.php
+++ tests/phpunit/tests/kses.php
@@ -1263,14 +1263,7 @@ EOF;
 	}
 
 	/**
-	 * Filter for disallowed characters never matches thus allowing all characters.
-	 */
-	function _safe_style_disallowed_chars_filter( $regex ) {
-		return '%a^%'; // Regex with no matches.
-
-	}
-	/**
-	 * Testing the safecss_filter_attr() function with the safe_style_disallowed_chars filter.
+	 * Testing the safecss_filter_attr() function with the safecss_filter_attr_allow_css filter.
 	 *
 	 * @ticket 37134
 	 *
@@ -1280,9 +1273,9 @@ EOF;
 	 * @param string $expected Expected string of CSS rules.
 	 */
 	public function test_safecss_filter_attr_filtered( $css, $expected ) {
-		add_filter( 'safe_style_disallowed_chars', array( $this, '_safe_style_disallowed_chars_filter' ) );
+		add_filter( 'safecss_filter_attr_allow_css', '__return_true' );
 		$this->assertSame( $expected, safecss_filter_attr( $css ) );
-		remove_filter( 'safe_style_disallowed_chars', array( $this, '_safe_style_disallowed_chars_filter' ) );
+		remove_filter( 'safecss_filter_attr_allow_css', '__return_true' );
 	}
 
 	/**
@@ -1303,37 +1296,37 @@ EOF;
 				'css'      => 'margin-top: 2px',
 				'expected' => 'margin-top: 2px',
 			),
-			// Backslash \ can be allowed with the 'safe_style_disallowed_chars' filter.
+			// Backslash \ can be allowed with the 'safecss_filter_attr_allow_css' filter.
 			array(
 				'css'      => 'margin-top: \2px',
 				'expected' => 'margin-top: \2px',
 			),
-			// Curly bracket } can be allowed with the 'safe_style_disallowed_chars' filter.
+			// Curly bracket } can be allowed with the 'safecss_filter_attr_allow_css' filter.
 			array(
 				'css'      => 'margin-bottom: 2px}',
 				'expected' => 'margin-bottom: 2px}',
 			),
-			// Parenthesis ) can be allowed with the 'safe_style_disallowed_chars' filter.
+			// Parenthesis ) can be allowed with the 'safecss_filter_attr_allow_css' filter.
 			array(
 				'css'      => 'margin-bottom: 2px)',
 				'expected' => 'margin-bottom: 2px)',
 			),
-			// Ampersand & can be allowed with the 'safe_style_disallowed_chars' filter.
+			// Ampersand & can be allowed with the 'safecss_filter_attr_allow_css' filter.
 			array(
 				'css'      => 'margin-bottom: 2px&',
 				'expected' => 'margin-bottom: 2px&',
 			),
-			// Expressions can be allowed with the 'safe_style_disallowed_chars' filter.
+			// Expressions can be allowed with the 'safecss_filter_attr_allow_css' filter.
 			array(
 				'css'      => 'height: expression( body.scrollTop + 50 + "px" )',
 				'expected' => 'height: expression( body.scrollTop + 50 + "px" )',
 			),
-			// RGB color values can be allowed with the 'safe_style_disallowed_chars' filter.
+			// RGB color values can be allowed with the 'safecss_filter_attr_allow_css' filter.
 			array(
 				'css'      => 'color: rgb( 100, 100, 100 )',
 				'expected' => 'color: rgb( 100, 100, 100 )',
 			),
-			// RGBA color values can be allowed with the 'safe_style_disallowed_chars' filter.
+			// RGBA color values can be allowed with the 'safecss_filter_attr_allow_css' filter.
 			array(
 				'css'      => 'color: rgb( 100, 100, 100, .4 )',
 				'expected' => 'color: rgb( 100, 100, 100, .4 )',
