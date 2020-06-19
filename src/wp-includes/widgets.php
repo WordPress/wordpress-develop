bc847f3090afe3708ee21aa1f3b17f102ccef2fd
Jonathan Desrosiers <desrosj@gmail.com>
2020-06-19 12:22:10 -0400

    Fix widget typo.

--- src/wp-includes/widgets.php
+++ src/wp-includes/widgets.php
@@ -1462,17 +1462,17 @@ function wp_map_sidebars_widgets( $existing_sidebars_widgets ) {
  * @since 4.9.0
  *
  * @param array $sidebars_widgets  List of sidebars and their widget instance IDs.
- * @param array $allowed_wiget_ids Optional. List of widget IDs to compare against. Default: Registered widgets.
+ * @param array $allowed_widget_ids Optional. List of widget IDs to compare against. Default: Registered widgets.
  * @return array Sidebars with allowed widgets.
  */
-function _wp_remove_unregistered_widgets( $sidebars_widgets, $allowed_wiget_ids = array() ) {
-	if ( empty( $allowed_wiget_ids ) ) {
-		$allowed_wiget_ids = array_keys( $GLOBALS['wp_registered_widgets'] );
+function _wp_remove_unregistered_widgets( $sidebars_widgets, $allowed_widget_ids = array() ) {
+	if ( empty( $allowed_widget_ids ) ) {
+		$allowed_widget_ids = array_keys( $GLOBALS['wp_registered_widgets'] );
 	}
 
 	foreach ( $sidebars_widgets as $sidebar => $widgets ) {
 		if ( is_array( $widgets ) ) {
-			$sidebars_widgets[ $sidebar ] = array_intersect( $widgets, $allowed_wiget_ids );
+			$sidebars_widgets[ $sidebar ] = array_intersect( $widgets, $allowed_widget_ids );
 		}
 	}
 
