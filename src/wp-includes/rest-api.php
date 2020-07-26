45c5aec34dbe955a354c68e3b40a1868b6c87c99
John R. D'Orazio <priest@johnromanodorazio.com>
2020-07-27 00:27:38 +0200

    add whitespacing in string concatenations

--- src/wp-includes/rest-api.php
+++ src/wp-includes/rest-api.php
@@ -42,8 +42,8 @@ function register_rest_route( $namespace, $route, $args = array(), $override = f
 			sprintf(
 				/* translators: 1: string value of the namespace, 2: string value of the route */
 				__( 'Routes must be namespaced with plugin or theme name and version. Instead there seems to be an empty namespace \'%1$s\' for route \'%2$s\'.' ),
-				'<code>'.$namespace.'</code>',
-				'<code>'.$route.'</code>'
+				'<code>' . $namespace . '</code>',
+				'<code>' . $route . '</code>'
 			),
 			'4.4.0'
 		);
@@ -54,8 +54,8 @@ function register_rest_route( $namespace, $route, $args = array(), $override = f
 			sprintf(
 				/* translators: 1: string value of the namespace, 2: string value of the route */
 				__( 'Route must be specified. Instead within the namespace \'%1$s\', there seems to be an empty route \'%2$s\'.' ),
-				'<code>'.$namespace.'</code>',
-				'<code>'.$route.'</code>'
+				'<code>' . $namespace . '</code>',
+				'<code>' . $route . '</code>'
 			),
 			'4.4.0'
 		);
@@ -70,8 +70,8 @@ function register_rest_route( $namespace, $route, $args = array(), $override = f
 			sprintf(
 				/* translators: 1: string value of the namespace, 2: string value of the route */
 				__( 'Namespace must not start or end with a slash. Instead namespace \'%1$s\' for route \'%2$s\' seems to contain a slash.' ),
-				'<code>'.$namespace.'</code>',
-				'<code>'.$route.'</code>'
+				'<code>' . $namespace . '</code>',
+				'<code>' . $route . '</code>'
 			),
 			'5.4.2'
 		);
@@ -84,8 +84,8 @@ function register_rest_route( $namespace, $route, $args = array(), $override = f
 				/* translators: 1: rest_api_init, 2: string value of the namespace, 3: string value of the route */
 				__( 'REST API routes must be registered on the %1$s action. Instead route \'%3$s\' with namespace \'%2$s\' was not registered on this action.' ),
 				'<code>rest_api_init</code>',
-				'<code>'.$namespace.'</code>',
-				'<code>'.$route.'</code>'
+				'<code>' . $namespace . '</code>',
+				'<code>' . $route . '</code>'
 			),
 			'5.1.0'
 		);
