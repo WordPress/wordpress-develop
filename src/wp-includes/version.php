70fcbf67dfb28b8d4fbadaeb33503a50f2e94a98
Jake Spurlock <whyisjake@git.wordpress.org>
2020-08-11 18:12:27 +0000

    WordPress 5.5
    git-svn-id: https://develop.svn.wordpress.org/branches/5.5@48785 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-RC3-48781-src';
+$wp_version = '5.5-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
