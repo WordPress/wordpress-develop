3c1b2c3dd0a3d525bbde816015265421c5a40bdb
Ari Stathopoulos <aristath@gmail.com>
2020-08-13 16:03:15 +0300

    Make sure the WPLANG option exists.

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -2203,6 +2203,20 @@ function upgrade_550() {
 }
 
 /**
+ * Executes changes made in WordPress 5.6.0.
+ *
+ * @ignore
+ * @since 5.6.0
+ */
+function upgrade_560() {
+
+	// Prevent an extra database query on each page-load if the WPLANG option doesn't exist.
+	if ( ! get_option( 'WPLANG' ) ) {
+		update_option( 'WPLANG', '' );
+	}
+}
+
+/**
  * Executes network-level upgrade routines.
  *
  * @since 3.0.0
