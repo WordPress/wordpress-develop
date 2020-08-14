ea5680ec51e0d12a4acabdc7b9c76c7de7898192
jrfnl <jrfnl@users.noreply.github.com>
2020-08-15 01:04:06 +0200

    Tests: use the namespaced PHPUnit `Exception` class
    Switch out uses of the `PHPUnit_Framework_Exception` class (alias) for the namespaced `PHPUnit\Framework\Exception` class.
    
    Includes adding import `use` statements to that effect where relevant.
    Includes removing a reference to the old class name from the PHPCS config file.

--- tests/phpunit/includes/exceptions.php
+++ tests/phpunit/includes/exceptions.php
@@ -1,6 +1,8 @@
 <?php
 
-class WP_Tests_Exception extends PHPUnit_Framework_Exception {
+use PHPUnit\Framework\Exception as PHPUnit_Exception;
+
+class WP_Tests_Exception extends PHPUnit_Exception {
 
 }
 
