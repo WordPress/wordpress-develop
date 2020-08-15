6ff7f903c7a7712838ae0d61805e1261440c1b81
jrfnl <jrfnl@users.noreply.github.com>
2020-08-15 03:40:19 +0200

    tests_get_phpunit_version(): remove redundant code
    No need for the feature toggle anymore, just return the plain version number.

--- tests/phpunit/includes/functions.php
+++ tests/phpunit/includes/functions.php
@@ -7,15 +7,7 @@ require_once __DIR__ . '/class-basic-object.php';
  * @return double The version number.
  */
 function tests_get_phpunit_version() {
-	if ( class_exists( 'PHPUnit_Runner_Version' ) ) {
-		$version = PHPUnit_Runner_Version::id();
-	} elseif ( class_exists( 'PHPUnit\Runner\Version' ) ) {
-		$version = PHPUnit\Runner\Version::id();
-	} else {
-		$version = 0;
-	}
-
-	return $version;
+	return PHPUnit\Runner\Version::id();
 }
 
 /**
