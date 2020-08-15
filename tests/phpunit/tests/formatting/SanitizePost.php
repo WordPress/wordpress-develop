92a84ec041cfd083b129f91e7874d7c0e885f26f
jrfnl <jrfnl@users.noreply.github.com>
2020-08-15 18:57:45 +0200

    SanitizePost::test_int_fields(): remove use of assertInternalType()
    Removes the `$int_fields` array and the loop in favour of plain assertions.

--- tests/phpunit/tests/formatting/SanitizePost.php
+++ tests/phpunit/tests/formatting/SanitizePost.php
@@ -9,17 +9,12 @@ class Tests_Formatting_SanitizePost extends WP_UnitTestCase {
 	 * @ticket 22324
 	 */
 	function test_int_fields() {
-		$post       = self::factory()->post->create_and_get();
-		$int_fields = array(
-			'ID'            => 'integer',
-			'post_parent'   => 'integer',
-			'menu_order'    => 'integer',
-			'post_author'   => 'string',
-			'comment_count' => 'string',
-		);
+		$post = self::factory()->post->create_and_get();
 
-		foreach ( $int_fields as $field => $type ) {
-			$this->assertInternalType( $type, $post->$field, "field $field" );
-		}
+		$this->assertIsInt( $post->ID, 'field ID' );
+		$this->assertIsInt( $post->post_parent, 'field post_parent' );
+		$this->assertIsInt( $post->menu_order, 'field menu_order' );
+		$this->assertIsString( $post->post_author, 'field post_author' );
+		$this->assertIsString( $post->comment_count, 'field comment_count' );
 	}
 }
