35001521aee25e461ed2dde945033667d4bc6661
Dion Hulse <contact@dd32.id.au>
2020-08-19 18:13:57 +1000

    Traits can't share traits until a later version of PHP

--- tests/phpunit/includes/phpunit-compat-traits/trait-wp-phpunit7-compat.php
+++ tests/phpunit/includes/phpunit-compat-traits/trait-wp-phpunit7-compat.php
@@ -1,9 +1,8 @@
 <?php
-
-require_once __DIR__ . '/trait-wp-phpunit-compat-methods-caller.php';
-
+/**
+ * Trait that contains any new/needed functionality from PHPUnit 7
+ */
 trait WP_PHPUnit7_Compat {
-	use WP_PHPUnit_Compat_Methods_Caller;
 
 	// New
 	function _assertEqualsWithDelta($expected, $actual, $delta, $message = '') {
--- tests/phpunit/includes/phpunit8/testcase.php
+++ tests/phpunit/includes/phpunit8/testcase.php
@@ -5,6 +5,7 @@
 
 require_once dirname( __DIR__ ) . '/abstract-testcase.php';
 require_once dirname( __DIR__ ) . '/phpunit-compat-traits/trait-wp-php72-test-framework.php';
+require_once dirname( __DIR__ ) . '/phpunit-compat-traits/trait-wp-phpunit-compat-caller.php';
 require_once dirname( __DIR__ ) . '/phpunit-compat-traits/trait-wp-phpunit9-compat.php';
 require_once dirname( __DIR__ ) . '/phpunit-compat-traits/trait-wp-phpunit8-compat.php';
 require_once dirname( __DIR__ ) . '/phpunit-compat-traits/trait-wp-phpunit7-compat.php';
@@ -21,6 +22,7 @@ require_once dirname( __DIR__ ) . '/phpunit-compat-traits/trait-wp-phpunit7-comp
 class WP_UnitTestCase extends WP_UnitTestCase_Base {
 	use WP_PHP72_Test_Framework;
 
+	use WP_PHPUnit_Compat_Caller;
 	use WP_PHPUnit9_Compat;
 	use WP_PHPUnit8_Compat;
 	use WP_PHPUnit7_Compat;
