7b236d5a6331d95544d8335f720fa78eb6a9aa8b
Dion Hulse <contact@dd32.id.au>
2020-08-19 12:47:25 +1000

    Add a WP_UnitTestCase that works in all versions of PHP, hopefully.

--- tools/php8-support.sh
+++ tools/php8-support.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# The docker changes need to be done upstream, see: https://github.com/WordPress/wpdev-docker-images/pull/33
+# The docker changes need to be done upstream, see: https://github.com/WordPress/wpdev-docker-images/pull/34
 
 # Make a PHP8 PHPUnit 9 dockerfile.
 echo "FROM wordpressdevelop/phpunit:8.0-fpm
@@ -11,6 +11,15 @@ RUN curl -sL https://phar.phpunit.de/phpunit-9.phar > /usr/local/bin/phpunit && 
 
 # Use the PHP8 + PHPUnit9 dockerfile
 sed -i 's!phpunit:$!phpunit:\n    build:\n      context: .\n      dockerfile: php8.dockerFile!i' docker-compose.yml
+
+# these functions definition needs to return void as of PHPUnit8, so we have have our own middleware.
+for void_function in setUpBeforeClass setUp assertPreConditions assertPostConditions tearDown tearDownAfterClass onNotSuccessfulTest
+do
+	echo Converting ${void_function}..
+	grep "function\s*${void_function}()\s*{" tests/phpunit/ -rli || echo No affected files.
+	grep "function\s*${void_function}()\s*{" tests/phpunit/ -rli | xargs -I% sed -i "s!function\s*${void_function}()!function _${void_function}()!gi" %
+	echo
+done
 
 
 # assertContains() no longer handles non-iterables, middleware it as _WPassertContains() fow now.
