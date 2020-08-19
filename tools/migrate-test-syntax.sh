59686167baf8c53fb8ca21a5486c590b932fa5b7
Dion Hulse <contact@dd32.id.au>
2020-08-19 13:39:14 +1000

    Migrate the test syntax for all PHPUnit versions, this is a temporary change to avoid updating every testcase in the PR. Add a compat method for assertStringContainsString()

---
+++ tools/migrate-test-syntax.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# these functions definition needs to return void as of PHPUnit8, so we have have our own middleware.
+for void_function in setUpBeforeClass setUp assertPreConditions assertPostConditions tearDown tearDownAfterClass onNotSuccessfulTest
+do
+	echo Converting ${void_function}..
+	grep "function\s*${void_function}()\s*{" tests/phpunit/ -rli || echo No affected files.
+	grep "function\s*${void_function}()\s*{" tests/phpunit/ -rli | xargs -I% sed -i "s!function\s*${void_function}()!function _${void_function}()!gi" %
+	grep "parent::${void_function}" tests/phpunit/ -rli | xargs -I% sed -i "s!parent::${void_function}!parent::_${void_function}!gi" %
+	echo
+done
+
+# assertContains() no longer handles non-iterables, middleware it as _WPassertContains() fow now.
+# This avoids having non-phpunit-related changes in this branch.
+grep assertContains tests/phpunit/ -rli | xargs -I% sed -i 's~\$this->assertContains~\$this->_WPassertContains~' %
+grep assertNotContains tests/phpunit/ -rli | xargs -I% sed -i 's~\$this->assertNotContains~\$this->_WPassertNotContains~' %
+
+# Output a diff of the modifications for reference.
+git diff .
+
+# Lint check the modified files.
+git diff --name-only tests/phpunit/ | xargs -I% php -l %
