f08b42cbaa93a959d46f47522126c9b0ac97add9
Dion Hulse <contact@dd32.id.au>
2020-08-19 16:19:07 +1000

    Refactor migrate-test-syntax.sh to fix the errors I keep making :)

--- tools/migrate-test-syntax.sh
+++ tools/migrate-test-syntax.sh
@@ -4,34 +4,44 @@ function find_matching_files() {
     /usr/bin/env grep "$@" tests/phpunit/ -rli --exclude-dir=phpunit-compat-traits
 }
 
+function search_replace_raw() {
+	while read -r file; do
+		sed -i "s~$1~$2~" $file
+	done < <( find_matching_files $1 )
+}
+
+function search_replace_assert() {
+	search_replace_raw "\$this->'$1'" "\$this->'$2'"
+}
+
 # these functions definition needs to return void as of PHPUnit8, so we have have our own middleware.
 for void_function in setUpBeforeClass setUp assertPreConditions assertPostConditions tearDown tearDownAfterClass onNotSuccessfulTest
 do
 	echo Converting ${void_function}..
-	find_matching_files "function\s*${void_function}()\s*{" || echo No affected files.
-	find_matching_files "function\s*${void_function}()\s*{" | xargs -I% sed -i "s~function\s*${void_function}()~function _${void_function}()~gi" %
+	search_replace_raw "function\s*${void_function}()\s*{" "function _${void_function}() {"
+	search_replace_raw "parent::${void_function}" "parent::_${void_function}"
 
-	# Convert parent:: calls too, except in abstract-testcase which no longer needs to call it's parent method.
-	sed -i "s~parent::${void_function}~// parent::_${void_function}~gi" tests/phpunit/includes/abstract-testcase.php
-	find_matching_files "parent::${void_function}" tests/phpunit/ -rli --exclude-dir=phpunit-compat-traits | xargs -I% sed -i "s~parent::${void_function}~parent::_${void_function}~gi" %
+	# abstract-testcase no longer needs to call it's parent methods.
+	sed -i "/parent::_${void_function}()/d" tests/phpunit/includes/abstract-testcase.php
 
 	echo
 done
 
 # assertContains() no longer handles non-iterables, middleware it as _WPassertContains() fow now.
 # This avoids having non-phpunit-related changes in this branch.
-find_matching_files assertContains    | xargs -I% sed -i 's~\$this->assertContains~\$this->_WPassertContains~' %
-find_matching_files assertNotContains | xargs -I% sed -i 's~\$this->assertNotContains~\$this->_WPassertNotContains~' %
+search_replace_assert assertContains _WPassertContains
+search_replace_assert assertNotContains _WPassertNotContains
+
 
 # Deprecated functions - Swap for direct replacements, avoids extra noise in PHP 8 tests while testing
 
 # assertFileNotExists() -> assertFileDoesNotExist()
-find_matching_files assertFileNotExists | xargs -I% sed -i 's~\$this->assertFileNotExists~\$this->assertFileDoesNotExist~' %
+search_replace_assert assertFileNotExists assertFileDoesNotExist
 
-# assertRegExp() -> assertMatchesRegularExpression()
+# assertRegExp()    -> assertMatchesRegularExpression()
 # assertNotRegExp() -> assertDoesNotMatchRegularExpression()
-find_matching_files assertRegExp    | xargs -I% sed -i 's~\$this->assertRegExp~\$this->assertMatchesRegularExpression~' %
-find_matching_files assertNotRegExp | xargs -I% sed -i 's~\$this->assertNotRegExp~\$this->assertDoesNotMatchRegularExpression~' %
+search_replace_assert assertRegExp assertMatchesRegularExpression
+search_replace_assert assertNotRegExp assertDoesNotMatchRegularExpression
 
 # Output a diff of the modifications for reference.
 git diff .
