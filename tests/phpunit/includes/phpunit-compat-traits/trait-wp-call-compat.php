e67786042dc7ec6b40aacc53f8a194c2a3349bf1
Dion Hulse <contact@dd32.id.au>
2020-08-19 13:10:10 +1000

    Add a __call() trait for compat methods

---
+++ tests/phpunit/includes/phpunit-compat-traits/trait-wp-call-compat.php
@@ -0,0 +1,21 @@
+<?php
+
+trait WPCallCompat {
+	function __call( $method, $args ) {
+		$compat_method = "_{$method}";
+		if ( is_callable( [ $this, $compat_method ] ) ) {
+			call_user_func_array( [ $this, $compat_method ], $args );
+		} else {
+			trigger_error( "Call to undefined method WP_UnitTestCase::{$method}()", E_USER_ERROR );
+		}
+	}
+
+	public static __callStatic( $method, $args ) {
+		$compat_method = "static::_{$method}";
+		if ( is_callable( $compat_method ) ) {
+			call_user_func_array( $compat_method, $args );
+		} else {
+			trigger_error( "Call to undefined method WP_UnitTestCase::{$method}()", E_USER_ERROR );
+		}
+	}
+}
