3b92ed99278677a7264c998121daa11b3a3027b2
Dion Hulse <contact@dd32.id.au>
2020-08-19 16:31:45 +1000

    Don't rebuild the PHPUnit 8 + PHP 7.4 image, one exists already, use that.

--- tools/phpunit8-support.sh
+++ tools/phpunit8-support.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-# Make a PHP8 PHPUnit 9 dockerfile.
-echo "FROM wordpressdevelop/phpunit:7.4-fpm
-
-# Use PHPUnit 9
-RUN curl -sL https://phar.phpunit.de/phpunit-8.phar > /usr/local/bin/phpunit && chmod +x /usr/local/bin/phpunit
-" > phpunit8.dockerFile
-
-# Use the PHPUnit 8 dockerfile
-sed -i 's!phpunit:$!phpunit:\n    build:\n      context: .\n      dockerfile: phpunit8.dockerFile!i' docker-compose.yml
+# Use the PHPUnit 8 + PHP 7.4 image.
+sed -i 's!wordpressdevelop/phpunit:.*!wordpressdevelop/phpunit:8-php-7.4-fpm!' docker-compose.yml
