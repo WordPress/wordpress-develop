ae0d3f98025e162d5ad755ece79cfad010c41f43
Dion Hulse <contact@dd32.id.au>
2020-08-20 10:46:13 +1000

    Clarify the old-php migration scripts reason

--- tools/tests-old-php.sh
+++ tools/tests-old-php.sh
@@ -4,21 +4,15 @@ function find_matching_files() {
     /usr/bin/env grep "$@" tests/phpunit/ -rli --exclude-dir=phpunit-compat-traits
 }
 
-function search_replace_raw() {
+function search_replace() {
 	while read -r file; do
 		sed -i "s~$1~$2~i" $file
 	done < <( find_matching_files $1 )
 }
 
-function search_replace_assert() {
-	echo Converting $1..
-
-	search_replace_raw "\$this->$1" "\$this->$2"
-}
-
-# these functions definition needs to return void as of PHPUnit8, which isn't compatible with old PHP.
+# As of PHPUnit 8, these functions have a void typehint, that's not supported in PHP 5.6-7.1. Strip them out for these versions of PHP.
 for void_function in setUpBeforeClass setUp assertPreConditions assertPostConditions tearDown tearDownAfterClass onNotSuccessfulTest
 do
 	echo Converting ${void_function}..
-	search_replace_raw "function\s*${void_function}():\s*void\s*{" "function ${void_function}() {"
+	search_replace "function\s*${void_function}():\s*void\s*{" "function ${void_function}() {"
 done
