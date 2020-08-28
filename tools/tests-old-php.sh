140f2323fb561a9fe1fc85a9666ad06d5fbc9dca
Dion Hulse <contact@dd32.id.au>
2020-08-28 16:19:51 +1000

    Clarify a comment that this is needed for PHP 5.6/7.0 only.

--- tools/tests-old-php.sh
+++ tools/tests-old-php.sh
@@ -10,10 +10,10 @@ function search_replace() {
 	done < <( find_matching_files $1 )
 }
 
-# As of PHPUnit 8, these functions have a void typehint, that's not supported in PHP 5.6-7.1. Strip them out for these versions of PHP.
+# As of PHPUnit 8, these functions have a void typehint, that's not supported in PHP 5.6-7.0. Strip them out for these versions of PHP.
 for void_function in setUpBeforeClass setUp assertPreConditions assertPostConditions tearDown tearDownAfterClass onNotSuccessfulTest
 do
 	echo Converting ${void_function}..
 
-	search_replace "function\s*${void_function}():\s*void\s*{" "function ${void_function}() {"
+	search_replace "function\s*${void_function}()\s*:\s*void\s*{" "function ${void_function}() {"
 done
