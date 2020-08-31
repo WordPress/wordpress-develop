ffb0bf91faafff122b12af5003d8956d5eb60479
Jonathan Desrosiers <desrosj@git.wordpress.org>
2020-08-31 22:49:18 +0000

    Tests: Move the tests for `get_the_modified_time()` to a more appropriate place.
    Add some new tests to better cover the functionality, for consistency with `get_the_date()` and `get_the_time()`.
    
    Follow-up to [48911], [48912], [48918].
    
    Props wittich.
    Merges [48924] to the 5.5 branch.
    Fixes #51184.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.5@48926 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/date/getCommentDate.php
+++ tests/phpunit/tests/date/getCommentDate.php
@@ -10,7 +10,7 @@ class Tests_Date_Get_Comment_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 51184
 	 */
-	function test_get_comment_date_returns_correct_time_with_comment_id() {
+	public function test_get_comment_date_returns_correct_time_with_comment_id() {
 		$c = self::factory()->comment->create( array( 'comment_date' => '2020-08-29 01:51:00' ) );
 
 		$this->assertEquals( 'August 29, 2020', get_comment_date( 'F j, Y', $c ) );
@@ -19,7 +19,7 @@ class Tests_Date_Get_Comment_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 51184
 	 */
-	function test_get_comment_date_returns_correct_time_with_empty_format() {
+	public function test_get_comment_date_returns_correct_time_with_empty_format() {
 		$c = self::factory()->comment->create( array( 'comment_date' => '2020-08-29 01:51:00' ) );
 
 		$this->assertEquals( 'August 29, 2020', get_comment_date( '', $c ) );
@@ -29,7 +29,7 @@ class Tests_Date_Get_Comment_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 51184
 	 */
-	function test_get_comment_time_returns_correct_time() {
+	public function test_get_comment_time_returns_correct_time() {
 		$c = self::factory()->comment->create( array( 'comment_date' => '2020-08-29 01:51:00' ) );
 
 		$GLOBALS['comment'] = get_comment( $c );
@@ -39,7 +39,7 @@ class Tests_Date_Get_Comment_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 51184
 	 */
-	function test_get_comment_time_returns_correct_time_with_empty_format() {
+	public function test_get_comment_time_returns_correct_time_with_empty_format() {
 		$c = self::factory()->comment->create( array( 'comment_date' => '2020-08-29 01:51:00' ) );
 
 		$GLOBALS['comment'] = get_comment( $c );
--- tests/phpunit/tests/date/getTheDate.php
+++ tests/phpunit/tests/date/getTheDate.php
@@ -10,7 +10,7 @@ class Tests_Date_Get_The_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 13771
 	 */
-	function test_get_the_date_returns_correct_time_with_post_id() {
+	public function test_get_the_date_returns_correct_time_with_post_id() {
 		$post_id = self::factory()->post->create( array( 'post_date' => '2014-03-01 16:35:00' ) );
 
 		$this->assertEquals( 'March 1, 2014', get_the_date( 'F j, Y', $post_id ) );
@@ -19,7 +19,7 @@ class Tests_Date_Get_The_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 28310
 	 */
-	function test_get_the_date_returns_false_with_null_or_non_existing_post() {
+	public function test_get_the_date_returns_false_with_null_or_non_existing_post() {
 		$this->assertFalse( get_the_date() );
 		$this->assertFalse( get_the_date( 'F j, Y h:i:s' ) );
 		$this->assertFalse( get_the_date( '', 9 ) );
@@ -29,7 +29,7 @@ class Tests_Date_Get_The_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 51184
 	 */
-	function test_get_the_date_returns_correct_time_with_empty_format() {
+	public function test_get_the_date_returns_correct_time_with_empty_format() {
 		$post_id = self::factory()->post->create( array( 'post_date' => '2020-08-29 01:51:00' ) );
 
 		$this->assertEquals( 'August 29, 2020', get_the_date( '', $post_id ) );
@@ -39,7 +39,7 @@ class Tests_Date_Get_The_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 28310
 	 */
-	function test_get_the_time_returns_correct_time_with_post_id() {
+	public function test_get_the_time_returns_correct_time_with_post_id() {
 		$post_id = self::factory()->post->create( array( 'post_date' => '2014-03-01 16:35:00' ) );
 
 		$this->assertEquals( '16:35:00', get_the_time( 'H:i:s', $post_id ) );
@@ -48,7 +48,7 @@ class Tests_Date_Get_The_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 28310
 	 */
-	function test_get_the_time_returns_false_with_null_or_non_existing_post() {
+	public function test_get_the_time_returns_false_with_null_or_non_existing_post() {
 		$this->assertFalse( get_the_time() );
 		$this->assertFalse( get_the_time( 'h:i:s' ) );
 		$this->assertFalse( get_the_time( '', 9 ) );
@@ -58,7 +58,7 @@ class Tests_Date_Get_The_Date extends WP_UnitTestCase {
 	/**
 	 * @ticket 51184
 	 */
-	function test_get_the_time_returns_correct_time_with_empty_format() {
+	public function test_get_the_time_returns_correct_time_with_empty_format() {
 		$post_id = self::factory()->post->create( array( 'post_date' => '2020-08-29 01:51:00' ) );
 
 		$this->assertEquals( '1:51 am', get_the_time( '', $post_id ) );
