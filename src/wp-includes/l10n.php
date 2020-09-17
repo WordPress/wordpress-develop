6a6375b4b6c03229a5f590b797d5ea65de76b271
Ari Stathopoulos <aristath@gmail.com>
2020-09-17 16:42:25 +0300

    Attempt to only do this for single-site

--- src/wp-includes/l10n.php
+++ src/wp-includes/l10n.php
@@ -51,7 +51,7 @@ function get_locale() {
 			$ms_locale = get_site_option( 'WPLANG' );
 		} else {
 			$ms_locale = get_option( 'WPLANG' );
-			if ( ! $ms_locale ) {
+			if ( false === $ms_locale ) {
 				$ms_locale = get_site_option( 'WPLANG' );
 			}
 		}
