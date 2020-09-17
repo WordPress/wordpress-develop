02c807ee0d7258cacfb6a999ab816b445ca7c2ed
Ari Stathopoulos <aristath@gmail.com>
2020-09-17 14:10:50 +0300

    this is getting weirder

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -2210,8 +2210,12 @@ function upgrade_550() {
  */
 function upgrade_560() {
 	// Prevent an extra database query on each page-load if the WPLANG option doesn't exist.
-	if ( false === get_site_option( 'WPLANG' ) ) {
-		update_site_option( 'WPLANG', '' );
+	if ( is_multisite() ) {
+		if ( false === get_site_option( 'WPLANG' ) ) {
+			update_site_option( 'WPLANG', '' );
+		}
+	} elseif ( false === get_option( 'WPLANG' ) ) {
+		update_option( 'WPLANG', '' );
 	}
 }
 
