6e650ba51fd483f04c23137f002a26d667e4020d
Dion Hulse <contact@dd32.id.au>
2020-09-18 12:42:51 +1000

    Update a unit test to use the short description field.

--- tests/phpunit/tests/rest-api/rest-block-directory-controller.php
+++ tests/phpunit/tests/rest-api/rest-block-directory-controller.php
@@ -153,7 +153,7 @@ class WP_REST_Block_Directory_Controller_Test extends WP_Test_REST_Controller_Te
 		$expected = array(
 			'name'                => 'sortabrilliant/guidepost',
 			'title'               => 'Guidepost',
-			'description'         => 'A guidepost gives you directions. It lets you know where you’re going. It gives you a preview of what’s to come. How does it work? Guideposts are magic, no they...',
+			'description'         => 'A guidepost gives you directions. It lets you know where you’re going. It gives you a preview of what’s to come.',
 			'id'                  => 'guidepost',
 			'rating'              => 4.3,
 			'rating_count'        => 90,
