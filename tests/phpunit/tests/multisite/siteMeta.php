e8617468a2dd9a34c015d40f5f07a0fd53c1db79
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2020-09-21 20:18:20 +0000

    Tests: Fix typo in some `markTestSkipped()` messages.
    See #51344.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@49030 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/multisite/siteMeta.php
+++ tests/phpunit/tests/multisite/siteMeta.php
@@ -166,7 +166,7 @@ if ( is_multisite() ) :
 
 		public function test_update_should_return_true_when_updating_existing_value_for_key() {
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			add_site_meta( self::$site_id, 'foo', 'bar' );
@@ -180,7 +180,7 @@ if ( is_multisite() ) :
 
 		public function test_delete_by_key() {
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			add_site_meta( self::$site_id, 'unique_delete_by_key', 'value', true );
@@ -197,7 +197,7 @@ if ( is_multisite() ) :
 
 		public function test_site_meta_should_be_deleted_when_site_is_deleted() {
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			$site_id = self::factory()->blog->create(
@@ -223,7 +223,7 @@ if ( is_multisite() ) :
 			global $wpdb;
 
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			update_site_meta( self::$site_id, 'foo', 'bar' );
@@ -238,7 +238,7 @@ if ( is_multisite() ) :
 			global $wpdb;
 
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			update_site_meta( self::$site_id, 'foo', 'bar' );
@@ -259,7 +259,7 @@ if ( is_multisite() ) :
 			global $wpdb;
 
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			update_site_meta( self::$site_id, 'foo', 'bar' );
@@ -281,7 +281,7 @@ if ( is_multisite() ) :
 		 */
 		public function test_add_site_meta_should_bust_get_sites_cache() {
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			add_site_meta( self::$site_id, 'foo', 'bar' );
@@ -323,7 +323,7 @@ if ( is_multisite() ) :
 		 */
 		public function test_update_site_meta_should_bust_get_sites_cache() {
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			add_site_meta( self::$site_id, 'foo', 'bar' );
@@ -366,7 +366,7 @@ if ( is_multisite() ) :
 		 */
 		public function test_delete_site_meta_should_bust_get_sites_cache() {
 			if ( ! is_site_meta_supported() ) {
-				$this->markTestSkipped( 'Tests only runs with the blogmeta database table installed.' );
+				$this->markTestSkipped( 'Test only runs with the blogmeta database table installed.' );
 			}
 
 			add_site_meta( self::$site_id, 'foo', 'bar' );
