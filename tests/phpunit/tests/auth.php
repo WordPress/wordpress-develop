f0503b6c05d5ea33464a8611babf1cd82f40fefa
George Stephanis <georgestephanis@automattic.com>
2020-09-21 10:14:13 -0400

    Add unit tests for application passwords via basic auth
    Pulling across from https://github.com/WordPress/application-passwords/pull/97
    
    Co-Authored-By: Kaspars Dambis <hi@kaspars.net>

--- tests/phpunit/tests/auth.php
+++ tests/phpunit/tests/auth.php
@@ -414,4 +414,48 @@ class Tests_Auth extends WP_UnitTestCase {
 		$this->assertInstanceOf( 'WP_User', wp_signon() );
 	}
 
+	/**
+	 * HTTP Auth headers are used to determine the current user.
+	 *
+	 * @covers wp_validate_application_password()
+	 */
+	public function test_application_password_authentication() {
+		$user_id = $this->factory->user->create(
+			array(
+				'user_login' => 'http_auth_login',
+				'user_pass'  => 'http_auth_pass', // Shouldn't be allowed for API login.
+			)
+		);
+
+		// Create a new app-only password.
+		$user_app_password = WP_Application_Passwords::create_new_application_password( $user_id, 'phpunit' );
+
+		// Fake a REST API request.
+		add_filter( 'application_password_is_api_request', '__return_true' );
+
+		// Fake an HTTP Auth request with the regular account password first.
+		$_SERVER['PHP_AUTH_USER'] = 'http_auth_login';
+		$_SERVER['PHP_AUTH_PW']   = 'http_auth_pass';
+
+		$this->assertEquals(
+			0,
+			wp_validate_application_password( null ),
+			'Regular user account password should not be allowed for API authenticaiton'
+		);
+
+		// Not try with an App password instead.
+		$_SERVER['PHP_AUTH_PW'] = $user_app_password[0];
+
+		$this->assertEquals(
+			$user_id,
+			wp_validate_application_password( null ),
+			'Application passwords should be allowed for API authentication'
+		);
+
+		remove_filter( 'application_password_is_api_request', '__return_true' );
+
+		// Cleanup all the global state.
+		unset( $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'] );
+	}
+
 }
