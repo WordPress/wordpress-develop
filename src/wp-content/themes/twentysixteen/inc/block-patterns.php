01fe7e32177b2c40cbdf10a75bdf036d7a691815
Maggie Cabrera <maggie.cabrera@automattic.com>
2020-09-25 10:49:41 +0200

    fixed lint errors

--- src/wp-content/themes/twentysixteen/inc/block-patterns.php
+++ src/wp-content/themes/twentysixteen/inc/block-patterns.php
@@ -10,7 +10,7 @@
  * Register Block Pattern Category.
  */
 if ( function_exists( 'register_block_pattern_category' ) ) {
-	
+
 	register_block_pattern_category(
 		'twentysixteen',
 		array( 'label' => __( 'Twenty Sixteen', 'twentysixteen' ) )
@@ -23,10 +23,10 @@ if ( function_exists( 'register_block_pattern_category' ) ) {
 if ( function_exists( 'register_block_pattern' ) ) {
 	register_block_pattern(
 		'twentysixteen/large-heading-short-description',
-		array (
-			'title'		=> __( 'Large heading with short description', 'twentysixteen' ),
-			'categories'=> array( 'twentysixteen' ),
-			'content'	=> '<!-- wp:group {"align":"full","backgroundColor":"background"} -->
+		array(
+			'title'      => __( 'Large heading with short description', 'twentysixteen' ),
+			'categories' => array( 'twentysixteen' ),
+			'content'    => '<!-- wp:group {"align":"full","backgroundColor":"background"} -->
             <div class="wp-block-group alignfull has-background-background-color has-background"><div class="wp-block-group__inner-container"><!-- wp:spacer {"height":60} -->
             <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
@@ -45,97 +45,97 @@ if ( function_exists( 'register_block_pattern' ) ) {
 
 	register_block_pattern(
 		'twentysixteen/big-title-two-columns-text',
-		array (
-			'title'		=> __( 'Big Title with Two Columns Text', 'twentysixteen' ),
-			'categories'=> array( 'twentysixteen' ),
-			'content'	=> '<!-- wp:spacer -->
+		array(
+			'title'      => __( 'Big Title with Two Columns Text', 'twentysixteen' ),
+			'categories' => array( 'twentysixteen' ),
+			'content'    => '<!-- wp:spacer -->
             <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
-            
+
             <!-- wp:heading {"level":1,"style":{"typography":{"fontSize":55}}} -->
             <h1 style="font-size:55px">Lonesome ghosts hovering their chairs</h1>
             <!-- /wp:heading -->
-            
+
             <!-- wp:spacer {"height":30} -->
             <div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
-            
+
             <!-- wp:columns -->
             <div class="wp-block-columns"><!-- wp:column -->
             <div class="wp-block-column"><!-- wp:paragraph {"dropCap":true} -->
             <p class="has-drop-cap">It might have been said that there he was only protecting his own. From the first he had been admitted to live in the intimacy of the family of the hotel-keeper who was a countryman of his. Old Giorgio Viola, a Genoese with a shaggy white leonine head—often called simply “the Garibaldino” (as Mohammedans are called after their prophet)—was, to use Captain Mitchell’s own words, the “respectable married friend” by whose advice Nostromo had left his ship to try for a run of shore luck in Costaguana.</p>
             <!-- /wp:paragraph -->
-            
+
             <!-- wp:paragraph -->
             <p>The old republican did not believe in saints, or in prayers, or in what he called “priest’s religion.” Liberty and Garibaldi were his divinities; but he tolerated “superstition” in women, preserving in these matters a lofty and silent attitude.</p>
             <!-- /wp:paragraph --></div>
             <!-- /wp:column -->
-            
+
             <!-- wp:column -->
             <div class="wp-block-column"><!-- wp:paragraph -->
             <p>The old man, full of scorn for the populace, as your austere republican so often is, had disregarded the preliminary sounds of trouble. He went on that day as usual pottering about the “casa” in his slippers, muttering angrily to himself his contempt of the non-political nature of the riot, and shrugging his shoulders. In the end he was taken unawares by the out-rush of the rabble. It was too late then to remove his family, and, indeed, where could he have run to with the portly Signora Teresa and two little girls on that great plain? So, barricading every opening, the old man sat down sternly in the middle of the darkened cafe with an old shot-gun on his knees. His wife sat on another chair by his side, muttering pious invocations to all the saints of the calendar.</p>
             <!-- /wp:paragraph --></div>
             <!-- /wp:column --></div>
             <!-- /wp:columns -->
-            
+
             <!-- wp:spacer -->
             <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->',
-        )
-    );
+		)
+	);
 
 	register_block_pattern(
 		'twentysixteen/large-blockquote',
-		array (
-			'title'		=> __( 'Large Blockquote', 'twentysixteen' ),
-			'categories'=> array( 'twentysixteen' ),
-			'content'	=> '<!-- wp:spacer -->
+		array(
+			'title'      => __( 'Large Blockquote', 'twentysixteen' ),
+			'categories' => array( 'twentysixteen' ),
+			'content'    => '<!-- wp:spacer -->
             <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
-            
+
             <!-- wp:separator {"color":"dark-gray","className":"is-style-wide"} -->
             <hr class="wp-block-separator has-text-color has-background has-dark-gray-background-color has-dark-gray-color is-style-wide"/>
             <!-- /wp:separator -->
-            
+
             <!-- wp:heading {"style":{"typography":{"lineHeight":"1.5","fontSize":40}}} -->
             <h2 style="font-size:40px;line-height:1.5"><em>Twenty Sixteen will make your WordPress look beautiful everywhere.</em></h2>
             <!-- /wp:heading -->
-            
+
             <!-- wp:paragraph {"textColor":"medium-gray"} -->
             <p class="has-medium-gray-color has-text-color">— Takashi Irie</p>
             <!-- /wp:paragraph -->
-            
+
             <!-- wp:spacer {"height":20} -->
             <div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
-            
+
             <!-- wp:separator {"color":"dark-gray","className":"is-style-wide"} -->
             <hr class="wp-block-separator has-text-color has-background has-dark-gray-background-color has-dark-gray-color is-style-wide"/>
             <!-- /wp:separator -->
-            
+
             <!-- wp:spacer -->
             <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->',
-        )
-    );
+		)
+	);
 
 	register_block_pattern(
 		'twentysixteen/call-to-action',
-		array (
-			'title'		=> __( 'Call to Action', 'twentysixteen' ),
-			'categories'=> array( 'twentysixteen' ),
-			'content'	=> '<!-- wp:spacer -->
+		array(
+			'title'      => __( 'Call to Action', 'twentysixteen' ),
+			'categories' => array( 'twentysixteen' ),
+			'content'    => '<!-- wp:spacer -->
             <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->
-            
+
             <!-- wp:separator {"color":"dark-gray","className":"is-style-wide"} -->
             <hr class="wp-block-separator has-text-color has-background has-dark-gray-background-color has-dark-gray-color is-style-wide"/>
             <!-- /wp:separator -->
-            
+
             <!-- wp:heading {"level":1,"style":{"typography":{"fontSize":35,"lineHeight":"1.5"}}} -->
             <h1 style="font-size:35px;line-height:1.5">My new book “Sail On” is available for pre-order on Amazon.</h1>
             <!-- /wp:heading -->
-            
+
             <!-- wp:columns -->
             <div class="wp-block-columns"><!-- wp:column -->
             <div class="wp-block-column"><!-- wp:buttons -->
@@ -144,17 +144,17 @@ if ( function_exists( 'register_block_pattern' ) ) {
             <!-- /wp:button --></div>
             <!-- /wp:buttons --></div>
             <!-- /wp:column -->
-            
+
             <!-- wp:column -->
             <div class="wp-block-column"><!-- wp:spacer {"height":54} -->
             <div style="height:54px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer --></div>
             <!-- /wp:column --></div>
             <!-- /wp:columns -->
-            
+
             <!-- wp:spacer -->
             <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
             <!-- /wp:spacer -->',
-        )
-    );
+		)
+	);
 }
