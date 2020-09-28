b636aca49792619d01664c3147e357e9fd856100
Andrew Ozz <743931+azaozz@users.noreply.github.com>
2020-09-28 17:33:39 +0300

    Fix inline comment.

--- src/js/_enqueues/vendor/jquery/ui/core.js
+++ src/js/_enqueues/vendor/jquery/ui/core.js
@@ -1,6 +1,6 @@
 /*! jQuery UI - v1.12.1 - 2020-09-25
 * http://jqueryui.com
-* Includes: core.js, widget.js, mouse.js, draggable.js, droppable.js, resizable.js, selectable.js, sortable.js, effect.js, data.js, disable-selection.js, escape-selector.js, focusable.js, form-reset-mixin.js, form.js, ie.js, jquery-1-7.js, keycode.js, labels.js, plugin.js, position.js, safe-active-element.js, safe-blur.js, scroll-parent.js, tabbable.js, unique-id.js, version.js
+* Includes: data.js, disable-selection.js, escape-selector.js, focusable.js, form-reset-mixin.js, form.js, ie.js, jquery-1-7.js, keycode.js, labels.js, plugin.js, position.js, safe-active-element.js, safe-blur.js, scroll-parent.js, tabbable.js, unique-id.js, version.js, widget.js
 * Copyright jQuery Foundation and other contributors; Licensed  */
 ( function( factory ) {
 	if ( typeof define === "function" && define.amd ) {
