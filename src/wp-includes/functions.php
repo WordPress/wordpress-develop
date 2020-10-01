4e5006a36fd82fdc49fd880d848cd5a13d14784d
David Baumwald <david.baumwald@gmail.com>
2020-10-01 14:27:58 -0400

    Refreshed patch.

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -3039,7 +3039,7 @@ function wp_get_image_mime( $file ) {
 			$imagetype = exif_imagetype( $file );
 			$mime      = ( $imagetype ) ? image_type_to_mime_type( $imagetype ) : false;
 		} elseif ( function_exists( 'getimagesize' ) ) {
-			$imagesize = @getimagesize( $file );
+			$imagesize = wp_getimagesize( $file );
 			$mime      = ( isset( $imagesize['mime'] ) ) ? $imagesize['mime'] : false;
 		} else {
 			$mime = false;
@@ -7659,4 +7659,21 @@ function is_php_version_compatible( $required ) {
  */
 function wp_fuzzy_number_match( $expected, $actual, $precision = 1 ) {
 	return abs( (float) $expected - (float) $actual ) <= $precision;
+}
+
+/**
+ * Allow PHPs getimagesize() to be debuggable when necessary.
+ *
+ * @since 5.6.0
+ *
+ * @param string $filename      The file path.
+ * @param array $imageinfo      Extended image information.
+ * @return array|false Array of image information or false on failure.
+ */
+function wp_getimagesize( $filename, &$imageinfo = array() ) {
+	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+		return getimagesize( $filename, $imageinfo );
+	}
+
+	return @getimagesize( $filename, $imageinfo );
 }
