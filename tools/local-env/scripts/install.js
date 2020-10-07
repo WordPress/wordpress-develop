c3c751e29c41d597a7cf8f94d17274aba20a4adf
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2020-10-07 15:46:52 -0400

    Try using `--allow-root`.

--- tools/local-env/scripts/install.js
+++ tools/local-env/scripts/install.js
@@ -6,7 +6,7 @@ const { renameSync, readFileSync, writeFileSync } = require( 'fs' );
 dotenv.config();
 
 // Create wp-config.php.
-wp_cli( 'config create --dbname=wordpress_develop --dbuser=root --dbpass=password --dbhost=mysql --path=/var/www/src --force' );
+wp_cli( 'config create --dbname=wordpress_develop --dbuser=root --dbpass=password --dbhost=mysql --path=/var/www/src --force --allow-root' );
 
 // Add the debug settings to wp-config.php.
 // Windows requires this to be done as an additional step, rather than using the --extra-php option in the previous step.
