3e79fc3782111b6735f6224bef0a20812a71696e
Alex Concha <alex@buayacorp.com>
2020-10-12 11:35:49 +0200

    wp-admin/includes/post.php: Update the `$post_data` parameter type, which might also be a WP_Error object.

--- src/wp-admin/includes/post.php
+++ src/wp-admin/includes/post.php
@@ -205,7 +205,7 @@ function _wp_translate_postdata( $update = false, $post_data = null ) {
  *
  * @since 5.0.1
  *
- * @param array $post_data Array of post data. Defaults to the contents of $_POST.
+ * @param array|WP_Error $post_data Array of post data. Defaults to the contents of $_POST.
  * @return array|WP_Error Array of post data on success, WP_Error on failure.
  */
 function _wp_get_allowed_postdata( $post_data = null ) {
