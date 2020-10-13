115b56953b1d803b1b8dd61fac4b2aaa05dff6e8
Dominik Schilling <dominikschilling+git@gmail.com>
2020-10-13 14:21:12 +0200

    Fix test_plugin_translation_after_switching_locale_twice() to actually test a successful switch

--- tests/phpunit/tests/l10n/loadTextdomainJustInTime.php
+++ tests/phpunit/tests/l10n/loadTextdomainJustInTime.php
@@ -170,6 +170,7 @@ class Tests_L10n_loadTextdomainJustInTime extends WP_UnitTestCase {
 
 	/**
 	 * @ticket 37997
+	 * @ticket 39210
 	 */
 	public function test_plugin_translation_after_switching_locale_twice() {
 		require_once DIR_TESTDATA . '/plugins/internationalized-plugin.php';
@@ -183,7 +184,7 @@ class Tests_L10n_loadTextdomainJustInTime extends WP_UnitTestCase {
 		restore_current_locale();
 
 		$this->assertSame( 'Das ist ein Dummy Plugin', $expected_de_de );
-		$this->assertSame( 'This is a dummy plugin', $expected_es_es );
+		$this->assertSame( 'Este es un plugin falso', $expected_es_es );
 	}
 
 	/**
