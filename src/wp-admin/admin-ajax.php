9bc403939d3cda2b7db267918e3165a53dfc91b5
Marius Jensen <marius@clorith.net>
2020-10-13 17:35:43 +0200

    Bring a fresh copy of Site Health via Rest API up to speed

--- src/wp-admin/admin-ajax.php
+++ src/wp-admin/admin-ajax.php
@@ -134,10 +134,6 @@ $core_actions_post = array(
 	'wp-privacy-export-personal-data',
 	'wp-privacy-erase-personal-data',
 	'health-check-site-status-result',
-	'health-check-dotorg-communication',
-	'health-check-is-in-debug-mode',
-	'health-check-background-updates',
-	'health-check-loopback-requests',
 	'health-check-get-sizes',
 	'toggle-auto-updates',
 );
