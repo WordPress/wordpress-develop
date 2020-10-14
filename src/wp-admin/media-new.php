420ebe6ef1b3be313c2673528a95c9e000d136c7
Enrico Carraro <enricocarraro@google.com>
2020-10-14 15:03:31 +0200

    Use wp_get_script_tag() and wp_get_inline_script_tag() in `wp-admin/network/media-new.php`.

--- src/wp-admin/media-new.php
+++ src/wp-admin/media-new.php
@@ -74,11 +74,10 @@ if ( get_user_setting( 'uploader' ) || isset( $_GET['browser-uploader'] ) ) {
 
 	<form enctype="multipart/form-data" method="post" action="<?php echo admin_url( 'media-new.php' ); ?>" class="<?php echo esc_attr( $form_class ); ?>" id="file-form">
 
-	<?php media_upload_form(); ?>
-
-	<script type="text/javascript">
-	var post_id = <?php echo $post_id; ?>, shortform = 3;
-	</script>
+	<?php
+	media_upload_form();
+	wp_print_inline_script_tag( "var post_id = $post_id, shortform = 3;" );
+	?>
 	<input type="hidden" name="post_id" id="post_id" value="<?php echo $post_id; ?>" />
 	<?php wp_nonce_field( 'media-form' ); ?>
 	<div id="media-items" class="hide-if-no-js"></div>
