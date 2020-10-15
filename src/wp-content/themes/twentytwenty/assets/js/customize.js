f138386bdb2c01c2e7a39e104f8f01c06c23c9e2
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2020-10-15 14:16:55 -0400

    Introduce a jshint warning.

--- src/wp-content/themes/twentytwenty/assets/js/customize.js
+++ src/wp-content/themes/twentytwenty/assets/js/customize.js
@@ -7,21 +7,20 @@
 		api.control( 'accent_hue_active' ).setting.bind( function( active ) {
 			var control = api.control( 'accent_hue' ); // Get the accent hue control.
 
-			if ( 'custom' === active ) {
+			if ( 'custom' === active )
 				// Activate the hue color picker control and focus it.
 				control.activate( {
 					completeCallback: function() {
 						control.focus();
 					}
 				} );
-			} else {
+			else
 				// If the `custom` option isn't selected, deactivate the hue color picker and set a default.
 				control.deactivate( {
 					completeCallback: function() {
 						control.setting.set( control.params.defaultValue );
 					}
 				} );
-			}
 		} );
 	} );
 }( jQuery, wp.customize ) );
