e0e3f6ae36a503db1350655c04577d6ec2de6f5c
Helen Hou-Sandi <helen.y.hou@gmail.com>
2020-10-16 13:43:26 -0600

    Import patch from Trac and change Current state to Used as
    https://core.trac.wordpress.org/attachment/ticket/42063/42063.1.patch

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -3984,6 +3984,11 @@ function wp_prepare_attachment_for_js( $attachment ) {
 		$response['compat'] = get_compat_media_markup( $attachment->ID, array( 'in_modal' => true ) );
 	}
 
+	$media_states = get_media_states( $attachment );
+	if ( ! empty( $media_states ) ) {
+		$response['mediaStates'] = implode( ', ', $media_states );
+	}
+
 	/**
 	 * Filters the attachment data prepared for JavaScript.
 	 *
