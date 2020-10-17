35e776262a9322b198f3c0d89fba7d0c7e7694de
Dominik Schilling <dominikschilling+git@gmail.com>
2020-10-17 14:37:07 +0200

    Don't use unload_textdomain() during locale switch

--- src/wp-includes/class-wp-locale-switcher.php
+++ src/wp-includes/class-wp-locale-switcher.php
@@ -196,7 +196,8 @@ class WP_Locale_Switcher {
 		load_default_textdomain( $locale );
 
 		foreach ( $domains as $domain ) {
-			unload_textdomain( $domain );
+			// unload_textdomain( $domain ); // TODO: Don't mark as unloaded so it can be reloaded. Is this ok?
+			unset( $l10n[ $domain ] );
 			get_translations_for_domain( $domain );
 		}
 	}
