bca74b739a193671dda33781da3d0d8bb1d0c29e
Dominik Schilling <dominikschilling+git@gmail.com>
2020-10-17 14:32:25 +0200

    Use plugin_basename()

--- tests/phpunit/data/plugins/custom-internationalized-plugin/custom-internationalized-plugin.php
+++ tests/phpunit/data/plugins/custom-internationalized-plugin/custom-internationalized-plugin.php
@@ -7,7 +7,7 @@ Version: 1.0.0
 Text Domain: custom-internationalized-plugin
 */
 
-load_plugin_textdomain( 'custom-internationalized-plugin', false, basename( dirname( __FILE__ ) ) . '/languages' );
+load_plugin_textdomain( 'custom-internationalized-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 
 function custom_i18n_plugin_test() {
 	return __( 'This is a dummy plugin', 'custom-internationalized-plugin' );
