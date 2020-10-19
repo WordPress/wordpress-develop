8cdc18dac06b7919e3c245b934a41819a15c515f
Felix Arntz <felixarntz@google.com>
2020-10-19 10:39:42 -0700

    Disable media search engine visibility checkbox based on general search engine visibility control.

--- src/wp-admin/includes/options.php
+++ src/wp-admin/includes/options.php
@@ -113,11 +113,19 @@ function options_reading_add_js() {
 		var section = $('#front-static-pages'),
 			staticPage = section.find('input:radio[value="page"]'),
 			selects = section.find('select'),
-			check_disabled = function(){
+			checkPageSelectsDisabled = function(){
 				selects.prop( 'disabled', ! staticPage.prop('checked') );
 			};
-		check_disabled();
-		section.find( 'input:radio' ).change( check_disabled );
+		checkPageSelectsDisabled();
+		section.find( 'input:radio' ).change( checkPageSelectsDisabled );
+
+		var discourage = $( 'input[name="blog_public"][value="0"]' ),
+			discourageMedia = $( 'input[name="media_search_engine_visibility"][value="0"]' ),
+			checkDiscourageMediaDisabled = function() {
+				discourageMedia.prop( 'disabled', !! discourage.prop( 'checked' ) );
+			};
+		checkDiscourageMediaDisabled();
+		discourage.change( checkDiscourageMediaDisabled );
 	});
 </script>
 	<?php
