24705a129e4537cd5d0f4b63d10509d6c827caa8
Helen Hou-Sandi <helen@git.wordpress.org>
2020-10-20 21:45:21 +0000

    WordPress 5.6 Beta 1.
    git-svn-id: https://develop.svn.wordpress.org/trunk@49260 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48683-src';
+$wp_version = '5.6-beta1-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
