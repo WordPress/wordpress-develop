902584148ee7925e84d3d55a7592a731f9ef1ad8
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2020-10-29 20:54:11 +0000

    Post WordPress 5.5.2 version bump. The 5.5 branch is now 5.5.3 alpha.
    git-svn-id: https://develop.svn.wordpress.org/branches/5.5@49449 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.2-src';
+$wp_version = '5.5.3-alpha-49449-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
