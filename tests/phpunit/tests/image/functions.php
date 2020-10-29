c045f6139973faaa3edd0f1d1a76aa247ebf5c42
John Watkins <johnwatkins0@gmail.com>
2020-10-28 23:10:44 -0500

    Fix test of intermediate image filesize

--- tests/phpunit/tests/image/functions.php
+++ tests/phpunit/tests/image/functions.php
@@ -668,7 +668,7 @@ class Tests_Image_Functions extends WP_UnitTestCase {
 		$metadata = wp_generate_attachment_metadata( $attachment_id, $test_file );
 
 		// Filesize results differ between environments.
-		$metadata['filesize'] = round_to_nearest_thousand( $metadata['filesize'] );
+		$metadata['sizes']['test-size']['filesize'] = round_to_nearest_thousand( $metadata['sizes']['test-size']['filesize'] );
 
 		$this->assertArrayHasKey( 'test-size', $metadata['sizes'], 'The `test-size` was not added to the metadata.' );
 		$this->assertSame( $expected, $metadata['sizes']['test-size'] );
