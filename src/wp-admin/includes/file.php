c37ac9a3923f5c2908fc35533b41b33d6a65a653
John Watkins <johnwatkins0@gmail.com>
2020-10-29 20:10:45 -0500

    Change since tags to 5.7

--- src/wp-admin/includes/file.php
+++ src/wp-admin/includes/file.php
@@ -2557,7 +2557,7 @@ function wp_opcache_invalidate( $filepath, $force = false ) {
 /**
  * Wrapper for PHP filesize with filters and casting the result as an integer.
  *
- * @since 5.6.0
+ * @since 5.7.0
  *
  * @link https://www.php.net/manual/en/function.filesize.php
  *
@@ -2568,7 +2568,7 @@ function wp_filesize( $path ) {
 	/**
 	 * Filters the result of wp_filesize before the PHP function is run.
 	 *
-	 * @since 5.6.0
+	 * @since 5.7.0
 	 *
 	 * @param null|int $size The unfiltered value. Returning an int from the callback bypasses the filesize call.
 	 * @param string   $path Path to the file.
@@ -2584,7 +2584,7 @@ function wp_filesize( $path ) {
 	/**
 	 * Filters the size of the file.
 	 *
-	 * @since 5.6.0
+	 * @since 5.7.0
 	 *
 	 * @param int    $size The result of PHP filesize on the file.
 	 * @param string $path Path to the file.
