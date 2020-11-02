140b29db8741e06af58895c5db7b84d749d9ca01
Helen Hou-Sandi <helen@git.wordpress.org>
2020-11-02 18:40:06 +0000

    Privacy: More precise checking of user request action names.
    Props garrett-eclipse.
    Fixes #46536.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@49475 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/qunit/fixtures/wp-api-generated.js
+++ tests/qunit/fixtures/wp-api-generated.js
@@ -6159,6 +6159,27 @@ mockedApiResponse.Schema = {
                 ]
             }
         },
+        "/wp-site-health/v1/tests/authorization-header": {
+            "namespace": "wp-site-health/v1",
+            "methods": [
+                "GET"
+            ],
+            "endpoints": [
+                {
+                    "methods": [
+                        "GET"
+                    ],
+                    "args": []
+                }
+            ],
+            "_links": {
+                "self": [
+                    {
+                        "href": "http://example.org/index.php?rest_route=/wp-site-health/v1/tests/authorization-header"
+                    }
+                ]
+            }
+        },
         "/wp-site-health/v1/directory-sizes": {
             "namespace": "wp-site-health/v1",
             "methods": [
