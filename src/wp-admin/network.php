b32eae70e595a3e3ea629c9d1e89931221b70ee2
hellofromtonya <hellofromtonya@knowthecode.io>
2020-11-09 18:00:16 -0600

    Type casts to string as parse_url can return string, bool, or null.

--- src/wp-admin/network.php
+++ src/wp-admin/network.php
@@ -94,7 +94,7 @@ if ( $_POST ) {
 	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 	// Create network tables.
 	install_network();
-	$base              = parse_url( trailingslashit( get_option( 'home' ) ), PHP_URL_PATH );
+	$base              = (string) parse_url( trailingslashit( get_option( 'home' ) ), PHP_URL_PATH );
 	$subdomain_install = allow_subdomain_install() ? ! empty( $_POST['subdomain_install'] ) : false;
 	if ( ! network_domain_check() ) {
 		$result = populate_network( 1, get_clean_basedomain(), sanitize_email( $_POST['email'] ), wp_unslash( $_POST['sitename'] ), $base, $subdomain_install );
