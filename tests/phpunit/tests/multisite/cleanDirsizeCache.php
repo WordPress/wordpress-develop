f29c7649a8ba220034d538da9ff9cd826531f814
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2020-12-03 15:15:23 +1100

    Unit tests for 5.5 transient.

--- tests/phpunit/tests/multisite/cleanDirsizeCache.php
+++ tests/phpunit/tests/multisite/cleanDirsizeCache.php
@@ -312,14 +312,14 @@ if ( is_multisite() ) :
 
 		function _get_mock_5_5_dirsize_cache( $site_id ) {
 			return array(
-				"/uploads/sites/$site_id/2/2"              => array( 'size' => 22 ),
-				"/uploads/sites/$site_id/2/1"              => array( 'size' => 21 ),
-				"/uploads/sites/$site_id/2"                => array( 'size' => 2 ),
-				"/uploads/sites/$site_id/1/3"              => array( 'size' => 13 ),
-				"/uploads/sites/$site_id/1/2"              => array( 'size' => 12 ),
-				"/uploads/sites/$site_id/1/1"              => array( 'size' => 11 ),
-				"/uploads/sites/$site_id/1"                => array( 'size' => 1 ),
-				"/uploads/sites/$site_id/custom_directory" => array( 'size' => 42 ),
+				"wp-content/uploads/sites/$site_id/2/2"              => array( 'size' => 22 ),
+				"wp-content/uploads/sites/$site_id/2/1"              => array( 'size' => 21 ),
+				"wp-content/uploads/sites/$site_id/2"                => array( 'size' => 2 ),
+				"wp-content/uploads/sites/$site_id/1/3"              => array( 'size' => 13 ),
+				"wp-content/uploads/sites/$site_id/1/2"              => array( 'size' => 12 ),
+				"wp-content/uploads/sites/$site_id/1/1"              => array( 'size' => 11 ),
+				"wp-content/uploads/sites/$site_id/1"                => array( 'size' => 1 ),
+				"wp-content/uploads/sites/$site_id/custom_directory" => array( 'size' => 42 ),
 			);
 		}
 	}
