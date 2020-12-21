f141418a5f4be1054aada8b0d524ba36bbe00bea
John Blackbourn <johnbillion@git.wordpress.org>
2020-12-21 00:31:54 +0000

    Query: Correct some coding standards after [49843].
    See #44183
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@49846 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/general/template.php
+++ tests/phpunit/tests/general/template.php
@@ -605,9 +605,11 @@ class Tests_General_Template extends WP_UnitTestCase {
 			)
 		);
 
-		$this->factory()->post->create( [
-			'post_author' => $user_with_posts->ID,
-		] );
+		$this->factory()->post->create(
+			array(
+				'post_author' => $user_with_posts->ID,
+			)
+		);
 
 		// Simplify the assertion by removing the default archive title prefix:
 		add_filter( 'get_the_archive_title_prefix', '__return_empty_string' );
