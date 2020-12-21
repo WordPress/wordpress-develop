70da62e85c2f005f80df87de3034e55c90155da8
John Blackbourn <johnbillion@gmail.com>
2020-12-21 01:08:45 +0100

    Use the error code instead of the object.

--- src/wp-includes/IXR/class-IXR-server.php
+++ src/wp-includes/IXR/class-IXR-server.php
@@ -130,7 +130,7 @@ EOD;
             $error = new IXR_Error($error, $message);
         }
 
-        status_header( $error ); #48215
+        status_header( $error->code );
         $this->output($error->getXml());
     }
 
