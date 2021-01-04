83db7000d8f9fe0c28da2258d98243f118712a90
John Blackbourn <johnbillion@gmail.com>
2021-01-04 18:35:29 +0100

    Move onto level 1 analysis, ignore more files.

--- tests/phpstan-bootstrap.php
+++ tests/phpstan-bootstrap.php
@@ -1,1 +1,90 @@
 <?php
+
+define( 'ABSPATH', '' );
+define( 'BLOGUPLOADDIR', '' );
+define( 'STYLESHEETPATH', '' );
+define( 'TEMPLATEPATH', '' );
+define( 'WP_CONTENT_DIR', '' );
+define( 'WP_CONTENT_URL', '' );
+define( 'WP_LANG_DIR', '' );
+define( 'WP_PLUGIN_DIR', '' );
+define( 'WP_PLUGIN_URL', '' );
+define( 'WPINC', '' );
+define( 'WPMU_PLUGIN_DIR', '' );
+define( 'WPMU_PLUGIN_URL', '' );
+
+define( 'DB_HOST', '' );
+define( 'DB_NAME', '' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_USER', '' );
+
+define( 'DAY_IN_SECONDS', 1 );
+define( 'HOUR_IN_SECONDS', 1 );
+define( 'MINUTE_IN_SECONDS', 1 );
+define( 'MONTH_IN_SECONDS', 1 );
+define( 'WEEK_IN_SECONDS', 1 );
+define( 'YEAR_IN_SECONDS', 1 );
+
+define( 'KB_IN_BYTES', 1 );
+define( 'MB_IN_BYTES', 1 );
+define( 'GB_IN_BYTES', 1 );
+define( 'TB_IN_BYTES', 1 );
+
+define( 'ADMIN_COOKIE_PATH', '' );
+define( 'AUTH_COOKIE', '' );
+define( 'COOKIE_DOMAIN', '' );
+define( 'COOKIEHASH', '' );
+define( 'COOKIEPATH', '' );
+define( 'LOGGED_IN_COOKIE', '' );
+define( 'PASS_COOKIE', '' );
+define( 'PLUGINS_COOKIE_PATH', '' );
+define( 'RECOVERY_MODE_COOKIE', '' );
+define( 'SECURE_AUTH_COOKIE', '' );
+define( 'SITECOOKIEPATH', '' );
+define( 'TEST_COOKIE', '' );
+define( 'USER_COOKIE', '' );
+
+define( 'MEDIA_TRASH', false );
+define( 'SCRIPT_DEBUG', false );
+define( 'WP_CACHE', false );
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'WP_DEBUG_LOG', false );
+define( 'WP_DEBUG', false );
+define( 'WPMU_ACCEL_REDIRECT', false );
+define( 'WPMU_SENDFILE', false );
+
+define( 'ARRAY_A', '' );
+define( 'ARRAY_N', '' );
+define( 'OBJECT', '' );
+define( 'OBJECT_K', '' );
+define( 'WP_DEFAULT_THEME', '' );
+
+define( 'AUTOSAVE_INTERVAL', 1 );
+define( 'EMPTY_TRASH_DAYS', 1 );
+define( 'FS_CHMOD_DIR', 1 );
+define( 'FS_CHMOD_FILE', 1 );
+define( 'FS_CONNECT_TIMEOUT', 1 );
+define( 'FS_TIMEOUT', 1 );
+define( 'PCLZIP_OPT_EXTRACT_AS_STRING', 1 );
+define( 'WP_CRON_LOCK_TIMEOUT', 1 );
+define( 'WP_MAX_MEMORY_LIMIT', 1 );
+define( 'WP_MEMORY_LIMIT', 1 );
+define( 'WP_POST_REVISIONS', 1 );
+define( 'WP_START_TIMESTAMP', 1 );
+
+define( 'EP_NONE', 1 );
+define( 'EP_PERMALINK', 1 );
+define( 'EP_ATTACHMENT', 1 );
+define( 'EP_DATE', 1 );
+define( 'EP_YEAR', 1 );
+define( 'EP_MONTH', 1 );
+define( 'EP_DAY', 1 );
+define( 'EP_ROOT', 1 );
+define( 'EP_COMMENTS', 1 );
+define( 'EP_SEARCH', 1 );
+define( 'EP_CATEGORIES', 1 );
+define( 'EP_TAGS', 1 );
+define( 'EP_AUTHORS', 1 );
+define( 'EP_PAGES', 1 );
+define( 'EP_ALL_ARCHIVES', 1 );
+define( 'EP_ALL', 1 );
