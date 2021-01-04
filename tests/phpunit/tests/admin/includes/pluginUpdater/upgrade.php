6eae37d07e72a5bcceed2c06ac9491bb086c00c6
hellofromtonya <hellofromtonya@knowthecode.io>
2021-01-04 15:00:30 -0600

    Standardizes the tests and data.

--- tests/phpunit/tests/admin/includes/pluginUpdater/upgrade.php
+++ tests/phpunit/tests/admin/includes/pluginUpdater/upgrade.php
@@ -11,18 +11,16 @@ require_once __DIR__ . '/testcase.php';
 class Tests_Admin_Includes_PluginUpdater_Upgrade extends Admin_Includes_PluginUpdater_TestCase {
 
 	/**
-	 * @dataProvider data_should_not_send_error_report
+	 * @dataProvider data_should_not_send_error_data
 	 *
 	 * @group        51928
 	 *
-	 * @param array $plugin           Array of plugin information.
-	 * @param array $update_plugins   Value for the "update_plugins" transient.
-	 * @param array $expected_message Array of expected admin output messages.
+	 * @param array $plugin         Array of plugin information.
+	 * @param array $update_plugins Value for the "update_plugins" transient.
+	 * @param array $expected       Array of expected results and/or messages.
 	 */
-	public function test_should_not_send_error_report( $plugin, $update_plugins, $expected_message ) {
+	public function test_should_not_send_error_data( $plugin, $update_plugins, $expected ) {
 		$this->plugin = $plugin;
-
-		set_site_transient( 'update_plugins', $update_plugins );
 
 		$plugin_upgrader = $this
 			->getMockBuilder( Plugin_Upgrader::class )
@@ -34,185 +32,201 @@ class Tests_Admin_Includes_PluginUpdater_Upgrade extends Admin_Includes_PluginUp
 			->expects( $this->never() )
 			->method( 'send_error_data' );
 
+		$this->install_older_version( $plugin_upgrader, $plugin['install'], $update_plugins );
+
+		// Do the upgrade.
 		ob_start();
-		$plugin_upgrader->upgrade( $plugin['plugin'] );
+		$result         = $plugin_upgrader->upgrade( $plugin['plugin'] );
 		$actual_message = ob_get_clean();
 
-		foreach ( $expected_message as $expected ) {
-			$this->assertContains( $expected, $actual_message );
+		// Validate the upgrade happened.
+		$this->assertTrue( $result );
+		foreach ( $expected['messages'] as $expected_message ) {
+			$this->assertContains( $expected_message, $actual_message );
 		}
 	}
 
-	public function data_should_not_send_error_report() {
+	public function data_should_not_send_error_data() {
 		return array(
 			'when local zip file exists'  => array(
-				'plugin'           => array(
-					'dest_file' => WP_PLUGIN_DIR . '/hello/hello.php',
-					'dest_dir'  => WP_PLUGIN_DIR . '/hello',
+				'plugin'         => array(
+					'install'   => DIR_TESTDATA . '/plugins/hello-1.6/hello-dolly.zip',
+					'dest_file' => WP_PLUGIN_DIR . '/hello-dolly/hello.php',
+					'dest_dir'  => WP_PLUGIN_DIR . '/hello-dolly',
 					'plugin'    => 'hello-dolly/hello.php',
 				),
-				'update_plugins'   => (object) array(
+				'update_plugins' => (object) array(
 					'last_checked' => time(),
 					'checked'      => array(
-						'hello.php' => '1.6',
+						'hello-dolly/hello.php' => '1.6',
 					),
 					'response'     => array(
 						'hello-dolly/hello.php' => (object) array(
 							'id'          => 'w.org/plugins/hello-dolly',
 							'slug'        => 'hello-dolly',
-							'plugin'      => 'hello.php',
+							'plugin'      => 'hello-dolly/hello.php',
 							'new_version' => '1.7.2',
 							'url'         => 'https://wordpress.org/plugins/hello-dolly/',
-							'package'     => DIR_TESTDATA . '/plugins/hello-1.7.2/hello.zip',
+							'package'     => DIR_TESTDATA . '/plugins/hello-1.7.2/hello-dolly.zip',
 						),
 					),
 				),
-				'expected_message' => array(
-					<<<MESSAGE
+				'expected'       => array(
+					'messages' => array(
+						<<<MESSAGE
 <p>Unpacking the update&#8230;</p>
 <p>Installing the latest version&#8230;</p>
 <p>Removing the old version of the plugin&#8230;</p>
 <p>Plugin updated successfully.</p>
 MESSAGE
-				,
+					,
+					),
 				),
 			),
 			'when downloading from w.org' => array(
-				'plugin'           => array(
+				'plugin'         => array(
+					'install'   => DIR_TESTDATA . '/plugins/hello-1.6/hello-dolly.zip',
 					'dest_file' => WP_PLUGIN_DIR . '/hello-dolly/hello.php',
 					'dest_dir'  => WP_PLUGIN_DIR . '/hello-dolly',
 					'plugin'    => 'hello-dolly/hello.php',
 				),
-				'update_plugins'   => (object) array(
+				'update_plugins' => (object) array(
 					'last_checked' => time(),
 					'checked'      => array(
-						'hello.php' => '1.6',
+						'hello-dolly/hello.php' => '1.6',
 					),
 					'response'     => array(
 						'hello-dolly/hello.php' => (object) array(
 							'id'          => 'w.org/plugins/hello-dolly',
 							'slug'        => 'hello-dolly',
-							'plugin'      => 'hello.php',
+							'plugin'      => 'hello-dolly/hello.php',
 							'new_version' => '1.7.2',
 							'url'         => 'https://wordpress.org/plugins/hello-dolly/',
 							'package'     => 'https://downloads.wordpress.org/plugin/hello-dolly.1.7.2.zip',
 						),
 					),
 				),
-				'expected_message' => array(
-					<<<MESSAGE
-<div class="wrap"><h1></h1><p>Downloading update from <span class="code">%s</span>&#8230;</p>
+				'expected'       => array(
+					'messages' => array(
+						<<<MESSAGE
+<p>Downloading update from <span class="code">%s</span>&#8230;</p>
 <p>The authenticity of <span class="code">hello-dolly.1.7.2.zip</span> could not be verified as no signature was found.</p>
 MESSAGE
-				,
-					<<<MESSAGE
+					,
+						<<<MESSAGE
 <p>Unpacking the update&#8230;</p>
 <p>Installing the latest version&#8230;</p>
 <p>Removing the old version of the plugin&#8230;</p>
 <p>Plugin updated successfully.</p>
 MESSAGE
-				,
+					,
+					),
 				),
 			),
 		);
 	}
 
 	/**
-	 * @dataProvider data_should_send_error_report
+	 * @dataProvider data_should_send_error_data
 	 *
 	 * @group        51928
 	 *
-	 * @param array $plugin           Array of plugin information.
-	 * @param array $update_plugins   Value for the "update_plugins" transient.
-	 * @param array $expected_message Array of expected admin output messages.
-	 * @param array $expected_stats   Array of expected stats.
+	 * @param array $plugin         Array of plugin information.
+	 * @param array $update_plugins Value for the "update_plugins" transient.
+	 * @param array $expected       Array of expected messages and stats.
 	 */
-	public function test_should_send_error_report( $plugin, $update_plugins, $expected_message, $expected_stats ) {
+	public function test_should_send_error_data( $plugin, $update_plugins, $expected ) {
 		$this->plugin = $plugin;
 
-		set_site_transient( 'update_plugins', $update_plugins );
-
 		$this->shortcircuit_w_org_download();
-		$this->capture_error_report();
+		$this->capture_error_data();
 
 		$plugin_upgrader = new Plugin_Upgrader( $this->mock_skin_feedback() );
 
+		$this->install_older_version( $plugin_upgrader, $plugin['install'], $update_plugins );
+
+		// Do the upgrade.
 		ob_start();
-		$plugin_upgrader->upgrade( $plugin['plugin'] );
+		$result = $plugin_upgrader->upgrade( $plugin['plugin'] );
 		$actual_message = ob_get_clean();
 
-		foreach ( $this->error_report as $index => $stats ) {
+		// Validate the upgrade did not happen.
+		$this->assertNull( $result );
+		foreach ( $expected['messages'] as $expected_message ) {
+			$this->assertContains( $expected_message, $actual_message );
+		}
+
+		// Validate the sent error data.
+		$expected_stats = $expected['stats'];
+		foreach ( $this->error_data as $index => $stats ) {
 			$this->assertContains( $expected_stats[ $index ], $stats );
 			$this->assertGreaterThan( 0.0, $stats['time_taken'] );
 		}
-
-		foreach ( $expected_message as $expected ) {
-			$this->assertContains( $expected, $actual_message );
-		}
 	}
 
-	public function data_should_send_error_report() {
-		$not_available_stats = array(
-			array(
-				'process'          => 'download_package',
-				'update_type'      => 'automatic_plugin_update',
-				'name'             => null,
-				'update_version'   => null,
-				'success'          => false,
-				'fs_method'        => 'direct',
-				'fs_method_forced' => true,
-				'fs_method_direct' => '',
-				'error_code'       => 'no_package',
-				'error_message'    => 'Installation package not available.',
-				'error_data'       => null,
-			),
-			array(
-				'process'          => 'plugin_install',
-				'update_type'      => 'automatic_plugin_update',
-				'name'             => null,
-				'update_version'   => null,
-				'success'          => false,
-				'fs_method'        => 'direct',
-				'fs_method_forced' => true,
-				'fs_method_direct' => '',
-				'error_code'       => 'no_package',
-				'error_message'    => 'Installation package not available.',
-				'error_data'       => null,
-			),
-		);
-
+	public function data_should_send_error_data() {
 		return array(
 			'when new version does not exist' => array(
-				'plugin'           => array(
-					'dest_file' => WP_PLUGIN_DIR . '/hello/hello.php',
-					'dest_dir'  => WP_PLUGIN_DIR . '/hello',
+				'plugin'         => array(
+					'install'   => DIR_TESTDATA . '/plugins/hello-1.6/hello-dolly.zip',
+					'dest_file' => WP_PLUGIN_DIR . '/hello-dolly/hello.php',
+					'dest_dir'  => WP_PLUGIN_DIR . '/hello-dolly',
 					'plugin'    => 'hello-dolly/hello.php',
 				),
-				'update_plugins'   => (object) array(
+				'update_plugins' => (object) array(
 					'last_checked' => time(),
 					'checked'      => array(
-						'hello.php' => '1.6',
+						'hello-dolly/hello.php' => '1.6',
 					),
 					'response'     => array(
 						'hello-dolly/hello.php' => (object) array(
 							'id'          => 'w.org/plugins/hello-dolly',
 							'slug'        => 'hello-dolly',
-							'plugin'      => 'hello.php',
+							'plugin'      => 'hello-dolly/hello.php',
 							'new_version' => '99999',
 							'url'         => 'https://wordpress.org/plugins/hello-dolly/',
-							'package'     => DIR_TESTDATA . '/plugins/hello-99999/hello.zip',
+							'package'     => DIR_TESTDATA . '/plugins/hello-99999/hello-dolly.zip',
 						),
 					),
 				),
-				'expected_message' => array(
-					'data/plugins/hello-99999/hello.zip',
-					<<<ERROR_MESSAGE
+				'expected'       => array(
+					'messages' => array(
+						'data/plugins/hello-99999/hello-dolly.zip',
+						<<<ERROR_MESSAGE
 <p>Unpacking the update&#8230;</p>
 <p>The package could not be installed. PCLZIP_ERR_MISSING_FILE (-4) : Missing archive file
 ERROR_MESSAGE
-				,
+					,
+					),
+					'stats'    => array(
+						array(
+							'process'          => 'download_package',
+							'update_type'      => 'automatic_plugin_update',
+							'name'             => null,
+							'update_version'   => null,
+							'success'          => false,
+							'fs_method'        => 'direct',
+							'fs_method_forced' => true,
+							'fs_method_direct' => '',
+							'error_code'       => 'no_package',
+							'error_message'    => 'Installation package not available.',
+							'error_data'       => null,
+						),
+						array(
+							'process'          => 'plugin_install',
+							'update_type'      => 'automatic_plugin_update',
+							'name'             => null,
+							'update_version'   => null,
+							'success'          => false,
+							'fs_method'        => 'direct',
+							'fs_method_forced' => true,
+							'fs_method_direct' => '',
+							'error_code'       => 'no_package',
+							'error_message'    => 'Installation package not available.',
+							'error_data'       => null,
+						),
+					),
 				),
-				'expected_stats'   => $not_available_stats,
 			),
 		);
 	}
