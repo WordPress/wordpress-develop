ad8c4b0f9b46b447455804c527624e047b100151
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2021-01-12 16:00:04 -0500

    Update the 3.8 branch to run on NodeJS 14.
    This backports all of the needed changes to allow the 3.8 branch to run on the latest LTS version of NodeJS (currently 14.x).
    
    This commit includes the following backports:
    - https://core.trac.wordpress.org/changeset/27053 (partial)
    - https://core.trac.wordpress.org/changeset/28796 (partial)
    - https://core.trac.wordpress.org/changeset/27053 (partial)
    - https://core.trac.wordpress.org/changeset/29270 (partial)
    - https://core.trac.wordpress.org/changeset/29505 (full)
    - https://core.trac.wordpress.org/changeset/30060 (full)
    - https://core.trac.wordpress.org/changeset/30062 (full)
    - https://core.trac.wordpress.org/changeset/30063 (full)
    - https://core.trac.wordpress.org/changeset/30066 (full)
    - https://core.trac.wordpress.org/changeset/31425 (full)
    - https://core.trac.wordpress.org/changeset/31648 (full)
    - https://core.trac.wordpress.org/changeset/31649 (full)
    - https://core.trac.wordpress.org/changeset/31650 (partial)
    - https://core.trac.wordpress.org/changeset/32356 (full)
    - https://core.trac.wordpress.org/changeset/32988 (partial)
    - https://core.trac.wordpress.org/changeset/33726 (full)
    - https://core.trac.wordpress.org/changeset/35363 (full)
    - https://core.trac.wordpress.org/changeset/35513 (full)
    - https://core.trac.wordpress.org/changeset/35521 (full)
    - https://core.trac.wordpress.org/changeset/35538 (full)
    - https://core.trac.wordpress.org/changeset/35539 (full)
    - https://core.trac.wordpress.org/changeset/35540 (full)
    - https://core.trac.wordpress.org/changeset/35541 (full)
    - https://core.trac.wordpress.org/changeset/36861 (full)
    - https://core.trac.wordpress.org/changeset/36862 (full)
    - https://core.trac.wordpress.org/changeset/36863 (full)
    - https://core.trac.wordpress.org/changeset/36864 (full)
    - https://core.trac.wordpress.org/changeset/36865 (full)
    - https://core.trac.wordpress.org/changeset/37017 (full)
    - https://core.trac.wordpress.org/changeset/37019 (full)
    - https://core.trac.wordpress.org/changeset/37020 (full)
    - https://core.trac.wordpress.org/changeset/37212 (full)
    - https://core.trac.wordpress.org/changeset/39110 (full)
    - https://core.trac.wordpress.org/changeset/39113 (full)
    - https://core.trac.wordpress.org/changeset/39115 (full)
    - https://core.trac.wordpress.org/changeset/39116 (full)
    - https://core.trac.wordpress.org/changeset/39117 (partial)
    - https://core.trac.wordpress.org/changeset/39478 (partial)
    - https://core.trac.wordpress.org/changeset/41835 (full)
    - https://core.trac.wordpress.org/changeset/42460 (full)
    - https://core.trac.wordpress.org/changeset/42461 (full)
    - https://core.trac.wordpress.org/changeset/42463 (full)
    - https://core.trac.wordpress.org/changeset/43320 (full)
    - https://core.trac.wordpress.org/changeset/43323 (partial)
    - https://core.trac.wordpress.org/changeset/43977 (partial)
    - https://core.trac.wordpress.org/changeset/44219 (partial)
    - https://core.trac.wordpress.org/changeset/44233 (partial)
    - https://core.trac.wordpress.org/changeset/45321 (partial)
    - https://core.trac.wordpress.org/changeset/46408 (partial)
    - https://core.trac.wordpress.org/changeset/47404 (partial)
    - https://core.trac.wordpress.org/changeset/47867 (partial)
    - https://core.trac.wordpress.org/changeset/47872 (full)
    - https://core.trac.wordpress.org/changeset/47873 (full)
    - https://core.trac.wordpress.org/changeset/49636 (full)
    - https://core.trac.wordpress.org/changeset/49933 (partial)
    - https://core.trac.wordpress.org/changeset/49937 (full)
    - https://core.trac.wordpress.org/changeset/49939 (partial)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,13 @@
 module.exports = function(grunt) {
 	var path = require('path'),
 		SOURCE_DIR = 'src/',
-		BUILD_DIR = 'build/';
+		BUILD_DIR = 'build/',
+		nodesass = require( 'node-sass' );
 
 	// Load tasks.
-	require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
+	require('matchdep').filterDev(['grunt-*', '!grunt-legacy-util']).forEach( grunt.loadNpmTasks );
+	// Load legacy utils
+	grunt.util = require('grunt-legacy-util');
 
 	// Project configuration.
 	grunt.initConfig({
@@ -84,6 +87,7 @@ module.exports = function(grunt) {
 				ext: '.css',
 				src: ['wp-admin/css/colors/*/colors.scss'],
 				options: {
+					implementation: nodesass,
 					outputStyle: 'expanded'
 				}
 			}
@@ -252,6 +256,9 @@ module.exports = function(grunt) {
 			}
 		},
 		uglify: {
+			options: {
+				'!ie8': false
+			},
 			core: {
 				expand: true,
 				cwd: SOURCE_DIR,
