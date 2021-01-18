35b3da9145fc4c68211cc3be43f932d797dfc7d3
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2021-01-18 15:50:50 +1100

    Fix trashed attachment URL.

--- tests/phpunit/tests/media.php
+++ tests/phpunit/tests/media.php
@@ -3156,7 +3156,7 @@ EOF;
 			array( 'publish-attachment', '/publish-post/publish-attachment', false ),
 			array( 'future-attachment', '/future-post/future-attachment', false ),
 			array( 'auto-draft-attachment', '/?attachment_id=%ID%', true ),
-			array( 'trash-attachment', '/?attachment_id=%ID%', false ),
+			array( 'trash-attachment', '/trash-attachment', false ),
 		);
 	}
 }
