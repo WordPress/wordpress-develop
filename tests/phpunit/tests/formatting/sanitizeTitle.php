de6c98ca7286e465b46483749ed2617ce4fe9690
hellofromtonya <hellofromtonya@knowthecode.io>
2021-01-24 11:06:58 -0600

    Adds tests for unicode space characters.

--- tests/phpunit/tests/formatting/sanitizeTitle.php
+++ tests/phpunit/tests/formatting/sanitizeTitle.php
@@ -16,4 +16,29 @@ class Tests_Formatting_SanitizeTitle extends WP_UnitTestCase {
 		$fallback = 'Captain Awesome';
 		$this->assertSame( $fallback, sanitize_title( $input, $fallback ) );
 	}
+
+	/**
+	 * @ticket 47594
+	 * @dataProvider data_unicode_space_characters
+	 */
+	function test_unicode_space_characters( $char ) {
+		$title    = "Handle{$char}space{$char}character";
+		$expected = 'handle-space-character';
+		$this->assertSame( $expected, sanitize_title( $title ) );
+	}
+
+	public function data_unicode_space_characters() {
+		return array(
+			array( '&nbsp;' ),
+			array( '%e2%80%af' ),
+			array( '%e2%80%87' ),
+			array( '%e2%81%a0' ),
+			array( '&#x2007;' ),
+			array( '&#8199;' ),
+			array( '&#x202F;' ),
+			array( '&#8239;' ),
+			array( '&#x2060;' ),
+			array( '&#8288;' ),
+		);
+	}
 }
