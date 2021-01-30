36a96f04ac7535b63b492e771f067c565b6ba47e
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-01-30 21:01:52 +0000

    Tests: Use `skipTestOnTimeout()` in more HTTP tests.
    Adjust it to handle more types of timeouts, e.g. "Resolving timed out", "Connection timed out".
    
    Merges [38757], [43511], [43512], [46682], [46996] to the 4.1 branch.
    See #51669.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/4.1@50103 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/http/functions.php
+++ tests/phpunit/tests/http/functions.php
@@ -17,6 +17,9 @@ class Tests_HTTP_Functions extends WP_UnitTestCase {
 		// this url give a direct 200 response
 		$url = 'https://asdftestblog1.files.wordpress.com/2007/09/2007-06-30-dsc_4700-1.jpg';
 		$response = wp_remote_head( $url );
+
+		$this->skipTestOnTimeout( $response );
+
 		$headers = wp_remote_retrieve_headers( $response );
 
 		$this->assertInternalType( 'array', $headers, "Reply wasn't array." );
@@ -29,58 +32,61 @@ class Tests_HTTP_Functions extends WP_UnitTestCase {
 		// this url will 301 redirect
 		$url = 'https://asdftestblog1.wordpress.com/files/2007/09/2007-06-30-dsc_4700-1.jpg';
 		$response = wp_remote_head( $url );
+
+		$this->skipTestOnTimeout( $response );
 		$this->assertEquals( '301', wp_remote_retrieve_response_code( $response ) );
 	}
 
 	function test_head_404() {
 		$url = 'https://asdftestblog1.files.wordpress.com/2007/09/awefasdfawef.jpg';
-		$headers = wp_remote_head( $url );
+		$response = wp_remote_head( $url );
 
-		$this->assertInternalType( 'array', $headers, "Reply wasn't array." );
-		$this->assertEquals( '404', wp_remote_retrieve_response_code( $headers ) );
+		$this->skipTestOnTimeout( $response );
+		$this->assertInternalType( 'array', $response, "Reply wasn't array." );
+		$this->assertEquals( '404', wp_remote_retrieve_response_code( $response ) );
 	}
 
 	function test_get_request() {
 		$url = 'https://asdftestblog1.files.wordpress.com/2007/09/2007-06-30-dsc_4700-1.jpg';
-		$file = tempnam('/tmp', 'testfile');
 
-		$headers = wp_get_http($url, $file);
+		$response = wp_remote_get( $url );
+
+		$this->skipTestOnTimeout( $response );
+
+		$headers = wp_remote_retrieve_headers( $response );
 
 		// should return the same headers as a head request
 		$this->assertInternalType( 'array', $headers, "Reply wasn't array." );
 		$this->assertEquals( 'image/jpeg', $headers['content-type'] );
 		$this->assertEquals( '40148', $headers['content-length'] );
-		$this->assertEquals( '200', $headers['response'] );
-
-		// make sure the file is ok
-		$this->assertEquals( 40148, filesize($file) );
-		$this->assertEquals( 'b0371a0fc575fcf77f62cd298571f53b', md5_file($file) );
+		$this->assertEquals( '200', wp_remote_retrieve_response_code( $response ) );
 	}
 
 	function test_get_redirect() {
 		// this will redirect to asdftestblog1.files.wordpress.com
 		$url = 'https://asdftestblog1.wordpress.com/files/2007/09/2007-06-30-dsc_4700-1.jpg';
-		$file = tempnam('/tmp', 'testfile');
 
-		$headers = wp_get_http($url, $file);
+		$response = wp_remote_get( $url );
+
+		$this->skipTestOnTimeout( $response );
+
+		$headers = wp_remote_retrieve_headers( $response );
 
 		// should return the same headers as a head request
 		$this->assertInternalType( 'array', $headers, "Reply wasn't array." );
 		$this->assertEquals( 'image/jpeg', $headers['content-type'] );
 		$this->assertEquals( '40148', $headers['content-length'] );
-		$this->assertEquals( '200', $headers['response'] );
-
-		// make sure the file is ok
-		$this->assertEquals( 40148, filesize($file) );
-		$this->assertEquals( 'b0371a0fc575fcf77f62cd298571f53b', md5_file($file) );
+		$this->assertEquals( '200', wp_remote_retrieve_response_code( $response ) );
 	}
 
 	function test_get_redirect_limit_exceeded() {
 		// this will redirect to asdftestblog1.files.wordpress.com
 		$url = 'https://asdftestblog1.wordpress.com/files/2007/09/2007-06-30-dsc_4700-1.jpg';
-		$file = tempnam('/tmp', 'testfile');
+
 		// pretend we've already redirected 5 times
-		$headers = wp_get_http( $url, $file, 6 );
-		$this->assertFalse( $headers );
+		$response = wp_remote_get( $url, array( 'redirection' => -1 ) );
+
+		$this->skipTestOnTimeout( $response );
+		$this->assertTrue( is_wp_error( $response ) );
 	}
 }
