6299c305062dd89c5d6d54b3c26190700bb960e3
John Blackbourn <johnbillion@git.wordpress.org>
2021-02-16 21:18:06 +0000

    Post WordPress 5.7 Beta 3 version bump.
    git-svn-id: https://develop.svn.wordpress.org/trunk@50369 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta3-src';
+$wp_version = '5.7-beta3-50368-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
