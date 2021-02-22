0aac78cfceadbba49c7f74f93e2f5dc037d1ddc3
Jonathan Desrosiers <desrosj@git.wordpress.org>
2021-02-22 15:41:35 +0000

    Post WordPress 5.6.2 version bump.
    The 5.6 branch is now 5.6.3 alpha.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.6@50405 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.2-src';
+$wp_version = '5.6.3-alpha-50405-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
