c7e1c62eba552715e070f861b759db3e197c20b9
tellthemachines <isabel@tellthemachines.com>
2021-03-11 13:50:58 +1100

    Target edit links directly instead of tabbing to them.

--- tests/e2e/specs/edit-posts.test.js
+++ tests/e2e/specs/edit-posts.test.js
@@ -47,7 +47,9 @@ describe( 'Edit Posts', () => {
 
 	it( 'displays a message in the posts table when no posts are present', async () => {
 		await visitAdminPage( '/edit.php' );
-		const noPostsMessage = await page.$x( '//td[text()="No posts found."]' );
+		const noPostsMessage = await page.$x(
+			'//td[text()="No posts found."]'
+		);
 		expect( noPostsMessage.length ).toBe( 1 );
 	} );
 
@@ -64,52 +66,55 @@ describe( 'Edit Posts', () => {
 		const [ firstPost ] = posts;
 
 		// Expect the title of the post to be correct.
-		const postTitle = await firstPost.$x( `//a[contains(@class, "row-title")][contains(text(), "${ title }")]` );
+		const postTitle = await firstPost.$x(
+			`//a[contains(@class, "row-title")][contains(text(), "${ title }")]`
+		);
 		expect( postTitle.length ).toBe( 1 );
 	} );
 
-	it( 'allows an existing post to be edited using the Edit button', async() => {
+	it( 'allows an existing post to be edited using the Edit button', async () => {
 		const title = 'Test Title';
 		await createNewPost( { title } );
 		await publishPost();
 		await visitAdminPage( '/edit.php' );
 
-		// Skip to the main content using the shortcut.
-		await pressKeyTimes( 'Tab', 1 );
-		await page.keyboard.press( 'Enter' );
-
-		// Tab to the Edit Post link
-		await pressKeyTimes( 'Tab', 20 );
+		// Click the post title (edit) link
+		const [ editLink ] = await page.$x(
+			`//a[contains(@class, "row-title")][contains(text(), "${ title }")]`
+		);
+		await editLink.click();
 
 		// Edit the post.
-		await page.keyboard.press( 'Enter' );
 		await page.waitForNavigation();
 
 		// Expect to now be in the editor with the correct post title shown.
-		const editorPostTitleInput = await page.$x( `//textarea[contains(@class, "editor-post-title__input")][contains(text(), "${ title }")]` );
+		const editorPostTitleInput = await page.$x(
+			`//textarea[contains(@class, "editor-post-title__input")][contains(text(), "${ title }")]`
+		);
 		expect( editorPostTitleInput.length ).toBe( 1 );
 	} );
 
-	it( 'allows an existing post to be quick edited using the Quick Edit button', async() => {
+	it( 'allows an existing post to be quick edited using the Quick Edit button', async () => {
 		const title = 'Test Title';
 		await createNewPost( { title } );
 		await publishPost();
 		await visitAdminPage( '/edit.php' );
 
-		// Skip to the main content using the shortcut.
-		await pressKeyTimes( 'Tab', 1 );
-		await page.keyboard.press( 'Enter' );
+		// Focus on the post title link.
+		const [ editLink ] = await page.$x(
+			`//a[contains(@class, "row-title")][contains(text(), "${ title }")]`
+		);
+		await editLink.focus();
 
 		// Tab to the Quick Edit button and press Enter to quick edit.
-		await pressKeyTimes( 'Tab', 21 );
+		await pressKeyTimes( 'Tab', 2 );
 		await page.keyboard.press( 'Enter' );
 
 		// Type in the currently focused (title) field to modify the title, testing that focus is moved to the input.
 		await page.keyboard.type( ' Edited' );
 
 		// Update the post.
-		await pressKeyTimes( 'Tab', 17 );
-		await page.keyboard.press( 'Enter' );
+		await page.click( '.button.save' );
 
 		// Wait for the quick edit button to reappear.
 		await page.waitForSelector( 'button.editinline', { visible: true } );
@@ -121,7 +126,9 @@ describe( 'Edit Posts', () => {
 		const [ firstPost ] = posts;
 
 		// Expect the title of the post to be correct.
-		const postTitle = await firstPost.$x( `//a[contains(@class, "row-title")][contains(text(), "${ title } Edited")]` );
+		const postTitle = await firstPost.$x(
+			`//a[contains(@class, "row-title")][contains(text(), "${ title } Edited")]`
+		);
 		expect( postTitle.length ).toBe( 1 );
 	} );
 } );
