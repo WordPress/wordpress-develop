166ed8c51f4ad69ffed1745821e38582ff5dc0b3
Michael Joseph Panaga <donmhico@gmail.com>
2021-03-26 22:20:46 +0800

    Fix CS

--- tests/phpunit/tests/formatting/wpTrimExcerpt.php
+++ tests/phpunit/tests/formatting/wpTrimExcerpt.php
@@ -117,7 +117,7 @@ class Tests_Formatting_wpTrimExcerpt extends WP_UnitTestCase {
 		);
 
 		// Default behavior
-		add_filter( 'excerpt_more', array( $this, 'remove_excerpt_more') );
+		add_filter( 'excerpt_more', array( $this, 'remove_excerpt_more' ) );
 		$this->assertSame( $words_55, wp_trim_excerpt( '', $post ) );
 		$this->assertSame( 'Overwrite', wp_trim_excerpt( 'Overwrite', $post ) );
 
