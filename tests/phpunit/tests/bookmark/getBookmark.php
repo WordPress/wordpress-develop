a93df3797b59ffdbf85dc918991df83677fa6611
hellofromtonya <hello@hellofromtonya.com>
2021-04-07 13:12:23 -0500

    Adds tests when given bookmark instance.

--- tests/phpunit/tests/bookmark/getBookmark.php
+++ tests/phpunit/tests/bookmark/getBookmark.php
@@ -18,7 +18,7 @@ class Tests_Bookmark_GetBookmark extends WP_UnitTestCase {
 	 *
 	 * @var stdClass
 	 */
-	private static $bookmark;
+	private $bookmark;
 
 	/**
 	 * Setup the test environment before running the tests in this class.
@@ -31,8 +31,6 @@ class Tests_Bookmark_GetBookmark extends WP_UnitTestCase {
 			self::$pre_state['global_link'] = $GLOBALS['link'];
 			unset( $GLOBALS['link'] );
 		}
-
-		self::$bookmark = $factory->bookmark->create_and_get();
 	}
 
 	/**
@@ -47,12 +45,19 @@ class Tests_Bookmark_GetBookmark extends WP_UnitTestCase {
 		parent::tearDownAfterClass();
 	}
 
+	public function setUp() {
+		parent::setUp();
+
+		$this->bookmark = $this->factory()->bookmark->create_and_get();
+		wp_cache_delete( $this->bookmark->link_id, 'bookmark' );
+	}
+
 	/**
 	 * Clean up the global and cache state after each test.
 	 */
 	public function tearDown() {
 		unset( $GLOBALS['link'] );
-		wp_cache_delete( self::$bookmark->link_id, 'bookmark' );
+		wp_cache_delete( $this->bookmark->link_id, 'bookmark' );
 
 		parent::tearDown();
 	}
@@ -67,14 +72,14 @@ class Tests_Bookmark_GetBookmark extends WP_UnitTestCase {
 		$this->assertNull( $actual_bookmark );
 
 		// Should bypass the cache.
-		$this->assertFalse( wp_cache_get( self::$bookmark->link_id, 'bookmark' ) );
+		$this->assertFalse( wp_cache_get( $this->bookmark->link_id, 'bookmark' ) );
 	}
 
 	/**
 	 * @dataProvider data_when_given_0_bookmark
 	 */
 	public function test_should_return_global_link_in_requested_output_format( $args ) {
-		$GLOBALS['link'] = self::$bookmark;
+		$GLOBALS['link'] = $this->bookmark;
 		$actual_bookmark = get_bookmark( ...$args );
 
 		// When given, convert the instance into the expected array format.
@@ -90,7 +95,7 @@ class Tests_Bookmark_GetBookmark extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'link', $GLOBALS );
 		$this->assertSame( $expected, $actual_bookmark );
 		// Should bypass the cache.
-		$this->assertFalse( wp_cache_get( self::$bookmark->link_id, 'bookmark' ) );
+		$this->assertFalse( wp_cache_get( $this->bookmark->link_id, 'bookmark' ) );
 	}
 
 	public function data_when_given_0_bookmark() {
@@ -98,6 +103,47 @@ class Tests_Bookmark_GetBookmark extends WP_UnitTestCase {
 			'with default args'                  => array( array( 0 ) ),
 			'with non-default output'            => array( array( 0, ARRAY_A ) ),
 			'with non-default output and filter' => array( array( 0, ARRAY_N, 'edit' ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider data_when_given_bookmark_instance
+	 */
+	public function test_should_cache_bookmark_when_given_instance( $output = OBJECT, $filter = 'raw' ) {
+		// Check the cache does not exist before the test.
+		$this->assertFalse( wp_cache_get( $this->bookmark->link_id, 'bookmark' ) );
+
+		get_bookmark( $this->bookmark, $output, $filter );
+
+		// Check the bookmark was cached.
+		$actual_cache = wp_cache_get( $this->bookmark->link_id, 'bookmark' );
+		$this->assertEquals( $this->bookmark, $actual_cache );
+	}
+
+	/**
+	 * @dataProvider data_when_given_bookmark_instance
+	 */
+	public function test_should_return_in_requested_output_format_when_given_instance( $output = OBJECT, $filter = 'raw' ) {
+		// Convert the instance into the expected array format.
+		if ( in_array( $output, array( ARRAY_A, ARRAY_N ), true ) ) {
+			$expected = get_object_vars( $this->bookmark );
+			if ( ARRAY_N === $output ) {
+				$expected = array_values( $expected );
+			}
+		} else {
+			$expected = $this->bookmark;
+		}
+
+		$actual_bookmark = get_bookmark( $this->bookmark, $output, $filter );
+
+		$this->assertSame( $expected, $actual_bookmark );
+	}
+
+	public function data_when_given_bookmark_instance() {
+		return array(
+			'with default args'                  => array(),
+			'with non-default output'            => array( ARRAY_A ),
+			'with non-default output and filter' => array( ARRAY_N, 'display' ),
 		);
 	}
 }
