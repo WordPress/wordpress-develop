0e78065d9d2ea0bd3b45a23f2aa4ebd431ee365c
Riad Benguella <benguella@gmail.com>
2021-04-15 13:19:19 +0100

    Update block supports

--- src/wp-includes/block-supports/custom-classname.php
+++ src/wp-includes/block-supports/custom-classname.php
@@ -8,15 +8,11 @@
 /**
  * Registers the custom classname block attribute for block types that support it.
  *
- * @access private
- *
  * @param WP_Block_Type $block_type Block Type.
  */
 function wp_register_custom_classname_support( $block_type ) {
-	$has_custom_classname_support = true;
-	if ( property_exists( $block_type, 'supports' ) ) {
-		$has_custom_classname_support = _wp_array_get( $block_type->supports, array( 'customClassName' ), true );
-	}
+	$has_custom_classname_support = block_has_support( $block_type, array( 'customClassName' ), true );
+
 	if ( $has_custom_classname_support ) {
 		if ( ! $block_type->attributes ) {
 			$block_type->attributes = array();
@@ -33,19 +29,14 @@ function wp_register_custom_classname_support( $block_type ) {
 /**
  * Add the custom classnames to the output.
  *
- * @access private
- *
  * @param  WP_Block_Type $block_type       Block Type.
  * @param  array         $block_attributes Block attributes.
  *
  * @return array Block CSS classes and inline styles.
  */
 function wp_apply_custom_classname_support( $block_type, $block_attributes ) {
-	$has_custom_classname_support = true;
+	$has_custom_classname_support = block_has_support( $block_type, array( 'customClassName' ), true );
 	$attributes                   = array();
-	if ( property_exists( $block_type, 'supports' ) ) {
-		$has_custom_classname_support = _wp_array_get( $block_type->supports, array( 'customClassName' ), true );
-	}
 	if ( $has_custom_classname_support ) {
 		$has_custom_classnames = array_key_exists( 'className', $block_attributes );
 
