ac9b87f52b5f18e1ccc5c9f7b5c4009ad6bf0cb3
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-04-27 19:49:29 +0200

    removed default domain from translatable strings

--- src/wp-includes/block-patterns/quote.php
+++ src/wp-includes/block-patterns/quote.php
@@ -6,7 +6,7 @@
  */
 
 return array(
-	'title'       => _x( 'Quote', 'Block pattern title', 'default' ),
+	'title'       => _x( 'Quote', 'Block pattern title' ),
 	'categories'  => array( 'text' ),
 	'blockTypes'  => array( 'core/quote' ),
 	'content'     => '<!-- wp:group -->
@@ -15,16 +15,16 @@ return array(
 	<!-- /wp:separator -->
 	
 	<!-- wp:image {"align":"center","id":null,"width":150,"height":150,"sizeSlug":"large","linkDestination":"none","className":"is-style-rounded"} -->
-	<div class="wp-block-image is-style-rounded"><figure class="aligncenter size-large is-resized"><img src="https://s.w.org/images/core/5.8/portrait.jpg" alt="' . esc_attr__( 'A side profile of a woman in a russet-colored turtleneck and white bag. She looks up with her eyes closed.', 'default' ) . '" width="150" height="150"/></figure></div>
+	<div class="wp-block-image is-style-rounded"><figure class="aligncenter size-large is-resized"><img src="https://s.w.org/images/core/5.8/portrait.jpg" alt="' . esc_attr__( 'A side profile of a woman in a russet-colored turtleneck and white bag. She looks up with her eyes closed.' ) . '" width="150" height="150"/></figure></div>
 	<!-- /wp:image -->
 	
 	<!-- wp:quote {"align":"center","className":"is-style-large"} -->
-	<blockquote class="wp-block-quote has-text-align-center is-style-large"><p>' . esc_html__( "\"Contributing makes me feel like I'm being useful to the planet.\"", 'default' ) . '</p><cite>' . wp_kses_post( __( '— Anna Wong, <em>Volunteer</em>', 'default' ) ) . '</cite></blockquote>
+	<blockquote class="wp-block-quote has-text-align-center is-style-large"><p>' . esc_html__( "\"Contributing makes me feel like I'm being useful to the planet.\"" ) . '</p><cite>' . wp_kses_post( __( '— Anna Wong, <em>Volunteer</em>' ) ) . '</cite></blockquote>
 	<!-- /wp:quote -->
 	
 	<!-- wp:separator {"className":"is-style-default"} -->
 	<hr class="wp-block-separator is-style-default"/>
 	<!-- /wp:separator --></div></div>
 	<!-- /wp:group -->',
-	'description' => _x( 'Testimonial quote with portrait', 'Block pattern description', 'default' ),
+	'description' => _x( 'Testimonial quote with portrait', 'Block pattern description' ),
 );
--- src/wp-includes/block-patterns/three-images-gallery.php
+++ src/wp-includes/block-patterns/three-images-gallery.php
@@ -6,12 +6,12 @@
  */
 
 return array(
-	'title'       => _x( 'Three columns with offset images', 'Block pattern title', 'default' ),
+	'title'       => _x( 'Three columns with offset images', 'Block pattern title' ),
 	'categories'  => array( 'gallery' ),
 	'content'     => '<!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"25%"} -->
 	<div class="wp-block-column" style="flex-basis:25%"><!-- wp:image {"id":null,"sizeSlug":"large","linkDestination":"none","className":"is-style-default"} -->
-	<figure class="wp-block-image size-large is-style-default"><img src="https://s.w.org/images/core/5.8/architecture-01.jpg" alt="' . esc_attr__( 'Close-up, abstract view of geometric architecture.', 'default' ) . '" /></figure>
+	<figure class="wp-block-image size-large is-style-default"><img src="https://s.w.org/images/core/5.8/architecture-01.jpg" alt="' . esc_attr__( 'Close-up, abstract view of geometric architecture.' ) . '" /></figure>
 	<!-- /wp:image --></div>
 	<!-- /wp:column -->
 	
@@ -25,13 +25,13 @@ return array(
 	<!-- /wp:spacer -->
 	
 	<!-- wp:image {"id":null,"sizeSlug":"large","linkDestination":"none"} -->
-	<figure class="wp-block-image size-large"><img src="https://s.w.org/images/core/5.8/architecture-02.jpg" alt="' . esc_attr__( 'Close-up, angled view of a window on a white building.', 'default' ) . '" /></figure>
+	<figure class="wp-block-image size-large"><img src="https://s.w.org/images/core/5.8/architecture-02.jpg" alt="' . esc_attr__( 'Close-up, angled view of a window on a white building.' ) . '" /></figure>
 	<!-- /wp:image --></div>
 	<!-- /wp:column -->
 	
 	<!-- wp:column {"width":"45%"} -->
 	<div class="wp-block-column" style="flex-basis:45%"><!-- wp:image {"id":null,"sizeSlug":"large","linkDestination":"none","className":"is-style-default"} -->
-	<figure class="wp-block-image size-large is-style-default"><img src="https://s.w.org/images/core/5.8/architecture-03.jpg" alt="' . esc_attr__( 'Close-up of the corner of a white, geometric building with both sharp points and round corners.', 'default' ) . '" /></figure>
+	<figure class="wp-block-image size-large is-style-default"><img src="https://s.w.org/images/core/5.8/architecture-03.jpg" alt="' . esc_attr__( 'Close-up of the corner of a white, geometric building with both sharp points and round corners.' ) . '" /></figure>
 	<!-- /wp:image -->
 	
 	<!-- wp:spacer {"height":285} -->
@@ -39,5 +39,5 @@ return array(
 	<!-- /wp:spacer --></div>
 	<!-- /wp:column --></div>
 	<!-- /wp:columns -->',
-	'description' => _x( 'Three columns with offset images', 'Block pattern description', 'default' ),
+	'description' => _x( 'Three columns with offset images', 'Block pattern description' ),
 );
