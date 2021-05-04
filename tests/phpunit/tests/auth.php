ee096ea61554a56e4e8e88cdf7eddaf758b0abe7
pbearne <pbearne@gmail.com>
2021-05-04 07:52:14 -0400

    Fixed Root tests

--- tests/phpunit/tests/auth.php
+++ tests/phpunit/tests/auth.php
@@ -124,7 +124,7 @@ class Tests_Auth extends WP_UnitTestCase {
 	function test_wp_hash_password_trimming() {
 
 		$password = ' pass with leading whitespace';
-		$this->assertTrue( wp_check_password( 'pass with leading whitespace', ( $password ) ) );
+		$this->assertTrue( wp_check_password( 'pass with leading whitespace', wp_hash_password( $password ) ) );
 
 		$password = 'pass with trailing whitespace ';
 		$this->assertTrue( wp_check_password( 'pass with trailing whitespace', wp_hash_password( $password ) ) );
