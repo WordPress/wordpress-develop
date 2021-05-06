65c6d19fc17ddde3e205f0524e30aec02ececb7c
JustinyAhin <justiny.ahinon@gmail.com>
2021-05-06 08:44:47 +0100

    Update add-new-category.test.js

--- tests/e2e/specs/add-new-category.test.js
+++ tests/e2e/specs/add-new-category.test.js
@@ -1,20 +1,46 @@
+/**
+ * WordPress dependencies
+ */
 import {
 	visitAdminPage,
 	pressKeyWithModifier
 } from '@wordpress/e2e-test-utils'
+import { addQueryArgs } from '@wordpress/url';
 
-/* Test scenario
+
+/** Test scenario
  * Go to the page /edit-tags.php?taxonomy=category
+ * If there is any other existing categorie apart from "Uncategorized", delete it
  * Add a category name
  * Click on the "Add New Category" button
  * Check if the new category appears in the category list
  */
 
 describe( 'Add new category', () => {
+	const query = addQueryArgs( '', {
+		taxonomy: 'category',
+	} );
+
 	it( 'shows the new category in the category list after it is added', async () => {
+		await visitAdminPage( 'edit-tags.php', query );
+
+		/**
+		 * Delete all categories before anything
+		 * This is useful because after running more than one test
+		 * there could be existing categories
+		 */
+		const bulkSelector = await page.$( '#bulk-action-selector-top' );
+
+		await page.waitForSelector( '[id^=cb-select-all-]' );
+		await page.click( '[id^=cb-select-all-]' );
+
+		// Select the "bulk actions" > "delete" option.
+		await page.select( '#bulk-action-selector-top', 'delete' );
+
+		// Submit the form to delete all existing cateogies (except the default "Uncategorized")
+		await page.click( '#doaction' );
+
 		const title = 'New Category';
-		
-		await visitAdminPage( '/edit-tags.php?taxonomy=category' );
 
 		await page.waitForSelector('#tag-name');
 		await page.focus( '#tag-name' );
@@ -23,17 +49,10 @@ describe( 'Add new category', () => {
 
 		await page.click( '#submit' );
 
-		// Expect one category and only one to have the "tag-2" id
-		// When a new category is created WordPress add the "tag-x"
-		// id to the category line in the list.
-		// The first one being the "Uncategorized" category
-		const newCategory = await page.$$( '#the-list #tag-2' );
-		expect( newCategory.length ).toBe( 1 );
+		await page.reload();
 
-		// Expect the title of the new created category to be correct
-		const categoryTitle = await newCategory.$x(
-			`//a[contains(@class, "row-title")][contains(text(), "${ title }")]`
-		);
-		expect( categoryTitle.length ).toBe( 1 );
+		// Expect there to be one row in the post list.
+		const categories = await page.$$( '#the-list tr' );
+		expect( categories.length ).toBe( 2 )
 	} );
 } );
