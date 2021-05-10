55c381502d3900c785a4e40813ccaf2dd0025d6d
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2021-05-10 12:02:16 -0400

    Continue registering the polyfill scripts, but don't load them in Core.

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -84,15 +84,29 @@ function wp_default_packages_vendor( $scripts ) {
 		'react-dom' => array( 'react' ),
 		'moment',
 		'lodash',
+		'wp-polyfill-fetch',
+		'wp-polyfill-formdata',
+		'wp-polyfill-node-contains',
+		'wp-polyfill-url',
+		'wp-polyfill-dom-rect',
+		'wp-polyfill-element-closest',
+		'wp-polyfill-object-fit',
 		'wp-polyfill',
 	);
 
 	$vendor_scripts_versions = array(
-		'react'       => '16.13.1',
-		'react-dom'   => '16.13.1',
-		'moment'      => '2.29.1',
-		'lodash'      => '4.17.19',
-		'wp-polyfill' => '7.4.4',
+		'react'                       => '16.13.1',
+		'react-dom'                   => '16.13.1',
+		'moment'                      => '2.29.1',
+		'lodash'                      => '4.17.19',
+		'wp-polyfill-fetch'           => '3.0.0',
+		'wp-polyfill-formdata'        => '3.0.20',
+		'wp-polyfill-node-contains'   => '3.104.0',
+		'wp-polyfill-url'             => '3.6.4',
+		'wp-polyfill-dom-rect'        => '3.104.0',
+		'wp-polyfill-element-closest' => '2.0.2',
+		'wp-polyfill-object-fit'      => '2.3.5',
+		'wp-polyfill'                 => '7.4.4',
 	);
 
 	foreach ( $vendor_scripts as $handle => $dependencies ) {
