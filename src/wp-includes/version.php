d7001071ddbcf7fdfa59f4dc51430487781aa388
Peter Wilson <peterwilsoncc@git.wordpress.org>
2021-05-12 23:13:44 +0000

    WordPress 5.3.8.
    git-svn-id: https://develop.svn.wordpress.org/branches/5.3@50873 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.7-src';
+$wp_version = '5.3.8-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
