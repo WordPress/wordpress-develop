87b78667c2565708f09c2aae8a762432695b21f4
pbearne <pbearne@gmail.com>
2021-05-13 12:44:14 -0400

    Fixed setup/tearDown

--- tests/phpunit/tests/comment/getCommentsPagesCount.php
+++ tests/phpunit/tests/comment/getCommentsPagesCount.php
@@ -16,8 +16,8 @@ class Tests_Comment_GetCommentsPagesCount extends WP_UnitTestCase {
 	function setUp() {
 		parent::setUp();
 		$this->option_page_comments = get_option( 'page_comments' );
-		$this->option_page_comments = get_option( 'comments_per_page' );
-		$this->option_page_comments = get_option( 'thread_comments' );
+		$this->option_comments_per_page = get_option( 'comments_per_page' );
+		$this->option_thread_comments = get_option( 'thread_comments' );
 		$this->option_posts_per_rss = get_option( 'posts_per_rss' );
 
 		update_option( 'page_comments', true );
@@ -28,8 +28,8 @@ class Tests_Comment_GetCommentsPagesCount extends WP_UnitTestCase {
 	 */
 	function tearDown() {
 		update_option( 'page_comments', $this->option_page_comments );
-		update_option( 'comments_per_page', $this->option_page_comments );
-		update_option( 'thread_comments', $this->option_page_comments );
+		update_option( 'comments_per_page', $this->option_comments_per_page );
+		update_option( 'thread_comments', $this->option_thread_comments );
 		update_option( 'posts_per_rss', $this->option_posts_per_rss );
 		parent::tearDown();
 	}
