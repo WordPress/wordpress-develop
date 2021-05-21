f5087bc468da95f49e5f337c1a185801add55c88
Herm Martini <hermpheus@gmail.com>
2021-05-21 08:01:28 -0700

    Re-order classes to satisfy coding standards
    If the first class in a file is not a test class, the coding standards
    insist that the filename be named after it.
    
    Fixed by placing the widget classes after the test class.

--- tests/phpunit/tests/widgets-namespaced.php
+++ tests/phpunit/tests/widgets-namespaced.php
@@ -1,22 +1,5 @@
 <?php
-// An example test namespaced widget
-namespace TestSpace\Sub\Sub {
-	class Testwidget extends \WP_Widget {
-		function __construct() {
-			// An empty first argument is passed to use the default id_base calculation
-			parent::__construct( '', 'unit-test-widget' );
-		}
-	}
-}
-
 namespace {
-	// A non-namespaced widget to ensure previous behavior still works
-	class Testwidget2 extends \WP_Widget {
-		function __construct() {
-			parent::__construct( '', 'unit-test-widget' );
-		}
-	}
-
 	/**
 	 * Test functions and classes for namespaced widgets.
 	 *
@@ -46,6 +29,23 @@ namespace {
 			global $wp_widget_factory;
 			register_widget( 'Testwidget2' );
 			$this->assertEquals( 'testwidget2', $wp_widget_factory->widgets['Testwidget2']->id_base );
+		}
+	}
+
+	// A non-namespaced widget to ensure previous behavior still works
+	class Testwidget2 extends \WP_Widget {
+		function __construct() {
+			parent::__construct( '', 'unit-test-widget' );
+		}
+	}
+}
+
+// An example test namespaced widget
+namespace TestSpace\Sub\Sub {
+	class Testwidget extends \WP_Widget {
+		function __construct() {
+			// An empty first argument is passed to use the default id_base calculation
+			parent::__construct( '', 'unit-test-widget' );
 		}
 	}
 }
