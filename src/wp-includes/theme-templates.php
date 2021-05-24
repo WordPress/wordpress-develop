0d0a58cc402df768cbdb82c51b31a15b3cfcb3d7
Riad Benguella <benguella@gmail.com>
2021-05-24 12:19:31 +0100

    clean theme-templates.php

--- src/wp-includes/theme-templates.php
+++ src/wp-includes/theme-templates.php
@@ -1,40 +1,81 @@
 <?php
 
 /**
- * Sets a custom slug when creating auto-draft templates.
- * This is only needed for auto-drafts created by the regular WP editor.
- * If this page is to be removed, this won't be necessary.
+ * Generates a unique slug for templates.
+ * 
+ * @access private
+ * @since 5.8.0
  *
- * @param int $post_id Post ID.
+ * @param string $override_slug The filtered value of the slug (starts as `null` from apply_filter).
+ * @param string $slug          The original/un-filtered slug (post_name).
+ * @param int    $post_ID       Post ID.
+ * @param string $post_status   No uniqueness checks are made if the post is still draft or pending.
+ * @param string $post_type     Post type.
+ * @return string The original, desired slug.
  */
-function set_unique_slug_on_create_template( $post_id ) {
-	$post = get_post( $post_id );
-	if ( 'auto-draft' !== $post->post_status ) {
-		return;
+function wp_filter_wp_template_unique_post_slug( $override_slug, $slug, $post_ID, $post_status, $post_type ) {
+	if ( 'wp_template' !== $post_type ) {
+		return $override_slug;
 	}
 
-	if ( ! $post->post_name ) {
-		wp_update_post(
+	if ( ! $override_slug ) {
+		$override_slug = $slug;
+	}
+
+	// Template slugs must be unique within the same theme.
+	// TODO - Figure out how to update this to work for a multi-theme
+	// environment.  Unfortunately using `get_the_terms` for the 'wp-theme'
+	// term does not work in the case of new entities since is too early in
+	// the process to have been saved to the entity.  So for now we use the
+	// currently activated theme for creation.
+	$theme = wp_get_theme()->get_stylesheet();
+	$terms = get_the_terms( $post_ID, 'wp_theme' );
+	if ( $terms && ! is_wp_error( $terms ) ) {
+		$theme = $terms[0]->name;
+	}
+
+	$check_query_args = array(
+		'post_name__in'  => array( $override_slug ),
+		'post_type'      => $post_type,
+		'posts_per_page' => 1,
+		'no_found_rows'  => true,
+		'post__not_in'   => array( $post_ID ),
+		'tax_query'      => array(
 			array(
-				'ID'        => $post_id,
-				'post_name' => 'custom_slug_' . uniqid(),
-			)
-		);
+				'taxonomy' => 'wp_theme',
+				'field'    => 'name',
+				'terms'    => $theme,
+			),
+		),
+	);
+	$check_query      = new WP_Query( $check_query_args );
+	$posts            = $check_query->get_posts();
+
+	if ( count( $posts ) > 0 ) {
+		$suffix = 2;
+		do {
+			$query_args                  = $check_query_args;
+			$alt_post_name               = _truncate_post_slug( $override_slug, 200 - ( strlen( $suffix ) + 1 ) ) . "-$suffix";
+			$query_args['post_name__in'] = array( $alt_post_name );
+			$query                       = new WP_Query( $query_args );
+			$suffix++;
+		} while ( count( $query->get_posts() ) > 0 );
+		$override_slug = $alt_post_name;
 	}
 
-	$terms = get_the_terms( $post_id, 'wp_theme' );
-	if ( ! $terms || ! count( $terms ) ) {
-		wp_set_post_terms( $post_id, wp_get_theme()->get_stylesheet(), 'wp_theme' );
-	}
+	return $override_slug;
 }
-add_action( 'save_post_wp_template', 'set_unique_slug_on_create_template' );
+add_filter( 'pre_wp_unique_post_slug', 'wp_filter_wp_template_unique_post_slug', 10, 5 );
 
 /**
  * Print the skip-link script & styles.
  *
+ * @access private
+ * @since 5.8.0
+ * 
  * @return void
  */
-function gutenberg_the_skip_link() {
+function the_block_template_skip_link() {
 
 	// Early exit if not an FSE theme.
 	if ( ! theme_supports_block_templates() ) {
@@ -110,7 +151,7 @@ function gutenberg_the_skip_link() {
 		skipLink = document.createElement( 'a' );
 		skipLink.classList.add( 'skip-link', 'screen-reader-text' );
 		skipLink.href = '#' + skipLinkTargetID;
-		skipLink.innerHTML = '<?php esc_html_e( 'Skip to content', 'gutenberg' ); ?>';
+		skipLink.innerHTML = '<?php esc_html_e( 'Skip to content' ); ?>';
 
 		// Inject the skip link.
 		parentEl.insertAdjacentElement( 'afterbegin', skipLink );
@@ -118,7 +159,7 @@ function gutenberg_the_skip_link() {
 	</script>
 	<?php
 }
-add_action( 'wp_footer', 'gutenberg_the_skip_link' );
+add_action( 'wp_footer', 'the_block_template_skip_link' );
 
 // By default, themes support block templates.
 add_theme_support( 'block-templates' );
