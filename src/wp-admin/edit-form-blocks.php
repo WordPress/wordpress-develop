c9f5176c1c50d12f162dc796362e6bbc60606bf9
Riad Benguella <benguella@gmail.com>
2021-05-24 09:01:07 +0100

    Add editor setting to enable template mode

--- src/wp-admin/edit-form-blocks.php
+++ src/wp-admin/edit-form-blocks.php
@@ -212,6 +212,7 @@ $editor_settings = array(
 	'supportsLayout'                       => WP_Theme_JSON_Resolver::theme_has_support(),
 	'__experimentalBlockPatterns'          => WP_Block_Patterns_Registry::get_instance()->get_all_registered(),
 	'__experimentalBlockPatternCategories' => WP_Block_Pattern_Categories_Registry::get_instance()->get_all_registered(),
+	'supportsTemplateMode'                 => gutenberg_supports_block_templates(),
 
 	// Whether or not to load the 'postcustom' meta box is stored as a user meta
 	// field so that we're not always loading its assets.
