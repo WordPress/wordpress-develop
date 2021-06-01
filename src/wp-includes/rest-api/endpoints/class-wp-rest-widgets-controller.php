342660fafaa96aa063edd826ef8bc7f3996af2a0
Robert Anderson <robert@noisysocks.com>
2021-06-01 14:27:01 +1000

    Add @global to docblock

--- src/wp-includes/rest-api/endpoints/class-wp-rest-widgets-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-widgets-controller.php
@@ -283,6 +283,8 @@ class WP_REST_Widgets_Controller extends WP_REST_Controller {
 	 *
 	 * @since 5.8.0
 	 *
+	 * @global array $wp_registered_widget_updates The registered widget update functions.
+	 *
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
