6959f570a5bdd21371d16474a248928fa6dc6185
ramonjd <ramonjd@gmail.com>
2021-06-17 22:00:50 +1000

    Coverting both double and single quotes. Using blog's charset Updating tests to check for single and double quote conversion.

--- tests/phpunit/tests/rest-api/rest-widget-types-controller.php
+++ tests/phpunit/tests/rest-api/rest-widget-types-controller.php
@@ -500,17 +500,17 @@ class WP_Test_REST_Widget_Types_Controller extends WP_Test_REST_Controller_Testc
 		$widget_id = 'archives';
 		wp_register_sidebar_widget(
 			$widget_id,
-			'Legacy &#8209; Archive &#8209; Widget',
+			'&#8216;Legacy&#8217; &#8209; Archive &#8209; Widget',
 			function() {},
 			array(
-				'description' => 'A great &amp; interesting archive of your site&#8217;s Posts!',
+				'description' => 'A great &amp; &#8220;interesting&#8221; archive of your site&#8217;s Posts!',
 			)
 		);
 		$request  = new WP_REST_Request( 'GET', '/wp/v2/widget-types/archives' );
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
-		$this->assertSame( 'Legacy ‑ Archive ‑ Widget', $data['name'] );
-		$this->assertSame( 'A great & interesting archive of your site’s Posts!', $data['description'] );
+		$this->assertSame( '‘Legacy’ ‑ Archive ‑ Widget', $data['name'] );
+		$this->assertSame( 'A great & “interesting” archive of your site’s Posts!', $data['description'] );
 	}
 
 	/**
