902df38cd21cfb6db8bb2d99177741a6c102a6c2
hellofromtonya <tonya.mork@automattic.com>
2021-06-17 09:41:28 -0500

    Adds @covers to Tests_Actions_Callbacks.

--- tests/phpunit/tests/actions/callbacks.php
+++ tests/phpunit/tests/actions/callbacks.php
@@ -7,6 +7,8 @@ class Tests_Actions_Callbacks extends WP_UnitTestCase {
 
 	/**
 	 * @ticket 23265
+	 *
+	 * @covers ::add_action
 	 */
 	function test_callback_representations() {
 		$tag = __FUNCTION__;
