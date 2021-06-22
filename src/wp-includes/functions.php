54cd7649332bee88bd79a2f3d6f56291b44bfb38
André <andres.maneiro@automattic.com>
2021-06-22 10:54:16 +0200

    Port tests for _wp_to_kebab_case function

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -4710,6 +4710,9 @@ function _wp_array_set( &$array, $path, $value = null ) {
  * (some of the output of lodash's kebabCase is saved in the post content).
  * We have to make the server behave like the client.
  *
+ * Changes to this function should follow updates in the client
+ * with the same logic.
+ *
  * @link https://github.com/lodash/lodash/blob/4.17/dist/lodash.js#L14369
  * @link https://github.com/lodash/lodash/blob/4.17/dist/lodash.js#L278
  * @link https://github.com/lodash-php/lodash-php/blob/master/src/String/kebabCase.php
