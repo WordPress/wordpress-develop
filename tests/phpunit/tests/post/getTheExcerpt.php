c3b10bd82fc67e41d083314a5e1e107598e6b561
Ari Stathopoulos <aristath@gmail.com>
2021-07-06 17:06:51 +0300

    Add PHPUnit test

--- tests/phpunit/tests/post/getTheExcerpt.php
+++ tests/phpunit/tests/post/getTheExcerpt.php
@@ -145,4 +145,42 @@ class Tests_Post_GetTheExcerpt extends WP_UnitTestCase {
 
 		$this->assertSame( 'Bar', $found );
 	}
+
+	/**
+	 * @ticket 53604
+	 */
+	public function test_inner_blocks_excerpt() {
+		$post_content = '<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Column 1</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Column 2</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->';
+		$post = self::factory()->post->create_and_get(
+			array(
+				'post_content' => $post_content,
+				'post_excerpt' => '',
+			)
+		);
+		$query  = new WP_Query(
+			array(
+				'p' => $post->ID,
+			)
+		);
+
+		$this->assertSame( 'Column 1 Column 2', get_the_excerpt( $query->posts[0] ) );
+	}
 }
