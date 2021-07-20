1f17d7308c0b3a28e588081963376ee9ac99da19
jrfnl <jrfnl@users.noreply.github.com>
2021-07-20 08:50:12 +0200

    Build/Tests: loosen the PHPUnit restriction
    **`composer.json`**:
    
    Remove the PHPUnit dependency in favour of allowing the PHPUnit Polyfills library to manage the supported PHPUnit version.
    This automatically now widens the supported PHPUnit versions to 5.7.21 to 9.5.8 (current).
    
    Letting the PHPUnit Polyfills handle the version constraints for PHPUnit prevents potential version conflicts in the future as well as allows WP to benefit straight away when a new PHPUnit version would be released and the PHPUnit Polyfills package adds support for that PHPUnit version.
    
    **Test Bootstrap**
    
    Update the supported version number for PHPUnit 5.x, as the minimum PHPUnit 5.x version supported by the PHPUnit Polyfills is PHPUnit 5.7.21 and remove the PHPUnit maximum.
    
    **.gitignore:**
    
    Add the PHPUnit cache file to the list of files to be ignored.
    
    Since PHPUnit 8, PHPUnit has a build in caching feature which creates a `.phpunit.result.cache` file. This file should not be committed.
    
    Note: This same change needs to be made to the `.svnignore` file.

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -37,12 +37,12 @@ if ( defined( 'WP_RUN_CORE_TESTS' ) && WP_RUN_CORE_TESTS && ! is_dir( ABSPATH ) 
 
 $phpunit_version = tests_get_phpunit_version();
 
-if ( version_compare( $phpunit_version, '5.7', '<' ) || version_compare( $phpunit_version, '8.0', '>=' ) ) {
+if ( version_compare( $phpunit_version, '5.7.21', '<' ) ) {
 	printf(
-		"Error: Looks like you're using PHPUnit %s. WordPress requires at least PHPUnit 5.7 and is currently only compatible with PHPUnit up to 7.x.\n",
+		"Error: Looks like you're using PHPUnit %s. WordPress requires at least PHPUnit 5.7.21.\n",
 		$phpunit_version
 	);
-	echo "Please use the latest PHPUnit version from the 7.x branch.\n";
+	echo "Please use the latest PHPUnit version supported for the PHP version you are running the tests on.\n";
 	exit( 1 );
 }
 
