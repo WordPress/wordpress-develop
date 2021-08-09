fff4242f1abd4639b45346ae67a6dfbb760dd7ba
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-08-09 17:19:21 +0000

    Code Modernization: Rename the `readonly()` function to `wp_readonly()`.
    Since PHP 8.1, `readonly` is a reserved keyword and cannot be used as a function name.
    
    In order to avoid PHP parser errors, the `readonly()` function was extracted to a separate file and is now only included conditionally on PHP < 8.1.
    
    This commit also:
    * Moves the tests for the `__checked_selected_helper()` function and all the related functions to their own file.
    * Switches to named data providers. This makes the output when using the `--testdox` option more descriptive and is helpful when trying to debug which data set from a data provider failed the test.
    * Improves the tests in question to make them feature-complete and expand test coverage.
    
    Props jrf, ayeshrajans, haosun, knutsp, swissspidy, SergeyBiryukov.
    Fixes #53858.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@51586 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/general/template.php
+++ tests/phpunit/tests/general/template.php
@@ -572,55 +572,6 @@ class Tests_General_Template extends WP_UnitTestCase {
 	}
 
 	/**
-	 * @ticket 9862
-	 * @ticket 51166
-	 * @dataProvider data_selected_and_checked_with_equal_values
-	 *
-	 * @covers ::selected
-	 * @covers ::checked
-	 */
-	function test_selected_and_checked_with_equal_values( $selected, $current ) {
-		$this->assertSame( " selected='selected'", selected( $selected, $current, false ) );
-		$this->assertSame( " checked='checked'", checked( $selected, $current, false ) );
-	}
-
-	function data_selected_and_checked_with_equal_values() {
-		return array(
-			array( 'foo', 'foo' ),
-			array( '1', 1 ),
-			array( '1', true ),
-			array( 1, 1 ),
-			array( 1, true ),
-			array( true, true ),
-			array( '0', 0 ),
-			array( 0, 0 ),
-			array( '', false ),
-			array( false, false ),
-		);
-	}
-
-	/**
-	 * @ticket 9862
-	 * @ticket 51166
-	 * @dataProvider data_selected_and_checked_with_non_equal_values
-	 *
-	 * @covers ::selected
-	 * @covers ::checked
-	 */
-	function test_selected_and_checked_with_non_equal_values( $selected, $current ) {
-		$this->assertSame( '', selected( $selected, $current, false ) );
-		$this->assertSame( '', checked( $selected, $current, false ) );
-	}
-
-	function data_selected_and_checked_with_non_equal_values() {
-		return array(
-			array( '0', '' ),
-			array( 0, '' ),
-			array( 0, false ),
-		);
-	}
-
-	/**
 	 * @ticket 44183
 	 *
 	 * @covers ::get_the_archive_title
