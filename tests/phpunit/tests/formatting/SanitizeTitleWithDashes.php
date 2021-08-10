fe61ac974cd243141670250cbfda4280da141775
costdev <79332690+costdev@users.noreply.github.com>
2021-08-11 00:38:51 +0100

    Update tests/phpunit/tests/formatting/SanitizeTitleWithDashes.php
    Co-authored-by: Tonya Mork <tonya.mork@automattic.com>

--- tests/phpunit/tests/formatting/SanitizeTitleWithDashes.php
+++ tests/phpunit/tests/formatting/SanitizeTitleWithDashes.php
@@ -149,21 +149,105 @@ class Tests_Formatting_SanitizeTitleWithDashes extends WP_UnitTestCase {
 
 	/**
 	 * @ticket 47912
+	 * @dataProvider data_removes_non_visible_characters_without_width
+	 *
+	 * @param string $title     The title to be sanitized.
+	 * @param string $expected  Expected sanitized title.
 	 */
-	function test_removes_non_visible_characters_without_width() {
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%8b', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%8c', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%8d', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%8e', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%8f', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%aa', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%ab', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%ac', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%ad', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%ae', '', 'save' ) );
-		$this->assertSame( '', sanitize_title_with_dashes( '%ef%bb%bf', '', 'save' ) );
-		$this->assertSame( 'invisible', sanitize_title_with_dashes( 'in%ef%bb%bfvisible', '', 'save' ) );
+	public function test_removes_non_visible_characters_without_width( $title, $expected = '' ) {
+		$this->assertSame( $expected, sanitize_title_with_dashes( $title, '', 'save' ) );
 	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_removes_non_visible_characters_without_width() {
+		return array(
+			// Only the non-visible characters.
+			'only %e2%80%8b'     => array( '%e2%80%8b' ),
+			'only %e2%80%8c'     => array( '%e2%80%8c' ),
+			'only %e2%80%8d'     => array( '%e2%80%8d' ),
+			'only %e2%80%8e'     => array( '%e2%80%8e' ),
+			'only %e2%80%8f'     => array( '%e2%80%8f' ),
+			'only %e2%80%aa'     => array( '%e2%80%aa' ),
+			'only %e2%80%ab'     => array( '%e2%80%ab' ),
+			'only %e2%80%ac'     => array( '%e2%80%ac' ),
+			'only %e2%80%ad'     => array( '%e2%80%ad' ),
+			'only %e2%80%ae'     => array( '%e2%80%ae' ),
+			'only %ef%bb%bf'     => array( '%ef%bb%bf' ),
+
+			// Non-visible characters within the title.
+			'in middle of title' => array(
+				'title'    => 'Nonvisible %ef%bb%bfin middle of title',
+				'expected' => 'nonvisible-in-middle-of-title',
+			),
+			'at start of title'  => array(
+				'title'    => '%e2%80%8bNonvisible at start of title',
+				'expected' => 'nonvisible-at-start-of-title',
+			),
+			'at end of title'    => array(
+				'title'    => 'Nonvisible at end of title %e2%80%8b',
+				'expected' => 'nonvisible-at-end-of-title',
+			),
+			'randomly in title'  => array(
+				'title'    => 'Nonvisible%ef%bb%bf %e2%80%aerandomly %e2%80%8ein the %e2%80%8e title%e2%80%8e',
+				'expected' => 'nonvisible-randomly-in-the-title',
+			),
+		);
+	}
+
+	/**
+	 * @ticket 47912
+	 * @dataProvider data_non_visible_characters_without_width_when_not_save
+	 *
+	 * @param string $title     The title to be sanitized.
+	 * @param string $expected  Expected sanitized title.
+	 */
+	public function test_non_visible_characters_without_width_when_not_save( $title, $expected = '' ) {
+		$this->assertSame( $expected, sanitize_title_with_dashes( $title ) );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_non_visible_characters_without_width_when_not_save() {
+		return array(
+			// Just the non-visible characters.
+			'only %e2%80%8b'     => array( '%e2%80%8b', '%e2%80%8b' ),
+			'only %e2%80%8c'     => array( '%e2%80%8c', '%e2%80%8c' ),
+			'only %e2%80%8d'     => array( '%e2%80%8d', '%e2%80%8d' ),
+			'only %e2%80%8e'     => array( '%e2%80%8e', '%e2%80%8e' ),
+			'only %e2%80%8f'     => array( '%e2%80%8f', '%e2%80%8f' ),
+			'only %e2%80%aa'     => array( '%e2%80%aa', '%e2%80%aa' ),
+			'only %e2%80%ab'     => array( '%e2%80%ab', '%e2%80%ab' ),
+			'only %e2%80%ac'     => array( '%e2%80%ac', '%e2%80%ac' ),
+			'only %e2%80%ad'     => array( '%e2%80%ad', '%e2%80%ad' ),
+			'only %e2%80%ae'     => array( '%e2%80%ae', '%e2%80%ae' ),
+			'only %ef%bb%bf'     => array( '%ef%bb%bf', '%ef%bb%bf' ),
+
+			// Non-visible characters within the title.
+			'in middle of title' => array(
+				'title'    => 'Nonvisible %ef%bb%bfin middle of title',
+				'expected' => 'nonvisible-%ef%bb%bfin-middle-of-title',
+			),
+			'at start of title'  => array(
+				'title'    => '%e2%80%8bNonvisible at start of title',
+				'expected' => '%e2%80%8bnonvisible-at-start-of-title',
+			),
+			'at end of title'    => array(
+				'title'    => 'Nonvisible at end of title %e2%80%8b',
+				'expected' => 'nonvisible-at-end-of-title-%e2%80%8b',
+			),
+			'randomly in title'  => array(
+				'title'    => 'Nonvisible%ef%bb%bf %e2%80%aerandomly %e2%80%8ein the %e2%80%8e title%e2%80%8e',
+				'expected' => 'nonvisible%ef%bb%bf-%e2%80%aerandomly-%e2%80%8ein-the-%e2%80%8e-title%e2%80%8e',
+			),
+		);
+	}	
 
 	function test_replaces_non_visible_characters_with_a_width_with_hyphen() {
 		$this->assertSame( '', sanitize_title_with_dashes( '%e2%80%80', '', 'save' ) );
