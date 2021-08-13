20e183f0c9f0fab94b0fbea74e6be48adde98ccb
Andrew Ozz <743931+azaozz@users.noreply.github.com>
2021-08-12 20:42:59 -0700

    Remove unneeded assertion

--- tests/phpunit/tests/functions.php
+++ tests/phpunit/tests/functions.php
@@ -2001,7 +2001,6 @@ class Tests_Functions extends WP_UnitTestCase {
 		$this->assertNotEquals( 'jpeg', wp_get_default_extension_for_mime_type( 'image/jpeg' ), 'jpeg should not be returned as default extension for "image/jpeg"' );
 		$this->assertEquals( 'png', wp_get_default_extension_for_mime_type( 'image/png' ), 'png not returned as default extension for "image/png"' );
 		$this->assertFalse( wp_get_default_extension_for_mime_type( 'wibble/wobble' ), 'false not returned for unrecognized mime type' );
-		$this->assertFalse( wp_get_default_extension_for_mime_type(), 'false not returned when no mime type supplied' );
 		$this->assertFalse( wp_get_default_extension_for_mime_type( '' ), 'false not returned when empty string as mime type supplied' );
 		$this->assertFalse( wp_get_default_extension_for_mime_type( '   ' ), 'false not returned when empty string as mime type supplied' );
 		$this->assertFalse( wp_get_default_extension_for_mime_type( 123 ), 'false not returned when int as mime type supplied' );
