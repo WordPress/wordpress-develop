17eb69e48d2e804dd99ce93e7c8589bda5b90612
pbearne <pbearne@gmail.com>
2021-08-16 14:36:04 -0400

    simplified the tests

--- tests/phpunit/tests/functions/wp_removable_query_args.php
+++ tests/phpunit/tests/functions/wp_removable_query_args.php
@@ -10,52 +10,16 @@ class Tests_Functions_wp_removable_query_args extends WP_UnitTestCase {
 	 * @ticket 53651
 	 */
 	public function test_wp_removable_query_args() {
-		$removable_query_args = array(
-			'activate',
-			'activated',
-			'admin_email_remind_later',
-			'approved',
-			'core-major-auto-updates-saved',
-			'deactivate',
-			'delete_count',
-			'deleted',
-			'disabled',
-			'doing_wp_cron',
-			'enabled',
-			'error',
-			'hotkeys_highlight_first',
-			'hotkeys_highlight_last',
-			'ids',
-			'locked',
-			'message',
-			'same',
-			'saved',
-			'settings-updated',
-			'skipped',
-			'spammed',
-			'trashed',
-			'unspammed',
-			'untrashed',
-			'update',
-			'updated',
-			'wp-post-new-reload',
-		);
 
-		$this->assertSame( $removable_query_args, wp_removable_query_args() );
+		$this->assertNotEmpty( wp_removable_query_args() );
 	}
 
 	/**
 	 * @ticket 53651
 	 */
-	public function test_wp_removable_query_args_filter() {
-		add_filter( 'removable_query_args', array( $this, 'removable_query_args_filter' ) );
+	public function test_wp_removable_query_args_applies_filter() {
+		add_filter( 'removable_query_args', static function( $args ) { return array(); } );
 
-		$this->assertSame( 'removable_query_args_filter', wp_removable_query_args() );
-
-		remove_filter( 'removable_query_args', array( $this, 'removable_query_args_filter' ) );
-	}
-
-	public function removable_query_args_filter() {
-		return 'removable_query_args_filter';
+		$this->assertSame( array(), wp_removable_query_args() );
 	}
 }
