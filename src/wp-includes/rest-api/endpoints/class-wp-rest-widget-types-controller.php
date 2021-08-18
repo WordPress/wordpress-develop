b68680383c3c371f58517411565fc764ad786647
Adam Zielinski <adam@adamziel.com>
2021-08-18 12:31:59 +0200

    Update src/wp-includes/rest-api/endpoints/class-wp-rest-widget-types-controller.php
    Co-authored-by: Anton Vlasenko <43744263+anton-vlasenko@users.noreply.github.com>

--- src/wp-includes/rest-api/endpoints/class-wp-rest-widget-types-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-widget-types-controller.php
@@ -424,7 +424,7 @@ class WP_REST_Widget_Types_Controller extends WP_REST_Controller {
 		return array(
 			'preview' => $this->render_legacy_widget_preview_iframe(
 				$request['id_base'],
-				$request['instance']
+				isset($request['instance']) ? $request['instance'] : null
 			),
 		);
 	}
