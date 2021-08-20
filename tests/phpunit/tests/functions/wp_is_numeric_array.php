af798ca9fee2174c6a3df3fac67c7f445dc5f7f5
pbearne <pbearne@gmail.com>
2021-08-20 18:15:46 -0400

    whitespace

--- tests/phpunit/tests/functions/wp_is_numeric_array.php
+++ tests/phpunit/tests/functions/wp_is_numeric_array.php
@@ -29,41 +29,41 @@ class Tests_Functions_wp_is_numeric_array extends WP_UnitTestCase {
 	 */
 	public function data_wp_is_numeric_array() {
 		return array(
-			'no index' => array(
-				'test_array' => array( 'www', 'eee' ),
-				'expected'  => true,
+			'no index' 		       => array(
+				'test_array'       => array( 'www', 'eee' ),
+				'expected'         => true,
 			),
-			'text index' => array(
-				'test_array' => array( 'www' => 'eee' ),
-				'expected'  => false,
+			'text index'           => array(
+				'test_array'       => array( 'www' => 'eee' ),
+				'expected'   	   => false,
 			),
-			'numeric index' => array(
-				'test_array' => array( 99 => 'eee' ),
-				'expected'  => true,
+			'numeric index'  	   => array(
+				'test_array' 	   => array( 99 => 'eee' ),
+				'expected'   	   => true,
 			),
-			'- numeric index' => array(
-				'test_array' => array( -11 => 'eee' ),
-				'expected'  => true,
+			'- numeric index'      => array(
+				'test_array'  	   => array( -11 => 'eee' ),
+				'expected'         => true,
 			),
 			'numeric string index' => array(
-				'test_array' => array( '11' => 'eee' ),
-				'expected'  => true,
+				'test_array' 	   => array( '11' => 'eee' ),
+				'expected'  	   => true,
 			),
-			'nested number index' => array(
-				'test_array' => array(
-					'next' => array(
-						11 => 'vvv',
+			'nested number index'  => array(
+				'test_array'       => array(
+					'next'         => array(
+						11         => 'vvv',
 					),
 				),
-				'expected' => false,
+				'expected'         => false,
 			),
-			'nested string index' => array(
-				'test_array' => array(
-					'11'  => array(
-						'eee' => 'vvv',
+			'nested string index'  => array(
+				'test_array'       => array(
+					'11'           => array(
+						'eee'      => 'vvv',
 					),
 				),
-				'expected' => true,
+				'expected'        => true,
 			),
 		);
 	}
