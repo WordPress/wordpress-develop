927bc80b92d8e4980e3b4defab638a488329d908
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-08-26 15:54:29 +0000

    Tests: Move `wp_list_pluck()` tests to their own file.
    The tests were partially duplicated in two separate files.
    
    Follow-up to [431/tests], [28900], [38928], [42527].
    
    See #53363, #53987.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@51663 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/functions/wpListFilter.php
+++ tests/phpunit/tests/functions/wpListFilter.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * Test wp_filter_object_list(), wp_list_filter(), wp_list_pluck().
+ * Test wp_filter_object_list().
  *
  * @group functions.php
  * @covers ::wp_filter_object_list
- * @covers ::wp_list_pluck
  */
 class Tests_Functions_wpListFilter extends WP_UnitTestCase {
 	public $object_list = array();
@@ -133,157 +132,6 @@ class Tests_Functions_wpListFilter extends WP_UnitTestCase {
 			'name'
 		);
 		$this->assertSame( array( 'baz' => 'baz' ), $list );
-	}
-
-	function test_wp_list_pluck() {
-		$list = wp_list_pluck( $this->object_list, 'name' );
-		$this->assertSame(
-			array(
-				'foo' => 'foo',
-				'bar' => 'bar',
-				'baz' => 'baz',
-			),
-			$list
-		);
-
-		$list = wp_list_pluck( $this->array_list, 'name' );
-		$this->assertSame(
-			array(
-				'foo' => 'foo',
-				'bar' => 'bar',
-				'baz' => 'baz',
-			),
-			$list
-		);
-	}
-
-	/**
-	 * @ticket 28666
-	 */
-	function test_wp_list_pluck_index_key() {
-		$list = wp_list_pluck( $this->array_list, 'name', 'id' );
-		$this->assertSame(
-			array(
-				'f' => 'foo',
-				'b' => 'bar',
-				'z' => 'baz',
-			),
-			$list
-		);
-	}
-
-	/**
-	 * @ticket 28666
-	 */
-	function test_wp_list_pluck_object_index_key() {
-		$list = wp_list_pluck( $this->object_list, 'name', 'id' );
-		$this->assertSame(
-			array(
-				'f' => 'foo',
-				'b' => 'bar',
-				'z' => 'baz',
-			),
-			$list
-		);
-	}
-
-	/**
-	 * @ticket 28666
-	 */
-	function test_wp_list_pluck_missing_index_key() {
-		$list = wp_list_pluck( $this->array_list, 'name', 'nonexistent' );
-		$this->assertSame(
-			array(
-				0 => 'foo',
-				1 => 'bar',
-				2 => 'baz',
-			),
-			$list
-		);
-	}
-
-	/**
-	 * @ticket 28666
-	 */
-	function test_wp_list_pluck_partial_missing_index_key() {
-		$array_list = $this->array_list;
-		unset( $array_list['bar']['id'] );
-		$list = wp_list_pluck( $array_list, 'name', 'id' );
-		$this->assertSame(
-			array(
-				'f' => 'foo',
-				0   => 'bar',
-				'z' => 'baz',
-			),
-			$list
-		);
-	}
-
-	/**
-	 * @ticket 28666
-	 */
-	function test_wp_list_pluck_mixed_index_key() {
-		$mixed_list        = $this->array_list;
-		$mixed_list['bar'] = (object) $mixed_list['bar'];
-		$list              = wp_list_pluck( $mixed_list, 'name', 'id' );
-		$this->assertSame(
-			array(
-				'f' => 'foo',
-				'b' => 'bar',
-				'z' => 'baz',
-			),
-			$list
-		);
-	}
-
-	/**
-	 * @ticket 16895
-	 */
-	function test_wp_list_pluck_containing_references() {
-		$ref_list = array(
-			& $this->object_list['foo'],
-			& $this->object_list['bar'],
-		);
-
-		$this->assertInstanceOf( 'stdClass', $ref_list[0] );
-		$this->assertInstanceOf( 'stdClass', $ref_list[1] );
-
-		$list = wp_list_pluck( $ref_list, 'name' );
-		$this->assertSame(
-			array(
-				'foo',
-				'bar',
-			),
-			$list
-		);
-
-		$this->assertInstanceOf( 'stdClass', $ref_list[0] );
-		$this->assertInstanceOf( 'stdClass', $ref_list[1] );
-	}
-
-	/**
-	 * @ticket 16895
-	 */
-	function test_wp_list_pluck_containing_references_keys() {
-		$ref_list = array(
-			& $this->object_list['foo'],
-			& $this->object_list['bar'],
-		);
-
-		$this->assertInstanceOf( 'stdClass', $ref_list[0] );
-		$this->assertInstanceOf( 'stdClass', $ref_list[1] );
-
-		$list = wp_list_pluck( $ref_list, 'name', 'id' );
-		$this->assertSame(
-			array(
-				'f' => 'foo',
-				'b' => 'bar',
-			),
-			$list
-		);
-
-		$this->assertInstanceOf( 'stdClass', $ref_list[0] );
-		$this->assertInstanceOf( 'stdClass', $ref_list[1] );
 	}
 
 	function test_filter_object_list_nested_array_and() {
