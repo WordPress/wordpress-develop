e2c1ef106f659a1e8749aad91e59d61cd84418e8
jrfnl <jrfnl@users.noreply.github.com>
2021-08-28 01:44:32 +0200

    Tests_Compat_mbSubstr: improve the tests
    Includes:
    * Adding `@covers` tags.
    * Adding visibility modifiers to all methods.
    * Add function availability test.
    * Making the `utf8_substrings()` data provider more readable by adding keys within the data sets.
    * Splitting the `test_mb_substr_phpcore()` method into two test methods based on the PHP Core test files they are emulating.
    * Make the actual test code within the `test_mb_substr_phpcore_basic()` method more readable by using descriptive variables and multi-line function calls.
    * Adding the `$message` parameter to all assertions in the `test_mb_substr_phpcore_basic()` test method.
    * Splitting the data used for the second part of the `test_mb_substr_phpcore()` function, now `test_mb_substr_phpcore_input_type_handling()`, off into a separate data provider with named data sets.
    * Removing duplicate data sets from the `data_mb_substr_phpcore_input_type_handling()`.
        The PHP native tests test against upper/lowercase `false`, `true`, `null` and some other text string single quote/double quote variations.
        As things were, those differentiations had been undone when the coding standards were put in place, so in effect those weren't being tested anymore.
        And as this is userland code, I see no point in adding these differentiations back as they will be handled the same by PHP anyway (and that is safeguarded via the PHP native tests).
    * Also removed the "undefined variable" and "unset variable" test cases as, while those are relevant to the C code in which PHP is written, they are not relevant for testing userland code and will behave the same as the test passing `null`.
    * Renaming the fixture class to a more unique name.

--- tests/phpunit/tests/compat/mbSubstr.php
+++ tests/phpunit/tests/compat/mbSubstr.php
@@ -3,33 +3,30 @@
 /**
  * @group compat
  * @group security-153
+ *
+ * @covers ::mb_substr
+ * @covers ::_mb_substr
  */
 class Tests_Compat_mbSubstr extends WP_UnitTestCase {
 
-	function utf8_substrings() {
-		return array(
-			// String, start, length, character_substring, byte_substring.
-			array( 'баба', 0, 3, 'баб', "б\xD0" ),
-			array( 'баба', 0, -1, 'баб', "баб\xD0" ),
-			array( 'баба', 1, null, 'аба', "\xB1аба" ),
-			array( 'баба', -3, null, 'аба', "\xB1а" ),
-			array( 'баба', -3, 2, 'аб', "\xB1\xD0" ),
-			array( 'баба', -1, 2, 'а', "\xB0" ),
-			array( 'I am your баба', 0, 11, 'I am your б', "I am your \xD0" ),
-		);
+	/**
+	 * Test that mb_substr() is always available (either from PHP or WP).
+	 */
+	public function test_mb_substr_availability() {
+		$this->assertTrue( function_exists( 'mb_substr' ) );
 	}
 
 	/**
 	 * @dataProvider utf8_substrings
 	 */
-	function test_mb_substr( $string, $start, $length, $expected_character_substring ) {
+	public function test_mb_substr( $string, $start, $length, $expected_character_substring ) {
 		$this->assertSame( $expected_character_substring, _mb_substr( $string, $start, $length, 'UTF-8' ) );
 	}
 
 	/**
 	 * @dataProvider utf8_substrings
 	 */
-	function test_mb_substr_via_regex( $string, $start, $length, $expected_character_substring ) {
+	public function test_mb_substr_via_regex( $string, $start, $length, $expected_character_substring ) {
 		_wp_can_use_pcre_u( false );
 		$this->assertSame( $expected_character_substring, _mb_substr( $string, $start, $length, 'UTF-8' ) );
 		_wp_can_use_pcre_u( 'reset' );
@@ -38,100 +35,207 @@ class Tests_Compat_mbSubstr extends WP_UnitTestCase {
 	/**
 	 * @dataProvider utf8_substrings
 	 */
-	function test_8bit_mb_substr( $string, $start, $length, $expected_character_substring, $expected_byte_substring ) {
+	public function test_8bit_mb_substr( $string, $start, $length, $expected_character_substring, $expected_byte_substring ) {
 		$this->assertSame( $expected_byte_substring, _mb_substr( $string, $start, $length, '8bit' ) );
 	}
 
-	function test_mb_substr_phpcore() {
-		/* https://github.com/php/php-src/blob/php-5.6.8/ext/mbstring/tests/mb_substr_basic.phpt */
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function utf8_substrings() {
+		return array(
+			array(
+				'string'                       => 'баба',
+				'start'                        => 0,
+				'length'                       => 3,
+				'expected_character_substring' => 'баб',
+				'expected_byte_substring'      => "б\xD0",
+			),
+			array(
+				'string'                       => 'баба',
+				'start'                        => 0,
+				'length'                       => -1,
+				'expected_character_substring' => 'баб',
+				'expected_byte_substring'      => "баб\xD0",
+			),
+			array(
+				'string'                       => 'баба',
+				'start'                        => 1,
+				'length'                       => null,
+				'expected_character_substring' => 'аба',
+				'expected_byte_substring'      => "\xB1аба",
+			),
+			array(
+				'string'                       => 'баба',
+				'start'                        => -3,
+				'length'                       => null,
+				'expected_character_substring' => 'аба',
+				'expected_byte_substring'      => "\xB1а",
+			),
+			array(
+				'string'                       => 'баба',
+				'start'                        => -3,
+				'length'                       => 2,
+				'expected_character_substring' => 'аб',
+				'expected_byte_substring'      => "\xB1\xD0",
+			),
+			array(
+				'string'                       => 'баба',
+				'start'                        => -1,
+				'length'                       => 2,
+				'expected_character_substring' => 'а',
+				'expected_byte_substring'      => "\xB0",
+			),
+			array(
+				'string'                       => 'I am your баба',
+				'start'                        => 0,
+				'length'                       => 11,
+				'expected_character_substring' => 'I am your б',
+				'expected_byte_substring'      => "I am your \xD0",
+			),
+		);
+	}
+
+	/**
+	 * @link https://github.com/php/php-src/blob/php-5.6.8/ext/mbstring/tests/mb_substr_basic.phpt
+	 */
+	public function test_mb_substr_phpcore_basic() {
 		$string_ascii = 'ABCDEF';
 		$string_mb    = base64_decode( '5pel5pys6Kqe44OG44Kt44K544OI44Gn44GZ44CCMDEyMzTvvJXvvJbvvJfvvJjvvJnjgII=' );
 
-		$this->assertSame( 'DEF', _mb_substr( $string_ascii, 3 ) );
-		$this->assertSame( 'DEF', _mb_substr( $string_ascii, 3, 5, 'ISO-8859-1' ) );
+		$this->assertSame(
+			'DEF',
+			_mb_substr( $string_ascii, 3 ),
+			'Substring does not match expected for offset 3'
+		);
+		$this->assertSame(
+			'DEF',
+			_mb_substr( $string_ascii, 3, 5, 'ISO-8859-1' ),
+			'Substring does not match expected for offset 3, length 5, with iso charset'
+		);
 
 		// Specific latin-1 as that is the default the core PHP test operates under.
-		$this->assertSame( 'peacrOiqng==', base64_encode( _mb_substr( $string_mb, 2, 7, 'latin-1' ) ) );
-		$this->assertSame( '6Kqe44OG44Kt44K544OI44Gn44GZ', base64_encode( _mb_substr( $string_mb, 2, 7, 'utf-8' ) ) );
+		$this->assertSame(
+			'peacrOiqng==',
+			base64_encode( _mb_substr( $string_mb, 2, 7, 'latin-1' ) ),
+			'Substring does not match expected for offset 2, length 7, with latin-1 charset'
+		);
+		$this->assertSame(
+			'6Kqe44OG44Kt44K544OI44Gn44GZ',
+			base64_encode( _mb_substr( $string_mb, 2, 7, 'utf-8' ) ),
+			'Substring does not match expected for offset 2, length 7, with utf-8 charset'
+		);
+	}
 
-		/* https://github.com/php/php-src/blob/php-5.6.8/ext/mbstring/tests/mb_substr_variation1.phpt */
-		$start     = 0;
-		$length    = 5;
-		$unset_var = 10;
-		unset( $unset_var );
-		$heredoc  = <<<EOT
+	/**
+	 * @link https://github.com/php/php-src/blob/php-5.6.8/ext/mbstring/tests/mb_substr_variation1.phpt
+	 *
+	 * @dataProvider data_mb_substr_phpcore_input_type_handling
+	 *
+	 * @param mixed  $input    Input to pass to the function.
+	 * @param string $expected Expected function output.
+	 */
+	public function test_mb_substr_phpcore_input_type_handling( $input, $expected ) {
+		$start  = 0;
+		$length = 5;
+
+		$this->assertSame( $expected, _mb_substr( $input, $start, $length ) );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_mb_substr_phpcore_input_type_handling() {
+		$heredoc = <<<EOT
 hello world
 EOT;
-		$inputs   = array(
-			0,
-			1,
-			12345,
-			-2345,
-			// Float data.
-			10.5,
-			-10.5,
-			12.3456789000e10,
-			12.3456789000E-10,
-			.5,
-			// Null data.
-			null,
-			null,
-			// Boolean data.
-			true,
-			false,
-			true,
-			false,
-			// Empty data.
-			'',
-			'',
-			// String data.
-			'string',
-			'string',
-			$heredoc,
-			// Object data.
-			new ClassA(),
-			// phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged -- intentionally undefined data
-			@$undefined_var,
-			// phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged -- intentionally unset data
-			@$unset_var,
-		);
-		$outputs  = array(
-			'0',
-			'1',
-			'12345',
-			'-2345',
-			'10.5',
-			'-10.5',
-			'12345',
-			'1.234',
-			'0.5',
-			'',
-			'',
-			'1',
-			'',
-			'1',
-			'',
-			'',
-			'',
-			'strin',
-			'strin',
-			'hello',
-			'Class',
-			'',
-			'',
-		);
-		$iterator = 0;
-		foreach ( $inputs as $input ) {
-			$this->assertSame( $outputs[ $iterator ], _mb_substr( $input, $start, $length ) );
-			$iterator++;
-		}
 
+		return array(
+			'integer zero'                   => array(
+				'input'    => 0,
+				'expected' => '0',
+			),
+			'integer 1'                      => array(
+				'input'    => 1,
+				'expected' => '1',
+			),
+			'positive integer'               => array(
+				'input'    => 12345,
+				'expected' => '12345',
+			),
+			'negative integer'               => array(
+				'input'    => -2345,
+				'expected' => '-2345',
+			),
+			// Float data.
+			'positive float with fraction'   => array(
+				'input'    => 10.5,
+				'expected' => '10.5',
+			),
+			'negative float with fraction'   => array(
+				'input'    => -10.5,
+				'expected' => '-10.5',
+			),
+			'float scientific whole number'  => array(
+				'input'    => 12.3456789000e10,
+				'expected' => '12345',
+			),
+			'float scientific with fraction' => array(
+				'input'    => 12.3456789000E-10,
+				'expected' => '1.234',
+			),
+			'float, fraction only'           => array(
+				'input'    => .5,
+				'expected' => '0.5',
+			),
+			// Null data.
+			'null'                           => array(
+				'input'    => null,
+				'expected' => '',
+			),
+			// Boolean data.
+			'boolean true'                   => array(
+				'input'    => true,
+				'expected' => '1',
+			),
+			'boolean false'                  => array(
+				'input'    => false,
+				'expected' => '',
+			),
+			// Empty data.
+			'empty string'                   => array(
+				'input'    => '',
+				'expected' => '',
+			),
+			// String data.
+			'double quoted string'           => array(
+				'input'    => "string'",
+				'expected' => 'strin',
+			),
+			'single quoted string'           => array(
+				'input'    => 'string',
+				'expected' => 'strin',
+			),
+			'heredoc string'                 => array(
+				'input'    => $heredoc,
+				'expected' => 'hello',
+			),
+			// Object data.
+			'object with __toString method'  => array(
+				'input'    => new ClassWithToStringForMbSubstr(),
+				'expected' => 'Class',
+			),
+		);
 	}
 }
 
-/* used in test_mb_substr_phpcore */
-class ClassA {
+/* used in data_mb_substr_phpcore_input_type_handling() */
+class ClassWithToStringForMbSubstr {
 	public function __toString() {
-		return 'Class A object';
+		return 'Class object';
 	}
 }
