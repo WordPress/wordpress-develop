560f824e56adfaf9b97e33e3e2826ff12953b252
Jonathan Desrosiers <desrosj@git.wordpress.org>
2021-09-08 21:18:49 +0000

    Grouped merges for 5.4.7.
    - Update `lodash` to the latest version `4.17.21`.
    - Disable some attributes for rich text.
    - Use hashed/deterministic moduleIDs in webpack config.
    
    Props ellatrix, peterwilsoncc, get_dave, mcsf, talldanwp, youknowriad, desrosj, nerrad, gziolo.
    Merges [50940-50941,50984-50985,51426] to the 5.4 branch.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.4@51754 602fd350-edb4-49c9-b593-d223f7449a82

--- tools/webpack/packages.js
+++ tools/webpack/packages.js
@@ -207,6 +207,9 @@ module.exports = function( env = { environment: 'production', watch: false, buil
 				},
 			],
 		},
+		optimization: {
+			moduleIds: mode === 'production' ? 'hashed' : 'named',
+		},
 		plugins: [
 			new DefinePlugin( {
 				// Inject the `GUTENBERG_PHASE` global, used for feature flagging.
@@ -272,7 +275,8 @@ module.exports = function( env = { environment: 'production', watch: false, buil
 		delete config.devtool;
 		config.mode = 'production';
 		config.optimization = {
-			minimize: false
+			minimize: false,
+			moduleIds: 'hashed',
 		};
 	}
 
