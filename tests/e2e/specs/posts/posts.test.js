bdaf15cf06a424e3f121fb9688d6885dfd775dac
JustinyAhin <justiny.ahinon@gmail.com>
2021-09-09 15:59:19 +0100

    Add test for restoring post from trash

--- tests/e2e/specs/posts/posts.test.js
+++ tests/e2e/specs/posts/posts.test.js
@@ -21,10 +21,12 @@ async function deleteAllTrashedPosts() {
         await page.select('#bulk-action-selector-top', 'delete');
         await page.focus('#doaction');
         await page.keyboard.press('Enter');
-    }
 
-    // Make sure that all trashed posts have been deleted permanently.
-    await page.waitForSelector('#the-list tr:only-child');
+        // Make sure that all trashed posts have been deleted permanently.
+        await page.waitForSelector('#the-list tr:only-child');
+    } else {
+        return;
+    }
 }
 
 describe('Manage posts', () => {
@@ -59,6 +61,32 @@ describe('Manage posts', () => {
         const trashedPost = await page.$('#the-list td.title');
         expect(
             await trashedPost.evaluate((element) => element.innerText)
+        ).toContain(title);
+    });
+
+    it('Correctly restore a post from trash', async() => {
+        const title = 'Post to be restored';
+        await createNewPost({ title });
+        await publishPost();
+        await trashAllPosts();
+
+        const query = addQueryArgs('', {
+            post_status: 'trash',
+            post_type: 'post',
+        });
+        await visitAdminPage('edit.php', query);
+
+        // Hover on the trashed post and click on the restore link
+        const trashedPost = await page.$('#the-list td.title');
+        await trashedPost.hover();
+        const restoredLink = await page.$('span.untrash a');
+        await restoredLink.click();
+
+        await visitAdminPage('edit.php');
+
+        const restoredPostLink = await page.$('a.row-title');
+        expect(
+            await restoredPostLink.evaluate((element) => element.innerText)
         ).toContain(title);
     });
 });
