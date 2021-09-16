1a0e0e5ce3f36d5d42205d41724c381437110b00
John Blackbourn <john@humanmade.com>
2021-09-16 19:46:24 +0200

    Add the old `apply_filters()` function for benchmarking comparison.

--- tests/Benchmark/ApplyFiltersBench.php
+++ tests/Benchmark/ApplyFiltersBench.php
@@ -4,6 +4,38 @@ declare( strict_types=1 );
 
 require_once dirname( __DIR__, 2 ) . '/src/wp-load.php';
 
+function apply_filters_old( $tag, $value ) {
+	global $wp_filter, $wp_current_filter;
+
+	$args = func_get_args();
+
+	// Do 'all' actions first.
+	if ( isset( $wp_filter['all'] ) ) {
+		$wp_current_filter[] = $tag;
+		_wp_call_all_hook( $args );
+	}
+
+	if ( ! isset( $wp_filter[ $tag ] ) ) {
+		if ( isset( $wp_filter['all'] ) ) {
+			array_pop( $wp_current_filter );
+		}
+		return $value;
+	}
+
+	if ( ! isset( $wp_filter['all'] ) ) {
+		$wp_current_filter[] = $tag;
+	}
+
+	// Don't pass the tag name to WP_Hook.
+	array_shift( $args );
+
+	$filtered = $wp_filter[ $tag ]->apply_filters( $value, $args );
+
+	array_pop( $wp_current_filter );
+
+	return $filtered;
+}
+
 class ApplyFiltersBench {
 	/**
 	 * @Revs(100000)
