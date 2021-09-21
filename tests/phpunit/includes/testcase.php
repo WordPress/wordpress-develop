0880a888ccd0c655d82dd6da1f1bb2a2018df21f
Tonya Mork <hellofromtonya@git.wordpress.org>
2021-09-21 23:02:13 +0000

    Build/Test Tools: Introduce the PHPUnit Polyfills package for easier cross branch testing.
    This backports the PHPUnit Polyfills package and related test infrastructure changes to make it easier for developers to continue testing on multiple versions WordPress while adding tests for newer versions of PHP, which require more modern PHPUnit practices.
    
    One of the changes included is the addition of wrappers for the new snake_case fixture methods in PHPUnit. This allows the native camelCase standard in PHPUnit to be used, but allows for developers to transition to the new naming conventions.
    
    Props hellofromTonya, jrf, SergeyBiryukov, johnbillion, netweb, schlessera, jeherve, lucatume, desrosj.
    Merges [51559,51560,51810-51813,51828] to the 5.4 branch.
    See #53911.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.4@51844 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/includes/testcase.php
+++ tests/phpunit/includes/testcase.php
@@ -1,5 +1,21 @@
 <?php
 
+use Yoast\PHPUnitPolyfills\Helpers\AssertAttributeHelper;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertClosedResource;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertEqualsSpecializations;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertFileDirectory;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertFileEqualsSpecializations;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertionRenames;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertIsType;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertNumericType;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertObjectEquals;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertStringContains;
+use Yoast\PHPUnitPolyfills\Polyfills\EqualToSpecializations;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectException;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectExceptionMessageMatches;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectExceptionObject;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectPHPException;
+
 require_once __DIR__ . '/abstract-testcase.php';
 
 /**
@@ -13,20 +29,47 @@ require_once __DIR__ . '/abstract-testcase.php';
  */
 class WP_UnitTestCase extends WP_UnitTestCase_Base {
 
+	use AssertAttributeHelper;
+	use AssertClosedResource;
+	use AssertEqualsSpecializations;
+	use AssertFileDirectory;
+	use AssertFileEqualsSpecializations;
+	use AssertionRenames;
+	use AssertIsType;
+	use AssertNumericType;
+	use AssertObjectEquals;
+	use AssertStringContains;
+	use EqualToSpecializations;
+	use ExpectException;
+	use ExpectExceptionMessageMatches;
+	use ExpectExceptionObject;
+	use ExpectPHPException;
+
 	/**
-	 * Asserts that a condition is not false.
-	 *
-	 * This method has been backported from a more recent PHPUnit version, as tests running on PHP 5.2 use
-	 * PHPUnit 3.6.x.
-	 *
-	 * @since 4.7.4
-	 *
-	 * @param bool   $condition Condition to check.
-	 * @param string $message   Optional. Message to display when the assertion fails.
-	 *
-	 * @throws PHPUnit_Framework_AssertionFailedError
+	 * Wrapper method for the `setUpBeforeClass()` method for forward-compatibility with WP 5.9.
 	 */
-	public static function assertNotFalse( $condition, $message = '' ) {
-		self::assertThat( $condition, self::logicalNot( self::isFalse() ), $message );
+	public static function set_up_before_class() {
+		static::setUpBeforeClass();
+	}
+
+	/**
+	 * Wrapper method for the `tearDownAfterClass()` method for forward-compatibility with WP 5.9.
+	 */
+	public static function tear_down_after_class() {
+		static::tearDownAfterClass();
+	}
+
+	/**
+	 * Wrapper method for the `setUp()` method for forward-compatibility with WP 5.9.
+	 */
+	public function set_up() {
+		static::setUp();
+	}
+
+	/**
+	 * Wrapper method for the `tearDown()` method for forward-compatibility with WP 5.9.
+	 */
+	public function tear_down() {
+		static::tearDown();
 	}
 }
