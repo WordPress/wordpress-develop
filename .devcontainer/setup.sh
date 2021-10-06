d1e943fc705fdf7f297d3457e256a19f19eb23cc
Helen Hou-Sandi <helen.y.hou@gmail.com>
2021-10-06 14:49:08 -0600

    Try dinhtungdu/wordpress-codespace

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+if [[ ! -z "$CODESPACE_NAME" ]]
+then
+	SITE_HOST="https://${CODESPACE_NAME}-8080.githubpreview.dev"
+else
+	SITE_HOST="http://localhost:8080"
+fi
+
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>setup.log 2>&1
+
+source ~/.bashrc
+
+# Install dependencies
+cd /var/www/html/
+npm install && npm run build:dev
+
+# Install WordPress and activate the plugin/theme.
+cd /var/www/html/
+echo "Setting up WordPress at $SITE_HOST"
+wp db reset --yes
+wp core install --url="$SITE_HOST" --title="WordPress Trunk" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
