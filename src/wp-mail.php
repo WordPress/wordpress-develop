51be7bc8baf0cf3ef215bbb07757754335ddd5fc
Thijs van der Heijden <thijsvanderheijden@yoast.com>
2021-10-08 14:11:50 +0200

    add comment

--- src/wp-mail.php
+++ src/wp-mail.php
@@ -146,7 +146,7 @@ for ( $i = 1; $i <= $count; $i++ ) {
 
 			if ( preg_match( '/Date: /i', $line ) ) { // Of the form '20 Mar 2002 20:32:37 +0100'.
 				$ddate = str_replace( 'Date: ', '', trim( $line ) );
-				// Remove parenthesised timezone string if it exists, as this confuses strtotime().
+				// Remove parenthesised timezone string if it exists, as this confuses ( new DateTimeImmutable( ) )->getTimestamp().
 				$ddate           = preg_replace( '!\s*\(.+\)\s*$!', '', $ddate );
 				$ddate_timestamp = ( new DateTimeImmutable( $ddate ) )->getTimestamp();
 				$post_date       = gmdate( 'Y-m-d H:i:s', $ddate_timestamp + $time_difference );
