f216e0b6230981b21df97969cad61a549f5a8ca4
JustinyAhin <justiny.ahinon@gmail.com>
2021-10-21 15:33:17 +0200

    Update upload-plugin-theme-new-version.test.js

--- tests/e2e/specs/upgrade/upload-plugin-theme-new-version.test.js
+++ tests/e2e/specs/upgrade/upload-plugin-theme-new-version.test.js
@@ -9,7 +9,6 @@ import path from 'path';
 import {
 	visitAdminPage,
 	installPlugin,
-	activatePlugin,
 	uninstallPlugin,
 	activateTheme,
 	deactivatePlugin,
@@ -22,7 +21,6 @@ describe('Manage uploading new plugin/theme version', () => {
 
 	it('should replace a plugin when uploading a new version', async () => {
 		await installPlugin('classic-editor', 'Classic Editor');
-		await activatePlugin('classic-editor');
 
 		await visitAdminPage('plugin-install.php', uploadPluginUrl);
 		const pluginPath = path.join(
@@ -31,14 +29,23 @@ describe('Manage uploading new plugin/theme version', () => {
 			'..',
 			'fixtures',
 			'plugins',
-			'classic-editor.1.6.2.zip'
+			'classic-editor.1.6.zip'
 		);
 		const input = await page.$('#pluginzip');
 		await input.uploadFile(pluginPath);
 		await page.click('#install-plugin-submit');
-		await page.waitForSelector('.button.button-primary');
 
-		await activatePlugin('classic-editor');
+		const upgradeHeading = await page.waitForSelector('.update-from-upload-heading');
+		expect(
+			await upgradeHeading.evaluate((element) => element.textContent)
+		).toContain('This plugin is already installed.');
+
+		await page.click('a.update-from-upload-overwrite');
+
+		const contentWrap = await page.waitForSelector('.wrap');
+		expect(
+			contentWrap.evaluate((element) => element.textContent)
+		).toContain('Installing plugin from uploaded file: classic-editor.1.6.2.zipUnpacking the package…Updating the plugin…Removing the current plugin…Plugin updated successfully.Go to Plugin Installer');
 
 		// Delete the plugin
 		await deactivatePlugin('classic-editor');
