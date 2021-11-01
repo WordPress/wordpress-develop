9d2ee3a82130a02c03fc1824c20e255cbbc1e794
pbearne <pbearne@gmail.com>
2021-11-01 19:49:45 -0400

    test for wp_get_direct_update_https_url

---
+++ tests/phpunit/tests/functions/wpGetDirectUpdateHttpsUrl.php
@@ -0,0 +1,35 @@
+<?php
+
+
+/**
+ * @group functions.php
+ * @covers ::wp_get_direct_update_https_url
+ */
+class Tests_wp_get_direct_update_https_url  extends WP_UnitTestCase {
+	function set_up() {
+		parent::set_up();
+		if ( ! defined( 'WP_DIRECT_UPDATE_HTTPS_URL' ) ) {
+			putenv( 'WP_DIRECT_UPDATE_HTTPS_URL=https://example.com/' );
+		}
+	}
+
+	function tear_down() {
+		parent::tear_down();
+		putenv( 'WP_DIRECT_UPDATE_HTTPS_URL=null' );
+	}
+
+	public function test_wp_get_direct_update_https_url() {
+
+		$this->assertEquals( 'https://example.com/', wp_get_direct_update_https_url() );
+	}
+
+	public function test_wp_get_direct_update_https_url_with_filter() {
+		add_filter(
+			'wp_direct_update_https_url',
+			static function( $url ) {
+				return 'https://filtered.com/';
+			}
+		);
+		$this->assertEquals( 'https://filtered.com/', wp_get_direct_update_https_url() );
+	}
+}
