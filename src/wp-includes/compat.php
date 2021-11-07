09d51f84a206e3546a60dabb870d54688b67df8f
pbearne <pbearne@gmail.com>
2021-11-07 11:25:44 -0500

    update from review feedback

--- src/wp-includes/compat.php
+++ src/wp-includes/compat.php
@@ -387,7 +387,7 @@ if ( ! function_exists( 'array_key_first' ) ) {
 	 * @param array $arr An array.
 	 * @return string|int|null The first key of array if the array is not empty. NULL otherwise.
 	 */
-	function array_key_first( $arr ) {
+	function array_key_first( array $arr ) {
 		foreach ( $arr as $key => $value ) {
 				return $key;
 		}
@@ -405,7 +405,7 @@ if ( ! function_exists( 'array_key_last' ) ) {
 	 * @param array $arr An array.
 	 * @return string|int|null The first key of array if the array is not empty. NULL otherwise.
 	 */
-	function array_key_last( $arr ) {
+	function array_key_last( array $arr ) {
 			end( $arr );
 			return key( $arr );
 	}
