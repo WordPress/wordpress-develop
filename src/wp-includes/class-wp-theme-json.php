57f193c8897c38cf75d34cb1693ffa860768c61c
André <583546+oandregal@users.noreply.github.com>
2021-11-08 14:10:07 +0100

    Add comment to complement the @private tag

--- src/wp-includes/class-wp-theme-json.php
+++ src/wp-includes/class-wp-theme-json.php
@@ -10,6 +10,10 @@
 /**
  * Class that encapsulates the processing of structures that adhere to the theme.json spec.
  *
+ * This class is for internal core usage and is not supposed to be used by extenders (plugins and/or themes).
+ * This is a low-level API that may need to do breaking changes. Please,
+ * use get_global_settings, get_global_styles, and get_global_stylesheet instead.
+ *
  * @access private
  */
 class WP_Theme_JSON {
