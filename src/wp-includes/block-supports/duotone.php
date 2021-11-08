735a17fe49c2f26911d5d685163e00afa4376dc3
André <583546+oandregal@users.noreply.github.com>
2021-11-08 09:57:58 +0100

    Update src/wp-includes/block-supports/duotone.php
    Co-authored-by: Tonya Mork <tonya.mork@automattic.com>

--- src/wp-includes/block-supports/duotone.php
+++ src/wp-includes/block-supports/duotone.php
@@ -393,7 +393,7 @@ function wp_render_duotone_filter_preset( $preset ) {
 		// and SVG filters won't render in the head of a document,
 		// so the next best place to put the SVG is in the footer.
 		is_admin() ? 'admin_footer' : 'wp_footer',
-		function () use ( $svg ) {
+		static function () use ( $svg ) {
 			echo $svg;
 		}
 	);
