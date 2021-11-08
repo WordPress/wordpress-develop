2d33164547a992a0f7f185621f0c28485f0b7505
Tonya Mork <tonya.mork@automattic.com>
2021-11-07 22:27:55 -0600

    Rename file and class. Add empty data sets.
    Rename file and class per coding standards.
    
    Add empty needle, haystack, and both tests.
    
    General formatting.

--- tests/phpunit/tests/compat/strEndsWith.php
+++ tests/phpunit/tests/compat/strEndsWith.php
@@ -3,24 +3,27 @@
 /**
  * @group compat
  *
- *
  * @covers ::str_ends_with
  */
-class Tests_Compat_str_ends_with extends WP_UnitTestCase {
+class Tests_Compat_StrEndsWith extends WP_UnitTestCase {
 
 	/**
 	 * Test that str_ends_with() is always available (either from PHP or WP).
+	 *
 	 * @ticket 54377
 	 */
 	public function test_str_ends_with_availability() {
 		$this->assertTrue( function_exists( 'str_ends_with' ) );
 	}
+
 	/**
-	 * @ticket 54377
 	 * @dataProvider data_str_ends_with
-	 * @param bool $expected Whether or not `$haystack` is expected to end with `$needle`.
+	 *
+	 * @ticket 54377
+	 *
+	 * @param bool   $expected Whether or not `$haystack` is expected to end with `$needle`.
 	 * @param string $haystack The string to search in.
-	 * @param string $needle The substring to search for at the end of `$haystack`.
+	 * @param string $needle   The substring to search for at the end of `$haystack`.
 	 */
 	public function test_str_ends_with( $expected, $haystack, $needle ) {
 		if ( ! function_exists( 'str_ends_with' ) ) {
@@ -35,58 +38,73 @@ class Tests_Compat_str_ends_with extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Data provider for test_str_ends_with.
+	 * Data provider.
 	 *
 	 * @return array[]
 	 */
 	public function data_str_ends_with() {
 		return array(
-			'lowercase'              => array(
+			'empty needle'              => array(
+				'expected' => true,
+				'haystack' => 'This is a test',
+				'needle'   => '',
+			),
+			'empty haystack and needle' => array(
+				'expected' => true,
+				'haystack' => '',
+				'needle'   => '',
+			),
+			'empty haystack'            => array(
+				'expected' => false,
+				'haystack' => '',
+				'needle'   => 'test',
+			),
+			'lowercase'                 => array(
 				'expected' => true,
 				'haystack' => 'This is a test',
 				'needle'   => 'test',
 			),
-			'uppercase'              => array(
+			'uppercase'                 => array(
 				'expected' => true,
 				'haystack' => 'This is a TEST',
 				'needle'   => 'TEST',
 			),
-			'first_leter_upprercase' => array(
+			'first leter uppercase'     => array(
 				'expected' => true,
 				'haystack' => 'This is a Test',
 				'needle'   => 'Test',
 			),
-			'cammelCase'             => array(
+			'camelCase'                 => array(
 				'expected' => true,
-				'haystack' => 'This is a cammelCase',
-				'needle'   => 'cammelCase',
+				'haystack' => 'This is a camelCase',
+				'needle'   => 'camelCase',
 			),
-			'null'                   => array(
+			'null'                      => array(
 				'expected' => true,
 				'haystack' => 'This is a null \x00test',
 				'needle'   => '\x00test',
 			),
-			'trademark'              => array(
+			'trademark'                 => array(
 				'expected' => true,
 				'haystack' => 'This is a trademark\x2122',
 				'needle'   => 'trademark\x2122',
 			),
-			'not_cammelCase'         => array(
+			'not camelCase'             => array(
 				'expected' => false,
 				'haystack' => 'This is a cammelcase',
 				'needle'   => 'cammelCase',
 			),
-			'missing'                => array(
+			'missing'                   => array(
 				'expected' => false,
 				'haystack' => 'This is a cammelcase',
 				'needle'   => 'cammelCase',
 			),
-			'not end'                => array(
+			'not end'                   => array(
 				'expected' => false,
 				'haystack' => 'This is a test extra',
 				'needle'   => 'test',
 			),
-			'extra_space'            => array(
+			'extra space'               => array(
 				'expected' => false,
 				'haystack' => 'This is a test ',
 				'needle'   => 'test',
