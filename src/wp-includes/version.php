c9f13b42e290bf8ff6d4ec166b7c710fb519aeb9
Jonathan Desrosiers <desrosj@git.wordpress.org>
2021-11-10 17:06:37 +0000

    WordPress 5.4.8.
    git-svn-id: https://develop.svn.wordpress.org/branches/5.4@52117 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4.7-src';
+$wp_version = '5.4.8-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
