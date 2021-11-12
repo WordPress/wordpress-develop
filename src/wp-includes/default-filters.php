9d4b32f886c240d22b6b3e3adc93c04493208671
Petter Walbø Johnsgård <petter@dekode.no>
2021-11-12 20:18:41 +0100

    Add iframe assets to site editor

--- src/wp-includes/default-filters.php
+++ src/wp-includes/default-filters.php
@@ -581,6 +581,7 @@ add_action( 'wp_footer', 'wp_maybe_inline_styles', 1 ); // Run for late-loaded s
 
 add_action( 'admin_footer-post.php', 'wp_add_iframed_editor_assets_html' );
 add_action( 'admin_footer-post-new.php', 'wp_add_iframed_editor_assets_html' );
+add_action( 'admin_footer-site-editor.php', 'wp_add_iframed_editor_assets_html' );
 add_action( 'admin_footer-widgets.php', 'wp_add_iframed_editor_assets_html' );
 
 add_action( 'use_block_editor_for_post_type', '_disable_block_editor_for_navigation_post_type', 10, 2 );
