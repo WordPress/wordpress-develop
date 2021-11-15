f24677f7e9de893fd82ff18c42f2ce321813ea7d
Tonya Mork <tonya.mork@automattic.com>
2021-11-15 11:17:53 -0600

    Updates and improves Tests_Link_GetEditTermLink
    * Changes fixture method `setUp()` to `set_up()`
    * Uses `assertIsInt()`
    * Names the data set and each of corresponding args
    * Adds passing term ID (int) and term object to each of the tests
    * Creates a helper method to get the term ID or term object for the test

--- tests/phpunit/tests/link/getEditTermLink.php
+++ tests/phpunit/tests/link/getEditTermLink.php
@@ -6,111 +6,175 @@
  */
 class Tests_Link_GetEditTermLink extends WP_UnitTestCase {
 
-	public static $term_ids;
-
+	public static $terms;
 	public static $user_ids;
 
 	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
-		self::_register_taxonomy();
+		self::register_custom_taxonomy();
 
 		$taxonomies = array( 'category', 'post_tag', 'custom_taxonomy' );
 		foreach ( $taxonomies as $taxonomy ) {
-			self::$term_ids[ $taxonomy ] = $factory->term->create( array( 'taxonomy' => $taxonomy ) );
+			self::$terms[ $taxonomy ] = $factory->term->create_and_get( array( 'taxonomy' => $taxonomy ) );
 		}
 
 		self::$user_ids['admin']      = $factory->user->create( array( 'role' => 'administrator' ) );
 		self::$user_ids['subscriber'] = $factory->user->create( array( 'role' => 'subscriber' ) );
 	}
 
-	public function setUp() {
-		parent::setUp();
-		self::_register_taxonomy();
+	public function set_up() {
+		parent::set_up();
+		self::register_custom_taxonomy();
 	}
 
 	/**
-	 * Register a custom taxonomy for use in tests.
+	 * @dataProvider data_get_edit_term_link
+	 *
+	 * @ticket 50225
+	 *
+	 * @param string $taxonomy Taxonomy been tested (used for index of term keys).
+	 * @param bool   $use_id   When true, pass term ID. Else, pass term object.
+	 * @param string $expected Expected URL within admin of edit link.
 	 */
-	public static function _register_taxonomy() {
+	public function test_get_edit_term_link_for_permitted_user( $taxonomy, $use_id, $expected ) {
+		wp_set_current_user( self::$user_ids['admin'] );
+		$term = $this->get_term( $taxonomy, $use_id );
+
+		// Term IDs are not known by the data provider so need to be replaced.
+		$expected = str_replace( '%ID%', $use_id ? $term : $term->term_id, $expected );
+		$expected = admin_url( $expected );
+
+		$this->assertSame( $expected, get_edit_term_link( $term, $taxonomy ) );
+		$this->assertSame( $expected, get_edit_term_link( get_term( $term, $taxonomy ), $taxonomy ) );
+	}
+
+	/**
+	 * @dataProvider data_get_edit_term_link
+	 *
+	 * @ticket 50225
+	 *
+	 * @param string $taxonomy Taxonomy been tested (used for index of term keys).
+	 * @param bool   $use_id   When true, pass term ID. Else, pass term object.
+	 */
+	public function test_get_edit_term_link_for_denied_user( $taxonomy, $use_id ) {
+		wp_set_current_user( self::$user_ids['subscriber'] );
+		$term = $this->get_term( $taxonomy, $use_id );
+
+		$this->assertNull( get_edit_term_link( $term, $taxonomy ) );
+		$this->assertNull( get_edit_term_link( get_term( $term, $taxonomy ), $taxonomy ) );
+	}
+
+	/**
+	 * @dataProvider data_get_edit_term_link
+	 *
+	 * @ticket 50225
+	 *
+	 * @param string $taxonomy Taxonomy been tested (used for index of term keys).
+	 * @param bool   $use_id   When true, pass term ID. Else, pass term object.
+	 */
+	public function test_get_edit_term_link_filter_is_int_by_term_id( $taxonomy, $use_id ) {
+		wp_set_current_user( self::$user_ids['admin'] );
+		$term = $this->get_term( $taxonomy, $use_id );
+
+		add_filter(
+			'get_edit_term_link',
+			function( $location, $term ) {
+				$this->assertIsInt( $term );
+			},
+			10,
+			2
+		);
+
+		get_edit_term_link( $term, $taxonomy );
+	}
+
+	/**
+	 * @dataProvider data_get_edit_term_link
+	 *
+	 * @ticket 50225
+	 *
+	 * @param string $taxonomy Taxonomy been tested (used for index of term keys).
+	 * @param bool   $use_id   When true, pass term ID. Else, pass term object.
+	 */
+	public function test_get_edit_term_link_filter_is_int_by_term_object( $taxonomy, $use_id ) {
+		wp_set_current_user( self::$user_ids['admin'] );
+		$term = $this->get_term( $taxonomy, $use_id );
+
+		add_filter(
+			'get_edit_term_link',
+			function( $location, $term ) {
+				$this->assertIsInt( $term );
+			},
+			10,
+			2
+		);
+
+		get_edit_term_link( get_term( $term, $taxonomy ), $taxonomy );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_get_edit_term_link() {
+		return array(
+			'category passing term_id'          => array(
+				'taxonomy' => 'category',
+				'use_id'   => false,
+				'expected' => 'term.php?taxonomy=category&tag_ID=%ID%&post_type=post',
+			),
+			'category passing term object'      => array(
+				'taxonomy' => 'category',
+				'use_id'   => true,
+				'expected' => 'term.php?taxonomy=category&tag_ID=%ID%&post_type=post',
+			),
+			'post_tag passing term_id'          => array(
+				'taxonomy' => 'post_tag',
+				'use_id'   => false,
+				'expected' => 'term.php?taxonomy=post_tag&tag_ID=%ID%&post_type=post',
+			),
+			'post_tag passing term object'      => array(
+				'taxonomy' => 'post_tag',
+				'use_id'   => true,
+				'expected' => 'term.php?taxonomy=post_tag&tag_ID=%ID%&post_type=post',
+			),
+			'a custom taxonomy passing term_id' => array(
+				'taxonomy' => 'custom_taxonomy',
+				'use_id'   => false,
+				'expected' => 'term.php?taxonomy=custom_taxonomy&tag_ID=%ID%&post_type=post',
+			),
+			'a custom taxonomy passing term_id' => array(
+				'taxonomy' => 'custom_taxonomy',
+				'use_id'   => true,
+				'expected' => 'term.php?taxonomy=custom_taxonomy&tag_ID=%ID%&post_type=post',
+			),
+		);
+	}
+
+	/**
+	 * Helper to register a custom taxonomy for use in tests.
+	 *
+	 * @since 5.9.0
+	 */
+	private static function register_custom_taxonomy() {
 		register_taxonomy( 'custom_taxonomy', 'post' );
 	}
 
 	/**
-	 * @dataProvider data_get_edit_term_link
+	 * Helper to get the term for the given taxonomy.
+	 *
+	 * @since 5.9.0
 	 *
 	 * @param string $taxonomy Taxonomy been tested (used for index of term keys).
-	 * @param string $expected Expected URL within admin of edit link.
-	 */
-	public function test_get_edit_term_link_for_permitted_user( $taxonomy, $expected ) {
-		wp_set_current_user( self::$user_ids['admin'] );
-		$term_id = self::$term_ids[ $taxonomy ];
+	 * @param bool   $use_id   When true, pass term ID. Else, pass term object.
+	 * @return WP_Term|int If $use_id is true, term ID is returned; else instance of WP_Term.
+	 */	
+	private function get_term( $taxonomy, $use_id ) {
+		$term = self::$terms[ $taxonomy ];
+		if ( $use_id ) {
+			$term = $term->term_id;
+		}
 
-		// Term IDs are not known by the data provider so need to be replaced.
-		$expected = str_replace( '%ID%', $term_id, $expected );
-
-		$this->assertSame( admin_url( $expected ), get_edit_term_link( $term_id, $taxonomy ) );
-		$this->assertSame( admin_url( $expected ), get_edit_term_link( get_term( $term_id, $taxonomy ), $taxonomy ) );
-	}
-
-	/**
-	 * @dataProvider data_get_edit_term_link
-	 *
-	 * @param string $taxonomy Taxonomy been tested (used for index of term keys).
-	 */
-	public function test_get_edit_term_link_for_denied_user( $taxonomy ) {
-		wp_set_current_user( self::$user_ids['subscriber'] );
-		$term_id = self::$term_ids[ $taxonomy ];
-
-		$this->assertNull( get_edit_term_link( $term_id, $taxonomy ) );
-		$this->assertNull( get_edit_term_link( get_term( $term_id, $taxonomy ), $taxonomy ) );
-	}
-
-	/**
-	 * @dataProvider data_get_edit_term_link
-	 *
-	 * @param string $taxonomy Taxonomy been tested (used for index of term keys).
-	 */
-	public function test_get_edit_term_link_filter_is_int_by_term_id( $taxonomy ) {
-		wp_set_current_user( self::$user_ids['admin'] );
-		$term_id = self::$term_ids[ $taxonomy ];
-
-		add_filter(
-			'get_edit_term_link',
-			function( $location, $term ) {
-				$this->assertInternalType( 'int', $term );
-			},
-			10,
-			2
-		);
-
-		get_edit_term_link( $term_id, $taxonomy );
-	}
-
-	/**
-	 * @dataProvider data_get_edit_term_link
-	 *
-	 * @param string $taxonomy Taxonomy been tested (used for index of term keys).
-	 */
-	public function test_get_edit_term_link_filter_is_int_by_term_object( $taxonomy ) {
-		wp_set_current_user( self::$user_ids['admin'] );
-		$term_id = self::$term_ids[ $taxonomy ];
-
-		add_filter(
-			'get_edit_term_link',
-			function( $location, $term ) {
-				$this->assertInternalType( 'int', $term );
-			},
-			10,
-			2
-		);
-
-		get_edit_term_link( get_term( $term_id, $taxonomy ), $taxonomy );
-	}
-
-	public function data_get_edit_term_link() {
-		return array(
-			array( 'category', 'term.php?taxonomy=category&tag_ID=%ID%&post_type=post' ),
-			array( 'post_tag', 'term.php?taxonomy=post_tag&tag_ID=%ID%&post_type=post' ),
-			array( 'custom_taxonomy', 'term.php?taxonomy=custom_taxonomy&tag_ID=%ID%&post_type=post' ),
-		);
+		return $term;
 	}
 }
