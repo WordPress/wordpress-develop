77611673870900f8c667fab378f857c11f9b0349
Jb Audras <audrasjb@gmail.com>
2021-11-15 21:30:05 +0100

    Coding standards fixes

--- tests/phpunit/tests/functions/xmlrpc.php
+++ tests/phpunit/tests/functions/xmlrpc.php
@@ -4,7 +4,7 @@
  * @group xmlrpc
  * @ticket 53490
  */
-class Tests_function_xmlrpc extends WP_UnitTestCase{
+class Tests_function_xmlrpc extends WP_UnitTestCase {
 
 	private $test_content = '
 	<title>title</title>
@@ -12,56 +12,56 @@ class Tests_function_xmlrpc extends WP_UnitTestCase{
 	<content>content</content>';
 
 	/**
-	 * tests that the title is returned
+	 * Tests that the title is returned.
 	 *
 	 * @covers ::xmlrpc_getposttitle
 	 */
-	public function test_xmlrpc_getposttitle(){
+	public function test_xmlrpc_getposttitle() {
 
 		$this->assertSame( 'title', xmlrpc_getposttitle( $this->test_content ) );
 	}
 
 	/**
-	 * tests that default title is return if no title in xml
+	 * Tests that default title is return if no title in XML.
 	 *
 	 * @covers ::xmlrpc_getposttitle
 	 */
-	public function test_xmlrpc_getposttitle_default(){
+	public function test_xmlrpc_getposttitle_default() {
 		global $post_default_title;
 		$post_default_title = 'post_default_title';
 
-		$this->assertSame( 'post_default_title', xmlrpc_getposttitle('' ) );
+		$this->assertSame( 'post_default_title', xmlrpc_getposttitle( '' ) );
 	}
 
 
 	/**
-	 * tests that the category(ies) are returned
+	 * Tests that the category(ies) are returned.
 	 *
 	 * @covers ::xmlrpc_getpostcategory
 	 */
-	public function test_xmlrpc_getpostcategory(){
+	public function test_xmlrpc_getpostcategory() {
 
-		$this->assertSame( array('category','category1'), xmlrpc_getpostcategory( $this->test_content ) );
+		$this->assertSame( array( 'category', 'category1' ), xmlrpc_getpostcategory( $this->test_content ) );
 	}
 
 	/**
-	 * tests that default category is returned in no category in xml
+	 * Tests that default category is returned in no category in XML.
 	 *
 	 * @covers ::xmlrpc_getpostcategory
 	 */
-	public function test_xmlrpc_getpostcategory_default(){
+	public function test_xmlrpc_getpostcategory_default() {
 		global $post_default_category;
 		$post_default_category = 'post_default_category';
 
-		$this->assertSame( 'post_default_category', xmlrpc_getpostcategory('' ) );
+		$this->assertSame( 'post_default_category', xmlrpc_getpostcategory( '' ) );
 	}
 
 	/**
-	 * tests that just the content is returned
+	 * Tests that just the content is returned.
 	 *
 	 * @covers ::xmlrpc_removepostdata
 	 */
-	public function test_xmlrpc_removepostdata(){
+	public function test_xmlrpc_removepostdata() {
 		global $post_default_category;
 		$post_default_category = 'post_default_category';
 
