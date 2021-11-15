b01238d7ffab2b9abcd6cc29c81a6e41326419dd
pbearne <pbearne@gmail.com>
2021-11-15 15:42:17 -0500

    added tests

---
+++ tests/phpunit/tests/multisite/ms-functions/update_posts_count.php
@@ -0,0 +1,48 @@
+<?php
+
+if ( is_multisite() ) :
+	/**
+	 * Test update_posts_count() get called via filters of WP_Site in multisite.
+	 *
+	 * @group ms-site
+	 * @group multisite
+	 *
+	 * @covers ::_update_posts_count_on_delete
+	 */
+	class Tests_update_posts_count_on_delete extends WP_UnitTestCase {
+
+		/**
+		 * This test that the posts count is updated correctly when a posts are added and deleted.
+		 * @ticket 53443
+		 */
+		public function test_update_posts_count_on_delete() {
+
+			$current_post_count = (int) get_option( 'post_count' );
+
+			$post_id = self::factory()->post->create(
+				array(
+					'post_type'   => 'post',
+					'post_author' => '1',
+					'post_date'   => '2012-10-23 19:34:42',
+				)
+			);
+
+			/**
+			 * checks the add_action( 'deleted_post', '_update_posts_count_on_delete' ); is called when a post is created.
+			 * and __update_posts_count_on_delete) is called on that filter which call update_posts_count to update the count.
+			 */
+			$this->assertEquals( $current_post_count + 1, (int) get_option( 'post_count' ) );
+
+			wp_delete_post( $post_id );
+
+			/**
+			 * checks the add_action( 'transition_post_status', '_update_posts_count_on_transition_post_status', 10, 3 ); is called when a post is created.
+			 * and _update_posts_count_on_transition_post_status() is called on that filter which call update_posts_count to update the count.
+			 */
+			$this->assertEquals( $current_post_count, (int) get_option( 'post_count' ) );
+
+		}
+	}
+
+
+endif;
