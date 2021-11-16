aa36e352761176b13410e8dd56c082bdcf7fc566
costdev <me@colin-stewart.co.uk>
2021-11-16 08:28:07 +0000

    Tests: Tidy up.

--- tests/phpunit/tests/media/getPostGalleries.php
+++ tests/phpunit/tests/media/getPostGalleries.php
@@ -16,7 +16,7 @@ class Tests_Functions_getPostGalleries extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test that an empty array is returns for a post that does not exist.
+	 * Test that an empty array is returned for a post that does not exist.
 	 *
 	 * @ticket 43826
 	 */
@@ -26,7 +26,7 @@ class Tests_Functions_getPostGalleries extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test that an empty array is returns for a post that has no gallery.
+	 * Test that an empty array is returned for a post that has no gallery.
 	 *
 	 * @ticket 43826
 	 */
@@ -39,185 +39,6 @@ class Tests_Functions_getPostGalleries extends WP_UnitTestCase {
 
 		$galleries = get_post_galleries( $post_id, false );
 		$this->assertEmpty( $galleries );
-	}
-
-	/**
-	 * Test that HTML is returned for a shortcode gallery.
-	 *
-	 * @ticket 43826
-	 *
-	 * @group shortcode
-	 */
-	public function test_returns_html_with_shortcode_gallery() {
-		$post_id = $this->factory->post->create(
-			array(
-				'post_content' => 'I have no gallery',
-			)
-		);
-
-		$post_id_two = $this->factory->post->create(
-			array(
-				'post_content' => "[gallery id='$post_id']",
-			)
-		);
-
-		$this->factory->attachment->create_object(
-			array(
-				'file'           => 'test.jpg',
-				'post_parent'    => $post_id,
-				'post_mime_type' => 'image/jpeg',
-				'post_type'      => 'attachment',
-			)
-		);
-
-		$expected  = 'src="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg"';
-		$galleries = get_post_galleries( $post_id_two );
-
-		// The method can return an empty array.
-		$this->assertNotEmpty(
-			$galleries,
-			'The galleries array is empty.'
-		);
-
-		// The method can return an array of arrays
-		// instead of an array of strings.
-		$this->assertIsString(
-			$galleries[0],
-			'Did not return the data as a string.'
-		);
-
-		$this->assertStringContainsString(
-			$expected,
-			$galleries[0],
-			'The returned data did not contain a src attribute with the expected image URL.'
-		);
-	}
-
-	/**
-	 * Test returns HTML for a block gallery.
-	 *
-	 * @ticket 43826
-	 *
-	 * @group blocks
-	 */
-	public function test_returns_html_with_block_gallery() {
-		$post_id = $this->factory->post->create(
-			array(
-				'post_content' => 'I have no gallery.',
-			)
-		);
-
-		// Set up an unattached image.
-		$image_id = $this->factory->attachment->create(
-			array(
-				'file'           => 'test.jpg',
-				'post_parent'    => $post_id,
-				'post_mime_type' => 'image/jpeg',
-				'post_type'      => 'attachment',
-			)
-		);
-
-		$image_url = 'http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg';
-
-		$blob = <<< BLOB
-<!-- wp:gallery -->
-<figure><img src="$image_url" data-id="$image_id" /></figure>
-<!-- /wp:gallery -->
-BLOB;
-
-		$post_id_two = $this->factory->post->create(
-			array(
-				'post_content' => $blob,
-			)
-		);
-
-		$expected  = 'src="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg"';
-		$galleries = get_post_galleries( $post_id_two );
-
-		// The method can return an empty array.
-		$this->assertNotEmpty(
-			$galleries,
-			'The galleries array is empty.'
-		);
-
-		// The method can return an array of arrays
-		// instead of an array of strings.
-		$this->assertIsString(
-			$galleries[0],
-			'Did not return the data as a string.'
-		);
-
-		$this->assertStringContainsString(
-			$expected,
-			$galleries[0],
-			'The returned data did not contain a src attribute with the expected image URL.'
-		);
-	}
-
-	/**
-	 * Test returns HTML for a block gallery v2.
-	 *
-	 * @ticket 43826
-	 *
-	 * @group blocks
-	 */
-	public function test_returns_html_with_block_gallery_v2() {
-		$image_id = $this->factory->attachment->create_object(
-			array(
-				'file'           => 'test.jpg',
-				'post_parent'    => 0,
-				'post_mime_type' => 'image/jpeg',
-				'post_type'      => 'attachment',
-			)
-		);
-
-		$image_url = 'http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg';
-
-		$blob = <<< BLOB
-<!-- wp:gallery {"linkTo":"none","className":"columns-2"} -->
-<figure
-	class="wp-block-gallery has-nested-images columns-default is-cropped columns-2"
->
-	<!-- wp:image {"id":$image_id,"sizeSlug":"large","linkDestination":"none"} -->
-	<figure class="wp-block-image size-large">
-		<img
-			src="$image_url"
-			alt="Image gallery image"
-			class="wp-image-$image_id"
-		/>
-	</figure>
-	<!-- /wp:image -->
-</figure>
-<!-- /wp:gallery -->
-BLOB;
-
-		$post_id = $this->factory->post->create(
-			array(
-				'post_content' => $blob,
-			)
-		);
-
-		$expected  = 'src="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg"';
-		$galleries = get_post_galleries( $post_id );
-
-		// The method can return an empty array.
-		$this->assertNotEmpty(
-			$galleries,
-			'The galleries array is empty.'
-		);
-
-		// The method can return an array of arrays
-		// instead of an array of strings.
-		$this->assertIsString(
-			$galleries[0],
-			'Did not return the data as a string.'
-		);
-
-		$this->assertStringContainsString(
-			$expected,
-			$galleries[0],
-			'The returned data did not contain a src attribute with the expected image URL.'
-		);
 	}
 
 	/**
@@ -268,13 +89,13 @@ BLOB;
 	}
 
 	/**
-		 * Test that no srcs are returned for a gallery block
-		 * in a post with no attached images.
-		 *
-		 * @ticket 43826
-		 *
-		 * @group blocks
-		 */
+	 * Test that no srcs are returned for a gallery block
+	 * in a post with no attached images.
+	 *
+	 * @ticket 43826
+	 *
+	 * @group blocks
+	 */
 	public function test_returns_no_srcs_with_block_in_post_with_no_attached_images() {
 		// Set up an unattached image.
 		$this->factory->attachment->create_object(
@@ -394,6 +215,185 @@ BLOB;
 			$expected_srcs,
 			$galleries[0]['src'],
 			'The expected and actual srcs are not the same.'
+		);
+	}
+
+	/**
+	 * Test that HTML is returned for a shortcode gallery.
+	 *
+	 * @ticket 43826
+	 *
+	 * @group shortcode
+	 */
+	public function test_returns_html_with_shortcode_gallery() {
+		$post_id = $this->factory->post->create(
+			array(
+				'post_content' => 'I have no gallery',
+			)
+		);
+
+		$post_id_two = $this->factory->post->create(
+			array(
+				'post_content' => "[gallery id='$post_id']",
+			)
+		);
+
+		$this->factory->attachment->create_object(
+			array(
+				'file'           => 'test.jpg',
+				'post_parent'    => $post_id,
+				'post_mime_type' => 'image/jpeg',
+				'post_type'      => 'attachment',
+			)
+		);
+
+		$expected  = 'src="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg"';
+		$galleries = get_post_galleries( $post_id_two );
+
+		// The method can return an empty array.
+		$this->assertNotEmpty(
+			$galleries,
+			'The galleries array is empty.'
+		);
+
+		// The method can return an array of arrays
+		// instead of an array of strings.
+		$this->assertIsString(
+			$galleries[0],
+			'Did not return the data as a string.'
+		);
+
+		$this->assertStringContainsString(
+			$expected,
+			$galleries[0],
+			'The returned data did not contain a src attribute with the expected image URL.'
+		);
+	}
+
+	/**
+	 * Test that HTML is returned for a block gallery.
+	 *
+	 * @ticket 43826
+	 *
+	 * @group blocks
+	 */
+	public function test_returns_html_with_block_gallery() {
+		$post_id = $this->factory->post->create(
+			array(
+				'post_content' => 'I have no gallery.',
+			)
+		);
+
+		// Set up an unattached image.
+		$image_id = $this->factory->attachment->create(
+			array(
+				'file'           => 'test.jpg',
+				'post_parent'    => $post_id,
+				'post_mime_type' => 'image/jpeg',
+				'post_type'      => 'attachment',
+			)
+		);
+
+		$image_url = 'http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg';
+
+		$blob = <<< BLOB
+<!-- wp:gallery -->
+<figure><img src="$image_url" data-id="$image_id" /></figure>
+<!-- /wp:gallery -->
+BLOB;
+
+		$post_id_two = $this->factory->post->create(
+			array(
+				'post_content' => $blob,
+			)
+		);
+
+		$expected  = 'src="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg"';
+		$galleries = get_post_galleries( $post_id_two );
+
+		// The method can return an empty array.
+		$this->assertNotEmpty(
+			$galleries,
+			'The galleries array is empty.'
+		);
+
+		// The method can return an array of arrays
+		// instead of an array of strings.
+		$this->assertIsString(
+			$galleries[0],
+			'Did not return the data as a string.'
+		);
+
+		$this->assertStringContainsString(
+			$expected,
+			$galleries[0],
+			'The returned data did not contain a src attribute with the expected image URL.'
+		);
+	}
+
+	/**
+	 * Test that HTML is returned for a block gallery v2.
+	 *
+	 * @ticket 43826
+	 *
+	 * @group blocks
+	 */
+	public function test_returns_html_with_block_gallery_v2() {
+		$image_id = $this->factory->attachment->create_object(
+			array(
+				'file'           => 'test.jpg',
+				'post_parent'    => 0,
+				'post_mime_type' => 'image/jpeg',
+				'post_type'      => 'attachment',
+			)
+		);
+
+		$image_url = 'http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg';
+
+		$blob = <<< BLOB
+<!-- wp:gallery {"linkTo":"none","className":"columns-2"} -->
+<figure
+	class="wp-block-gallery has-nested-images columns-default is-cropped columns-2"
+>
+	<!-- wp:image {"id":$image_id,"sizeSlug":"large","linkDestination":"none"} -->
+	<figure class="wp-block-image size-large">
+		<img
+			src="$image_url"
+			alt="Image gallery image"
+			class="wp-image-$image_id"
+		/>
+	</figure>
+	<!-- /wp:image -->
+</figure>
+<!-- /wp:gallery -->
+BLOB;
+
+		$post_id = $this->factory->post->create(
+			array(
+				'post_content' => $blob,
+			)
+		);
+
+		$expected  = 'src="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/test.jpg"';
+		$galleries = get_post_galleries( $post_id );
+
+		// The method can return an empty array.
+		$this->assertNotEmpty(
+			$galleries,
+			'The galleries array is empty.'
+		);
+
+		// The method can return an array of arrays
+		// instead of an array of strings.
+		$this->assertIsString(
+			$galleries[0],
+			'Did not return the data as a string.'
+		);
+
+		$this->assertStringContainsString(
+			$expected,
+			$galleries[0],
+			'The returned data did not contain a src attribute with the expected image URL.'
 		);
 	}
 
@@ -710,6 +710,130 @@ BLOB;
 		);
 	}
 
+	/**
+	 * Test that galleries only contain images specified in the
+	 * id attribute of their respective shortcode and block.
+	 *
+	 * @ticket 43826
+	 *
+	 * @group blocks
+	 * @group shortcode
+	 */
+	public function test_respects_shortcode_and_block_id_attributes() {
+		// Test the get_post_galleries() function in $html=false mode, with both shortcode and block galleries
+		$ids      = array();
+		$imgs     = array();
+		$ids_srcs = array();
+		foreach ( range( 1, 6 ) as $i ) {
+			$attachment_id = self::factory()->attachment->create_object(
+				"image$i.jpg",
+				0,
+				array(
+					'post_mime_type' => 'image/jpeg',
+					'post_type'      => 'attachment',
+				)
+			);
+			$metadata      = array_merge( array( 'file' => "image$i.jpg" ), $this->img_meta );
+			wp_update_attachment_metadata( $attachment_id, $metadata );
+			$ids[]      = $attachment_id;
+			$url        = 'http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/' . "image$i.jpg";
+			$ids_srcs[] = $url;
+			$imgs[]     = '<figure><img src="' . $url . '" data-id="' . $i . '" /></figure>';
+
+		}
+
+		$ids1_joined = join( ',', array_slice( $ids, 0, 3 ) );
+		$ids2_joined = join( ',', array_slice( $ids, 3, 3 ) );
+
+		$blob = <<<BLOB
+[gallery ids="$ids1_joined"]
+
+<!-- wp:gallery {"ids":[$ids2_joined]} -->
+<!-- /wp:gallery -->
+BLOB;
+
+		$post_id = self::factory()->post->create( array( 'post_content' => $blob ) );
+
+		$galleries = get_post_galleries( $post_id, false );
+		$this->assertSameSetsWithIndex(
+			array(
+				array(
+					'ids' => $ids1_joined,
+					'src' => array_slice( $ids_srcs, 0, 3 ),
+				),
+				array(
+					'ids' => $ids2_joined,
+					'src' => array_slice( $ids_srcs, 3, 3 ),
+				),
+			),
+			$galleries
+		);
+
+	}
+
+	/**
+	 * Test that galleries contain the additional attributes
+	 * specified for their respective shortcode and block.
+	 *
+	 * @ticket 43826
+	 *
+	 * @group blocks
+	 * @group shortcode
+	 */
+	public function test_respects_additional_shortcode_and_block_attributes() {
+		// Test attributes returned by get_post_galleries() function in $html=false mode, with both shortcode and block galleries
+		$ids      = array();
+		$imgs     = array();
+		$ids_srcs = array();
+		foreach ( range( 1, 6 ) as $i ) {
+			$attachment_id = self::factory()->attachment->create_object(
+				"image$i.jpg",
+				0,
+				array(
+					'post_mime_type' => 'image/jpeg',
+					'post_type'      => 'attachment',
+				)
+			);
+			$metadata      = array_merge( array( 'file' => "image$i.jpg" ), $this->img_meta );
+			wp_update_attachment_metadata( $attachment_id, $metadata );
+			$ids[]      = $attachment_id;
+			$url        = 'http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/' . "image$i.jpg";
+			$ids_srcs[] = $url;
+			$imgs[]     = '<figure><img src="' . $url . '" data-id="' . $i . '" /></figure>';
+
+		}
+
+		$ids1_joined = join( ',', array_slice( $ids, 0, 3 ) );
+		$ids2_joined = join( ',', array_slice( $ids, 3, 3 ) );
+		$blob        = <<<BLOB
+[gallery ids="$ids1_joined" type="type" foo="bar"]
+
+<!-- wp:gallery {"ids":[$ids2_joined],"columns":3,"imageCrop":false,"linkTo":"media"} -->
+<!-- /wp:gallery -->
+BLOB;
+
+		$post_id = self::factory()->post->create( array( 'post_content' => $blob ) );
+
+		$galleries = get_post_galleries( $post_id, false );
+		$this->assertSameSetsWithIndex(
+			array(
+				array(
+					'ids'  => $ids1_joined,
+					// The shortcode code passes arbitrary attributes
+					'type' => 'type',
+					'foo'  => 'bar',
+					'src'  => array_slice( $ids_srcs, 0, 3 ),
+				),
+				array(
+					'ids' => $ids2_joined,
+					// The block only passes ids, no other attributes
+					'src' => array_slice( $ids_srcs, 3, 3 ),
+				),
+			),
+			$galleries
+		);
+
+	}
 
 	/**
 	 * Test that srcs are retrieved from the HTML of a block gallery
@@ -841,130 +965,5 @@ BLOB;
 			$galleries[0]['src'],
 			'The src key of the first gallery is empty.'
 		);
-	}
-
-	/**
-	 * Test that galleries only contain images specified in the
-	 * id attribute of their respective shortcode and block.
-	 *
-	 * @ticket 43826
-	 *
-	 * @group blocks
-	 * @group shortcode
-	 */
-	public function test_respects_shortcode_and_block_id_attributes() {
-		// Test the get_post_galleries() function in $html=false mode, with both shortcode and block galleries
-		$ids      = array();
-		$imgs     = array();
-		$ids_srcs = array();
-		foreach ( range( 1, 6 ) as $i ) {
-			$attachment_id = self::factory()->attachment->create_object(
-				"image$i.jpg",
-				0,
-				array(
-					'post_mime_type' => 'image/jpeg',
-					'post_type'      => 'attachment',
-				)
-			);
-			$metadata      = array_merge( array( 'file' => "image$i.jpg" ), $this->img_meta );
-			wp_update_attachment_metadata( $attachment_id, $metadata );
-			$ids[]      = $attachment_id;
-			$url        = 'http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/' . "image$i.jpg";
-			$ids_srcs[] = $url;
-			$imgs[]     = '<figure><img src="' . $url . '" data-id="' . $i . '" /></figure>';
-
-		}
-
-		$ids1_joined = join( ',', array_slice( $ids, 0, 3 ) );
-		$ids2_joined = join( ',', array_slice( $ids, 3, 3 ) );
-
-		$blob = <<<BLOB
-[gallery ids="$ids1_joined"]
-
-<!-- wp:gallery {"ids":[$ids2_joined]} -->
-<!-- /wp:gallery -->
-BLOB;
-
-		$post_id = self::factory()->post->create( array( 'post_content' => $blob ) );
-
-		$galleries = get_post_galleries( $post_id, false );
-		$this->assertSameSetsWithIndex(
-			array(
-				array(
-					'ids' => $ids1_joined,
-					'src' => array_slice( $ids_srcs, 0, 3 ),
-				),
-				array(
-					'ids' => $ids2_joined,
-					'src' => array_slice( $ids_srcs, 3, 3 ),
-				),
-			),
-			$galleries
-		);
-
-	}
-
-	/**
-	 * Test that galleries contain the additional attributes
-	 * specified for their respective shortcode and block.
-	 *
-	 * @ticket 43826
-	 *
-	 * @group blocks
-	 * @group shortcode
-	 */
-	public function test_respects_additional_shortcode_and_block_attributes() {
-		// Test attributes returned by get_post_galleries() function in $html=false mode, with both shortcode and block galleries
-		$ids      = array();
-		$imgs     = array();
-		$ids_srcs = array();
-		foreach ( range( 1, 6 ) as $i ) {
-			$attachment_id = self::factory()->attachment->create_object(
-				"image$i.jpg",
-				0,
-				array(
-					'post_mime_type' => 'image/jpeg',
-					'post_type'      => 'attachment',
-				)
-			);
-			$metadata      = array_merge( array( 'file' => "image$i.jpg" ), $this->img_meta );
-			wp_update_attachment_metadata( $attachment_id, $metadata );
-			$ids[]      = $attachment_id;
-			$url        = 'http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/' . "image$i.jpg";
-			$ids_srcs[] = $url;
-			$imgs[]     = '<figure><img src="' . $url . '" data-id="' . $i . '" /></figure>';
-
-		}
-
-		$ids1_joined = join( ',', array_slice( $ids, 0, 3 ) );
-		$ids2_joined = join( ',', array_slice( $ids, 3, 3 ) );
-		$blob        = <<<BLOB
-[gallery ids="$ids1_joined" type="type" foo="bar"]
-
-<!-- wp:gallery {"ids":[$ids2_joined],"columns":3,"imageCrop":false,"linkTo":"media"} -->
-<!-- /wp:gallery -->
-BLOB;
-
-		$post_id = self::factory()->post->create( array( 'post_content' => $blob ) );
-
-		$galleries = get_post_galleries( $post_id, false );
-		$this->assertSameSetsWithIndex(
-			array(
-				array(
-					'ids'  => $ids1_joined,
-					// The shortcode code passes arbitrary attributes
-					'type' => 'type',
-					'foo'  => 'bar',
-					'src'  => array_slice( $ids_srcs, 0, 3 ),
-				),
-				array(
-					'ids' => $ids2_joined,
-					// The block only passes ids, no other attributes
-					'src' => array_slice( $ids_srcs, 3, 3 ),
-				),
-			),
-			$galleries
-		);
-
 	}
 }
