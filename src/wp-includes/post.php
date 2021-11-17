351bda87fbc9789b6219de3172521af81d346661
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2021-11-17 14:05:38 +1100

    Docblock.

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -7642,6 +7642,7 @@ function _publish_post_hook( $post_id ) {
  * Returns the ID of the post's parent.
  *
  * @since 3.1.0
+ * @since 5.9.0 The `$post` parameter was made optional.
  *
  * @param int|WP_Post|null $post Optional. Post ID or post object. Defaults to global $post.
  * @return int|false Post parent ID (which can be 0 if there is no parent),
