45a0c0481a07c00317153d0f080496845d0064f9
Bernie Reiter <ockham@raz.or.at>
2021-11-19 20:44:22 +0100

    Backport block template resolution algorithm unit tests

--- tests/phpunit/tests/block-template.php
+++ tests/phpunit/tests/block-template.php
@@ -40,6 +40,106 @@ class Block_Template_Test extends WP_UnitTestCase {
 		unset( $_wp_current_template_content );
 	}
 
+	function test_page_home_block_template_takes_precedence_over_less_specific_block_templates() {
+		global $_wp_current_template_content;
+		$type                   = 'page';
+		$templates              = array(
+			'page-home.php',
+			'page-1.php',
+			'page.php',
+		);
+		$resolved_template_path = locate_block_template( get_stylesheet_directory() . '/page-home.php', $type, $templates );
+		$this->assertEquals( self::$template_canvas_path, $resolved_template_path );
+		$this->assertStringEqualsFile( get_stylesheet_directory() . '/block-templates/page-home.html', $_wp_current_template_content );
+	}
+
+	function test_page_block_template_takes_precedence() {
+		global $_wp_current_template_content;
+		$type                   = 'page';
+		$templates              = array(
+			'page-slug-doesnt-exist.php',
+			'page-1.php',
+			'page.php',
+		);
+		$resolved_template_path = locate_block_template( get_stylesheet_directory() . '/page.php', $type, $templates );
+		$this->assertEquals( self::$template_canvas_path, $resolved_template_path );
+		$this->assertStringEqualsFile( get_stylesheet_directory() . '/block-templates/page.html', $_wp_current_template_content );
+	}
+
+	function test_block_template_takes_precedence_over_equally_specific_php_template() {
+		global $_wp_current_template_content;
+		$type                   = 'index';
+		$templates              = array(
+			'index.php',
+		);
+		$resolved_template_path = locate_block_template( get_stylesheet_directory() . '/index.php', $type, $templates );
+		$this->assertEquals( self::$template_canvas_path, $resolved_template_path );
+		$this->assertStringEqualsFile( get_stylesheet_directory() . '/block-templates/index.html', $_wp_current_template_content );
+	}
+
+	/**
+	 * In a hybrid theme, a PHP template of higher specificity will take precedence over a block template
+	 * with lower specificity.
+	 *
+	 * Covers https://github.com/WordPress/gutenberg/pull/29026.
+	 */
+	function test_more_specific_php_template_takes_precedence_over_less_specific_block_template() {
+		$page_id_template       = 'page-1.php';
+		$page_id_template_path  = get_stylesheet_directory() . '/' . $page_id_template;
+		$type                   = 'page';
+		$templates              = array(
+			'page-slug-doesnt-exist.php',
+			'page-1.php',
+			'page.php',
+		);
+		$resolved_template_path = locate_block_template( $page_id_template_path, $type, $templates );
+		$this->assertEquals( $page_id_template_path, $resolved_template_path );
+	}
+
+	/**
+	 * If a theme is a child of a block-based parent theme but has php templates for some of its pages,
+	 * a php template of the child will take precedence over the parent's block template if they have
+	 * otherwise equal specificity.
+	 *
+	 * Covers https://github.com/WordPress/gutenberg/pull/31123.
+	 */
+	function test_child_theme_php_template_takes_precedence_over_equally_specific_parent_theme_block_template() {
+		switch_theme( 'block-theme-child' );
+
+		$page_slug_template      = 'page-home.php';
+		$page_slug_template_path = get_stylesheet_directory() . '/' . $page_slug_template;
+		$type                    = 'page';
+		$templates               = array(
+			'page-home.php',
+			'page-1.php',
+			'page.php',
+		);
+		$resolved_template_path  = locate_block_template( $page_slug_template_path, $type, $templates );
+		$this->assertEquals( $page_slug_template_path, $resolved_template_path );
+
+		switch_theme( 'block-theme' );
+	}
+
+	function test_child_theme_block_template_takes_precedence_over_equally_specific_parent_theme_php_template() {
+		global $_wp_current_template_content;
+
+		switch_theme( 'block-theme-child' );
+
+		$page_template                   = 'page-1.php';
+		$parent_theme_page_template_path = get_template_directory() . '/' . $page_template;
+		$type                            = 'page';
+		$templates                       = array(
+			'page-slug-doesnt-exist.php',
+			'page-1.php',
+			'page.php',
+		);
+		$resolved_template_path          = locate_block_template( $parent_theme_page_template_path, $type, $templates );
+		$this->assertEquals( self::$template_canvas_path, $resolved_template_path );
+		$this->assertStringEqualsFile( get_stylesheet_directory() . '/block-templates/page-1.html', $_wp_current_template_content );
+
+		switch_theme( 'block-theme' );
+	}
+
 	/**
 	 * Regression: https://github.com/WordPress/gutenberg/issues/31399.
 	 */
