796b7424eb532e8b0749fe3b982bfa24e3751da5
pbearne <pbearne@gmail.com>
2021-11-22 19:31:26 -0500

    fixed phpcs

--- tests/phpunit/tests/functions/get_num_queries.php
+++ tests/phpunit/tests/functions/get_num_queries.php
@@ -11,11 +11,11 @@ class Tests_Functions_get_num_queries extends WP_UnitTestCase {
 
 		$current_count = get_num_queries();
 		$this->assertIsInt( $current_count );
+
 		// do a single db query
 		$wpdb->query( "select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='tableName'" );
 
 		// check the count updated by 1
 		$this->assertEquals( $current_count + 1, get_num_queries() );
 	}
-
 }
