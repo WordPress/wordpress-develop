2e0b4a1cfcc6a9139e39d5b1cf56438bc3451864
Anton Vlasenko <vlasenko.anton@gmail.com>
2021-11-25 17:16:12 +0100

    Better PHPDoc block description.

--- src/wp-admin/includes/dashboard.php
+++ src/wp-admin/includes/dashboard.php
@@ -1987,6 +1987,7 @@ function wp_dashboard_empty() {}
  * Displays a welcome panel to introduce users to WordPress.
  *
  * @since 3.3.0
+ * @since 5.9.0 Send users to the Site Editor if the current theme is block-based.
  */
 function wp_welcome_panel() {
 	$customize_url          = null;
