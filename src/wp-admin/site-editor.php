0d0a349aa049446a38d93692df7bcac6bd76bf18
Jonny Harris <jon@spacedmonkey.co.uk>
2021-11-29 22:30:52 +0000

    Fix lint.

--- src/wp-admin/site-editor.php
+++ src/wp-admin/site-editor.php
@@ -83,7 +83,7 @@ if ( isset( $_GET['postType'] ) && ! isset( $_GET['postId'] ) ) {
 
 	$active_global_styles_id = WP_Theme_JSON_Resolver::get_user_custom_post_type_id();
 	$active_theme            = wp_get_theme()->get_stylesheet();
-	$preload_paths = array(
+	$preload_paths           = array(
 		array( rest_get_route_for_post_type_items( 'attachment' ), 'OPTIONS' ),
 		'/',
 		'/wp/v2/types?context=edit',
