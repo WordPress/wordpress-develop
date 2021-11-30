5585807a8690cd899997fc4ae226bc6c798aea1a
Bernie Reiter <ockham@raz.or.at>
2021-11-30 21:18:21 +0100

    typo

--- src/wp-includes/block-template.php
+++ src/wp-includes/block-template.php
@@ -96,7 +96,7 @@ function locate_block_template( $template, $type, array $templates ) {
  *
  * @param string   $template_type      The current template type.
  * @param string[] $template_hierarchy The current template hierarchy, ordered by priority.
- * @param string   $fallback_template  A PHP fallback template to use if no block matching template is found.
+ * @param string   $fallback_template  A PHP fallback template to use if no matching block template is found.
  * @return WP_Block_Template|null template A template object, or null if none could be found.
  */
 function resolve_block_template( $template_type, $template_hierarchy, $fallback_template ) {
