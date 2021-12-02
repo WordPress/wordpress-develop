690031166fe2d9b3dec521c844083e91a673412c
Tonya Mork <tonya.mork@automattic.com>
2021-12-02 08:26:01 -0600

    Comments: alignment, multiline format, annotation.

--- src/wp-includes/class-wp-theme-json.php
+++ src/wp-includes/class-wp-theme-json.php
@@ -346,8 +346,8 @@ class WP_Theme_JSON {
 	 * @since 5.8.0
 	 *
 	 * @param array  $theme_json A structure that follows the theme.json schema.
-	 * @param string $origin    Optional. What source of data this object represents.
-	 *                          One of 'default', 'theme', or 'custom'. Default 'theme'.
+	 * @param string $origin     Optional. What source of data this object represents.
+	 *                           One of 'default', 'theme', or 'custom'. Default 'theme'.
 	 */
 	public function __construct( $theme_json = array(), $origin = 'theme' ) {
 		if ( ! in_array( $origin, self::VALID_ORIGINS, true ) ) {
@@ -764,12 +764,14 @@ class WP_Theme_JSON {
 				}
 			}
 
-			// Reset default browser margin on the root body element.
-			// We set this on the root selector **before** generating the ruleset
-			// from the `theme.json`. This is to ensure that if the `theme.json` declares
-			// `margin` in its `spacing` declaration for the `body` element then these
-			// user-generated values take precedence in the CSS cascade.
-			// See: https://github.com/WordPress/gutenberg/issues/36147.
+			/*
+			 * Reset default browser margin on the root body element.
+			 * This is set on the root selector **before** generating the ruleset
+			 * from the `theme.json`. This is to ensure that if the `theme.json` declares
+			 * `margin` in its `spacing` declaration for the `body` element then these
+			 * user-generated values take precedence in the CSS cascade.
+			 * @link https://github.com/WordPress/gutenberg/issues/36147.
+			 */
 			if ( self::ROOT_BLOCK_SELECTOR === $selector ) {
 				$block_rules .= "body { margin: 0; }\n";
 			}
