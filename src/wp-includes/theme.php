09d406230dde56eade1c2f27879e73f8cd7ac006
Anton Vlasenko <vlasenko.anton@gmail.com>
2021-12-06 15:35:11 +0100

    Use wp_get_theme()->is_block_theme() inside the wp_is_block_theme function. See the discussion here: https://core.trac.wordpress.org/ticket/54552

--- src/wp-includes/theme.php
+++ src/wp-includes/theme.php
@@ -4091,6 +4091,5 @@ function create_initial_theme_features() {
  * @return boolean Whether the current theme is a block-based theme or not.
  */
 function wp_is_block_theme() {
-	return is_readable( get_theme_file_path( '/block-templates/index.html' ) ) ||
-		is_readable( get_theme_file_path( '/templates/index.html' ) );
+	return wp_get_theme()->is_block_theme();
 }
