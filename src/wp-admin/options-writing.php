eb2db6a1168725756fab683a287556c951f4176c
costdev <me@colin-stewart.co.uk>
2021-12-19 20:38:09 +0000

    Add visibility switch to password field.

--- src/wp-admin/options-writing.php
+++ src/wp-admin/options-writing.php
@@ -54,6 +54,8 @@ get_current_screen()->set_help_sidebar(
 	'<p>' . __( '<a href="https://wordpress.org/support/">Support</a>' ) . '</p>'
 );
 
+wp_enqueue_script( 'user-profile' );
+
 require_once ABSPATH . 'wp-admin/admin-header.php';
 ?>
 
@@ -163,11 +165,22 @@ if ( apply_filters( 'enable_post_by_email_configuration', true ) ) {
 <th scope="row"><label for="mailserver_login"><?php _e( 'Login Name' ); ?></label></th>
 <td><input name="mailserver_login" type="text" id="mailserver_login" value="<?php form_option( 'mailserver_login' ); ?>" class="regular-text ltr" /></td>
 </tr>
-<tr>
-<th scope="row"><label for="mailserver_pass"><?php _e( 'Password' ); ?></label></th>
-<td>
-<input name="mailserver_pass" type="text" id="mailserver_pass" value="<?php form_option( 'mailserver_pass' ); ?>" class="regular-text ltr" />
-</td>
+<tr class="mailserver-pass-wrap">
+	<th scope="row">
+		<label for="mailserver_pass">
+			<?php _e( 'Password' ); ?>
+		</label>
+	</th>
+	<td>
+		<input class="hidden" value=" " /><!-- #24364 workaround -->
+		<span class="password-input-wrapper">
+			<input type="text" name="mailserver_pass" id="mailserver_pass" class="regular-text" autocomplete="off" data-reveal="1" data-pw="<?php echo esc_attr( get_option( 'mailserver_pass' ) ); ?>" />
+		</span>
+		<button type="button" class="button wp-hide-pw hide-if-no-js" data-toggle="0" data-start-masked="1" aria-label="<?php esc_attr_e( 'Hide password' ); ?>">
+			<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+			<span class="text"><?php _e( 'Show' ); ?></span>
+		</button>
+	</td>
 </tr>
 <tr>
 <th scope="row"><label for="default_email_category"><?php _e( 'Default Mail Category' ); ?></label></th>
