16aca51d8be4d9d82e82e6daa8ef69bfad23a0c7
Isabel Brison <isabel_brison@git.wordpress.org>
2021-12-21 07:00:16 +0000

    Update @wordpress packages for Beta 4
    Update packages with these bug fixes from Gutenberg:
    
    Navigation: Remove hardcoded typography units
    Handle parsed request
    Navigation: Refactor modal padding to be simpler and more flexible
    Show notice on save in site editor
    Add aria-pressed true/false to Toggle navigation button based on state
    Components FontSizePicker: Use incremental sequence of numbers
    Custom keys from theme.json: fix kebabCase conversion
    Template Part: Fix 'isMissing' condition check
    Multi-Entity Saving: Decode HTML entities in item titles
    Font sizes: update default values
    Query Loop: Add useBlockPreview, fix Query Loop wide alignment
    Only add dialog role to navigation when modal is open
    Fix navigation appender
    Show a UI warning when user does not have permission to update/edit a Navigation block
    Block editor: Fix Enter handling for nested blocks
    Update: Use subtitle styles for the palette names
    Allow publishing a post while not saving changes to non-post entities
    Update: Block top level useSetting paths
    Fix Site Logo block alignment issues
    Editor: when Toggle navigation receives state false, focus
    ToolsPanel: Allow items to register when panelId is null
    Block Support Panels - Make block support tools panels compatible
    Gallery: Fix block registration hook priority
    Navigation: Fix page list issues in overlay
    Ensure the overlay menu works when inserting navigation block pattern
    Restrict Navigation permissions and show UI warning if cannot create
    Add block gap support for group blocks
    Try cascading nav styles through classnames
    Fix: Impossible to edit theme and default colors
    Fix: Color editor discards colors with default name
    Site Editor: Fix template author avatar check
    Template Editing Mode: Fix options dropdown
    Avoid undo issues when reset parent blocks for controlled blocks
    Add comment-form and comment-list to html5 theme support and fix comment layout
    
    Props hellofromtonya.
    See #54487.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@52402 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/theme/wpThemeJson.php
+++ tests/phpunit/tests/theme/wpThemeJson.php
@@ -647,12 +647,12 @@ class Tests_Theme_wpThemeJson extends WP_UnitTestCase {
 	/**
 	 * @ticket 54336
 	 */
-	public function test_get_stylesheet_generates_proper_classes_from_slugs() {
+	public function test_get_stylesheet_generates_proper_classes_and_css_vars_from_slugs() {
 		$theme_json = new WP_Theme_JSON(
 			array(
 				'version'  => WP_Theme_JSON::LATEST_SCHEMA,
 				'settings' => array(
-					'color' => array(
+					'color'  => array(
 						'palette' => array(
 							array(
 								'slug'  => 'grey',
@@ -672,6 +672,9 @@ class Tests_Theme_wpThemeJson extends WP_UnitTestCase {
 							),
 						),
 					),
+					'custom' => array(
+						'white2black' => 'value',
+					),
 				),
 			)
 		);
@@ -681,7 +684,7 @@ class Tests_Theme_wpThemeJson extends WP_UnitTestCase {
 			$theme_json->get_stylesheet( array( 'presets' ) )
 		);
 		$this->assertEquals(
-			'body{--wp--preset--color--grey: grey;--wp--preset--color--dark-grey: grey;--wp--preset--color--light-grey: grey;--wp--preset--color--white-2-black: grey;}',
+			'body{--wp--preset--color--grey: grey;--wp--preset--color--dark-grey: grey;--wp--preset--color--light-grey: grey;--wp--preset--color--white-2-black: grey;--wp--custom--white-2-black: value;}',
 			$theme_json->get_stylesheet( array( 'variables' ) )
 		);
 
