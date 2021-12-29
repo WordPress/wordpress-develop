40d2062168f3be8685fe035725087a86aa7aae2f
Marlo Kessler <marlo.kessler@connapptivity.de>
2021-12-29 11:47:09 +0100

    Indent with tabs in Tests_User_RetrievePassword

--- tests/phpunit/tests/user/retrievePassword.php
+++ tests/phpunit/tests/user/retrievePassword.php
@@ -6,20 +6,20 @@
  */
 class Tests_User_RetrievePassword extends WP_UnitTestCase {
 
-    public function set_up() {
-        parent::set_up();
+	public function set_up() {
+		parent::set_up();
 
-        // Create the user.
-        $this->user = self::factory()->user->create_and_get(
+		// Create the user.
+		$this->user = self::factory()->user->create_and_get(
 			array(
 				'user_login' => 'jane',
-                'user_email' => 'r.jane@example.com',
+				'user_email' => 'r.jane@example.com',
 			)
 		);
-    }
+	}
 
-    public function retrieve_password_reset_notification_email() {
-        $message = 'Sending password reset notification email failed.';
-        $this->assertNotWPError( retrieve_password( $this->user->user_login ), $message );
-    }
+	public function retrieve_password_reset_notification_email() {
+		$message = 'Sending password reset notification email failed.';
+		$this->assertNotWPError( retrieve_password( $this->user->user_login ), $message );
+	}
 }
