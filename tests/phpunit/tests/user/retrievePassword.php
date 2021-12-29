54d577d5e75de559f031c320d21d167bec83f091
Marlo Kessler <marlo.kessler@connapptivity.de>
2021-12-29 11:36:29 +0100

    Unit test for retrieve_password

---
+++ tests/phpunit/tests/user/retrievePassword.php
@@ -0,0 +1,21 @@
+<?php
+
+class Tests_User_RetrievePassword extends WP_UnitTestCase {
+
+    public function set_up() {
+        parent::set_up();
+
+        // Create the user.
+        $this->user = self::factory()->user->create_and_get(
+			array(
+				'user_login' => 'jane',
+                'user_email' => 'r.jane@example.com',
+			)
+		);
+    }
+
+    public function retrieve_password_reset_notification_email() {
+        $message = 'Sending password reset notification email failed.';
+        $this->assertNotWPError( retrieve_password( $this->user->user_login ), $message );
+    }
+}
