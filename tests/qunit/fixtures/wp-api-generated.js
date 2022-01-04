bd08b221273b8b367a18fef8c9efb873819ac89d
Robert Anderson <noisysocks@git.wordpress.org>
2022-01-04 05:37:25 +0000

    Update @wordpress packages
    Update packages to include these bug fixes from Gutenberg:
    
    - Site Logo: Add option to set site icon from Site Logo block
    - Navigation: Enable even more compact setup state.
    - Remove template parts from post content inserter an __unstable filter
    - Template Editor Mode: Hide editor mode switcher
    - Avoid using CSS variables for block gap styles
    - Try to fix auto resizing in template part focus mode
    - Lower the specificity of font size CSS Custom Properties in the editor
    - Site icon: Fix site icon styling to display non-square site icons within a square button
    - [Site Editor]: Register block editor shortcuts
    - Update regex to handle 404 template slug
    - Site Editor: Remove dead code
    - [Block Editor]: Restrict delete multi selected blocks shortcut
    - Fix: Gradients are not being applied by class
    - Update: Make the global styles subtitles font smaller
    - Post Content/Title: Reflect changes when previewing post
    - ServerSideRender: Fix loading state
    - [Block Library]: Fix editable post blocks in Query Loop with zero queryId
    - Post Excerpt: Fix previews
    - WP59: Contextualize "Export" string to differentiate it from other occurrences in WP Core
    - Tools Panel: Fix race conditions caused by conditionally displayed ToolsPanelItems
    - ToolsPanel: Allow items to register when panelId is null
    - Font Size Picker: Allow non-integers as simple CSS values and in hints
    - [Components - FontSizePicker]: Use incremental sequence of numbers as labels for the available font-sizes at the segmented control (conditionally)
    
    See #54487.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@52434 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/qunit/fixtures/wp-api-generated.js
+++ tests/qunit/fixtures/wp-api-generated.js
@@ -5035,7 +5035,7 @@ mockedApiResponse.Schema = {
                             "description": "Unique slug identifying the template.",
                             "type": "string",
                             "minLength": 1,
-                            "pattern": "[a-zA-Z_\\-]+",
+                            "pattern": "[a-zA-Z0-9_\\-]+",
                             "required": true
                         },
                         "theme": {
@@ -5189,7 +5189,7 @@ mockedApiResponse.Schema = {
                             "description": "Unique slug identifying the template.",
                             "type": "string",
                             "minLength": 1,
-                            "pattern": "[a-zA-Z_\\-]+",
+                            "pattern": "[a-zA-Z0-9_\\-]+",
                             "required": false
                         },
                         "theme": {
@@ -5503,7 +5503,7 @@ mockedApiResponse.Schema = {
                             "description": "Unique slug identifying the template.",
                             "type": "string",
                             "minLength": 1,
-                            "pattern": "[a-zA-Z_\\-]+",
+                            "pattern": "[a-zA-Z0-9_\\-]+",
                             "required": false
                         },
                         "theme": {
@@ -5682,7 +5682,7 @@ mockedApiResponse.Schema = {
                             "description": "Unique slug identifying the template.",
                             "type": "string",
                             "minLength": 1,
-                            "pattern": "[a-zA-Z_\\-]+",
+                            "pattern": "[a-zA-Z0-9_\\-]+",
                             "required": true
                         },
                         "theme": {
@@ -5841,7 +5841,7 @@ mockedApiResponse.Schema = {
                             "description": "Unique slug identifying the template.",
                             "type": "string",
                             "minLength": 1,
-                            "pattern": "[a-zA-Z_\\-]+",
+                            "pattern": "[a-zA-Z0-9_\\-]+",
                             "required": false
                         },
                         "theme": {
@@ -6160,7 +6160,7 @@ mockedApiResponse.Schema = {
                             "description": "Unique slug identifying the template.",
                             "type": "string",
                             "minLength": 1,
-                            "pattern": "[a-zA-Z_\\-]+",
+                            "pattern": "[a-zA-Z0-9_\\-]+",
                             "required": false
                         },
                         "theme": {
@@ -9630,6 +9630,11 @@ mockedApiResponse.Schema = {
                             "description": "Site logo.",
                             "type": "integer",
                             "required": false
+                        },
+                        "site_icon": {
+                            "description": "Site icon.",
+                            "type": "integer",
+                            "required": false
                         }
                     }
                 }
@@ -12212,5 +12217,6 @@ mockedApiResponse.settings = {
     "posts_per_page": 10,
     "default_ping_status": "open",
     "default_comment_status": "open",
-    "site_logo": null
+    "site_logo": null,
+    "site_icon": 0
 };
