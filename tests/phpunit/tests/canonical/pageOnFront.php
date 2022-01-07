2fa4fcb4e5723d2c8e692735df6006e42f6bc36b
Dion Hulse <contact@dd32.id.au>
2022-01-07 14:33:05 +1000

    Add a test case that /embed/ should work with a page_on_front

--- tests/phpunit/tests/canonical/pageOnFront.php
+++ tests/phpunit/tests/canonical/pageOnFront.php
@@ -66,6 +66,8 @@ class Tests_Canonical_PageOnFront extends WP_Canonical_UnitTestCase {
 			array( '/blog-page/?page=2', '/blog-page/', 45337 ),
 			// The posts page supports regular pagination.
 			array( '/blog-page/?paged=2', '/blog-page/page/2/', 20385 ),
+			// The front page should be embeddable.
+			array( '/embed/', array( 'embed' => 'true' ) )
 		);
 	}
 }
