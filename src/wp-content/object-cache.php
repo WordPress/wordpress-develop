02ada5d559f6b3687f14e193b993d94c7b600a10
pbearne <pbearne@gmail.com>
2022-01-14 11:07:51 -0500

    work so far

--- src/wp-content/object-cache.php
+++ src/wp-content/object-cache.php
@@ -17,16 +17,16 @@ new xwp\conditional_options\conditional_options_cache();
 
 if ( ! function_exists( 'conditional_options_preload' ) ) {
 
-	add_filter( 'conditional_options_preload', 'conditional_options_preload' );
+	add_filter( 'pre_get_alloptions', 'conditional_options_preload' );
 
-	function conditional_options_preload( $force_cache = false ) {
-		return xwp\conditional_options\conditional_options_cache::conditional_options_preload( $force_cache );
+	function conditional_options_preload() {
+		return xwp\conditional_options\conditional_options_cache::conditional_options_preload();
 	}
 }
 //
 if ( ! function_exists( 'conditional_get_options' ) ) {
 
-	add_filter( 'conditional_get_options', 'conditional_get_options', 10, 3 );
+	add_filter( 'pre_option_all', 'conditional_get_options', 10, 3 );
 
 	function conditional_get_options( $pre, $option, $default ) {
 		return xwp\conditional_options\conditional_options_cache::get_option( $pre, $option, $default );
