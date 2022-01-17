f5e9419fd60ece7818820e7b1aecea1658964492
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-01-17 13:47:11 +0000

    Date/Time: Fix race conditions in `current_time()` tests.
    * Restore default timezone before performing assertions to avoid affecting other tests in case of failure.
    * Use delta comparison for timestamps to avoid race conditions.
    
    Props SergeyBiryukov, desrosj.
    Merges [45857] to the 5.2 branch.
    Fixes #45821.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.2@52586 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/date/currentTime.php
+++ tests/phpunit/tests/date/currentTime.php
@@ -14,11 +14,18 @@ class Tests_Date_CurrentTime extends WP_UnitTestCase {
 		$datetime = new DateTime( 'now', new DateTimeZone( $timezone_string ) );
 
 		date_default_timezone_set( $timezone_string );
-		$this->assertEquals( gmdate( $format ), current_time( $format, true ) );
-		$this->assertEquals( $datetime->format( $format ), current_time( $format ) );
+
+		$current_time_custom_timezone_gmt = current_time( $format, true );
+		$current_time_custom_timezone     = current_time( $format );
 
 		date_default_timezone_set( 'UTC' );
-		$this->assertEquals( gmdate( $format ), current_time( $format, true ) );
-		$this->assertEquals( $datetime->format( $format ), current_time( $format ) );
+
+		$current_time_gmt = current_time( $format, true );
+		$current_time     = current_time( $format );
+
+		$this->assertEquals( strtotime( gmdate( $format ) ), strtotime( $current_time_custom_timezone_gmt ), 'The dates should be equal', 2 );
+		$this->assertEquals( strtotime( $datetime->format( $format ) ), strtotime( $current_time_custom_timezone ), 'The dates should be equal', 2 );
+		$this->assertEquals( strtotime( gmdate( $format ) ), strtotime( $current_time_gmt ), 'The dates should be equal', 2 );
+		$this->assertEquals( strtotime( $datetime->format( $format ) ), strtotime( $current_time ), 'The dates should be equal', 2 );
 	}
 }
