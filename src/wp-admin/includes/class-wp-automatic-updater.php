21a0590bb26050e79f4528ffba435ffada6b62bb
Erik <11491369+kebbet@users.noreply.github.com>
2022-01-20 17:45:26 +0100

    Use `get_option` in `send_debug_email`.

--- src/wp-admin/includes/class-wp-automatic-updater.php
+++ src/wp-admin/includes/class-wp-automatic-updater.php
@@ -1353,8 +1353,8 @@ class WP_Automatic_Updater {
 			$body[] = '';
 		}
 
-		if ( '' !== get_bloginfo( 'name' ) ) {
-			$site_title = wp_specialchars_decode( get_bloginfo( 'name' ), ENT_QUOTES );
+		if ( '' !== get_option( 'blogname' ) ) {
+			$site_title = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
 		} else {
 			$site_title = parse_url( home_url(), PHP_URL_HOST );
 		}
