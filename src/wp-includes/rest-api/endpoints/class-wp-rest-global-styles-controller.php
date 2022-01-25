6ab75e1b15ff4f1f5c0d7a1ad355513615a6da15
ntsekouras <ntsekouras@outlook.com>
2022-01-25 13:17:20 +0200

    Fix PHP warning in `WP_REST_Global_Styles_Controller` if no `styles` exist in theme.json

--- src/wp-includes/rest-api/endpoints/class-wp-rest-global-styles-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-global-styles-controller.php
@@ -563,7 +563,8 @@ class WP_REST_Global_Styles_Controller extends WP_REST_Controller {
 		}
 
 		if ( rest_is_field_included( 'styles', $fields ) ) {
-			$data['styles'] = $theme->get_raw_data()['styles'];
+			$raw_data       = $theme->get_raw_data();
+			$data['styles'] = isset( $raw_data['styles'] ) ? $raw_data['styles'] : array();
 		}
 
 		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
