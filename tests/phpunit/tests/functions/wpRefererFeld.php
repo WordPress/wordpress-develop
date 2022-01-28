de8db74ea7d797cc69d5b3ec1b763215d3034abd
pbearne <pbearne@gmail.com>
2022-01-28 18:58:15 -0500

    Fixes #54106 and adds tests

---
+++ tests/phpunit/tests/functions/wpRefererFeld.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @group functions.php
+ * @covers ::wp_referer_field
+ */
+class Tests_Functions_wpRefererField extends WP_UnitTestCase {
+
+	/**
+	 * @ticket 54106
+	 */
+	public function test_wp_referer_field() {
+		ob_start();
+		wp_referer_field();
+		$content = ob_get_clean();
+
+		$this->assertSame( '<input type="hidden" name="_wp_http_referer" value="" />', $content );
+	}
+	/**
+	 * @ticket 54106
+	 */
+	public function test_wp_referer_field_no_echo() {
+		$actual = wp_referer_field( false );
+
+		$this->assertSame( '<input type="hidden" name="_wp_http_referer" value="" />', $actual );
+	}
+	/**
+	 * @ticket 54106
+	 */
+	public function test_wp_referer_field_with_referer() {
+		$old_request_uri        = $_SERVER['REQUEST_URI'];
+		$_SERVER['REQUEST_URI'] = 'edit.php?_wp_http_referer=edit.php';
+
+		$actual = wp_referer_field( false );
+
+		$_SERVER['REQUEST_URI'] = $old_request_uri;
+
+		$this->assertSame( '<input type="hidden" name="_wp_http_referer" value="edit.php" />', $actual );
+	}
+}
+
