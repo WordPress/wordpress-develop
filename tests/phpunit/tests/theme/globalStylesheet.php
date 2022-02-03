8647bf4901f5472246482aa50e19808c73456ae6
André <583546+oandregal@users.noreply.github.com>
2022-02-03 17:31:13 +0100

    Set up theme dir

--- tests/phpunit/tests/theme/globalStylesheet.php
+++ tests/phpunit/tests/theme/globalStylesheet.php
@@ -5,6 +5,43 @@
  */
 class Tests_Global_Stylesheet extends WP_UnitTestCase {
 
+	public function set_up() {
+		parent::set_up();
+
+		$this->orig_theme_dir = $GLOBALS['wp_theme_directories'];
+		$this->theme_root     = realpath( DIR_TESTDATA . '/themedir1' );
+
+		// /themes is necessary as theme.php functions assume /themes is the root if there is only one root.
+		$GLOBALS['wp_theme_directories'] = array( WP_CONTENT_DIR . '/themes', $this->theme_root );
+
+		// Set up the new root.
+		add_filter( 'theme_root', array( $this, 'filter_set_theme_root' ) );
+		add_filter( 'stylesheet_root', array( $this, 'filter_set_theme_root' ) );
+		add_filter( 'template_root', array( $this, 'filter_set_theme_root' ) );
+
+		// Clear caches.
+		wp_clean_themes_cache();
+		unset( $GLOBALS['wp_themes'] );
+	}
+
+	public function tear_down() {
+		$GLOBALS['wp_theme_directories'] = $this->orig_theme_dir;
+
+		// Clear up the filters to modify the theme root.
+		remove_filter( 'theme_root', array( $this, 'filter_set_theme_root' ) );
+		remove_filter( 'stylesheet_root', array( $this, 'filter_set_theme_root' ) );
+		remove_filter( 'template_root', array( $this, 'filter_set_theme_root' ) );
+
+		wp_clean_themes_cache();
+		unset( $GLOBALS['wp_themes'] );
+
+		parent::tear_down();
+	}
+
+	public function filter_set_theme_root() {
+		return $this->theme_root;
+	}
+
 	public function test_block_theme_using_variables() {
 		switch_theme( 'block-theme' );
 
@@ -104,6 +141,9 @@ class Tests_Global_Stylesheet extends WP_UnitTestCase {
 		switch_theme( WP_DEFAULT_THEME );
 	}
 
+	/**
+	 * @group my-unit-test
+	 */
 	public function test_variables_in_classic_theme_with_presets_using_defaults() {
 		switch_theme( 'classic-with-presets' );
 
