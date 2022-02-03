8b763d78b263ec84d2be4ef0e24643a4a19ac379
André <583546+oandregal@users.noreply.github.com>
2022-02-03 11:46:20 +0100

    Fix lint issues for globalStylesheet.php

--- tests/phpunit/tests/theme/globalStylesheet.php
+++ tests/phpunit/tests/theme/globalStylesheet.php
@@ -5,75 +5,75 @@
  */
 class Tests_Global_Stylesheet extends WP_UnitTestCase {
 
-    public function test_block_theme() {
-        switch_theme( 'block-theme' );
+	public function test_block_theme() {
+		switch_theme( 'block-theme' );
 
-        $styles = wp_get_global_stylesheet( array( 'variables' ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--small: 13px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--large: 36px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--custom: 100px;" ) );
+		$styles = wp_get_global_stylesheet( array( 'variables' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--small: 13px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--medium: 20px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--large: 36px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--x-large: 42px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--custom: 100px;' ) );
 
-        $styles = wp_get_global_stylesheet( array( 'presets' ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--small: 13px" ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--large: 36px" ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--custom: 100px;" ) );
+		$styles = wp_get_global_stylesheet( array( 'presets' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--small: 13px' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--medium: 20px' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--large: 36px' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--x-large: 42px' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--custom: 100px;' ) );
 
-        $styles = wp_get_global_stylesheet();
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--small: 13px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--large: 36px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--custom: 100px;" ) );
-    }
+		$styles = wp_get_global_stylesheet();
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--small: 13px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--medium: 20px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--large: 36px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--x-large: 42px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--custom: 100px;' ) );
+	}
 
-    public function test_variables_in_classic_theme_with_no_presets() {
-        switch_theme( 'default' );
+	public function test_variables_in_classic_theme_with_no_presets() {
+		switch_theme( 'default' );
 
-        $styles = wp_get_global_stylesheet( array( 'variables' ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--small: 13px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--large: 36px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
+		$styles = wp_get_global_stylesheet( array( 'variables' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--small: 13px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--medium: 20px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--large: 36px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--x-large: 42px' ) );
 
-        $styles = wp_get_global_stylesheet( array( 'presets' ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--small: 13px" ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--large: 36px" ) );
-        $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
+		$styles = wp_get_global_stylesheet( array( 'presets' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--small: 13px' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--medium: 20px' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--large: 36px' ) );
+		$this->assertFalse( str_contains( $styles, '--wp--preset--font-size--x-large: 42px' ) );
 
-        $styles = wp_get_global_stylesheet();
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--small: 13px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--large: 36px" ) );
-        $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
-    }
+		$styles = wp_get_global_stylesheet();
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--small: 13px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--medium: 20px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--large: 36px' ) );
+		$this->assertTrue( str_contains( $styles, '--wp--preset--font-size--x-large: 42px' ) );
+	}
 
-    public function test_variables_in_classic_theme_with_presets() {
-        switch_theme( 'classic-with-presets' );
+	public function test_variables_in_classic_theme_with_presets() {
+		switch_theme( 'classic-with-presets' );
 
-        $this->assertTrue( current_theme_supports( 'editor-font-sizes') );
+		$this->assertTrue( current_theme_supports( 'editor-font-sizes' ) );
 
-        // $styles = wp_get_global_stylesheet( array( 'variables' ) );
-        // $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--small: 18px" ) );
-        // $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        // $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--large: 26.25px" ) );
-        // $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
+		// $styles = wp_get_global_stylesheet( array( 'variables' ) );
+		// $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--small: 18px" ) );
+		// $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
+		// $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--large: 26.25px" ) );
+		// $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
 
-        // $styles = wp_get_global_stylesheet( array( 'presets' ) );
-        // $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--small: 18px" ) );
-        // $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        // $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--large: 26.25px" ) );
-        // $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
+		// $styles = wp_get_global_stylesheet( array( 'presets' ) );
+		// $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--small: 18px" ) );
+		// $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
+		// $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--large: 26.25px" ) );
+		// $this->assertFalse( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
 
-        // $styles = wp_get_global_stylesheet();
-        // $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--small: 18px" ) );
-        // $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
-        // $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--large: 26.25px" ) );
-        // $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
-    }
+		// $styles = wp_get_global_stylesheet();
+		// $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--small: 18px" ) );
+		// $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--medium: 20px" ) );
+		// $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--large: 26.25px" ) );
+		// $this->assertTrue( str_contains( $styles, "--wp--preset--font-size--x-large: 42px" ) );
+	}
 
 }
