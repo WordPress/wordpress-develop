323c51de3c006c614716d1ffbaa8a60eb740e7b1
konradyoast <konrad@yoast.com>
2022-02-04 14:11:53 +0100

    #54700 - Check if there is URL match before taking it.

--- src/wp-includes/vars.php
+++ src/wp-includes/vars.php
@@ -29,7 +29,7 @@ if ( is_admin() ) {
 	} else {
 		preg_match( '#/wp-admin/?(.*?)$#i', $_SERVER['PHP_SELF'], $self_matches );
 	}
-	$pagenow = $self_matches[1];
+	$pagenow = !empty($self_matches[1]) ? $self_matches[1] : '';
 	$pagenow = trim( $pagenow, '/' );
 	$pagenow = preg_replace( '#\?.*?$#', '', $pagenow );
 	if ( '' === $pagenow || 'index' === $pagenow || 'index.php' === $pagenow ) {
