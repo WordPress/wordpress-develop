dc4e8af2c4fa03d47d5c5d9dc41f1ce299a848ea
Rafi Ahmed <rafi201822@gmail.com>
2022-02-08 03:31:14 +0600

    Validation added for block templates support
    Trac ticket: https://core.trac.wordpress.org/ticket/54917

--- src/wp-includes/block-template.php
+++ src/wp-includes/block-template.php
@@ -46,6 +46,11 @@ function _add_template_loader_filters() {
  * @return string The path to the Full Site Editing template canvas file, or the fallback PHP template.
  */
 function locate_block_template( $template, $type, array $templates ) {
+	// If currently active theme doesn't support block-templates then just return $template
+	if ( ! current_theme_supports( 'block-templates' ) ) {
+		return $template;
+	}
+
 	global $_wp_current_template_content;
 
 	if ( $template ) {
