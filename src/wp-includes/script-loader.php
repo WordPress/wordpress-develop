9f0f594c7ffb00817be97d0aff8516594474e83d
Anton Vlasenko <vlasenko.anton@gmail.com>
2022-02-10 18:02:59 +0100

    Fix code style.

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -1154,7 +1154,7 @@ function wp_default_scripts( $scripts ) {
 	$scripts->add( 'customize-models', '/wp-includes/js/customize-models.js', array( 'underscore', 'backbone' ), false, 1 );
 	$scripts->add( 'customize-views', '/wp-includes/js/customize-views.js', array( 'jquery', 'underscore', 'imgareaselect', 'customize-models', 'media-editor', 'media-views' ), false, 1 );
 	$scripts->add( 'customize-controls', "/wp-admin/js/customize-controls$suffix.js", array( 'customize-base', 'wp-a11y', 'wp-util', 'jquery-ui-core' ), false, 1 );
-	$switch_to_site_editor_label = __('Switch to Site Editor');
+	$switch_to_site_editor_label = __( 'Switch to Site Editor' );
 	did_action( 'init' ) && $scripts->localize(
 		'customize-controls',
 		'_wpCustomizeControlsL10n',
@@ -1224,11 +1224,11 @@ function wp_default_scripts( $scripts ) {
 			'invalidValue'            => __( 'Invalid value.' ),
 			'blockThemeNotification'  => sprintf(
 				/* translators: 1. %s: URL to the Site Editor admin screen, 2: "Switch to Site Editor" button placeholder. */
-				__( 'To get the best customization experience from your block theme, use the Site Editor. <a href="%s" target="_blank">Tell me more</a>. %s' ),
+				__( 'To get the best customization experience from your block theme, use the Site Editor. <a href="%1$s" target="_blank">Tell me more</a>. %2$s' ),
 				'https://wordpress.org/support/article/site-editor/',
 				sprintf(
-					/* translators: 1. %s: URL to the Site Editor admin screen, 2. %s: Label for the button that takes a user to the Site Editor admin screen, 3: Same as the previous item. */
-					'<button type="button" data-action="%s" class="button switch-to-editor" aria-label="%s">%s</button>',
+					/* translators: 1. %s: URL to the Site Editor admin screen, 2. %s: Label for the button that takes a user to the Site Editor admin screen, 3: Same as the previous placeholder. */
+					'<button type="button" data-action="%1$s" class="button switch-to-editor" aria-label="%2$s">%3$s</button>',
 					esc_url( admin_url( 'site-editor.php' ) ),
 					$switch_to_site_editor_label,
 					$switch_to_site_editor_label
