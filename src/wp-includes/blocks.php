963d885236fb83da52d183f1f9c2bab543a8d14f
neychok <n.kalaidjiev@gmail.com>
2022-02-11 19:39:16 +0200

    Added asset file path in  `_doing_it_wrong()` notice in `register_block_script_handle()`

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -89,12 +89,13 @@ function register_block_script_handle( $metadata, $field_name ) {
 			substr_replace( $script_path, '.asset.php', - strlen( '.js' ) )
 		)
 	);
-	if ( ! file_exists( $script_asset_path ) ) {
+	if ( false !== $script_asset_path ) {
 		_doing_it_wrong(
 			__FUNCTION__,
 			sprintf(
 				/* translators: 1: Field name, 2: Block name. */
-				__( 'The asset file for the "%1$s" defined in "%2$s" block definition is missing.' ),
+				__( 'The asset file ("%1$s") for the "%2$s" defined in "%3$s" block definition is missing.' ),
+				$script_asset_path,
 				$field_name,
 				$metadata['name']
 			),
