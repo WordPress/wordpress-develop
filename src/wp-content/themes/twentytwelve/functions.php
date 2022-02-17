36962d0488a46c2a5dfe3eb98553fbe296dbcacd
Felix Arntz <felixarntz@google.com>
2022-02-17 15:37:27 -0500

    Update script version.

--- src/wp-content/themes/twentytwelve/functions.php
+++ src/wp-content/themes/twentytwelve/functions.php
@@ -199,7 +199,7 @@ function twentytwelve_scripts_styles() {
 	}
 
 	// Adds JavaScript for handling the navigation menu hide-and-show behavior.
-	wp_enqueue_script( 'twentytwelve-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20210916', true );
+	wp_enqueue_script( 'twentytwelve-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20220217', true );
 
 	$font_url = twentytwelve_get_font_url();
 	if ( ! empty( $font_url ) ) {
