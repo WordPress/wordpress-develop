29d1f74184299be5e654a33c8df92e62d53cff06
George Mamadashvili <georgemamadashvili@gmail.com>
2022-02-17 12:51:52 +0400

    Move deprecated function

--- src/wp-includes/block-supports/duotone.php
+++ src/wp-includes/block-supports/duotone.php
@@ -493,23 +493,6 @@ function wp_register_duotone_support( $block_type ) {
 }
 
 /**
- * Renders the duotone filter SVG and returns the CSS filter property to
- * reference the rendered SVG.
- *
- * @since 5.9.0
- * @deprecated 5.9.1 Use `wp_get_duotone_filter_property` introduced in 5.9.1.
- *
- * @see wp_get_duotone_filter_property()
- *
- * @param array $preset Duotone preset value as seen in theme.json.
- * @return string Duotone CSS filter property.
- */
-function wp_render_duotone_filter_preset( $preset ) {
-	_deprecated_function( __FUNCTION__, '5.9.1', 'wp_get_duotone_filter_property()' );
-	return wp_get_duotone_filter_property( $preset );
-}
-
-/**
  * Render out the duotone stylesheet and SVG.
  *
  * @since 5.8.0
