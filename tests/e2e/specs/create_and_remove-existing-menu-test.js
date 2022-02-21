62302d3e5b33f3b8322991f3cd03ab5abbabaf2e
Pavan  Patil <44057535+pavanpatil1@users.noreply.github.com>
2022-02-21 15:43:56 +0530

    Update create_and_remove-existing-menu-test.js

--- tests/e2e/specs/create_and_remove-existing-menu-test.js
+++ tests/e2e/specs/create_and_remove-existing-menu-test.js
@@ -1,15 +1,16 @@
-import { loginUser, visitAdminPage } from '@wordpress/e2e-test-utils';
+import { loginUser, visitAdminPage,activateTheme } from '@wordpress/e2e-test-utils';
 
 describe( 'Should create a new menu and remove existing menu', () => {
 
     beforeEach( async () => {
         await loginUser();
+	await activateTheme("twentytwentyone");
     } );
 
 
 	it( 'Add a new menu', async () => {
 		
-		await visitAdminPage("nav-menus.php");
+        await visitAdminPage("nav-menus.php");
         
         //check if it's a first menu
         await page.waitForSelector(".first-menu-message")
@@ -28,12 +29,10 @@ describe( 'Should create a new menu and remove existing menu', () => {
 
     it( 'Remove existing menu', async () => {
 		
-		    await visitAdminPage("nav-menus.php");
-
-       // await page.waitForSelector(".add-edit-menu-action");
+	await visitAdminPage("nav-menus.php");
 
         const createmenu = await page.$x("//a[normalize-space()='create a new menu']");
-		    await createmenu[0].click();
+        await createmenu[0].click();
 
         await page.waitForSelector("#menu-name", {timeout: 60000})
 
@@ -42,7 +41,7 @@ describe( 'Should create a new menu and remove existing menu', () => {
         await page.click("#save_menu_footer");
         
         await page.waitForSelector("#nav-menu-footer", {timeout: 60000})
-       // await page.click(".submitdelete.deletion.menu-delete");
+     
         await page.waitForSelector(".delete-action",{timeout: 60000});
         const deletemenu = await page.$x("//a[normalize-space()='Delete Menu']");
 		    await deletemenu[0].click();
