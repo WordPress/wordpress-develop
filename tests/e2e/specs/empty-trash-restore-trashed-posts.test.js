07ccba004a7973e98cdf236e2a2c41580786f34a
Pooja Killekar <41000648+pooja-muchandikar@users.noreply.github.com>
2022-03-01 16:03:11 +0530

    Resolved code review feedbacks

---
+++ tests/e2e/specs/empty-trash-restore-trashed-posts.test.js
@@ -0,0 +1,72 @@
+import {
+    visitAdminPage,
+    createNewPost,
+    trashAllPosts,
+    publishPost
+} from '@wordpress/e2e-test-utils';
+
+describe( 'Empty Trash', () => {
+
+
+    async function createPost() {
+
+        // Create a Post
+        const title = 'Test Title';
+        await createNewPost( { title } );
+        await publishPost();
+
+    }
+
+
+    afterEach( async () => {
+        await trashAllPosts();
+    } );
+
+    it( 'Empty Trash', async () => {
+
+        await createPost();
+       
+        await visitAdminPage( '/edit.php' );  
+
+        // Move post to trash
+        await page.waitForSelector( '#the-list .type-post' );
+        await page.hover('.row-title');
+        await page.click("a[aria-label='Move “Test Title” to the Trash']");
+
+        // Empty trash 
+        const trashbutton = await page.$x("//li[@class='trash']");
+        await trashbutton[0].click();   
+        const deleteAllButton = await page.waitForSelector('input[value="Empty Trash"]');
+        await deleteAllButton.click();
+        await page.waitForSelector("#message");
+        
+    } );
+
+
+    it( 'Restore trash post', async () => {
+
+        await createPost();
+    
+        await visitAdminPage( '/edit.php' );  
+
+        // Move one post to trash
+        await page.waitForSelector( '#the-list .type-post' );
+        await page.hover('.row-title');
+        await page.click("a[aria-label='Move “Test Title” to the Trash']");
+
+        // Remove post from trash 
+        await page.click("a[href='edit.php?post_status=trash&post_type=post']");
+        await page.waitForSelector( '#the-list .type-post' );
+        await page.hover('.page-title');
+        await page.click(".untrash");
+
+        // expect for sucess message for trashed post. 
+        const messageElement = await page.waitForSelector('#message'); 
+
+        expect( 
+            await messageElement.evaluate( ( element ) => element.innerText ) 
+        ).toContain( '1 post restored from the Trash.' )
+
+    } );
+   
+} );
