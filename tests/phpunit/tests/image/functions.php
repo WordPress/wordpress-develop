96a006aeda4e27ca74b8158a812d77940a5952fa
Jonny Harris <jon@spacedmonkey.co.uk>
2022-03-01 13:52:08 +0000

    Remove function.

--- tests/phpunit/tests/image/functions.php
+++ tests/phpunit/tests/image/functions.php
@@ -642,9 +642,9 @@ class Tests_Image_Functions extends WP_UnitTestCase {
 
 		// Different environments produce slightly different filesize results.
 		foreach ( $metadata['sizes'] as &$item ) {
-			$item['filesize'] = round_to_nearest_thousand( $item['filesize'] );
+			$item['filesize'] = round( $item['filesize'], -3 );
 		}
-		$metadata['filesize'] = round_to_nearest_thousand( $metadata['filesize'] );
+		$metadata['filesize'] = round( $metadata['filesize'], -3 );
 
 		$this->assertSame( $expected, $metadata );
 
@@ -699,7 +699,7 @@ class Tests_Image_Functions extends WP_UnitTestCase {
 		$metadata = wp_generate_attachment_metadata( $attachment_id, $test_file );
 
 		// Different environments produce slightly different filesize results.
-		$metadata['sizes']['test-size']['filesize'] = round_to_nearest_thousand( $metadata['sizes']['test-size']['filesize'] );
+		$metadata['sizes']['test-size']['filesize'] = round( $metadata['sizes']['test-size']['filesize'], -3 );
 		$this->assertSame( $metadata['sizes']['test-size'], $expected );
 
 		$this->assertArrayHasKey( 'test-size', $metadata['sizes'], 'The `test-size` was not added to the metadata.' );
