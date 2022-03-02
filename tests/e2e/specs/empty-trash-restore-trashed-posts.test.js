d61c8483c12904070b27c0e9ec84c1ca2aa2514e
Pooja Killekar <41000648+pooja-muchandikar@users.noreply.github.com>
2022-03-02 13:16:00 +0530

    Resolved Feedbacks

--- tests/e2e/specs/empty-trash-restore-trashed-posts.test.js
+++ tests/e2e/specs/empty-trash-restore-trashed-posts.test.js
@@ -5,18 +5,17 @@ import {
     publishPost
 } from '@wordpress/e2e-test-utils';
 
+
+const posttitle = 'Test Title';
+
 describe( 'Empty Trash', () => {
 
-
-    async function createPost() {
-
-        // Create a Post
-        const title = 'Test Title';
-        await createNewPost( { title } );
+    async function createPost(title) {
+        // Create a Post     
+        await createNewPost( {title} );
         await publishPost();
 
     }
-
 
     afterEach( async () => {
         await trashAllPosts();
@@ -24,17 +23,18 @@ describe( 'Empty Trash', () => {
 
     it( 'Empty Trash', async () => {
 
-        await createPost();
+        await createPost(posttitle);
        
         await visitAdminPage( '/edit.php' );  
 
         // Move post to trash
         await page.waitForSelector( '#the-list .type-post' );
         await page.hover('.row-title');
-        await page.click("a[aria-label='Move “Test Title” to the Trash']");
+        await page.click("a[aria-label='Move “Test Title” to the Trash']")
 
         // Empty trash 
-        await page.click("a[href='edit.php?post_status=trash&post_type=post']");  
+        const trashbutton = await page.$x("//li[@class='trash']");
+        await trashbutton[0].click();   
         const deleteAllButton = await page.waitForSelector('input[value="Empty Trash"]');
         await deleteAllButton.click();
         await page.waitForSelector("#message");
@@ -44,7 +44,7 @@ describe( 'Empty Trash', () => {
 
     it( 'Restore trash post', async () => {
 
-        await createPost();
+        await createPost(posttitle);
     
         await visitAdminPage( '/edit.php' );  
 
@@ -54,10 +54,12 @@ describe( 'Empty Trash', () => {
         await page.click("a[aria-label='Move “Test Title” to the Trash']");
 
         // Remove post from trash 
-        await page.click("a[href='edit.php?post_status=trash&post_type=post']");
+        const trashbutton = await page.$x("//li[@class='trash']");
+        await trashbutton[0].click();   
+        
         await page.waitForSelector( '#the-list .type-post' );
-        await page.hover('.page-title');
-        await page.click(".untrash");
+        await page.hover('.page-title'); 
+        await page.click(`[aria-label="Restore “${posttitle}” from the Trash"]`);
 
         // expect for sucess message for trashed post. 
         const messageElement = await page.waitForSelector('#message'); 
