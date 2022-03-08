7a4207ab4d5eb9371f880f6e5fa81ca2ad94e75d
Pooja Killekar <41000648+pooja-muchandikar@users.noreply.github.com>
2022-03-08 11:26:35 +0530

    Refactored Code

--- tests/e2e/specs/empty-trash-restore-trashed-posts.test.js
+++ tests/e2e/specs/empty-trash-restore-trashed-posts.test.js
@@ -1,63 +1,72 @@
 import {
-    visitAdminPage,
-    createNewPost,
-    trashAllPosts,
-    publishPost,
-  } from "@wordpress/e2e-test-utils";
-  
-  const posttitle = "Test Title";
-  
-  describe("Empty Trash", () => {
-    async function createPost(title) {
-      // Create a Post
-      await createNewPost({ title });
-      await publishPost();
-    }
-  
-    afterEach(async () => {
-      await trashAllPosts();
-    });
-  
-    it("Empty Trash", async () => {
-      await createPost(posttitle);
-  
-      await visitAdminPage("/edit.php");
-  
-      // Move post to trash
-      await page.waitForSelector("#the-list .type-post");
-      await page.hover(".row-title");
-      await page.click("a[aria-label='Move “Test Title” to the Trash']");
-  
-      // Empty trash
-      await page.waitForSelector(".subsubsub"); // Wait for current page Menu
-      await page.click(".trash"); // Click on trash post
-      const deleteAllButton = await page.waitForSelector('input[value="Empty Trash"]');
-      await deleteAllButton.click();
-      await page.waitForSelector("#message"); // TODO
-    });
-  
-    it("Restore trash post", async () => {
-      await createPost(posttitle);
-  
-      await visitAdminPage("/edit.php");
-  
-      // Move one post to trash
-      await page.waitForSelector("#the-list .type-post");
-      await page.hover(".row-title");
-      await page.click("a[aria-label='Move “Test Title” to the Trash']");
-  
-      // Remove post from trash
-      await page.waitForSelector(".subsubsub"); // Wait for current page Menu
-      await page.click(".trash"); // Click on trash post
-      await page.waitForSelector("#the-list .type-post");
-      await page.hover(".page-title");
-      await page.click(`[aria-label="Restore “${posttitle}” from the Trash"]`);
-  
-      // Expect for sucess message for trashed post.
-      const messageElement = await page.waitForSelector("#message");
-  
-      expect(
-        await messageElement.evaluate((element) => element.innerText)
-      ).toContain("1 post restored from the Trash.");
-    });
+  visitAdminPage,
+  createNewPost,
+  trashAllPosts,
+  publishPost,
+} from "@wordpress/e2e-test-utils";
+
+const POST_TITLE = "Test Title";
+
+describe("Empty Trash", () => {
+  async function createPost(title) {
+    // Create a Post
+    await createNewPost({ title });
+    await publishPost();
+  }
+
+  afterEach(async () => {
+    await trashAllPosts();
   });
+
+  it("Empty Trash", async () => {
+    await createPost(POST_TITLE);
+
+    await visitAdminPage("/edit.php");
+
+    // Move post to trash
+    await page.hover(`[aria-label^="“${POST_TITLE}”"]`);
+    await page.click(`[aria-label='Move “${POST_TITLE}” to the Trash']`);
+
+    // Empty trash
+    const trashTab = await page.waitForXPath('//h2[text()="Filter posts list"]/following-sibling::ul//a[contains(text(), "Trash")]');
+    await Promise.all([
+      trashTab.click(),
+      page.waitForNavigation(),
+    ]);
+    const deleteAllButton = await page.waitForSelector('input[value="Empty Trash"]');
+    await Promise.all([
+      deleteAllButton.click(),
+      page.waitForNavigation(),
+    ]);
+
+    const messageElement = await page.waitForSelector("#message");
+    const message = await messageElement.evaluate((node) => node.innerText);
+    // Until we have `deleteAllPosts`, the number of posts being deleted could be dynamic.
+    expect(message).toMatch(/\d+ posts? permanently deleted\./);
+  });
+
+  it("Restore trash post", async () => {
+    await createPost(POST_TITLE);
+
+    await visitAdminPage("/edit.php");
+
+    // Move one post to trash.
+    await page.hover(`[aria-label^="“${POST_TITLE}”"]`);
+    await page.click(`[aria-label='Move “${POST_TITLE}” to the Trash']`);
+
+    // Remove post from trash.
+    const trashTab = await page.waitForXPath('//h2[text()="Filter posts list"]/following-sibling::ul//a[contains(text(), "Trash")]');
+    await Promise.all([
+      trashTab.click(),
+      page.waitForNavigation(),
+    ]);
+    const [postTitle] = await page.$x(`//*[text()="${POST_TITLE}"]`);
+    await postTitle.hover();
+    await page.click(`[aria-label="Restore “${POST_TITLE}” from the Trash"]`);
+
+    // Expect for success message for trashed post.
+    const messageElement = await page.waitForSelector("#message");
+    const message = await messageElement.evaluate((element) => element.innerText);
+    expect(message).toContain("1 post restored from the Trash.");
+  });
+});
