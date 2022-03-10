fcf0c8fa5b12c69757c04913c88ef6a982a16fe0
Paul Bearne <pbearne@gmail.com>
2022-03-10 14:54:08 -0500

    Update tests/phpunit/tests/cache.php
    Co-authored-by: Till Krüss <tillkruss@users.noreply.github.com>

--- tests/phpunit/tests/cache.php
+++ tests/phpunit/tests/cache.php
@@ -458,8 +458,8 @@ class Tests_Cache extends WP_UnitTestCase {
 
 		$results = wp_cache_flush_group( array( 'group-test', 'group-test2' ) );
 
-		$this->assertIsArray( $results[0] );
-		$this->assertCount( 2, $results[0] );
+		$this->assertIsArray( $results );
+		$this->assertCount( 2, $results );
 		$this->assertFalse( wp_cache_get( $key, 'group-test' ), 'test_wp_cache_flush_groups: group-test should return false' );
 		$this->assertFalse( wp_cache_get( $key, 'group-test2' ), 'test_wp_cache_flush_groups: group-test2 should return false' );
 		$this->assertSame( $val, wp_cache_get( $key, 'group-kept' ), 'test_wp_cache_flush_groups: group-kept should still contain my-val' );
