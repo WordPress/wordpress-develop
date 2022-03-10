5945df7fb6c3f7b3490bc056917c2ae5f83d9fec
Jonny Harris <jon@spacedmonkey.co.uk>
2022-03-10 11:23:01 +0000

    Add covers.

--- tests/phpunit/tests/media/filesize.php
+++ tests/phpunit/tests/media/filesize.php
@@ -15,6 +15,7 @@ class Tests_Image_Filesize extends WP_UnitTestCase {
 	 * Check that filesize meta is generated for jpegs.
 	 *
 	 * @ticket 49412
+	 * @covers ::wp_get_attachment_metadata
 	 */
 	function test_filesize_in_jpg_meta() {
 		$attachment = $this->factory->attachment->create_upload_object( DIR_TESTDATA . '/images/33772.jpg' );
@@ -34,6 +35,7 @@ class Tests_Image_Filesize extends WP_UnitTestCase {
 	 * Check that filesize meta is generated for pngs.
 	 *
 	 * @ticket 49412
+	 * @covers ::wp_get_attachment_metadata
 	 */
 	function test_filesize_in_png_meta() {
 		$attachment = $this->factory->attachment->create_upload_object( DIR_TESTDATA . '/images/test-image.png' );
@@ -51,6 +53,7 @@ class Tests_Image_Filesize extends WP_UnitTestCase {
 	 * Check that filesize meta is generated for pdfs.
 	 *
 	 * @ticket 49412
+	 * @covers ::wp_get_attachment_metadata
 	 */
 	function test_filesize_in_pdf_meta() {
 		$attachment = $this->factory->attachment->create_upload_object( DIR_TESTDATA . '/images/wordpress-gsoc-flyer.pdf' );
@@ -75,6 +78,7 @@ class Tests_Image_Filesize extends WP_UnitTestCase {
 	 * Check that filesize meta is generated for psds.
 	 *
 	 * @ticket 49412
+	 * @covers ::wp_get_attachment_metadata
 	 */
 	function test_filesize_in_psd_meta() {
 		// PSD mime type is not allowed by default on multisite.
