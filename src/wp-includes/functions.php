4f064acaa52233a1902cf1ade8c47b9db899d895
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-03-16 10:43:35 -0700

    Update DocBlock with fix description and ticket

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -811,6 +811,7 @@ function xmlrpc_removepostdata( $content ) {
  * Use RegEx to extract URLs from arbitrary content.
  *
  * @since 3.7.0
+ * @since 6.0.0 Fixes support for HTML entities (Trac 30580).
  *
  * @param string $content Content to extract URLs from.
  * @return string[] Array of URLs found in passed string.
