29127987a1ba4c0d87daa64fa5976eba9fc59751
audrasjb <audrasjb@gmail.com>
2022-03-17 15:09:16 +0100

    Adding more occurrences

--- src/wp-admin/includes/class-wp-theme-install-list-table.php
+++ src/wp-admin/includes/class-wp-theme-install-list-table.php
@@ -359,7 +359,7 @@ class WP_Theme_Install_List_Table extends WP_Themes_List_Table {
 
 		?>
 		<a class="screenshot install-theme-preview" href="<?php echo esc_url( $preview_url ); ?>" title="<?php echo esc_attr( $preview_title ); ?>">
-			<img src="<?php echo esc_url( $theme->screenshot_url ); ?>" width="150" alt="" />
+			<img src="<?php echo esc_url( $theme->screenshot_url . '?ver=' . $theme->version ); ?>" width="150" alt="" />
 		</a>
 
 		<h3><?php echo $name; ?></h3>
