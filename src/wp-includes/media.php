96dd3a322d7b8ae68bee19109033f2ac6223596e
Paul Bearne <pbearne@gmail.com>
2022-03-24 08:25:31 -0400

    Update src/wp-includes/media.php
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -1844,7 +1844,7 @@ function wp_filter_content_tags( $content, $context = null ) {
 			}
 
 			/**
-			 * Filters img tag that will be injected into the content.
+			 * Filters an img tag that within the content for a given context.
 			 *
 			 * @since 6.0.0
 			 *
