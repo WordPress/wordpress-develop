eb91389822a119868dedd5286e7f8a06a4e01c05
Andy Fragen <andy@thefragens.com>
2022-03-29 09:06:32 -0700

    more opinionated, default to environment variable if present

--- src/wp-includes/load.php
+++ src/wp-includes/load.php
@@ -250,12 +250,10 @@ function wp_get_environment_type() {
  *
  * @since 6.0.0
  *
- * @return string|false The current runtime environment type, default is ''. false on failure.
+ * @return string The current runtime environment type, default is ''.
  */
 function wp_get_runtime_environment() {
 	static $current_runtime_env = '';
-	static $runtime_constant    = '';
-	static $runtime_getenv      = '';
 
 	if ( ! defined( 'WP_RUN_CORE_TESTS' ) && $current_runtime_env ) {
 		return $current_runtime_env;
@@ -265,7 +263,7 @@ function wp_get_runtime_environment() {
 
 	// Fetch the runtime environment from a constant.
 	if ( defined( 'WP_RUNTIME_ENVIRONMENT' ) ) {
-		$runtime_constant = WP_RUNTIME_ENVIRONMENT;
+		$current_runtime_env = WP_RUNTIME_ENVIRONMENT;
 	}
 
 	// Fetch the runtime environment from global system variable.
@@ -273,34 +271,13 @@ function wp_get_runtime_environment() {
 		// Check if the runtime environment variable has been set, if `getenv` is available on the system.
 		$has_runtime_env = getenv( 'WP_RUNTIME_ENVIRONMENT' );
 		if ( false !== $has_runtime_env ) {
-			$runtime_getenv = $has_runtime_env;
+			$current_runtime_env = $has_runtime_env;
 		}
 	}
 
-	// If WP_RUNTIME_ENVIRONMENT constant and environment variable are both set and are different.
-	if ( ( ! empty( $runtime_constant ) && ! empty( $runtime_getenv ) )
-		&& $runtime_constant !== $runtime_getenv
-	) {
-		_doing_it_wrong(
-			__FUNCTION__,
-			__( 'The WP_RUNTIME_ENVIRONMENT environment variable and constant do not match. Fallbacks will be used, which may negatively impact performance.' ),
-			'6.0.0'
-		);
-
-		return false;
-	}
-
 	// If set to something in $wp_runtime_environments use it, default is ''.
-	// Preference to a set constant.
-	switch ( true ) {
-		case in_array( $runtime_constant, $wp_runtime_environments, true ):
-			$current_runtime_env = $runtime_constant;
-			break;
-		case in_array( $runtime_getenv, $wp_runtime_environments, true ):
-			$current_runtime_env = $runtime_getenv;
-			break;
-		default:
-			$current_runtime_env = '';
+	if ( ! in_array( $current_runtime_env, $wp_runtime_environments, true ) ) {
+		$current_runtime_env = '';
 	}
 
 	return $current_runtime_env;
