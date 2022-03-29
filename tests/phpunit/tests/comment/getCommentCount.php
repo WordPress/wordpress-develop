1ad6cfdc37a87e4a070755d8471910f94e72d665
Uday Kokitkar <61972002+uday-kokitkar@users.noreply.github.com>
2022-03-29 15:45:03 +0530

    add comment status change test

--- tests/phpunit/tests/comment/getCommentCount.php
+++ tests/phpunit/tests/comment/getCommentCount.php
@@ -99,7 +99,12 @@ class Tests_Get_Comment_Count extends WP_UnitTestCase {
 		$this->assertSame( 0, $count['total_comments'] );
 	}
 
-	public function test_get_comment_count_validate_cache() {
+	/**
+	 * @ticket 19901
+	 *
+	 * @covers get_comment_count
+	 */
+	public function test_get_comment_count_validate_cache_comment_deleted() {
 
 		$comment_id = self::factory()->comment->create();
 
@@ -114,7 +119,12 @@ class Tests_Get_Comment_Count extends WP_UnitTestCase {
 		$this->assertSame( 0, $count['total_comments'] );
 	}
 
-	public function test_get_comment_count_validate_cache_post() {
+	/**
+	 * @ticket 19901
+	 *
+	 * @covers get_comment_count
+	 */
+	public function test_get_comment_count_validate_cache_post_post_deleted() {
 
 		$post_id = self::factory()->post->create();
 
@@ -132,6 +142,29 @@ class Tests_Get_Comment_Count extends WP_UnitTestCase {
 
 		$count = get_comment_count( $post_id );
 
+		$this->assertSame( 0, $count['total_comments'] );
+	}
+
+	/**
+	 * @ticket 19901
+	 *
+	 * @covers get_comment_count
+	 */
+	public function test_get_comment_count_validate_cache_comment_status() {
+		$comment_id = self::factory()->comment->create();
+
+		$count = get_comment_count();
+
+		$this->assertSame( 1, $count['approved'] );
+		$this->assertSame( 0, $count['trash'] );
+		$this->assertSame( 1, $count['total_comments'] );
+
+		wp_set_comment_status( $comment_id, 'trash' );
+
+		$count = get_comment_count();
+
+		$this->assertSame( 0, $count['approved'] );
+		$this->assertSame( 1, $count['trash'] );
 		$this->assertSame( 0, $count['total_comments'] );
 	}
 }
