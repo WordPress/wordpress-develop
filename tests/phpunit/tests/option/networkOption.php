01bbbce1f2d34c9e5c1d18ac33a5cbd13d42a550
Jonny Harris <jon@spacedmonkey.co.uk>
2022-03-30 00:54:26 +0100

    Add some more unit tests.

--- tests/phpunit/tests/option/networkOption.php
+++ tests/phpunit/tests/option/networkOption.php
@@ -171,4 +171,56 @@ class Tests_Option_NetworkOption extends WP_UnitTestCase {
 		add_metadata( 'site', $network_id, $option, 'wednesday', true );
 		$this->assertEquals( 'monday', get_network_option( $network_id, $option, true ) );
 	}
+
+	/**
+	 * @ticket 37181
+	 * @group ms-required
+	 */
+	public function test_register_meta_network_option_single_false() {
+		$network_id = self::factory()->network->create();
+		$option     = __FUNCTION__;
+		$value      = __FUNCTION__;
+		register_meta( 'site', $option, array(
+			'type'    => 'string',
+			'default' => $value,
+			'single'  => false,
+		) );
+
+		$this->assertSame( $value, get_network_option( $network_id, $option ) );
+	}
+
+	/**
+	 * @ticket 37181
+	 * @group ms-required
+	 */
+	public function test_register_meta_network_option_single_true() {
+		$network_id = self::factory()->network->create();
+		$option     = __FUNCTION__;
+		$value      = __FUNCTION__;
+		register_meta( 'site', $option, array(
+			'type'    => 'string',
+			'default' => $value,
+			'single'  => true,
+		) );
+
+		$this->assertSame( $value, get_network_option( $network_id, $option ) );
+	}
+
+	/**
+	 * @ticket 37181
+	 * @group ms-required
+	 */
+	public function test_register_meta_network_option_value() {
+		$network_id = self::factory()->network->create();
+		$option     = __FUNCTION__;
+		$value      = __FUNCTION__;
+		register_meta( 'site', $option, array(
+			'type'    => 'string',
+			'default' => $value,
+			'single'  => true,
+		) );
+
+		add_metadata( 'site', $network_id, $option, 'monday', true );
+		$this->assertSame( 'monday', get_network_option( $network_id, $option ) );
+	}
 }
