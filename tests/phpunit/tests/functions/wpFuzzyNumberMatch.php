00cf6f0476b1a3db29435ea8547a655cec1d4402
Paul Bearne <pbearne@gmail.com>
2022-03-31 11:05:33 -0400

    Update tests/phpunit/tests/functions/wpFuzzyNumberMatch.php
    Co-authored-by: Colin Stewart <79332690+costdev@users.noreply.github.com>

--- tests/phpunit/tests/functions/wpFuzzyNumberMatch.php
+++ tests/phpunit/tests/functions/wpFuzzyNumberMatch.php
@@ -24,7 +24,6 @@ class Tests_Functions_WpFuzzyNumberMatch extends WP_UnitTestCase {
 	 * @return array[]
 	 */
 	public function data_wp_fuzzy_number_match() {
-
 		return array(
 			1            => array(
 				'expected'  => 1,
