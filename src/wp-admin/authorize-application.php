89411c40cc27250920c784953947aaed8036266e
audrasjb <audrasjb@gmail.com>
2022-04-08 00:57:59 +0200

    WPCS

--- src/wp-admin/authorize-application.php
+++ src/wp-admin/authorize-application.php
@@ -167,9 +167,9 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					<?php
 					/* translators: 1: URL to my-sites.php, 2: Number of sites the user has. */
 					$msg_fmt  = _n(
-							'This will grant access to <a href="%1$s">the %2$s site in this installation that you have permissions on</a>.',
-							'This will grant access to <a href="%1$s">all %2$s sites in this installation that you have permissions on</a>.',
-							$blogs_count
+						'This will grant access to <a href="%1$s">the %2$s site in this installation that you have permissions on</a>.',
+						'This will grant access to <a href="%1$s">all %2$s sites in this installation that you have permissions on</a>.',
+						$blogs_count
 					);
 					if ( is_super_admin() ) {
 						$msg_fmt  = _n(
