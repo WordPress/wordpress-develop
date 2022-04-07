ed4999d9676745bf2f45a2b68378e62b889a46aa
Justin Ahinon <justiny.ahinon@gmail.com>
2022-04-07 13:14:05 +0200

    Rename $readonly to $readonly_value

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -4850,15 +4850,15 @@ function disabled( $disabled, $current = true, $display = true ) {
  *
  * @since 5.9.0
  *
- * @param mixed $readonly One of the values to compare.
- * @param mixed $current  Optional. The other value to compare if not just true.
- *                        Default true.
- * @param bool  $display  Optional. Whether to echo or just return the string.
- *                        Default true.
+ * @param mixed $readonly_value One of the values to compare.
+ * @param mixed $current        Optional. The other value to compare if not just true.
+ *                              Default true.
+ * @param bool  $display        Optional. Whether to echo or just return the string.
+ *                              Default true.
  * @return string HTML attribute or empty string.
  */
-function wp_readonly( $readonly, $current = true, $display = true ) {
-	return __checked_selected_helper( $readonly, $current, $display, 'readonly' );
+function wp_readonly( $readonly_value, $current = true, $display = true ) {
+	return __checked_selected_helper( $readonly_value, $current, $display, 'readonly' );
 }
 
 /*
