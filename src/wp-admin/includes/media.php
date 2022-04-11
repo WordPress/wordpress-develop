feb4412c3c2b3e0918ab707b70ea74f37e1303e1
Erik <11491369+kebbet@users.noreply.github.com>
2022-04-11 13:32:31 +0200

    Use fieldset or wrapping element of search inputs.

--- src/wp-admin/includes/media.php
+++ src/wp-admin/includes/media.php
@@ -2738,11 +2738,11 @@ function media_upload_library_form( $errors ) {
 	<input type="hidden" name="post_mime_type" value="<?php echo isset( $_GET['post_mime_type'] ) ? esc_attr( $_GET['post_mime_type'] ) : ''; ?>" />
 	<input type="hidden" name="context" value="<?php echo isset( $_GET['context'] ) ? esc_attr( $_GET['context'] ) : ''; ?>" />
 
-	<div id="media-search" class="search-box">
+	<fieldset id="media-search" class="search-box">
 		<label class="screen-reader-text" for="media-search-input"><?php _e( 'Search Media' ); ?>:</label>
 		<input type="search" id="media-search-input" name="s" value="<?php the_search_query(); ?>" />
 		<?php submit_button( __( 'Search Media' ), '', '', false ); ?>
-	</div>
+	</fieldset>
 
 	<ul class="subsubsub">
 		<?php
