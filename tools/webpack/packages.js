1c63bca8fba948713bc858efbf4ba14e44fddf39
Adam Zieliński <adam@adamziel.com>
2022-04-11 17:45:43 +0200

    Adjust webpack configuration to reflect changes from Gutenberg
    * Add the @wordpress/preferences package
    * Rename GUTENBERG_PHASE1 to IS_GUTENBERG_PLUGIN
    * Remove COMPONENT_SYSTEM_PHASE

--- tools/webpack/packages.js
+++ tools/webpack/packages.js
@@ -149,10 +149,8 @@ module.exports = function( env = { environment: 'production', watch: false, buil
 		},
 		plugins: [
 			new DefinePlugin( {
-				// Inject the `GUTENBERG_PHASE` global, used for feature flagging.
-				'process.env.GUTENBERG_PHASE': 1,
-				// Inject the `COMPONENT_SYSTEM_PHASE` global, used for controlling Component System roll-out.
-				'process.env.COMPONENT_SYSTEM_PHASE': 0,
+				// Inject the `IS_GUTENBERG_PLUGIN` global, used for feature flagging.
+				'process.env.IS_GUTENBERG_PLUGIN': 1,
 				'process.env.FORCE_REDUCED_MOTION': JSON.stringify(
 					process.env.FORCE_REDUCED_MOTION
 				),
