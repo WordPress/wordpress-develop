c5994cda2207fa27b438b3a835feed70648a23c1
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-04-11 13:51:48 -0700

    Add `edit` context for consistency

--- src/wp-includes/rest-api/endpoints/class-wp-rest-block-pattern-categories-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-block-pattern-categories-controller.php
@@ -134,13 +134,13 @@ class WP_REST_Block_Pattern_Categories_Controller extends WP_REST_Controller {
 					'description' => __( 'The category name.' ),
 					'type'        => 'string',
 					'readonly'    => true,
-					'context'     => array( 'view', 'embed' ),
+					'context'     => array( 'view', 'edit', 'embed' ),
 				),
 				'label' => array(
 					'description' => __( 'The category label, in human readable format.' ),
 					'type'        => 'string',
 					'readonly'    => true,
-					'context'     => array( 'view', 'embed' ),
+					'context'     => array( 'view', 'edit', 'embed' ),
 				),
 			),
 		);
