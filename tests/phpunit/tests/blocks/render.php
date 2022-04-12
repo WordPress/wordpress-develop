be58de19ea6864e8db279b9b068d8385322aebb5
Adam Zieliński <adam@adamziel.com>
2022-04-12 12:19:11 +0200

    Normalize the gallery block classname of wp_unique_id( 'wp-block-gallery-' )
    Replace the random-ish output such as wp-block-gallery-12 with a
    predictable one like wp-block-gallery-1

--- tests/phpunit/tests/blocks/render.php
+++ tests/phpunit/tests/blocks/render.php
@@ -224,6 +224,10 @@ class Tests_Blocks_Render extends WP_UnitTestCase {
 		// the container will receive an added classname of `wp_unique_id( 'wp-container-' )`
 		// so we need to normalize the random id.
 		$normalized_html = preg_replace( '/wp-container-\d+/', 'wp-container-1', $html );
+
+		// The gallery block uses a unique class name of `wp_unique_id( 'wp-block-gallery-' )`
+		// so we need to normalize the random id.
+		$normalized_html = preg_replace( '/wp-block-gallery-\d+\s/', 'wp-block-gallery-1', $normalized_html );
 		$expected_html   = self::strip_r( file_get_contents( $server_html_path ) );
 
 		$this->assertSame(
