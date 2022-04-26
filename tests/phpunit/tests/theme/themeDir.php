30d5aafb69e31db7dfcb98ea0d43cb92e396e995
hellofromtonya <tonya.mork@automattic.com>
2022-04-26 08:57:00 -0500

    Add new test theme to Tests_Theme_ThemeDir::test_theme_list.

--- tests/phpunit/tests/theme/themeDir.php
+++ tests/phpunit/tests/theme/themeDir.php
@@ -166,6 +166,7 @@ class Tests_Theme_ThemeDir extends WP_UnitTestCase {
 			'Block Theme [0.4.0]',
 			'Block Theme [1.0.0] in subdirectory',
 			'Webfonts theme',
+			'Empty `fontFace` in theme.json - no webfonts defined',
 		);
 
 		sort( $theme_names );
