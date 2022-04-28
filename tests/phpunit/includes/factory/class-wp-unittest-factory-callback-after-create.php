dcbf7765dfcecbe979403782e0b3f7a78a18b3a8
Justin Ahinon <justiny.ahinon@gmail.com>
2022-04-28 13:15:52 +0200

    Rename $object to $created

--- tests/phpunit/includes/factory/class-wp-unittest-factory-callback-after-create.php
+++ tests/phpunit/includes/factory/class-wp-unittest-factory-callback-after-create.php
@@ -19,11 +19,11 @@ class WP_UnitTest_Factory_Callback_After_Create {
 	/**
 	 * Calls the set callback on a given object.
 	 *
-	 * @param mixed $object The object to apply the callback on.
+	 * @param mixed $created The object to apply the callback on.
 	 *
 	 * @return mixed The possibly altered object.
 	 */
-	public function call( $object ) {
-		return call_user_func( $this->callback, $object );
+	public function call( $created ) {
+		return call_user_func( $this->callback, $created );
 	}
 }
