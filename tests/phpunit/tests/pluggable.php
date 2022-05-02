28180c524a80a1cfa01022e1142ee20d5fe7a9db
costdev <me@colin-stewart.co.uk>
2022-05-02 18:55:34 +0100

    Add tests.

--- tests/phpunit/tests/pluggable.php
+++ tests/phpunit/tests/pluggable.php
@@ -354,4 +354,98 @@ class Tests_Pluggable extends WP_UnitTestCase {
 
 		$this->assertSame( $current_user, $from_get_user_by );
 	}
+
+	/**
+	 * Tests that wp_rand() returns zero.
+	 *
+	 * @ticket 55194
+	 * @dataProvider data_wp_rand_should_return_zero
+	 * @covers ::wp_rand
+	 *
+	 * @param mixed $min Lower limit for the generated number.
+	 * @param mixed $max Upper limit for the generated number.
+	 */
+	public function test_wp_rand_should_return_zero( $min, $max ) {
+		$this->assertSame( 0, wp_rand( $min, $max ) );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_wp_rand_should_return_zero() {
+		return array(
+			'min and max as 0'      => array(
+				'min' => 0,
+				'max' => 0,
+			),
+			'min and max as 0.0'    => array(
+				'min' => 0.0,
+				'max' => 0.0,
+			),
+			'min as null, max as 0' => array(
+				'min' => null,
+				'max' => 0,
+			),
+		);
+	}
+
+	/**
+	 * Tests that wp_rand() returns a value between 1 and 99.
+	 *
+	 * @ticket 55194
+	 * @dataProvider data_wp_rand_should_return_between_1_and_99
+	 * @covers ::wp_rand
+	 *
+	 * @param int $min Lower limit for the generated number.
+	 * @param int $max Upper limit for the generated number.
+	 */
+	public function test_wp_rand_should_return_between_1_and_99( $min, $max ) {
+		$this->assertGreaterThan(
+			0,
+			wp_rand( $min, $max ),
+			'The value was not greater than 0'
+		);
+
+		$this->assertLessThan(
+			100,
+			wp_rand( $min, $max ),
+			'The value was not less than 100'
+		);
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_wp_rand_should_return_between_1_and_99() {
+		return array(
+			'1 and 99'       => array(
+				'min' => 1,
+				'max' => 99,
+			),
+			'-1 and 99'      => array(
+				'min' => -1,
+				'max' => 99,
+			),
+			'1 and -99'      => array(
+				'min' => 1,
+				'max' => -99,
+			),
+			'-1 and -99'     => array(
+				'min' => -1,
+				'max' => -99,
+			),
+			'1.0 and 99.0'   => array(
+				'min' => 1.0,
+				'max' => 99.0,
+			),
+			'-1.0 and -99.0' => array(
+				'min' => -1.0,
+				'max' => -99.0,
+			),
+		);
+	}
 }
