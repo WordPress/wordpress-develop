a2af3b8fd48f0bed9f3521b0c202cd21ced572e5
NekoJonez <NekoJonez@users.noreply.github.com>
2022-05-03 09:56:59 +0200

    Update ajax-actions.php

--- src/wp-admin/includes/ajax-actions.php
+++ src/wp-admin/includes/ajax-actions.php
@@ -1291,7 +1291,7 @@ function wp_ajax_replyto_comment( $action ) {
 	if ( empty( $post->post_status ) ) {
 		wp_die( 1 );
 	} elseif ( in_array( $post->post_status, array( 'draft', 'pending', 'trash' ), true ) ) {
-		wp_die( __( '<strong>Error:</strong> You cannot reply to a comment on a draft post.' ) );
+		wp_die( __( 'You cannot reply to a comment on a draft post.' ) );
 	}
 
 	$user = wp_get_current_user();
