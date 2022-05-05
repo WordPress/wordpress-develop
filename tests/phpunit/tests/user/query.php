5891788b35f2e027ee2e673e8a9d32fdbb008785
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2022-05-05 14:58:38 +1000

    subset not subsset.

--- tests/phpunit/tests/user/query.php
+++ tests/phpunit/tests/user/query.php
@@ -1974,7 +1974,7 @@ class Tests_User_Query extends WP_UnitTestCase {
 	 * @param string $field
 	 * @param mixed $expected_results
 	 */
-	public function test_returning_field_subsset_as_string( string $field, $expected_results ) {
+	public function test_returning_field_subset_as_string( string $field, $expected_results ) {
 		$q       = new WP_User_Query(
 			array(
 				'fields'  => $field,
@@ -2019,7 +2019,7 @@ class Tests_User_Query extends WP_UnitTestCase {
 	 * @param array $field
 	 * @param mixed $expected_results
 	 */
-	public function test_returning_field_subsset_as_array( array $field, $expected_results ) {
+	public function test_returning_field_subset_as_array( array $field, $expected_results ) {
 		$q       = new WP_User_Query(
 			array(
 				'fields'  => $field,
