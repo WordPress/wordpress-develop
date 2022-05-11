e189b2e001184dfeb43153650f723b250107a8c8
stevenlin-x <stevenlin-x@users.noreply.github.com>
2022-05-11 23:36:12 +0800

    updated with a translator comment

--- src/wp-includes/class-wp-customize-manager.php
+++ src/wp-includes/class-wp-customize-manager.php
@@ -5672,10 +5672,10 @@ final class WP_Customize_Manager {
 		$section_description  = '<p>';
 		$section_description .= __( 'Add your own CSS code here to customize the appearance and layout of your site.' );
 		$section_description .= sprintf(
+			/* translators: 1: HelpHub CSS article URL 2: Title description of the article 3: Accessibility text. */
 			' <a href="%1$s" class="external-link" target="_blank">%2$s<span class="screen-reader-text"> %3$s</span></a>',
 			esc_url( __( 'https://wordpress.org/support/article/css/' ) ),
 			__( 'Learn more about CSS' ),
-			/* translators: Accessibility text. */
 			__( '(opens in a new tab)' )
 		);
 		$section_description .= '</p>';
