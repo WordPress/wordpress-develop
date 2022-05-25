89ef69a256697171049809c74f0d45785834fa54
Graham May <graham.may@soapbox.co.uk>
2022-05-25 16:28:28 +0100

    Adding support for thickbox.js to accept .webp files.
    Issue: https://core.trac.wordpress.org/ticket/55786

--- src/js/_enqueues/vendor/thickbox/thickbox.js
+++ src/js/_enqueues/vendor/thickbox/thickbox.js
@@ -76,10 +76,10 @@ function tb_show(caption, url, imageGroup) {//function called when the user clic
 	   		baseURL = url;
 	   }
 
-	   var urlString = /\.jpg$|\.jpeg$|\.png$|\.gif$|\.bmp$/;
-	   var urlType = baseURL.toLowerCase().match(urlString);
+		var urlString = /\.webp|\.jpg$|\.jpeg$|\.png$|\.gif$|\.bmp$/;
+		var urlType = baseURL.toLowerCase().match(urlString);
 
-		if(urlType == '.jpg' || urlType == '.jpeg' || urlType == '.png' || urlType == '.gif' || urlType == '.bmp'){//code to show images
+		if(urlType == '.webp' || urlType == '.jpg' || urlType == '.jpeg' || urlType == '.png' || urlType == '.gif' || urlType == '.bmp'){//code to show images
 
 			TB_PrevCaption = "";
 			TB_PrevURL = "";
