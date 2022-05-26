ac42fad371a04970d71a33e083e2627ac592d17d
Anton Vlasenko <vlasenko.anton@gmail.com>
2022-05-26 20:37:38 +0200

    Update @covers tag for Tests_Ajax_AddMeta class

--- tests/phpunit/tests/ajax/AddMeta.php
+++ tests/phpunit/tests/ajax/AddMeta.php
@@ -15,15 +15,16 @@ class Tests_Ajax_AddMeta extends WP_Ajax_UnitTestCase {
 	 * @ticket 43559
 	 *
 	 * @covers ::add_post_meta
+	 * @covers ::wp_ajax_add_meta
 	 */
 	public function test_post_add_meta_empty_is_allowed_ajax() {
-		$p = self::factory()->post->create();
+		$post = self::factory()->post->create();
 
 		// Become an administrator.
 		$this->_setRole( 'administrator' );
 
 		$_POST = array(
-			'post_id'              => $p,
+			'post_id'              => $post,
 			'metakeyinput'         => 'testkey',
 			'metavalue'            => '',
 			'_ajax_nonce-add-meta' => wp_create_nonce( 'add-meta' ),
@@ -36,27 +37,28 @@ class Tests_Ajax_AddMeta extends WP_Ajax_UnitTestCase {
 			unset( $e );
 		}
 
-		$this->assertSame( '', get_post_meta( $p, 'testkey', true ) );
+		$this->assertSame( '', get_post_meta( $post, 'testkey', true ) );
 	}
 
 	/**
 	 * @ticket 43559
 	 *
-	 * @covers ::update_post_meta
+	 * @covers ::update_metadata_by_mid
+	 * @covers ::wp_ajax_add_meta
 	 */
-	public function test_post_update_meta_empty_is_allowed_ajax() {
-		$p = self::factory()->post->create();
+	public function test_update_metadata_by_mid_allows_empty_values_ajax() {
+		$post = self::factory()->post->create();
 
-		$m = add_post_meta( $p, 'testkey', 'hello' );
+		$meta_id = add_post_meta( $post, 'testkey', 'hello' );
 
 		// Become an administrator.
 		$this->_setRole( 'administrator' );
 
 		$_POST = array(
 			'_ajax_nonce-add-meta' => wp_create_nonce( 'add-meta' ),
-			'post_id'              => $p,
+			'post_id'              => $post,
 			'meta'                 => array(
-				$m => array(
+				$meta_id => array(
 					'key'   => 'testkey',
 					'value' => '',
 				),
@@ -70,6 +72,6 @@ class Tests_Ajax_AddMeta extends WP_Ajax_UnitTestCase {
 			unset( $e );
 		}
 
-		$this->assertSame( '', get_post_meta( $p, 'testkey', true ) );
+		$this->assertSame( '', get_post_meta( $post, 'testkey', true ) );
 	}
 }
