96f49f739c4d263c8505a967d8a78620d500a77f
bhrugesh96 <bhrugeshpatel96@gmail.com>
2022-05-27 21:24:11 +0530

    Remove Code - By Mistake merge in wrong PR

--- src/wp-includes/comment-template.php
+++ src/wp-includes/comment-template.php
@@ -2371,7 +2371,7 @@ function comment_form( $args = array(), $post_id = null ) {
 				( $req ? $required_indicator : '' )
 			),
 			sprintf(
-				'<input id="author" name="author" type="text" value="%s" size="30" maxlength="245"%s autocomplete="name" />',
+				'<input id="author" name="author" type="text" value="%s" size="30" maxlength="245"%s />',
 				esc_attr( $commenter['comment_author'] ),
 				( $req ? $required_attribute : '' )
 			)
@@ -2384,7 +2384,7 @@ function comment_form( $args = array(), $post_id = null ) {
 				( $req ? $required_indicator : '' )
 			),
 			sprintf(
-				'<input id="email" name="email" %s value="%s" size="30" maxlength="100" aria-describedby="email-notes"%s autocomplete="email" />',
+				'<input id="email" name="email" %s value="%s" size="30" maxlength="100" aria-describedby="email-notes"%s />',
 				( $html5 ? 'type="email"' : 'type="text"' ),
 				esc_attr( $commenter['comment_author_email'] ),
 				( $req ? $required_attribute : '' )
@@ -2397,7 +2397,7 @@ function comment_form( $args = array(), $post_id = null ) {
 				__( 'Website' )
 			),
 			sprintf(
-				'<input id="url" name="url" %s value="%s" size="30" maxlength="200" autocomplete="url" />',
+				'<input id="url" name="url" %s value="%s" size="30" maxlength="200" />',
 				( $html5 ? 'type="url"' : 'type="text"' ),
 				esc_attr( $commenter['comment_author_url'] )
 			)
