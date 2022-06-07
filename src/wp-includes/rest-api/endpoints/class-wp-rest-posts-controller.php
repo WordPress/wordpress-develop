534b008fa884ff4a136ca2013531283596ad4894
manuelRod <manuel.rodriguez.rosado@gmail.com>
2022-06-07 15:10:58 +0200

    count and last revision may not be set

--- src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php
@@ -2018,8 +2018,8 @@ class WP_REST_Posts_Controller extends WP_REST_Controller {
 
 		if ( in_array( $post->post_type, array( 'post', 'page' ), true ) || post_type_supports( $post->post_type, 'revisions' ) ) {
 			$revision        = wp_get_last_post_revision_id_and_count( $post->ID );
-			$last_revision   = $revision['last_revision'];
-			$revisions_count = $revision['count'];
+			$last_revision   = isset( $revision['last_revision'] ) ? $revision['last_revision'] : '';
+			$revisions_count = isset( $revision['count'] ) ? $revision['count'] : 0;
 
 			$links['version-history'] = array(
 				'href'  => rest_url( trailingslashit( $base ) . $post->ID . '/revisions' ),
