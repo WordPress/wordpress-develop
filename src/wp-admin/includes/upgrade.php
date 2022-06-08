eb7b41d67e2ae4a985b5b718c3a7d3f485bf4f0b
Brandon Hubbard <brandon.hubbard@appward.com>
2022-06-08 12:21:40 -0700

    table name checks added

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -2497,12 +2497,24 @@ function maybe_create_table( $table_name, $create_ddl ) {
  * Maybe Rename table.
  * @param  string $old_table_name Old Table Name.
  * @param  string $new_table_name New Table Name.
- * @return true True, when done with execution.
+ * @return bool True, when done with execution.
  */
 function maybe_rename_table( $old_table_name, $new_table_name ) {
 	global $wpdb;
 
-	return $wpdb->query( "RENAME TABLE `$old_table_name` TO `$new_table_name`" );
+	$query = $wpdb->prepare( 'SHOW TABLES LIKE %s', $wpdb->esc_like( $old_table_name ) );
+
+	if ( $wpdb->get_var( $query ) === $old_table_name ) {
+		return true;
+	}
+
+	$wpdb->query( "RENAME TABLE `$old_table_name` TO `$new_table_name`" );
+
+	if ( $wpdb->get_var( $query ) === $new_table_name ) {
+		return true;
+	}
+
+	return false;
 
 }
 
