53baa8e766878b7a4fd14aea1ca3a4f38dd39d49
manuelRod <manuel.rodriguez.rosado@gmail.com>
2022-06-13 18:16:43 +0200

    refactor test

--- tests/phpunit/tests/post/nav-menu.php
+++ tests/phpunit/tests/post/nav-menu.php
@@ -223,20 +223,13 @@ class Tests_Post_Nav_Menu extends WP_UnitTestCase {
 
 		wp_cache_delete( $post_id, 'posts' );
 		$action = new MockAction();
-		add_filter( 'query', array( $action, 'filter' ), 10, 2 );
+		add_filter( 'update_post_metadata_cache', array( $action, 'filter' ), 10, 2 );
 
 		update_menu_item_cache( $query_result );
 
-		$args                    = $action->get_args();
-		$primed_post_query_found = false;
-		foreach ( $args as $arg ) {
-			// Primed query will use WHERE ID IN clause.
-			if ( str_contains( $arg[0], 'WHERE ID IN (' . $post_id ) ) {
-				$primed_post_query_found = true;
-				break;
-			}
-		}
-		$this->assertTrue( $primed_post_query_found, '_prime_post_caches was not executed.' );
+		$args = $action->get_args();
+		$last = end( $args );
+		$this->assertEqualSets( array( $post_id ), $last[1], '_prime_post_caches was not executed.' );
 	}
 
 	/**
