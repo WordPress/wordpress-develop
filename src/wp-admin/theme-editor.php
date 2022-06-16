87462795e542f9ba91fb29e44da4d867d67fd3b2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-06-16 14:03:47 +0300

    Correct the escaping in plugin/theme editor

--- src/wp-admin/theme-editor.php
+++ src/wp-admin/theme-editor.php
@@ -165,9 +165,9 @@ if ( ! empty( $posted_content ) ) {
 		$functions = wp_doc_link_parse( $content );
 
 		$docs_select  = '<select name="docs-list" id="docs-list">';
-		$docs_select .= '<option value="">' . esc_attr__( 'Function Name&hellip;' ) . '</option>';
+		$docs_select .= '<option value="">' . esc_html__( 'Function Name&hellip;' ) . '</option>';
 		foreach ( $functions as $function ) {
-			$docs_select .= '<option value="' . esc_attr( urlencode( $function ) ) . '">' . htmlspecialchars( $function ) . '()</option>';
+			$docs_select .= '<option value="' . esc_attr( $function ) . '">' . esc_html( $function ) . '()</option>';
 		}
 		$docs_select .= '</select>';
 	}
