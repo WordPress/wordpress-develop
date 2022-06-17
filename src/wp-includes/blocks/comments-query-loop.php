0231dc191b4009deb47233a572ac896f2d3406d4
Carlos Bravo <carlos.bravo@automattic.com>
2022-06-17 13:54:20 +0200

    Add translation comment on file

--- src/wp-includes/blocks/comments-query-loop.php
+++ src/wp-includes/blocks/comments-query-loop.php
@@ -7,6 +7,8 @@
 
 /**
  * Registers the `core/comments-query-loop` block on the server.
+ * We need this file in order to have the title and description for the block translations.
+ * More info in the issue: https://github.com/WordPress/gutenberg/issues/41292
  */
 function register_block_core_comments_query_loop() {
 	register_block_type_from_metadata(
