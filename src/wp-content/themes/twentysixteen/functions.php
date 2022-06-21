51a67a9f136c330b1ea57b7ed6a73b4221ce396a
Hendrik Luehrsen <hendrik@luehrsen.de>
2022-06-21 09:58:24 +0200

    Register Merriweather

--- src/wp-content/themes/twentysixteen/functions.php
+++ src/wp-content/themes/twentysixteen/functions.php
@@ -294,6 +294,31 @@ if ( ! function_exists( 'twentysixteen_fonts_url' ) ) :
 	 * @return string Fonts URL for the theme.
 	 */
 	function twentysixteen_fonts_url() {
+
+		/*
+		 * translators: If there are characters in your language that are not supported
+		 * by Merriweather, translate this to 'off'. Do not translate into your own language.
+		 */
+		if ( 'off' !== _x( 'on', 'Merriweather font: on or off', 'twentysixteen' ) ) {
+			$fonts[] = 'Merriweather:400,700,900,400italic,700italic,900italic';
+		}
+
+		/*
+		 * translators: If there are characters in your language that are not supported
+		 * by Montserrat, translate this to 'off'. Do not translate into your own language.
+		 */
+		if ( 'off' !== _x( 'on', 'Montserrat font: on or off', 'twentysixteen' ) ) {
+			$fonts[] = 'Montserrat:400,700';
+		}
+
+		/*
+		 * translators: If there are characters in your language that are not supported
+		 * by Inconsolata, translate this to 'off'. Do not translate into your own language.
+		 */
+		if ( 'off' !== _x( 'on', 'Inconsolata font: on or off', 'twentysixteen' ) ) {
+			$fonts[] = 'Inconsolata:400';
+		}
+
 		$fonts_url = get_stylesheet_directory_uri() . '/fonts.css';
 		return $fonts_url;
 	}
