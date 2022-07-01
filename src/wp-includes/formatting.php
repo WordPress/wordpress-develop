1808010312c146b36f335feafec9346a7c719285
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-07-01 10:30:26 -0700

    Strip object replacement character from slugs

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -2329,6 +2329,8 @@ function sanitize_title_with_dashes( $title, $raw_title = '', $context = 'displa
 				'%e2%80%ad', // Left-to-right override.
 				'%e2%80%ae', // Right-to-left override.
 				'%ef%bb%bf', // Byte order mark.
+				// Object replacement character.
+				'%ef%bf%bc',
 			),
 			'',
 			$title
