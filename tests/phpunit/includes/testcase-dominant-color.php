faa980faad545ddc6d3d91b59f2c1a756f3f2269
Paul Bearne <pbearne@gmail.com>
2022-07-07 13:39:31 -0400

    whitespace

--- tests/phpunit/includes/testcase-dominant-color.php
+++ tests/phpunit/includes/testcase-dominant-color.php
@@ -9,64 +9,64 @@ abstract class DominantColorTestCase extends WP_Image_UnitTestCase {
 	 */
 	public function provider_get_dominant_color() {
 		$data = array(
-			'animated_gif' => array(
+			'animated_gif'  => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/animated.gif',
 				'expected_color'        => array( '874e4e', '864e4e', 'df7f7f' ),
 				'expected_transparency' => true,
 			),
-			'red_jpg'      => array(
+			'red_jpg'       => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/red.jpg',
 				'expected_color'        => array( 'ff0000', 'fe0000' ),
 				'expected_transparency' => false,
 			),
-			'green_jpg'    => array(
+			'green_jpg'     => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/green.jpg',
 				'expected_color'        => array( '00ff00', '00ff01' ),
 				'expected_transparency' => false,
 			),
-			'white_jpg'    => array(
+			'white_jpg'     => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/white.jpg',
 				'expected_color'        => array( 'ffffff' ),
 				'expected_transparency' => false,
 			),
 
-			'red_gif'      => array(
+			'red_gif'       => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/red.gif',
 				'expected_color'        => array( 'ff0000' ),
 				'expected_transparency' => false,
 			),
-			'green_gif'    => array(
+			'green_gif'     => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/green.gif',
 				'expected_color'        => array( '00ff00' ),
 				'expected_transparency' => false,
 			),
-			'white_gif'    => array(
+			'white_gif'     => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/white.gif',
 				'expected_color'        => array( 'ffffff' ),
 				'expected_transparency' => false,
 			),
-			'trans_gif'    => array(
+			'trans_gif'     => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/trans.gif',
 				'expected_color'        => array( '5a5a5a', '020202' ),
 				'expected_transparency' => true,
 			),
 
-			'red_png'      => array(
+			'red_png'       => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/red.png',
 				'expected_color'        => array( 'ff0000' ),
 				'expected_transparency' => false,
 			),
-			'green_png'    => array(
+			'green_png'     => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/green.png',
 				'expected_color'        => array( '00ff00' ),
 				'expected_transparency' => false,
 			),
-			'white_png'    => array(
+			'white_png'     => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/white.png',
 				'expected_color'        => array( 'ffffff' ),
 				'expected_transparency' => false,
 			),
-			'trans_png'    => array(
+			'trans_png'     => array(
 				'image_path'            => DIR_TESTDATA . '/images/dominant-color/trans.png',
 				'expected_color'        => array( '000000' ),
 				'expected_transparency' => true,
