ce154a4b66f5a3446aa4d997966f46e9119bb626
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-07-25 19:18:41 +0000

    Revisions: Rename the function for retrieving the latest revision ID and total count.
    The new name is `wp_get_latest_revision_id_and_total_count()`.
    
    This aims to reduce ambiguity about what exactly is the "first" or "last" revision, and bring more consistency with similar wording elsewhere in core, e.g. latest posts, latest comments, etc.
    
    Follow-up to [53759], [53769].
    
    Props peterwilsoncc.
    See #55857.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@53778 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/post/revisions.php
+++ tests/phpunit/tests/post/revisions.php
@@ -656,12 +656,13 @@ class Tests_Post_Revisions extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that wp_get_last_revision_id_and_total_count() returns the last revision ID and total count.
+	 * Tests that wp_get_latest_revision_id_and_total_count() returns the latest revision ID and total count.
 	 *
+	 * @covers ::wp_get_latest_revision_id_and_total_count
 	 * @ticket 55857
 	 * @dataProvider data_wp_get_post_revisions_url
 	 */
-	public function test_wp_get_last_revision_id_and_total_count( $revisions ) {
+	public function test_wp_get_latest_revision_id_and_total_count( $revisions ) {
 		$post_id = self::factory()->post->create();
 		for ( $i = 0; $i < $revisions; ++$i ) {
 			wp_update_post(
@@ -672,14 +673,14 @@ class Tests_Post_Revisions extends WP_UnitTestCase {
 			);
 		}
 
-		$post_revisions     = wp_get_post_revisions( $post_id );
-		$last_post_revision = current( $post_revisions );
-		$revision           = wp_get_last_revision_id_and_total_count( $post_id );
+		$post_revisions       = wp_get_post_revisions( $post_id );
+		$latest_post_revision = current( $post_revisions );
+		$revision             = wp_get_latest_revision_id_and_total_count( $post_id );
 
 		$this->assertSame(
-			$last_post_revision->ID,
+			$latest_post_revision->ID,
 			$revision['revision'],
-			'The last revision ID does not match.'
+			'The latest revision ID does not match.'
 		);
 
 		$this->assertSame(
@@ -690,19 +691,20 @@ class Tests_Post_Revisions extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that wp_get_last_revision_id_and_total_count() returns a WP_Error when no revisions exist.
+	 * Tests that wp_get_latest_revision_id_and_total_count() returns a WP_Error when no revisions exist.
 	 *
+	 * @covers ::wp_get_latest_revision_id_and_total_count
 	 * @ticket 55857
 	 */
-	public function test_wp_get_last_revision_id_and_total_count_no_revisions() {
-		$revision = wp_get_last_revision_id_and_total_count( null );
+	public function test_wp_get_latest_revision_id_and_total_count_no_revisions() {
+		$revision = wp_get_latest_revision_id_and_total_count( null );
 
 		$this->assertWPError( $revision, 'Invalid post, no revisions should exist.' );
 		$this->assertSame( $revision->get_error_code(), 'invalid_post' );
 
 		add_filter( 'wp_revisions_to_keep', '__return_zero' );
 		$post_id  = self::factory()->post->create();
-		$revision = wp_get_last_revision_id_and_total_count( $post_id );
+		$revision = wp_get_latest_revision_id_and_total_count( $post_id );
 
 		$this->assertWPError( $revision, 'Revisions should not be enabled.' );
 		$this->assertSame( $revision->get_error_code(), 'revisions_not_enabled' );
