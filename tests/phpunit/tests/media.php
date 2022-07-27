94f85c3757244fe059bab2c9d00d240c149ca808
Adam Silverstein <adamjs@google.com>
2022-07-27 12:43:13 -0600

    add test coverage for add_image_size additional mimes parameter

--- tests/phpunit/tests/media.php
+++ tests/phpunit/tests/media.php
@@ -1156,6 +1156,24 @@ VIDEO;
 	}
 
 	/**
+	 * @ticket 56288
+	 */
+	public function test_add_image_size_additional_mimes() {
+		add_image_size( 'test-size-with-additional-mimes', 200, 600, false, true );
+		add_image_size( 'test-size-without-additional-mimes', 200, 600, false, false );
+
+		$sizes = wp_get_additional_image_sizes();
+
+		remove_image_size( 'test-size-with-additional-mimes' );
+		remove_image_size( 'test-size-without-additional-mimes' );
+
+		$this->assertArrayHasKey( 'test-size-with-additional-mimes', $sizes );
+		$this->assertArrayHasKey( 'test-size-without-additional-mimes', $sizes );
+		$this->assertTrue( $sizes['test-size-with-additional-mimes']['additional_mimes'] );
+		$this->assertFalse( $sizes['test-size-without-additional-mimes']['additional_mimes'] );
+	}
+
+	/**
 	 * @ticket 26768
 	 */
 	public function test_remove_image_size() {
