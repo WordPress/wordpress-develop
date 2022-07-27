700b225a473db76acf4048a8224dc48198d71a0e
Adam Silverstein <adamjs@google.com>
2022-07-27 12:15:21 -0600

    Remove test changes that aren’t required

--- tests/phpunit/tests/media.php
+++ tests/phpunit/tests/media.php
@@ -1143,7 +1143,7 @@ VIDEO;
 		remove_image_size( 'test-size' );
 
 		$this->assertArrayNotHasKey( 'test-size', $_wp_additional_image_sizes );
-		add_image_size( 'test-size', 200, 600, false, true );
+		add_image_size( 'test-size', 200, 600 );
 
 		$sizes = wp_get_additional_image_sizes();
 
@@ -1159,7 +1159,7 @@ VIDEO;
 	 * @ticket 26768
 	 */
 	public function test_remove_image_size() {
-		add_image_size( 'test-size', 200, 600, false, true );
+		add_image_size( 'test-size', 200, 600 );
 		$this->assertTrue( has_image_size( 'test-size' ) );
 		remove_image_size( 'test-size' );
 		$this->assertFalse( has_image_size( 'test-size' ) );
@@ -1169,7 +1169,7 @@ VIDEO;
 	 * @ticket 26951
 	 */
 	public function test_has_image_size() {
-		add_image_size( 'test-size', 200, 600, false, true );
+		add_image_size( 'test-size', 200, 600 );
 		$this->assertTrue( has_image_size( 'test-size' ) );
 
 		// Clean up.
