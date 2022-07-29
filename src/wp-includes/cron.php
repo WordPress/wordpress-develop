36f63a5ecac8ad0ff7b2b4c98916f56604f8b463
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2022-07-29 10:50:29 +1000

    Add return type note to docblock.

--- src/wp-includes/cron.php
+++ src/wp-includes/cron.php
@@ -1162,6 +1162,7 @@ function wp_get_ready_cron_jobs() {
  * Retrieve cron info array option.
  *
  * @since 2.1.0
+ * @since 6.1.0 Return type modified to consistenty return an array.
  * @access private
  *
  * @return array[] Array of cron events.
