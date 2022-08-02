f79ad3077557620c9a3c51f646953156272e7e66
costdev <me@colin-stewart.co.uk>
2022-08-02 07:09:06 +0100

    Use a data provider to stabilise the tests.

--- tests/phpunit/tests/post/getAttachedFile.php
+++ tests/phpunit/tests/post/getAttachedFile.php
@@ -8,8 +8,14 @@ class Tests_Post_GetAttachedFile extends WP_UnitTestCase {
 
 	/**
 	 * @ticket 36308
+	 *
+	 * @dataProvider data_get_attached_file_with_windows_paths
+	 *
+	 * @param string $file     The file path to attach to the post.
+	 * @param string $expected The expected attached file path.
+	 * @param string $message  The message when an assertion fails.
 	 */
-	public function test_get_attached_file_with_windows_paths() {
+	public function test_get_attached_file_with_windows_paths( $file, $expected, $message ) {
 		$post = self::factory()->post->create_and_get(
 			array(
 				'post_title' => 'example-page',
@@ -17,32 +23,33 @@ class Tests_Post_GetAttachedFile extends WP_UnitTestCase {
 			)
 		);
 
-		// Windows local file system path.
 		$attachment = self::factory()->attachment->create_and_get(
 			array(
 				'post_parent' => $post->ID,
-				'file'        => 'C:/WWW/Sites/demo/htdocs/wordpress/wp-content/uploads/2016/03/example.jpg',
+				'file'        => $file,
 			)
 		);
 
-		$this->assertSame(
-			'C:/WWW/Sites/demo/htdocs/wordpress/wp-content/uploads/2016/03/example.jpg',
-			get_attached_file( $attachment->ID ),
-			'Windows local filesystem paths should be equal'
-		);
+		$this->assertSame( $expected, get_attached_file( $attachment->ID ), $message );
+	}
 
-		// Windows network shares path.
-		$attachment = self::factory()->attachment->create_and_get(
-			array(
-				'post_parent' => $post->ID,
-				'file'        => '//ComputerName/ShareName/SubfolderName/example.txt',
-			)
-		);
-
-		$this->assertSame(
-			'//ComputerName/ShareName/SubfolderName/example.txt',
-			get_attached_file( $attachment->ID ),
-			'Network share paths should be equal'
+	/**
+	 * Data provider with Windows paths.
+	 *
+	 * @return array
+	 */
+	public function data_get_attached_file_with_windows_paths() {
+		return array(
+			'a local path'         => array(
+				'file'     => 'C:/WWW/Sites/demo/htdocs/wordpress/wp-content/uploads/2016/03/example.jpg',
+				'expected' => 'C:/WWW/Sites/demo/htdocs/wordpress/wp-content/uploads/2016/03/example.jpg',
+				'message'  => 'Windows local filesystem paths should be equal',
+			),
+			'a network share path' => array(
+				'file'     => '//ComputerName/ShareName/SubfolderName/example.txt',
+				'expected' => '//ComputerName/ShareName/SubfolderName/example.txt',
+				'message'  => 'Network share paths should be equal',
+			),
 		);
 	}
 
