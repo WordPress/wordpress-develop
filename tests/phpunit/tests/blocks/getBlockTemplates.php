2db2a7dc954d30aa81ce758ae51a67697b2cf35c
Anton Vlasenko <vlasenko.anton@gmail.com>
2022-08-02 17:52:48 +0200

    Refactor Tests_Blocks_GetBlockTemplates to use the data provider.

--- tests/phpunit/tests/blocks/getBlockTemplates.php
+++ tests/phpunit/tests/blocks/getBlockTemplates.php
@@ -1,40 +1,56 @@
 <?php
-
+/**
+ * @group blocks
+ *
+ * @covers ::get_block_templates
+ */
 class Tests_Blocks_GetBlockTemplates extends WP_UnitTestCase {
 
 	const TEST_THEME = 'block-theme';
+
+	/**
+	 * @var WP_Post
+	 */
+	private static $template;
+
+	/**
+	 * @var WP_Post
+	 */
+	private static $template_part;
 
 	public static function wpSetUpBeforeClass() {
 
 		// This template has to have the same ID ("block-theme/index") as the template that is shipped with the
 		// "block-theme" theme. This is needed for testing purposes.
-		$args             = array(
-			'post_type' => 'wp_template',
-			'post_name' => 'index',
-			'tax_input' => array(
-				'wp_theme' => array(
-					static::TEST_THEME,
+		static::$template = self::factory()->post->create_and_get(
+			array(
+				'post_type' => 'wp_template',
+				'post_name' => 'index',
+				'tax_input' => array(
+					'wp_theme' => array(
+						static::TEST_THEME,
+					),
 				),
-			),
+			)
 		);
-		static::$template = self::factory()->post->create_and_get( $args );
 		wp_set_post_terms( static::$template->ID, static::TEST_THEME, 'wp_theme' );
 
 		// This template part has to have the same ID ("block-theme/small-header") as the template that is shipped with the
 		// "block-theme" theme. This is needed for testing purposes.
-		$template_part_args  = array(
-			'post_type' => 'wp_template_part',
-			'post_name' => 'small-header',
-			'tax_input' => array(
-				'wp_theme'              => array(
-					static::TEST_THEME,
+		self::$template_part = self::factory()->post->create_and_get(
+			array(
+				'post_type' => 'wp_template_part',
+				'post_name' => 'small-header',
+				'tax_input' => array(
+					'wp_theme'              => array(
+						static::TEST_THEME,
+					),
+					'wp_template_part_area' => array(
+						WP_TEMPLATE_PART_AREA_HEADER,
+					),
 				),
-				'wp_template_part_area' => array(
-					WP_TEMPLATE_PART_AREA_HEADER,
-				),
-			),
+			)
 		);
-		self::$template_part = self::factory()->post->create_and_get( $template_part_args );
 		wp_set_post_terms( self::$template_part->ID, WP_TEMPLATE_PART_AREA_HEADER, 'wp_template_part_area' );
 		wp_set_post_terms( self::$template_part->ID, static::TEST_THEME, 'wp_theme' );
 	}
@@ -49,7 +65,33 @@ class Tests_Blocks_GetBlockTemplates extends WP_UnitTestCase {
 		switch_theme( static::TEST_THEME );
 	}
 
-	public function test_it_returns_unique_templates_and_template_parts() {
+	/**
+	 * Data provider for test_it_returns_unique_entities().
+	 *
+	 * @return array
+	 */
+	public function data_test_it_returns_unique_entities() {
+		return array(
+			'wp_template template type'      => array(
+				'template_type' => 'wp_template',
+				'error_message' => 'get_block_templates() must return unique templates.',
+			),
+			'wp_template_part template type' => array(
+				'template_type' => 'wp_template',
+				'error_message' => 'get_block_templates() must return unique template parts.',
+			),
+		);
+	}
+
+	/**
+	 * @ticket 56271
+	 *
+	 * @dataProvider data_test_it_returns_unique_entities
+	 *
+	 * @param string $post_type The tempalte type.
+	 * @param string $error_message An error message to display if the test fails.
+	 */
+	public function test_it_returns_unique_entities( $template_type, $error_message ) {
 		$templates    = get_block_templates( array(), 'wp_template' );
 		$template_ids = array_map(
 			static function( WP_Block_Template $template ) {
@@ -59,19 +101,6 @@ class Tests_Blocks_GetBlockTemplates extends WP_UnitTestCase {
 		);
 
 		$this->assertNotEmpty( $template_ids, 'get_block_templates() must return a non-empty value.' );
-		$this->assertSame( count( array_unique( $template_ids ) ), count( $template_ids ), 'get_block_templates() must return unique templates.' );
-	}
-
-	public function test_it_returns_unique_template_parts() {
-		$templates    = get_block_templates( array(), 'wp_template_part' );
-		$template_ids = array_map(
-			static function( WP_Block_Template $template ) {
-				return $template->id;
-			},
-			$templates
-		);
-
-		$this->assertNotEmpty( $template_ids, 'get_block_templates() must return a non-empty value.' );
-		$this->assertSame( count( array_unique( $template_ids ) ), count( $template_ids ), 'get_block_templates() must return unique template parts.' );
+		$this->assertSame( count( array_unique( $template_ids ) ), count( $template_ids ), $error_message );
 	}
 }
