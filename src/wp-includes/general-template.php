8eb3629569cdbf67a04e1c615bbd21a9f6bc56c3
Adam Silverstein <adamjs@google.com>
2022-08-03 16:45:33 -0600

    Improve doc block

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -3428,9 +3428,11 @@ function wp_resource_hints() {
  *
  * Gives directive to browsers to preload specific resources that website will
  * need very soon, this ensures that they are available earlier and are less
- * likely to block the page's render.
+ * likely to block the page's render. Preload directives should not be used for
+ * non-render-blocking elements, as then they would compete with the
+ * render-blocking ones, slowing down the render.
  *
- * These performance improving indicators work by using `<link rel"preload">`.
+ * These performance improving indicators work by using `<link rel="preload">`.
  *
  * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/preload
  * @link https://web.dev/preload-responsive-images/
