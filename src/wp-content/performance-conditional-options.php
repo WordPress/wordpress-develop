bcb5dbd931ef70cf929dae1b2b723c3123c9dc1d
Paul Bearne <pbearne@gmail.com>
2022-08-03 17:39:45 -0400

    added test for cache

--- src/wp-content/performance-conditional-options.php
+++ src/wp-content/performance-conditional-options.php
@@ -17,13 +17,15 @@ function performance_conditional_options_options_preload( $pre, $force_cache ) {
 		}
 	}
 
-	if( ! empty( $alloptions_names ) ) {
+	if ( ! empty( $alloptions_names ) ) {
 		return $pre;
 	}
 	$alloptions_names = array();
-
-	$maybe_option_ids = $wpdb->get_results( "SELECT option_value FROM `$wpdb->options` WHERE option_name = '" . performance_conditional_options_get_context() . "'" );
-
+	if ( performance_conditional_has_persistent_caching() ) {
+		$maybe_option_ids = wp_cache_get( performance_conditional_options_get_context(), 'wp_conditional_options' );
+	} else {
+		$maybe_option_ids = $wpdb->get_results( "SELECT option_value FROM `$wpdb->options` WHERE option_name = '" . performance_conditional_options_get_context() . "'" );
+	}
 
 	if ( empty( $maybe_option_ids ) ) {
 		return $pre;
@@ -36,15 +38,15 @@ function performance_conditional_options_options_preload( $pre, $force_cache ) {
 	$alloptions = array();
 	foreach ( (array) $alloptions_db as $o ) {
 		$alloptions[ $o->option_name ] = $o->option_value;
-		$alloptions_names[] = $o->option_name;
+		$alloptions_names[]            = $o->option_name;
 	}
 
 	if ( ! empty( $alloptions ) ) {
 		if ( ! is_array( $alloptions_used ) ) {
 			$alloptions_used = array();
 		}
-
 		wp_cache_add( 'alloptions', $alloptions, 'options' );
+
 		return $alloptions;
 	}
 
@@ -86,19 +88,21 @@ function performance_conditional_options_save_options_cache() {
 
 		$ids = implode( ',', wp_list_pluck( $db_ids, 'option_id' ) );
 
-		$wpdb->replace(
-			$wpdb->options,
-			array(
-				'option_name'  => performance_conditional_options_get_context(),
-				'option_value' => $ids,
-				'autoload'     => 'no',
-			),
-			array( '%s', '%s', '%s' )
-		);
-
-//		update_option( performance_conditional_options_get_context(), 'wp_conditional_options', $ids );
-		//wp_cache_add( performance_conditional_options_get_context(), $ids, 'wp_conditional_options' );
-
+		if ( performance_conditional_has_persistent_caching() ) {
+			wp_cache_add( performance_conditional_options_get_context(), $ids, 'wp_conditional_options', DAY_IN_SECONDS );
+			delete_option( performance_conditional_options_get_context() );
+		} else {
+			$wpdb->replace(
+				$wpdb->options,
+				array(
+					'option_name'  => performance_conditional_options_get_context(),
+					'option_value' => $ids,
+					'autoload'     => 'no',
+				),
+				array( '%s', '%s', '%s' )
+			);
+		}
+		wp_cache_add( 'persistent_test', 'cache_active', 'wp_conditional_options', DAY_IN_SECONDS );
 	}
 }
 add_action( 'shutdown', 'performance_conditional_options_save_options_cache' );
@@ -140,4 +144,14 @@ function performance_conditional_options_get_context() {
 	// TODO: add logged in
 	$coc = md5( $queryied_name );
 	return md5( $coc );
+}
+
+function performance_conditional_has_persistent_caching() {
+	$cached_value = wp_cache_get( 'persistent_test', 'wp_conditional_options' );
+
+	if ( 'cache_active' == $cached_value ) {
+		return true;
+	}
+
+	return false;
 }
