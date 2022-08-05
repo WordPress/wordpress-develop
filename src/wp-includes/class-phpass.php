661f3f64493db92301d1e3801b3619b78a17ee57
hanshenrik <divinity76@gmail.com>
2022-08-05 09:48:55 +0200

    mumblemumble
    doesn't hurt

--- src/wp-includes/class-phpass.php
+++ src/wp-includes/class-phpass.php
@@ -173,7 +173,7 @@ class PasswordHash {
 		$itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 		$output = '$2a$';
-		$output .= chr(ord('0') + $this->iteration_count_log2 / 10);
+		$output .= chr((int)(ord('0') + $this->iteration_count_log2 / 10));
 		$output .= chr((int)(ord('0') + $this->iteration_count_log2 % 10));
 		$output .= '$';
 
