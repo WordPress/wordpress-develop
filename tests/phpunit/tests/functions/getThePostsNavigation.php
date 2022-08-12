c65f5045706c1bdb220820fb60a1de09c26009d4
Andres A <andres@netzstrategen.com>
2022-08-12 15:00:30 +0200

    Addresed PR suggestions.

--- tests/phpunit/tests/functions/getThePostsNavigation.php
+++ tests/phpunit/tests/functions/getThePostsNavigation.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Tests get_the_posts_navigation function
+ * Tests get_the_posts_navigation function.
  *
  * @ticket 55751
- * @since 6.1
+ * @since 6.1.0
  *
  * @covers ::get_the_posts_navigation
  */
@@ -15,22 +15,8 @@ class Tests_Functions_GetThePostsNavigation extends WP_UnitTestCase {
 	 *
 	 * @param WP_UnitTest_Factory $factory
 	 */
-	public static function wpsetupBeforeClass( WP_UnitTest_Factory $factory ) {
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
 		$factory->post->create_many( 3 );
-	}
-
-	/**
-	 * Data provider method for testing `get_the_posts_navigation()`.
-	 *
-	 * @return array
-	 */
-	public function data_get_the_posts_navigation() {
-		return array(
-			array( 1, 1, true, false, false ),
-			array( 1, 3, false, true, false ),
-			array( 1, 2, true, true, false ),
-			array( 3, 1, false, false, true ),
-		);
 	}
 
 	/**
@@ -38,11 +24,11 @@ class Tests_Functions_GetThePostsNavigation extends WP_UnitTestCase {
 	 *
 	 * @dataProvider data_get_the_posts_navigation
 	 *
-	 * @param int       $post_per_page      Posts per page to be queried.
-	 * @param int       $paged_num          Pagination page number.
-	 * @param boolean   $assert_older_posts Assert older posts nav string.
-	 * @param boolean   $assert_newer_posts Assert newer posts nav string.
-	 * @param boolean   $assert_empty       Assert empty posts nav string.
+	 * @param int    $post_per_page      Posts per page to be queried.
+	 * @param int    $paged_num          Pagination page number.
+	 * @param bool   $assert_older_posts Assert older posts nav string.
+	 * @param bool   $assert_newer_posts Assert newer posts nav string.
+	 * @param bool   $assert_empty       Assert empty posts nav string.
 	 */
 	public function test_get_the_posts_navigation( $post_per_page, $paged_num, $assert_older_posts, $assert_newer_posts, $assert_empty ) {
 		global $wp_query, $paged;
@@ -55,17 +41,58 @@ class Tests_Functions_GetThePostsNavigation extends WP_UnitTestCase {
 			)
 		);
 
+		$actual = get_the_posts_navigation();
+
 		if ( $assert_older_posts ) {
-			$this->assertStringContainsString( 'Older posts', get_the_posts_navigation(), 'Posts navigation must contain string Older posts.' );
+			$this->assertStringContainsString( 'Older posts', $actual, 'Posts navigation must contain string Older posts.' );
 		}
 
 		if ( $assert_newer_posts ) {
-			$this->assertStringContainsString( 'Newer posts', get_the_posts_navigation(), 'Posts navigation must contain string Newer posts.' );
+			$this->assertStringContainsString( 'Newer posts', $actual, 'Posts navigation must contain string Newer posts.' );
 		}
 
 		if ( $assert_empty ) {
-			$this->assertEmpty( get_the_posts_navigation(), 'Posts navigation must return empty string.' );
+			$this->assertEmpty( $actual, 'Posts navigation must return empty string.' );
 		}
 
 	}
+
+	/**
+	 * Data provider method for testing `get_the_posts_navigation()`.
+	 *
+	 * @return array
+	 */
+	public function data_get_the_posts_navigation() {
+		return array(
+			'Assert Older posts navigation string' => array(
+				'post_per_page'      => 1,
+				'paged_num'          => 1,
+				'assert_older_posts' => true,
+				'assert_newer_posts' => false,
+				'assert_empty'       => false,
+			),
+			'Assert Newer posts navigation string' => array(
+				'post_per_page'      => 1,
+				'paged_num'          => 3,
+				'assert_older_posts' => false,
+				'assert_newer_posts' => true,
+				'assert_empty'       => false,
+			),
+			'Assert Newer posts and Older navigation string' => array(
+				'post_per_page'      => 1,
+				'paged_num'          => 2,
+				'assert_older_posts' => true,
+				'assert_newer_posts' => true,
+				'assert_empty'       => false,
+			),
+			'Empty navigation string'              => array(
+				'post_per_page'      => 3,
+				'paged_num'          => 1,
+				'assert_older_posts' => false,
+				'assert_newer_posts' => false,
+				'assert_empty'       => true,
+			),
+		);
+	}
+
 }
