531f696ff8af16e8b93ca22aa2794a1acf33f4f5
costdev <me@colin-stewart.co.uk>
2022-08-17 21:44:42 +0100

    Add `exact_search` to `test_registered_query_params()` tests.

--- tests/phpunit/tests/rest-api/rest-posts-controller.php
+++ tests/phpunit/tests/rest-api/rest-posts-controller.php
@@ -183,6 +183,7 @@ class WP_Test_REST_Posts_Controller extends WP_Test_REST_Post_Type_Controller_Te
 				'categories',
 				'categories_exclude',
 				'context',
+				'exact_search',
 				'exclude',
 				'include',
 				'modified_after',
