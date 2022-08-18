9e7e2a1686515a37dbb51238cfd5b2341174cf0f
Mukesh Panchal <mukeshpanchal27@gmail.com>
2022-08-18 18:56:24 +0530

    Fix copy JS issue

--- tests/phpunit/tests/media.php
+++ tests/phpunit/tests/media.php
@@ -3869,14 +3869,11 @@ EOF;
 		$GLOBALS['_wp_image_mime_fallback_should_load'] = true;
 
 		// `wp_print_image_mime_fallback_script()` assumes `wp-includes/js/wp-image-mime-fallback-loader.js` is present:
-		copy(
-			ABSPATH . 'js/_enqueues/wp/image-mime-fallback-loader.js',
-			ABSPATH . WPINC . '/js/wp-image-mime-fallback-loader.js'
-		);
+		self::touch( ABSPATH . WPINC . '/js/wp-image-mime-fallback-loader.js' );
 
 		$output = get_echo( 'wp_print_image_mime_fallback_script' );
 		$this->assertNotSame( '', $output );
-		$this->assertStringContainsString( 'data:image/webp;base64,UklGR', $output );
+		$this->assertStringContainsString( 'window._wpImageMimeFallbackSettings', $output );
 	}
 }
 
