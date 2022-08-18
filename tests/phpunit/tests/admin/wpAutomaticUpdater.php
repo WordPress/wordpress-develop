2722e147d397bdeaa9efce7cd143d8fce005a2d4
costdev <me@colin-stewart.co.uk>
2022-08-18 01:49:04 +0100

    Switch to `wpSetUpBeforeClass()`.
    This method is used more widely in the test suite.

--- tests/phpunit/tests/admin/wpAutomaticUpdater.php
+++ tests/phpunit/tests/admin/wpAutomaticUpdater.php
@@ -24,9 +24,7 @@ class Tests_Admin_WpAutomaticUpdater extends WP_UnitTestCase {
 	/**
 	 * Sets up shared fixtures.
 	 */
-	public static function set_up_before_class() {
-		parent::set_up_before_class();
-
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
 		require_once ABSPATH . 'wp-admin/includes/class-wp-automatic-updater.php';
 		self::$updater = new WP_Automatic_Updater();
 
