2bfda1b92d36e2393ecce26b1774f066746513fc
Robin <robin@wpdeveloper.com>
2022-08-25 15:11:25 +0600

    @since 6.1.0 note added

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -7905,7 +7905,8 @@ function _update_term_count_on_transition_post_status( $new_status, $old_status,
  * Adds any posts from the given IDs to the cache that do not already exist in cache.
  *
  * @since 3.4.0
- *
+ * @since 6.1.0 This function is no longer marked as "private".
+ * 
  * @see update_post_caches()
  *
  * @global wpdb $wpdb WordPress database abstraction object.
