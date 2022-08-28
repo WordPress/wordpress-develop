5945f30c86593d0e1147d76826b3b6325c772370
costdev <me@colin-stewart.co.uk>
2022-08-28 06:40:08 +0100

    Tests: Replace `&` with `&#038;` to fix failures.

--- tests/phpunit/tests/admin/wpPostCommentsListTable.php
+++ tests/phpunit/tests/admin/wpPostCommentsListTable.php
@@ -27,7 +27,7 @@ class Tests_Admin_wpPostCommentsListTable extends WP_UnitTestCase {
 
 		$expected = array(
 			'all'       => '<a href="http://example.org/wp-admin/edit-comments.php?comment_status=all" class="current" aria-current="page">All <span class="count">(<span class="all-count">0</span>)</span></a>',
-			'mine'      => '<a href="http://example.org/wp-admin/edit-comments.php?comment_status=mine&user_id=0">Mine <span class="count">(<span class="mine-count">0</span>)</span></a>',
+			'mine'      => '<a href="http://example.org/wp-admin/edit-comments.php?comment_status=mine&#038;user_id=0">Mine <span class="count">(<span class="mine-count">0</span>)</span></a>',
 			'moderated' => '<a href="http://example.org/wp-admin/edit-comments.php?comment_status=moderated">Pending <span class="count">(<span class="pending-count">0</span>)</span></a>',
 			'approved'  => '<a href="http://example.org/wp-admin/edit-comments.php?comment_status=approved">Approved <span class="count">(<span class="approved-count">0</span>)</span></a>',
 			'spam'      => '<a href="http://example.org/wp-admin/edit-comments.php?comment_status=spam">Spam <span class="count">(<span class="spam-count">0</span>)</span></a>',
