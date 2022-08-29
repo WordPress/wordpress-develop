affaf94bfad3fa37265e5a1e763179ad09e7dca6
Bernie Reiter <ockham@raz.or.at>
2022-08-29 22:13:16 +0200

    Trailing comma

--- src/wp-content/themes/twentytwentytwo/comments.php
+++ src/wp-content/themes/twentytwentytwo/comments.php
@@ -61,6 +61,6 @@ if ( post_password_required() ) { ?>
 <?php comment_form(
 	array(
 		'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-		'title_reply_after'  => '</h2>'
+		'title_reply_after'  => '</h2>',
 	)
 ); ?>
