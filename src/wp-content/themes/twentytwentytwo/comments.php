1f213db60a45255a2f36f995dbcea2c2d61149cc
Bernie Reiter <ockham@raz.or.at>
2022-08-29 17:41:18 +0200

    Remove deprecation notice

--- src/wp-content/themes/twentytwentytwo/comments.php
+++ src/wp-content/themes/twentytwentytwo/comments.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * @package WordPress
- * @subpackage Theme_Compat
- * @deprecated 3.0.0
- *
- * This file is here for backward compatibility with old themes and will be removed in a future version
- */
-_deprecated_file(
-	/* translators: %s: Template name. */
-	sprintf( __( 'Theme without %s' ), basename( __FILE__ ) ),
-	'3.0.0',
-	null,
-	/* translators: %s: Template name. */
-	sprintf( __( 'Please include a %s template in your theme.' ), basename( __FILE__ ) )
-);
-
 // Do not delete these lines.
 if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && 'comments.php' === basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
 	die( 'Please do not load this page directly. Thanks!' );
