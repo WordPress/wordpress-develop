c52080cad5c7224f167ab70834c4201de20063cf
John James Jacoby <johnjamesjacoby@me.com>
2022-08-31 10:15:49 -0500

    Update tests/phpunit/tests/option/networkOption.php
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- tests/phpunit/tests/option/networkOption.php
+++ tests/phpunit/tests/option/networkOption.php
@@ -215,6 +215,7 @@ class Tests_Option_NetworkOption extends WP_UnitTestCase {
 
 	/**
 	 * @ticket 37181
+	 *
 	 * @group ms-required
 	 */
 	public function test_register_meta_network_option_single_false() {
