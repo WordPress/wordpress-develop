959346612331bee27659176423cb86accec3ad7c
Adam Silverstein <adamjs@google.com>
2022-09-01 14:45:11 -0600

    missing space for phpcs

--- tests/phpunit/tests/image/editorGd.php
+++ tests/phpunit/tests/image/editorGd.php
@@ -17,7 +17,7 @@ class Tests_Image_Editor_GD extends WP_Image_UnitTestCase {
 		require_once ABSPATH . WPINC . '/class-wp-image-editor.php';
 		require_once ABSPATH . WPINC . '/class-wp-image-editor-gd.php';
 
-		add_filter('image_editor_output_format', '__return_empty_array' );
+		add_filter( 'image_editor_output_format', '__return_empty_array' );
 
 		// This needs to come after the mock image editor class is loaded.
 		parent::set_up();
