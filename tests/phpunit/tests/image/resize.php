6a67bbaf130042e2c84cac1a379ec5c1fb65ca83
Adam Silverstein <adamjs@google.com>
2022-09-02 11:02:52 -0600

    remove output mapping for resize tests

--- tests/phpunit/tests/image/resize.php
+++ tests/phpunit/tests/image/resize.php
@@ -14,6 +14,7 @@ abstract class WP_Tests_Image_Resize_UnitTestCase extends WP_Image_UnitTestCase 
 		parent::set_up();
 
 		add_filter( 'wp_image_editors', array( $this, 'wp_image_editors' ) );
+		add_filter( 'image_editor_output_format', '__return_empty_array' );
 	}
 
 	public function wp_image_editors() {
