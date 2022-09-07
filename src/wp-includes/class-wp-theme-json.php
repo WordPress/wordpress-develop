49efa0b9e0d2c545034a32ba94ec2c4b593eedd6
MaggieCabrera <maggie.cabrera@automattic.com>
2022-09-07 09:16:25 +0200

    Theme Json: Don't output double selectors for elements inside blocks #40889
    Co-authored-by: Ben Dwyer <ben@scruffian.com>

--- src/wp-includes/class-wp-theme-json.php
+++ src/wp-includes/class-wp-theme-json.php
@@ -611,6 +611,10 @@ class WP_Theme_JSON {
 			foreach ( static::ELEMENTS as $el_name => $el_selector ) {
 				$element_selector = array();
 				foreach ( $block_selectors as $selector ) {
+					if ( $selector === $el_selector ) {
+						$element_selector = array( $el_selector );
+						break;
+					}
 					$element_selector[] = $selector . ' ' . $el_selector;
 				}
 				static::$blocks_metadata[ $block_name ]['elements'][ $el_name ] = implode( ',', $element_selector );
