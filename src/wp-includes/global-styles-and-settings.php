c5b8388cc186075389dd38decdc01af92d821450
MaggieCabrera <maggie.cabrera@automattic.com>
2022-09-07 16:49:17 +0200

    moved gs function to correct file

--- src/wp-includes/global-styles-and-settings.php
+++ src/wp-includes/global-styles-and-settings.php
@@ -192,3 +192,23 @@ function wp_get_global_styles_svg_filters() {
 
 	return $svgs;
 }
+
+/**
+ * Adds global style rules to the inline style for each block.
+ * 
+ * @since 6.1.0
+ * 
+ */
+function wp_add_global_styles_for_blocks() {
+	$tree = WP_Theme_JSON_Resolver::get_merged_data();
+	// TODO some nodes dont have a name...
+	$block_nodes = $tree->get_styles_block_nodes();
+	foreach ( $block_nodes as $metadata ) {
+		$block_css  = $tree->get_styles_for_block( $metadata );
+		$block_name = str_replace( 'core/', '', $metadata['name'] );
+		// These block styles are added on block_render.
+		// This hooks inline CSS to them so that they are loaded conditionally
+		// based on whether or not the block is used on the page.
+		wp_add_inline_style( 'wp-block-' . $block_name, $block_css );
+	}
+}
