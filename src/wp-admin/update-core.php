98bdc8dfb3cefb71baa1758a6fd0718901498f24
Robin <robin@wpdeveloper.com>
2022-09-08 13:59:59 +0600

    Security notice added for 4.0 or older version

--- src/wp-admin/update-core.php
+++ src/wp-admin/update-core.php
@@ -269,6 +269,17 @@ function core_upgrade_preamble() {
 			__( 'https://wordpress.org/support/article/updating-wordpress/' )
 		);
 		echo '</p></div>';
+
+		if( ! empty( $wp_version ) && $wp_version <= 4.0 ){
+			echo '<div class="notice notice-warning inline"><p>';
+			printf(
+				/* translators: 1: Documentation on WordPress backups, 2: Documentation on updating WordPress. */
+				__( '<strong>Important:</strong> Your version of WordPress (%1$s) will stop receiving security updates in the near future. To keep your site secure, your site should use WordPress 4.1 or later. However, it\'s recommended to <a href="%2$s">update to the latest version</a> of WordPress.' ),
+				__( esc_attr( $wp_version ) ),
+				__( 'https://wordpress.org/support/article/updating-wordpress/' )
+			);
+			echo '</p></div>';
+		}
 	} elseif ( $is_development_version ) {
 		echo '<h2 class="response">' . __( 'You are using a development version of WordPress.' ) . '</h2>';
 	} else {
