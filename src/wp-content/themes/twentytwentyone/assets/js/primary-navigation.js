a1cc9e00297c20d2aad5153d1c702d757af81ef1
Carolina Nymark <hi@themesbycarolina.com>
2022-09-09 14:00:15 +0200

    Update primary-navigation.js

--- src/wp-content/themes/twentytwentyone/assets/js/primary-navigation.js
+++ src/wp-content/themes/twentytwentyone/assets/js/primary-navigation.js
@@ -172,20 +172,18 @@ function twentytwentyoneExpandSubMenu( el ) { // jshint ignore:line
 		 *
 		 * @since Twenty Twenty-One 1.1
 		 */
-		document.addEventListener( 'click', function( event ) {
-			if ( document.getElementById( 'site-navigation' ).contains( event.target ) ) {
-				// If target onclick is <a> with # within the href attribute
-				if ( event.target.hash && event.target.hash.includes( '#' ) ) {
-					wrapper.classList.remove( id + '-navigation-open', 'lock-scrolling' );
-					twentytwentyoneToggleAriaExpanded( mobileButton );
-					// Wait 550 and scroll to the anchor.
-					setTimeout(function () {
-						var anchor = document.getElementById(event.target.hash.slice(1));
-						if ( anchor ) {
-							anchor.scrollIntoView();
-						}
-					}, 550);
-				}
+		document.getElementById( 'site-navigation' ).addEventListener( 'click', function( event ) {
+			// If target onclick is <a> with # within the href attribute
+			if ( event.target.hash ) {
+				wrapper.classList.remove( id + '-navigation-open', 'lock-scrolling' );
+				twentytwentyoneToggleAriaExpanded( mobileButton );
+				// Wait 550 and scroll to the anchor.
+				setTimeout(function () {
+					var anchor = document.getElementById(event.target.hash.slice(1));
+					if ( anchor ) {
+						anchor.scrollIntoView();
+					}
+				}, 550);
 			}
 		} );
 
