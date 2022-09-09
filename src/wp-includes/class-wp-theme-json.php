c4c6059a685a770430778f3e1733759635b4acd1
MaggieCabrera <maggie.cabrera@automattic.com>
2022-09-09 10:51:23 +0200

    Styles API: Fixed selectors for nested elements #43988

--- src/wp-includes/class-wp-theme-json.php
+++ src/wp-includes/class-wp-theme-json.php
@@ -685,9 +685,9 @@ class WP_Theme_JSON {
 						$element_selector = array( $el_selector );
 						break;
 					}
-					$element_selector = static::append_to_selector( $el_selector, $selector . ' ', 'left' );
+					$element_selector[] = static::append_to_selector( $el_selector, $selector . ' ', 'left' );
 				}
-				static::$blocks_metadata[ $block_name ]['elements'][ $el_name ] = $element_selector;
+				static::$blocks_metadata[ $block_name ]['elements'][ $el_name ] = implode( ',', $element_selector );
 			}
 		}
 
