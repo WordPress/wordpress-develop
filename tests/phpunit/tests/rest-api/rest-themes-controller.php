477a1a8f7f44791fd55fb805721f91f6718d2517
hellofromtonya <tonya.mork@automattic.com>
2022-09-14 12:34:37 -0500

    PR 3205 on top of trunk.

--- tests/phpunit/tests/rest-api/rest-themes-controller.php
+++ tests/phpunit/tests/rest-api/rest-themes-controller.php
@@ -396,6 +396,7 @@ class WP_Test_REST_Themes_Controller extends WP_Test_REST_Controller_Testcase {
 		$this->assertArrayHasKey( 'dark-editor-style', $theme_supports );
 		$this->assertArrayHasKey( 'disable-custom-font-sizes', $theme_supports );
 		$this->assertArrayHasKey( 'disable-custom-gradients', $theme_supports );
+		$this->assertArrayHasKey( 'disable-layout-styles', $theme_supports );
 		$this->assertArrayHasKey( 'editor-color-palette', $theme_supports );
 		$this->assertArrayHasKey( 'editor-font-sizes', $theme_supports );
 		$this->assertArrayHasKey( 'editor-gradient-presets', $theme_supports );
@@ -406,7 +407,7 @@ class WP_Test_REST_Themes_Controller extends WP_Test_REST_Controller_Testcase {
 		$this->assertArrayHasKey( 'responsive-embeds', $theme_supports );
 		$this->assertArrayHasKey( 'title-tag', $theme_supports );
 		$this->assertArrayHasKey( 'wp-block-styles', $theme_supports );
-		$this->assertCount( 21, $theme_supports );
+		$this->assertCount( 22, $theme_supports );
 	}
 
 	/**
