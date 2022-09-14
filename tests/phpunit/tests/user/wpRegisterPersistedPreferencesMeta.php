94e20f87582f0858c03ced223044151f1a91a90a
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-09-14 15:11:52 -0700

    Add unit tests for persisted preferences registration

---
+++ tests/phpunit/tests/user/wpRegisterPersistedPreferencesMeta.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @group user
+ *
+ * @covers wp_register_persisted_preferences_meta
+ */
+class Tests_User_wpRegisterPersistedPreferencesMeta extends WP_UnitTestCase {
+
+	/**
+	 * Test that user persisted preferences meta is registered.
+	 *
+	 * @ticket 56467
+	 */
+	public function test_should_register_persisted_preferences_meta() {
+		global $wpdb, $wp_meta_keys;
+		$meta_key = $wpdb->get_blog_prefix() . 'persisted_preferences';
+
+		// Test that meta key is registered.
+		unregister_meta_key( 'user', $meta_key );
+		wp_register_persisted_preferences_meta();
+
+		$this->assertIsArray( $wp_meta_keys, 'No meta keys exist' );
+		$this->assertArrayHasKey(
+			$meta_key,
+			$wp_meta_keys['user'][''],
+			'The expected meta key was not registered'
+		);
+
+		// Test to detect changes in meta key structure.
+		$structure = $wp_meta_keys['user'][''][ $meta_key ];
+
+		$this->assertSame(
+			array(
+				'type'              => 'object',
+				'description'       => '',
+				'single'            => true,
+				'sanitize_callback' => null,
+				'auth_callback'     => '__return_true',
+				'show_in_rest'      => array(
+					'name'    => 'persisted_preferences',
+					'type'    => 'object',
+					'context' => array( 'edit' ),
+					'schema'  => array(
+						'type'                 => 'object',
+						'properties'           => array(
+							'_modified' => array(
+								'description' => __( 'The date and time the preferences were updated.' ),
+								'type'        => 'string',
+								'format'      => 'date-time',
+								'readonly'    => false,
+							),
+						),
+						'additionalProperties' => true,
+					),
+				),
+			),
+			$structure,
+			'The registered metadata did not have the expected structure'
+		);
+	}
+
+}
