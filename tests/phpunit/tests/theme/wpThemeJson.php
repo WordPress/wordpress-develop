6351f4e9a6101b1bfdaa1e9c8ca93230acf0dbbb
Glen Davies <glen.davies@automattic.com>
2022-09-15 17:02:25 +1200

    Appease the linter

--- tests/phpunit/tests/theme/wpThemeJson.php
+++ tests/phpunit/tests/theme/wpThemeJson.php
@@ -3496,7 +3496,7 @@ class Tests_Theme_wpThemeJson extends WP_UnitTestCase {
 
 	/**
 	 * @dataProvider data_generate_spacing_scale_fixtures
-	 * 
+	 *
 	 * @ticket 56467
 	 *
 	 * Tests generating the spacing presets array based on the spacing scale provided.
@@ -3737,7 +3737,7 @@ class Tests_Theme_wpThemeJson extends WP_UnitTestCase {
 
 	/**
 	 * @dataProvider data_set_spacing_sizes_when_invalid
-	 * 
+	 *
 	 * @ticket 56467
 	 *
 	 * Tests generating the spacing presets array based on the spacing scale provided.
