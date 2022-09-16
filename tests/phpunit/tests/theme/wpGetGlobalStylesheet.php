36bf56f625c7674afa1f65095c9b1b6fdc30e18b
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-09-16 11:18:25 +1000

    Update tests so that expected output depends on SCRIPT_DEBUG value

--- tests/phpunit/tests/theme/wpGetGlobalStylesheet.php
+++ tests/phpunit/tests/theme/wpGetGlobalStylesheet.php
@@ -256,18 +256,64 @@ class Tests_Theme_wpGetGlobalStylesheet extends WP_UnitTestCase {
 			)
 		);
 
+		// If `SCRIPT_DEBUG` is defined as true, style output will be prettified.
 		wp_enqueue_stored_styles();
 
-		$this->assertEquals(
-			array( '.saruman{color:white;height:100px;border-style:solid;}' ),
-			wp_styles()->registered['core-block-supports']->extra['after'],
-			'Registered styles with handle of "core-block-supports" do not match expected value from Style Engine store.'
-		);
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+			// In `-src` versions of WordPress, `defaults-constants.php` will set SCRIPT_DEBUG to true.
+			$this->assertEquals(
+				array(
+					implode(
+						'',
+						array(
+							"/**\n",
+							" * Core styles: block-supports\n",
+							" */\n",
+							".saruman {\n",
+							"	color: white;\n",
+							"	height: 100px;\n",
+							"	border-style: solid;\n",
+							"}\n",
+						)
+					)
+				),
+				wp_styles()->registered['core-block-supports']->extra['after'],
+				'Registered styles with handle of "core-block-supports" do not match expected value from Style Engine store.'
+			);			
+		} else {
+			// In production versions of WordPress, `defaults-constants.php` will set SCRIPT_DEBUG to false.
+			$this->assertEquals(
+				array( '.saruman{color:white;height:100px;border-style:solid;}' ),
+				wp_styles()->registered['core-block-supports']->extra['after'],
+				'Registered styles with handle of "core-block-supports" do not match expected value from Style Engine store.'
+			);
+		}
 
-		$this->assertEquals(
-			array( '.gandalf{color:grey;height:90px;border-style:dotted;}' ),
-			wp_styles()->registered['wp-style-engine-my-styles']->extra['after'],
-			'Registered styles with handle of "wp-style-engine-my-styles" do not match expected value from the Style Engine store.'
-		);
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+			// In `-src` versions of WordPress, `defaults-constants.php` will set SCRIPT_DEBUG to true.
+			$this->assertEquals(
+				array(
+					implode(
+						'',
+						array(
+							".gandalf {\n",
+							"	color: grey;\n",
+							"	height: 90px;\n",
+							"	border-style: dotted;\n",
+							"}\n"
+						)
+					)
+				),
+				wp_styles()->registered['wp-style-engine-my-styles']->extra['after'],
+				'Registered styles with handle of "core-block-supports" do not match expected value from Style Engine store.'
+			);			
+		} else {
+			// In production versions of WordPress, `defaults-constants.php` will set SCRIPT_DEBUG to false.
+			$this->assertEquals(
+				array( '.gandalf{color:grey;height:90px;border-style:dotted;}' ),
+				wp_styles()->registered['wp-style-engine-my-styles']->extra['after'],
+				'Registered styles with handle of "wp-style-engine-my-styles" do not match expected value from the Style Engine store.'
+			);
+		}
 	}
 }
