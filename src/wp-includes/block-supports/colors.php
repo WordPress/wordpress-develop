9f2682c910eb4ecc31ba4e64d904a01ae0a083ff
Jb Audras <audrasjb@gmail.com>
2022-09-19 21:00:08 +0200

    Update colors.php
    better consistency for `since` mentions.

--- src/wp-includes/block-supports/colors.php
+++ src/wp-includes/block-supports/colors.php
@@ -10,7 +10,7 @@
  * Registers the style and colors block attributes for block types that support it.
  *
  * @since 5.6.0
- * @since 6.1.0 Optimizing $color_support assignment.
+ * @since 6.1.0 Improved $color_support assignment optimization.
  * @access private
  *
  * @param WP_Block_Type $block_type Block Type.
