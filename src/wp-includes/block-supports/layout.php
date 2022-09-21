0099959174adbd77f9c8590391be5ee8782393db
Jb Audras <audrasjb@gmail.com>
2022-09-21 12:29:58 +0200

    `return` mention improvement
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- src/wp-includes/block-supports/layout.php
+++ src/wp-includes/block-supports/layout.php
@@ -44,7 +44,7 @@ function wp_register_layout_support( $block_type ) {
  * @param bool    $should_skip_gap_serialization Whether to skip applying the user-defined value set in the editor.
  * @param string  $fallback_gap_value            The block gap value to apply.
  * @param array   $block_spacing                 Custom spacing set on the block.
- * @return string CSS style.
+ * @return string CSS style or an empty string.
  */
 function wp_get_layout_style( $selector, $layout, $has_block_gap_support = false, $gap_value = null, $should_skip_gap_serialization = false, $fallback_gap_value = '0.5em', $block_spacing = null ) {
 	$layout_type   = isset( $layout['type'] ) ? $layout['type'] : 'default';
