c30457f6b4b34677a3fab8b9290e1a300b9c5fd5
Valter Lorran <valter.lorran@automattic.com>
2022-09-23 13:41:33 -0300

    Adding command to cli

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -5,10 +5,3 @@ apk add nodejs npm
 npm install
 # Run npm build
 npm run build:dev
-# Creates the wp-config.php file from docker-compose
-docker-compose run --rm cli config create \
-    --dbname=wordpress_develop \
-    --dbuser=root \
-    --dbpass=password \
-    --dbhost=mysql \
-    --dbprefix=wp_ 
