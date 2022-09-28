06db015b69dd2ac53a9380852ffeee6c40229c88
Paul Bearne <pbearne@gmail.com>
2022-09-28 19:51:10 -0400

    whitespace

--- tests/phpunit/tests/functions/wpRefererFeld.php
+++ tests/phpunit/tests/functions/wpRefererFeld.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @group functions.php
  *
@@ -6,37 +7,37 @@
  */
 class Tests_Functions_wpRefererField extends WP_UnitTestCase {
 
-/**
- * Tests that the echo argument is respected.
- *
- * @ticket 54106
- *
- * @dataProvider data_wp_referer_field_should_respect_echo_arg
- *
- * @param mixed $echo Whether to echo or return the referer field.
- */
- public function test_wp_referer_field_should_respect_echo_arg( $echo ) {
-	$actual = $echo ? get_echo( 'wp_referer_field' ) ? wp_referer_field( false );
-    
-        $this->assertSame( '<input type="hidden" name="_wp_http_referer" value="" />', $actual );
-}
- 
-/**
- * Data provider for test_wp_referer_field_should_respect_echo_arg().
- *
- * @return array
- */
- public function data_wp_referer_field_should_respect_echo_arg() {
-    return array(
-        'true'         => array( true ),
-        '(int) 1'      => array( 1 ),
-        '(string) "1"' => array( '1' ),
-        'false'        => array( false ),
-        'null'         => array( null ),
-        '(int) 0'      => array( 0 ),
-        '(string) "0"' => array( '0' ),
-    );
-}
+	/**
+	 * Tests that the echo argument is respected.
+	 *
+	 * @ticket 54106
+	 *
+	 * @dataProvider data_wp_referer_field_should_respect_echo_arg
+	 *
+	 * @param mixed $echo Whether to echo or return the referer field.
+	 */
+	public function test_wp_referer_field_should_respect_echo_arg( $echo ) {
+		$actual = $echo ? get_echo( 'wp_referer_field' ) ? wp_referer_field( false );
+
+		$this->assertSame( '<input type="hidden" name="_wp_http_referer" value="" />', $actual );
+	}
+
+	/**
+	 * Data provider for test_wp_referer_field_should_respect_echo_arg().
+	 *
+	 * @return array
+	 */
+	public function data_wp_referer_field_should_respect_echo_arg() {
+		return array(
+			'true'         => array( true ),
+			'(int) 1'      => array( 1 ),
+			'(string) "1"' => array( '1' ),
+			'false'        => array( false ),
+			'null'         => array( null ),
+			'(int) 0'      => array( 0 ),
+			'(string) "0"' => array( '0' ),
+		);
+	}
 
 	/**
 	 * @ticket 54106
