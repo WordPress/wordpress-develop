a76051820804519459a4e896f363e7baf19fdbb9
Valter Lorran <valter.lorran@automattic.com>
2022-10-07 16:55:52 -0300

    Fix sed command

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -12,7 +12,7 @@ CODESPACE_NAME=$(jq -r ".CODESPACE_NAME" /workspaces/.codespaces/shared/environm
 echo "Creating wp-config.php..."
 WP_CONFIG_CODESPACES=".devcontainer/wp-config-codespaces.php"
 cp $WP_CONFIG_CODESPACES wp-config.php
-sed -i 's/#HOST#/test/' wp-config.php
+sed -i "s/#HOST#/$CODESPACE_NAME/g" wp-config.php
 
 # Install nodejs and npm on the Alpine Linux image
 echo "Installing nodejs and npm..."
