95164e3c83c12fe78b189f3fb312a9b9959567b9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-10 21:16:58 +0300

    Update one more DocBlock

--- tests/phpunit/includes/object-cache.php
+++ tests/phpunit/includes/object-cache.php
@@ -289,8 +289,9 @@ function wp_cache_flush( $delay = 0 ) {
  *
  * @since 6.1.0
  *
- * @param string $feature Name of the feature to check for.
- *                        Possible values include: 'flush_group'.
+ * @param string $feature Name of the feature to check for. Possible values include:
+ *                        'add_multiple', 'set_multiple', 'get_multiple', 'delete_multiple',
+ *                        'flush_runtime', 'flush_group'.
  * @return bool True if the feature is supported, false otherwise.
  */
 function wp_cache_supports( $feature ) {
