c9a48159115db54aaf837e721b43af99fcfdf8f7
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2022-10-17 19:38:29 +0000

    WordPress 3.8.40.
    git-svn-id: https://develop.svn.wordpress.org/branches/3.8@54576 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.8.39-src';
+$wp_version = '3.8.40-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
