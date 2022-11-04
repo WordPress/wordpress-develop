10b12a67d0777fd309aa94436467ce023146ce81
Marie Comet <mariecomet666@gmail.com>
2022-11-04 16:14:29 +0100

    Fix description for default archive template is not accurate
    See https://core.trac.wordpress.org/ticket/57001

--- src/wp-includes/block-template-utils.php
+++ src/wp-includes/block-template-utils.php
@@ -139,7 +139,7 @@ function get_default_block_template_types() {
 		),
 		'archive'        => array(
 			'title'       => _x( 'Archive', 'Template name' ),
-			'description' => __( 'Displays post categories, tags, and other archives.' ),
+			'description' => __( 'Displays posts by categories, tags, and other archives.' ),
 		),
 		'author'         => array(
 			'title'       => _x( 'Author', 'Template name' ),
