5ed9e45441d7577f7716c0a037a8739918e82091
Dion Hulse <contact@dd32.id.au>
2022-11-04 13:18:42 +1000

    Add a changelog entry for the behaviour change.

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -1873,6 +1873,7 @@ function update_user_caches( $user ) {
  *
  * @since 3.0.0
  * @since 4.4.0 'clean_user_cache' action was added.
+ * @since 6.2.0 User metadata caches are now cleared.
  *
  * @param WP_User|int $user User object or ID to be cleaned from the cache
  */
