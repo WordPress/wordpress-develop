a27f0a6a126b6a186422e1d5c41de7057f4bb1a8
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-11-09 15:50:26 -0700

    Remove "we", simplify

--- src/wp-settings.php
+++ src/wp-settings.php
@@ -60,7 +60,7 @@ global $blog_id;
 // Set initial default constants including WP_MEMORY_LIMIT, WP_MAX_MEMORY_LIMIT, WP_DEBUG, SCRIPT_DEBUG, WP_CONTENT_DIR and WP_CACHE.
 wp_initial_constants();
 
-// Make sure we register the shutdown handler for fatal errors as soon as possible.
+// Register the shutdown handler for fatal errors as soon as possible.
 wp_register_fatal_error_handler();
 
 // WordPress calculates offsets from UTC.
