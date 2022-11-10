ca54bdbfe4c8f092f26ef8b5c12ae5eb5e2ee7eb
Martin Krcho <1962663+martinkrcho@users.noreply.github.com>
2022-11-10 09:27:11 +0100

    Code formatting adjustment.

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -5017,7 +5017,7 @@ function map_deep( $value, $callback ) {
 	} elseif ( is_object( $value ) ) {
 		$object_vars = get_object_vars( $value );
 		foreach ( $object_vars as $property_name => $property_value ) {
-			if (ord($property_name) === 0) {
+			if ( 0 === ord( $property_name ) ) {
 				continue;
 			}
 			$value->$property_name = map_deep( $property_value, $callback );
