0462aa01d45774d93856d5e3bd5b0fa265ea625f
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-11-09 17:08:53 -0700

    Remove "we" and clarify

--- src/wp-admin/install-helper.php
+++ src/wp-admin/install-helper.php
@@ -102,7 +102,7 @@ if ( ! function_exists( 'maybe_add_column' ) ) :
 		// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared -- No applicable variables for this query.
 		$wpdb->query( $create_ddl );
 
-		// We cannot directly tell whether this succeeded!
+		// Check if column was created.
 		// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared -- Cannot be prepared. Fetches columns for table names.
 		foreach ( $wpdb->get_col( "DESC $table_name", 0 ) as $column ) {
 			if ( $column === $column_name ) {
