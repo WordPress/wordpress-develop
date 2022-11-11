e1f2eadbaef42d91deddf088f72fa8ced7f24d1c
Paul Kevan <paul.kevan@automattic.com>
2022-11-11 00:43:11 +0000

    Specify empty string

--- src/wp-includes/class-wp-error.php
+++ src/wp-includes/class-wp-error.php
@@ -103,7 +103,7 @@ class WP_Error {
 	 *
 	 * @since 2.1.0
 	 *
-	 * @param string|int $code Optional. Retrieve messages matching code, if exists. Default empty.
+	 * @param string|int $code Optional. Retrieve messages matching code, if exists. Default empty string.
 	 * @return string[] Error strings on success, or empty array if there are none.
 	 */
 	public function get_error_messages( $code = '' ) {
@@ -132,7 +132,7 @@ class WP_Error {
 	 *
 	 * @since 2.1.0
 	 *
-	 * @param string|int $code Optional. Error code to retrieve message. Default empty.
+	 * @param string|int $code Optional. Error code to retrieve message. Default empty string.
 	 * @return string The error message.
 	 */
 	public function get_error_message( $code = '' ) {
@@ -151,7 +151,7 @@ class WP_Error {
 	 *
 	 * @since 2.1.0
 	 *
-	 * @param string|int $code Optional. Error code. Default empty.
+	 * @param string|int $code Optional. Error code. Default empty string.
 	 * @return mixed Error data, if it exists.
 	 */
 	public function get_error_data( $code = '' ) {
@@ -185,7 +185,7 @@ class WP_Error {
 	 *
 	 * @param string|int $code    Error code.
 	 * @param string     $message Error message.
-	 * @param mixed      $data    Optional. Error data. Default empty.
+	 * @param mixed      $data    Optional. Error data. Default empty string.
 	 */
 	public function add( $code, $message, $data = '' ) {
 		$this->errors[ $code ][] = $message;
