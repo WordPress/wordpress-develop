472fc2245ade7a6b77e5ed1916227239e756c3ab
Adam Silverstein <adamjs@google.com>
2022-11-14 11:49:47 -0700

    Only apply button style changes to twentyten theme

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -3658,25 +3658,6 @@ function _wp_theme_json_webfonts_handler() {
 }
 
 /**
- * Loads classic theme styles on classic themes in the frontend.
- *
- * This is needed for backwards compatibility for button blocks specifically.
- *
- * @since 6.1.0
- */
-function wp_enqueue_classic_theme_styles() {
-	if ( ! WP_Theme_JSON_Resolver::theme_has_support() ) {
-		// Only enqueue classic-themes CSS when the content includes a button.
-		if ( ! has_block( 'button' ) ) {
-			return;
-		}
-		$suffix = wp_scripts_get_suffix();
-		wp_register_style( 'classic-theme-styles', '/' . WPINC . "/css/classic-themes$suffix.css", array(), true );
-		wp_enqueue_style( 'classic-theme-styles' );
-	}
-}
-
-/**
  * Loads classic theme styles on classic themes in the editor.
  *
  * This is needed for backwards compatibility for button blocks specifically.
