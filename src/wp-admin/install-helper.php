340cd8ee4f9d6efee001e6bfe659e77ad3a09e3f
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-11-17 08:23:52 -0800

    Add article for added clarity
    Co-authored-by: Sergey Biryukov <sergeybiryukov.ru@gmail.com>

--- src/wp-admin/install-helper.php
+++ src/wp-admin/install-helper.php
@@ -137,7 +137,7 @@ function maybe_drop_column( $table_name, $column_name, $drop_ddl ) {
 			// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared -- No applicable variables for this query.
 			$wpdb->query( $drop_ddl );
 
-			// Check if column was dropped.
+			// Check if the column was dropped.
 			// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared -- Cannot be prepared. Fetches columns for table names.
 			foreach ( $wpdb->get_col( "DESC $table_name", 0 ) as $column ) {
 				if ( $column === $column_name ) {
