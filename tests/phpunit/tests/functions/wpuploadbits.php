775c30d5367bb07c5dd5c4176cad4882ac359eeb
Paul Bearne <pbearne@gmail.com>
2022-11-22 13:53:46 -0500

    feedback from code review

--- tests/phpunit/tests/functions/wpuploadbits.php
+++ tests/phpunit/tests/functions/wpuploadbits.php
@@ -10,34 +10,44 @@
 class tests_wp_upload_bits extends WP_UnitTestCase {
 
 	/**
-	 * @expectedDeprecated wp_upload_bits
-	 * @return void
+	 * @ticket 57130
 	 *
+	 * @expectedDeprecated wp_upload_bits
 	 */
-	public function test_second_parm_present() {
+	public function test_wp_upload_bits_should_throw_Deprecated_error_if_second_parm_present_and_not_null() {
 		wp_upload_bits( 'filename.txt', 'not_null', 'bits' );
 	}
+
 	/**
+	 * @ticket 57130
+	 *
 	 * return error if no filename pass in
 	 **/
-	public function test_no_name_present() {
+	public function test_wp_upload_bits_should_return_an_array_with_error_message_if_no_name_present() {
 		$this->assertSameSets( array( 'error' => __( 'Empty filename' ) ), wp_upload_bits( '', '', 'bits' ) );
 	}
 
 	/**
+	 * @ticket 57130
+	 *
 	 * return an array with error message in bad/no extension in the file name
 	 **/
-	public function test_no_ext_present() {
+	public function test_wp_upload_bits_should_return_an_array_with_error_message_if_filename_without_an_extension() {
 		$this->assertSameSets( array( 'error' => __( 'Sorry, you are not allowed to upload this file type.' ) ), wp_upload_bits( 'filename', '', 'bits' ) );
 	}
 
+	/**
+	 * @ticket 57130
+	 *
+	 *
+	 **/
 	public function test_bad_time_present() {
 		$this->assertSameSets(
 			array(
-				'path'    => '/var/www/src/wp-content/uploads/../1/',
+				'path'    => ABSPATH . 'wp-content/uploads/../1/',
 				'url'     => 'http://example.org/wp-content/uploads/../1/',
 				'subdir'  => '/../1/',
-				'basedir' => '/var/www/src/wp-content/uploads',
+				'basedir' => ABSPATH . 'wp-content/uploads',
 				'baseurl' => 'http://example.org/wp-content/uploads',
 				'error'   => 'Unable to create directory wp-content/uploads/../1/. Is its parent directory writable by the server?',
 			),
@@ -45,11 +55,15 @@ class tests_wp_upload_bits extends WP_UnitTestCase {
 		);
 	}
 
+	/**
+	 * @ticket 57130
+	 *
+	 * @return void
+	 */
 	public function test_file_content() {
-		$filename = '/var/www/src/wp-content/uploads/99/1//filename.txt';
-		$content = 'file content';
+		$filename = ABSPATH . 'wp-content/uploads/99/1//filename.txt';
+		$content  = 'file content';
 
-		$this->unlink( $filename );
 		$this->assertSameSets(
 			array(
 				'error' => false,
@@ -60,9 +74,11 @@ class tests_wp_upload_bits extends WP_UnitTestCase {
 			),
 			wp_upload_bits( 'filename.txt', null, $content, '99/12' )
 		);
-		$file = fopen( $filename, 'r' );
-		$this->assertSame( $content, fread( $file, filesize( $filename ) ) );
+		$file          = fopen( $filename, 'rb' );
+		$file_contents = fread( $file, filesize( $filename ) );
 		fclose( $file );
 		$this->unlink( $filename );
+
+		$this->assertSame( $content, $file_contents );
 	}
 }
