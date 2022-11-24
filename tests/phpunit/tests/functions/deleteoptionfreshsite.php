6c5506e6db839acfa6cc066584a01cf286f6d60b
Paul Bearne <pbearne@gmail.com>
2022-11-23 19:55:21 -0500

    test for _delete_option_fresh_site

---
+++ tests/phpunit/tests/functions/deleteoptionfreshsite.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Tests for _delete_option_fresh_site function.
+ *
+ * @group functions.php
+ *
+ * @covers ::_delete_option_fresh_site
+ */
+class Tests_Functions_DeleteOptionFreshSite extends WP_UnitTestCase {
+
+	/**
+	 * @ticket 57191
+	 */
+	public function test__delete_option_fresh_site() {
+		$current_option = get_option( 'fresh_site' );
+		update_option( 'fresh_site', '1' );
+
+		_delete_option_fresh_site();
+		$this->assertEquals( 0, get_option( 'fresh_site' ) );
+
+		update_option( 'fresh_site', $current_option );
+	}
+}
