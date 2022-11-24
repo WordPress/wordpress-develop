b62d69fc0cde47a97954f766e938f8dac066ceb2
Bernie Reiter <ockham@raz.or.at>
2022-11-24 15:29:14 +0100

    E2E Tests: Add basic coverage for the Gutenberg plugin

---
+++ tests/e2e/specs/gutenberg-plugin.test.js
@@ -0,0 +1,24 @@
+import {
+	activatePlugin,
+	deactivatePlugin,
+	installPlugin,
+	uninstallPlugin,
+} from '@wordpress/e2e-test-utils';
+
+describe( 'Gutenberg plugin', () => {
+	beforeAll( async () => {
+		await installPlugin( 'gutenberg' );
+	} );
+
+	afterAll( async () => {
+		await uninstallPlugin( 'gutenberg' );
+	} );
+
+	it( 'Activate the Gutenberg plugin', async () => {
+		await activatePlugin( 'gutenberg' );
+		// If plugin activation fails, it will time out and throw an error,
+		// since the activatePlugin helper is looking for a `.deactivate` link
+		// which is only there if activation succeeds.
+		await deactivatePlugin( 'gutenberg' );
+	} );
+} );
