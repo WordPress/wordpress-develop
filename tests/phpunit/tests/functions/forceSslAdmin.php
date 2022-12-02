351a2e41bcc46e0c0d321723d5b89cebc1fce4bb
Paul Bearne <pbearne@gmail.com>
2022-12-02 17:34:31 -0500

    tests for force_ssl_admin

--- tests/phpunit/tests/functions/forceSslAdmin.php
+++ tests/phpunit/tests/functions/forceSslAdmin.php
@@ -20,20 +20,4 @@ class Tests_Functions_ForceSslAdmin extends WP_UnitTestCase {
 		// reset to dafault
 		$this->assertTrue( force_ssl_admin( $default ), 'set false and back to default' );
 	}
-
-	/**
-	 * Check passing string to force_ssl_admin doesn't set it.
-	 * Needs #57262 for this test to work.
-	 * @ticket 57261
-	 *
-	 * @expectedDoingItWrong
-	 */
-	public function test_force_ssl_admin_try_test_string_which_should_fail() {
-		$expected = force_ssl_admin();
-		$this->assertSame( $expected, force_ssl_admin(), 'default' );
-
-		$this->assertSame( $expected, force_ssl_admin( 'a string' ), 'try to set a string' );
-		$this->setExpectedIncorrectUsage( 'force_ssl_admin' );
-		$this->assertSame( $expected, force_ssl_admin(), 'check is still as expecting after setting a string' );
-	}
 }
