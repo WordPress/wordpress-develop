ed7f6cb4c97cb9fc0a6c9c969fa0c5d540f1593f
Paul Bearne <pbearne@gmail.com>
2022-12-02 18:36:00 -0500

    Update wpSuspendCacheAddition.php

--- tests/phpunit/tests/functions/wpSuspendCacheAddition.php
+++ tests/phpunit/tests/functions/wpSuspendCacheAddition.php
@@ -14,8 +14,8 @@ class Tests_Function_WpSuspendCacheAddition extends WP_UnitTestCase {
 	public function test_WpSuspendCacheAddition() {
 		$default = wp_suspend_cache_addition();
 		$this->assertSame( $default, wp_suspend_cache_addition(), 'default' );
-		$this->assertFalse( wp_suspend_cache_addition( false ), 'set true' );
-		$this->assertFalse( wp_suspend_cache_addition(), 'check is still true' );
+		$this->assertFalse( wp_suspend_cache_addition( false ), 'set false' );
+		$this->assertFalse( wp_suspend_cache_addition(), 'check is still false' );
 
 		$this->assertFalse( wp_suspend_cache_addition( 'true' ), 'Try to set string' );
 		$this->assertFalse( wp_suspend_cache_addition(), 'check is still false' );
