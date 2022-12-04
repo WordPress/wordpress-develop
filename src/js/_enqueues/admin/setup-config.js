532d957d8c415dd49fbccca4e1c05234fa2e604f
Brennan Goewert <bgoewert@goewert.me>
2022-12-04 11:05:20 -0600

    Remove aria show/hide label

--- src/js/_enqueues/admin/setup-config.js
+++ src/js/_enqueues/admin/setup-config.js
@@ -5,7 +5,6 @@
  * @output wp-admin/js/setup-config.js
  */
 
-/* global setupConfigL10n */
 ( function () {
 	var toggle, status, input, icon, label;
 
@@ -24,15 +23,11 @@
 			input.setAttribute( 'type', 'text' );
 			icon.classList.remove( 'dashicons-visibility' );
 			icon.classList.add( 'dashicons-hidden' );
-			label.innerHTML = setupConfigL10n.hide;
-			toggle.setAttribute( 'aria-label', setupConfigL10n.ariaShow );
 		} else {
 			toggle.setAttribute( 'data-toggle', 0 );
 			input.setAttribute( 'type', 'password' );
 			icon.classList.remove( 'dashicons-hidden' );
 			icon.classList.add( 'dashicons-visibility' );
-			label.innerHTML = setupConfigL10n.show;
-			toggle.setAttribute( 'aria-label', setupConfigL10n.ariaHide );
 		}
 	}
 } )();
