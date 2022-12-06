60bd4bb7eb97ec1742da031eed266f4b67b873cc
Paul Bearne <pbearne@gmail.com>
2022-12-06 15:06:53 -0500

    added protection for no booloen values passed to wp_suspend_cache_invalidation

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -6375,13 +6375,14 @@ function wp_suspend_cache_addition( $suspend = null ) {
  * Turns cache invalidation on and off. Useful during imports where you don't want to do
  * invalidations every time a post is inserted. Callers must be sure that what they are
  * doing won't lead to an inconsistent cache when invalidation is suspended.
- * @ticket #57266 added check for boolean value
+ *
+ * @ticket #57266 added check for if $suspend is boolean value
  *
  * @param bool $suspend Optional. Whether to suspend or enable cache invalidation. Default true.
  *
  * @return bool The current suspend setting.
  * @since 2.7.0
- * @since 6.1.1
+ * @since 6.2.0
  *
  * @global bool $_wp_suspend_cache_invalidation
  *
@@ -6389,6 +6390,20 @@ function wp_suspend_cache_addition( $suspend = null ) {
 function wp_suspend_cache_invalidation( $suspend = true ) {
 	global $_wp_suspend_cache_invalidation;
 	$current_suspend = $_wp_suspend_cache_invalidation;
+
+	if ( ! is_bool( $suspend ) ) {
+		_doing_it_wrong(
+			__FUNCTION__,
+			sprintf(
+				/* translators: %s: The "$suspend" variable name. */
+				__( '"%s" must be of type "bool".' ),
+				$suspend
+			),
+			'6.2.0'
+		);
+
+		$suspend = (bool) $suspend;
+	}
 
 	if ( is_bool( $suspend ) ) {
 		$_wp_suspend_cache_invalidation = $suspend;
