3dfbad3a4d41f69dbbac15c10397ce504715546b
André <583546+oandregal@users.noreply.github.com>
2022-12-13 18:04:27 +0100

    Port cache group management from PR 3712

--- src/wp-includes/global-styles-and-settings.php
+++ src/wp-includes/global-styles-and-settings.php
@@ -264,6 +264,22 @@ function wp_add_global_styles_for_blocks() {
  * @return boolean
  */
 function wp_theme_has_theme_json() {
+	/**
+	 * By using the 'theme_json' group, this data is marked to be non-persistent across requests.
+	 * See `wp_cache_add_non_persistent_groups` in src/wp-includes/load.php and other places.
+	 *
+	 * The rationale for this is to make sure derived data from theme.json
+	 * is always fresh from the potential modifications done via hooks
+	 * that can use dynamic data (modify the stylesheet depending on some option,
+	 * settings depending on user permissions, etc.).
+	 * See some of the existing hooks to modify theme.json behaviour:
+	 * https://make.wordpress.org/core/2022/10/10/filters-for-theme-json-data/
+	 *
+	 * A different alternative considered was to invalidate the cache upon certain
+	 * events such as options add/update/delete, user meta, etc.
+	 * It was judged not enough, hence this approach.
+	 * See https://github.com/WordPress/gutenberg/pull/45372
+	 */
 	$cache_group       = 'theme_json';
 	$cache_key         = 'wp_theme_has_theme_json';
 	$theme_has_support = wp_cache_get( $cache_key, $cache_group );
@@ -294,4 +310,15 @@ function wp_theme_has_theme_json() {
 	wp_cache_set( $cache_key, $theme_has_support, $cache_group );
 
 	return (bool) $theme_has_support;
+}
+
+/**
+ * Private function to clean the caches under the theme_json group.
+ *
+ * @since 6.1.2
+ * @access private
+ */
+function _wp_clean_theme_json_caches() {
+	wp_cache_delete( 'wp_theme_has_theme_json', 'theme_json' );
+	WP_Theme_JSON_Resolver::clean_cached_data();
 }
