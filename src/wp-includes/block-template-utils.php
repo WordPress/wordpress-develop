0e14274b736758a49859c08ace0d45aeee267597
Jonny Harris <jon@spacedmonkey.co.uk>
2022-12-14 11:48:44 +0000

    Also change `_add_block_template_part_area_info`.

--- src/wp-includes/block-template-utils.php
+++ src/wp-includes/block-template-utils.php
@@ -372,7 +372,7 @@ function _add_block_template_info( $template_item ) {
  */
 function _add_block_template_part_area_info( $template_info ) {
 	if ( WP_Theme_JSON_Resolver::theme_has_support() ) {
-		$theme_data = WP_Theme_JSON_Resolver::get_theme_data()->get_template_parts();
+		$theme_data = WP_Theme_JSON_Resolver::get_theme_data( array(), array( 'with_supports' => false ) )->get_template_parts();
 	}
 
 	if ( isset( $theme_data[ $template_info['slug'] ]['area'] ) ) {
