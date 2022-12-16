0255bfbbeb44e35e0eee95b7d3de05264507c874
costdev <me@colin-stewart.co.uk>
2022-12-16 17:24:32 +0000

    Include the old Requests class.

---
+++ src/wp-includes/Requests/library/Requests.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * This file includes the old Requests class
+ * for plugins or themes that still use it.
+ *
+ * @package WordPress
+ * @since 6.2.0
+ */
+
+include_once ABSPATH . WPINC . '/class-requests.php';
