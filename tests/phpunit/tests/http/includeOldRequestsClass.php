8f3453f9587ed3fa574c98b8bb1d80cf3c4fc9b7
costdev <me@colin-stewart.co.uk>
2022-12-17 12:53:29 +0000

    Remove `$expected` variable from test.

--- tests/phpunit/tests/http/includeOldRequestsClass.php
+++ tests/phpunit/tests/http/includeOldRequestsClass.php
@@ -16,10 +16,8 @@ class Tests_HTTP_IncludeOldRequestsClass extends WP_UnitTestCase {
 	 * @coversNothing
 	 */
 	public function test_should_include_old_requests_class() {
-		$expected = 'The PSR-0 `Requests_...` class names in the Request library are deprecated.';
-
 		$this->expectDeprecation();
-		$this->expectDeprecationMessage( $expected );
+		$this->expectDeprecationMessage( 'The PSR-0 `Requests_...` class names in the Request library are deprecated.' );
 
 		new Requests();
 	}
