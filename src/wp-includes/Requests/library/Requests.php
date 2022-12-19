e65a80e37f37a46b4ee45e80f9cf6c69f2254e89
Tonya Mork <tonya.mork@automattic.com>
2022-12-19 08:13:11 -0600

    Improves file docblock description

--- src/wp-includes/Requests/library/Requests.php
+++ src/wp-includes/Requests/library/Requests.php
@@ -1,10 +1,11 @@
 <?php
-
 /**
- * This file includes the old Requests class
- * for plugins or themes that still use it.
+ * Loads the old Requests class file when the autoloader
+ * references the original PSR-0 Requests class.
  *
+ * @deprecated 6.2.0
  * @package WordPress
+ * @subpackage Requests
  * @since 6.2.0
  */
 
