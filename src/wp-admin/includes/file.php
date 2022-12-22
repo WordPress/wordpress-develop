7035c10eb58cf4a76922a86359ba4954a1443920
Andy Fragen <andy@thefragens.com>
2022-12-22 08:24:43 -0800

    add move_dir()

--- src/wp-admin/includes/file.php
+++ src/wp-admin/includes/file.php
@@ -1947,6 +1947,68 @@ function copy_dir( $from, $to, $skip_list = array() ) {
 }
 
 /**
+ * Moves a directory from one location to another via the rename() PHP function.
+ * If the renaming failed, falls back to copy_dir().
+ *
+ * Assumes that WP_Filesystem() has already been called and setup.
+ *
+ * @since 6.2.0
+ *
+ * @global WP_Filesystem_Base $wp_filesystem WordPress filesystem subclass.
+ *
+ * @param string $from        Source directory.
+ * @param string $to          Destination directory.
+ * @return true|WP_Error True on success, WP_Error on failure.
+ */
+function move_dir( $from, $to ) {
+	global $wp_filesystem;
+
+	$result = false;
+
+	/**
+	 * Fires before move_dir().
+	 *
+	 * @since 6.2.0
+	 */
+	do_action( 'pre_move_dir' );
+
+	if ( 'direct' === $wp_filesystem->method ) {
+		$wp_filesystem->rmdir( $to );
+
+		$result = @rename( $from, $to );
+	}
+
+	// Non-direct filesystems use some version of rename without a fallback.
+	if ( 'direct' !== $wp_filesystem->method ) {
+		$result = $wp_filesystem->move( $from, $to );
+	}
+
+	if ( ! $result ) {
+		if ( ! $wp_filesystem->is_dir( $to ) ) {
+			if ( ! $wp_filesystem->mkdir( $to, FS_CHMOD_DIR ) ) {
+				return new \WP_Error( 'mkdir_failed_move_dir', __( 'Could not create directory.' ), $to );
+			}
+		}
+
+		$result = copy_dir( $from, $to, array( basename( $to ) ) );
+
+		// Clear the source directory.
+		if ( ! is_wp_error( $result ) ) {
+			$wp_filesystem->delete( $from, true );
+		}
+	}
+
+	/**
+	 * Fires after move_dir().
+	 *
+	 * @since 6.2.0
+	 */
+	do_action( 'post_move_dir' );
+
+	return $result;
+}
+
+/**
  * Initializes and connects the WordPress Filesystem Abstraction classes.
  *
  * This function will include the chosen transport and attempt connecting.
