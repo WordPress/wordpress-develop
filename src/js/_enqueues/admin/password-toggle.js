31210f927c70885775c7b80df56fba0ca54ec3a9
Brennan Goewert <bgoewert@goewert.me>
2023-01-05 22:45:31 -0600

    Change `aria-label` on toggle including translation support

--- src/js/_enqueues/admin/password-toggle.js
+++ src/js/_enqueues/admin/password-toggle.js
@@ -23,11 +23,13 @@
 
 		if ( 0 === parseInt( status, 10 ) ) {
 			this.setAttribute( 'data-toggle', 1 );
+			this.setAttribute( 'aria-label', this.getAttribute( 'data-hide-label' ) );
 			input.setAttribute( 'type', 'text' );
 			icon.classList.remove( 'dashicons-visibility' );
 			icon.classList.add( 'dashicons-hidden' );
 		} else {
 			this.setAttribute( 'data-toggle', 0 );
+			this.setAttribute( 'aria-label', this.getAttribute( 'data-show-label' ) );
 			input.setAttribute( 'type', 'password' );
 			icon.classList.remove( 'dashicons-hidden' );
 			icon.classList.add( 'dashicons-visibility' );
