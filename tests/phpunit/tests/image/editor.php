fb80d1481477a1e0cb10fc447a522be70dcc66ed
Adam Silverstein <adamjs@google.com>
2023-01-06 10:38:28 -0700

    WEBP -> WebP

--- tests/phpunit/tests/image/editor.php
+++ tests/phpunit/tests/image/editor.php
@@ -125,15 +125,10 @@ class Tests_Image_Editor extends WP_Image_UnitTestCase {
 		// Quality setting for the source image. For PNG the fallback default of 82 is used.
 		$this->assertSame( 82, $editor->get_quality(), 'Default quality setting is 82.' );
 
-		// When saving, quality should change to the output format's value.
-		// A PNG image will be converted to WebP whose quality should be 86.
-		$editor->save();
-		$this->assertSame( 86, $editor->get_quality(), 'Output image format is WebP. Quality setting for it should be 86.' );
-
 		// Saving again should not change the quality.
-		// A PNG image will be converted to WEBP whose quality should be 82.
+		// A PNG image will be converted to WebP whose quality should be 82.
 		$editor->save();
-		$this->assertSame( 82, $editor->get_quality(), 'Output image format is WEBP. Quality setting for it should be 82.' );
+		$this->assertSame( 82, $editor->get_quality(), 'Output image format is WebP. Quality setting for it should be 82.' );
 
 		// Removing PNG to WebP conversion on save. Quality setting should reset to the default.
 		remove_filter( 'image_editor_output_format', array( $this, 'image_editor_output_formats' ) );
@@ -154,9 +149,9 @@ class Tests_Image_Editor extends WP_Image_UnitTestCase {
 		$this->assertSame( 56, $editor->get_quality(), 'Filtered default quality for JPEG is 56.' );
 
 		// Quality should change to the output format's value as filtered above.
-		// A JPEG image will be converted to WEBP whose quality should be 42.
+		// A JPEG image will be converted to WebP whose quality should be 42.
 		$editor->save();
-		$this->assertSame( 42, $editor->get_quality(), 'Image conversion from JPEG to WEBP. Filtered WEBP quality should be 42.' );
+		$this->assertSame( 42, $editor->get_quality(), 'Image conversion from JPEG to WebP. Filtered WebP quality should be 42.' );
 
 		// After removing the conversion the quality setting should reset to the filtered value for the original image type, JPEG.
 		remove_filter( 'image_editor_output_format', array( $this, 'image_editor_output_formats' ) );
