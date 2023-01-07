bc42fd421fd3b3ff2c8724e0b4d8022bcfb1e43b
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-01-07 15:47:07 +0100

    Remove the preconnect, as that leaks IP to google (GDPR)

--- src/wp-content/themes/twentyseventeen/functions.php
+++ src/wp-content/themes/twentyseventeen/functions.php
@@ -312,13 +312,6 @@ function twentyseventeen_fonts_url() {
  * @return array URLs to print for resource hints.
  */
 function twentyseventeen_resource_hints( $urls, $relation_type ) {
-	if ( wp_style_is( 'twentyseventeen-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
-		$urls[] = array(
-			'href' => 'https://fonts.gstatic.com',
-			'crossorigin',
-		);
-	}
-
 	return $urls;
 }
 add_filter( 'wp_resource_hints', 'twentyseventeen_resource_hints', 10, 2 );
