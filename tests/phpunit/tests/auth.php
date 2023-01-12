f2f817910f3626a0c656c721770b6c8f0feb7ee5
audrasjb <audrasjb@gmail.com>
2023-01-12 16:18:41 +0100

    Fix unit test user id

--- tests/phpunit/tests/auth.php
+++ tests/phpunit/tests/auth.php
@@ -640,23 +640,16 @@ class Tests_Auth extends WP_UnitTestCase {
 	 * @ticket 57436
 	 */
 	public function test_wp_set_password_action() {
-		$user_id = self::factory()->user->create(
-			array(
-				'role'       => 'administrator',
-				'user_login' => 'janedoe',
-			)
-		);
-
 		$expected_meta_value = 'Meta value';
 		add_action(
 			'wp_set_password',
-			function ( $password, $user_id ) {
-				update_user_meta( $user_id, 'my-password-user-meta', $expected_meta_value );
+			function ( $password, $this->user->ID ) {
+				update_user_meta( $this->user->ID, 'my-password-user-meta', $expected_meta_value );
 			}
 		);
 
-		wp_set_password( 'A simple password', $user_id );
-		$user_meta_value = get_user_meta( $user_id, 'my-password-user-meta', true );
+		wp_set_password( 'A simple password', $this->user->ID );
+		$user_meta_value = get_user_meta( $this->user->ID, 'my-password-user-meta', true );
 
 		$this->assertSame( $expected_meta_value, $user_meta_value );
 	}
