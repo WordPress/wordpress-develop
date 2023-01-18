f827f07d2928d871722fd2223118c8d8d060d3f3
ntsekouras <ntsekouras@outlook.com>
2023-01-18 18:26:15 +0200

    Add new query args to Pattern Directory Controller

--- tests/phpunit/tests/rest-api/rest-pattern-directory-controller.php
+++ tests/phpunit/tests/rest-api/rest-pattern-directory-controller.php
@@ -31,6 +31,15 @@ class WP_REST_Pattern_Directory_Controller_Test extends WP_Test_REST_Controller_
 	private static $controller;
 
 	/**
+	 * List of URLs captured.
+	 *
+	 * @since 6.2.0
+	 *
+	 * @var string[]
+	 */
+	protected static $http_request_urls;
+
+	/**
 	 * Set up class test fixtures.
 	 *
 	 * @since 5.8.0
@@ -44,7 +53,28 @@ class WP_REST_Pattern_Directory_Controller_Test extends WP_Test_REST_Controller_
 			)
 		);
 
+		self::$http_request_urls = array();
+
 		static::$controller = new WP_REST_Pattern_Directory_Controller();
+	}
+
+	/**
+	 * Tear down after class.
+	 *
+	 * @since 6.2.0
+	 */
+	public static function wpTearDownAfterClass() {
+		self::delete_user( self::$contributor_id );
+	}
+
+	/**
+	 * Clear the captured request URLs after each test.
+	 *
+	 * @since 6.2.0
+	 */
+	public function tear_down() {
+		self::$http_request_urls = array();
+		parent::tear_down();
 	}
 
 	/**
@@ -308,6 +338,108 @@ class WP_REST_Pattern_Directory_Controller_Test extends WP_Test_REST_Controller_
 		$patterns = $response->get_data();
 
 		$this->assertSame( 'modified the cache', $patterns[0] );
+	}
+
+	/**
+	 * Tests if the provided query args are passed through to the wp.org API.
+	 *
+	 * @dataProvider data_get_items_query_args
+	 *
+	 * @covers WP_REST_Pattern_Directory_Controller::get_items
+	 *
+	 * @since 6.2.0
+	 *
+	 * @param string $param    Query parameter name (ex, page).
+	 * @param mixed  $value    Query value to test.
+	 * @param bool   $is_error Whether this value should error or not.
+	 * @param mixed  $expected Expected value (or expected error code).
+	 */
+	public function test_get_items_query_args( $param, $value, $is_error, $expected ) {
+		wp_set_current_user( self::$contributor_id );
+		self::capture_http_urls();
+
+		$request = new WP_REST_Request( 'GET', '/wp/v2/pattern-directory/patterns' );
+		if ( $value ) {
+			$request->set_query_params( array( $param => $value ) );
+		}
+
+		$response = rest_do_request( $request );
+		$data     = $response->get_data();
+		if ( $is_error ) {
+			$this->assertSame( $expected, $data['code'] );
+			$this->assertStringContainsString( $param, $data['message'] );
+		} else {
+			$this->assertCount( 1, self::$http_request_urls );
+			$this->assertStringContainsString( $param . '=' . $expected, self::$http_request_urls[0] );
+		}
+	}
+
+	/**
+	 * Data provider for test_get_items_query_args.
+	 *
+	 * @since 6.2.0
+	 */
+	public function data_get_items_query_args() {
+		return array(
+			'per_page default'   => array( 'per_page', false, false, 100 ),
+			'per_page custom-1'  => array( 'per_page', 5, false, 5 ),
+			'per_page custom-2'  => array( 'per_page', 50, false, 50 ),
+			'per_page invalid-1' => array( 'per_page', 200, true, 'rest_invalid_param' ),
+			'per_page invalid-2' => array( 'per_page', 'abc', true, 'rest_invalid_param' ),
+
+			'page default'       => array( 'page', false, false, 1 ),
+			'page custom'        => array( 'page', 5, false, 5 ),
+			'page invalid'       => array( 'page', 'abc', true, 'rest_invalid_param' ),
+
+			'offset custom'      => array( 'offset', 5, false, 5 ),
+			'offset invalid-1'   => array( 'offset', 'abc', true, 'rest_invalid_param' ),
+
+			'order default'      => array( 'order', false, false, 'desc' ),
+			'order custom'       => array( 'order', 'asc', false, 'asc' ),
+			'order invalid-1'    => array( 'order', 10, true, 'rest_invalid_param' ),
+			'order invalid-2'    => array( 'order', 'fake', true, 'rest_invalid_param' ),
+
+			'orderby default'    => array( 'orderby', false, false, 'date' ),
+			'orderby custom-1'   => array( 'orderby', 'title', false, 'title' ),
+			'orderby custom-2'   => array( 'orderby', 'date', false, 'date' ),
+			'orderby custom-3'   => array( 'orderby', 'favorite_count', false, 'favorite_count' ),
+			'orderby invalid-1'  => array( 'orderby', 10, true, 'rest_invalid_param' ),
+			'orderby invalid-2'  => array( 'orderby', 'fake', true, 'rest_invalid_param' ),
+		);
+	}
+
+	/**
+	 * Attach a filter to capture requested wp.org URL.
+	 *
+	 * @since 6.2.0
+	 */
+	private static function capture_http_urls() {
+		add_filter(
+			'pre_http_request',
+			function ( $preempt, $args, $url ) {
+				if ( 'api.wordpress.org' !== wp_parse_url( $url, PHP_URL_HOST ) ) {
+					return $preempt;
+				}
+
+				self::$http_request_urls[] = $url;
+
+				// Return a response to prevent external API request.
+				$response = array(
+					'headers'  => array(),
+					'response' => array(
+						'code'    => 200,
+						'message' => 'OK',
+					),
+					'body'     => '[]',
+					'cookies'  => array(),
+					'filename' => null,
+				);
+
+				return $response;
+			},
+			10,
+			3
+		);
 	}
 
 	/**
