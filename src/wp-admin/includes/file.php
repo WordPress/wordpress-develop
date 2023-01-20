7e351ab3cfc5e521155dc4caa69633ece5147c01
Andy Fragen <andy@thefragens.com>
2023-01-19 16:59:28 -0800

    docblock update

--- src/wp-admin/includes/file.php
+++ src/wp-admin/includes/file.php
@@ -2627,14 +2627,15 @@ function wp_opcache_invalidate( $filepath, $force = false ) {
 }
 
 /**
- * Invalidate OPcache of directory of files.
+ * Attempts to clear the opcode cache for a directory of files.
  *
  * @since 6.2.0
  *
+ * @see wp_opcache_invalidate()
+ * @link https://www.php.net/manual/en/function.opcache-invalidate.php
  * @global WP_Filesystem_Base $wp_filesystem WordPress filesystem subclass.
  *
- * @param string $dir The path to invalidate.
- * @return void
+ * @param string $dir The path to the directory for which the opcode cache is to be cleared.
  */
 function wp_opcache_invalidate_directory( $dir ) {
 	global $wp_filesystem;
