3e471b968661679818bf608ac380d22c105fb65e
George Mamadashvili <georgemamadashvili@gmail.com>
2023-01-24 22:00:13 +0400

    Remove get_default_block_editor_settings from theme JSON resolver

--- src/wp-includes/block-editor.php
+++ src/wp-includes/block-editor.php
@@ -216,13 +216,6 @@ function get_default_block_editor_settings() {
 		'allowedMimeTypes'                 => get_allowed_mime_types(),
 		'defaultEditorStyles'              => $default_editor_styles,
 		'blockCategories'                  => get_default_block_categories(),
-		'disableCustomColors'              => get_theme_support( 'disable-custom-colors' ),
-		'disableCustomFontSizes'           => get_theme_support( 'disable-custom-font-sizes' ),
-		'disableCustomGradients'           => get_theme_support( 'disable-custom-gradients' ),
-		'disableLayoutStyles'              => get_theme_support( 'disable-layout-styles' ),
-		'enableCustomLineHeight'           => get_theme_support( 'custom-line-height' ),
-		'enableCustomSpacing'              => get_theme_support( 'custom-spacing' ),
-		'enableCustomUnits'                => get_theme_support( 'custom-units' ),
 		'isRTL'                            => is_rtl(),
 		'imageDefaultSize'                 => $image_default_size,
 		'imageDimensions'                  => $image_dimensions,
@@ -233,23 +226,9 @@ function get_default_block_editor_settings() {
 		'__unstableGalleryWithImageBlocks' => true,
 	);
 
-	// Theme settings.
-	$color_palette = current( (array) get_theme_support( 'editor-color-palette' ) );
-	if ( false !== $color_palette ) {
-		$editor_settings['colors'] = $color_palette;
-	}
+	$theme_settings = get_legacy_theme_supports_block_editor_settings();
 
-	$font_sizes = current( (array) get_theme_support( 'editor-font-sizes' ) );
-	if ( false !== $font_sizes ) {
-		$editor_settings['fontSizes'] = $font_sizes;
-	}
-
-	$gradient_presets = current( (array) get_theme_support( 'editor-gradient-presets' ) );
-	if ( false !== $gradient_presets ) {
-		$editor_settings['gradients'] = $gradient_presets;
-	}
-
-	return $editor_settings;
+	return array_merge( $editor_settings, $theme_settings );
 }
 
 /**
@@ -693,4 +672,41 @@ function get_block_editor_theme_styles() {
 	}
 
 	return $styles;
+}
+
+/**
+ * Returns the legacy theme supports settings for block editor.
+ *
+ * @since 6.2.0
+ *
+ * @return array The legacy theme supports settings.
+ */
+function get_legacy_theme_supports_block_editor_settings() {
+	$theme_settings = array(
+		'disableCustomColors'    => get_theme_support( 'disable-custom-colors' ),
+		'disableCustomFontSizes' => get_theme_support( 'disable-custom-font-sizes' ),
+		'disableCustomGradients' => get_theme_support( 'disable-custom-gradients' ),
+		'disableLayoutStyles'    => get_theme_support( 'disable-layout-styles' ),
+		'enableCustomLineHeight' => get_theme_support( 'custom-line-height' ),
+		'enableCustomSpacing'    => get_theme_support( 'custom-spacing' ),
+		'enableCustomUnits'      => get_theme_support( 'custom-units' ),
+	);
+
+	// Theme settings.
+	$color_palette = current( (array) get_theme_support( 'editor-color-palette' ) );
+	if ( false !== $color_palette ) {
+		$theme_settings['colors'] = $color_palette;
+	}
+
+	$font_sizes = current( (array) get_theme_support( 'editor-font-sizes' ) );
+	if ( false !== $font_sizes ) {
+		$theme_settings['fontSizes'] = $font_sizes;
+	}
+
+	$gradient_presets = current( (array) get_theme_support( 'editor-gradient-presets' ) );
+	if ( false !== $gradient_presets ) {
+		$theme_settings['gradients'] = $gradient_presets;
+	}
+
+	return $theme_settings;
 }
