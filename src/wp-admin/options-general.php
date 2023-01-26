11404fb8abfb624308355601b0fa327c3f6a1466
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-01-26 16:49:49 +0300

    Use strict comparison in options-general.php

--- src/wp-admin/options-general.php
+++ src/wp-admin/options-general.php
@@ -220,9 +220,9 @@ if ( false !== strpos( $tzstring, 'Etc/GMT' ) ) {
 
 if ( empty( $tzstring ) ) { // Create a UTC+- zone if no timezone string exists.
 	$check_zone_info = false;
-	if ( 0 == $current_offset ) {
+	if ( 0 === (int) $current_offset ) {
 		$tzstring = 'UTC+0';
-	} elseif ( $current_offset < 0 ) {
+	} elseif ( (int) $current_offset < 0 ) {
 		$tzstring = 'UTC' . $current_offset;
 	} else {
 		$tzstring = 'UTC+' . $current_offset;
@@ -398,7 +398,7 @@ foreach ( $time_formats as $format ) {
 global $wp_locale;
 
 for ( $day_index = 0; $day_index <= 6; $day_index++ ) :
-	$selected = ( get_option( 'start_of_week' ) == $day_index ) ? 'selected="selected"' : '';
+	$selected = ( (int) get_option( 'start_of_week' ) === $day_index ) ? 'selected="selected"' : '';
 	echo "\n\t<option value='" . esc_attr( $day_index ) . "' $selected>" . $wp_locale->get_weekday( $day_index ) . '</option>';
 endfor;
 ?>
