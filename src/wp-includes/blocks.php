f0fd49420d3586e1abae66a6f1ff0929e5d9b32f
Neycho Kalaydzhiev <45290296+Neychok@users.noreply.github.com>
2023-01-26 04:14:34 +0200

    Update blocks.php

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -119,7 +119,8 @@ function register_block_script_handle( $metadata, $field_name, $index = 0 ) {
 			__FUNCTION__,
 			sprintf(
 				/* translators: 1: Field name, 2: Block name. */
-				__( 'The asset file for the "%1$s" defined in "%2$s" block definition is missing.' ),
+				__( 'The asset file (%1$s) for the "%2$s" defined in "%3$s" block definition is missing.' ),
+				dirname( $metadata['file'] ) . '/' . substr_replace( $script_path, '.asset.php', - strlen( '.js' ) )
 				$field_name,
 				$metadata['name']
 			),
