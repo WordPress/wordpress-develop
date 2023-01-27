bc2ec6af4765c2d6f466339385c15def36c14c82
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-01-27 13:17:01 +1100

    Unit tests. Props @costdev

--- tests/phpunit/tests/admin/includesUser.php
+++ tests/phpunit/tests/admin/includesUser.php
@@ -55,4 +55,63 @@ class Tests_Admin_IncludesUser extends WP_UnitTestCase {
 			),
 		);
 	}
+
+	/**
+	 * Tests that wp_is_authorize_application_password_request_valid() accepts
+	 * an insecure scheme for a local environment.
+	 *
+	 * @covers ::wp_is_authorize_application_password_request_valid
+	 */
+	public function test_should_accept_insecure_scheme_for_local_environment() {
+		$request_success = array( 'success_url' => 'http://example.org' );
+		$request_reject  = array( 'reject_url' => 'http://example.org' );
+
+		putenv( 'WP_ENVIRONMENT_TYPE=local' );
+
+		$actual_success = wp_is_authorize_application_password_request_valid( $request_success, get_userdata( 1 ) );
+		$actual_reject  = wp_is_authorize_application_password_request_valid( $request_reject, get_userdata( 1 ) );
+
+		putenv( 'WP_ENVIRONMENT_TYPE' );
+
+		$this->assertNotWPError( $actual_success, 'A WP_Error object was returned for valid success URL.' );
+		$this->assertNotWPError( $actual_reject, 'A WP_Error object was returned for valid rejection URL.' );
+	}
+
+	/**
+	 * Tests that wp_is_authorize_application_password_request_valid() accepts
+	 * an insecure scheme for a local environment.
+	 *
+	 * @dataProvider data_should_not_accept_insecure_scheme_for_non_local_environment
+	 *
+	 * @covers ::wp_is_authorize_application_password_request_valid
+	 *
+	 * @param string $env The environment type.
+	 */
+	public function test_should_not_accept_insecure_scheme_for_non_local_environment( $env ) {
+		$request_success = array( 'success_url' => 'http://example.org' );
+		$request_reject  = array( 'reject_url' => 'http://example.org' );
+
+		putenv( "WP_ENVIRONMENT_TYPE=$env" );
+
+		$actual_success = wp_is_authorize_application_password_request_valid( $request_success, get_userdata( 1 ) );
+		$actual_reject  = wp_is_authorize_application_password_request_valid( $request_reject, get_userdata( 1 ) );
+
+		putenv( 'WP_ENVIRONMENT_TYPE' );
+
+		$this->assertWPError( $actual_success, 'A WP_Error object was not returned for an invalid success URL.' );
+		$this->assertWPError( $actual_reject, 'A WP_Error object was not returned for an invalid rejection URL.' );
+	}
+
+	/**
+	 * Data provider for test_should_not_accept_insecure_scheme_for_non_local_environment().
+	 *
+	 * @return array[]
+	 */
+	public function data_should_not_accept_insecure_scheme_for_non_local_environment() {
+		return array(
+			'production'  => array( 'production' ),
+			'development' => array( 'development' ),
+			'staging'     => array( 'staging' ),
+		);
+	}
 }
