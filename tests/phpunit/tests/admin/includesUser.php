4f098ebe9902a73e1976a9e3674c4b7227453aca
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-01-27 13:52:03 +1100

    Just extend the existing test.

--- tests/phpunit/tests/admin/includesUser.php
+++ tests/phpunit/tests/admin/includesUser.php
@@ -7,115 +7,100 @@
 class Tests_Admin_IncludesUser extends WP_UnitTestCase {
 
 	/**
-	 * @ticket       42790
+	 * Test redirect URLs for application password authorization requests.
+	 *
+	 * @ticket 42790
+	 * @ticket 52617
+	 *
+	 * @covers ::wp_is_authorize_application_password_request_valid
+	 *
 	 * @dataProvider data_is_authorize_application_password_request_valid
-	 * @param array  $request    The request data to validate.
-	 * @param string $error_code The expected error code, empty if no error.
+	 *
+	 * @param array  $request             The request data to validate.
+	 * @param string $expected_error_code The expected error code, empty if no error is expected.
+	 * @param string $env                 The environment type. Defaults to 'production'.
 	 */
-	public function test_is_authorize_application_password_request_valid( $request, $error_code ) {
-		$error = wp_is_authorize_application_password_request_valid( $request, get_userdata( 1 ) );
+	public function test_is_authorize_application_password_request_valid( $request, $expected_error_code, $env = 'production' ) {
+		putenv( "WP_ENVIRONMENT_TYPE=$env" );
 
-		if ( $error_code ) {
-			$this->assertWPError( $error );
-			$this->assertSame( $error_code, $error->get_error_code() );
+		$actual = wp_is_authorize_application_password_request_valid( $request, get_userdata( 1 ) );
+
+		putenv( 'WP_ENVIRONMENT_TYPE' );
+
+		if ( $expected_error_code ) {
+			$this->assertWPError( $actual, 'A WP_Error object is expected.' );
+			$this->assertSame( $expected_error_code, $actual->get_error_code(), 'Unexpected error code.' );
 		} else {
-			$this->assertNotWPError( $error );
+			$this->assertNotWPError( $actual, 'A WP_Error object is not expected.' );
 		}
 	}
 
 	public function data_is_authorize_application_password_request_valid() {
 		return array(
-			array(
+			'prod, empty request args'      => array(
 				array(),
 				'',
 			),
-			array(
+			'prod, http success URL'        => array(
 				array( 'success_url' => 'http://example.org' ),
 				'invalid_redirect_scheme',
 			),
-			array(
+			'prod, http reject URL'         => array(
 				array( 'reject_url' => 'http://example.org' ),
 				'invalid_redirect_scheme',
 			),
-			array(
+			'prod, https success URL'       => array(
 				array( 'success_url' => 'https://example.org' ),
 				'',
 			),
-			array(
+			'prod, https reject URL'        => array(
 				array( 'reject_url' => 'https://example.org' ),
 				'',
 			),
-			array(
+			'prod, app scheme success URL'  => array(
 				array( 'success_url' => 'wordpress://example' ),
 				'',
 			),
-			array(
+			'prod, app scheme reject URL'   => array(
 				array( 'reject_url' => 'wordpress://example' ),
 				'',
 			),
-		);
-	}
+			'local, empty request args'     => array(
+				array(),
+				'',
+				'local',
+			),
+			'local, http success URL'       => array(
+				array( 'success_url' => 'http://example.org' ),
+				'',
+				'local',
+			),
+			'local, http reject URL'        => array(
+				array( 'reject_url' => 'http://example.org' ),
+				'',
+				'local',
+			),
+			'local, https success URL'      => array(
+				array( 'success_url' => 'https://example.org' ),
+				'',
+				'local',
+			),
+			'local, https reject URL'       => array(
+				array( 'reject_url' => 'https://example.org' ),
+				'',
+				'local',
+			),
+			'local, app scheme success URL' => array(
+				array( 'success_url' => 'wordpress://example' ),
+				'',
+				'local',
+			),
+			'local, app scheme reject URL'  => array(
+				array( 'reject_url' => 'wordpress://example' ),
+				'',
+				'local',
+			),
 
-	/**
-	 * Tests that wp_is_authorize_application_password_request_valid() accepts
-	 * an insecure scheme for a local environment.
-	 *
-	 * @ticket 52617
-	 *
-	 * @covers ::wp_is_authorize_application_password_request_valid
-	 */
-	public function test_should_accept_insecure_scheme_for_local_environment() {
-		$request_success = array( 'success_url' => 'http://example.org' );
-		$request_reject  = array( 'reject_url' => 'http://example.org' );
-
-		putenv( 'WP_ENVIRONMENT_TYPE=local' );
-
-		$actual_success = wp_is_authorize_application_password_request_valid( $request_success, get_userdata( 1 ) );
-		$actual_reject  = wp_is_authorize_application_password_request_valid( $request_reject, get_userdata( 1 ) );
-
-		putenv( 'WP_ENVIRONMENT_TYPE' );
-
-		$this->assertNotWPError( $actual_success, 'A WP_Error object was returned for valid success URL.' );
-		$this->assertNotWPError( $actual_reject, 'A WP_Error object was returned for valid rejection URL.' );
-	}
-
-	/**
-	 * Tests that wp_is_authorize_application_password_request_valid() accepts
-	 * an insecure scheme for a local environment.
-	 *
-	 * @ticket 52617
-	 *
-	 * @covers ::wp_is_authorize_application_password_request_valid
-	 *
-	 * @dataProvider data_should_not_accept_insecure_scheme_for_non_local_environment
-	 *
-	 * @param string $env The environment type.
-	 */
-	public function test_should_not_accept_insecure_scheme_for_non_local_environment( $env ) {
-		$request_success = array( 'success_url' => 'http://example.org' );
-		$request_reject  = array( 'reject_url' => 'http://example.org' );
-
-		putenv( "WP_ENVIRONMENT_TYPE=$env" );
-
-		$actual_success = wp_is_authorize_application_password_request_valid( $request_success, get_userdata( 1 ) );
-		$actual_reject  = wp_is_authorize_application_password_request_valid( $request_reject, get_userdata( 1 ) );
-
-		putenv( 'WP_ENVIRONMENT_TYPE' );
-
-		$this->assertWPError( $actual_success, 'A WP_Error object was not returned for an invalid success URL.' );
-		$this->assertWPError( $actual_reject, 'A WP_Error object was not returned for an invalid rejection URL.' );
-	}
-
-	/**
-	 * Data provider for test_should_not_accept_insecure_scheme_for_non_local_environment().
-	 *
-	 * @return array[]
-	 */
-	public function data_should_not_accept_insecure_scheme_for_non_local_environment() {
-		return array(
-			'production'  => array( 'production' ),
-			'development' => array( 'development' ),
-			'staging'     => array( 'staging' ),
 		);
 	}
 }
