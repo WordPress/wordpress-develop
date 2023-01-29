98c5af799a502a01f1b4c9eb4d20a64f86b4d874
Paul Bearne <pbearne@gmail.com>
2023-01-29 18:14:53 -0500

    Update tests/phpunit/tests/option/wpLoadAlloptions.php
    Co-authored-by: Colin Stewart <79332690+costdev@users.noreply.github.com>

--- tests/phpunit/tests/option/wpLoadAlloptions.php
+++ tests/phpunit/tests/option/wpLoadAlloptions.php
@@ -122,7 +122,7 @@ class Tests_Option_wpLoadAlloptions extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test ensuring that `$alloptions` can be filtered with a custom value, short circuiting `wp_load_alloptions()`.
+	 * Tests that `$alloptions` can be filtered with a custom value, short circuiting `wp_load_alloptions()`.
 	 *
 	 * @ticket 56045
 	 */
