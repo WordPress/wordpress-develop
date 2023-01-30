694e6839dd1b5e192ea1138685968b32b73839fe
Martin Krcho <1962663+martinkrcho@users.noreply.github.com>
2023-01-30 10:04:44 +0100

    Update phpdoc for WP_Test_RunsFileUploadTests.

--- tests/phpunit/includes/trait-runs-file-upload-tests.php
+++ tests/phpunit/includes/trait-runs-file-upload-tests.php
@@ -1,13 +1,7 @@
 <?php
 
 /**
- * Defines a basic fixture to run multiple tests.
- *
- * Resets the state of the WordPress installation before and after every test.
- *
- * Includes utility functions and assertions useful for testing WordPress.
- *
- * All WordPress unit tests should inherit from this class.
+ * Defines helper functions for dealing with file uploads and attachment creation.
  */
 trait WP_Test_RunsFileUploadTests {
 
