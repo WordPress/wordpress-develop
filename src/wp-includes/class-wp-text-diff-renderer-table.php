80d4d7e94cdf8196fe70fb59daf80f4d61db8b32
Anton Vlasenko <vlasenko.anton@gmail.com>
2023-01-31 20:37:02 +0100

    Make linter fail (for testing purposes).

--- src/wp-includes/class-wp-text-diff-renderer-table.php
+++ src/wp-includes/class-wp-text-diff-renderer-table.php
@@ -114,6 +114,11 @@ class WP_Text_Diff_Renderer_Table extends Text_Diff_Renderer {
 	 * @return string
 	 */
 	public function addedLine( $line ) {
+		$a = 5;
+		while ( $a < 5 ) {
+			// Nothing to do here.
+		}
+
 		return "<td class='diff-addedline'><span aria-hidden='true' class='dashicons dashicons-plus'></span><span class='screen-reader-text'>" . __( 'Added:' ) . " </span>{$line}</td>";
 
 	}
