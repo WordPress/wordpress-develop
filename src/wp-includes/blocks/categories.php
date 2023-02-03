bb8720000ef17475f96237fbc599b35c816d9841
Erik <11491369+kebbet@users.noreply.github.com>
2023-02-03 13:03:00 +0100

    Don not touch version controlled file.

--- src/wp-includes/blocks/categories.php
+++ src/wp-includes/blocks/categories.php
@@ -32,10 +32,7 @@ function render_block_core_categories( $attributes ) {
 		$id                       = 'wp-block-categories-' . $block_id;
 		$args['id']               = $id;
 		$args['show_option_none'] = __( 'Select Category' );
-		$wrapper_markup           = '<div %1$s><label class="screen-reader-text" for="' . esc_attr( $id ) . '">' .
-		/* translators: hidden accessibility text */
-		__( 'Categories' ) .
-		'</label>%2$s</div>';
+		$wrapper_markup           = '<div %1$s><label class="screen-reader-text" for="' . esc_attr( $id ) . '">' . __( 'Categories' ) . '</label>%2$s</div>';
 		$items_markup             = wp_dropdown_categories( $args );
 		$type                     = 'dropdown';
 
