f0a3f4b343db6690f71d1e818e6907a2e8878034
Erik <11491369+kebbet@users.noreply.github.com>
2023-02-03 10:44:58 +0100

    More translator texts in wp-admin class-files

--- src/wp-admin/includes/nav-menu.php
+++ src/wp-admin/includes/nav-menu.php
@@ -461,7 +461,7 @@ function wp_nav_menu_item_post_type_meta_box( $data_object, $box ) {
 			'format'             => '',
 			'prev_text'          => '<span aria-label="' . esc_attr__( 'Previous page' ) . '">' . __( '&laquo;' ) . '</span>',
 			'next_text'          => '<span aria-label="' . esc_attr__( 'Next page' ) . '">' . __( '&raquo;' ) . '</span>',
-			'before_page_number' => '<span class="screen-reader-text">' . __( 'Page' ) . '</span> ',
+			'before_page_number' => '<span class="screen-reader-text">' . /* translators: hidden accessibility text */ __( 'Page' ) . '</span> ',
 			'total'              => $num_pages,
 			'current'            => $pagenum,
 		)
@@ -582,7 +582,12 @@ function wp_nav_menu_item_post_type_meta_box( $data_object, $box ) {
 			}
 			?>
 			<p class="quick-search-wrap">
-				<label for="quick-search-posttype-<?php echo $post_type_name; ?>" class="screen-reader-text"><?php _e( 'Search' ); ?></label>
+				<label for="quick-search-posttype-<?php echo $post_type_name; ?>" class="screen-reader-text">
+					<?php
+					/* translators: hidden accessibility text */
+					_e( 'Search' );
+					?>
+				</label>
 				<input type="search"<?php wp_nav_menu_disabled_check( $nav_menu_selected_id ); ?> class="quick-search" value="<?php echo $searched; ?>" name="quick-search-posttype-<?php echo $post_type_name; ?>" id="quick-search-posttype-<?php echo $post_type_name; ?>" />
 				<span class="spinner"></span>
 				<?php submit_button( __( 'Search' ), 'small quick-search-submit hide-if-js', 'submit', false, array( 'id' => 'submit-quick-search-posttype-' . $post_type_name ) ); ?>
@@ -757,7 +762,7 @@ function wp_nav_menu_item_taxonomy_meta_box( $data_object, $box ) {
 			'format'             => '',
 			'prev_text'          => '<span aria-label="' . esc_attr__( 'Previous page' ) . '">' . __( '&laquo;' ) . '</span>',
 			'next_text'          => '<span aria-label="' . esc_attr__( 'Next page' ) . '">' . __( '&raquo;' ) . '</span>',
-			'before_page_number' => '<span class="screen-reader-text">' . __( 'Page' ) . '</span> ',
+			'before_page_number' => '<span class="screen-reader-text">' . /* translators: hidden accessibility text */ __( 'Page' ) . '</span> ',
 			'total'              => $num_pages,
 			'current'            => $pagenum,
 		)
@@ -877,7 +882,12 @@ function wp_nav_menu_item_taxonomy_meta_box( $data_object, $box ) {
 			}
 			?>
 			<p class="quick-search-wrap">
-				<label for="quick-search-taxonomy-<?php echo $taxonomy_name; ?>" class="screen-reader-text"><?php _e( 'Search' ); ?></label>
+				<label for="quick-search-taxonomy-<?php echo $taxonomy_name; ?>" class="screen-reader-text">
+					<?php
+					/* translators: hidden accessibility text */
+					_e( 'Search' );
+					?>
+				</label>
 				<input type="search" class="quick-search" value="<?php echo $searched; ?>" name="quick-search-taxonomy-<?php echo $taxonomy_name; ?>" id="quick-search-taxonomy-<?php echo $taxonomy_name; ?>" />
 				<span class="spinner"></span>
 				<?php submit_button( __( 'Search' ), 'small quick-search-submit hide-if-js', 'submit', false, array( 'id' => 'submit-quick-search-taxonomy-' . $taxonomy_name ) ); ?>
