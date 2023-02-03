8fbe21a37a2649c7f0ee140d2a99fe72ca73c94e
Jb Audras <audrasjb@git.wordpress.org>
2023-02-03 13:33:18 +0000

    HTTP API: Fix request header inconsistencies.
    This changeset improves the consistency in capitalization of fetching and outputting of request headers. It also updates occurrences found in some docblocks.
    
    Props johnjamesjacoby, costdev, audrasjb, petitphp, mhkuu, SergeyBiryukov.
    Fixes #54225.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@55210 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/functions/doEnclose.php
+++ tests/phpunit/tests/functions/doEnclose.php
@@ -265,8 +265,8 @@ class Tests_Functions_DoEnclose extends WP_UnitTestCase {
 		$fake_headers = array(
 			'mp4' => array(
 				'headers' => array(
-					'content-length' => 123,
-					'content-type'   => 'video/mp4',
+					'Content-Length' => 123,
+					'Content-Type'   => 'video/mp4',
 				),
 			),
 			'ogg' => array(
@@ -289,8 +289,8 @@ class Tests_Functions_DoEnclose extends WP_UnitTestCase {
 		// Fallback header.
 		return array(
 			'headers' => array(
-				'content-length' => 0,
-				'content-type'   => '',
+				'Content-Length' => 0,
+				'Content-Type'   => '',
 			),
 		);
 	}
