9507ff958709a3403d6103594035ce763f1184d4
Clement Boirie <c.boirie@gmail.com>
2023-02-03 11:15:51 +0100

    Ensure query_string do not force trailing slash

--- tests/phpunit/tests/link/getPagenumLink.php
+++ tests/phpunit/tests/link/getPagenumLink.php
@@ -6,10 +6,6 @@
  */
 class Tests_Link_GetPagenumLink extends WP_UnitTestCase {
 
-	public function get_pagenum_link_cb( $url ) {
-		return $url . '/WooHoo';
-	}
-
 	/**
 	 * @ticket 8847
 	 */
@@ -31,18 +27,64 @@ class Tests_Link_GetPagenumLink extends WP_UnitTestCase {
 	/**
 	 * @ticket 2877
 	 */
-	function test_get_pagenum_link_firstpage_trailingslash() {
+	public function test_get_pagenum_link_firstpage_no_trailingslash() {
 		$old_req_uri = $_SERVER['REQUEST_URI'];
 
 		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%' );
-
-		add_filter( 'home_url', array( $this, 'get_pagenum_link_cb' ) );
-		$_SERVER['REQUEST_URI'] = '/woohoo';
+		$_SERVER['REQUEST_URI'] = '/woohoo/page/2/';
 		$paged                  = get_pagenum_link( 1 );
 
-		remove_filter( 'home_url', array( $this, 'get_pagenum_link_cb' ) );
-		$this->assertEquals( home_url( '/WooHoo' ), $paged );
+		$this->assertEquals( home_url( '/woohoo' ), $paged );
 
 		$_SERVER['REQUEST_URI'] = $old_req_uri;
+	}
+
+	/**
+	 * @ticket 2877
+	 */
+	public function test_get_pagenum_link_paged_no_trailingslash() {
+		$old_req_uri = $_SERVER['REQUEST_URI'];
+
+		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%' );
+		$_SERVER['REQUEST_URI'] = '/woohoo';
+		$paged                  = get_pagenum_link( 2 );
+
+		$this->assertEquals( home_url( '/woohoo/page/2' ), $paged );
+
+		$_SERVER['REQUEST_URI'] = $old_req_uri;
+	}
+
+	/**
+	 * @ticket 2877
+	 */
+	public function test_get_pagenum_link_firstpage_no_trailingslash_queryargs() {
+		$old_req_uri = $_SERVER['REQUEST_URI'];
+
+		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%' );
+		$_SERVER['REQUEST_URI'] = '/woohoo/page/2?test=1234';
+		$paged                  = get_pagenum_link( 1 );
+
+		$this->assertEquals( home_url( '/woohoo?test=1234' ), $paged );
+
+		$_SERVER['REQUEST_URI'] = $old_req_uri;
+	}
+
+	/**
+	 * @ticket 2877
+	 */
+	public function test_get_pagenum_link_paged_no_trailingslash_query_args() {
+		$old_req_uri = $_SERVER['REQUEST_URI'];
+
+		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%' );
+		$_SERVER['REQUEST_URI'] = '/woohoo?test=1234';
+		$paged                  = get_pagenum_link( 2 );
+
+		$this->assertEquals( home_url( '/woohoo/page/2?test=1234' ), $paged );
+
+		$_SERVER['REQUEST_URI'] = $old_req_uri;
+	}
+
+	public function get_pagenum_link_cb( $url ) {
+		return $url . '/WooHoo';
 	}
 }
