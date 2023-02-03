da0581032cbd8f2d0b70be7d4f9a2fc99c2dc6ea
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-02-03 13:02:45 +0100

    Revert "Completely remove the resource hints filter"
    This reverts commit 4f281b67d75975a5b19d6bf901a562ba64e2f6f5.

--- src/wp-content/themes/twentytwelve/functions.php
+++ src/wp-content/themes/twentytwelve/functions.php
@@ -275,12 +275,15 @@ add_action( 'enqueue_block_editor_assets', 'twentytwelve_block_editor_styles' );
  * Add preconnect for Fonts.
  *
  * @since Twenty Twelve 2.2
- * @deprecated 3.9
  *
  * @param array   $urls          URLs to print for resource hints.
  * @param string  $relation_type The relation type the URLs are printed.
  * @return array URLs to print for resource hints.
  */
+function twentytwelve_resource_hints( $urls, $relation_type ) {
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'twentytwelve_resource_hints', 10, 2 );
 
 /**
  * Filter TinyMCE CSS path to include Google Fonts.
