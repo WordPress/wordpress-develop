1539c8bf3b934c6d84c0e3ed42ddd43ca8548025
Jb Audras <audrasjb@gmail.com>
2023-02-06 09:30:54 +0100

    Update src/wp-includes/comment-template.php
    Co-authored-by: Colin Stewart <79332690+costdev@users.noreply.github.com>

--- src/wp-includes/comment-template.php
+++ src/wp-includes/comment-template.php
@@ -1040,7 +1040,8 @@ function comment_text( $comment_ID = 0, $args = array() ) {
  * @param bool           $gmt        Optional. Whether to use the GMT date. Default false.
  * @param bool           $translate  Optional. Whether to translate the time (for use in feeds).
  *                                   Default true.
- * @param int|WP_Comment $comment_ID WP_Comment or ID of the comment for which to get the date.
+ * @param int|WP_Comment $comment_ID Optional. WP_Comment or ID of the comment for which to get the date.
+                                     Default is 0, or the global comment.
  * @return string The formatted time.
  */
 function get_comment_time( $format = '', $gmt = false, $translate = true, $comment_ID = 0 ) {
