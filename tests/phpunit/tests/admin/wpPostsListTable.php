d7e4fac6f728fae72ad2f964762a0615797a258c
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-02-07 13:53:40 +0530

    Update wpPostsListTable.php

--- tests/phpunit/tests/admin/wpPostsListTable.php
+++ tests/phpunit/tests/admin/wpPostsListTable.php
@@ -405,7 +405,7 @@ class Tests_Admin_wpPostsListTable extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Data provider.
+	 * Data provider for test_handle_row_actions_should_conditionally_render_quick_edit_markup().
 	 *
 	 * @return array[]
 	 */
