1bb90cd6a5ef73877323432f4d2a6707d5273434
costdev <me@colin-stewart.co.uk>
2023-02-10 13:01:33 +0000

    Add test to show the issue.

---
+++ tests/phpunit/tests/passwordHash.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Tests for the PasswordHash external library.
+ *
+ * @covers PasswordHash
+ */
+class Tests_PasswordHash extends WP_UnitTestCase {
+
+	public static function set_up_before_class() {
+		parent::set_up_before_class();
+
+		require_once ABSPATH . WPINC . '/class-phpass.php';
+	}
+
+	/**
+	 * Tests that PasswordHash::gensalt_blowfish() does not throw a deprecation
+	 * notice on PHP 8.1 for "Implicit conversion from float to int loses precision".
+	 *
+	 * @ticket 56340
+	 *
+	 * @covers PasswordHash::gensalt_blowfish
+	 */
+	public function test_gensalt_blowfish_should_not_throw_deprecation_notice_in_php81() {
+		if ( version_compare( PHP_VERSION, '8.1.0', '<' ) ) {
+			$this->markTestSkipped( 'This test should only run on PHP 8.1 or greater.' );
+		}
+
+		try {
+			$this->expectNotToPerformAssertions();
+
+			$hasher = new PasswordHash( 8, true );
+			$hasher->gensalt_blowfish( 'a password string' );
+		} catch ( \Exception $exception ) {
+			$message = $exception->getMessage();
+
+			if ( str_contains( $message, 'Implicit conversion from float' ) ) {
+				$this->fail( 'The deprecation notice was thrown: ' . $message );
+			} else {
+				$this->fail( 'The deprecation notice was not thrown, but another exception was: ' . $message );
+			}
+		}
+	}
+
+}
