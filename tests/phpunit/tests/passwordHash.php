df7b113e5015049c8819e4e235c3a2c2bdb59ac4
costdev <me@colin-stewart.co.uk>
2023-02-11 10:43:48 +0000

    Allow the test to error upon failure.

--- tests/phpunit/tests/passwordHash.php
+++ tests/phpunit/tests/passwordHash.php
@@ -17,6 +17,8 @@ class Tests_PasswordHash extends WP_UnitTestCase {
 	 * Tests that PasswordHash::gensalt_blowfish() does not throw a deprecation
 	 * notice on PHP 8.1 for "Implicit conversion from float to int loses precision".
 	 *
+	 * Should this test fail, it will produce an error "E" in the results.
+	 *
 	 * @ticket 56340
 	 *
 	 * @covers PasswordHash::gensalt_blowfish
@@ -31,15 +33,6 @@ class Tests_PasswordHash extends WP_UnitTestCase {
 
 			$hasher = new PasswordHash( 8, true );
 			$hasher->gensalt_blowfish( 'a password string' );
-		} catch ( \Exception $exception ) {
-			$message = $exception->getMessage();
-
-			if ( str_contains( $message, 'Implicit conversion from float' ) ) {
-				$this->fail( 'The deprecation notice was thrown: ' . $message );
-			} else {
-				$this->fail( 'The deprecation notice was not thrown, but another exception was: ' . $message );
-			}
-		}
 	}
 
 }
