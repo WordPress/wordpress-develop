f134408cd5cafa1465e9d5ea87257081722b6f58
Robin <robin@wpdeveloper.com>
2023-02-11 20:13:36 +0600

    Version bump 2015

--- src/wp-content/themes/twentyfifteen/functions.php
+++ src/wp-content/themes/twentyfifteen/functions.php
@@ -422,7 +422,7 @@ function twentyfifteen_scripts() {
 	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '20201026' );
 
 	// Load our main stylesheet.
-	wp_enqueue_style( 'twentyfifteen-style', get_stylesheet_uri(), array(), '20221101' );
+	wp_enqueue_style( 'twentyfifteen-style', get_stylesheet_uri(), array(), '20230328' );
 
 	// Theme block stylesheet.
 	wp_enqueue_style( 'twentyfifteen-block-style', get_template_directory_uri() . '/css/blocks.css', array( 'twentyfifteen-style' ), '20220914' );
