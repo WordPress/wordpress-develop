6d4dce96374b222cbf4977523b2bbd02bf0017d0
Robin <robin@wpdeveloper.com>
2023-02-11 20:03:13 +0600

    Version bump 2012

--- src/wp-content/themes/twentytwelve/functions.php
+++ src/wp-content/themes/twentytwelve/functions.php
@@ -190,7 +190,7 @@ function twentytwelve_scripts_styles() {
 	}
 
 	// Loads our main stylesheet.
-	wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri(), array(), '20221101' );
+	wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri(), array(), '20230328' );
 
 	// Theme block stylesheet.
 	wp_enqueue_style( 'twentytwelve-block-style', get_template_directory_uri() . '/css/blocks.css', array( 'twentytwelve-style' ), '20190406' );
