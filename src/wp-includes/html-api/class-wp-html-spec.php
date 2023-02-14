6a3080c0c2dcac8252e5817eb2b3c8fc23e083e7
Dennis Snell <dennis.snell@automattic.com>
2023-02-14 14:44:09 -0700

    Remove leading space: how'd that get there?

--- src/wp-includes/html-api/class-wp-html-spec.php
+++ src/wp-includes/html-api/class-wp-html-spec.php
@@ -2,27 +2,27 @@
 
 class WP_HTML_Spec {
 	/**
- 	 * @see https://html.spec.whatwg.org/#elements-2
- 	 */
- 	public static function is_void_element( $tag_name ) {
- 		switch ( strtoupper( $tag_name ) ) {
- 			case 'AREA':
- 			case 'BASE':
- 			case 'BR':
- 			case 'COL':
- 			case 'EMBED':
- 			case 'HR':
- 			case 'IMG':
- 			case 'INPUT':
- 			case 'LINK':
- 			case 'META':
- 			case 'SOURCE':
- 			case 'TRACK':
- 			case 'WBR':
- 				return true;
+	 * @see https://html.spec.whatwg.org/#elements-2
+	 */
+	public static function is_void_element( $tag_name ) {
+		switch ( strtoupper( $tag_name ) ) {
+			case 'AREA':
+			case 'BASE':
+			case 'BR':
+			case 'COL':
+			case 'EMBED':
+			case 'HR':
+			case 'IMG':
+			case 'INPUT':
+			case 'LINK':
+			case 'META':
+			case 'SOURCE':
+			case 'TRACK':
+			case 'WBR':
+				return true;
 
- 			default:
- 				return false;
- 		}
- 	}
+			default:
+				return false;
+		}
+	}
 }
