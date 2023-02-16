f7cf551b4aaae131ac0c208919925fae5ed41d96
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2023-02-16 14:31:13 -0600

    Update documentation URL

--- src/wp-admin/options-general.php
+++ src/wp-admin/options-general.php
@@ -32,7 +32,7 @@ if ( ! is_multisite() ) {
 		'<p>' . sprintf(
 			/* translators: %s: Documentation URL. */
 			__( 'Though the terms refer to two different concepts, in practice, they can be the same address or different. For example, you can have the WordPress core files in the root directory (<code>https://example.com</code>), in which case the two URLs would be the same. Or the <a href="%s">WordPress files can be in a subdirectory</a> (<code>https://example.com/wordpress</code>). In that case, the site URL and the WordPress URL would be different.' ),
-			__( 'https://wordpress.org/support/article/giving-wordpress-its-own-directory/' )
+			__( 'https://wordpress.org/documentation/article/giving-wordpress-its-own-directory/' )
 		) . '</p>' .
 		'<p>' . sprintf(
 			/* translators: 1: http://, 2: https:// */
