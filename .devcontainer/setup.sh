5a87725ed976b2c436bfd61364c09d7df5f810ea
Helen Hou-Sandi <helen@git.wordpress.org>
2023-02-16 03:48:26 +0000

    Build/Test Tools: Improve devcontainer setup for non-Codespaces usage
    This now runs in local Docker as well. Sets the executable propset to explicitly identify `install-tool.sh` and `setup.sh` as executable. Unclear if this will sync via the Git mirror; ideally it would as otherwise the two files show as modified in a Git clone even in a fresh container.
    
    Props samruddhikhandale.
    See #57187.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@55353 602fd350-edb4-49c9-b593-d223f7449a82

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-if [ "${CODESPACE_NAME}" = "" ]; then
+if [ -z ${CODESPACE_NAME+x} ]; then
 	SITE_HOST="http://localhost:8080"
 else
 	SITE_HOST="https://${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
