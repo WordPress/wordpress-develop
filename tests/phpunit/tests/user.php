cb5eb45fab44da6c68efff21572e18565874035e
Adam Silverstein <adamsilverstein@git.wordpress.org>
2023-02-17 08:08:19 +0000

    Login and Registration: prevent registering with username that matches previous user email.
    When registering a new user, check that no existing user has an email matching the username.
    
    Prevents a login name collision when one user registers with the email address user@test.com and a second user tries to register with the username user@test.com.
    
    Props buutqn, dunhakdis, roytanck, ajayver.
    Fixes #57394.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@55358 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/user.php
+++ tests/phpunit/tests/user.php
@@ -935,6 +935,24 @@ class Tests_User extends WP_UnitTestCase {
 	}
 
 	/**
+	 * @ticket 57394
+	 */
+	public function test_wp_insert_user_should_reject_username_that_matches_existing_user_email() {
+		$existing_email = get_option( 'admin_email' );
+		$username       = wp_insert_user(
+			array(
+				'user_login'    => $existing_email,
+				'user_email'    => 'whatever@example.com',
+				'user_pass'     => 'whatever',
+				'user_nicename' => 'whatever',
+			)
+		);
+
+		$this->assertWPError( $username );
+		$this->assertSame( 'existing_user_login_as_email', $username->get_error_code() );
+	}
+
+	/**
 	 * @ticket 33793
 	 */
 	public function test_wp_insert_user_should_not_truncate_to_a_duplicate_user_nicename() {
