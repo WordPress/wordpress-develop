7aa7a3c2bf490be7c09a240c3453e052a5959012
Mukesh Panchal <mukeshpanchal27@gmail.com>
2023-02-17 10:40:04 +0700

    Address review feedback

--- tests/performance/results.js
+++ tests/performance/results.js
@@ -8,9 +8,16 @@ const testSuites = [
 
 console.log( '\n>> 🎉 Results 🎉 \n' );
 
+/**
+ * Computes the median number from an array numbers.
+ *
+ * @param {number[]} array
+ *
+ * @return {number} Median.
+ */
 function median( array ) {
-	const mid = Math.floor( array.length / 2 ),
-		numbers = [ ...array ].sort( ( a, b ) => a - b );
+	const mid = Math.floor( array.length / 2 );
+	const numbers = [ ...array ].sort( ( a, b ) => a - b );
 	return array.length % 2 !== 0
 		? numbers[ mid ]
 		: ( numbers[ mid - 1 ] + numbers[ mid ] ) / 2;
