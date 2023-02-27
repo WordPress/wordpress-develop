b337e1e328e85495492f871f39f62bf6d171e33f
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2023-02-27 14:56:28 -0600

    Correction: I found `arrow_right`

--- src/wp-content/themes/twentynineteen/classes/class-twentynineteen-svg-icons.php
+++ src/wp-content/themes/twentynineteen/classes/class-twentynineteen-svg-icons.php
@@ -148,7 +148,7 @@ class TwentyNineteen_SVG_Icons {
 	<path fill="none" d="M0 0h24v24H0V0z"></path>
 </svg>',
 
-		'keyboard_arrow_right'     => /* material-design – arrow_left, flipped horizontally */ '
+		'keyboard_arrow_right'     => /* material-design – arrow_right */ '
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 	<path d="M10 17l5-5-5-5v10z"></path>
 	<path fill="none" d="M0 24V0h24v24H0z"></path>
