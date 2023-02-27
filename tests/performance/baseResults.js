2b46e6f19aa8e36713ac279a5472cfa3cd7acbbd
Mukesh Panchal <mukeshpanchal27@gmail.com>
2023-02-27 11:25:30 +0530

    Print base result in workflow

---
+++ tests/performance/baseResults.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+/**
+ * External dependencies
+ */
+const fs = require( 'fs' );
+const { median } = require( './utils' );
+
+const testSuites = [
+    'base-home-classic-theme',
+    'base-home-block-theme',
+];
+
+console.log( '\n>> 🎉 Results 🎉 \n' );
+
+for ( const testSuite of testSuites ) {
+    const resultsFilename = __dirname + '/specs/' + testSuite + '.test.results.json';
+    fs.readFile( resultsFilename, "utf8", ( err, data ) => {
+        if ( err ) {
+            console.log( "File read failed:", err );
+            return;
+        }
+        const convertString = testSuite.charAt( 0 ).toUpperCase() + testSuite.slice( 1 );
+        console.log( convertString.replace( /[-]+/g, " " ) + ':' );
+
+        tableData = JSON.parse( data );
+        const rawResults = [];
+
+        for ( var key in tableData ) {
+            if ( tableData.hasOwnProperty( key ) ) {
+                rawResults[ key ] = median( tableData[ key ] );
+            }
+        }
+        console.table( rawResults );
+    });
+}
