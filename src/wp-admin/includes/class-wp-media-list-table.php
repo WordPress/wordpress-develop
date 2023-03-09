a6ee605eee363655631e6c637a606f51b9c1fc6a
Paul Biron <paul@sparrowhawkcomputing.com>
2023-03-08 17:52:35 -0700

    Allow the "Copy URL" and "Download file" row actions to show even when the "Unattached" view/filter has been applied.  Also, all the "Download file" row action to show when the "Trash" view/filter is applied (to give users more more change to download something before they delete it)..

--- src/wp-admin/includes/class-wp-media-list-table.php
+++ src/wp-admin/includes/class-wp-media-list-table.php
@@ -853,25 +853,27 @@ class WP_Media_List_Table extends WP_List_Table {
 						__( 'View' )
 					);
 				}
-
-				$actions['copy'] = sprintf(
-					'<span class="copy-to-clipboard-container"><button type="button" class="button-link copy-attachment-url media-library" data-clipboard-text="%s" aria-label="%s">%s</button><span class="success hidden" aria-hidden="true">%s</span></span>',
-					esc_url( wp_get_attachment_url( $post->ID ) ),
-					/* translators: %s: Attachment title. */
-					esc_attr( sprintf( __( 'Copy &#8220;%s&#8221; URL to clipboard' ), $att_title ) ),
-					__( 'Copy URL' ),
-					__( 'Copied!' )
-				);
-
-				$actions['download'] = sprintf(
-					'<a href="%s" aria-label="%s" download>%s</a>',
-					esc_url( wp_get_attachment_url( $post->ID ) ),
-					/* translators: %s: Attachment title. */
-					esc_attr( sprintf( __( 'Download &#8220;%s&#8221;' ), $att_title ) ),
-					__( 'Download file' )
-				);
 			}
 		}
+
+		if ( ! $this->is_trash ) {
+			$actions['copy'] = sprintf(
+				'<span class="copy-to-clipboard-container"><button type="button" class="button-link copy-attachment-url media-library" data-clipboard-text="%s" aria-label="%s">%s</button><span class="success hidden" aria-hidden="true">%s</span></span>',
+				esc_url( wp_get_attachment_url( $post->ID ) ),
+				/* translators: %s: Attachment title. */
+				esc_attr( sprintf( __( 'Copy &#8220;%s&#8221; URL to clipboard' ), $att_title ) ),
+				__( 'Copy URL' ),
+				__( 'Copied!' )
+			);
+		}
+
+		$actions['download'] = sprintf(
+			'<a href="%s" aria-label="%s" download>%s</a>',
+			esc_url( wp_get_attachment_url( $post->ID ) ),
+			/* translators: %s: Attachment title. */
+			esc_attr( sprintf( __( 'Download &#8220;%s&#8221;' ), $att_title ) ),
+			__( 'Download file' )
+		);
 
 		/**
 		 * Filters the action links for each attachment in the Media list table.
