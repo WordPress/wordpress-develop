1017c1e5667208fc0c15087919412bcf3c3f1b2f
Till Krüss <tillkruss@users.noreply.github.com>
2023-03-12 20:43:20 -0700

    Update src/wp-includes/functions.php
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -7690,7 +7690,7 @@ function wp_cache_get_last_changed( $group ) {
 /**
  * Sets last changed date for the specified cache group to now.
  *
- * @since n.e.x.t.
+ * @since n.e.x.t
  *
  * @param string $group Where the cache contents are grouped.
  * @return string UNIX timestamp when the group was last changed.
