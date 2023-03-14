c125f0e1221cfa097a5d1625bc68dcf65e4a2874
Andrei Draganescu <me@andreidraganescu.info>
2023-03-14 09:35:12 +0200

    Update src/wp-includes/link-template.php
    Co-authored-by: Jonny Harris <spacedmonkey@users.noreply.github.com>

--- src/wp-includes/link-template.php
+++ src/wp-includes/link-template.php
@@ -1476,7 +1476,7 @@ function get_edit_post_link( $post = 0, $context = 'display' ) {
 	}
 
 	if (  'wp_template' === $post->post_type || 'wp_template_part' === $post->post_type ) {
-		$slug = urlencode( wp_get_theme()->get_stylesheet() . '//' . $post->post_name );
+		$slug = urlencode( get_stylesheet() . '//' . $post->post_name );
 		$link = admin_url( sprintf( $post_type_object->_edit_link, $slug ) );
 	}
 
