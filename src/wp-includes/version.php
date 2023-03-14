1552ad70ab7aa29e16c5d524532904f98ee346f0
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2023-03-14 17:56:53 +0000

    Post WordPress 6.2 RC2 version bump.
    git-svn-id: https://develop.svn.wordpress.org/branches/6.2@55551 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-RC2-src';
+$wp_version = '6.2-RC2-55551-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
