7be210314a91df45b8408f51c94e6aa1c191dce1
Andrei Draganescu <me@andreidraganescu.info>
2023-03-14 09:35:19 +0200

    Update src/wp-includes/post.php
    Co-authored-by: Jonny Harris <spacedmonkey@users.noreply.github.com>

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -357,7 +357,7 @@ function create_initial_post_types() {
 			'description'           => __( 'Templates to include in your theme.' ),
 			'public'                => false,
 			'_builtin'              => true, /* internal use only. don't use this when registering your own post type. */
-			'_edit_link'            => 'site-editor.php?postType=wp_template&postId=%s&canvas=edit', /* internal use only. don't use this when registering your own post type. */
+			'_edit_link'            => 'site-editor.php?postType=%s&postId=%s&canvas=edit', /* internal use only. don't use this when registering your own post type. */
 			'has_archive'           => false,
 			'show_ui'               => false,
 			'show_in_menu'          => false,
