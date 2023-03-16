31eeb2a2227ebb711b1851763bdf23a708ae8911
Jonny Harris <jon@spacedmonkey.co.uk>
2023-03-16 12:45:54 +0000

    Code styling and feedback.

--- src/wp-includes/class-wp-query.php
+++ src/wp-includes/class-wp-query.php
@@ -4714,17 +4714,17 @@ class WP_Query {
 
 		$authordata = get_userdata( $post->post_author );
 
+		$currentday   = false;
+		$currentmonth = false;
+
 		$post_date = $post->post_date;
 		if ( ! empty( $post_date ) && '0000-00-00 00:00:00' !== $post_date ) {
-			$month = substr( $post_date, 5, 2 );
-			$day   = substr( $post_date, 8, 2 );
-			$year  = substr( $post_date, 2, 2 );
+			// Avoid using mysql2date for performance reasons.
+			$currentmonth = substr( $post_date, 5, 2 );
+			$day          = substr( $post_date, 8, 2 );
+			$year         = substr( $post_date, 2, 2 );
 
-			$currentday   = sprintf( '%s.%s.%s', $day, $month, $year );
-			$currentmonth = $month;
-		} else {
-			$currentday   = false;
-			$currentmonth = false;
+			$currentday = sprintf( '%s.%s.%s', $day, $currentmonth, $year );
 		}
 
 		$numpages  = 1;
