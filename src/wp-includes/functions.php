4f3b765a30ad11405e24a20a350789b8a2e1e45e
Till Krüss <tillkruss@users.noreply.github.com>
2023-03-27 07:20:28 -0700

    Apply suggestions from code review
    Co-authored-by: Jonny Harris <spacedmonkey@users.noreply.github.com>

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -7690,7 +7690,7 @@ function wp_cache_get_last_changed( $group ) {
 /**
  * Sets last changed date for the specified cache group to now.
  *
- * @since n.e.x.t
+ * @since 6.3.0
  *
  * @param string $group Where the cache contents are grouped.
  * @return string UNIX timestamp when the group was last changed.
@@ -7705,7 +7705,7 @@ function wp_cache_set_last_changed( $group ) {
 	/**
 	 * Fires after a cache group `last_changed` time is updated.
 	 *
-	 * @since n.e.x.t
+	 * @since 6.3.0
 	 *
 	 * @param string    $group         The cache group name.
 	 * @param int       $time          The new last changed time.
