91caf4d1a6344cb83e2cab3b9591a132ebccf074
Jonny Harris <jon@spacedmonkey.co.uk>
2023-03-29 12:09:01 +0100

    Remove lines.

--- src/wp-admin/includes/class-wp-comments-list-table.php
+++ src/wp-admin/includes/class-wp-comments-list-table.php
@@ -165,7 +165,6 @@ class WP_Comments_List_Table extends WP_List_Table {
 		$_comments = get_comments( $args );
 
 		if ( is_array( $_comments ) ) {
-
 			$this->items       = array_slice( $_comments, 0, $comments_per_page );
 			$this->extra_items = array_slice( $_comments, $comments_per_page );
 
--- src/wp-content/themes/twentyfifteen/comments.php
+++ src/wp-content/themes/twentyfifteen/comments.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The template for displaying comments
  *
