6162521c47e98b55029d33fbaab124c8141079c1
Pieterjan Deneys <NekoJonez@users.noreply.github.com>
2023-04-11 22:47:11 +0200

    As requested

--- src/wp-signup.php
+++ src/wp-signup.php
@@ -704,7 +704,7 @@ function confirm_user_signup( $user_name, $user_email ) {
 	<p><?php _e( 'But, before you can start using your new username, <strong>you must activate it</strong>.' ); ?></p>
 	<p>
 	<?php
-	/* translators: %s: The user Email address. */
+	/* translators: %s: The user email address. */
 	printf( __( 'Check your inbox at %s and click on the given link.' ), '<strong>' . $user_email . '</strong>' );
 	?>
 	</p>
