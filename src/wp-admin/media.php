1ced4e46e14b5d2d99df1da786663d531e39af10
Erik <11491369+kebbet@users.noreply.github.com>
2023-04-12 11:15:16 +0200

    WPCS: use tabs

--- src/wp-admin/media.php
+++ src/wp-admin/media.php
@@ -18,25 +18,25 @@ $submenu_file = 'upload.php';
 wp_reset_vars( array( 'action' ) );
 
 switch ( $action ) {
-        case 'editattachment':
-        case 'edit':
-                // Used in the HTML title tag.
-                $title = __( 'Edit Media' );
+		case 'editattachment':
+		case 'edit':
+				// Used in the HTML title tag.
+				$title = __( 'Edit Media' );
 
-                if ( empty( $errors ) ) {
-                        $errors = null;
-                }
+				if ( empty( $errors ) ) {
+						$errors = null;
+				}
 
-                if ( empty( $_GET['attachment_id'] ) ) {
-                        wp_redirect( admin_url( 'upload.php' ) );
-                        exit;
-                }
-                $att_id = (int) $_GET['attachment_id'];
+				if ( empty( $_GET['attachment_id'] ) ) {
+						wp_redirect( admin_url( 'upload.php' ) );
+						exit;
+				}
+				$att_id = (int) $_GET['attachment_id'];
 
-                wp_redirect( admin_url( "upload.php?item={$att_id}&error=throw-depreciated-media.php" ) );
-                exit;
+				wp_redirect( admin_url( "upload.php?item={$att_id}&error=throw-depreciated-media.php" ) );
+				exit;
 
-        default:
-                wp_redirect( admin_url( 'upload.php?error=throw-depreciated-media.php' ) );
-                exit;
+		default:
+				wp_redirect( admin_url( 'upload.php?error=throw-depreciated-media.php' ) );
+				exit;
 }
