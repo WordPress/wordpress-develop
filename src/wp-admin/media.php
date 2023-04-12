b7258ff65f5be28cf32ed781c09b697f4bb68d86
Erik <11491369+kebbet@users.noreply.github.com>
2023-04-12 11:10:02 +0200

    Add redirect and throw error

--- src/wp-admin/media.php
+++ src/wp-admin/media.php
@@ -2,11 +2,38 @@
 /**
  * Media management action handler.
  *
- * This file is deprecated, use 'wp-admin/upload.php' instead.
- *
- * @deprecated 6.3.0
  * @package WordPress
  * @subpackage Administration
  */
 
-_deprecated_file( basename( __FILE__ ), '6.3.0', 'wp-admin/upload.php' );
+/** Load WordPress Administration Bootstrap */
+require_once __DIR__ . '/admin.php';
+
+$parent_file  = 'upload.php';
+$submenu_file = 'upload.php';
+
+wp_reset_vars( array( 'action' ) );
+
+switch ( $action ) {
+        case 'editattachment':
+        case 'edit':
+                // Used in the HTML title tag.
+                $title = __( 'Edit Media' );
+
+                if ( empty( $errors ) ) {
+                        $errors = null;
+                }
+
+                if ( empty( $_GET['attachment_id'] ) ) {
+                        wp_redirect( admin_url( 'upload.php' ) );
+                        exit;
+                }
+                $att_id = (int) $_GET['attachment_id'];
+
+                wp_redirect( admin_url( "upload.php?item={$att_id}&error=throw-depreciated-media.php" ) );
+                exit;
+
+        default:
+                wp_redirect( admin_url( 'upload.php?error=throw-depreciated-media.php' ) );
+                exit;
+}
