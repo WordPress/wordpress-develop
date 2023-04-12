626e2bfdad29b297bb894da80e58e2c28c07f5b9
Karthik Thayyil <karthik.thayyil@get10up.com>
2023-04-12 12:52:35 +0100

    default false

--- src/wp-includes/functions.wp-scripts.php
+++ src/wp-includes/functions.wp-scripts.php
@@ -170,7 +170,7 @@ function wp_add_inline_script( $handle, $data, $position = 'after', $standalone 
  * @param array             $args     {
  *      Optional. An array of additional script strategies. Default empty array.
  *
- *      @type boolean   $in_footer    Optional. Default true.
+ *      @type boolean   $in_footer    Optional. Default false.
  *      @type string    $strategy     Optional. Values blocking|defer|async. Default 'blocking'.
  * }
  * @return bool Whether the script has been registered. True on success, false on failure.
@@ -347,7 +347,7 @@ function wp_deregister_script( $handle ) {
  * @param array            $args      {
  *      Optional. An array of additional script strategies. Default empty array.
  *
- *      @type boolean   $in_footer    Optional. Default true.
+ *      @type boolean   $in_footer    Optional. Default false.
  *      @type string    $strategy     Optional. Values blocking|defer|async. Default 'blocking'.
  * }
  *
