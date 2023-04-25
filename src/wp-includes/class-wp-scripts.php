9813ba8d5425aa6aba40e876ba1c44615ea3d8d6
Simon <simon.dowdles@10up.com>
2023-04-25 23:44:08 +0200

    Added @since 6.3.0 for newly added has_only_deferrable_dependents function

--- src/wp-includes/class-wp-scripts.php
+++ src/wp-includes/class-wp-scripts.php
@@ -958,6 +958,8 @@ JS;
 	/**
 	 * Check if all of a scripts dependents are deferrable, which is required to maintain execution order.
 	 *
+	 * @since 6.3.0
+	 *
 	 * @param string $handle  The script handle.
 	 * @param array  $checked Optional. An array of already checked script handles, used to avoid recursive loops.
 	 * @return bool True if all dependents are deferrable, false otherwise.
