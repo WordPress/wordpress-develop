c9f0644c68c9664283960a18709d3e484834d47d
Andy Fragen <andy@thefragens.com>
2023-04-28 11:44:52 -0700

    revert

--- tests/phpunit/tests/admin/includes/class-wp-upgrader-testcase.php
+++ tests/phpunit/tests/admin/includes/class-wp-upgrader-testcase.php
@@ -239,10 +239,11 @@ abstract class WP_Upgrader_TestCase extends WP_UnitTestCase {
 	protected function init_theme_data_provider() {
 		require_once __DIR__ . '/themeUpgrader/class-theme-upgrader-data-provider.php';
 
-		if ( ! $this->theme_data_provider instanceof Theme_Upgrader_Data_Provider ) {
-			$this->theme_data_provider = new Theme_Upgrader_Data_Provider();
+		if ( $this->theme_data_provider instanceof Theme_Upgrader_Data_Provider ) {
+			return;
 		}
 
+		$this->theme_data_provider = new Theme_Upgrader_Data_Provider();
 		$this->theme_data_provider->init();
 	}
 
@@ -254,10 +255,11 @@ abstract class WP_Upgrader_TestCase extends WP_UnitTestCase {
 	protected function init_plugin_data_provider() {
 		require_once __DIR__ . '/pluginUpgrader/class-plugin-upgrader-data-provider.php';
 
-		if ( ! $this->plugin_data_provider instanceof Plugin_Upgrader_Data_Provider ) {
-			$this->plugin_data_provider = new Plugin_Upgrader_Data_Provider();
+		if ( $this->plugin_data_provider instanceof Plugin_Upgrader_Data_Provider ) {
+			return;
 		}
 
+		$this->plugin_data_provider = new Plugin_Upgrader_Data_Provider();
 		$this->plugin_data_provider->init();
 	}
 
