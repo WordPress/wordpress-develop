11087416e9b0d3bf6e9369a1b746f334d5a0b079
Todd Lahman <toddlahman@gmail.com>
2023-04-28 10:27:50 -0700

    Cast (int) $cron->time - time() to prevent String - int error

--- src/wp-admin/includes/class-wp-site-health.php
+++ src/wp-admin/includes/class-wp-site-health.php
@@ -2799,7 +2799,7 @@ class WP_Site_Health {
 		}
 
 		foreach ( $this->crons as $id => $cron ) {
-			if ( ( $cron->time - time() ) < $this->timeout_missed_cron ) {
+			if ( ( (int) $cron->time - time() ) < $this->timeout_missed_cron ) {
 				$this->last_missed_cron = $cron->hook;
 				return true;
 			}
@@ -2825,7 +2825,7 @@ class WP_Site_Health {
 		}
 
 		foreach ( $this->crons as $id => $cron ) {
-			$cron_offset = $cron->time - time();
+			$cron_offset = (int) $cron->time - time();
 			if (
 				$cron_offset >= $this->timeout_missed_cron &&
 				$cron_offset < $this->timeout_late_cron
