de9a6e5c96437d462df4c119d149919617192e3f
Monzur Alam <46376609+monzuralam@users.noreply.github.com>
2023-05-11 15:34:47 +0600

    #58296 - css coding standards issue has been fixed

--- src/wp-activate.php
+++ src/wp-activate.php
@@ -107,7 +107,12 @@ function wpmu_activate_stylesheet() {
 	<style type="text/css">
 		.wp-activate-container { width: 90%; margin: 0 auto; }
 		.wp-activate-container form { margin-top: 2em; }
-		#submit, #key { width: 100%; font-size: 24px; box-sizing: border-box; }
+		#submit,
+		#key {
+			width: 100%;
+			font-size: 24px;
+			box-sizing: border-box;
+		}
 		#language { margin-top: 0.5em; }
 		.wp-activate-container .error { background: #f66; color: #333; }
 		span.h3 { padding: 0 8px; font-size: 1.3em; font-weight: 600; }
