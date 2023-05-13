02d89bc44d19ec72a036427a46804a52440f493b
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2023-05-13 13:33:33 +0000

    Tests: Split the tests from `user/author.php` into individual test classes.
    This aims to make `get_the_author_*()` tests more discoverable and easier to expand.
    
    Follow-up to [691/tests], [692/tests], [695/tests], [28362], [31098], [34687], [53147], [54717], [54726], [55591].
    
    See #57841.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@55754 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/user/getTheAuthor.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @group author
+ * @group user
+ *
+ * @covers ::get_the_author
+ */
+class Tests_User_GetTheAuthor extends WP_UnitTestCase {
+	protected static $author_id = 0;
+	protected static $post_id   = 0;
+
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		self::$author_id = $factory->user->create(
+			array(
+				'role'         => 'author',
+				'user_login'   => 'test_author',
+				'display_name' => 'Test Author',
+				'description'  => 'test_author',
+				'user_url'     => 'http://example.com',
+			)
+		);
+
+		self::$post_id = $factory->post->create(
+			array(
+				'post_author'  => self::$author_id,
+				'post_status'  => 'publish',
+				'post_content' => 'content',
+				'post_title'   => 'title',
+				'post_type'    => 'post',
+			)
+		);
+	}
+
+	public function set_up() {
+		parent::set_up();
+
+		setup_postdata( get_post( self::$post_id ) );
+	}
+
+	public function test_get_the_author() {
+		$author_name = get_the_author();
+		$user        = new WP_User( self::$author_id );
+
+		$this->assertSame( $user->display_name, $author_name );
+		$this->assertSame( 'Test Author', $author_name );
+	}
+}
