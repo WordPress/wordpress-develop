26d84de1ab114f93234f81da27892ad1d7c8ef3e
Felix Arntz <felixarntz@google.com>
2023-05-15 13:10:40 -0700

    Make special context lookup slightly more efficient.

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -5461,7 +5461,11 @@ function wp_get_loading_attr_default( $context ) {
 	 * Without this clause, they would already be counted below which skews the number and can result in the first
 	 * post content image being lazy-loaded only because there are images elsewhere in the post content.
 	 */
-	if ( ( 'the_post_thumbnail' === $context || 'wp_get_attachment_image' === $context ) && doing_filter( 'the_content' ) ) {
+	static $special_content_contexts = array(
+		'the_post_thumbnail'      => true,
+		'wp_get_attachment_image' => true,
+	);
+	if ( isset( $special_content_contexts[ $context ] ) && doing_filter( 'the_content' ) ) {
 		return false;
 	}
 
