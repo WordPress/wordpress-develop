8768607685ff9cfe0cb8e0059211a963a2a4bd7a
Jb Audras <audrasjb@git.wordpress.org>
2023-05-16 15:13:50 +0000

    Grouped backports to the 6.1 branch.
    - Media: Prevent CSRF setting attachment thumbnails.
    - Embeds: Add protocol validation for WordPress Embed code.
    - I18N: Introduce sanitization function for locale.
    - Editor: Ensure block comments are of a valid form.
    - Editor: Remove shortcode support from block templates.
    
    Merges [55760-55764] to the 6.1 branch.
    Props dd32, isabel_brison, martinkrcho, matveb, ocean90, paulkevan, peterwilsoncc, timothyblynjacobs, xknown, youknowriad.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.1@55771 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1.2-alpha-54847-src';
+$wp_version = '6.1.2-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
