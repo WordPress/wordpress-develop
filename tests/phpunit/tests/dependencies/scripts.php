bac4a21814ed951d3edfbfe965fba8a944257ce0
Simon <simon.dowdles@10up.com>
2023-05-16 16:14:42 +0200

    Added @covers to test test_get_normalized_script_args

--- tests/phpunit/tests/dependencies/scripts.php
+++ tests/phpunit/tests/dependencies/scripts.php
@@ -692,6 +692,10 @@ EXP;
 	 * Test normalized script args.
 	 *
 	 * @ticket 12009
+	 *
+	 * @covers WP_Scripts::do_item
+	 * @covers WP_Scripts::get_normalized_script_args
+	 * @covers ::wp_enqueue_script
 	 */
 	public function test_get_normalized_script_args() {
 		global $wp_scripts;
