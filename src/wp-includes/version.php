f7ca55eb038767c0be79a35565b3170a999bd0d5
Jb Audras <audrasjb@git.wordpress.org>
2023-05-16 18:38:52 +0000

    Post WordPress 6.1.1 version bump. The 6.2 branch is now `6.2.2-alpha`.
    git-svn-id: https://develop.svn.wordpress.org/branches/6.2@55815 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2.1-src';
+$wp_version = '6.2.2-alpha-55815-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
