93398b562774e29d66633486000d2b1c9add370a
Simon <simon.dowdles@10up.com>
2023-05-16 22:40:13 +0200

    Docblock formatting changes

--- tests/phpunit/tests/dependencies/scripts.php
+++ tests/phpunit/tests/dependencies/scripts.php
@@ -539,6 +539,7 @@ EXP;
 	 * Test valid defer loading strategy cases.
 	 *
 	 * @ticket 12009
+	 *
 	 * @dataProvider data_loading_strategy_with_valid_defer_registration
 	 * @covers WP_Scripts::do_item
 	 * @covers WP_Scripts::get_eligible_loading_strategy
