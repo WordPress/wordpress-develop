e1f5752387c26b4f201fc9df8e5c16a6371532bf
Jb Audras <audrasjb@git.wordpress.org>
2023-05-16 15:35:46 +0000

    Grouped backports to the 5.7 branch.
    - Media: Prevent CSRF setting attachment thumbnails.
    - Embeds: Add protocol validation for WordPress Embed code.
    - I18N: Introduce sanitization function for locale.
    - Editor: Ensure block comments are of a valid form.
    
    Merges [55760-55764] to the 5.7 branch.
    Props dd32, isabel_brison, martinkrcho, matveb, ocean90, paulkevan, peterwilsoncc, timothyblynjacobs, xknown, youknowriad.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.7@55778 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.8-src';
+$wp_version = '5.7.9-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
