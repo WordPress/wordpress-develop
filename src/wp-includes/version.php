5be408c072fcb1ddd308e9a8cf454fc8c5eab34d
Jb Audras <audrasjb@git.wordpress.org>
2023-05-16 16:34:51 +0000

    WordPress 6.2.1.
    git-svn-id: https://develop.svn.wordpress.org/branches/6.2@55792 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2.1-RC1-55740-src';
+$wp_version = '6.2.1-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
