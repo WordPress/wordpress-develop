578399cf5a838461cff69d44b844c1e119444d14
Simon <simon.dowdles@10up.com>
2023-05-18 16:49:36 +0200

    Removed comment that defined what valid loading strategies are

--- src/wp-includes/class-wp-scripts.php
+++ src/wp-includes/class-wp-scripts.php
@@ -309,7 +309,6 @@ class WP_Scripts extends WP_Dependencies {
 			$before_handle = sprintf( "<script%s id='%s-js-before'>\n%s\n</script>\n", $this->type_attr, esc_attr( $handle ), $before_handle );
 		}
 
-		// Eligible loading strategies will only be 'async', 'defer', or ''.
 		$strategy     = $this->get_eligible_loading_strategy( $handle );
 		$after        = ( '' === $strategy ) ? 'after' : 'after-standalone';
 		$after_handle = $this->print_inline_script( $handle, $after, false );
