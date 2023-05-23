ea0f7c5948f291664ae1bd4891ad23fd645ece68
Shalin Shah <sahilshh723@gmail.com>
2023-05-23 17:47:20 +0530

    TestCase Updated Removed the deprecated function from the testcase.

--- tests/phpunit/tests/query/lazyLoadCommentMeta.php
+++ tests/phpunit/tests/query/lazyLoadCommentMeta.php
@@ -30,9 +30,8 @@ class Tests_Lazy_Load_Comment_Meta extends WP_UnitTestCase {
 	public function test_wp_queue_comments_for_comment_meta_lazyload() {
 		$filter = new MockAction();
 		add_filter( 'update_comment_metadata_cache', array( $filter, 'filter' ), 10, 2 );
-		$comments   = array_map( 'get_comment', self::$comment_ids );
+		wp_lazyload_comment_meta( self::$comment_ids );
 		$comment_id = reset( self::$comment_ids );
-		wp_queue_comments_for_comment_meta_lazyload( $comments );
 		get_comment_meta( $comment_id );
 
 		$args             = $filter->get_args();
@@ -49,13 +48,12 @@ class Tests_Lazy_Load_Comment_Meta extends WP_UnitTestCase {
 	public function test_wp_queue_comments_for_comment_meta_lazyload_new_comment() {
 		$filter = new MockAction();
 		add_filter( 'update_comment_metadata_cache', array( $filter, 'filter' ), 10, 2 );
-		$comments   = array_map( 'get_comment', self::$comment_ids );
+		wp_lazyload_comment_meta( self::$comment_ids );
 		$comment_id = self::factory()->comment->create(
 			array(
 				'comment_post_ID' => self::$post_id,
 			)
 		);
-		wp_queue_comments_for_comment_meta_lazyload( $comments );
 		get_comment_meta( $comment_id );
 
 		$args             = $filter->get_args();
