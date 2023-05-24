7a7722d4b56dc659255160ddb3133cf881cffe36
Ari Stathopoulos <aristath@gmail.com>
2023-05-24 10:01:50 +0300

    wp-admin/link-manager.php

--- src/wp-admin/link-manager.php
+++ src/wp-admin/link-manager.php
@@ -39,7 +39,8 @@ if ( $doaction && isset( $_REQUEST['linkcheck'] ) ) {
 	}
 	wp_redirect( $redirect_to );
 	exit;
-} elseif ( ! empty( $_GET['_wp_http_referer'] ) ) {
+}
+if ( ! empty( $_GET['_wp_http_referer'] ) ) {
 	wp_redirect( remove_query_arg( array( '_wp_http_referer', '_wpnonce' ), wp_unslash( $_SERVER['REQUEST_URI'] ) ) );
 	exit;
 }
