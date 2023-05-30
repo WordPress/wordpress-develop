a8dfbf647954025985849cabff816f32982796d5
Michal Czaplinski <mmczaplinski@gmail.com>
2023-05-30 17:20:01 +0200

    Formatting

--- src/wp-includes/behaviors.php
+++ src/wp-includes/behaviors.php
@@ -14,7 +14,7 @@
  * @param array $editor_settings The array of editor settings.
  * @return array A filtered array of editor settings.
  */
-function wp_add_behaviors($settings)
+function wp_add_behaviors( $settings )
 {
 	$theme_data = WP_Theme_JSON_Resolver::get_merged_data()->get_data();
 	if (array_key_exists('behaviors', $theme_data)) {
