a15aae8b4efaddbdc8224a97b7173eb83638e3da
Weston Ruter <westonruter@google.com>
2023-06-05 09:54:18 -0700

    Add missing default after value for position arg

--- src/wp-includes/class-wp-scripts.php
+++ src/wp-includes/class-wp-scripts.php
@@ -500,7 +500,7 @@ class WP_Scripts extends WP_Dependencies {
 	 *                         before the handle or after. Default 'after'.
 	 * @return string Inline script, which may be empty string.
 	 */
-	public function get_inline_script_data( $handle, $position ) {
+	public function get_inline_script_data( $handle, $position = 'after' ) {
 		$data = $this->get_data( $handle, $position );
 		if ( empty( $data ) || ! is_array( $data ) ) {
 			return '';
@@ -520,7 +520,7 @@ class WP_Scripts extends WP_Dependencies {
 	 *                         before the handle or after. Default 'after'.
 	 * @return string Inline script, which may be empty string.
 	 */
-	public function get_inline_script_tag( $handle, $position ) {
+	public function get_inline_script_tag( $handle, $position, $position = 'after' ) {
 		$js = $this->get_inline_script_data( $handle, $position );
 		if ( empty( $js ) ) {
 			return '';
