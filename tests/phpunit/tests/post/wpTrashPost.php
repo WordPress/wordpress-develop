c59da6aafbe3e4282faa933adadb503def749daa
Hug0-Drelon <hugo.drelon@gmail.com>
2023-06-09 01:06:51 +0300

    Apply suggestions from code review.

--- tests/phpunit/tests/post/wpTrashPost.php
+++ tests/phpunit/tests/post/wpTrashPost.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @group post
+ * @covers ::wp_trash_post
+ */
 class Tests_Post_wpTrashPost extends WP_UnitTestCase {
 	/**
 	 * @var WP_Post
@@ -16,60 +20,14 @@ class Tests_Post_wpTrashPost extends WP_UnitTestCase {
 		);
 	}
 
+	/**
+	 * @ticket 58392
+	 * @covers ::wp_trash_post
+	 */
 	public function test_trash_post() {
-		add_filter(
-			'pre_trash_post',
-			function ( $trash, $post, $previous_status ) {
-				$this->assertNull( $trash, 'pre_trash_post first parameter should be null.' );
-				$this->assertSame( $this->post->ID, $post->ID, 'pre_trash_post second parameter should be the trashed post ID.' );
-				$this->assertSame( $this->post->post_status, $previous_status, 'pre_trash_post third parameter should be the previous trashed post status.' );
-
-				return $trash;
-			},
-			10,
-			3
-		);
-		add_action(
-			'wp_trash_post',
-			function ( $post_id, $previous_status ) {
-				$this->assertSame( $this->post->ID, $post_id, 'wp_trash_post first parameter should be the trashed post ID.' );
-				$this->assertSame( $this->post->post_status, $previous_status, 'wp_trash_post second parameter should be the previous trashed post status.' );
-			},
-			10,
-			2
-		);
-		add_action(
-			'trashed_post',
-			function ( $post_id, $previous_status ) {
-				$this->assertSame( $this->post->ID, $post_id, 'trashed_post first parameter should be the trashed post ID.' );
-				$this->assertSame( $this->post->post_status, $previous_status, 'trashed_post second parameter should be the previous trashed post status.' );
-			},
-			10,
-			2
-		);
-
 		$result = wp_trash_post( $this->post->ID );
 
 		$this->assertInstanceOf( 'WP_Post', $result, 'wp_trash_post returned value should be an instance of WP_Post.' );
-
-		$expected_hooks = array(
-			array( 'filter', 'pre_trash_post' ),
-			array( 'action', 'wp_trash_post' ),
-			array( 'action', 'trashed_post' ),
-		);
-
-		foreach ( $expected_hooks as $hook ) {
-			$hook_type = $hook[0];
-			$hook_tag  = $hook[1];
-
-			if ( 'filter' === $hook_type ) {
-				$this->assertGreaterThan( 0, did_filter( $hook_tag ), "$hook_tag filter was not called." );
-
-				continue;
-			}
-
-			$this->assertGreaterThan( 0, did_action( $hook_tag ), "$hook_tag action was not called." );
-		}
 
 		$trashed = get_posts(
 			array(
@@ -87,5 +45,68 @@ class Tests_Post_wpTrashPost extends WP_UnitTestCase {
 		$this->assertSame( $this->post->post_status, reset( $trashed_post_metas['_wp_trash_meta_status'] ), 'Trashed post should have _wp_trash_meta_status meta set to previous post status.' );
 		$this->assertArrayHasKey( '_wp_trash_meta_time', $trashed_post_metas, 'Trashed post should have _wp_trash_meta_time meta set.' );
 		$this->assertCount( 1, $trashed_post_metas['_wp_trash_meta_time'], 'Trashed post should have only one _wp_trash_meta_time meta set.' );
+	}
+
+	/**
+	 * @ticket 58392
+	 * @covers ::wp_trash_post
+	 */
+	public function test_pre_trash_post_hook() {
+		add_filter(
+			'pre_trash_post',
+			function ( $trash, $post, $previous_status ) {
+				$this->assertNull( $trash, 'pre_trash_post first parameter should be null.' );
+				$this->assertSame( $this->post->ID, $post->ID, 'pre_trash_post second parameter should be the trashed post ID.' );
+				$this->assertSame( $this->post->post_status, $previous_status, 'pre_trash_post third parameter should be the previous trashed post status.' );
+
+				return $trash;
+			},
+			10,
+			3
+		);
+
+		wp_trash_post( $this->post->ID );
+
+		$this->assertGreaterThan( 0, did_filter( 'pre_trash_post' ), 'pre_trash_post filter was not called.' );
+	}
+
+	/**
+	 * @ticket 58392
+	 * @covers ::wp_trash_post
+	 */
+	public function test_wp_trash_post_hook() {
+		add_action(
+			'wp_trash_post',
+			function ( $post_id, $previous_status ) {
+				$this->assertSame( $this->post->ID, $post_id, 'wp_trash_post first parameter should be the trashed post ID.' );
+				$this->assertSame( $this->post->post_status, $previous_status, 'wp_trash_post second parameter should be the previous trashed post status.' );
+			},
+			10,
+			2
+		);
+
+		wp_trash_post( $this->post->ID );
+
+		$this->assertGreaterThan( 0, did_action( 'wp_trash_post' ), 'wp_trash_post action was not called.' );
+	}
+
+	/**
+	 * @ticket 58392
+	 * @covers ::wp_trash_post
+	 */
+	public function test_trashed_post_hook() {
+		add_action(
+			'trashed_post',
+			function ( $post_id, $previous_status ) {
+				$this->assertSame( $this->post->ID, $post_id, 'trashed_post first parameter should be the trashed post ID.' );
+				$this->assertSame( $this->post->post_status, $previous_status, 'trashed_post second parameter should be the previous trashed post status.' );
+			},
+			10,
+			2
+		);
+
+		wp_trash_post( $this->post->ID );
+
+		$this->assertGreaterThan( 0, did_action( 'trashed_post' ), 'trashed_post action was not called.' );
 	}
 }
--- tests/phpunit/tests/post/wpUntrashPost.php
+++ tests/phpunit/tests/post/wpUntrashPost.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @group post
+ * @covers ::wp_untrash_post
+ */
 class Tests_Post_wpUntrashPost extends WP_UnitTestCase {
 	/**
 	 * @var WP_Post
@@ -18,60 +22,14 @@ class Tests_Post_wpUntrashPost extends WP_UnitTestCase {
 		);
 	}
 
+	/**
+	 * @ticket 58392
+	 * @covers ::wp_untrash_post
+	 */
 	public function test_untrash_post() {
-		add_filter(
-			'pre_untrash_post',
-			function ( $trash, $post, $previous_status ) {
-				$this->assertNull( $trash, 'pre_untrash_post first parameter should be null.' );
-				$this->assertSame( $this->trashed_post->ID, $post->ID, 'pre_untrash_post second parameter should be the trashed post ID.' );
-				$this->assertSame( $this->trashed_post->post_status, $previous_status, 'pre_untrash_post third parameter should be the previous trashed post status.' );
-
-				return $trash;
-			},
-			10,
-			3
-		);
-		add_action(
-			'untrash_post',
-			function ( $post_id, $previous_status ) {
-				$this->assertSame( $this->trashed_post->ID, $post_id, 'untrash_post first parameter should be the trashed post ID.' );
-				$this->assertSame( $this->trashed_post->post_status, $previous_status, 'untrash_post second parameter should be the previous trashed post status.' );
-			},
-			10,
-			2
-		);
-		add_action(
-			'untrashed_post',
-			function ( $post_id, $previous_status ) {
-				$this->assertSame( $this->trashed_post->ID, $post_id, 'untrashed_post first parameter should be the trashed post ID.' );
-				$this->assertSame( $this->trashed_post->post_status, $previous_status, 'untrashed_post second parameter should be the previous trashed post status.' );
-			},
-			10,
-			2
-		);
-
 		$result = wp_untrash_post( $this->trashed_post->ID );
 
 		$this->assertInstanceOf( 'WP_Post', $result, 'wp_untrash_post returned value should be an instance of WP_Post.' );
-
-		$expected_hooks = array(
-			array( 'filter', 'pre_untrash_post' ),
-			array( 'action', 'untrash_post' ),
-			array( 'action', 'untrashed_post' ),
-		);
-
-		foreach ( $expected_hooks as $hook ) {
-			$hook_type = $hook[0];
-			$hook_tag  = $hook[1];
-
-			if ( 'filter' === $hook_type ) {
-				$this->assertGreaterThan( 0, did_filter( $hook_tag ), "$hook_tag filter was not called." );
-
-				continue;
-			}
-
-			$this->assertGreaterThan( 0, did_action( $hook_tag ), "$hook_tag action was not called." );
-		}
 
 		$trashed = get_posts(
 			array(
@@ -90,5 +48,68 @@ class Tests_Post_wpUntrashPost extends WP_UnitTestCase {
 		$post = get_post( $this->trashed_post->ID );
 
 		$this->assertSame( 'draft', $post->post_status, 'Untrashed post should have its previous status set correctly.' );
+	}
+
+	/**
+	 * @ticket 58392
+	 * @covers ::wp_untrash_post
+	 */
+	public function test_pre_untrash_post_hook() {
+		add_filter(
+			'pre_untrash_post',
+			function ( $trash, $post, $previous_status ) {
+				$this->assertNull( $trash, 'pre_untrash_post first parameter should be null.' );
+				$this->assertSame( $this->trashed_post->ID, $post->ID, 'pre_untrash_post second parameter should be the trashed post ID.' );
+				$this->assertSame( $this->trashed_post->post_status, $previous_status, 'pre_untrash_post third parameter should be the previous trashed post status.' );
+
+				return $trash;
+			},
+			10,
+			3
+		);
+
+		wp_untrash_post( $this->trashed_post->ID );
+
+		$this->assertGreaterThan( 0, did_filter( 'pre_untrash_post' ), 'pre_untrash_post filter was not called.' );
+	}
+
+	/**
+	 * @ticket 58392
+	 * @covers ::wp_untrash_post
+	 */
+	public function test_untrash_post_hook() {
+		add_action(
+			'untrash_post',
+			function ( $post_id, $previous_status ) {
+				$this->assertSame( $this->trashed_post->ID, $post_id, 'untrash_post first parameter should be the trashed post ID.' );
+				$this->assertSame( $this->trashed_post->post_status, $previous_status, 'untrash_post second parameter should be the previous trashed post status.' );
+			},
+			10,
+			2
+		);
+
+		wp_untrash_post( $this->trashed_post->ID );
+
+		$this->assertGreaterThan( 0, did_action( 'untrash_post' ), 'untrash_post action was not called.' );
+	}
+
+	/**
+	 * @ticket 58392
+	 * @covers ::wp_untrash_post
+	 */
+	public function test_untrashed_post_hook() {
+		add_action(
+			'untrashed_post',
+			function ( $post_id, $previous_status ) {
+				$this->assertSame( $this->trashed_post->ID, $post_id, 'untrashed_post first parameter should be the trashed post ID.' );
+				$this->assertSame( $this->trashed_post->post_status, $previous_status, 'untrashed_post second parameter should be the previous trashed post status.' );
+			},
+			10,
+			2
+		);
+
+		wp_untrash_post( $this->trashed_post->ID );
+
+		$this->assertGreaterThan( 0, did_action( 'untrashed_post' ), 'untrashed_post action was not called.' );
 	}
 }
