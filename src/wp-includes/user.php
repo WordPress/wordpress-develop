f1531c8c10e27fc3de652e464459c2c276eb4518
Dominik Schilling <dominikschilling+git@gmail.com>
2023-06-09 09:44:36 +0300

    Add back support for WP_Error

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -5016,7 +5016,7 @@ function wp_cache_set_users_last_changed() {
  * @since 6.3.0
  *
  * @param int|WP_User $user The user to check.
- * @return bool
+ * @return bool|WP_Error True if allowed, false or WP_Error otherwise.
  */
 function wp_is_password_reset_allowed_for_user( $user ) {
 	if ( ! is_object( $user ) ) {
