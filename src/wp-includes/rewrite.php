6f9f70ab43765df7c5d35c0dd99d698cd0fc5e7c
Roland Soos <roland@nextendweb.com>
2023-06-12 15:40:27 +0200

    Update rewrite.php

--- src/wp-includes/rewrite.php
+++ src/wp-includes/rewrite.php
@@ -649,7 +649,7 @@ function url_to_postid( $url ) {
 			if ( ! empty( $query->posts ) && $query->is_singular ) {
 				return $query->post->ID;
 			} else {
-				if ($query->get_queried_object() instanceof WP_POST) {
+				if ( $query->get_queried_object() instanceof WP_POST ) {
 					return $query->queried_object_id;
 				}
 
