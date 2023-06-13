4d3eb2d65445efbd607e4ce21574b2b0daae6286
Dylan Jech <dylan.jech@thomsonreuters.com>
2023-06-13 07:59:51 -0500

    Added check to ensure 'password' is defined in options before using it. password is optional when using pub/private keys

--- src/wp-admin/includes/class-wp-filesystem-ssh2.php
+++ src/wp-admin/includes/class-wp-filesystem-ssh2.php
@@ -150,7 +150,7 @@ class WP_Filesystem_SSH2 extends WP_Filesystem_Base {
 				return false;
 			}
 		} else {
-			if ( ! @ssh2_auth_pubkey_file( $this->link, $this->options['username'], $this->options['public_key'], $this->options['private_key'], $this->options['password'] ) ) {
+			if ( ! @ssh2_auth_pubkey_file( $this->link, $this->options['username'], $this->options['public_key'], $this->options['private_key'], $this->options['password'] ?? null ) ) {
 				$this->errors->add(
 					'auth',
 					sprintf(
