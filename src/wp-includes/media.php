af34f08f3f5317ac77110b56562e3a54881ad093
Karthik Thayyil <30643833+kt-12@users.noreply.github.com>
2023-06-13 14:02:03 +0530

    fix array usage in comment
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -5695,11 +5695,10 @@ function wp_get_loading_attr_default( $context ) {
  *
  * @since 6.3.0
  *
- * @param string  $tag_name The tag name.
- * @param array[] $attr     Array of the attributes for the tag.
- * @param string  $context  Context for the element for which the loading optimization attribute is requested.
- *
- * @return array[] Loading optimization attributes.
+ * @param string $tag_name The tag name.
+ * @param array  $attr     Array of the attributes for the tag.
+ * @param string $context  Context for the element for which the loading optimization attribute is requested.
+ * @return array Loading optimization attributes.
  */
 function wp_get_loading_optimization_attributes( $tag_name, $attr, $context ) {
 	global $wp_query;
