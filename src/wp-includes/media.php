91bed5cb68822772ecee56e82a21a2e6e3c17bc0
Karthik Thayyil <30643833+kt-12@users.noreply.github.com>
2023-06-14 16:39:50 +0530

    remove extra space
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -1980,7 +1980,6 @@ function wp_img_tag_add_loading_attr( $image, $context ) {
  *
  * @param string $image   The HTML `img` tag where the attribute should be added.
  * @param string $context Additional context to pass to the filters.
- *
  * @return string Converted `img` tag with optimization attributes added.
  */
 function wp_img_tag_add_loading_optimization_attrs( $image, $context ) {
