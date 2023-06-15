8dc7158147762ca375d77e0d9681b2553eeddd8f
Alex Lende <alex@lende.xyz>
2023-06-15 14:09:20 -0500

    Add duotone hooks

--- src/wp-includes/block-supports/duotone.php
+++ src/wp-includes/block-supports/duotone.php
@@ -40,6 +40,28 @@ WP_Block_Supports::get_instance()->register(
 	)
 );
 
+// Set up metadata prior to rendering any blocks.
+add_action( 'wp_loaded', array( 'WP_Duotone', 'set_global_styles_presets' ), 10 );
+add_action( 'wp_loaded', array( 'WP_Duotone', 'set_global_style_block_names' ), 10 );
+
+// Add classnames to blocks using duotone support.
+add_filter( 'render_block', array( 'WP_Duotone', 'render_duotone_support' ), 10, 2 );
+
+// Enqueue styles.
+// Block styles (core-block-supports-inline-css) before the style engine (gutenberg_enqueue_stored_styles).
+// Global styles (global-styles-inline-css) after the other global styles (gutenberg_enqueue_global_styles).
+add_action( 'wp_enqueue_scripts', array( 'WP_Duotone', 'output_block_styles' ), 9 );
+add_action( 'wp_enqueue_scripts', array( 'WP_Duotone', 'output_global_styles' ), 11 );
+
+// Add SVG filters to the footer. Also, for classic themes, output block styles (core-block-supports-inline-css).
+add_action( 'wp_footer', array( 'WP_Duotone', 'output_footer_assets' ), 10 );
+
+// Add styles and SVGs for use in the editor via the EditorStyles component.
+add_filter( 'block_editor_settings_all', array( 'WP_Duotone', 'add_editor_settings' ), 10 );
+
+// Migrate the old experimental duotone support flag.
+add_filter( 'block_type_metadata_settings', array( 'WP_Duotone', 'migrate_experimental_duotone_support_flag' ), 10, 2 );
+
 /*
  * Deprecated functions below. All new functions should be added in class-wp-duotone.php.
  */
