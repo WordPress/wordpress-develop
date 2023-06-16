7c78729893f79c3b5fc9f1e87c4b3c577b0b6a97
MaggieCabrera <maggie.cabrera@automattic.com>
2023-06-16 15:37:44 +0200

    Removed script to include the live preview button

--- src/wp-admin/includes/theme-previews.php
+++ src/wp-admin/includes/theme-previews.php
@@ -51,67 +51,6 @@ function wp_attach_theme_preview_middleware() {
 }
 
 /**
- * Temporary function to add a live preview button to block themes.
- * Remove when https://core.trac.wordpress.org/ticket/58190 lands.
- */
-function add_live_preview_button() {
-	global $pagenow;
-	if ( 'themes.php' === $pagenow ) {
-		?>
-<script type="text/javascript">
-	jQuery( document ).ready( function() {
-		addLivePreviewButton();
-		//themes are loaded as we scroll so we need to add the button to the newer ones.
-		jQuery('.themes').on('DOMSubtreeModified', function(){
-			addLivePreviewButton();
-		});
-	});
-	function addLivePreviewButton() {
-		document.querySelectorAll('.theme').forEach((el, index) => {
-			const themeInfo = el.querySelector('.theme-id-container');
-			const canAddButton =
-				!themeInfo ||
-				el.classList.contains('active') ||
-				themeInfo.querySelector('.theme-actions')?.childElementCount > 1;
-			if ( canAddButton ) {
-				return;
-			}
-			const themePath = themeInfo.querySelector('h2.theme-name').id.replace('-name', '');
-			const themeName = themeInfo.querySelector('h2.theme-name').innerText;
-			const livePreviewButton = document.createElement('a');
-			<?php
-				/* translators: %s: theme name */
-				$button_label = esc_attr_x( 'Live Preview %s', 'theme' );
-			?>
-			livePreviewButton.setAttribute('aria-label', '<?php echo $button_label; ?>'.replace('%s', themeName));
-			livePreviewButton.setAttribute('class', 'button button-primary');
-			livePreviewButton.setAttribute(
-				'href',
-				`/wp-admin/site-editor.php?wp_theme_preview=${themePath}&return=themes.php`
-			);
-			livePreviewButton.innerHTML = '<?php echo esc_html_e( 'Live Preview' ); ?>';
-			themeInfo.querySelector('.theme-actions').appendChild(livePreviewButton);
-		});
-	}
-</script>
-		<?php
-	}
-
-}
-
-/**
- * Adds a nonce for the theme activation link.
- */
-function block_theme_activate_nonce() {
-	$nonce_handle = 'switch-theme_' . wp_get_theme_preview_path();
-	?>
-<script type="text/javascript">
-	window.BLOCK_THEME_ACTIVATE_NONCE = '<?php echo wp_create_nonce( $nonce_handle ); ?>';
-</script>
-	<?php
-}
-
-/**
  * Attaches filters to enable theme previews in the Site Editor.
  */
 if ( ! empty( $_GET['wp_theme_preview'] ) ) {
@@ -119,5 +58,3 @@ if ( ! empty( $_GET['wp_theme_preview'] ) ) {
 	add_filter( 'template', 'wp_get_theme_preview_path' );
 	add_filter( 'init', 'wp_attach_theme_preview_middleware' );
 }
-add_action( 'admin_head', 'block_theme_activate_nonce' );
-add_action( 'admin_print_footer_scripts', 'add_live_preview_button', 11 );
