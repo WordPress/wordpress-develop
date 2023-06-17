5dac270b246403a5759df59f8f2486309b7fedd8
Jonny Harris <jon@spacedmonkey.co.uk>
2023-06-17 18:45:09 +0100

    Feedback.

--- src/wp-includes/blocks/index.php
+++ src/wp-includes/blocks/index.php
@@ -13,11 +13,15 @@ require BLOCKS_PATH . 'widget-group.php';
 require BLOCKS_PATH . 'require-dynamic-blocks.php';
 
 /**
- * Registers core block types styles.
+ * Registers core block style handles.
+ *
+ * While {@see register_block_style_handle()} is typically used for that, the way it is
+ * implemented is inefficient for core block styles. Registering those style handles here
+ * avoids unnecessary logic and filesystem lookups in the other function.
  *
  * @since 6.3.0
  */
-function register_core_block_types_styles() {
+function register_core_block_style_handles() {
 	static $core_blocks_meta;
 	if ( ! $core_blocks_meta ) {
 		$core_blocks_meta = require ABSPATH . WPINC . '/blocks/blocks-json.php';
@@ -30,11 +34,13 @@ function register_core_block_types_styles() {
 		'style',
 	);
 
+	$wp_styles = wp_styles();
+
 	foreach ( $core_blocks_meta as $name => $schema ) {
 		foreach ( $style_fields as $style_field ) {
 			if ( ! isset( $schema[ $style_field ] ) ) {
 				$style_handle = 'style' === $style_field ? "wp-block-{$name}" : "wp-block-{$name}-editor";
-				wp_register_style(
+				$wp_styles->add(
 					$style_handle,
 					false
 				);
@@ -45,20 +51,20 @@ function register_core_block_types_styles() {
 				continue;
 			}
 			$path = "/wp-includes/blocks/$name/style{$suffix}.css";
-			wp_register_style( $style_handle, $path );
-			wp_style_add_data( $style_handle, 'path', ABSPATH . $path );
+			$wp_styles->add( $style_handle, $path );
+			$wp_styles->add_data( $style_handle, 'path', ABSPATH . $path );
 
 			$rtl_file = str_replace( "{$suffix}.css", "-rtl{$suffix}.css", ABSPATH . $path );
 			if ( is_rtl() && file_exists( $rtl_file ) ) {
-				wp_style_add_data( $style_handle, 'rtl', 'replace' );
-				wp_style_add_data( $style_handle, 'suffix', $suffix );
-				wp_style_add_data( $style_handle, 'path', $rtl_file );
+				$wp_styles->add_data( $style_handle, 'rtl', 'replace' );
+				$wp_styles->add_data( $style_handle, 'suffix', $suffix );
+				$wp_styles->add_data( $style_handle, 'path', $rtl_file );
 			}
 
 		}
 	}
 }
-add_action( 'init', 'register_core_block_types_styles', 9 );
+add_action( 'init', 'register_core_block_style_handles', 9 );
 
 /**
  * Registers core block types using metadata files.
