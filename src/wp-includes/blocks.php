f0b96eb56900276a007ad02b48b3ece92970f489
dilipbheda <bhedadilip8462@gmail.com>
2023-06-19 11:51:46 +0530

    Improve inline document for register_block_style

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -1166,7 +1166,7 @@ function block_version( $content ) {
  *
  * @param string $block_name       Block type name including namespace.
  * @param array  $style_properties Array containing the properties of the style name,
- *                                 label, style (name of the stylesheet to be enqueued),
+ *                                 label, style_handle (name of the stylesheet to be enqueued),
  *                                 inline_style (string containing the CSS to be added).
  * @return bool True if the block style was registered with success and false otherwise.
  */
