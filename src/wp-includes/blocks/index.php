6567ae6aea0795d767f8d42c1faee2e07b1e9eb1
Jonny Harris <jon@spacedmonkey.co.uk>
2023-06-20 12:47:38 +0100

    Reusable.

--- src/wp-includes/blocks/index.php
+++ src/wp-includes/blocks/index.php
@@ -56,7 +56,7 @@ function register_core_block_style_handles() {
 			$style_path = "blocks/{$name}/style{$suffix}.css";
 			$wp_styles->add( $style_handle, $includes_url . $style_path );
 			$path = ABSPATH . WPINC . DIRECTORY_SEPARATOR . $style_path;
-			$wp_styles->add_data( $style_handle, 'path', ABSPATH . WPINC . DIRECTORY_SEPARATOR . $style_path );
+			$wp_styles->add_data( $style_handle, 'path', $path );
 
 			$rtl_file = str_replace( "{$suffix}.css", "-rtl{$suffix}.css", $path );
 			if ( is_rtl() && file_exists( $rtl_file ) ) {
