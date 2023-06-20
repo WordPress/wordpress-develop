7b79ce296b631ad54d705c6da22f6dd55f53f382
Jonny Harris <jon@spacedmonkey.co.uk>
2023-06-20 15:14:51 +0100

    Remove require in wp-settings.php.

--- src/wp-settings.php
+++ src/wp-settings.php
@@ -318,8 +318,6 @@ require ABSPATH . WPINC . '/class-wp-block-styles-registry.php';
 require ABSPATH . WPINC . '/class-wp-block-type-registry.php';
 require ABSPATH . WPINC . '/class-wp-block.php';
 require ABSPATH . WPINC . '/class-wp-block-list.php';
-require ABSPATH . WPINC . '/class-wp-block-parser-block.php';
-require ABSPATH . WPINC . '/class-wp-block-parser-frame.php';
 require ABSPATH . WPINC . '/class-wp-block-parser.php';
 require ABSPATH . WPINC . '/blocks.php';
 require ABSPATH . WPINC . '/blocks/index.php';
