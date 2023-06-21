95fd9ffbb1aa53701244cd2fafdb135ed9585b47
Dave Smith <getdavemail@gmail.com>
2023-06-21 10:23:01 +0100

    Inline comment

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -521,8 +521,7 @@ function create_initial_post_types() {
 				'filter_items_list'     => __( 'Filter Navigation Menu list' ),
 				'items_list_navigation' => __( 'Navigation Menus list navigation' ),
 				'items_list'            => __( 'Navigation Menus list' ),
-				/* internal use only. don't use this when registering your own post type. */
-				'_edit_link'            => $navigation_post_edit_link,
+				'_edit_link'            => $navigation_post_edit_link, /* internal use only. don't use this when registering your own post type. */
 			),
 			'description'           => __( 'Navigation menus that can be inserted into your site.' ),
 			'public'                => false,
