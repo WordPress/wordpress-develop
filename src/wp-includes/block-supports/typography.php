3380152ef3b4988edf0c84906d608719dc2c2d47
ramon <ramonjd@gmail.com>
2023-06-21 10:20:04 +1000

    Added annotation

--- src/wp-includes/block-supports/typography.php
+++ src/wp-includes/block-supports/typography.php
@@ -10,6 +10,7 @@
  * Registers the style and typography block attributes for block types that support it.
  *
  * @since 5.6.0
+ * @since 6.3.0 Added support for text-columns.
  * @access private
  *
  * @param WP_Block_Type $block_type Block Type.
