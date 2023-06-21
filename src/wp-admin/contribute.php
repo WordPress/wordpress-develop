191745a5782272980d4e3beb91bd9758b0f54a02
Olga Gleckler <oglekler@gmail.com>
2023-06-21 09:11:14 +0300

    Code review

--- src/wp-admin/contribute.php
+++ src/wp-admin/contribute.php
@@ -43,7 +43,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 			<img src="data:image/svg+xml,%3Csvg width='436' height='436' viewbox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='100%25' height='100%25' fill='%231d35b4' /%3E%3C/svg%3E" alt="" />
 		</div>
 		<div class="column is-vertically-aligned-center">
-			<p><?php _e( 'You can contribute to WordPress development bringing your ideas, collaborating on subjects close to your heart and trying many different roles and possibilities, learning continuously and meeting people who share passion to WordPress' ); ?></p>
+			<p><?php _e( 'You can contribute to WordPress development bringing your ideas, collaborating on subjects close to your heart and trying many different roles and possibilities, learning continuously and meeting people who share passion to WordPress.' ); ?></p>
 
 			<p>
 				<?php
@@ -67,10 +67,10 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 	<div class="about__section has-2-columns">
 		<div class="column is-vertically-aligned-center">
 			<ul>
-				<li><?php _e( 'Pick a team' ); ?></li>
+				<li><?php _e( 'Choose a team' ); ?></li>
 				<li><?php _e( 'Follow its news to see how you can participate' ); ?></li>
-				<li><?php _e( "Attend team's meetings" ); ?></li>
-				<li><?php _e( 'Ask for Good First issue to solve' ); ?></li>
+				<li><?php _e( "Attend team&#8217;s meetings" ); ?></li>
+				<li><?php _e( 'Ask for a Good First issue to solve' ); ?></li>
 			</ul>
 		</div>
 		<div class="column">
@@ -83,10 +83,10 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 		<div class="column is-vertically-aligned-center">
 			<h3 class="is-smaller-heading"><?php _e( 'Non-technical areas' ); ?></h3>
 			<ul>
-				<li><a href="<?php _e( '#' ); ?>"><?php _e( 'Translate strings for WordPress and related sites' ); ?></a></li>
-				<li><a href="<?php _e( '#' ); ?>"><?php _e( 'Submit photography for Photo Directory' ); ?></a></li>
-				<li><a href="<?php _e( '#' ); ?>"><?php _e( 'Help someone on the Support forum' ); ?></a></li>
-				<li><a href="<?php _e( '#' ); ?>"><?php _e( 'Translate WordPress News articles into your native language' ); ?></a></li>
+				<li><?php _e( 'Translate strings for WordPress, plugins, themes and other WordPress.org related projects' ); ?></li>
+				<li><?php _e( 'Submit photography for Photo Directory' ); ?></li>
+				<li><?php _e( 'Help someone on the Support forum' ); ?></li>
+				<li><?php _e( 'Translate WordPress News articles into your native language' ); ?></li>
 				<li><?php _e( 'Test WordPress before releases, checking how new features and fixes works' ); ?></li>
 			</ul>
 		</div>
@@ -94,19 +94,24 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 		<div class="column is-vertically-aligned-center">
 			<h3 class="is-smaller-heading"><?php _e( 'Technical areas' ); ?></h3>
 			<ul>
-				<li><?php _e( 'Pick your favourite language or library: PHP, JavaScript, Kotlin, Java, Swift, Objective-C, Vue, Python, TypeScript' ); ?></li>
+				<li><?php _e( 'Pick your favourite language or library:' ); ?>
+					<ul>
+						<li><?php _e( 'HTML, CSS, PHP, JavaScript and React for the Core of the CMS and the Block Editor (Gutenberg project' ); ?></li>
+						<li><?php _e( 'Kotlin, Java, Swift, Objective-C, Vue, Python, TypeScript for mobile apps' ); ?></li>
+					</ul>
+				</li>
 				<li><?php _e( 'Choose what you want to do: testing, creating patches, working on documentation' ); ?></li>
 			</ul>
 		</div>
 		<div class="column">
-			<img src="data:image/svg+xml,%3Csvg width='436' height='218' viewbox='0 0 100 50' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='100%25' height='100%25' fill='%231d35b4' /%3E%3C/svg%3E" alt="" />
+			<img src="data:image/svg+xml,%3Csvg width='436' height='280' viewbox='0 0 100 50' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='100%25' height='100%25' fill='%231d35b4' /%3E%3C/svg%3E" alt="" />
 		</div>
 
 	</div>
 
 	<div class="about__section">
 		<div class="column aligncenter">
-			<p><a href="<?php _e( '#' ); ?>"><?php _e( 'Orientation tool' ); ?></a></p>
+			<p><a href="https://make.wordpress.org/contribute/" target="_blank"><?php _e( 'Discover your Make WordPress team' ); ?></a></p>
 		</div>
 	</div>
 
