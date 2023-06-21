c5231b6996b1004dfae4083b22102edb2cd4b75f
Karthik Thayyil <karthik.thayyil@get10up.com>
2023-06-21 15:56:32 +0100

    coding standards fix

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -1058,8 +1058,9 @@ function wp_get_attachment_image( $attachment_id, $size = 'thumbnail', $icon = f
 		 * @param string $context The context. Default 'wp_get_attachment_image'.
 		 */
 		$context = apply_filters( 'wp_get_attachment_image_context', 'wp_get_attachment_image' );
+		$attr    = wp_parse_args( $attr, $default_attr );
 
-		$loading_attr              = $attr = wp_parse_args( $attr, $default_attr );
+		$loading_attr              = $attr;
 		$loading_attr['width']     = $width;
 		$loading_attr['height']    = $height;
 		$loading_optimization_attr = wp_get_loading_optimization_attributes(
