bca353a28e742c5fb56c16538ae6880eafab86ee
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-06-22 16:01:00 +1000

    Revert "Force the E2E tests to fail. Alwasy. Not Sometimes."
    This reverts commit 9d10ecaf567249cf56bc82990609508b2ac0c3b5.

--- tests/e2e/specs/dashboard.test.js
+++ tests/e2e/specs/dashboard.test.js
@@ -44,7 +44,7 @@ describe( 'Quick Draft', () => {
 
 		expect(
 			await postsListDraft.evaluate( ( element ) => element.innerText )
-		).toContain( 'Peter edited this to force the tests to fail. Note to self: Revert.' );
+		).toContain( 'Test Draft Title' );
 	} );
 
 	it( 'Allows draft to be created without Title or Content', async () => {
