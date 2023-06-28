5ba0c89e1b92059df7424e4c09e557b348b21363
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-06-28 15:40:18 +1000

    CS: Whitespace fix.

--- src/wp-includes/global-styles-and-settings.php
+++ src/wp-includes/global-styles-and-settings.php
@@ -412,16 +412,16 @@ function wp_theme_has_theme_json() {
 	} else {
 		$path = get_template_directory() . '/theme.json';
 	}
-	
+
 	/** This filter is documented in wp-includes/link-template.php */
 	$path = apply_filters( 'theme_file_path', $path, 'theme.json' );
-	
+
 	if ( file_exists( $path ) ) {
 		$theme_has_support = true;
 	} else {
 		$theme_has_support = false;
 	}
-	
+
 	return $theme_has_support;
 }
 
