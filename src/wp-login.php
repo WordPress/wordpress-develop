c171904214af20696dd8932d9e09d5206f5ef635
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2023-06-28 19:14:01 -0500

    Give the button a `pwd-toggle` class and a hidden text span

--- src/wp-login.php
+++ src/wp-login.php
@@ -1432,8 +1432,9 @@ switch ( $action ) {
 				<label for="user_pass"><?php _e( 'Password' ); ?></label>
 				<div class="wp-pwd">
 					<input type="password" name="pwd" id="user_pass"<?php echo $aria_describedby; ?> class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required" />
-					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Show password' ); ?>">
+					<button type="button" class="button button-secondary wp-hide-pw pwd-toggle hide-if-no-js" data-toggle="0" aria-label="<?php esc_attr_e( 'Show password' ); ?>">
 						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+						<span class="text hidden" aria-hidden="true"></span>
 					</button>
 				</div>
 			</div>
