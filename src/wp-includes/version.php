1815f4c76c9f51665f046d79284552d09849597b
David Baumwald <davidbaumwald@git.wordpress.org>
2023-07-03 17:05:33 +0000

    Post WordPress 6.3 Beta 3 version bump.
    git-svn-id: https://develop.svn.wordpress.org/trunk@56130 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-beta3-src';
+$wp_version = '6.3-beta3-56130-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
