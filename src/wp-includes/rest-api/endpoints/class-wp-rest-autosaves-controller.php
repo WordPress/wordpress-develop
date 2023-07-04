2ef8c6fae3bffac14c5fa37aafc4634e3f0a5ca5
Adam Silverstein <adamjs@google.com>
2023-07-04 17:29:35 -0600

    When autosave unchanged, return existing autosave

--- src/wp-includes/rest-api/endpoints/class-wp-rest-autosaves-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-autosaves-controller.php
@@ -360,10 +360,26 @@ class WP_REST_Autosaves_Controller extends WP_REST_Revisions_Controller {
 			return $post;
 		}
 
+		// Only create an autosave when it is different from the saved post.
+		$autosave_is_different = false;
+		$new_autosave          = _wp_post_revision_data( $post_data, true );
+
+		foreach ( array_intersect( array_keys( $new_autosave ), array_keys( _wp_post_revision_fields( $post ) ) ) as $field ) {
+			if ( normalize_whitespace( $new_autosave[ $field ] ) !== normalize_whitespace( $post->$field ) ) {
+				$autosave_is_different = true;
+				break;
+			}
+		}
+
 		$user_id = get_current_user_id();
 
 		// Store one autosave per author. If there is already an autosave, overwrite it.
 		$old_autosave = wp_get_post_autosave( $post_id, $user_id );
+
+		if ( ! $autosave_is_different && $old_autosave  ) {
+			// Nothing to save, return the existing autosave.
+			return $old_autosave->ID;
+		}
 
 		if ( $old_autosave ) {
 			$new_autosave['ID']          = $old_autosave->ID;
