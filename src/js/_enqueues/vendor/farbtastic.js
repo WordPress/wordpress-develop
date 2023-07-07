fdd54220e19f9bda06a6d940d0df3164b2aeab52
mrinal013 <mrinalhaque99@gmail.com>
2023-07-07 18:55:12 +0600

    mousedown event issue fixed

--- src/js/_enqueues/vendor/farbtastic.js
+++ src/js/_enqueues/vendor/farbtastic.js
@@ -262,7 +262,7 @@ $._farbtastic = function (container, callback) {
   };
 
   // Install mousedown handler (the others are set on the document on-demand)
-  $(document).on('mousedown', '*', e, fb.mousedown)
+  $( '*', e ).on('mousedown', '*', e, fb.mousedown)
 
     // Init color
   fb.setColor('#000000');
