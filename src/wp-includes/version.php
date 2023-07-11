ba1ac233ff3b6f471f561f48d94fbf0f219d45db
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2023-07-11 16:11:25 +0000

    WordPress 6.3 Beta 4.
    git-svn-id: https://develop.svn.wordpress.org/trunk@56215 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-beta3-56130-src';
+$wp_version = '6.3-beta4-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
