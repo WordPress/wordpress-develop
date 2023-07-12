8b7f28c9a94fd86a104c5ebca00162d7c1eef578
Olga Gleckler <oglekler@gmail.com>
2023-07-12 08:10:17 +0300

    white spaces change

--- src/wp-admin/contribute.php
+++ src/wp-admin/contribute.php
@@ -108,7 +108,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 		<div class="column">
 			<h2><?php _e( 'Help Make WordPress Better' ); ?></h2>
 			<p><?php _e( 'Whether you speak at a local meetup, provide your perspective on a feature proposal, or submit a bug report, your contributions will make a difference.' ); ?></p>
-		<p class="aligncenter"><a href="https://make.wordpress.org/contribute/" target="_blank"><?php _e( 'Find your team &rarr;' ); ?></a></p>
+			<p class="aligncenter"><a href="https://make.wordpress.org/contribute/" target="_blank"><?php _e( 'Find your team &rarr;' ); ?></a></p>
 		</div>
 	</div>
 
