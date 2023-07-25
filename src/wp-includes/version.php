b49c476401e77323238ea7771f76543e8ccc53b2
Jb Audras <audrasjb@git.wordpress.org>
2023-07-25 16:38:34 +0000

    Post WordPress 6.3 RC2 version bump.
    git-svn-id: https://develop.svn.wordpress.org/branches/6.3@56307 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-RC2-src';
+$wp_version = '6.3-RC2-56307-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
