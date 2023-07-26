885640d8c0a5501deb963546735231bdee4c18a6
Adam Silverstein <adamjs@google.com>
2023-07-26 10:03:44 -0600

    Adjust expected for `test_should_register_persisted_preferences_meta`

--- tests/phpunit/tests/user/wpRegisterPersistedPreferencesMeta.php
+++ tests/phpunit/tests/user/wpRegisterPersistedPreferencesMeta.php
@@ -52,6 +52,7 @@ class Tests_User_WpRegisterPersistedPreferencesMeta extends WP_UnitTestCase {
 						'additionalProperties' => true,
 					),
 				),
+				'revisions_enabled' => false,
 			),
 			$wp_meta_keys['user'][''][ $meta_key ],
 			'The registered metadata did not have the expected structure'
