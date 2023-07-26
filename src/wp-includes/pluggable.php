0066c46d9afe7a3cb2701a108c16078d1b0387a7
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2023-07-26 14:49:54 +0300

    Destroy logged in sessions after successfully password reset Trac ticket: https://core.trac.wordpress.org/ticket/58911

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -2780,6 +2780,9 @@ if ( ! function_exists( 'wp_set_password' ) ) :
 
 		clean_user_cache( $user_id );
 
+		$user_sessions = WP_Session_Tokens::get_instance( $user_id );
+		$user_sessions->destroy_all();
+
 		/**
 		 * Fires after the user password is set.
 		 *
