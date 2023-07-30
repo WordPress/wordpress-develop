d6d2d8fa866eb1e32b1966eaf7426a31bda1d204
Rajinsharwar <rajinsharwar@gmail.com>
2023-07-30 16:03:53 +0600

    Revert "Update admin.php"
    This reverts commit b79431af9ae7ebdc9e792798ba00c09de836eadc.

--- src/wp-admin/admin.php
+++ src/wp-admin/admin.php
@@ -33,12 +33,6 @@ if ( isset( $_GET['import'] ) && ! defined( 'WP_LOAD_IMPORTERS' ) ) {
 
 require_once dirname( __DIR__ ) . '/wp-load.php';
 
-//Redirect to the admin dashboard when /admin,php is accessed without any query parameters.
-if ( strpos( $_SERVER['REQUEST_URI'], 'wp-admin/admin.php' ) !== false && empty( $_SERVER['QUERY_STRING'] ) ) {
-	wp_redirect( admin_url() );
-	exit;
-}
-
 nocache_headers();
 
 if ( get_option( 'db_upgraded' ) ) {
