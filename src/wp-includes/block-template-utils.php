2c191f77e8605d882e599c806e71650e515851a3
André <583546+oandregal@users.noreply.github.com>
2023-07-31 16:41:09 +0200

    Fix for template retrieval: do not include both child and parent templates.

--- src/wp-includes/block-template-utils.php
+++ src/wp-includes/block-template-utils.php
@@ -360,6 +360,17 @@ function _get_block_templates_files( $template_type, $query = array() ) {
 				continue;
 			}
 
+			// The child theme items (stylesheet) are processed before the parent theme's (template).
+			// If a child theme defines a template, prevent the parent template from being added to the list as well.
+			$slug_already_present = false !== array_search(
+				$template_slug,
+				wp_list_pluck( $template_files, 'slug' ),
+				true
+			);
+			if ( $slug_already_present ) {
+				continue;
+			}
+
 			$new_template_item = array(
 				'slug'  => $template_slug,
 				'path'  => $template_file,
