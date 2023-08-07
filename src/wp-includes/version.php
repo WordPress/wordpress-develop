1660b5c550d6abd36ed8656e06db54e087791992
David Baumwald <davidbaumwald@git.wordpress.org>
2023-08-07 18:51:58 +0000

    WordPress 6.3 RC4.
    git-svn-id: https://develop.svn.wordpress.org/branches/6.3@56368 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-RC3-56344-src';
+$wp_version = '6.3-RC4-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
