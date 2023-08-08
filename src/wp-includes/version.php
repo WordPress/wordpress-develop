ad23a305a4e4bb20e611ae8770a1b9c8c8b1af3f
David Baumwald <davidbaumwald@git.wordpress.org>
2023-08-08 20:30:14 +0000

    Post WordPress 6.3 version bump.
    The 6.3 branch is now 6.3.1-alpha.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.3@56373 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-src';
+$wp_version = '6.3.1-alpha-56373-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
