da82115d6916f45ecc9cf36416d753e447f0563d
costdev <me@colin-stewart.co.uk>
2023-08-09 01:01:02 +0100

    Add missing `@return` for `wp_get_admin_notice()`.

--- src/wp-admin/includes/misc.php
+++ src/wp-admin/includes/misc.php
@@ -1660,6 +1660,7 @@ function wp_check_php_version() {
  *     @type string[] $additional_classes Optional. A string array of class names. Default empty array.
  *     @type bool     $paragraph_wrap     Optional. Whether to wrap the message in paragraph tags. Default true.
  * }
+ * @return string The markup for an admin notice.
  */
 function wp_get_admin_notice( $message, $args = array() ) {
 	$defaults = array(
