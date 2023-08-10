d3db2ac3278094ee6e27c63d830c73b5cf0c3068
Marco Pereirinha <marco.pereirinha@xwp.co>
2023-08-10 11:46:20 +0100

    The responsability to validate the `decoding` attribute has moved to `wp_get_loading_optimization_attributes()`

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -2934,7 +2934,7 @@ if ( ! function_exists( 'get_avatar' ) ) :
 			$extra_attr .= "loading='{$args['loading']}'";
 		}
 
-		if ( in_array( $args['decoding'], array( 'async', 'sync', 'auto' ), true )
+		if ( ! empty( $args['decoding'] )
 			&& ! preg_match( '/\bdecoding\s*=/', $extra_attr )
 		) {
 			if ( ! empty( $extra_attr ) ) {
