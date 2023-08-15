60fda6b4b477cc29660f1aea092c1e9e5a7f90ac
tellthemachines <isabel@tellthemachines.com>
2023-08-15 13:28:10 +1000

    Update theme test

--- tests/phpunit/tests/theme/themeDir.php
+++ tests/phpunit/tests/theme/themeDir.php
@@ -179,6 +179,7 @@ class Tests_Theme_ThemeDir extends WP_UnitTestCase {
 			'Block Theme',
 			'Block Theme Child Theme',
 			'Block Theme Child with no theme.json',
+			'Block Theme Child Theme With Fluid Layout',
 			'Block Theme Child Theme With Fluid Typography',
 			'Block Theme Child Theme With Fluid Typography Config',
 			'Block Theme Non Latin',
