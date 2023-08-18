14ecd1a42dbc2b57f2d5ec2f79702e6c38e34f4d
Rajinsharwar <rajinsharwar@gmail.com>
2023-08-18 15:51:46 +0600

    Fixing coding standards

--- tests/phpunit/tests/user.php
+++ tests/phpunit/tests/user.php
@@ -925,12 +925,12 @@ class Tests_User extends WP_UnitTestCase {
 	public function test_wp_insert_user_should_reject_user_login_that_matches_existing_user_email() {
 		$existing_email = get_option( 'admin_email' );
 		$user_id        = wp_insert_user(
-				array(
-						'user_login'    => $existing_email,
-						'user_email'    => 'whatever@example.com',
-						'user_pass'     => 'whatever',
-						'user_nicename' => 'whatever',
-				)
+			array(
+				'user_login'    => $existing_email,
+				'user_email'    => 'whatever@example.com',
+				'user_pass'     => 'whatever',
+				'user_nicename' => 'whatever',
+			)
 		);
 
 		$this->assertWPError( $user_id, 'An error should have been returned as the user_login matches an existing email.' );
@@ -947,12 +947,12 @@ class Tests_User extends WP_UnitTestCase {
 	public function test_wp_update_user_should_not_reject_user_login_that_matches_existing_user_email() {
 		$new_email = 'new.email@example.com';
 		$user_id   = wp_insert_user(
-				array(
-						'user_login'    => $new_email,
-						'user_email'    => $new_email,
-						'user_pass'     => 'whatever',
-						'user_nicename' => 'whatever',
-				)
+			array(
+				'user_login'    => $new_email,
+				'user_email'    => $new_email,
+				'user_pass'     => 'whatever',
+				'user_nicename' => 'whatever',
+			)
 		);
 
 		$this->assertNotWPError( $user_id, 'The test user could not be created.' );
