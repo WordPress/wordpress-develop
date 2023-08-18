d669cc98e343713b157773160a473eb67064e9bf
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-08-18 11:35:23 +0200

    Add description to @return tag

--- src/wp-includes/global-styles-and-settings.php
+++ src/wp-includes/global-styles-and-settings.php
@@ -446,7 +446,7 @@ function wp_get_theme_directory_pattern_slugs() {
  *
  * @since 6.4.0
  *
- * @return array
+ * @return array Associative array of `$template_name => $template_data` pairs, with `$template_data` having "title" and "postTypes" fields.
  */
 function wp_get_theme_data_custom_templates() {
 	return WP_Theme_JSON_Resolver::get_theme_data( array(), array( 'with_supports' => false ) )->get_custom_templates();
