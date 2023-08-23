929d9d221f6385f7fd78c31d0ca5ef23cef45f71
Andy Fragen <andy@thefragens.com>
2023-08-23 11:53:24 -0700

    move to src/js/_enqueues/admin

--- src/wp-admin/includes/class-wp-plugin-dependencies.php
+++ src/wp-admin/includes/class-wp-plugin-dependencies.php
@@ -91,7 +91,6 @@ class WP_Plugin_Dependencies {
 			add_filter( 'plugin_install_action_links', array( $this, 'empty_package_remove_install_button' ), 10, 2 );
 
 			add_action( 'admin_init', array( $this, 'modify_plugin_row' ), 15 );
-			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 			add_action( 'admin_notices', array( $this, 'admin_notices' ) );
 			add_action( 'network_admin_notices', array( $this, 'admin_notices' ) );
 
@@ -102,28 +101,6 @@ class WP_Plugin_Dependencies {
 		$this->slugs      = $this->sanitize_required_headers( $required_headers );
 		$this->get_dot_org_data();
 		$this->deactivate_unmet_dependencies();
-	}
-
-	/**
-	 * Enqueues scripts for plugin dependencies on the "Add New" plugins screen.
-	 *
-	 * @global string $wp_version The WordPress version string.
-	 * @global string $pagenow    The filename of the current screen.
-	 *
-	 * @return void
-	 */
-	public function enqueue_scripts() {
-		global $wp_version, $pagenow;
-
-		if ( 'plugin-install.php' === $pagenow ) {
-			wp_enqueue_script(
-				'wp-plugin-dependencies-updates',
-				plugins_url( 'wp-admin/js/updates.js', 'wp-plugin-dependencies/plugin.php' ),
-				array( 'updates' ),
-				$wp_version,
-				true
-			);
-		}
 	}
 
 	/**
