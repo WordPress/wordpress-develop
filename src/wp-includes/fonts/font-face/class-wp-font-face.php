f6c42668cf426456a5d5e36e1ad5d15636dc523e
Tonya Mork <tonya.mork@automattic.com>
2023-08-24 10:23:08 -0500

    Reference @fonts param definition
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- src/wp-includes/fonts/font-face/class-wp-font-face.php
+++ src/wp-includes/fonts/font-face/class-wp-font-face.php
@@ -109,31 +109,9 @@ class WP_Font_Face {
 	 *
 	 * @since 6.4.0
 	 *
-	 * @param array[][] $fonts {
-	 *     Optional. The font-families and their font variations. Default empty array.
-	 *
-	 *     @type string $font-family => array[] $variations {
-	 *         Optional. An associated array of font variations for this font-family.
-	 *         Each variation has the following structure.
-	 *
-	 *         @type array $font_variation {
-	 *             @type string          $font-family             The font-family property.
-	 *             @type string|string[] $src                     The URL(s) to each resource containing the font data.
-	 *             @type string          $font_style              Optional. The font-style property. Default 'normal'.
-	 *             @type string          $font-weight             Optional. The font-weight property. Default '400'.
-	 *             @type string          $font-display            Optional. The font-display property. Default 'fallback'.
-	 *             The following font-face properties are optional and will not be generated if not provided.
-	 *             @type string          $ascent-override         Optional. The ascent-override property.
-	 *             @type string          $descent-override        Optional. The descent-override property.
-	 *             @type string          $font-stretch            Optional. The font-stretch property.
-	 *             @type string          $font-variant            Optional. The font-variant property.
-	 *             @type string          $font-feature-settings   Optional. The font-feature-settings property.
-	 *             @type string          $font-variation-settings Optional. The font-variation-settings property.
-	 *             @type string          $line-gap-override       Optional. The line-gap-override property.
-	 *             @type string          $size-adjust             Optional. The size-adjust property.
-	 *             @type string          $unicode-range           Optional. The unicode-range property.
-	 *         }
-	 *     }
+	 * @param array[][] $fonts Optional. The font-families and their font variations.
+	 *                         See {@see wp_print_font_faces()} for the supported fields.
+	 *                         Default empty array.
 	 * }
 	 */
 	public function generate_and_print( array $fonts ) {
