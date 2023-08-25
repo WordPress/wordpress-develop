e3a04544eba1192e0d19c0119d5db0ca11bb9c92
Ryan Leeson <ryan.leeson@kanopistudios.com>
2023-08-25 15:46:12 -0400

    - Fix PHPCS errors for tests

--- tests/phpunit/tests/date/currentGmtOffset.php
+++ tests/phpunit/tests/date/currentGmtOffset.php
@@ -23,7 +23,7 @@ class Tests_Date_CurrentGmtOffset extends WP_UnitTestCase {
 	public function test_current_gmt_offset_empty_is_default() {
 		delete_option( 'gmt_offset' );
 
-		$this->assertIsFloat( current_gmt_offset(), 'Missing current GMT offset is default of 0');
+		$this->assertIsFloat( current_gmt_offset(), 'Missing current GMT offset is default of 0' );
 	}
 
 	/**
@@ -32,7 +32,7 @@ class Tests_Date_CurrentGmtOffset extends WP_UnitTestCase {
 	public function test_current_gmt_offset_invalid_boolean_true() {
 		update_option( 'gmt_offset', true );
 
-		$this->assertEquals( 0, current_gmt_offset(), 'Invalid current GMT offset of boolean true is 0');
+		$this->assertEquals( 0, current_gmt_offset(), 'Invalid current GMT offset of boolean true is 0' );
 	}
 
 	/**
@@ -41,7 +41,7 @@ class Tests_Date_CurrentGmtOffset extends WP_UnitTestCase {
 	public function test_current_gmt_offset_invalid_string() {
 		update_option( 'gmt_offset', 'NinePointFive' );
 
-		$this->assertEquals( 0, current_gmt_offset(), 'Invalid current GMT offset of non-numeric text is 0');
+		$this->assertEquals( 0, current_gmt_offset(), 'Invalid current GMT offset of non-numeric text is 0' );
 	}
 
 	/**
@@ -50,6 +50,6 @@ class Tests_Date_CurrentGmtOffset extends WP_UnitTestCase {
 	public function test_current_gmt_offset_is_float() {
 		update_option( 'gmt_offset', 9.5 );
 
-		$this->assertIsFloat( current_gmt_offset(), 'Current GMT offset is a float');
+		$this->assertIsFloat( current_gmt_offset(), 'Current GMT offset is a float' );
 	}
 }
