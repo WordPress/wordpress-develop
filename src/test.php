b1d4412a40bfe77b40d200c0d09ec351ef96b83a
= <=>
2023-08-26 21:46:54 +0200

    NASQUERON Settings
    parent f15646741076b80d6fb0e723bc2cc755bdc8ad1d
    author = <=> 1693079214 +0200
    committer Kurt Gysin <=> 1724607315 +0200
    
    NASQUERON Settings - docker-compose.yml aktualisiert
    
    Local Settings
    script-loader.php, default.template und php-config.ini aktualisiert
    
    Revert "NASQUERON Settings - docker-compose.yml aktualisiert"
    
    Dies reverted den Commit d08b879b76e0a49eca9ea395acd3138d500d4324.
    
    Revert "Local Settings script-loader.php, default.template und php-config.ini aktualisiert"
    
    Dies reverted den Commit 644c6d8fca41248f3ddb1fd88c4167b43f657170.
    
    Removed debugging helper.
    
    Better nginx logging
    
    More local changes

---
+++ src/test.php
@@ -0,0 +1,5 @@
+<?php
+echo '$_SERVER[\'HTTP_HOST\'] : ' . $_SERVER['HTTP_HOST'];
+echo '<br/>' . '$_SERVER[\'PHP_SELF\'] : ' . $_SERVER['PHP_SELF'];
+echo '<br/>' . '$_SERVER[\'REQUEST_URI\'] : ' . $_SERVER['REQUEST_URI'];
+phpinfo();
