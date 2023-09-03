f606d4fe1422941738ba178c697ec388cb656063
Andy Fragen <andy@thefragens.com>
2023-09-03 13:08:55 -0700

    load class

--- src/wp-settings.php
+++ src/wp-settings.php
@@ -387,6 +387,8 @@ wp_plugin_directory_constants();
 
 $GLOBALS['wp_plugin_paths'] = array();
 
+require ABSPATH . WPINC . '/class-wp-plugin-dependencies.php';
+
 // Load must-use plugins.
 foreach ( wp_get_mu_plugins() as $mu_plugin ) {
 	$_wp_plugin_file = $mu_plugin;
