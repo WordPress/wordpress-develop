935f3fa3b11516b3299d9ec77cdff354e0b4f566
Joe McGill <joe.mcgill@10up.com>
2023-09-05 17:28:42 -0500

    Fix URL path during registration

--- src/wp-includes/blocks/index.php
+++ src/wp-includes/blocks/index.php
@@ -99,7 +99,7 @@ function register_core_block_style_handles() {
 			return;
 		}
 
-		$wp_styles->add( $style_handle, $includes_url . $style_path );
+		$wp_styles->add( $style_handle, $includes_url . '/blocks/' . $style_path );
 		$wp_styles->add_data( $style_handle, 'path', $path );
 
 		$rtl_file = str_replace( "{$suffix}.css", "-rtl{$suffix}.css", $path );
