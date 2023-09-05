c001610cb7dbad464dee764324760db3731d3eda
Brad Jorsch <brad.jorsch@automattic.com>
2023-09-05 09:09:41 -0400

    Chain to existing handlers, in case someone configured some in PHP's config.
    That also requires separating the "print" logic into a separate helper function.

--- src/wp-error-handling.php
+++ src/wp-error-handling.php
@@ -29,7 +29,25 @@ if ( function_exists( 'error_reporting' ) ) {
 }
 
 /**
- * Handler for PHP's `set_error_handler`.
+ * Previous error handler for chaining. Private.
+ *
+ * @since 6.4.0
+ * @private
+ * @var callable|null
+ */
+$_wp_error_handling_previous_error_handler = set_error_handler( 'wp_error_handler' );
+
+/**
+ * Previous exception handler for chaining. Private.
+ *
+ * @since 6.4.0
+ * @private
+ * @var callable|null
+ */
+$_wp_error_handling_previous_exception_handler = set_exception_handler( 'wp_exception_handler' );
+
+/**
+ * Display errors like PHP's `display_errors` would.
  *
  * We have two conflicting goals here:
  *  1. We want to not print error text containing arbitrary HTML to the browser.
@@ -42,15 +60,14 @@ if ( function_exists( 'error_reporting' ) ) {
  * error was unhandled so PHP does everything else it would normally do.
  *
  * @since 6.4.0
+ * @private
  *
  * @param int         $errno   Level of the error raised.
  * @param string      $errstr  Error message.
  * @param string|null $errfile Filename the error was raised in.
  * @param int         $errline Line number where the error was raised.
- *
- * @return bool True to indicate the error was handled, false if not.
  */
-function wp_error_handler( $errno, $errstr, $errfile, $errline ) {
+function _wp_error_handler_printer( $errno, $errstr, $errfile, $errline ) {
 	static $display_errors = false;
 
 	$v = ini_get( 'display_errors' );
@@ -69,112 +86,142 @@ function wp_error_handler( $errno, $errstr, $errfile, $errline ) {
 		}
 	}
 
-	if ( $display_errors && ( error_reporting() & $errno ) !== 0 ) {
+	if ( ! $display_errors || ( error_reporting() & $errno ) === 0 ) {
+		return;
+	}
 
-		// From https://github.com/php/php-src/blob/3f38105740d160e448881b268ebc44bfed20c7db/main/main.c#L1236-L1250
-		if ( ini_get( 'ignore_repeated_errors' ) ) {
-			$last = error_get_last();
-			if ( $last && $last['message'] === $errstr && (
-				ini_get( 'ignore_repeated_source' ) ||
-				(int) $last['line'] === (int) $errline && $last['file'] === $errfile
-			) ) {
-				return false;
-			}
-		}
-
-		switch ( $errno ) {
-			case E_ERROR:
-			case E_CORE_ERROR:
-			case E_COMPILE_ERROR:
-			case E_USER_ERROR:
-				$type_str = 'Fatal error';
-				break;
-			case E_RECOVERABLE_ERROR:
-				$type_str = 'Recoverable fatal error';
-				break;
-			case E_WARNING:
-			case E_CORE_WARNING:
-			case E_COMPILE_WARNING:
-			case E_USER_WARNING:
-				$type_str = 'Warning';
-				break;
-			case E_PARSE:
-				$type_str = 'Parse error';
-				break;
-			case E_NOTICE:
-			case E_USER_NOTICE:
-				$type_str = 'Notice';
-				break;
-			case E_STRICT:
-				$type_str = 'Strict Standards';
-				break;
-			case E_DEPRECATED:
-			case E_USER_DEPRECATED:
-				$type_str = 'Deprecated';
-				break;
-			default:
-				$type_str = 'Unknown error';
-				break;
-		}
-
-		$errfile = (string) $errfile;
-		if ( '' === $errfile ) {
-			$errfile = 'Unknown';
-		}
-
-		// Adapted from https://github.com/php/php-src/blob/3f38105740d160e448881b268ebc44bfed20c7db/main/main.c#L1352-L1379
-		// with the addition of some `htmlspecialchars()` calls.
-		if ( ini_get( 'xmlrpc_errors' ) ) {
-			printf(
-				'<?xml version="1.0"?><methodResponse><fault><value><struct><member><name>faultCode</name><value><int>%d</int></value></member><member><name>faultString</name><value><string>%s:%s in %s on line %u</string></value></member></struct></value></fault></methodResponse>',
-				ini_get( 'xmlrpc_error_number' ),
-				$type_str,
-				htmlspecialchars( $errstr, ENT_QUOTES | ENT_XML1 ),
-				htmlspecialchars( $errfile, ENT_QUOTES | ENT_XML1 ),
-				$errline
-			);
-		} else {
-			$error_prepend_string = (string) ini_get( 'error_prepend_string' );
-			$error_append_string  = (string) ini_get( 'error_append_string' );
-
-			if ( PHP_SAPI !== 'cli' && PHP_SAPI !== 'phpdbg' ) {
-				$errstr  = htmlspecialchars( $errstr, ENT_QUOTES | ENT_HTML401 );
-				$errfile = htmlspecialchars( $errfile, ENT_QUOTES | ENT_HTML401 );
-			}
-
-			if ( ini_get( 'html_errors' ) ) {
-				printf( "%s<br />\n<b>%s</b>:  %s in <b>%s</b> on line <b>%u</b><br />\n%s", $error_prepend_string, $type_str, $errstr, $errfile, $errline, $error_append_string );
-			} elseif ( 'stderr' === $display_errors && ( PHP_SAPI === 'cli' || PHP_SAPI === 'cgi' || PHP_SAPI === 'phpdbg' ) ) {
-				fprintf( STDERR, "%s: %s in %s on line %u\n", $type_str, $errstr, $errfile, $errline );
-				fflush( STDERR );
-			} else {
-				printf( "%s\n%s: %s in %s on line %u\n%s", $error_prepend_string, $type_str, $errstr, $errfile, $errline, $error_append_string );
-			}
+	// From https://github.com/php/php-src/blob/3f38105740d160e448881b268ebc44bfed20c7db/main/main.c#L1236-L1250
+	if ( ini_get( 'ignore_repeated_errors' ) ) {
+		$last = error_get_last();
+		if ( $last && $last['message'] === $errstr && (
+			ini_get( 'ignore_repeated_source' ) ||
+			(int) $last['line'] === (int) $errline && $last['file'] === $errfile
+		) ) {
+			return;
 		}
 	}
 
-	return false;
+	switch ( $errno ) {
+		case E_ERROR:
+		case E_CORE_ERROR:
+		case E_COMPILE_ERROR:
+		case E_USER_ERROR:
+			$type_str = 'Fatal error';
+			break;
+		case E_RECOVERABLE_ERROR:
+			$type_str = 'Recoverable fatal error';
+			break;
+		case E_WARNING:
+		case E_CORE_WARNING:
+		case E_COMPILE_WARNING:
+		case E_USER_WARNING:
+			$type_str = 'Warning';
+			break;
+		case E_PARSE:
+			$type_str = 'Parse error';
+			break;
+		case E_NOTICE:
+		case E_USER_NOTICE:
+			$type_str = 'Notice';
+			break;
+		case E_STRICT:
+			$type_str = 'Strict Standards';
+			break;
+		case E_DEPRECATED:
+		case E_USER_DEPRECATED:
+			$type_str = 'Deprecated';
+			break;
+		default:
+			$type_str = 'Unknown error';
+			break;
+	}
+
+	$errfile = (string) $errfile;
+	if ( '' === $errfile ) {
+		$errfile = 'Unknown';
+	}
+
+	// Adapted from https://github.com/php/php-src/blob/3f38105740d160e448881b268ebc44bfed20c7db/main/main.c#L1352-L1379
+	// with the addition of some `htmlspecialchars()` calls.
+	if ( ini_get( 'xmlrpc_errors' ) ) {
+		printf(
+			'<?xml version="1.0"?><methodResponse><fault><value><struct><member><name>faultCode</name><value><int>%d</int></value></member><member><name>faultString</name><value><string>%s:%s in %s on line %u</string></value></member></struct></value></fault></methodResponse>',
+			ini_get( 'xmlrpc_error_number' ),
+			$type_str,
+			htmlspecialchars( $errstr, ENT_QUOTES | ENT_XML1 ),
+			htmlspecialchars( $errfile, ENT_QUOTES | ENT_XML1 ),
+			$errline
+		);
+	} else {
+		$error_prepend_string = (string) ini_get( 'error_prepend_string' );
+		$error_append_string  = (string) ini_get( 'error_append_string' );
+
+		if ( PHP_SAPI !== 'cli' && PHP_SAPI !== 'phpdbg' ) {
+			$errstr  = htmlspecialchars( $errstr, ENT_QUOTES | ENT_HTML401 );
+			$errfile = htmlspecialchars( $errfile, ENT_QUOTES | ENT_HTML401 );
+		}
+
+		if ( ini_get( 'html_errors' ) ) {
+			printf( "%s<br />\n<b>%s</b>:  %s in <b>%s</b> on line <b>%u</b><br />\n%s", $error_prepend_string, $type_str, $errstr, $errfile, $errline, $error_append_string );
+		} elseif ( 'stderr' === $display_errors && ( PHP_SAPI === 'cli' || PHP_SAPI === 'cgi' || PHP_SAPI === 'phpdbg' ) ) {
+			fprintf( STDERR, "%s: %s in %s on line %u\n", $type_str, $errstr, $errfile, $errline );
+			fflush( STDERR );
+		} else {
+			printf( "%s\n%s: %s in %s on line %u\n%s", $error_prepend_string, $type_str, $errstr, $errfile, $errline, $error_append_string );
+		}
+	}
+}
+
+/**
+ * Handler for PHP's `set_error_handler`.
+ *
+ * Calls `_wp_error_handler_printer()` to handle printing and resetting of `display_errors`, then
+ * chains to the next handler or returns false to let the default handler handle the rest of it.
+ *
+ * @since 6.4.0
+ *
+ * @param int         $errno   Level of the error raised.
+ * @param string      $errstr  Error message.
+ * @param string|null $errfile Filename the error was raised in.
+ * @param int         $errline Line number where the error was raised.
+ *
+ * @return bool True to indicate the error was handled, false if not.
+ */
+function wp_error_handler( $errno, $errstr, $errfile, $errline ) {
+	global $_wp_error_handling_previous_error_handler;
+
+	_wp_error_handler_printer( $errno, $errstr, $errfile, $errline );
+
+	if ( $_wp_error_handling_previous_error_handler ) {
+		return $_wp_error_handling_previous_error_handler( $errno, $errstr, $errfile, $errline );
+	} else {
+		return false;
+	}
 }
 
 /**
  * Handler for PHP's `set_exception_handler`.
  *
- * Calls `wp_error_handler()` to handle printing and resetting of `display_errors`, then
- * rethrows the exception to let the default handler handle the rest of it.
+ * Calls `_wp_error_handler_printer()` to handle printing and resetting of `display_errors`, then
+ * chains to the next handler or rethrows the exception to let the default handler handle
+ * the rest of it.
  *
  * @since 6.4.0
  *
  * @param Throwable $ex Object that was thrown.
  */
 function wp_exception_handler( Throwable $ex ) {
+	global $_wp_error_handling_previous_exception_handler;
+
 	try {
 		// Adapted from https://github.com/php/php-src/blob/3f38105740d160e448881b268ebc44bfed20c7db/Zend/zend_exceptions.c#L954-L956
-		wp_error_handler( E_ERROR, "Uncaught {$ex->__toString()}\n  thrown", $ex->getFile(), $ex->getLine() );
+		_wp_error_handler_printer( E_ERROR, "Uncaught {$ex->__toString()}\n  thrown", $ex->getFile(), $ex->getLine() );
 	} catch ( Throwable $t ) {
 		// Probably $ex->__toString() is broken. Let the default handler handle that.
 	}
-	throw $ex;
+	if ( $_wp_error_handling_previous_exception_handler ) {
+		$_wp_error_handling_previous_exception_handler( $ex );
+	} else {
+		throw $ex;
+	}
 }
-
-set_error_handler( 'wp_error_handler' );
-set_exception_handler( 'wp_exception_handler' );
