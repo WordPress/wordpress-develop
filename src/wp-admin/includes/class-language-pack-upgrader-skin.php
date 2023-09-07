83901b7de83253476cfc671a4daa600e8cbc934a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-09-07 13:10:14 +0300

    Update class-language-pack-upgrader-skin.php

--- src/wp-admin/includes/class-language-pack-upgrader-skin.php
+++ src/wp-admin/includes/class-language-pack-upgrader-skin.php
@@ -24,6 +24,7 @@ class Language_Pack_Upgrader_Skin extends WP_Upgrader_Skin {
 	/**
 	 * @param array $args
 	 */
+	#[\Override]
 	public function __construct( $args = array() ) {
 		$defaults = array(
 			'url'                => '',
@@ -42,6 +43,7 @@ class Language_Pack_Upgrader_Skin extends WP_Upgrader_Skin {
 
 	/**
 	 */
+	#[\Override]
 	public function before() {
 		$name = $this->upgrader->get_name_for_update( $this->language_update );
 
@@ -56,6 +58,7 @@ class Language_Pack_Upgrader_Skin extends WP_Upgrader_Skin {
 	 *
 	 * @param string|WP_Error $errors Errors.
 	 */
+	#[\Override]
 	public function error( $errors ) {
 		echo '<div class="lp-error">';
 		parent::error( $errors );
@@ -64,12 +67,14 @@ class Language_Pack_Upgrader_Skin extends WP_Upgrader_Skin {
 
 	/**
 	 */
+	#[\Override]
 	public function after() {
 		echo '</div>';
 	}
 
 	/**
 	 */
+	#[\Override]
 	public function bulk_footer() {
 		$this->decrement_update_count( 'translation' );
 
