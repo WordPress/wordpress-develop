225a91bf5404c62d3d336fa9ec0b36089dd21510
Adam Silverstein <adamjs@google.com>
2023-09-08 13:15:49 -0600

    Clean up default filters after trunk merge

--- src/wp-includes/default-filters.php
+++ src/wp-includes/default-filters.php
@@ -722,12 +722,13 @@ add_action( 'init', 'wp_register_persisted_preferences_meta' );
 // CPT wp_block custom postmeta field.
 add_action( 'init', 'wp_create_initial_post_meta' );
 
-// Including revisioned meta when considering whether a post revision has changed.
-// Font management.
+// Include revisioned meta when considering whether a post revision has changed.
 add_filter( 'wp_save_post_revision_post_has_changed', 'wp_check_revisioned_meta_fields_have_changed', 10, 3 );
 
 // Save revisioned post meta immediately after a revision is saved
 add_action( '_wp_put_post_revision', 'wp_save_revisioned_meta_fields', 10, 2 );
+
+// Font management.
 add_action( 'wp_head', 'wp_print_font_faces', 50 );
 
 unset( $filter, $action );
