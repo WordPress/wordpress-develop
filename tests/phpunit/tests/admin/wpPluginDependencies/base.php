0996a24bf7b501981e23d3a03331e1c8993b14cf
Andy Fragen <andy@thefragens.com>
2023-09-07 21:25:24 -0700

    lint something

--- tests/phpunit/tests/admin/wpPluginDependencies/base.php
+++ tests/phpunit/tests/admin/wpPluginDependencies/base.php
@@ -40,7 +40,7 @@ abstract class WP_PluginDependencies_UnitTestCase extends WP_UnitTestCase {
 	 */
 	public static function set_up_before_class() {
 		parent::set_up_before_class();
-		
+
 		self::$instance    = new WP_Plugin_Dependencies();
 		self::$plugins_dir = WP_PLUGIN_DIR . '/wp_plugin_dependencies_plugin';
 		@mkdir( self::$plugins_dir );
