f74302bf1886c8d0e5f5ba7f29c5d760e83a27d9
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-09-13 14:31:07 +1000

    2012: legacy footer.

--- src/wp-content/themes/twentytwelve/functions.php
+++ src/wp-content/themes/twentytwelve/functions.php
@@ -713,7 +713,7 @@ function twentytwelve_customize_partial_blogdescription() {
  * @since Twenty Twelve 1.0
  */
 function twentytwelve_customize_preview_js() {
-	wp_enqueue_script( 'twentytwelve-customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20200516', array( 'in_footer' => true ) );
+	wp_enqueue_script( 'twentytwelve-customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20200516', true );
 }
 add_action( 'customize_preview_init', 'twentytwelve_customize_preview_js' );
 
