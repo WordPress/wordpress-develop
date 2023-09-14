967798d2311df1fdd5095e4c713baad8febd123d
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2023-09-14 17:46:37 +0200

    Ensure that only an array gets validated for block hooks

--- src/wp-includes/class-wp-block-type.php
+++ src/wp-includes/class-wp-block-type.php
@@ -523,13 +523,18 @@ class WP_Block_Type {
 
 		foreach ( $args as $property_name => $property_value ) {
 			// Avoid infinite recursion in block hooks (hooking to itself).
-			if ( 'block_hooks' === $property_name && array_key_exists( $this->name, $property_value ) ) {
-				_doing_it_wrong(
-					__METHOD__,
-					__( 'Cannot hook block to itself.' ),
-					'6.4.0'
-				);
-				unset( $property_value[ $this->name ] );
+			if ( 'block_hooks' === $property_name ) {
+				if ( ! is_array( $property_value ) ) {
+					continue;
+				}
+				if ( array_key_exists( $this->name, $property_value ) ) {
+					_doing_it_wrong(
+						__METHOD__,
+						__( 'Cannot hook block to itself.' ),
+						'6.4.0'
+					);
+					unset( $property_value[ $this->name ] );
+				}
 			}
 			$this->$property_name = $property_value;
 		}
