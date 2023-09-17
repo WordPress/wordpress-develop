6c34973d1a31d05bddfdc2a250c4159b3fdcbbae
Joe Dolson <design@joedolson.com>
2023-09-17 12:49:14 -0500

    Initial draft additional attributes & tests

--- src/wp-admin/includes/misc.php
+++ src/wp-admin/includes/misc.php
@@ -1658,6 +1658,7 @@ function wp_check_php_version() {
  *     @type bool     $dismissible        Optional. Whether the admin notice is dismissible. Default false.
  *     @type string   $id                 Optional. The value of the admin notice's ID attribute. Default empty string.
  *     @type string[] $additional_classes Optional. A string array of class names. Default empty array.
+ *     @type string[] $attributes         Optional. Additional attributes for the notice div. Default empty array.
  *     @type bool     $paragraph_wrap     Optional. Whether to wrap the message in paragraph tags. Default true.
  * }
  * @return string The markup for an admin notice.
@@ -1668,6 +1669,7 @@ function wp_get_admin_notice( $message, $args = array() ) {
 		'dismissible'        => false,
 		'id'                 => '',
 		'additional_classes' => array(),
+		'attributes'         => array(),
 		'paragraph_wrap'     => true,
 	);
 
@@ -1721,11 +1723,22 @@ function wp_get_admin_notice( $message, $args = array() ) {
 		$classes .= ' ' . implode( ' ', $args['additional_classes'] );
 	}
 
+	if ( is_array( $args['attributes'] ) && ! empty( $args['attributes'] ) ) {
+		$attributes = '';
+		foreach ( $args['attributes'] as $attr => $val ) {
+			if ( $val ) {
+				$attributes .= ' ' . $attr . '="' . esc_attr( $val ) . '"';
+			} else {
+				$attributes .= ' ' . $attr;
+			}
+		}
+	}
+
 	if ( false !== $args['paragraph_wrap'] ) {
 		$message = "<p>$message</p>";
 	}
 
-	$markup = sprintf( '<div %1$sclass="%2$s">%3$s</div>', $id, $classes, $message );
+	$markup = sprintf( '<div %1$sclass="%2$s"%3$s>%4$s</div>', $id, $classes, $attributes, $message );
 
 	/**
 	 * Filters the markup for an admin notice.
--- tests/phpunit/tests/admin/wpAdminNotice.php
+++ tests/phpunit/tests/admin/wpAdminNotice.php
@@ -212,6 +212,58 @@ class Tests_Admin_WpAdminNotice extends WP_UnitTestCase {
 				),
 				'expected' => '<div class="notice"><p>A notice with additional classes that are not an array.</p></div>',
 			),
+			'additional attribute with a value' => array(
+				'message'  => 'A notice with an additional attribute with a value.',
+				'args'     => array(
+					'attributes' => array( 'aria-live' => 'assertive' ),
+				),
+				'expected' => '<div class="notice" aria-live="assertive"><p>A notice with an additional attribute with a value.</p></div>',
+			),
+			'additional attribute with no value' => array(
+				'message'  => 'A notice with an additional attribute with no value.',
+				'args'     => array(
+					'attributes' => array( 'hidden' ),
+				),
+				'expected' => '<div class="notice" hidden><p>A notice with an additional attribute with no value.</p></div>',
+			),
+			'additional attribute with numeric value' => array(
+				'message'  => 'A notice with an additional attribute with a numeric value.',
+				'args'     => array(
+					'attributes' => array( 'tabindex' => -1 ),
+				),
+				'expected' => '<div class="notice" tabindex="-1"><p>A notice with an additional attribute with a numeric value.</p></div>',
+			),
+			'additional attribute with role' => array(
+				'message'  => 'A notice with an additional attribute role.',
+				'args'     => array(
+					'attributes' => array( 'role' => 'alert' ),
+				),
+				'expected' => '<div class="notice" role="alert"><p>A notice with an additional attribute role.</p></div>',
+			),
+			'multiple additional attributes' => array(
+				'message'  => 'A notice with multiple additional attributes.',
+				'args'     => array(
+					'attributes' => array(
+						'role'     => 'alert',
+						'tabindex' => '-1',
+					),
+				),
+				'expected' => '<div class="notice" role="alert" tabindex="-1"><p>A notice with multiple additional attributes.</p></div>',
+			),
+			'data attribute with unsafe value' => array(
+				'message'  => 'A notice with an additional attribute with an unsafe value.',
+				'args'     => array(
+					'attributes' => array( 'data-unsafe' => '<script>alert( "Howdy, admin!" );</script>' ),
+				),
+				'expected' => '<div class="notice"><p>A notice with an additional attribute with an unsafe value.</p></div>',
+			),
+			'additional invalid attribute' => array(
+				'message'  => 'A notice with an additional attribute that is invalid.',
+				'args'     => array(
+					'attributes' => array( 'not-valid' => 'not-valid' ),
+				),
+				'expected' => '<div class="notice"><p>A notice with an additional attribute that is invalid.</p></div>',
+			),
 			'paragraph wrapping as a falsy value rather than (bool) false' => array(
 				'message'  => 'A notice with paragraph wrapping as a falsy value rather than (bool) false.',
 				'args'     => array(
