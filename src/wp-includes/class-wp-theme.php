0a27edfd86b0be3fe3c02e32ec68086b7d430d0f
Karthik Thayyil <karthik.thayyil@get10up.com>
2023-09-18 13:28:55 +0100

    change defaulting logic

--- src/wp-includes/class-wp-theme.php
+++ src/wp-includes/class-wp-theme.php
@@ -203,6 +203,17 @@ final class WP_Theme implements ArrayAccess {
 	private $block_template_folders;
 
 	/**
+	 * Default values for template folders.
+	 *
+	 * @since 6.4.0
+	 * @var array
+	 */
+	private $default_template_folders = array(
+		'wp_template'      => 'templates',
+		'wp_template_part' => 'parts',
+	);
+
+	/**
 	 * Flag for whether the themes cache bucket should be persistently cached.
 	 *
 	 * Default is false. Can be set with the {@see 'wp_cache_themes_persistently'} filter.
@@ -297,7 +308,7 @@ final class WP_Theme implements ArrayAccess {
 			}
 			$this->template               = $this->stylesheet;
 			$this->block_theme            = false;
-			$this->block_template_folders = $this->get_block_template_folders( true ); // set to default value.
+			$this->block_template_folders = $this->default_template_folders;
 			$this->cache_add(
 				'theme',
 				array(
@@ -318,7 +329,7 @@ final class WP_Theme implements ArrayAccess {
 			$this->errors                 = new WP_Error( 'theme_stylesheet_not_readable', __( 'Stylesheet is not readable.' ) );
 			$this->template               = $this->stylesheet;
 			$this->block_theme            = false;
-			$this->block_template_folders = $this->get_block_template_folders( true ); // set to default value.
+			$this->block_template_folders = $this->default_template_folders;
 			$this->cache_add(
 				'theme',
 				array(
@@ -1738,7 +1749,6 @@ final class WP_Theme implements ArrayAccess {
 	 *
 	 * @since 6.4.0
 	 *
-	 * @param bool $default Optional. Whether to return the default values. Default false.
 	 * @return string[] {
 	 *     Folder names used by block themes.
 	 *
@@ -1746,21 +1756,13 @@ final class WP_Theme implements ArrayAccess {
 	 *     @type string $wp_template_part Theme-relative directory name for block template parts.
 	 * }
 	 */
-	public function get_block_template_folders( $default = false ) {
-		$default_template_folders = array(
-			'wp_template'      => 'templates',
-			'wp_template_part' => 'parts',
-		);
-		if ( $default ) {
-			return $default_template_folders;
-		}
-
+	public function get_block_template_folders() {
 		// Return set/cached value if available.
 		if ( isset( $this->block_template_folders ) ) {
 			return $this->block_template_folders;
 		}
 
-		$this->block_template_folders = $default_template_folders;
+		$this->block_template_folders = $this->default_template_folders;
 
 		$stylesheet_directory = $this->get_stylesheet_directory();
 		// If the theme uses deprecated block template folders.
