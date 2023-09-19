13aa0cfdd576535b45a74db5156bba8d045f5f57
Bernie Reiter <ockham@raz.or.at>
2023-09-19 14:23:18 +0200

    Blocks: Implement block insertion functions.
    For #59313, we need to implement functions to insert a given parsed block into another parsed block's inner blocks, and to prepend and append to that array, respectively.
    
    We will use those functions in combination with `traverse_and_serialize_blocks` (see #59327) to implement automatic insertion of hooked blocks into block templates and patterns.
    
    Props gziolo.
    Fixes #59385.

--- tests/phpunit/tests/blocks/blockHooks.php
+++ tests/phpunit/tests/blocks/blockHooks.php
@@ -17,9 +17,12 @@ class Tests_Blocks_BlockHooks extends WP_UnitTestCase {
 	 * @since 6.4.0
 	 */
 	public function tear_down() {
-		$registry = WP_Block_Type_Registry::get_instance();
-
-		foreach ( array( 'tests/my-block', 'tests/my-container-block' ) as $block_name ) {
+		$registry    = WP_Block_Type_Registry::get_instance();
+		$block_names = array(
+			'tests/injected-one',
+			'tests/injected-two',
+		);
+		foreach ( $block_names as $block_name ) {
 			if ( $registry->is_registered( $block_name ) ) {
 				$registry->unregister( $block_name );
 			}
@@ -96,5 +99,158 @@ class Tests_Blocks_BlockHooks extends WP_UnitTestCase {
 			get_hooked_blocks( 'tests/hooked-at-last-child' ),
 			'block hooked at the last child position'
 		);
+	}
+
+	/**
+	 * @ticket 59385
+	 *
+	 * @covers ::insert_inner_block
+	 *
+	 * @dataProvider data_insert_inner_block
+	 *
+	 * @param string $block_index     Block index to insert the block at.
+	 * @param string $expected_markup Expected markup after the block is inserted.
+	 */
+	public function test_insert_inner_block( $block_index, $expected_markup ) {
+		$original_markup = <<<HTML
+<!-- wp:tests/group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph -->
+			<p>Foo</p>
+		<!-- /wp:paragraph -->
+	</div>
+<!-- /wp:tests/group -->
+HTML;
+
+		$inserted_block = array(
+			'blockName'    => 'tests/hooked-block',
+			'attrs'        => array(),
+			'innerBlocks'  => array(),
+			'innerHTML'    => '',
+			'innerContent' => array(),
+		);
+
+		$expected = parse_blocks( $expected_markup )[0];
+		$block    = parse_blocks( $original_markup )[0];
+		insert_inner_block( $block, $block_index, 1, $inserted_block );
+		$this->assertSame( $expected, $block );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array[]
+	 */
+	public function data_insert_inner_block() {
+		$expected_before_markup = <<<HTML
+<!-- wp:tests/group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:tests/hooked-block /--><!-- wp:paragraph -->
+			<p>Foo</p>
+		<!-- /wp:paragraph -->
+	</div>
+<!-- /wp:tests/group -->
+HTML;
+
+		$expected_after_markup = <<<HTML
+<!-- wp:tests/group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph -->
+			<p>Foo</p>
+		<!-- /wp:paragraph --><!-- wp:tests/hooked-block /-->
+	</div>
+<!-- /wp:tests/group -->
+HTML;
+
+		return array(
+			'insert before given block' => array(
+				'block_index'     => 0,
+				'expected_markup' => $expected_before_markup,
+			),
+			'insert after given block'  => array(
+				'block_index'     => 1,
+				'expected_markup' => $expected_after_markup,
+			),
+		);
+	}
+
+	/**
+	 * @ticket 59385
+	 *
+	 * @covers ::prepend_inner_block
+	 */
+	public function test_prepend_inner_block() {
+		$original_markup = <<<HTML
+<!-- wp:tests/group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph -->
+			<p>Foo</p>
+		<!-- /wp:paragraph -->
+	</div>
+<!-- /wp:tests/group -->
+HTML;
+
+		$inserted_block = array(
+			'blockName'    => 'tests/hooked-block',
+			'attrs'        => array(),
+			'innerBlocks'  => array(),
+			'innerHTML'    => '',
+			'innerContent' => array(),
+		);
+
+		$expected_markup = <<<HTML
+<!-- wp:tests/group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:tests/hooked-block /--><!-- wp:paragraph -->
+			<p>Foo</p>
+		<!-- /wp:paragraph -->
+	</div>
+<!-- /wp:tests/group -->
+HTML;
+
+		$expected = parse_blocks( $expected_markup )[0];
+		$block    = parse_blocks( $original_markup )[0];
+		prepend_inner_block( $block, $inserted_block );
+		$this->assertSame( $expected, $block );
+	}
+
+	/**
+	 * @ticket 59385
+	 *
+	 * @covers ::append_inner_block
+	 */
+	public function test_append_inner_block() {
+		$original_markup = <<<HTML
+<!-- wp:tests/group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph -->
+			<p>Foo</p>
+		<!-- /wp:paragraph -->
+	</div>
+<!-- /wp:tests/group -->
+HTML;
+
+		$inserted_block = array(
+			'blockName'    => 'tests/hooked-block',
+			'attrs'        => array(),
+			'innerBlocks'  => array(),
+			'innerHTML'    => '',
+			'innerContent' => array(),
+		);
+
+		$expected_markup = <<<HTML
+<!-- wp:tests/group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph -->
+			<p>Foo</p>
+		<!-- /wp:paragraph --><!-- wp:tests/hooked-block /-->
+	</div>
+<!-- /wp:tests/group -->
+HTML;
+
+		$expected = parse_blocks( $expected_markup )[0];
+		$block    = parse_blocks( $original_markup )[0];
+		append_inner_block( $block, $inserted_block );
+		$this->assertSame( $expected, $block );
 	}
 }
