b95e02881b6dfafec575a3f987faacb647b75546
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2023-09-20 11:03:26 +0200

    Build: Introduce `SCRIPT_DEBUG` global in webpack processing

--- tools/webpack/shared.js
+++ tools/webpack/shared.js
@@ -1,13 +1,14 @@
 /**
  * External dependencies
  */
+const { DefinePlugin } = require( 'webpack' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 const postcss = require( 'postcss' );
 const { join } = require( 'path' );
 
 const baseDir = join( __dirname, '../../' );
 
-const baseConfig = ( env ) => {
+const getBaseConfig = ( env ) => {
 	const mode = env.environment;
 
 	const config = {
@@ -41,6 +42,19 @@ const baseConfig = ( env ) => {
 		},
 		stats: 'errors-only',
 		watch: env.watch,
+		plugins: [
+			new DefinePlugin( {
+				// Inject the `IS_GUTENBERG_PLUGIN` global, used for feature flagging.
+				'process.env.IS_GUTENBERG_PLUGIN': false,
+				// Inject the `IS_WORDPRESS_CORE` global, used for feature flagging.
+				'process.env.IS_WORDPRESS_CORE': true,
+				// Inject the `SCRIPT_DEBUG` global, used for dev versions of JavaScript.
+				SCRIPT_DEBUG: mode === 'development',
+				'process.env.FORCE_REDUCED_MOTION': JSON.stringify(
+					process.env.FORCE_REDUCED_MOTION
+				),
+			} ),
+		],
 	};
 
 	if ( mode === 'development' && env.buildTarget === 'build/' ) {
@@ -79,7 +93,7 @@ const normalizeJoin = ( ...paths ) => join( ...paths ).replace( /\\/g, '/' );
 
 module.exports = {
 	baseDir,
-	baseConfig,
+	getBaseConfig,
 	normalizeJoin,
 	stylesTransform,
 };
