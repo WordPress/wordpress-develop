a15f935cb422f58f5cc96110f86214dd631e4b37
Jonny Harris <jon@spacedmonkey.co.uk>
2023-09-22 17:49:52 +0100

    Improve tests.

--- tests/phpunit/tests/blocks/getBlockAssetUrl.php
+++ tests/phpunit/tests/blocks/getBlockAssetUrl.php
@@ -11,6 +11,16 @@
  * @covers ::get_block_asset_url
  */
 class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
+
+	/**
+	 * Set up each test method.
+	 */
+	public function set_up() {
+		parent::set_up();
+		add_filter( 'template_directory_uri', array( $this, 'fix_url' ), 100, 2 );
+		add_filter( 'stylesheet_directory_uri', array( $this, 'fix_url' ), 100, 2 );
+	}
+
 	/**
 	 * @ticket 58525
 	 */
@@ -18,6 +28,7 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 		$path = ABSPATH . WPINC . '/blocks/file/view.min.js';
 		$url  = get_block_asset_url( $path );
 
+		$this->assertStringNotContainsString( ABSPATH . WPINC, $url );
 		$this->assertSame( includes_url( '/blocks/file/view.min.js' ), $url );
 	}
 
@@ -27,10 +38,12 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 	public function test_parent_theme() {
 		switch_theme( 'block-theme' );
 
-		$path = wp_normalize_path( get_template_directory() ) . '/blocks/example-block/view.js';
+		$path = get_template_directory() . '/blocks/example-block/view.js';
 		$url  = get_block_asset_url( $path );
 
+		$this->assertStringNotContainsString( get_template_directory(), $url );
 		$this->assertSame( get_template_directory_uri() . '/blocks/example-block/view.js', $url );
+
 	}
 
 	/**
@@ -39,10 +52,12 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 	public function test_child_theme() {
 		switch_theme( 'block-theme-child' );
 
-		$path = wp_normalize_path( get_stylesheet_directory() ) . '/blocks/example-block/view.js';
+		$path = get_stylesheet_directory() . '/blocks/example-block/view.js';
 		$url  = get_block_asset_url( $path );
 
+		$this->assertStringNotContainsString( get_stylesheet_directory(), $url );
 		$this->assertSame( get_stylesheet_directory_uri() . '/blocks/example-block/view.js', $url );
+
 	}
 
 	/**
@@ -52,8 +67,10 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 		$path = WP_PLUGIN_DIR . '/test-plugin/blocks/example-block/view.js';
 		$url  = get_block_asset_url( $path );
 
+		$this->assertStringNotContainsString( WP_PLUGIN_DIR, $url );
 		$this->assertSame( plugins_url( 'view.js', $path ), $url );
 		$this->assertStringStartsWith( WP_PLUGIN_URL, $url );
+
 	}
 
 	/**
@@ -63,7 +80,13 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 		$path = WPMU_PLUGIN_DIR . '/test-plugin/example-block/view.js';
 		$url  = get_block_asset_url( $path );
 
+		$this->assertStringNotContainsString( WPMU_PLUGIN_DIR, $url );
 		$this->assertSame( plugins_url( 'view.js', $path ), $url );
 		$this->assertStringStartsWith( WPMU_PLUGIN_URL, $url );
+	}
+
+
+	public function fix_url( $current, $template ) {
+		return content_url( 'themes' ) . '/' . $template;
 	}
 }
