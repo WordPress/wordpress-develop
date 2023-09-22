6f27722b8e9bca689fcf92e4ac3cd53433c9b81f
Jonny Harris <jon@spacedmonkey.co.uk>
2023-09-23 00:08:08 +0100

    Test 3

--- tests/phpunit/tests/blocks/getBlockAssetUrl.php
+++ tests/phpunit/tests/blocks/getBlockAssetUrl.php
@@ -11,7 +11,6 @@
  * @covers ::get_block_asset_url
  */
 class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
-
 	/**
 	 * Original theme directory.
 	 *
@@ -30,9 +29,6 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 
 		wp_clean_themes_cache();
 		unset( $GLOBALS['wp_themes'] );
-
-		add_filter( 'template_directory_uri', array( $this, 'fix_url' ), 100, 2 );
-		add_filter( 'stylesheet_directory_uri', array( $this, 'fix_url' ), 100, 2 );
 	}
 
 	public function tear_down() {
@@ -63,10 +59,9 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 	public function test_parent_theme() {
 		switch_theme( 'block-theme' );
 
-		$path = get_template_directory() . '/blocks/example-block/view.js';
+		$path = wp_normalize_path( realpath( DIR_TESTDATA . '/themedir1/block-theme/blocks/example-block/view.js' ));
 		$url  = get_block_asset_url( $path );
-		
-		$this->assertStringNotContainsString( get_template_directory(), $url );
+
 		$this->assertSame( get_template_directory_uri() . '/blocks/example-block/view.js', $url );
 
 	}
@@ -77,10 +72,9 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 	public function test_child_theme() {
 		switch_theme( 'block-theme-child' );
 
-		$path = get_stylesheet_directory() . '/blocks/example-block/view.js';
+		$path = wp_normalize_path( realpath( DIR_TESTDATA . '/themedir1/block-theme-child/blocks/example-block/view.js' ) );
 		$url  = get_block_asset_url( $path );
 
-		$this->assertStringNotContainsString( get_stylesheet_directory(), $url );
 		$this->assertSame( get_stylesheet_directory_uri() . '/blocks/example-block/view.js', $url );
 
 	}
@@ -108,10 +102,5 @@ class Tests_Get_Block_Asset_Url extends WP_UnitTestCase {
 		$this->assertStringNotContainsString( WPMU_PLUGIN_DIR, $url );
 		$this->assertSame( plugins_url( 'view.js', $path ), $url );
 		$this->assertStringStartsWith( WPMU_PLUGIN_URL, $url );
-	}
-
-
-	public function fix_url( $current, $template ) {
-		return content_url( 'themes' ) . '/' . $template;
 	}
 }
