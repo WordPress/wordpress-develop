c4cf5d4a813b4492b3dfdce7f9c03e27275872c3
Jonny Harris <jon@spacedmonkey.co.uk>
2023-09-25 22:42:35 +0100

    Charge variable.

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -7811,12 +7811,12 @@ function _prime_post_parents_caches( array $ids ) {
 		$fresh_posts = $wpdb->get_results( sprintf( "SELECT $wpdb->posts.ID, $wpdb->posts.post_parent FROM $wpdb->posts WHERE ID IN (%s)", implode( ',', $non_cached_ids ) ) );
 
 		if ( $fresh_posts ) {
-			$post_parrent_data = array();
+			$post_parent_data = array();
 			foreach ( $fresh_posts as $fresh_post ) {
-				$post_parrent_data[ (int) $fresh_post->ID ] = (int) $fresh_post->post_parent;
+				$post_parent_data[ (int) $fresh_post->ID ] = (int) $fresh_post->post_parent;
 			}
 
-			wp_cache_add_multiple( $post_parrent_data, 'post_parent' );
+			wp_cache_add_multiple( $post_parent_data, 'post_parent' );
 		}
 	}
 }
