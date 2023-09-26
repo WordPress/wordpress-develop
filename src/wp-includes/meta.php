73f6a2328ba4aedcc47aab8d392ce44a5401c3ad
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2023-09-26 08:17:53 -0600

    Update src/wp-includes/meta.php
    Co-authored-by: Jonny Harris <spacedmonkey@users.noreply.github.com>

--- src/wp-includes/meta.php
+++ src/wp-includes/meta.php
@@ -1463,14 +1463,18 @@ function register_meta( $object_type, $meta_key, $args, $deprecated = null ) {
 		}
 	}
 
-	// If the object_type is set, require a type that supports revisions when setting revisions_enabled to true.
-	if ( ! empty( $object_type ) && $args['revisions_enabled'] && ! post_type_supports( $object_type, 'revisions' ) ) {
+	$object_subtype = ! empty( $args['object_subtype'] ) ? $args['object_subtype'] : '';
+	if( $args['revisions_enabled'] ) {
+	     if( 'post' !== $object_type ) {
+	       _doing_it_wrong( __FUNCTION__, __( 'Meta keys cannot enable revisions support unless the object type supports revisions.' ), '6.4.0' );
+
+		  return false;
+	     } else if ( ! empty( $object_subtype ) && ! post_type_supports( $object_subtype, 'revisions' ) ) {
 		_doing_it_wrong( __FUNCTION__, __( 'Meta keys cannot enable revisions support unless the object type supports revisions.' ), '6.4.0' );
 
 		return false;
+	    }
 	}
-
-	$object_subtype = ! empty( $args['object_subtype'] ) ? $args['object_subtype'] : '';
 
 	// If `auth_callback` is not provided, fall back to `is_protected_meta()`.
 	if ( empty( $args['auth_callback'] ) ) {
