f1f7ce1b0e9de17d1b18f3b1e2d1e63115f8a616
Pascal Birchler <pascalb@google.com>
2023-09-27 21:09:45 +0200

    Fix mask config

--- tests/visual-regression/specs/visual-snapshots.test.js
+++ tests/visual-regression/specs/visual-snapshots.test.js
@@ -10,77 +10,77 @@ test.describe( 'Admin Visual Snapshots', () => {
 	test( 'All Posts', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/edit.php' );
 		await expect( page ).toHaveScreenshot( 'All Posts.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Categories', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/edit-tags.php', 'taxonomy=category' );
 		await expect( page ).toHaveScreenshot( 'Categories.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Tags', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/edit-tags.php', 'taxonomy=post_tag' );
 		await expect( page ).toHaveScreenshot( 'Tags.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Media Library', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/upload.php' );
 		await expect( page ).toHaveScreenshot( 'Media Library.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Add New Media', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/media-new.php' );
 		await expect( page ).toHaveScreenshot( 'Add New Media.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'All Pages', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/edit.php', 'post_type=page' );
 		await expect( page ).toHaveScreenshot( 'All Pages.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Comments', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/edit-comments.php' );
 		await expect( page ).toHaveScreenshot( 'Comments.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Widgets', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/widgets.php' );
 		await expect( page ).toHaveScreenshot( 'Widgets.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Menus', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/nav-menus.php' );
 		await expect( page ).toHaveScreenshot( 'Menus.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Plugins', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/plugins.php' );
 		await expect( page ).toHaveScreenshot( 'Plugins.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'All Users', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/users.php' );
 		await expect( page ).toHaveScreenshot( 'All Users.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
@@ -97,70 +97,70 @@ test.describe( 'Admin Visual Snapshots', () => {
 	test( 'Your Profile', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/profile.php' );
 		await expect( page ).toHaveScreenshot( 'Your Profile.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Available Tools', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/tools.php' );
 		await expect( page ).toHaveScreenshot( 'Available Tools.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Import', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/import.php' );
 		await expect( page ).toHaveScreenshot( 'Import.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Export', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/export.php' );
 		await expect( page ).toHaveScreenshot( 'Export.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Export Personal Data', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/export-personal-data.php' );
 		await expect( page ).toHaveScreenshot( 'Export Personal Data.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Erase Personal Data', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/erase-personal-data.php' );
 		await expect( page ).toHaveScreenshot( 'Erase Personal Data.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Reading Settings', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/options-reading.php' );
 		await expect( page ).toHaveScreenshot( 'Reading Settings.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Discussion Settings', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/options-discussion.php' );
 		await expect( page ).toHaveScreenshot( 'Discussion Settings.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Media Settings', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/options-media.php' );
 		await expect( page ).toHaveScreenshot( 'Media Settings.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 
 	test( 'Privacy Settings', async ({ admin, page }) => {
 		await admin.visitAdminPage( '/options-privacy.php' );
 		await expect( page ).toHaveScreenshot( 'Privacy Settings.png', {
-			masks: elementsToHide.map( ( selector ) => page.locator( selector ) ),
+			mask: elementsToHide.map( ( selector ) => page.locator( selector ) ),
 		});
 	} );
 } );
