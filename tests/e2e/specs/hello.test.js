1cd3f2f2801a7e175819787a39216104302c71c9
Pascal Birchler <pascalb@google.com>
2023-09-28 14:12:15 +0200

    Fix test title

--- tests/e2e/specs/hello.test.js
+++ tests/e2e/specs/hello.test.js
@@ -3,7 +3,7 @@
  */
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 
-test.describe( 'Site editor title', () => {
+test.describe( 'Hello World', () => {
 	test( 'Should load properly', async ( { admin, page }) => {
 		await admin.visitAdminPage( '/' );
 		await expect(
