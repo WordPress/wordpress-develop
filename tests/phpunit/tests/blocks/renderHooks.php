c1713a323d8b60517bc95624b9b6c34f7f75da71
Luis Herranz <luisherranz@gmail.com>
2023-09-28 18:41:12 +0200

    Inline test filters

--- tests/phpunit/tests/blocks/renderHooks.php
+++ tests/phpunit/tests/blocks/renderHooks.php
@@ -13,47 +13,49 @@ class Tests_Blocks_Render_Hooks extends WP_UnitTestCase {
 	 * @ticket 59131
 	 */
 	public function test_do_blocks_pre_render_filter() {
+		$remove_blocks_containing_wordpress = static function( $blocks ) {
+			foreach ( $blocks as $index => $block ) {
+				if ( strpos( $block['innerHTML'], 'WordPress' ) !== false ) {
+					unset( $blocks[ $index ] );
+				}
+			}
+			return $blocks;
+		};
+
 		$block_content = '
 			<!-- wp:core/paragraph --><p>Hello</p><!-- /wp:core/paragraph -->
 			<!-- wp:core/paragraph --><p>WordPress</p><!-- /wp:core/paragraph -->
 		';
 
-		add_filter( 'do_blocks_pre_render', array( $this, 'remove_blocks_containing_wordpress' ) );
+		add_filter( 'do_blocks_pre_render', $remove_blocks_containing_wordpress );
 
 		$result = do_blocks( $block_content );
 
-		remove_filter( 'do_blocks_pre_render', array( $this, 'remove_blocks_containing_wordpress' ) );
+		remove_filter( 'do_blocks_pre_render', $remove_blocks_containing_wordpress );
 
 		$this->assertSame( '<p>Hello</p>', trim( $result ) );
-	}
-	public function remove_blocks_containing_wordpress( $blocks ) {
-		foreach ( $blocks as $index => $block ) {
-			if ( strpos( $block['innerHTML'], 'WordPress' ) !== false ) {
-				unset( $blocks[ $index ] );
-			}
-		}
-		return $blocks;
 	}
 
 	/**
 	 * @ticket 59131
 	 */
 	public function test_do_blocks_post_render_filter() {
+		$remove_wordpress_paragraph = static function( $content ) {
+			$content = str_replace( '<p>WordPress</p>', '', $content );
+			return $content;
+		};
+
 		$block_content = '
 			<!-- wp:core/paragraph --><p>Hello</p><!-- /wp:core/paragraph -->
 			<!-- wp:core/paragraph --><p>WordPress</p><!-- /wp:core/paragraph -->
 		';
 
-		add_filter( 'do_blocks_post_render', array( $this, 'remove_wordpress_paragraph' ) );
+		add_filter( 'do_blocks_post_render', $remove_wordpress_paragraph );
 
 		$result = do_blocks( $block_content );
 
-		remove_filter( 'do_blocks_post_render', array( $this, 'remove_wordpress_paragraph' ) );
+		remove_filter( 'do_blocks_post_render', $remove_wordpress_paragraph );
 
 		$this->assertSame( '<p>Hello</p>', trim( $result ) );
-	}
-	public function remove_wordpress_paragraph( $content ) {
-		$content = str_replace( '<p>WordPress</p>', '', $content );
-		return $content;
 	}
 }
