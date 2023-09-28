44dc2e8e3c8c5117964ae9d8e406a01144889c99
Luis Herranz <luisherranz@gmail.com>
2023-09-28 18:41:12 +0200

    Rename test filters to explain what they do

--- tests/phpunit/tests/blocks/renderHooks.php
+++ tests/phpunit/tests/blocks/renderHooks.php
@@ -15,18 +15,18 @@ class Tests_Blocks_Render_Hooks extends WP_UnitTestCase {
 	public function test_do_blocks_pre_render_filter() {
 		$block_content = '
 			<!-- wp:core/paragraph --><p>Hello</p><!-- /wp:core/paragraph -->
-			<!-- wp:core/paragraph --><p>WordPress!</p><!-- /wp:core/paragraph -->
+			<!-- wp:core/paragraph --><p>WordPress</p><!-- /wp:core/paragraph -->
 		';
 
-		add_filter( 'do_blocks_pre_render', array( $this, 'do_blocks_pre_render_filter' ) );
+		add_filter( 'do_blocks_pre_render', array( $this, 'remove_blocks_containing_wordpress' ) );
 
 		$result = do_blocks( $block_content );
 
-		remove_filter( 'do_blocks_pre_render', array( $this, 'do_blocks_pre_render_filter' ) );
+		remove_filter( 'do_blocks_pre_render', array( $this, 'remove_blocks_containing_wordpress' ) );
 
 		$this->assertSame( '<p>Hello</p>', trim( $result ) );
 	}
-	public function do_blocks_pre_render_filter( $blocks ) {
+	public function remove_blocks_containing_wordpress( $blocks ) {
 		foreach ( $blocks as $index => $block ) {
 			if ( strpos( $block['innerHTML'], 'WordPress' ) !== false ) {
 				unset( $blocks[ $index ] );
@@ -41,19 +41,19 @@ class Tests_Blocks_Render_Hooks extends WP_UnitTestCase {
 	public function test_do_blocks_post_render_filter() {
 		$block_content = '
 			<!-- wp:core/paragraph --><p>Hello</p><!-- /wp:core/paragraph -->
-			<!-- wp:core/paragraph --><p>WordPress!</p><!-- /wp:core/paragraph -->
+			<!-- wp:core/paragraph --><p>WordPress</p><!-- /wp:core/paragraph -->
 		';
 
-		add_filter( 'do_blocks_post_render', array( $this, 'do_blocks_post_render_filter' ) );
+		add_filter( 'do_blocks_post_render', array( $this, 'remove_wordpress_paragraph' ) );
 
 		$result = do_blocks( $block_content );
 
-		remove_filter( 'do_blocks_post_render', array( $this, 'do_blocks_post_render_filter' ) );
+		remove_filter( 'do_blocks_post_render', array( $this, 'remove_wordpress_paragraph' ) );
 
 		$this->assertSame( '<p>Hello</p>', trim( $result ) );
 	}
-	public function do_blocks_post_render_filter( $content ) {
-		$content = str_replace( '<p>WordPress!</p>', '', $content );
+	public function remove_wordpress_paragraph( $content ) {
+		$content = str_replace( '<p>WordPress</p>', '', $content );
 		return $content;
 	}
 }
