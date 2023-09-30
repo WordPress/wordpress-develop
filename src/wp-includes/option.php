ef306ee0f9d14443e4bc398679f543ee0aebf75e
Jonny Harris <jon@spacedmonkey.co.uk>
2023-09-30 18:42:40 +0100

    Use transients in network transients.

--- src/wp-includes/option.php
+++ src/wp-includes/option.php
@@ -2223,7 +2223,9 @@ function delete_site_transient( $transient ) {
 	 */
 	do_action( "delete_site_transient_{$transient}", $transient );
 
-	if ( wp_using_ext_object_cache() || wp_installing() ) {
+	if ( ! is_multisite() ) {
+		$result = delete_transient( '_site_' . $transient );
+	} elseif ( wp_using_ext_object_cache() || wp_installing() ) {
 		$result = wp_cache_delete( $transient, 'site-transient' );
 	} else {
 		$option_timeout = '_site_transient_timeout_' . $transient;
@@ -2287,7 +2289,9 @@ function get_site_transient( $transient ) {
 		return $pre;
 	}
 
-	if ( wp_using_ext_object_cache() || wp_installing() ) {
+	if ( ! is_multisite() ) {
+		$value = get_transient( '_site_' . $transient );
+	} elseif ( wp_using_ext_object_cache() || wp_installing() ) {
 		$value = wp_cache_get( $transient, 'site-transient' );
 	} else {
 		// Core transients that do not have a timeout. Listed here so querying timeouts can be avoided.
@@ -2368,7 +2372,9 @@ function set_site_transient( $transient, $value, $expiration = 0 ) {
 	 */
 	$expiration = apply_filters( "expiration_of_site_transient_{$transient}", $expiration, $value, $transient );
 
-	if ( wp_using_ext_object_cache() || wp_installing() ) {
+	if ( ! is_multisite() ) {
+		$result = set_transient( '_site_' . $transient, $value, $expiration );
+	} elseif ( wp_using_ext_object_cache() || wp_installing() ) {
 		$result = wp_cache_set( $transient, $value, 'site-transient', $expiration );
 	} else {
 		$transient_timeout = '_site_transient_timeout_' . $transient;
