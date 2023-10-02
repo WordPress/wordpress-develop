e2e21cfef137c59ddc7c21c85572ad4fc441c5b5
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2023-10-02 13:37:31 +0200

    Add tests covering loading template and template part

--- tests/phpunit/tests/blocks/blockHooks.php
+++ tests/phpunit/tests/blocks/blockHooks.php
@@ -8,8 +8,16 @@
  * @since 6.4.0
  *
  * @group blocks
+ * @group block-hooks
  */
 class Tests_Blocks_BlockHooks extends WP_UnitTestCase {
+
+	/**
+	 * Registered block names.
+	 *
+	 * @var string[]
+	 */
+	private $registered_block_names = array();
 
 	/**
 	 * Tear down after each test.
@@ -17,15 +25,32 @@ class Tests_Blocks_BlockHooks extends WP_UnitTestCase {
 	 * @since 6.4.0
 	 */
 	public function tear_down() {
-		$registry = WP_Block_Type_Registry::get_instance();
-
-		foreach ( array( 'tests/my-block', 'tests/my-container-block' ) as $block_name ) {
-			if ( $registry->is_registered( $block_name ) ) {
-				$registry->unregister( $block_name );
-			}
+		while ( ! empty( $this->registered_block_names ) ) {
+			$block_name = array_pop( $this->registered_block_names );
+			unregister_block_type( $block_name );
 		}
 
 		parent::tear_down();
+	}
+
+	/**
+	 * @see register_block_type()
+	 */
+	private function register_block_type( $block_type, $args = array() ) {
+		$result = register_block_type( $block_type, $args );
+		$this->registered_block_names[] = $result->name;
+
+		return $result;
+	}
+
+	private function switch_to_block_theme_hooked_blocks() {
+		switch_theme( 'block-theme-hooked-blocks' );
+
+		$theme_blocks_dir = wp_normalize_path( realpath( get_theme_file_path( 'blocks' ) ) );
+		$this->register_block_type( $theme_blocks_dir . '/hooked-before' );
+		$this->register_block_type( $theme_blocks_dir . '/hooked-after' );
+		$this->register_block_type( $theme_blocks_dir . '/hooked-first-child' );
+		$this->register_block_type( $theme_blocks_dir . '/hooked-last-child' );
 	}
 
 	/**
@@ -45,7 +70,7 @@ class Tests_Blocks_BlockHooks extends WP_UnitTestCase {
 	 * @covers ::get_hooked_blocks
 	 */
 	public function test_get_hooked_blocks_matches_found() {
-		register_block_type(
+		$this->register_block_type(
 			'tests/injected-one',
 			array(
 				'block_hooks' => array(
@@ -55,7 +80,7 @@ class Tests_Blocks_BlockHooks extends WP_UnitTestCase {
 				),
 			)
 		);
-		register_block_type(
+		$this->register_block_type(
 			'tests/injected-two',
 			array(
 				'block_hooks' => array(
@@ -117,6 +142,64 @@ class Tests_Blocks_BlockHooks extends WP_UnitTestCase {
 			),
 			get_hooked_blocks( 'tests/hooked-at-before-and-after' ),
 			'block hooked before one block and after another'
+		);
+	}
+
+	/**
+	 * @ticket 59383
+	 *
+	 * @covers ::get_hooked_blocks
+	 * @covers ::get_block_file_template
+	 */
+	public function test_template_with_hooked_blocks() {
+		$this->switch_to_block_theme_hooked_blocks();
+
+		$template = get_block_file_template( get_stylesheet() . '//single' );
+
+		$this->assertStringNotContainsString(
+			'<!-- wp:tests/hooked-before /-->',
+			$template->content
+		);
+		$this->assertStringContainsString(
+			'<!-- wp:post-content {"layout":{"type":"constrained"}} /--><!-- wp:tests/hooked-after /-->',
+			$template->content
+		);
+		$this->assertStringNotContainsString(
+			'<!-- wp:tests/hooked-first-child /-->',
+			$template->content
+		);
+		$this->assertStringNotContainsString(
+			'<!-- wp:tests/hooked-last-child /-->',
+			$template->content
+		);
+	}
+
+	/**
+	 * @ticket 59383
+	 *
+	 * @covers ::get_hooked_blocks
+	 * @covers ::get_block_file_template
+	 */
+	public function test_template_part_with_hooked_blocks() {
+		$this->switch_to_block_theme_hooked_blocks();
+
+		$template = get_block_file_template( get_stylesheet() . '//header', 'wp_template_part' );
+
+		$this->assertStringContainsString(
+			'<!-- wp:tests/hooked-before /--><!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} /-->',
+			$template->content
+		);
+		$this->assertStringNotContainsString(
+			'<!-- wp:tests/hooked-after /-->',
+			$template->content
+		);
+		$this->assertStringNotContainsString(
+			'<!-- wp:tests/hooked-first-child /-->',
+			$template->content
+		);
+		$this->assertStringNotContainsString(
+			'<!-- wp:tests/hooked-last-child /-->',
+			$template->content
 		);
 	}
 }
