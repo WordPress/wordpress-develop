c9cb758bb6e988078d0d08a6b56657d0c53e8a85
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-10-04 09:28:53 +1100

    Rename function for clarity.

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -7797,13 +7797,17 @@ function _prime_post_caches( $ids, $update_term_cache = true, $update_meta_cache
 }
 
 /**
- * Prime post parent caches.
+ * Prime post parent ID caches.
+ *
+ * Prime the cache containing the parent ID of various post objects.
+ *
+ * @since 6.4.0
  *
  * @global wpdb $wpdb WordPress database abstraction object.
  *
  * @param int[] $ids ID list.
  */
-function _prime_post_parents_caches( array $ids ) {
+function _prime_post_parent_id_caches( array $ids ) {
 	global $wpdb;
 
 	$non_cached_ids = _get_non_cached_ids( $ids, 'post_parent' );
