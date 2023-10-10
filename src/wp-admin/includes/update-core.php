527f24185bd57bddc1d4937c3e2b1b9fa28606b5
hellofromtonya <tonya.mork@automattic.com>
2023-10-10 09:10:06 -0500

    Add the since annotation

--- src/wp-admin/includes/update-core.php
+++ src/wp-admin/includes/update-core.php
@@ -1845,6 +1845,7 @@ function _upgrade_440_force_deactivate_incompatible_plugins() {
  * @since 5.8.0
  * @since 5.9.0 The minimum compatible version of Gutenberg is 11.9.
  * @since 6.1.1 The minimum compatible version of Gutenberg is 14.1.
+ * @since 6.4.0 The minimum compatible version of Gutenberg is 16.5.
  */
 function _upgrade_core_deactivate_incompatible_plugins() {
 	if ( defined( 'GUTENBERG_VERSION' ) && version_compare( GUTENBERG_VERSION, '16.5', '<' ) ) {
