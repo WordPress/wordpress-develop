fb782808e879273f85a91a24b5bcddc76ba7d6b8
Pascal Birchler <pascalb@google.com>
2023-10-10 11:16:18 +0200

    Extend base PW config from wp-scripts

--- tests/performance/playwright.config.js
+++ tests/performance/playwright.config.js
@@ -3,7 +3,12 @@
  */
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
+
+/**
+ * WordPress dependencies
+ */
+import baseConfig from '@wordpress/scripts/config/playwright.config';
 
 process.env.WP_ARTIFACTS_PATH ??= path.join( process.cwd(), 'artifacts' );
 process.env.STORAGE_STATE_PATH ??= path.join(
@@ -13,6 +18,10 @@ process.env.STORAGE_STATE_PATH ??= path.join(
 process.env.TEST_RUNS ??= '20';
 
 const config = defineConfig( {
+	...baseConfig,
+	globalSetup: fileURLToPath(
+		new URL( './config/global-setup.js', 'file:' + __filename ).href
+	),
 	reporter: process.env.CI
 		? './config/performance-reporter.js'
 		: [ [ 'list' ], [ './config/performance-reporter.js' ] ],
@@ -22,45 +31,15 @@ const config = defineConfig( {
 	timeout: parseInt( process.env.TIMEOUT || '', 10 ) || 600_000, // Defaults to 10 minutes.
 	// Don't report slow test "files", as we will be running our tests in serial.
 	reportSlowTests: null,
-	testDir: './specs',
-	outputDir: path.join( process.env.WP_ARTIFACTS_PATH, 'test-results' ),
-	snapshotPathTemplate:
-		'{testDir}/{testFileDir}/__snapshots__/{arg}-{projectName}{ext}',
-	globalSetup: fileURLToPath(
-		new URL( './config/global-setup.js', 'file:' + __filename ).href
-	),
-	fullyParallel: false,
+	webServer: {
+		...baseConfig.webServer,
+		command: 'npm run env:start',
+	},
 	use: {
-		baseURL: process.env.WP_BASE_URL || 'http://localhost:8889',
-		headless: true,
-		viewport: {
-			width: 960,
-			height: 700,
-		},
-		ignoreHTTPSErrors: true,
-		locale: 'en-US',
-		contextOptions: {
-			reducedMotion: 'reduce',
-			strictSelectors: true,
-		},
-		storageState: process.env.STORAGE_STATE_PATH,
-		actionTimeout: 10_000, // 10 seconds.
-		trace: 'retain-on-failure',
-		screenshot: 'only-on-failure',
+		...baseConfig.default.use,
 		video: 'off',
 	},
-	webServer: {
-		command: 'npm run env:start',
-		port: 8889,
-		timeout: 120_000, // 120 seconds.
-		reuseExistingServer: true,
-	},
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices[ 'Desktop Chrome' ] },
-		},
-	],
 } );
 
 export default config;
+
--- tests/visual-regression/playwright.config.js
+++ tests/visual-regression/playwright.config.js
@@ -2,8 +2,12 @@
  * External dependencies
  */
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
+
+/**
+ * WordPress dependencies
+ */
+const baseConfig = require( '@wordpress/scripts/config/playwright.config' );
 
 process.env.WP_ARTIFACTS_PATH ??= path.join( process.cwd(), 'artifacts' );
 process.env.STORAGE_STATE_PATH ??= path.join(
@@ -12,49 +16,12 @@ process.env.STORAGE_STATE_PATH ??= path.join(
 );
 
 const config = defineConfig( {
-	reporter: process.env.CI ? [ [ 'github' ] ] : [ [ 'list' ] ],
-	forbidOnly: !! process.env.CI,
-	workers: 1,
-	retries: process.env.CI ? 2 : 0,
-	timeout: parseInt( process.env.TIMEOUT || '', 10 ) || 100_000, // Defaults to 100 seconds.
-	// Don't report slow test "files", as we will be running our tests in serial.
-	reportSlowTests: null,
-	testDir: './specs',
-	outputDir: path.join( process.env.WP_ARTIFACTS_PATH, 'test-results' ),
-	snapshotPathTemplate:
-		'{testDir}/{testFileDir}/__snapshots__/{arg}-{projectName}{ext}',
-	fullyParallel: false,
-	use: {
-		baseURL: process.env.WP_BASE_URL || 'http://localhost:8889',
-		headless: true,
-		viewport: {
-			width: 1000,
-			height: 750,
-		},
-		ignoreHTTPSErrors: true,
-		locale: 'en-US',
-		contextOptions: {
-			reducedMotion: 'reduce',
-			strictSelectors: true,
-		},
-		storageState: process.env.STORAGE_STATE_PATH,
-		actionTimeout: 10_000, // 10 seconds.
-		trace: 'retain-on-failure',
-		screenshot: 'only-on-failure',
-		video: 'on-first-retry',
-	},
+	...baseConfig,
+	globalSetup: undefined,
 	webServer: {
+		...baseConfig.webServer,
 		command: 'npm run env:start',
-		port: 8889,
-		timeout: 120_000, // 120 seconds.
-		reuseExistingServer: true,
 	},
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices[ 'Desktop Chrome' ] },
-		},
-	],
 } );
 
 export default config;
