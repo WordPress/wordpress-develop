802deb63d3e5ed92b28aa53c28771908eab3b33c
aristath <aristath@gmail.com>
2023-10-11 10:43:47 +0300

    set_up_before_class no longer necessary

--- tests/phpunit/tests/user/passwordHash.php
+++ tests/phpunit/tests/user/passwordHash.php
@@ -7,10 +7,6 @@
  */
 class Tests_User_PasswordHash extends WP_UnitTestCase {
 
-	public static function set_up_before_class() {
-		parent::set_up_before_class();
-	}
-
 	/**
 	 * Tests that PasswordHash::gensalt_blowfish() does not throw a deprecation notice on PHP 8.1.
 	 *
