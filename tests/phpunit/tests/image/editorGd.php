885a92a4386ed98d85907f7946eb0f36ccfb0c2e
aristath <aristath@gmail.com>
2023-10-11 10:40:58 +0300

    remove another set_up() which is no longer needed

--- tests/phpunit/tests/image/editorGd.php
+++ tests/phpunit/tests/image/editorGd.php
@@ -13,11 +13,6 @@ class Tests_Image_Editor_GD extends WP_Image_UnitTestCase {
 
 	public $editor_engine = 'WP_Image_Editor_GD';
 
-	public function set_up() {
-		// This needs to come after the mock image editor class is loaded.
-		parent::set_up();
-	}
-
 	public function tear_down() {
 		$folder = DIR_TESTDATA . '/images/waffles-*.jpg';
 
