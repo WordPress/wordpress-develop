e66402afb1b517aadf8010b64ce14b02d149bc00
Beda Schmid <beda@tukutoi.com>
2023-10-13 17:32:22 +0700

    Update shortcodes.php
    Simplifies the return value of `shortcode_parse_atts` to be always an array (either empty or with attributes)
    
    Signed-off-by: Beda Schmid <beda@tukutoi.com>

--- src/wp-includes/shortcodes.php
+++ src/wp-includes/shortcodes.php
@@ -602,9 +602,8 @@ function get_shortcode_atts_regex() {
  * @since 2.5.0
  *
  * @param string $text Shortcode arguments list.
- * @return array|string Array of attribute values keyed by attribute name.
- *                      Returns empty array if there are no attributes.
- *                      Returns the original arguments string if it cannot be parsed.
+ * @return array Array of attribute values keyed by attribute name.
+ *                      Returns empty array if there are no attributes or the original arguments string if it cannot be parsed.
  */
 function shortcode_parse_atts( $text ) {
 	$atts    = array();
@@ -635,8 +634,6 @@ function shortcode_parse_atts( $text ) {
 				}
 			}
 		}
-	} else {
-		$atts = ltrim( $text );
 	}
 
 	return $atts;
