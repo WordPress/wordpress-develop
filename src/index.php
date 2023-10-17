8ad88f14355b96e1540e609468c8a4d4de34607d
Adam Zieliński <adam@adamziel.com>
2023-10-17 17:10:55 +0200

    Dummy change to showcase the pr previewer

--- src/index.php
+++ src/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if($_GET['test']) {
+	die("If you see this message, the PR previewer work!");
+}
+
 /**
  * Note: this file exists only to remind developers to build the assets.
  * For the real index.php that gets built and boots WordPress,
