9ac30572f52df7c8eb25ac1c82eb5fccabfa0238
Joe McGill <joe.mcgill@10up.com>
2023-10-19 16:33:18 -0500

    Misc docblock and naming improvements to tests

--- tests/phpunit/tests/blocks/wpGetBlockPatterns.php
+++ tests/phpunit/tests/blocks/wpGetBlockPatterns.php
@@ -31,7 +31,7 @@ class Tests_Blocks_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 	/**
 	 * @ticket 59490
 	 *
-	 * @dataProvider data_wp_get_block_patterns
+	 * @dataProvider data_get_block_patterns
 	 *
 	 * @param string $theme_slug The theme's slug.
 	 * @param array  $expected   The expected pattern data.
@@ -60,31 +60,31 @@ class Tests_Blocks_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 				),
 			),
 			$this->get_pattern_cache( $theme ),
-			'The transient for block theme patterns should be set'
+			'The cache for block theme patterns should be set.'
 		);
 		$theme->delete_pattern_cache();
 		$this->assertFalse(
 			$this->get_pattern_cache( $theme ),
-			'The transient for block theme patterns should have been cleared'
+			'The cache for block theme patterns should have been cleared.'
 		);
 	}
 
 	/**
 	 * @ticket 59490
 	 */
-	public function test_should_clear_transient_after_switching_theme() {
+	public function test_should_clear_cache_after_switching_theme() {
 		switch_theme( 'block-theme' );
 		$theme1 = wp_get_theme();
 		$theme1->get_block_patterns();
 		$this->assertSameSets(
 			array(),
 			$this->get_pattern_cache( $theme1 ),
-			'The transient for block theme should be set'
+			'The cache for block theme should be set.'
 		);
 		switch_theme( 'block-theme-patterns' );
-		$this->assertFalse( $this->get_pattern_cache( $theme1 ), 'Transient should not be set for block theme after switch theme' );
+		$this->assertFalse( $this->get_pattern_cache( $theme1 ), 'Cache should not be set for block theme after switch theme.' );
 		$theme2 = wp_get_theme();
-		$this->assertFalse( $this->get_pattern_cache( $theme2 ), 'Transient should not be set for block theme patterns before being requested' );
+		$this->assertFalse( $this->get_pattern_cache( $theme2 ), 'Cache should not be set for block theme patterns before being requested.' );
 		$theme2->get_block_patterns( $theme2 );
 		$this->assertSameSets(
 			array(
@@ -97,7 +97,7 @@ class Tests_Blocks_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 
 			),
 			$this->get_pattern_cache( $theme2 ),
-			'The transient for block theme patterns should be set'
+			'The cache for block theme patterns should be set.'
 		);
 	}
 
@@ -106,7 +106,7 @@ class Tests_Blocks_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 	 *
 	 * @return array[]
 	 */
-	public function data_wp_get_block_patterns() {
+	public function data_get_block_patterns() {
 		return array(
 			array(
 				'theme'    => 'block-theme',
@@ -139,11 +139,11 @@ class Tests_Blocks_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that _wp_get_block_patterns() clears existing transient when in theme development mode.
+	 * Tests that WP_Theme::get_block_patterns() clears existing cache when in theme development mode.
 	 *
 	 * @ticket 59591
 	 */
-	public function test_should_clear_existing_transient_when_in_development_mode() {
+	public function test_should_clear_existing_cache_when_in_development_mode() {
 		$theme = wp_get_theme( 'block-theme-patterns' );
 
 		// Calling the function should set the cache.
@@ -158,7 +158,7 @@ class Tests_Blocks_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 				),
 			),
 			$this->get_pattern_cache( $theme ),
-			'The transient for block theme patterns should be set'
+			'The cache for block theme patterns should be set'
 		);
 
 		// Calling the function while in theme development mode should clear the cache.
@@ -167,7 +167,7 @@ class Tests_Blocks_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 		unset( $GLOBALS['_wp_tests_development_mode'] ); // Reset to not pollute other tests.
 		$this->assertFalse(
 			$this->get_pattern_cache( $theme ),
-			'The transient for block theme patterns should have been cleared due to theme development mode'
+			'The cache for block theme patterns should have been cleared due to theme development mode.'
 		);
 	}
 }
