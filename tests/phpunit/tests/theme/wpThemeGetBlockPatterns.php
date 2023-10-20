88fa6ec7cfb3e3810a234dc7dbfcb8062a346b47
Joe McGill <joe.mcgill@10up.com>
2023-10-20 11:49:32 -0500

    Fix test failures due to development mode bleed

--- tests/phpunit/tests/theme/wpThemeGetBlockPatterns.php
+++ tests/phpunit/tests/theme/wpThemeGetBlockPatterns.php
@@ -7,10 +7,21 @@
  * @since 6.4.0
  *
  * @group blocks
+ * @group themes
  *
  * @covers WP_Theme::get_block_patterns
  */
 class Tests_Theme_WPThemeGetBlockPatterns extends WP_UnitTestCase {
+
+	public static function wpSetUpBeforeClass() {
+		// Ensure development mode is reset before running these tests.
+		$GLOBALS['_wp_tests_development_mode'] = '';
+	}
+
+	public static function wpTearDownAfterClass() {
+		// Ensure development mode is reset after running these tests.
+		$GLOBALS['_wp_tests_development_mode'] = '';
+	}
 
 	/**
 	 * Test helper to access the private get_pattern_cache method of a theme.
@@ -49,7 +60,11 @@ class Tests_Theme_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 	 */
 	public function test_delete_pattern_cache() {
 		$theme = wp_get_theme( 'block-theme-patterns' );
+
+		$this->assertTrue( $theme->exists(), 'The test theme could not be found.' );
+
 		$theme->get_block_patterns();
+
 		$this->assertSameSets(
 			array(
 				'cta.php' => array(
@@ -75,16 +90,24 @@ class Tests_Theme_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 	public function test_should_clear_cache_after_switching_theme() {
 		switch_theme( 'block-theme' );
 		$theme1 = wp_get_theme();
+
+		$this->assertTrue( $theme1->exists(), 'The block-theme test theme could not be found.' );
+
 		$theme1->get_block_patterns();
 		$this->assertSameSets(
 			array(),
 			$this->get_pattern_cache( $theme1 ),
 			'The cache for block theme should be empty.'
 		);
+
 		switch_theme( 'block-theme-patterns' );
-		$this->assertFalse( $this->get_pattern_cache( $theme1 ), 'Cache should not be set for block theme after switch theme.' );
+
 		$theme2 = wp_get_theme();
+		$this->assertTrue( $theme2->exists(), 'The block-theme-patterns test theme could not be found.' );
+
+		$this->assertFalse( $this->get_pattern_cache( $theme1 ), 'Cache should not be set for block theme after switch theme.' );
 		$this->assertFalse( $this->get_pattern_cache( $theme2 ), 'Cache should not be set for block theme patterns before being requested.' );
+
 		$theme2->get_block_patterns( $theme2 );
 		$this->assertSameSets(
 			array(
@@ -146,6 +169,8 @@ class Tests_Theme_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 	public function test_should_clear_existing_cache_when_in_development_mode() {
 		$theme = wp_get_theme( 'block-theme-patterns' );
 
+		$this->assertTrue( $theme->exists(), 'The test theme could not be found.' );
+
 		// Calling the function should set the cache.
 		$theme->get_block_patterns();
 		$this->assertSameSets(
@@ -158,7 +183,7 @@ class Tests_Theme_WPThemeGetBlockPatterns extends WP_UnitTestCase {
 				),
 			),
 			$this->get_pattern_cache( $theme ),
-			'The cache for block theme patterns should be set'
+			'The cache for block theme patterns should be set.'
 		);
 
 		// Calling the function while in theme development mode should clear the cache.
