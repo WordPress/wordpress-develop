f4f99fc242a565eda8548a678cdd8423faf13e65
hellofromtonya <tonya.mork@automattic.com>
2023-10-23 19:28:01 -0500

    Tests: Ensure unhappy path IDs are unique.
    Unhappy path tests have non-string prefixes, which will be changed to the default empty string.
    
    To further check uniqueness within the dataset's expected results, the unhappy path test now allows each dataset to specify how many unique IDs to generate for its tests.

--- tests/phpunit/tests/functions/wpUniquePrefixedId.php
+++ tests/phpunit/tests/functions/wpUniquePrefixedId.php
@@ -79,16 +79,23 @@ class Tests_Functions_WpUniquePrefixedId extends WP_UnitTestCase {
 	 * @runInSeparateProcess
 	 * @preserveGlobalState disabled
 	 *
-	 * @param mixed  $non_string_prefix Non-string prefix.
-	 * @param string $expected_message  Expected notice message.
-	 * @param string $expected_value    Expected unique ID.
+	 * @param mixed  $non_string_prefix         Non-string prefix.
+	 * @param int    $number_of_ids_to_generate Number of IDs to generate.
+	 *                                          As the prefix will default to an empty string, changing the number of IDs generated within each dataset further tests ID uniqueness.
+	 * @param string $expected_message          Expected notice message.
+	 * @param array  $expected_ids              Expected unique IDs.
 	 */
-	public function test_should_raise_notice_and_use_empty_string_prefix_when_nonstring_given( $non_string_prefix, $expected_message, $expected_value ) {
+	public function test_should_raise_notice_and_use_empty_string_prefix_when_nonstring_given( $non_string_prefix, $number_of_ids_to_generate, $expected_message, $expected_ids ) {
 		$this->expectNotice();
 		$this->expectNoticeMessage( $expected_message );
 
-		$actual = wp_unique_prefixed_id( $non_string_prefix );
-		$this->assertSame( $expected_value, $actual );
+		$ids = array();
+		for ( $i = 0; $i < $number_of_ids_to_generate; $i++ ) {
+			$ids[] = wp_unique_prefixed_id( $non_string_prefix );
+		}
+
+		$this->assertSameSets( $ids, array_unique( $ids ), 'IDs are not unique.' );
+		$this->assertSameSets( $expected_ids, $ids, 'The IDs did not match the expected values.' );
 	}
 
 	/**
@@ -100,24 +107,28 @@ class Tests_Functions_WpUniquePrefixedId extends WP_UnitTestCase {
 		$message = 'wp_unique_prefixed_id(): The prefix must be a string. "%s" data type given.';
 		return array(
 			'prefix as null'         => array(
-				'non_string_prefix' => null,
-				'expected_message'  => sprintf( $message, 'NULL' ),
-				'expected_value'    => '3',
+				'non_string_prefix'         => null,
+				'number_of_ids_to_generate' => 2,
+				'expected_message'          => sprintf( $message, 'NULL' ),
+				'expected_ids'              => array( '1', '2' ),
 			),
 			'prefix as (int) 0'      => array(
-				'non_string_prefix' => 0,
-				'expected_message'  => sprintf( $message, 'integer' ),
-				'expected_value'    => '4',
+				'non_string_prefix'         => 0,
+				'number_of_ids_to_generate' => 3,
+				'expected_message'          => sprintf( $message, 'integer' ),
+				'expected_ids'              => array( '1', '2', '3' ),
 			),
 			'prefix as (int) 1'      => array(
-				'non_string_prefix'  => 1,
-				'expected_data_type' => sprintf( $message, 'integer' ),
-				'expected_value'     => '5',
+				'non_string_prefix'         => 1,
+				'number_of_ids_to_generate' => 4,
+				'expected_data_type'        => sprintf( $message, 'integer' ),
+				'expected_ids'              => array( '1', '2', '3', '4' ),
 			),
 			'prefix as (bool) false' => array(
-				'non_string_prefix'  => false,
-				'expected_data_type' => sprintf( $message, 'boolean' ),
-				'expected_value'     => '6',
+				'non_string_prefix'         => false,
+				'number_of_ids_to_generate' => 5,
+				'expected_data_type'        => sprintf( $message, 'boolean' ),
+				'expected_ids'              => array( '1', '2', '3', '4', '5' ),
 			),
 		);
 	}
