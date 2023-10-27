3e8dcbf74aba3fe0109bf75163448ffcc961bc29
jrfnl <jrfnl@users.noreply.github.com>
2023-10-27 21:42:59 +0200

    Docs: use proper case for `@todo` tags
    The correct tag is `@todo`, not `@TODO` or `@todo:` (note the trailing colon).
    
    Ref: https://developer.wordpress.org/coding-standards/inline-documentation-standards/php/#phpdoc-tags

--- src/wp-includes/html-api/class-wp-html-tag-processor.php
+++ src/wp-includes/html-api/class-wp-html-tag-processor.php
@@ -2031,7 +2031,7 @@ class WP_HTML_Tag_Processor {
 		 *
 		 * @see https://html.spec.whatwg.org/#attributes-2
 		 *
-		 * @TODO as the only regex pattern maybe we should take it out? are
+		 * @todo As the only regex pattern maybe we should take it out? are
 		 *       Unicode patterns available broadly in Core?
 		 */
 		if ( preg_match(
