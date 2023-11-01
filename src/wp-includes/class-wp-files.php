d6aa9ceb5882f3868454f648259a4da68b266a90
Karthik Thayyil <thayyilkarthik@gmail.com>
2023-11-01 01:47:31 +0000

    phpcs fixes

--- src/wp-includes/class-wp-files.php
+++ src/wp-includes/class-wp-files.php
@@ -9,29 +9,50 @@
 
 #[AllowDynamicProperties]
 class WP_Files {
+	/**
+	 * @var array
+	 */
 	private $scanned_files = array();
 
+	/**
+	 * WP_Files constructor.
+	 */
 	public function __construct() {
 		$cached_files = wp_cache_get( 'files_exists', '', true );
-		if( ! empty( $cached_files ) ) {
-			$this->scanned_files = $cached_files;	
+		if ( ! empty( $cached_files ) ) {
+			$this->scanned_files = $cached_files;
 		}
 	}
 
+	/**
+	 * Check if file exists.
+	 *
+	 * @param string $file
+	 *
+	 * @return bool
+	 */
 	public function exists( $file ) {
-		if ( isset( $this->scanned_files[ $file ] ) && 0 === $this->scanned_files[ $file ] ) {
-			return (bool)$this->scanned_files[ $file ];
+		if ( isset( $this->scanned_files[ $file ] ) ) {
+			return (bool) $this->scanned_files[ $file ];
 		}
 
-		$exists = file_exists( $file );
+		$exists                       = file_exists( $file );
 		$this->scanned_files[ $file ] = (int) $exists;
 		return $exists;
 	}
 
+	/**
+	 * Update exists cache.
+	 */
 	public function update_exists_cache() {
 		wp_cache_set( 'files_exists', $this->scanned_files );
 	}
 
+	/**
+	 * Get scanned files.
+	 *
+	 * @return array
+	 */
 	public function get_scanned_files() {
 		return $this->scanned_files;
 	}
