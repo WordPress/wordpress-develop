23354e877825b1c2d06dfa66f5e536bb056d14c6
Paul Bearne <pbearne@gmail.com>
2023-11-01 15:20:22 -0400

    Updated comments

--- tests/phpunit/tests/functions/wpDeleteFileFromDirectory.php
+++ tests/phpunit/tests/functions/wpDeleteFileFromDirectory.php
@@ -9,6 +9,8 @@
 class Tests_functions_wpDeleteFileFromDirectory extends WP_UnitTestCase{
 
 	/**
+	 * call with file and correct directory.
+	 *
 	 * @ticket 59781
 	 */
 	public function test_wp_delete_file_from_directory() {
@@ -20,15 +22,24 @@ class Tests_functions_wpDeleteFileFromDirectory extends WP_UnitTestCase{
 		$this->assertFileDoesNotExist( $file );
 	}
 
+	/**
+	 * Call with missing file.
+	 *
+	 * @ticket 59781
+	 */
 	public function test_wp_delete_file_from_directory_no_file() {
 		$file      = realpath( DIR_TESTDATA ) . '/temp_file.txt';
 		$directory = realpath( DIR_TESTDATA );
-//		touch( $file );
 
 		$this->assertFalse( wp_delete_file_from_directory( $file, $directory ) );
 		$this->assertFileDoesNotExist( $file );
 	}
 
+	/**
+	 * Call with steamed file.
+	 *
+	 * @ticket 59781
+	 */
 	public function test_wp_delete_file_from_directory_file_steam() {
 		$file      = realpath( DIR_TESTDATA ) . '/temp_file.txt';
 		$directory = realpath( DIR_TESTDATA );
@@ -38,6 +49,11 @@ class Tests_functions_wpDeleteFileFromDirectory extends WP_UnitTestCase{
 		$this->assertFileDoesNotExist( $file );
 	}
 
+	/**
+	 * Call with unsorted stream file.
+	 *
+	 * @ticket 59781
+	 */
 	public function test_wp_delete_file_from_directory_non_file_steam() {
 		$file      = realpath( DIR_TESTDATA ) . '/temp_file.txt';
 		$directory = realpath( DIR_TESTDATA );
@@ -47,6 +63,12 @@ class Tests_functions_wpDeleteFileFromDirectory extends WP_UnitTestCase{
 		$this->assertFileExists( $file );
 		@unlink( $file );
 	}
+
+	/**
+	 * Call with incorrect directory.
+	 *
+	 * @ticket 59781
+	 */
 	public function test_wp_delete_file_from_directory_no_folder() {
 		$file      = realpath( DIR_TESTDATA ) . '/temp_file.txt';
 		$directory = realpath( DIR_TESTDATA ) . '/images';
