3400bd11d22ef1c14af8240daa8ea50b53474738
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-11-06 09:50:03 +1100

    Various test changes.

--- tests/phpunit/tests/functions/wpGetNocacheHeaders.php
+++ tests/phpunit/tests/functions/wpGetNocacheHeaders.php
@@ -1,19 +1,53 @@
 <?php
-
 /**
- * @group functions.php
+ * Tests for wp_get_nocache_headers().
+ *
+ * @group functions
+ *
  * @covers ::wp_get_nocache_headers
  */
 class Tests_Functions_wpGetNocacheHeaders extends WP_UnitTestCase {
+	/**
+	 * User ID.
+	 *
+	 * @var int
+	 */
+	public static $user_id;
+
+	/**
+	 * Create user account for tests.
+	 *
+	 * @param WP_UnitTest_Factory $factory
+	 */
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		self::$user_id = $factory->user->create();
+	}
 
 	/**
 	 * @ticket 54490
 	 */
-	public function test_wp_get_nocache_headers() {
+	public function test_wp_get_nocache_headers_for_logged_out_user() {
 		$this->assertSameSetsWithIndex(
 			array(
 				'Expires'       => 'Wed, 11 Jan 1984 05:00:00 GMT',
 				'Cache-Control' => 'no-cache, must-revalidate, max-age=0',
+				'Last-Modified' => false,
+			),
+			wp_get_nocache_headers()
+		);
+	}
+
+	/**
+	 * @ticket 21938
+	 * @ticket 54490
+	 */
+	public function test_wp_get_nocache_headers_for_logged_in_user() {
+		wp_set_current_user( self::$user_id );
+
+		$this->assertSameSetsWithIndex(
+			array(
+				'Expires'       => 'Wed, 11 Jan 1984 05:00:00 GMT',
+				'Cache-Control' => 'no-cache, must-revalidate, max-age=0, no-store, private',
 				'Last-Modified' => false,
 			),
 			wp_get_nocache_headers()
@@ -26,7 +60,7 @@ class Tests_Functions_wpGetNocacheHeaders extends WP_UnitTestCase {
 	public function test_filter_nocache_headers() {
 		add_filter(
 			'nocache_headers',
-			static function() {
+			static function () {
 				return array( 'filter_name' => 'nocache_headers' );
 			}
 		);
