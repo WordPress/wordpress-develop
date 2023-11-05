6534d177fd91022cb5c48650aaf45b8fa3aa8e01
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-11-06 09:32:48 +1100

    Various changes to get_num_queries test.

--- tests/phpunit/tests/functions/getNumQueries.php
+++ tests/phpunit/tests/functions/getNumQueries.php
@@ -1,23 +1,27 @@
 <?php
-
 /**
- * @group functions.php
+ * Test get_num_queries().
+ *
+ * @group functions
+ *
  * @covers ::get_num_queries
  */
 class Tests_Functions_getNumQueries extends WP_UnitTestCase {
 	/**
+	 * Ensure making a database query increases the number of queries.
+	 *
 	 * @ticket 54490
 	 */
-	public function test_wp_get_num_queries() {
+	public function test_get_num_queries() {
 		global $wpdb;
 
 		$current_count = get_num_queries();
 		$this->assertIsInt( $current_count, 'get_num_queries() did not return an integer.' );
 
-		// do a single db query
-		$wpdb->query( "select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='tableName'" );
+		// Do a single database query.
+		$wpdb->query( 'SELECT NOW();' );
 
-		// check the count updated by 1
-		$this->assertSame( $current_count + 1, get_num_queries(), 'The number of queries did not increase by 1.' );
+		// Check the count increased by one.
+		$this->assertSame( $current_count + 1, get_num_queries(), 'The number of queries did not increase by one.' );
 	}
 }
