ff39116c22c325f9da8fbef283d00a1a70956b46
Pascal Birchler <pascalb@google.com>
2023-11-06 16:12:28 +0100

    Add admin tests

---
+++ tests/performance/specs/admin.test.js
@@ -0,0 +1,52 @@
+/**
+ * WordPress dependencies
+ */
+import { test } from '@wordpress/e2e-test-utils-playwright';
+
+/**
+ * Internal dependencies
+ */
+import { camelCaseDashes } from '../utils';
+
+const results = {
+	timeToFirstByte: [],
+	largestContentfulPaint: [],
+	lcpMinusTtfb: [],
+};
+
+test.describe( 'Admin', () => {
+	test.use( {
+		storageState: {}, // User will be logged out.
+	} );
+
+	test.beforeAll( async ( { requestUtils } ) => {
+		await requestUtils.activateTheme( 'twentytwentyone' );
+	} );
+
+	test.afterAll( async ( {}, testInfo ) => {
+		await testInfo.attach( 'results', {
+			body: JSON.stringify( results, null, 2 ),
+			contentType: 'application/json',
+		} );
+	} );
+
+	const iterations = Number( process.env.TEST_RUNS );
+	for ( let i = 1; i <= iterations; i++ ) {
+		test( `Measure load time metrics (${ i } of ${ iterations })`, async ( {
+			page,
+			metrics,
+		} ) => {
+			await page.goto( '/' );
+
+			const serverTiming = await metrics.getServerTiming();
+
+			for (const [key, value] of Object.entries( serverTiming ) ) {
+				results[ camelCaseDashes( key ) ] ??= [];
+				results[ camelCaseDashes( key ) ].push( value );
+			}
+
+			const ttfb = await metrics.getTimeToFirstByte();
+			results.timeToFirstByte.push( ttfb );
+		} );
+	}
+} );
