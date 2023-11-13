2c36b8cd8a2b6efe5542746ebe2dd8cd3dafb1b7
Pascal Birchler <pascalb@google.com>
2023-11-13 18:00:32 +0100

    Update API fixture file

--- tests/qunit/fixtures/wp-api-generated.js
+++ tests/qunit/fixtures/wp-api-generated.js
@@ -10228,6 +10228,11 @@ mockedApiResponse.Schema = {
                             "type": "string",
                             "required": false
                         },
+                        "admin_language": {
+                            "description": "WordPress admin locale code.",
+                            "type": "string",
+                            "required": false
+                        },
                         "use_smilies": {
                             "description": "Convert emoticons like :-) and :-P to graphics on display.",
                             "type": "boolean",
@@ -13086,6 +13091,7 @@ mockedApiResponse.settings = {
     "time_format": "g:i a",
     "start_of_week": 1,
     "language": "en_US",
+    "admin_language": "en_US",
     "use_smilies": true,
     "default_category": 1,
     "default_post_format": "0",
