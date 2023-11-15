2bc890f73b5f5c6578582e156e52c673351b5b12
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-11-15 13:04:40 -0800

    Support Port in Site Creation
    Rather than sanitizing the domain as if it was a username we should
    sanitize it using the character set defined in the `site-new.php` form.

--- tests/phpunit/tests/multisite/site.php
+++ tests/phpunit/tests/multisite/site.php
@@ -1140,6 +1140,22 @@ if ( is_multisite() ) :
 						'lang_id'  => 1,
 					),
 				),
+				array(
+					array(
+						'domain' => 'example.com:8888',
+					),
+					array(
+						'domain'     => 'example.com:8888',
+						'path'       => '/',
+						'network_id' => 1,
+						'public'     => 1,
+						'archived'   => 0,
+						'mature'     => 0,
+						'spam'       => 0,
+						'deleted'    => 0,
+						'lang_id'    => 0,
+					),
+				),
 			);
 		}
 
@@ -1240,6 +1256,16 @@ if ( is_multisite() ) :
 						'spam'     => 1,
 						'deleted'  => 1,
 						'lang_id'  => 1,
+					),
+				),
+				array(
+					array(
+						'domain'     => 'example.com:8888',
+						'network_id' => 2,
+					),
+					array(
+						'domain'  => 'example.com:8888',
+						'site_id' => 2,
 					),
 				),
 			);
