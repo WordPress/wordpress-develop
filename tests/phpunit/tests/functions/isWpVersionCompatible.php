a01276383bdbb91c93e8efbb8c10b86bbdd9dfd7
Andy Fragen <andy@thefragens.com>
2023-11-16 10:16:28 -0800

    wpcs fix

--- tests/phpunit/tests/functions/isWpVersionCompatible.php
+++ tests/phpunit/tests/functions/isWpVersionCompatible.php
@@ -87,15 +87,14 @@ class Tests_Functions_IsWpVersionCompatible extends WP_UnitTestCase {
 			),
 
 			// Improper WP version.
-			'improper trailing x.x.0'            => array(
+			'improper trailing x.x.0'   => array(
 				'required' => '5.2.0',
 				'expected' => true,
 			),
-			'correct version'            => array(
+			'correct version'           => array(
 				'required' => '5.2',
 				'expected' => true,
 			),
-
 		);
 	}
 
