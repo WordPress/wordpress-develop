e08ac43c630a4f91d2d98faaa5ebcb7a6907b558
Andy Fragen <andy@thefragens.com>
2023-11-16 11:21:27 -0800

    refactor check, update $version before wp_trigger_error

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -8716,14 +8716,15 @@ function is_wp_version_compatible( $required ) {
 	// Strip off any -alpha, -RC, -beta, -src suffixes.
 	list( $version ) = explode( '-', $wp_version );
 
-	if ( is_string( $required ) && str_ends_with( $required, '.0' ) ) {
+	if ( is_string( $required ) && substr_count( $required, '.' ) > 2 && str_ends_with( $required, '.0' ) ) {
+		$required = rtrim( $required, '.0' );
+
 		wp_trigger_error(
 			__FUNCTION__,
-			"`{$required}` is not a valid WordPress version string.",
+			'Not a valid WordPress version string.',
 			E_USER_NOTICE
 		);
 
-		$required = rtrim( $required, '.0' );
 	}
 
 	return empty( $required ) || version_compare( $version, $required, '>=' );
