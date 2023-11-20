8e42b10f53bbc95fd1a22570d9742986cec55e59
Rajinsharwar <rajinsharwar@gmail.com>
2023-11-20 16:36:06 -0500

    Fixing unit tests

--- tests/phpunit/tests/theme/autoloadThemeMods.php
+++ tests/phpunit/tests/theme/autoloadThemeMods.php
@@ -8,44 +8,44 @@
 class Tests_Autoload_Theme_Mods extends WP_UnitTestCase {
 
 	/**
-	 * Test switching themes and verifying autoload values.
-	 */
-	public function test_switch_theme_autoload() {
-		// Set up the old theme.
-		$old_theme_stylesheet = 'old-theme';
-		$old_theme_mods = array(
-			'option1' => 'value1',
-			'option2' => 'value2',
-		);
-		update_option( "theme_mods_$old_theme_stylesheet", $old_theme_mods );
+     * Test switching themes and verifying autoload values.
+     */
+    public function test_switch_theme_autoload() {
+        // Set up the old theme.
+        $old_theme_stylesheet = 'old-theme';
+        $old_theme_mods = array(
+            'option1' => 'value1',
+            'option2' => 'value2',
+        );
+        update_option( "theme_mods_$old_theme_stylesheet", $old_theme_mods );
 
-		// Set up the new theme.
-		$new_theme_stylesheet = 'new-theme';
-		$new_theme_mods = array(
-			'option3' => 'value3',
-			'option4' => 'value4',
-		);
-		update_option( "theme_mods_$new_theme_stylesheet", $new_theme_mods );
+        // Set up the new theme.
+        $new_theme_stylesheet = 'new-theme';
+        $new_theme_mods = array(
+            'option3' => 'value3',
+            'option4' => 'value4',
+        );
+        update_option( "theme_mods_$new_theme_stylesheet", $new_theme_mods );
 
-		// Switch themes.
-		switch_theme( $new_theme_stylesheet );
+        // Switch themes.
+        switch_theme( $new_theme_stylesheet );
 
-		// Verify autoload values.
-		$autoload_old_theme = get_option( "theme_mods_$old_theme_stylesheet_autoload" );
-		$autoload_new_theme = get_option( "theme_mods_$new_theme_stylesheet_autoload" );
+        // Verify autoload values.
+        $autoload_old_theme = wp_cache_get( "autoload_theme_mods_$old_theme_stylesheet", 'options' );
+        $autoload_new_theme = wp_cache_get( "autoload_theme_mods_$new_theme_stylesheet", 'options' );
 
-		// Expect autoload value for the old theme to be 'no'.
-		$this->assertEquals( 'no', $autoload_old_theme );
+        // Expect autoload value for the old theme to be 'no'.
+        $this->assertFalse( $autoload_old_theme );
 
-		// Expect autoload value for the new theme to be 'yes'.
-		$this->assertEquals( 'yes', $autoload_new_theme );
+        // Expect autoload value for the new theme to be 'yes'.
+        $this->assertTrue( $autoload_new_theme );
 
-		// Clean up.
-		delete_option( "theme_mods_$old_theme_stylesheet" );
-		delete_option( "theme_mods_$new_theme_stylesheet" );
-		delete_option( 'theme_switched' );
-		delete_option( 'current_theme' );
-	}
+        // Clean up.
+        delete_option( "theme_mods_$old_theme_stylesheet" );
+        delete_option( "theme_mods_$new_theme_stylesheet" );
+        delete_option( 'theme_switched' );
+        delete_option( 'current_theme' );
+    }
 
 	/**
 	 * Test switching themes and verifying that options are loaded properly.
