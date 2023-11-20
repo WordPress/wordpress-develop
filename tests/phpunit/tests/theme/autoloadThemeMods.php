e6819805306d8a450330c6f23c99a815b3a0aff1
Rajinsharwar <rajinsharwar@gmail.com>
2023-11-20 16:48:54 -0500

    Fixing unit tests - 2

--- tests/phpunit/tests/theme/autoloadThemeMods.php
+++ tests/phpunit/tests/theme/autoloadThemeMods.php
@@ -5,74 +5,38 @@
  *
  * @covers ::switch_theme
  */
-class Tests_Autoload_Theme_Mods extends WP_UnitTestCase {
+class WP_Test_Autoload_Theme_Mods extends WP_UnitTestCase {
 
-	/**
-     * Test switching themes and verifying autoload values.
-     */
-    public function test_switch_theme_autoload() {
-        // Set up the old theme.
-        $old_theme_stylesheet = 'old-theme';
-        $old_theme_mods = array(
-            'option1' => 'value1',
-            'option2' => 'value2',
-        );
-        update_option( "theme_mods_$old_theme_stylesheet", $old_theme_mods );
+    public function test_on_switch_theme_previous_mods_should_not_be_autoload() {
+        global $wpdb;
 
-        // Set up the new theme.
-        $new_theme_stylesheet = 'new-theme';
-        $new_theme_mods = array(
-            'option3' => 'value3',
-            'option4' => 'value4',
-        );
-        update_option( "theme_mods_$new_theme_stylesheet", $new_theme_mods );
+        $current_theme_stylesheet = get_stylesheet();
 
-        // Switch themes.
+        // Set a theme mod for the current theme
+        $new_theme_stylesheet = 'theme1';
+        set_theme_mod( 'default-theme-option', 'a-value' );
+
         switch_theme( $new_theme_stylesheet );
 
-        // Verify autoload values.
-        $autoload_old_theme = wp_cache_get( "autoload_theme_mods_$old_theme_stylesheet", 'options' );
-        $autoload_new_theme = wp_cache_get( "autoload_theme_mods_$new_theme_stylesheet", 'options' );
+        $this->assertEquals( 'no', $wpdb->get_var( "SELECT autoload FROM $wpdb->options WHERE option_name = 'theme_mods_$current_theme_stylesheet'" ) );
+        $this->assertEquals( 'yes', $wpdb->get_var( "SELECT autoload FROM $wpdb->options WHERE option_name = 'theme_mods_$new_theme_stylesheet'" ) );
 
-        // Expect autoload value for the old theme to be 'no'.
-        $this->assertFalse( $autoload_old_theme );
+        // Make sure that autoloaded options are cached properly
+        $autoloaded_options = wp_cache_get( 'alloptions', 'options' );
+        $this->assertTrue( array_key_exists( "theme_mods_$new_theme_stylesheet", $autoloaded_options ) );
+        $this->assertFalse( array_key_exists( "theme_mods_$current_theme_stylesheet", $autoloaded_options ) );
 
-        // Expect autoload value for the new theme to be 'yes'.
-        $this->assertTrue( $autoload_new_theme );
+        switch_theme( $current_theme_stylesheet );
 
-        // Clean up.
-        delete_option( "theme_mods_$old_theme_stylesheet" );
-        delete_option( "theme_mods_$new_theme_stylesheet" );
-        delete_option( 'theme_switched' );
-        delete_option( 'current_theme' );
+        $this->assertEquals( 'yes', $wpdb->get_var( "SELECT autoload FROM $wpdb->options WHERE option_name = 'theme_mods_$current_theme_stylesheet'" ) );
+        $this->assertEquals( 'no', $wpdb->get_var( "SELECT autoload FROM $wpdb->options WHERE option_name = 'theme_mods_$new_theme_stylesheet'" ) );
+
+        // Make sure that autoloaded options are cached properly
+        $autoloaded_options = wp_cache_get( 'alloptions', 'options' );
+        $this->assertFalse( array_key_exists( "theme_mods_$new_theme_stylesheet", $autoloaded_options ) );
+        $this->assertTrue( array_key_exists( "theme_mods_$current_theme_stylesheet", $autoloaded_options ) );
+
+        // And that we haven't lost the mods
+        $this->assertEquals( 'a-value', get_theme_mod( 'default-theme-option' ) );
     }
-
-	/**
-	 * Test switching themes and verifying that options are loaded properly.
-	 */
-	public function test_switch_theme_options_load() {
-		// Set up the new theme.
-		$new_theme_stylesheet = 'new-theme';
-		$new_theme_mods = array(
-			'option5' => 'value5',
-			'option6' => 'value6',
-		);
-		update_option( "theme_mods_$new_theme_stylesheet", $new_theme_mods );
-
-		// Switch themes.
-		switch_theme( $new_theme_stylesheet );
-
-		// Verify that options are loaded properly for the new theme.
-		$option5_value = get_theme_mod( 'option5' );
-		$option6_value = get_theme_mod( 'option6' );
-
-		// Expect the values to match the ones set during setup.
-		$this->assertEquals( 'value5', $option5_value );
-		$this->assertEquals( 'value6', $option6_value );
-
-		// Clean up.
-		delete_option( "theme_mods_$new_theme_stylesheet" );
-		delete_option( 'theme_switched' );
-		delete_option( 'current_theme' );
-	}
 }
