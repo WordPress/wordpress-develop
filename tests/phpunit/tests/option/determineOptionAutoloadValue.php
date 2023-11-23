9ac849a41d00b78f5bb9aa317c4d7f4c97f89798
Paul Bearne <pbearne@gmail.com>
2023-11-23 14:13:02 -0500

    moved test to correct file

--- tests/phpunit/tests/option/determineOptionAutoloadValue.php
+++ tests/phpunit/tests/option/determineOptionAutoloadValue.php
@@ -7,11 +7,59 @@
  */
 class Tests_Determine_Option_Autoload_Value extends WP_UnitTestCase {
 	public function set_up() {
+		add_filter( 'max_option_size', array( $this, 'filter_max_option_size' ) );
 		parent::set_up();
 		add_filter( 'max_option_size', array( $this, 'filter_max_option_size' ) );
 	}
 
 	/**
+	 * @ticket 42441
+	 *
+	 * @dataProvider data_values
+	 *
+	 * @param $autoload
+	 * @param $expected
+	 */
+	public function test_get_autoload_value_values( $autoload, $expected ) {
+		$test = determine_option_autoload_value( null, '', $autoload );
+		$this->assertSame( $expected, $test );
+	}
+
+	public function data_values() {
+		return array(
+			'yes'         => array(
+				'autoload' => 'yes',
+				'expected' => 'yes',
+			),
+			'true'        => array(
+				'autoload' => true,
+				'expected' => 'yes',
+			),
+			'no'          => array(
+				'autoload' => 'no',
+				'expected' => 'no',
+			),
+			'false'       => array(
+				'autoload' => false,
+				'expected' => 'no',
+			),
+			'default-yes' => array(
+				'autoload' => 'default-yes',
+				'expected' => 'default-yes',
+			),
+			'default-no'  => array(
+				'autoload' => 'default-no',
+				'expected' => 'default-no',
+			),
+			'null'        => array(
+				'autoload' => null,
+				'expected' => 'default-yes',
+			),
+		);
+	}
+
+	/**
+	 *
 	 * @ticket 42441
 	 */
 	public function test_small_option() {
