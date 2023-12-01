e9a2619dd06d79fe7a64a7411673a68b532bceb3
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-12-01 09:15:32 +0530

    Apply suggestions from code review
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- tests/phpunit/tests/theme/autoloadThemeMods.php
+++ tests/phpunit/tests/theme/autoloadThemeMods.php
@@ -31,22 +31,12 @@ class Tests_Autoload_Theme_Mods extends WP_Theme_UnitTestCase {
 		$this->assertSame( 'no', $wpdb->get_var( $wpdb->prepare( "SELECT autoload FROM $wpdb->options WHERE option_name = %s", "theme_mods_$current_theme_stylesheet" ) ), 'Theme mods autoload value not set to no in database' );
 		$this->assertSame( 'yes', $wpdb->get_var( $wpdb->prepare( "SELECT autoload FROM $wpdb->options WHERE option_name = %s", "theme_mods_$new_theme_stylesheet" ) ), 'Theme mods autoload value not set to yes in database' );
 
-		// Make sure that autoloaded options are cached properly.
-		$autoloaded_options = wp_cache_get( 'alloptions', 'options' );
-		$this->assertArrayHasKey( "theme_mods_$new_theme_stylesheet", $autoloaded_options, "Option theme_mods_$current_theme_stylesheet unexpectedly deleted from alloptions cache" );
-		$this->assertArrayNotHasKey( "theme_mods_$current_theme_stylesheet", $autoloaded_options, "Option theme_mods_$new_theme_stylesheet not deleted from alloptions cache" );
-
 		switch_theme( $current_theme_stylesheet );
 
 		$this->assertSame( 'yes', $wpdb->get_var( $wpdb->prepare( "SELECT autoload FROM $wpdb->options WHERE option_name = %s", "theme_mods_$current_theme_stylesheet" ) ), 'Theme mods autoload value not set to yes in database' );
 		$this->assertSame( 'no', $wpdb->get_var( $wpdb->prepare( "SELECT autoload FROM $wpdb->options WHERE option_name = %s", "theme_mods_$new_theme_stylesheet" ) ), 'Theme mods autoload value not set to no in database' );
 
-		// Make sure that autoloaded options are cached properly.
-		$autoloaded_options = wp_cache_get( 'alloptions', 'options' );
-		$this->assertArrayNotHasKey( "theme_mods_$new_theme_stylesheet", $autoloaded_options, "Option theme_mods_$new_theme_stylesheet not deleted from alloptions cache" );
-		$this->assertArrayHasKey( "theme_mods_$current_theme_stylesheet", $autoloaded_options, "Option theme_mods_$current_theme_stylesheet unexpectedly deleted from alloptions cache" );
-
-		// And that we haven't lost the mods.
+		// Basic assertion to make sure that we haven't lost the mods.
 		$this->assertSame( 'a-value', get_theme_mod( 'foo-bar-option' ) );
 	}
 }
