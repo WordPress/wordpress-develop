d23acc8c4eff344f130c61eefd2ce66a849ef712
Paul Bearne <pbearne@gmail.com>
2023-12-05 18:10:33 -0500

    fixed wp_suspend_cache_invalidation

--- tests/phpunit/tests/functions/wpSuspendCacheInvalidation.php
+++ tests/phpunit/tests/functions/wpSuspendCacheInvalidation.php
@@ -29,15 +29,18 @@ class Tests_Functions_wpSuspendCacheInvalidation extends WP_UnitTestCase{
 
 		$this->assertTrue( wp_suspend_cache_invalidation( 'false' ) );
 		$this->assertNotEmpty( $_wp_suspend_cache_invalidation );
+		$this->assertTrue( $_wp_suspend_cache_invalidation );
 
 		$this->assertNotEmpty( wp_suspend_cache_invalidation( 'not_empty' ) );
 		$this->assertNotEmpty( $_wp_suspend_cache_invalidation );
 
 		$this->assertNotEmpty( wp_suspend_cache_invalidation( '' ) );
 		$this->assertEmpty( $_wp_suspend_cache_invalidation );
+		$this->assertFalse( $_wp_suspend_cache_invalidation );
 
 
 		$this->assertEmpty( wp_suspend_cache_invalidation( 0 ) );
 		$this->assertEmpty( $_wp_suspend_cache_invalidation );
+		$this->assertFalse( $_wp_suspend_cache_invalidation );
 	}
 }
