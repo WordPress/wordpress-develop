9e7167a3d1b4b4d1e4525072b2f96779788fb92d
Dennis Snell <dennis.snell@automattic.com>
2023-12-20 07:06:13 -0600

    HTML API: Prevent out-of-bounds string access.
    Rebuild of 8905faf8fd390eb888a25a8f546a66e2feccd2c5 after #5725.
    
    Co-authored-by: Jon Surrell <sirreal@users.noreply.github.com>
    Co-authored-by: Bernie Reiter <bernhard-reiter@git.wordpress.org>

--- tests/phpunit/tests/html-api/wpHtmlTagProcessor.php
+++ tests/phpunit/tests/html-api/wpHtmlTagProcessor.php
@@ -2691,4 +2691,22 @@ HTML
 			),
 		);
 	}
+
+	/**
+	 * @covers WP_HTML_Tag_Processor::next_tag
+	 */
+	public function test_handles_malformed_taglike_open_short_html() {
+		$p      = new WP_HTML_Tag_Processor( '<' );
+		$result = $p->next_tag();
+		$this->assertFalse( $result, 'Did not handle "<" html properly.' );
+	}
+
+	/**
+	 * @covers WP_HTML_Tag_Processor::next_tag
+	 */
+	public function test_handles_malformed_taglike_close_short_html() {
+		$p      = new WP_HTML_Tag_Processor( '</ ' );
+		$result = $p->next_tag();
+		$this->assertFalse( $result, 'Did not handle "</ " html properly.' );
+	}
 }
