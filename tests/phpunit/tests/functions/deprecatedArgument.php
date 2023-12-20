30b7da6ccce9f3a6cc799834d34a5f1623f19f38
Paul Bearne <pbearne@gmail.com>
2023-12-20 15:57:01 -0500

    Update tests/phpunit/tests/functions/deprecatedArgument.php
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- tests/phpunit/tests/functions/deprecatedArgument.php
+++ tests/phpunit/tests/functions/deprecatedArgument.php
@@ -18,7 +18,7 @@ class Tests_Functions_deprecatedArgument extends WP_UnitTestCase {
 	public function set_up() {
 		parent::set_up();
 
-		// remove ethe spherical handling for _deprecated_argumentin the PHPUnint setup so we can test it
+		// Remove the spherical handling for _deprecated_argument in the PHPUnint setup so we can test it.
 		remove_action( 'deprecated_argument_run', array( $this, 'deprecated_function_run' ), 10, 3 );
 		remove_action( 'deprecated_argument_trigger_error', '__return_false' );
 	}
