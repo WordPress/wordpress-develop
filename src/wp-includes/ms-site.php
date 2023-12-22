8d2957671131a537a0ce94a253f5447cfec601d0
naveen <kmnaveen101@gmail.com>
2023-12-22 18:16:14 +0530

    skip foreign key checks when deleting site tables

--- src/wp-includes/ms-site.php
+++ src/wp-includes/ms-site.php
@@ -835,10 +835,12 @@ function wp_uninitialize_site( $site_id ) {
 	 */
 	$drop_tables = apply_filters( 'wpmu_drop_tables', $tables, $site->id );
 
+	$wpdb->query( 'SET foreign_key_checks = 0' );
 	foreach ( (array) $drop_tables as $table ) {
 		// Disable foreign key checks when deleting site.
-		$wpdb->query( "SET FOREIGN_KEY_CHECKS=0; DROP TABLE IF EXISTS `$table`; SET FOREIGN_KEY_CHECKS=1;" ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+		$wpdb->query( "DROP TABLE IF EXISTS `$table`;" ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
 	}
+	$wpdb->query( 'SET foreign_key_checks = 1' );
 
 	/**
 	 * Filters the upload base directory to delete when the site is deleted.
