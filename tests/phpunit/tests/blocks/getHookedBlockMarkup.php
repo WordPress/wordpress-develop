4ac28f91a12a5419ee071dcd88e608b1d0d3edfb
Bernie Reiter <ockham@raz.or.at>
2024-01-03 16:58:40 +0100

    Fix get_hooked_block_markup tests

--- tests/phpunit/tests/blocks/getHookedBlockMarkup.php
+++ tests/phpunit/tests/blocks/getHookedBlockMarkup.php
@@ -17,11 +17,15 @@ class Tests_Blocks_GetHookedBlockMarkup extends WP_UnitTestCase {
 	 * @covers ::get_hooked_block_markup
 	 */
 	public function test_get_hooked_block_markup_adds_metadata() {
+		$hooked_block = array(
+			'blockName' => 'tests/hooked-block',
+		);
+
 		$anchor_block = array(
 			'blockName' => 'tests/anchor-block',
 		);
 
-		$actual = get_hooked_block_markup( $anchor_block, 'tests/hooked-block' );
+		$actual = get_hooked_block_markup( $hooked_block, $anchor_block );
 		$this->assertSame( array( 'tests/hooked-block' ), $anchor_block['attrs']['metadata']['ignoredHookedBlocks'] );
 		$this->assertSame( '<!-- wp:tests/hooked-block /-->', $actual );
 	}
@@ -32,6 +36,10 @@ class Tests_Blocks_GetHookedBlockMarkup extends WP_UnitTestCase {
 	 * @covers ::get_hooked_block_markup
 	 */
 	public function test_get_hooked_block_markup_if_block_is_already_hooked() {
+		$hooked_block = array(
+			'blockName' => 'tests/hooked-block',
+		);
+
 		$anchor_block = array(
 			'blockName' => 'tests/anchor-block',
 			'attrs'     => array(
@@ -41,7 +49,7 @@ class Tests_Blocks_GetHookedBlockMarkup extends WP_UnitTestCase {
 			),
 		);
 
-		$actual = get_hooked_block_markup( $anchor_block, 'tests/hooked-block' );
+		$actual = get_hooked_block_markup( $hooked_block, $anchor_block );
 		$this->assertSame( array( 'tests/hooked-block' ), $anchor_block['attrs']['metadata']['ignoredHookedBlocks'] );
 		$this->assertSame( '', $actual );
 	}
@@ -52,6 +60,10 @@ class Tests_Blocks_GetHookedBlockMarkup extends WP_UnitTestCase {
 	 * @covers ::get_hooked_block_markup
 	 */
 	public function test_get_hooked_block_markup_adds_to_ignored_hooked_blocks() {
+		$other_hooked_block = array(
+			'blockName' => 'tests/other-hooked-block',
+		);
+
 		$anchor_block = array(
 			'blockName' => 'tests/anchor-block',
 			'attrs'     => array(
@@ -61,7 +73,7 @@ class Tests_Blocks_GetHookedBlockMarkup extends WP_UnitTestCase {
 			),
 		);
 
-		$actual = get_hooked_block_markup( $anchor_block, 'tests/other-hooked-block' );
+		$actual = get_hooked_block_markup( $other_hooked_block, $anchor_block );
 		$this->assertSame( array( 'tests/hooked-block', 'tests/other-hooked-block' ), $anchor_block['attrs']['metadata']['ignoredHookedBlocks'] );
 		$this->assertSame( '<!-- wp:tests/other-hooked-block /-->', $actual );
 	}
