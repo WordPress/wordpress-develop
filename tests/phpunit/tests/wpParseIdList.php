c82ff99245e50b8fc0541a5944e2eca6337e820d
Paul Bearne <pbearne@gmail.com>
2024-01-09 19:15:28 -0500

    Refactor variable name and extend tests in wpParseIdList.php
    The variable 'list' was renamed to 'input_list' for clarify and a new test case 'dup_id' was added. The new test case will further enhance test coverage by handling an additional scenario where there are duplicate identifiers in the input list.

--- tests/phpunit/tests/wpParseIdList.php
+++ tests/phpunit/tests/wpParseIdList.php
@@ -14,40 +14,47 @@ class Tests_functions_wpParseIdList extends WP_UnitTestCase {
 	 *
 	 * @dataProvider data_wp_parse_id_list
 	 */
-	public function test_wp_parse_id_list( $list, $expected ) {
+	public function test_wp_parse_id_list( $input_list, $expected ) {
 
-		$this->assertSameSets( $expected, wp_parse_id_list( $list ) );
+		$this->assertSameSets( $expected, wp_parse_id_list( $input_list ) );
 	}
 
+	/**
+	 * data for test_wp_parse_id_list
+	 *
+	 * @ticket 60218
+	 *
+	 * @return array[]
+	 */
 	public function data_wp_parse_id_list() {
 		return array(
 			'simple'             => array(
-				'list'     => array( '1', 2, 'string with spaces' ),
-				'expected' => array( 0, 1, 2 ),
+				'input_list' => array( '1', 2, 'string with spaces' ),
+				'expected'   => array( 0, 1, 2 ),
 			),
 			'simple_with_comma'  => array(
-				'list'     => '1,2,string with spaces',
-				'expected' => array( 0, 1, 2 ),
+				'input_list' => '1,2,string with spaces',
+				'expected'   => array( 0, 1, 2 ),
 			),
 			'array_with_spaces'  => array(
-				'list'     => array( '1 2 string with spaces' ),
-				'expected' => array( 1 ),
+				'input_list' => array( '1 2 string with spaces' ),
+				'expected'   => array( 1 ),
 			),
 			'simple_with_spaces' => array(
-				'list'     => '1 2 string with spaces',
-				'expected' => array( 0, 1, 2 ),
+				'input_list' => '1 2 string with spaces',
+				'expected'   => array( 0, 1, 2 ),
 			),
 			'array_html'         => array(
-				'list'     => array( '1', 2, 'string <strong>with</strong> <h1>HEADING</h1>' ),
-				'expected' => array( 0, 1, 2 ),
+				'input_list' => array( '1', 2, 'string <strong>with</strong> <h1>HEADING</h1>' ),
+				'expected'   => array( 0, 1, 2 ),
 			),
 			'simple_html_spaces' => array(
-				'list'     => '1 2 string <strong>with</strong> <h1>HEADING</h1>',
-				'expected' => array( 0, 1, 2 ),
+				'input_list' => '1 2 string <strong>with</strong> <h1>HEADING</h1>',
+				'expected'   => array( 0, 1, 2 ),
 			),
 			'dup_id'             => array(
-				'list'     => '1 1 string string',
-				'expected' => array( 0, 1 ),
+				'input_list' => '1 1 string string',
+				'expected'   => array( 0, 1 ),
 			),
 		);
 	}
