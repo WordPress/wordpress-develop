966fd5fcbd27f01151a674da7275c587349e14ea
Paul Bearne <pbearne@gmail.com>
2024-01-09 19:13:34 -0500

    Refactor variable names and add new tests in wpParseSlugList.php
    Updated the variable name 'list' to 'input_list' to get a more descriptive and precise meaning in wpParseSlugList.php. Also, a new test 'dup_id' is added to cover a scenario with duplicate identifiers in the input list. This will improve the comprehensiveness of the tests.

--- tests/phpunit/tests/functions/wpParseSlugList.php
+++ tests/phpunit/tests/functions/wpParseSlugList.php
@@ -14,36 +14,40 @@ class Tests_functions_wpParseSlugList extends WP_UnitTestCase {
 	 *
 	 * @dataProvider data_wp_parse_slug_list
 	 */
-	public function test_wp_parse_slug_list( $list, $expected ) {
+	public function test_wp_parse_slug_list( $input_list, $expected ) {
 
-		$this->assertSameSets( $expected, wp_parse_slug_list( $list ) );
+		$this->assertSameSets( $expected, wp_parse_slug_list( $input_list ) );
 	}
 
 	public function data_wp_parse_slug_list() {
 		return array(
 			'simple'             => array(
-				'list'     => array( '1', 2, 'string with spaces' ),
-				'expected' => array( '1', '2', 'string-with-spaces' ),
+				'input_list' => array( '1', 2, 'string with spaces' ),
+				'expected'   => array( '1', '2', 'string-with-spaces' ),
 			),
 			'simple_with_comma'  => array(
-				'list'     => '1,2,string with spaces',
-				'expected' => array( '1', '2', 'string', 'with', 'spaces' ),
+				'input_list' => '1,2,string with spaces',
+				'expected'   => array( '1', '2', 'string', 'with', 'spaces' ),
 			),
 			'array_with_spaces'  => array(
-				'list'     => array( '1 2 string with spaces' ),
-				'expected' => array( '1-2-string-with-spaces' ),
+				'input_list' => array( '1 2 string with spaces' ),
+				'expected'   => array( '1-2-string-with-spaces' ),
 			),
 			'simple_with_spaces' => array(
-				'list'     => '1 2 string with spaces',
-				'expected' => array( '1', '2', 'string', 'with', 'spaces' ),
+				'input_list' => '1 2 string with spaces',
+				'expected'   => array( '1', '2', 'string', 'with', 'spaces' ),
 			),
 			'array_html'         => array(
-				'list'     => array( '1', 2, 'string <strong>with</strong> <h1>HEADING</h1>' ),
-				'expected' => array( '1', '2', 'string-with-heading' ),
+				'input_list' => array( '1', 2, 'string <strong>with</strong> <h1>HEADING</h1>' ),
+				'expected'   => array( '1', '2', 'string-with-heading' ),
 			),
 			'simple_html_spaces' => array(
-				'list'     => '1 2 string <strong>with</strong> <h1>HEADING</h1>',
-				'expected' => array( '1', '2', 'string', 'with', 'heading' ),
+				'input_list' => '1 2 string <strong>with</strong> <h1>HEADING</h1>',
+				'expected'   => array( '1', '2', 'string', 'with', 'heading' ),
+			),
+			'dup_id'             => array(
+				'input_list' => '1 1 string string',
+				'expected'   => array( '1', 'string' ),
 			),
 		);
 	}
