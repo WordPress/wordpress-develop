b60446432b73bc73923a46dc66fc4be34dfa5869
Karthik Thayyil <thayyilkarthik@gmail.com>
2024-01-16 16:34:12 +0000

    revert back to trunk

--- src/wp-includes/blocks/post-terms.php
+++ src/wp-includes/blocks/post-terms.php
@@ -59,11 +59,9 @@ function render_block_core_post_terms( $attributes, $content, $block ) {
 }
 
 /**
- * Returns the available variations for the `core/post-terms` block.
- *
- * @return array The available variations for the block.
+ * Registers the `core/post-terms` block on the server.
  */
-function build_post_term_block_variations() {
+function register_block_core_post_terms() {
 	$taxonomies = get_taxonomies(
 		array(
 			'publicly_queryable' => true,
@@ -104,18 +102,12 @@ function build_post_term_block_variations() {
 			$custom_variations[] = $variation;
 		}
 	}
-	return array_merge( $built_ins, $custom_variations );
-}
 
-/**
- * Registers the `core/post-terms` block on the server.
- */
-function register_block_core_post_terms() {
 	register_block_type_from_metadata(
 		__DIR__ . '/post-terms',
 		array(
-			'render_callback'    => 'render_block_core_post_terms',
-			'variation_callback' => 'build_post_term_block_variations',
+			'render_callback' => 'render_block_core_post_terms',
+			'variations'      => array_merge( $built_ins, $custom_variations ),
 		)
 	);
 }
