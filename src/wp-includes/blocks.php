bd439be0bc53e9208e25431fb10f7b73171b7414
jrfnl <jrfnl@users.noreply.github.com>
2024-01-17 23:05:54 +0100

    QA: use strict in_array() calls
    .. to prevent type juggling causing incorrect results.

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -778,7 +778,7 @@ function get_hooked_block_markup( &$anchor_block, $hooked_block_type ) {
 		$anchor_block['attrs']['metadata']['ignoredHookedBlocks'] = array();
 	}
 
-	if ( in_array( $hooked_block_type, $anchor_block['attrs']['metadata']['ignoredHookedBlocks'] ) ) {
+	if ( in_array( $hooked_block_type, $anchor_block['attrs']['metadata']['ignoredHookedBlocks'], true ) ) {
 		return '';
 	}
 
