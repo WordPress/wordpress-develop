52d0b05b270731c809c6390f23c61f49d3143c37
Paul Bearne <pbearne@gmail.com>
2024-01-18 13:24:17 -0500

    Update src/wp-includes/option.php
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- src/wp-includes/option.php
+++ src/wp-includes/option.php
@@ -1163,10 +1163,11 @@ function delete_option( $option ) {
  * @param string           $option   The name of the option.
  * @param mixed            $value    The value of the option to check its autoload value.
  * @param bool|null $autoload The autoload value to check.
- *                                   Accepts 'yes'|true to enable or 'no'|false to disable, or
- *                                   'auto-yes', 'auto-no', or 'auto' for internal purposes.
- *                                   Any other autoload value will be forced to either `auto-yes`,
- *                                   `auto-no`, or `auto`.
+ *                                   Accepts 'on'|true to enable or 'off'|false to disable, or
+ *                                   'auto-on', 'auto-off', or 'auto' for internal purposes.
+ *                                   Any other autoload value will be forced to either 'auto-on',
+ *                                   'auto-off', or 'auto'.
+ *                                   'yes' and 'no' are supported for backward compatibility.
  * @return string Returns the original $autoload value if valid, or 'auto-no' if the size exceeds
  *                the maximum allowed size, or 'auto-yes' otherwise.
  */
