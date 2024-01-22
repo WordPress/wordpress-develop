4a651d4a9111c1fae98aea3aa475eebcb5a5e9d7
Rajinsharwar <rajinsharwar@gmail.com>
2024-01-21 20:42:51 -0500

    Clear the activation key if exists

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -110,6 +110,20 @@ function wp_signon( $credentials = array(), $secure_cookie = '' ) {
 	}
 
 	wp_set_auth_cookie( $user->ID, $credentials['remember'], $secure_cookie );
+
+	// Flush `user_activation_key` if exists after successful login.
+	global $wpdb;
+	
+	if ( ! empty( $user->user_activation_key ) ) {
+		$wpdb->update(
+			$wpdb->users,
+			array(
+				'user_activation_key' => '',
+			),
+			array( 'ID' => $user->ID )
+		);
+	}
+
 	/**
 	 * Fires after the user has successfully logged in.
 	 *
