0f7283d3076f6987e4623e29f3ced4e5a7e3648f
Jon Surrell <sirreal@users.noreply.github.com>
2024-01-23 15:08:15 +0100

    Fix some "script" references

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -128,14 +128,14 @@ function get_block_asset_url( $path ) {
  * Finds a script module ID for the selected block metadata field. It detects
  * when a path to file was provided and finds a corresponding asset file
  * with details necessary to register the script module under an automatically
- * generated handle name. It returns unprocessed script handle otherwise.
+ * generated handle name. It returns unprocessed script module ID otherwise.
  *
  * @since 6.5.0
  *
  * @param array  $metadata   Block metadata.
  * @param string $field_name Field name to pick from metadata.
- * @param int    $index      Optional. Index of the script to register when multiple items passed.
- *                           Default 0.
+ * @param int    $index      Optional. Index of the script module ID to register when multiple
+ *                           items passed. Default 0.
  * @return string|false Script module ID or false on failure.
  */
 function register_block_script_module_id( $metadata, $field_name, $index = 0 ) {
