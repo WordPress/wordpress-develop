153514728b21b33138880ed320d95fe4959f6e33
Pascal Birchler <pascal.birchler@gmail.com>
2024-01-24 17:05:16 +0100

    Remove duplicate line
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- tests/phpunit/tests/rest-api/wpRestMenuItemsController.php
+++ tests/phpunit/tests/rest-api/wpRestMenuItemsController.php
@@ -156,7 +156,6 @@ class Tests_REST_WpRestMenuItemsController extends WP_Test_REST_Post_Type_Contro
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
 		$keys     = array_keys( $data['endpoints'][0]['args'] );
-		$keys     = array_keys( $data['endpoints'][0]['args'] );
 		$this->assertEqualSets( array( 'context', 'id' ), $keys );
 	}
 
