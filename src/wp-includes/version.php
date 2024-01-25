89c53f4db76bf4a0f541516f8032dac690af8a3a
Joe McGill <joemcgill@git.wordpress.org>
2024-01-25 20:23:20 +0000

    Post WordPress 6.4.3 RC1 version bump.
    git-svn-id: https://develop.svn.wordpress.org/branches/6.4@57360 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4.3-RC1-src';
+$wp_version = '6.4.3-alpha-57360-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
