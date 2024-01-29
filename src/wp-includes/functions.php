bcbff688401e1882ccfc41cbdb1319b1de629252
Andy Fragen <andy@thefragens.com>
2024-01-29 10:36:52 -0800

    revert notice and changes to other REST test

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -8736,13 +8736,7 @@ function is_wp_version_compatible( $required ) {
 	list( $version ) = explode( '-', $wp_version );
 
 	if ( is_string( $required ) && substr_count( $required, '.' ) > 1 && str_ends_with( $required, '.0' ) ) {
-		$passed   = $required;
-		$required = preg_replace( '/\.0/', '', $passed, 1 );
-		wp_trigger_error(
-			__FUNCTION__,
-			/* translators: s: version string sent to function */
-			sprintf( __( '`%1$s` Not a valid WordPress version string. `%2$s` is assumed.' ), $passed, $required )
-		);
+		$required = preg_replace( '/\.0/', '', $required, 1 );
 	}
 
 	return empty( $required ) || version_compare( $version, $required, '>=' );
