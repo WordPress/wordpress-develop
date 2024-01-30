65a7c09ba7bc8f22499dea3657c03ac026d7c8f1
Michal Czaplinski <mmczaplinski@gmail.com>
2024-01-30 17:18:57 +0000

    Test passing arguments to the source

--- tests/phpunit/tests/block-bindings/block-bindings.php
+++ tests/phpunit/tests/block-bindings/block-bindings.php
@@ -13,6 +13,11 @@
  */
 class WP_Block_Bindings_Test extends WP_UnitTestCase {
 
+	const SOURCE_NAME  = 'test/source';
+	const SOURCE_LABEL = array(
+		'label' => 'Test source',
+	);
+
 	/**
 	 * Set up before each test.
 	 *
@@ -29,35 +34,70 @@ class WP_Block_Bindings_Test extends WP_UnitTestCase {
 	}
 
 	/**
-	* Test if the block content is updated with the value returned by the source.
-	*
-	* @ticket 60282
-	*
-	* @covers register_block_bindings_source
-	*/
-	public function test_replace_html_for_paragraph_content() {
-		$source_name        = 'test/source';
-		$label              = 'Test Source';
+	 * Test if the block content is updated with the value returned by the source.
+	 *
+	 * @ticket 60282
+	 *
+	 * @covers register_block_bindings_source
+	 */
+	public function test_update_block_with_value_from_source() {
 		$get_value_callback = function () {
 			return 'test source value';
 		};
 
 		register_block_bindings_source(
-			$source_name,
+			self::SOURCE_NAME,
 			array(
-				'label'              => $label,
+				'label'              => self::SOURCE_LABEL,
 				'get_value_callback' => $get_value_callback,
 			)
 		);
 
 		$block_content = <<<HTML
-<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":{"name":"test/source"}}}}} --><p>This should not appear</p><!-- /wp:paragraph -->
+<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"test/source"}}}} --><p>This should not appear</p><!-- /wp:paragraph -->
 HTML;
 
 		$parsed_blocks = parse_blocks( $block_content );
 		$block         = new WP_Block( $parsed_blocks[0] );
 
 		$expected = '<p>test source value</p>';
+		$result   = $block->render();
+
+		// Check if the block content was updated correctly.
+		$this->assertEquals( $expected, $result, 'The block content should be updated with the value returned by the source.' );
+	}
+
+	/**
+	 * Test passing arguments to the source.
+	 *
+	 * @ticket 60282
+	 *
+	 * @covers register_block_bindings_source
+	 */
+	public function test_passing_arguments_to_source() {
+		$get_value_callback = function ( $source_args, $block_instance, $attribute_name ) {
+			$value = $source_args['key'];
+			return "The attribute name is '$attribute_name' and its binding has argument 'key' with value '$value'.";
+		};
+
+		register_block_bindings_source(
+			self::SOURCE_NAME,
+			array(
+				'label'              => self::SOURCE_LABEL,
+				'get_value_callback' => $get_value_callback,
+			)
+		);
+
+		$key = 'test';
+
+		$block_content = <<<HTML
+<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"test/source", "args": {"key": "$key"}}}}} --><p>This should not appear</p><!-- /wp:paragraph -->
+HTML;
+
+		$parsed_blocks = parse_blocks( $block_content );
+		$block         = new WP_Block( $parsed_blocks[0] );
+
+		$expected = "<p>The attribute name is 'content' and its binding has argument 'key' with value 'test'.</p>";
 		$result   = $block->render();
 
 		// Check if the block content was updated correctly.
