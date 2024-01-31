1226e21439fcb8433a8ecddc389a0a752b5a74fd
tellthemachines <isabel@tellthemachines.com>
2024-01-31 14:32:27 +1100

    Fix spacing

--- tests/phpunit/tests/theme/wpEnqueueBlockGlobalStyles.php
+++ tests/phpunit/tests/theme/wpEnqueueBlockGlobalStyles.php
@@ -11,58 +11,56 @@ require_once __DIR__ . '/base.php';
  */
 
 class Tests_Theme_WpEnqueueBlockGlobalStyles extends WP_Theme_UnitTestCase {
-            
-    /**
-    * Test blocks to unregister at cleanup.
-    *
-    * @var array
-    */
-    private $test_blocks = array();
 
-    public function set_up() {
-        parent::set_up();
-        remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    }
+	/**
+	* Test blocks to unregister at cleanup.
+	*
+	* @var array
+	*/
+	private $test_blocks = array();
 
-    public function tear_down() {
-        // Unregister test blocks.
-        if ( ! empty( $this->test_blocks ) ) {
-            foreach ( $this->test_blocks as $test_block ) {
-                unregister_block_type( $test_block );
-            }
-            $this->test_blocks = array();
-        }
+	public function set_up() {
+		parent::set_up();
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	}
 
-        parent::tear_down();
-    }
+	public function tear_down() {
+		// Unregister test blocks.
+		if ( ! empty( $this->test_blocks ) ) {
+			foreach ( $this->test_blocks as $test_block ) {
+				unregister_block_type( $test_block );
+			}
+			$this->test_blocks = array();
+		}
 
-    /**
-     * @ticket 56915
-     * @ticket 60280
-     */
-    public function test_third_party_blocks_inline_styles_get_registered_to_global_styles() {
-        $this->set_up_third_party_block();
+		parent::tear_down();
+	}
 
-
-        $this->assertNotContains(
-            '.wp-block-my-third-party-block{background-color: hotpink;}',
-            $this->get_global_styles_blocks(),
-            'Third party block inline style should not be registered before running wp_enqueue_block_global_styles()'
-        );
-
-        wp_enqueue_block_global_styles();
-
-        $this->assertContains(
-            '.wp-block-my-third-party-block{background-color: hotpink;}',
-            $this->get_global_styles_blocks(),
-            'Third party block inline style should be registered after running wp_enqueue_block_global_styles()'
-        );
-
-    }
-
-    /**
+	/**
 	 * @ticket 56915
-     * @ticket 60280
+	 * @ticket 60280
+	 */
+	public function test_third_party_blocks_inline_styles_get_registered_to_global_styles() {
+		$this->set_up_third_party_block();
+
+		$this->assertNotContains(
+			'.wp-block-my-third-party-block{background-color: hotpink;}',
+			$this->get_global_styles_blocks(),
+			'Third party block inline style should not be registered before running wp_enqueue_block_global_styles()'
+		);
+
+		wp_enqueue_block_global_styles();
+
+		$this->assertContains(
+			'.wp-block-my-third-party-block{background-color: hotpink;}',
+			$this->get_global_styles_blocks(),
+			'Third party block inline style should be registered after running wp_enqueue_block_global_styles()'
+		);
+	}
+
+	/**
+	 * @ticket 56915
+	 * @ticket 60280
 	 */
 	public function test_blocks_inline_styles_get_rendered() {
 		wp_enqueue_block_global_styles();
@@ -81,12 +79,12 @@ class Tests_Theme_WpEnqueueBlockGlobalStyles extends WP_Theme_UnitTestCase {
 		);
 	}
 
-    /**
+	/**
 	 * @ticket 57868
-     * @ticket 60280
+	 * @ticket 60280
 	 */
 	public function test_third_party_blocks_inline_styles_for_elements_get_rendered() {
-        wp_enqueue_block_global_styles();
+		wp_enqueue_block_global_styles();
 
 		$actual = get_echo( 'wp_print_styles' );
 
@@ -97,7 +95,7 @@ class Tests_Theme_WpEnqueueBlockGlobalStyles extends WP_Theme_UnitTestCase {
 	}
 
 
-    private function set_up_third_party_block() {
+	private function set_up_third_party_block() {
 		switch_theme( 'block-theme' );
 
 		$name     = 'my/third-party-block';
@@ -111,8 +109,8 @@ class Tests_Theme_WpEnqueueBlockGlobalStyles extends WP_Theme_UnitTestCase {
 		$this->test_blocks[] = $name;
 	}
 
-    private function get_global_styles_blocks() {
-        $actual = wp_styles()->get_data( 'global-styles-blocks', 'after' );
-        return is_array( $actual ) ? $actual : array();
-    }
+	private function get_global_styles_blocks() {
+		$actual = wp_styles()->get_data( 'global-styles-blocks', 'after' );
+		return is_array( $actual ) ? $actual : array();
+	}
 }
