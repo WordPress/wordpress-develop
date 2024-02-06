5d11ea25b623659fc8f7eab846dd23f0e2843a2c
Utsav Patel <utsav.patel1512@outlook.com>
2024-02-07 00:09:59 +0530

    Fix: spelling mistake in docblock

--- src/wp-includes/theme.php
+++ src/wp-includes/theme.php
@@ -2612,18 +2612,18 @@ function get_theme_starter_content() {
  * @since 5.3.0 The `html5` feature now also accepts 'script' and 'style'.
  * @since 5.3.0 Formalized the existing and already documented `...$args` parameter
  *              by adding it to the function signature.
- * @since 5.4.0 The `disable-custom-gradients` feature limites to default gradients or gradients added
+ * @since 5.4.0 The `disable-custom-gradients` feature limits to default gradients or gradients added
  *              through `editor-gradient-presets` theme support.
  * @since 5.5.0 The `core-block-patterns` feature was added and is enabled by default.
  * @since 5.5.0 The `custom-logo` feature now also accepts 'unlink-homepage-logo'.
  * @since 5.6.0 The `post-formats` feature warns if no array is passed as the second parameter.
  * @since 5.8.0 The `widgets-block-editor` feature enables the Widgets block editor.
- * @since 5.8.0 The `block-template-parts` feature allow to edit any reusable template part from site editor.
+ * @since 5.8.0 The `block-template-parts` feature allows to edit any reusable template part from site editor.
  * @since 6.0.0 The `html5` feature warns if no array is passed as the second parameter.
+ * @since 6.0.1 The `block-templates` feature indicates whether a theme uses block-based templates.
  * @since 6.1.0 The `disable-layout-styles` feature disables the default layout styles.
  * @since 6.1.0 The `link-color` feature allows to enable the link color setting.
  * @since 6.3.0 The `border` feature allows themes without theme.json to add border styles to blocks.
- * @since 6.4.0 The `block-templates` feature allow to add editable block templates to post or page.
  * @since 6.5.0 The `appearance-tools` feature enables a few design tools for blocks,
  *              see `WP_Theme_JSON::APPEARANCE_TOOLS_OPT_INS` for a complete list.
  *
