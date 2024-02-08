9976428f3459ac6f1a0971babf6f1404e3209cbf
Utsav Patel <utsav.patel1512@outlook.com>
2024-02-08 10:10:59 +0530

    Fix: version issue of link-color and block-template-parts

--- src/wp-includes/theme.php
+++ src/wp-includes/theme.php
@@ -2618,11 +2618,11 @@ function get_theme_starter_content() {
  * @since 5.5.0 The `custom-logo` feature now also accepts 'unlink-homepage-logo'.
  * @since 5.6.0 The `post-formats` feature warns if no array is passed as the second parameter.
  * @since 5.8.0 The `widgets-block-editor` feature enables the Widgets block editor.
- * @since 5.8.0 The `block-template-parts` feature allows to edit any reusable template part from site editor.
  * @since 5.8.0 The `block-templates` feature indicates whether a theme uses block-based templates.
  * @since 6.0.0 The `html5` feature warns if no array is passed as the second parameter.
+ * @since 6.1.0 The `block-template-parts` feature allows to edit any reusable template part from site editor.
  * @since 6.1.0 The `disable-layout-styles` feature disables the default layout styles.
- * @since 6.1.0 The `link-color` feature allows to enable the link color setting.
+ * @since 6.3.0 The `link-color` feature allows to enable the link color setting.
  * @since 6.3.0 The `border` feature allows themes without theme.json to add border styles to blocks.
  * @since 6.5.0 The `appearance-tools` feature enables a few design tools for blocks,
  *              see `WP_Theme_JSON::APPEARANCE_TOOLS_OPT_INS` for a complete list.
