26e43742b1080cf73af0553a2bf1af4b81467086
Dennis Snell <dennis.snell@automattic.com>
2024-02-07 19:08:54 -0700

    Generate memory ratio graph as well

--- domain-report.R
+++ domain-report.R
@@ -5,6 +5,9 @@ library(patchwork)
 data <- read.csv('./domain-report.csv')
 data$mbps.tags = ( data$bytes / 1e6 ) / ( data$tag.time.in.nanoseconds / 1e9 )
 data$mbps.html = ( data$bytes / 1e6 ) / ( data$time.in.nanoseconds / 1e9 )
+data$mem.ratio = data$html.mem / data$tags.mem
+data$ms.tags   = data$tag.time.in.nanoseconds / 1e6
+data$ms.html   = data$time.in.nanoseconds / 1e6
 
 plot <- ggplot(filter(data, tag.count < 10000, success == 'success'))
 
@@ -12,7 +15,7 @@ chart <- (
     ( plot
             + geom_histogram(bins=200, aes(ratio), alpha=0.7)
             + xlab('Ratio of runtime: HTML vs. Tags')
-            + scale_x_log10()
+            + scale_x_log10(limits=c(0.1, 100), breaks=c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100))
     ) +
     ( plot
             + geom_histogram(bins=200, aes(ms.tags))
@@ -46,8 +49,14 @@ chart <- (
     (plot
         + geom_histogram(bins=200, aes(mbps.html))
         + xlab('MBps HTML')
-        + scale_x_continuous(limits=c(0, 40), breaks=seq(0, 40, 5))
+#         + scale_x_continuous(limits=c(0, 40), breaks=seq(0, 40, 5))
+		+ scale_x_log10(limits=c(1, 40), breaks=seq(1, 40, 2))
     )
+) / (
+	ggplot(filter(data, tag.count < 10000, success == 'success'))
+		+ geom_histogram(bins=200, aes(mem.ratio))
+		+ xlab('Ratio of runtime memory use: HTML vs. Tags')
+		+ scale_x_continuous(limits=c(-5, 5))
 )
 
 ggsave('./domain-report-chart.png', plot=chart, width=20, height=10, units='in', dpi=300)
