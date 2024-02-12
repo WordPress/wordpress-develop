ff779226e9e9c5be9b4c0ec5a46d8c58f36d1a45
Adam Silverstein <adamjs@google.com>
2024-02-12 10:30:57 -0700

    Add whitespace

--- src/wp-includes/class-wp-image-editor-gd.php
+++ src/wp-includes/class-wp-image-editor-gd.php
@@ -503,6 +503,7 @@ class WP_Image_Editor_GD extends WP_Image_Editor {
 		if ( ! $filename ) {
 			$filename = $this->generate_filename( null, null, $extension );
 		}
+
 		if ( function_exists( 'imageinterlace' ) ) {
 			/** This filter is documented in wp-includes/class-wp-image-editor-imagick.php */
 			imageinterlace( $image, apply_filters( 'image_save_progressive', false, $mime_type ) );
