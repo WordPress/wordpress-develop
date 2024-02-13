3b103d83faf1cf2939085020183a58add6353b74
Jonny Harris <jonathan.harris@news.co.uk>
2024-02-13 14:53:03 +0000

    Set default wp_global_styles

--- src/wp-includes/rest-api/endpoints/class-wp-rest-global-styles-revisions-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-global-styles-revisions-controller.php
@@ -39,7 +39,7 @@ class WP_REST_Global_Styles_Revisions_Controller extends WP_REST_Revisions_Contr
 	 *
 	 * @param string $parent_post_type Post type of the parent.
 	 */
-	public function __construct( $parent_post_type ) {
+	public function __construct( $parent_post_type = 'wp_global_styles' ) {
 		parent::__construct( $parent_post_type );
 		$post_type_object  = get_post_type_object( $parent_post_type );
 		$parent_controller = $post_type_object->get_rest_controller();
