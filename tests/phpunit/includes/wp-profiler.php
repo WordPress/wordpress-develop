bb6190333235ea505a9420ed0dfec47e762a0b6e
Linkon Miyan <rudlinkon@gmail.com>
2024-02-17 22:52:48 +0600

    changed single asterisks to double asterisks on the wp-profiler.php for ticket #60513

--- tests/phpunit/includes/wp-profiler.php
+++ tests/phpunit/includes/wp-profiler.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * A simple manually-instrumented profiler for WordPress.
  *
  * This records basic execution time, and a summary of the actions and SQL queries run within each block.
