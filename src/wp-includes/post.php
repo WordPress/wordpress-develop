33d9ee0c0bff61f93036a7a7d4d0fd0cda4dd7e1
Joe Dolson <design@joedolson.com>
2024-02-18 11:20:15 -0600

    Revert "TEST: prevent mime type icon fetching from cache"
    This reverts commit 6b3ccb2c4d38082f5ca407750bbe555b2f7a29fe.

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -6807,6 +6807,10 @@ function wp_attachment_is_image( $post = null ) {
  * @return string|false Icon, false otherwise.
  */
 function wp_mime_type_icon( $mime = 0 ) {
+	if ( ! is_numeric( $mime ) ) {
+		$icon = wp_cache_get( "mime_type_icon_$mime" );
+	}
+
 	$post_id = 0;
 	if ( empty( $icon ) ) {
 		$post_mimes = array();
@@ -6832,7 +6836,7 @@ function wp_mime_type_icon( $mime = 0 ) {
 			$post_mimes[] = $mime;
 		}
 
-		$icon_files = false;
+		$icon_files = wp_cache_get( 'icon_files' );
 
 		if ( ! is_array( $icon_files ) ) {
 			/**
