395ccbf923c8997d47523ee6d68c19e2abd62b3d
Joe McGill <801097+joemcgill@users.noreply.github.com>
2024-02-21 12:56:03 -0600

    Add `@since` annotation
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- src/wp-includes/template.php
+++ src/wp-includes/template.php
@@ -682,6 +682,8 @@ function get_attachment_template() {
 /**
  * Set up the globals used for template loading.
  *
+ * @since 6.5.0
+ *
  * @global string $wp_stylesheet_path Path to current theme's stylesheet directory.
  * @global string $wp_template_path   Path to current theme's template directory.
  */
