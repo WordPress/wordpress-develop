9b68ef50fed31830067f10347e354a618d9d243d
Aaron Jorbin <aaronjorbin@users.noreply.github.com>
2024-02-22 11:51:00 -0600

    use prettier to format CSS and JS
    npx prettier@npm:wp-prettier@latest --write src/js/_enqueues/admin/site-icon.js
    
    npx prettier@npm:wp-prettier@latest --write src/wp-admin/css/site-icon.css

--- src/js/_enqueues/admin/site-icon.js
+++ src/js/_enqueues/admin/site-icon.js
@@ -7,7 +7,7 @@
 
 /* global jQuery, wp */
 
-( function( $ ) {
+( function ( $ ) {
 	var $chooseButton = $( '#choose-from-library-button' ),
 		$iconPreview = $( '#site-icon-preview' ),
 		$browserIconPreview = $( '#browser-icon-preview' ),
@@ -32,7 +32,9 @@
 			ratio = xInit / yInit,
 			xImg = xInit,
 			yImg = yInit,
-			x1, y1, imgSelectOptions;
+			x1,
+			y1,
+			imgSelectOptions;
 
 		if ( realWidth / realHeight > ratio ) {
 			yInit = realHeight;
@@ -58,7 +60,7 @@
 			x1: x1,
 			y1: y1,
 			x2: xInit + x1,
-			y2: yInit + y1
+			y2: yInit + y1,
 		};
 
 		return imgSelectOptions;
@@ -69,51 +71,49 @@
 	 *
 	 * @since 6.5.0
 	 */
-	$chooseButton.on( 'click', function() {
+	$chooseButton.on( 'click', function () {
 		var $el = $( this );
 
 		// Create the media frame.
-		frame = wp.media({
+		frame = wp.media( {
 			button: {
-
 				// Set the text of the button.
 				text: $el.data( 'update' ),
 
 				// Don't close, we might need to crop.
-				close: false
+				close: false,
 			},
 			states: [
-				new wp.media.controller.Library({
+				new wp.media.controller.Library( {
 					title: $el.data( 'choose-text' ),
-					library: wp.media.query({ type: 'image' }),
+					library: wp.media.query( { type: 'image' } ),
 					date: false,
 					suggestedWidth: $el.data( 'size' ),
-					suggestedHeight: $el.data( 'size' )
-				}),
-				new wp.media.controller.SiteIconCropper({
+					suggestedHeight: $el.data( 'size' ),
+				} ),
+				new wp.media.controller.SiteIconCropper( {
 					control: {
 						params: {
 							width: $el.data( 'size' ),
-							height: $el.data( 'size' )
-						}
+							height: $el.data( 'size' ),
+						},
 					},
-					imgSelectOptions: calculateImageSelectOptions
-				})
-			]
-		});
+					imgSelectOptions: calculateImageSelectOptions,
+				} ),
+			],
+		} );
 
-		frame.on( 'cropped', function( attachment ) {
+		frame.on( 'cropped', function ( attachment ) {
 			$hiddenDataField.val( attachment.id );
 			switchToUpdate( attachment );
 			frame.close();
 
 			// Start over with a frame that is so fresh and so clean clean.
 			frame = null;
-		});
+		} );
 
 		// When an image is selected, run a callback.
-		frame.on( 'select', function() {
-
+		frame.on( 'select', function () {
 			// Grab the selected attachment.
 			var attachment = frame.state().get( 'selection' ).first();
 
@@ -129,10 +129,10 @@
 			} else {
 				frame.setState( 'cropper' );
 			}
-		});
+		} );
 
 		frame.open();
-	});
+	} );
 
 	/**
 	 * Update the UI when a site icon is selected.
@@ -158,48 +158,53 @@
 		} else {
 			i18nAppAlternativeString = wp.i18n.sprintf(
 				/* Translators: %s: The selected image filename. */
-				wp.i18n.__( 'App icon preview: The current image has no alternative text. The file name is: %s' ),
+				wp.i18n.__(
+					'App icon preview: The current image has no alternative text. The file name is: %s'
+				),
 				attributes.filename
 			);
 			i18nBrowserAlternativeString = wp.i18n.sprintf(
 				/* Translators: %s: The selected image filename. */
-				wp.i18n.__( 'Browser icon preview: The current image has no alternative text. The file name is: %s' ),
+				wp.i18n.__(
+					'Browser icon preview: The current image has no alternative text. The file name is: %s'
+				),
 				attributes.filename
 			);
 		}
 
 		// Set site-icon-img src and alternative text to app icon preview.
-		$appIconPreview.attr({
-			'src': attributes.url,
-			'alt': i18nAppAlternativeString
-		});
+		$appIconPreview.attr( {
+			src: attributes.url,
+			alt: i18nAppAlternativeString,
+		} );
 
 		// Set site-icon-img src and alternative text to browser preview.
-		$browserIconPreview.attr({
-			'src': attributes.url,
-			'alt': i18nBrowserAlternativeString
-		});
+		$browserIconPreview.attr( {
+			src: attributes.url,
+			alt: i18nBrowserAlternativeString,
+		} );
 
 		// Remove hidden class from icon preview div and remove button.
 		$iconPreview.removeClass( 'hidden' );
 		$removeButton.removeClass( 'hidden' );
 
 		// Set the global CSS variable for --site-icon-url to the selected image URL.
-		document.documentElement.style.setProperty( '--site-icon-url', 'url(' + attributes.url + ')' );
+		document.documentElement.style.setProperty(
+			'--site-icon-url',
+			'url(' + attributes.url + ')'
+		);
 
 		// If the choose button is not in the update state, swap the classes.
 		if ( $chooseButton.attr( 'data-state' ) !== '1' ) {
-			$chooseButton.attr({
-				'class': $chooseButton.attr( 'data-alt-classes' ),
+			$chooseButton.attr( {
+				class: $chooseButton.attr( 'data-alt-classes' ),
 				'data-alt-classes': $chooseButton.attr( 'class' ),
-				'data-state': '1'
-			});
+				'data-state': '1',
+			} );
 		}
 
 		// Swap the text of the choose button.
-		$chooseButton.text(
-			$chooseButton.attr( 'data-update-text' )
-		);
+		$chooseButton.text( $chooseButton.attr( 'data-update-text' ) );
 	}
 
 	/**
@@ -207,18 +212,18 @@
 	 *
 	 * @since 6.5.0
 	 */
-	$removeButton.on( 'click', function() {
+	$removeButton.on( 'click', function () {
 		$hiddenDataField.val( 'false' );
 		$( this ).toggleClass( 'hidden' );
 		$iconPreview.toggleClass( 'hidden' );
-		$browserIconPreview.attr({
-			'src': '',
-			'alt': ''
-		});
-		$appIconPreview.attr({
-			'src': '',
-			'alt': ''
-		});
+		$browserIconPreview.attr( {
+			src: '',
+			alt: '',
+		} );
+		$appIconPreview.attr( {
+			src: '',
+			alt: '',
+		} );
 
 		/**
 		 * Resets state to the button, for correct visual style and state.
@@ -226,21 +231,21 @@
 		 * Sets focus state to the button.
 		 */
 		$chooseButton
-			.attr({
-				'class': $chooseButton.attr( 'data-alt-classes' ),
+			.attr( {
+				class: $chooseButton.attr( 'data-alt-classes' ),
 				'data-alt-classes': $chooseButton.attr( 'class' ),
-				'data-state': ''
-			})
+				'data-state': '',
+			} )
 			.text( $chooseButton.attr( 'data-choose-text' ) )
 			.trigger( 'focus' );
-	});
+	} );
 
 	/**
 	 * Update the site icon preview when the site title changes.
 	 *
 	 * @since 6.5.0
 	 */
-	$( '#blogname' ).on( 'input', function() {
+	$( '#blogname' ).on( 'input', function () {
 		$( '#site-icon-preview-site-title' ).text( $( this ).val() );
-	});
-})( jQuery );
+	} );
+} )( jQuery );
