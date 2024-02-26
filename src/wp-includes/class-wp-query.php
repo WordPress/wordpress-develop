7a4658b8f123dc67d91f700c4f0703ecda7adf08
LukaszJaro <l_jarqs@hotmail.com>
2024-02-26 15:47:35 -0500

    updated to multiline strings with no spaces

--- src/wp-includes/class-wp-query.php
+++ src/wp-includes/class-wp-query.php
@@ -3104,7 +3104,13 @@ class WP_Query {
 			$found_rows = 'SQL_CALC_FOUND_ROWS';
 		}
 
-		$old_request = "SELECT $found_rows $distinct $fields FROM {$wpdb->posts} $join WHERE 1=1 $where $groupby $orderby $limits";
+		$old_request =
+			"SELECT $found_rows $distinct $fields
+			 FROM {$wpdb->posts} $join
+			 WHERE 1=1 $where
+			 $groupby
+			 $orderby
+			 $limits";
 
 		$this->request = $old_request;
 
@@ -3300,8 +3306,13 @@ class WP_Query {
 			if ( $split_the_query ) {
 				// First get the IDs and then fill in the objects.
 
-				$this->request = "SELECT $found_rows $distinct {$wpdb->posts}.ID FROM {$wpdb->posts} $join WHERE 1=1 $where $groupby $orderby $limits
-				";
+				$this->request =
+					"SELECT $found_rows $distinct {$wpdb->posts}.ID
+					 FROM {$wpdb->posts} $join
+					 WHERE 1=1 $where
+					 $groupby
+					 $orderby
+					 $limits";
 
 				/**
 				 * Filters the Post IDs SQL request before sending.
