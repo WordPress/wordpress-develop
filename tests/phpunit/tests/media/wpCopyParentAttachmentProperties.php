d3f3b6bfd61d0a7ac27eda60ab5e884c859a0dfc
Joe Dolson <design@joedolson.com>
2024-03-01 14:16:22 -0600

    Update previous tests

--- tests/phpunit/tests/media/wpCopyParentAttachmentProperties.php
+++ tests/phpunit/tests/media/wpCopyParentAttachmentProperties.php
@@ -16,6 +16,7 @@ class Tests_Media_wpCopyParentAttachmentProperties extends WP_UnitTestCase {
 
 	public function test_wp_copy_parent_attachment_properties() {
 		$attachment = $this->factory->attachment->create_upload_object( DIR_TESTDATA . '/images/canola.jpg' );
+		$parent_url = get_post( $attachment )->guid;
 		// Add alternative text.
 		update_post_meta( $attachment, '_wp_attachment_image_alt', 'Alt text' );
 		// Add image description.
@@ -35,15 +36,16 @@ class Tests_Media_wpCopyParentAttachmentProperties extends WP_UnitTestCase {
 			100
 		);
 
-		$object = wp_copy_parent_attachment_properties( $cropped, $attachment );
+		$object  = wp_copy_parent_attachment_properties( $cropped, $attachment );
+		$cropped = str_replace( wp_basename( $parent_url ), 'cropped-canola.jpg', $parent_url );
 
-		$this->assertSame( $object['post_title'], 'cropped-canola.jpg' );
-		$this->assertSame( $object['context'], '' );
-		$this->assertSame( $object['post_mime_type'], 'image/jpeg' );
-		$this->assertSame( $object['post_content'], $cropped );
-		$this->assertSame( $object['guid'], $cropped );
-		$this->assertSame( $object['meta_input']['_wp_attachment_image_alt'], 'Alt text' );
-		$this->assertSame( $object['post_excerpt'], 'Image description' );
+		$this->assertSame( $object['post_title'], 'cropped-canola.jpg', 'Attachment title is not identical' );
+		$this->assertSame( $object['context'], '', 'Attachment context is not identical' );
+		$this->assertSame( $object['post_mime_type'], 'image/jpeg', 'Attachment mime type is not identical' );
+		$this->assertSame( $object['post_content'], $cropped, 'Attachment content is not identical' );
+		$this->assertSame( $object['guid'], $cropped, 'Attachment GUID is not identical' );
+		$this->assertSame( $object['meta_input']['_wp_attachment_image_alt'], 'Alt text', 'Attachment alt text is not identical' );
+		$this->assertSame( $object['post_excerpt'], 'Image description', 'Attachment description is not identical' );
 
 		unlink( $cropped );
 	}
