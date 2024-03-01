3151dcbc6e50b3493c8ce83cab30a8f4571c32b7
Paul Bearne <pbearne@gmail.com>
2024-03-01 17:51:16 -0500

    Adjust unit test for send_frame_options_header function
    The unit test for send_frame_options_header function is updated to manage PHP Warnings and check response headers. setUp and tearDown methods are added to manage error levels, and the test_send_frame_options_header method now checks the 'X-Frame-Options' in the response headers. Also, this update necessitated the inclusion of xdebug as part of the test suite.

--- tests/phpunit/tests/functions/sendFrameOptionsHeader.php
+++ tests/phpunit/tests/functions/sendFrameOptionsHeader.php
@@ -2,21 +2,51 @@
 
 /**
  * Tests for the send_frame_options_header function.
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
  *
  * @group functions
+ * @group xdebug
  *
  * @covers ::send_frame_options_header
  */
 class Tests_Functions_SendFrameOptionsHeader extends WP_UnitTestCase {
+
+	public function set_up() {
+		parent::set_up();
+
+
+		// Suppress warnings from "Cannot modify header information - headers already sent by".
+		$this->_error_level = error_reporting();
+		error_reporting( $this->_error_level & ~E_WARNING );
+	}
+
+	/**
+	 * Tear down the test fixture.
+	 * Remove the wp_die() override, restore error reporting
+	 */
+	public function tear_down() {
+		error_reporting( $this->_error_level );
+		parent::tear_down();
+	}
 
 
 	/**
 	 * Just test for function
 	 *
 	 * @ticket 59851
+	 *
+	 * @requires function xdebug_get_headers
 	 */
 	public function test_send_frame_options_header() {
-
 		$this->assertTrue( function_exists('send_frame_options_header') );
+
+		ob_start();
+
+		// Check the header.
+		$headers = xdebug_get_headers();
+		ob_end_clean();
+
+		$this->assertStringContainsString('X-Frame-Options: SAMEORIGIN', $headers );
 	}
 }
