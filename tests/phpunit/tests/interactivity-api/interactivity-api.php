d61c50889628535c6c871b67ab6f62acea965487
Pascal Birchler <swissspidy@git.wordpress.org>
2024-03-04 15:37:53 +0000

    Interactivity API: Rename `data_wp_context()` to `wp_interactivity_data_wp_context()`.
    Increases clarity about where the function belongs to, bringing it in line with other related functions.
    
    After initially merging this change in [57742] and reverting it in [57743], this reintroduces it now that the Gutenberg packages have been updated accordingly in [57760].
    
    Props swissspidy, gziolo, cbravobernal, youknowriad, ankitmaru, westonruter, luisherranz, darerodz.
    Fixes #60575.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@57762 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/interactivity-api/interactivity-api.php
+++ tests/phpunit/tests/interactivity-api/interactivity-api.php
@@ -314,18 +314,18 @@ class Tests_Interactivity_API_Functions extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that data_wp_context function correctly converts different array
+	 * Tests that wp_interactivity_data_wp_context function correctly converts different array
 	 * structures to a JSON string.
 	 *
 	 * @ticket 60356
 	 *
-	 * @covers ::data_wp_context
+	 * @covers ::wp_interactivity_data_wp_context
 	 */
-	public function test_data_wp_context_with_different_arrays() {
-		$this->assertEquals( 'data-wp-context=\'{}\'', data_wp_context( array() ) );
+	public function test_wp_interactivity_data_wp_context_with_different_arrays() {
+		$this->assertEquals( 'data-wp-context=\'{}\'', wp_interactivity_data_wp_context( array() ) );
 		$this->assertEquals(
 			'data-wp-context=\'{"a":1,"b":"2","c":true}\'',
-			data_wp_context(
+			wp_interactivity_data_wp_context(
 				array(
 					'a' => 1,
 					'b' => '2',
@@ -335,27 +335,27 @@ class Tests_Interactivity_API_Functions extends WP_UnitTestCase {
 		);
 		$this->assertEquals(
 			'data-wp-context=\'{"a":[1,2]}\'',
-			data_wp_context( array( 'a' => array( 1, 2 ) ) )
+			wp_interactivity_data_wp_context( array( 'a' => array( 1, 2 ) ) )
 		);
 		$this->assertEquals(
 			'data-wp-context=\'[1,2]\'',
-			data_wp_context( array( 1, 2 ) )
+			wp_interactivity_data_wp_context( array( 1, 2 ) )
 		);
 	}
 
 	/**
-	 * Tests that data_wp_context function correctly converts different array
+	 * Tests that wp_interactivity_data_wp_context function correctly converts different array
 	 * structures to a JSON string and adds a namespace.
 	 *
 	 * @ticket 60356
 	 *
-	 * @covers ::data_wp_context
+	 * @covers ::wp_interactivity_data_wp_context
 	 */
-	public function test_data_wp_context_with_different_arrays_and_a_namespace() {
-		$this->assertEquals( 'data-wp-context=\'myPlugin::{}\'', data_wp_context( array(), 'myPlugin' ) );
+	public function test_wp_interactivity_data_wp_context_with_different_arrays_and_a_namespace() {
+		$this->assertEquals( 'data-wp-context=\'myPlugin::{}\'', wp_interactivity_data_wp_context( array(), 'myPlugin' ) );
 		$this->assertEquals(
 			'data-wp-context=\'myPlugin::{"a":1,"b":"2","c":true}\'',
-			data_wp_context(
+			wp_interactivity_data_wp_context(
 				array(
 					'a' => 1,
 					'b' => '2',
@@ -366,28 +366,28 @@ class Tests_Interactivity_API_Functions extends WP_UnitTestCase {
 		);
 		$this->assertEquals(
 			'data-wp-context=\'myPlugin::{"a":[1,2]}\'',
-			data_wp_context( array( 'a' => array( 1, 2 ) ), 'myPlugin' )
+			wp_interactivity_data_wp_context( array( 'a' => array( 1, 2 ) ), 'myPlugin' )
 		);
 		$this->assertEquals(
 			'data-wp-context=\'myPlugin::[1,2]\'',
-			data_wp_context( array( 1, 2 ), 'myPlugin' )
+			wp_interactivity_data_wp_context( array( 1, 2 ), 'myPlugin' )
 		);
 	}
 
 	/**
-	 * Tests that data_wp_context function correctly applies the JSON encoding
+	 * Tests that wp_interactivity_data_wp_context function correctly applies the JSON encoding
 	 * flags. This ensures that characters like `<`, `>`, `'`, or `&` are
 	 * properly escaped in the JSON-encoded string to prevent potential XSS
 	 * attacks.
 	 *
 	 * @ticket 60356
 	 *
-	 * @covers ::data_wp_context
+	 * @covers ::wp_interactivity_data_wp_context
 	 */
-	public function test_data_wp_context_with_json_flags() {
-		$this->assertEquals( 'data-wp-context=\'{"tag":"\u003Cfoo\u003E"}\'', data_wp_context( array( 'tag' => '<foo>' ) ) );
-		$this->assertEquals( 'data-wp-context=\'{"apos":"\u0027bar\u0027"}\'', data_wp_context( array( 'apos' => "'bar'" ) ) );
-		$this->assertEquals( 'data-wp-context=\'{"quot":"\u0022baz\u0022"}\'', data_wp_context( array( 'quot' => '"baz"' ) ) );
-		$this->assertEquals( 'data-wp-context=\'{"amp":"T\u0026T"}\'', data_wp_context( array( 'amp' => 'T&T' ) ) );
+	public function test_wp_interactivity_data_wp_context_with_json_flags() {
+		$this->assertEquals( 'data-wp-context=\'{"tag":"\u003Cfoo\u003E"}\'', wp_interactivity_data_wp_context( array( 'tag' => '<foo>' ) ) );
+		$this->assertEquals( 'data-wp-context=\'{"apos":"\u0027bar\u0027"}\'', wp_interactivity_data_wp_context( array( 'apos' => "'bar'" ) ) );
+		$this->assertEquals( 'data-wp-context=\'{"quot":"\u0022baz\u0022"}\'', wp_interactivity_data_wp_context( array( 'quot' => '"baz"' ) ) );
+		$this->assertEquals( 'data-wp-context=\'{"amp":"T\u0026T"}\'', wp_interactivity_data_wp_context( array( 'amp' => 'T&T' ) ) );
 	}
 }
