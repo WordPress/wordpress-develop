9ac2ddc848418beafea401291be9b46b683e9235
Paul Bearne <pbearne@gmail.com>
2024-03-04 09:12:03 -0500

    Update sendFrameOptionsHeader.php
    remove not needed test

--- tests/phpunit/tests/functions/sendFrameOptionsHeader.php
+++ tests/phpunit/tests/functions/sendFrameOptionsHeader.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Tests for the send_frame_options_header function.
  * @runTestsInSeparateProcesses
@@ -20,8 +19,6 @@ class Tests_Functions_SendFrameOptionsHeader extends WP_UnitTestCase {
 	 * @requires function xdebug_get_headers
 	 */
 	public function test_send_frame_options_header() {
-		$this->assertTrue( function_exists( 'send_frame_options_header' ) );
-
 		ob_start();
 		send_frame_options_header();
 		// Check the header.
