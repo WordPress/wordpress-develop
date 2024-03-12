e655888b2abf3e1d7f9bf8c2265405f87b54877b
Matias Benedetto <matias.benedetto@gmail.com>
2024-03-12 11:01:43 -0300

    do not suppres errors

--- src/wp-includes/fonts.php
+++ src/wp-includes/fonts.php
@@ -120,7 +120,7 @@ function wp_default_font_dir( $refresh_cache = false ) {
 		'error'   => false,
 	);
 
-	@wp_mkdir_p( $defaults['path'] );
+	wp_mkdir_p( $defaults['path'] );
 
 	if ( ! wp_is_writable( $defaults['path'] ) ) {
 		$defaults           = wp_upload_dir();
