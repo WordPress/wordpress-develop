424b648912a975cd5f024a50bb053f3b327785f4
alessandro stoppato <alexrah@gmail.com>
2024-03-14 16:51:24 +0100

    tests/phpunit/tests/admin/includesFile.php: fix PHPCS

--- tests/phpunit/tests/admin/includesFile.php
+++ tests/phpunit/tests/admin/includesFile.php
@@ -29,9 +29,9 @@ class Tests_Admin_IncludesFile extends WP_UnitTestCase {
 		$_SERVER['SCRIPT_FILENAME'] = 'S:/home/wordpress/trunk/wp/wp-admin/options-permalink.php';
 		$this->assertSame( 'S:/home/wordpress/trunk/', get_home_path() );
 
-		add_filter('get_home_path',array($this,'filter_get_home_path'));
-		$this->assertSame( 'D:/Users/foo/public_html/trunk',get_home_path());
-		remove_filter('get_home_path',array($this,'filter_get_home_path'));
+		add_filter( 'get_home_path', array( $this, 'filter_get_home_path' ) );
+		$this->assertSame( 'D:/Users/foo/public_html/trunk', get_home_path() );
+		remove_filter( 'get_home_path', array( $this, 'filter_get_home_path' ) );
 
 		update_option( 'home', $home );
 		update_option( 'siteurl', $siteurl );
