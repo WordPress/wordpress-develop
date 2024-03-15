9ddf515910478b1e805d1f7e3c3446d4c63e2aa0
colinduwe <colinduwe@gmail.com>
2024-03-15 09:08:55 -0700

    add support for __experimentalSkiSerialization to the shadow attribute

--- src/wp-includes/block-supports/shadow.php
+++ src/wp-includes/block-supports/shadow.php
@@ -52,7 +52,10 @@ function wp_register_shadow_support( $block_type ) {
 function wp_apply_shadow_support( $block_type, $block_attributes ) {
 	$has_shadow_support = block_has_support( $block_type, 'shadow', false );
 
-	if ( ! $has_shadow_support ) {
+	if (
+		! $has_shadow_support ||
+		wp_should_skip_block_supports_serialization( $block_type, 'shadow' )
+	) {
 		return array();
 	}
 
