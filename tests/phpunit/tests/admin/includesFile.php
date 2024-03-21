5015b512ded1b5367cc246869393bf545ec72f98
Carolina Nymark <hi@themesbycarolina.com>
2024-03-21 07:12:16 +0100

    Test/Admin/IncludesFile: Split off the test for get_home_path to a separate test class
    Split the test for `get_home_path()` from `IncludesFile` into a separate class, to ensure that the test classes only tests one function.
    
    Moves `@covers` from the function to the test class level.

--- tests/phpunit/tests/admin/includesFile.php
+++ tests/phpunit/tests/admin/includesFile.php
@@ -7,34 +7,6 @@
 class Tests_Admin_IncludesFile extends WP_UnitTestCase {
 
 	/**
-	 * @ticket 20449
-	 *
-	 * @covers ::get_home_path
-	 */
-	public function test_get_home_path() {
-		$home    = get_option( 'home' );
-		$siteurl = get_option( 'siteurl' );
-		$sfn     = $_SERVER['SCRIPT_FILENAME'];
-		$this->assertSame( str_replace( '\\', '/', ABSPATH ), get_home_path() );
-
-		update_option( 'home', 'http://localhost' );
-		update_option( 'siteurl', 'http://localhost/wp' );
-
-		$_SERVER['SCRIPT_FILENAME'] = 'D:\root\vhosts\site\httpdocs\wp\wp-admin\options-permalink.php';
-		$this->assertSame( 'D:/root/vhosts/site/httpdocs/', get_home_path() );
-
-		$_SERVER['SCRIPT_FILENAME'] = '/Users/foo/public_html/trunk/wp/wp-admin/options-permalink.php';
-		$this->assertSame( '/Users/foo/public_html/trunk/', get_home_path() );
-
-		$_SERVER['SCRIPT_FILENAME'] = 'S:/home/wordpress/trunk/wp/wp-admin/options-permalink.php';
-		$this->assertSame( 'S:/home/wordpress/trunk/', get_home_path() );
-
-		update_option( 'home', $home );
-		update_option( 'siteurl', $siteurl );
-		$_SERVER['SCRIPT_FILENAME'] = $sfn;
-	}
-
-	/**
 	 * @ticket 43329
 	 *
 	 * @covers ::download_url
