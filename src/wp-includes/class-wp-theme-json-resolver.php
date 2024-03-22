f4fa53972ce22a272361cdadde68b15d670bd569
Alex Lende <alex+github.com@lende.xyz>
2024-03-22 09:14:45 -0500

    Allow classic themes to opt-in to default shadow presets with appearance-tools

--- src/wp-includes/class-wp-theme-json-resolver.php
+++ src/wp-includes/class-wp-theme-json-resolver.php
@@ -337,6 +337,7 @@ class WP_Theme_JSON_Resolver {
 			// Allow themes to enable appearance tools via theme_support.
 			if ( current_theme_supports( 'appearance-tools' ) ) {
 				$theme_support_data['settings']['appearanceTools'] = true;
+				$theme_support_data['settings']['shadow']['defaultPresets'] = true;
 			}
 		}
 		$with_theme_supports = new WP_Theme_JSON( $theme_support_data );
