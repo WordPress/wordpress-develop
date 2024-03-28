df3483d36a88776cd0b1122ff78d01539b34f244
Bernie Reiter <ockham@raz.or.at>
2024-03-28 12:51:45 +0100

    Bump since to 6.5.1

--- src/wp-includes/block-template-utils.php
+++ src/wp-includes/block-template-utils.php
@@ -731,7 +731,7 @@ function _wp_build_title_and_description_for_taxonomy_block_template( $taxonomy,
  * It is self-sufficient in that it only uses information passed as arguments; it does not
  * query the database for additional information.
  *
- * @since 6.5.0
+ * @since 6.5.1
  * @access private
  *
  * @param WP_Post $post  Template post.
