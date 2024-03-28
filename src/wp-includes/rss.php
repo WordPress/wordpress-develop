3b9c1056740b421b816bfc3edc9f4277d281b3a3
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2024-03-28 01:53:45 -0500

    Simplify `set`, `get` and `check_cache` in `RSSCache` class

--- src/wp-includes/rss.php
+++ src/wp-includes/rss.php
@@ -503,58 +503,16 @@ class RSSCache {
 		self::__construct( $base, $age );
 	}
 
-/*=======================================================================*\
-	Function:	set
-	Purpose:	add an item to the cache, keyed on url
-	Input:		url from which the rss file was fetched
-	Output:		true on success
-\*=======================================================================*/
-	function set ($url, $rss) {
-		$cache_option = 'rss_' . $this->file_name( $url );
-
-		set_transient($cache_option, $rss, $this->MAX_AGE);
-
-		return $cache_option;
+	function set() {
+		return '';
 	}
 
-/*=======================================================================*\
-	Function:	get
-	Purpose:	fetch an item from the cache
-	Input:		url from which the rss file was fetched
-	Output:		cached object on HIT, false on MISS
-\*=======================================================================*/
-	function get ($url) {
-		$this->ERROR = "";
-		$cache_option = 'rss_' . $this->file_name( $url );
-
-		if ( ! $rss = get_transient( $cache_option ) ) {
-			$this->debug(
-				"Cache does not contain: $url (cache option: $cache_option)"
-			);
-			return 0;
-		}
-
-		return $rss;
+	function get() {
+		return 0;
 	}
 
-/*=======================================================================*\
-	Function:	check_cache
-	Purpose:	check a url for membership in the cache
-				and whether the object is older then MAX_AGE (ie. STALE)
-	Input:		url from which the rss file was fetched
-	Output:		cached object on HIT, false on MISS
-\*=======================================================================*/
-	function check_cache ( $url ) {
-		$this->ERROR = "";
-		$cache_option = 'rss_' . $this->file_name( $url );
-
-		if ( get_transient($cache_option) ) {
-			// object exists and is current
-				return 'HIT';
-		} else {
-			// object does not exist
-			return 'MISS';
-		}
+	function check_cache() {
+		return 'MISS';
 	}
 
 /*=======================================================================*\
