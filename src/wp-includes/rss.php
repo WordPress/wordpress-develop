e3e82f268f2420c408d384ffd5aabf9ccee05d12
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2024-03-28 01:20:52 -0500

    Simplify fetch_rss, parse_w3cdtf, wp_rss, get_rss

--- src/wp-includes/rss.php
+++ src/wp-includes/rss.php
@@ -395,143 +395,11 @@ class MagpieRSS {
 
 }
 
-if ( !function_exists('fetch_rss') ) :
-/**
- * Build Magpie object based on RSS from URL.
- *
- * @since 1.5.0
- * @package External
- * @subpackage MagpieRSS
- *
- * @param string $url URL to retrieve feed.
- * @return MagpieRSS|false MagpieRSS object on success, false on failure.
- */
-function fetch_rss ($url) {
-	// initialize constants
-	init();
-
-	if ( !isset($url) ) {
-		// error("fetch_rss called without a url");
+if ( ! function_exists( 'fetch_rss' ) ) {
+	function fetch_rss() {
 		return false;
 	}
-
-	// if cache is disabled
-	if ( !MAGPIE_CACHE_ON ) {
-		// fetch file, and parse it
-		$resp = _fetch_remote_file( $url );
-		if ( is_success( $resp->status ) ) {
-			return _response_to_rss( $resp );
-		}
-		else {
-			// error("Failed to fetch $url and cache is off");
-			return false;
-		}
-	}
-	// else cache is ON
-	else {
-		// Flow
-		// 1. check cache
-		// 2. if there is a hit, make sure it's fresh
-		// 3. if cached obj fails freshness check, fetch remote
-		// 4. if remote fails, return stale object, or error
-
-		$cache = new RSSCache( MAGPIE_CACHE_DIR, MAGPIE_CACHE_AGE );
-
-		if (MAGPIE_DEBUG and $cache->ERROR) {
-			debug($cache->ERROR, E_USER_WARNING);
-		}
-
-		$cache_status 	 = 0;		// response of check_cache
-		$request_headers = array(); // HTTP headers to send with fetch
-		$rss 			 = 0;		// parsed RSS object
-		$errormsg		 = 0;		// errors, if any
-
-		if (!$cache->ERROR) {
-			// return cache HIT, MISS, or STALE
-			$cache_status = $cache->check_cache( $url );
-		}
-
-		// if object cached, and cache is fresh, return cached obj
-		if ( $cache_status == 'HIT' ) {
-			$rss = $cache->get( $url );
-			if ( isset($rss) and $rss ) {
-				$rss->from_cache = 1;
-				if ( MAGPIE_DEBUG > 1) {
-				debug("MagpieRSS: Cache HIT", E_USER_NOTICE);
-			}
-				return $rss;
-			}
-		}
-
-		// else attempt a conditional get
-
-		// set up headers
-		if ( $cache_status == 'STALE' ) {
-			$rss = $cache->get( $url );
-			if ( isset($rss->etag) and $rss->last_modified ) {
-				$request_headers['If-None-Match'] = $rss->etag;
-				$request_headers['If-Last-Modified'] = $rss->last_modified;
-			}
-		}
-
-		$resp = _fetch_remote_file( $url, $request_headers );
-
-		if (isset($resp) and $resp) {
-			if ($resp->status == '304' ) {
-				// we have the most current copy
-				if ( MAGPIE_DEBUG > 1) {
-					debug("Got 304 for $url");
-				}
-				// reset cache on 304 (at minutillo insistent prodding)
-				$cache->set($url, $rss);
-				return $rss;
-			}
-			elseif ( is_success( $resp->status ) ) {
-				$rss = _response_to_rss( $resp );
-				if ( $rss ) {
-					if (MAGPIE_DEBUG > 1) {
-						debug("Fetch successful");
-					}
-					// add object to cache
-					$cache->set( $url, $rss );
-					return $rss;
-				}
-			}
-			else {
-				$errormsg = "Failed to fetch $url. ";
-				if ( $resp->error ) {
-					# compensate for Snoopy's annoying habit to tacking
-					# on '\n'
-					$http_error = substr($resp->error, 0, -2);
-					$errormsg .= "(HTTP Error: $http_error)";
-				}
-				else {
-					$errormsg .=  "(HTTP Response: " . $resp->response_code .')';
-				}
-			}
-		}
-		else {
-			$errormsg = "Unable to retrieve RSS file for unknown reasons.";
-		}
-
-		// else fetch failed
-
-		// attempt to return cached object
-		if ($rss) {
-			if ( MAGPIE_DEBUG ) {
-				debug("Returning STALE object for $url");
-			}
-			return $rss;
-		}
-
-		// else we totally failed
-		// error( $errormsg );
-
-		return false;
-
-	} // end if ( !MAGPIE_CACHE_ON ) {
-} // end fetch_rss()
-endif;
+}
 
 /**
  * Retrieve URL headers and content using WP HTTP Request API.
@@ -836,115 +704,20 @@ class RSSCache {
 	}
 }
 
-if ( !function_exists('parse_w3cdtf') ) :
-function parse_w3cdtf ( $date_str ) {
-
-	# regex to match W3C date/time formats
-	$pat = "/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})(:(\d{2}))?(?:([-+])(\d{2}):?(\d{2})|(Z))?/";
-
-	if ( preg_match( $pat, $date_str, $match ) ) {
-		list( $year, $month, $day, $hours, $minutes, $seconds) =
-			array( $match[1], $match[2], $match[3], $match[4], $match[5], $match[7]);
-
-		# calc epoch for current date assuming GMT
-		$epoch = gmmktime( $hours, $minutes, $seconds, $month, $day, $year);
-
-		$offset = 0;
-		if ( $match[11] == 'Z' ) {
-			# zulu time, aka GMT
-		}
-		else {
-			list( $tz_mod, $tz_hour, $tz_min ) =
-				array( $match[8], $match[9], $match[10]);
-
-			# zero out the variables
-			if ( ! $tz_hour ) { $tz_hour = 0; }
-			if ( ! $tz_min ) { $tz_min = 0; }
-
-			$offset_secs = (($tz_hour*60)+$tz_min)*60;
-
-			# is timezone ahead of GMT?  then subtract offset
-			#
-			if ( $tz_mod == '+' ) {
-				$offset_secs = $offset_secs * -1;
-			}
-
-			$offset = $offset_secs;
-		}
-		$epoch = $epoch + $offset;
-		return $epoch;
-	}
-	else {
+if ( ! function_exists( 'parse_w3cdtf' ) ) {
+	function parse_w3cdtf() {
 		return -1;
 	}
 }
-endif;
 
-if ( !function_exists('wp_rss') ) :
-/**
- * Display all RSS items in a HTML ordered list.
- *
- * @since 1.5.0
- * @package External
- * @subpackage MagpieRSS
- *
- * @param string $url URL of feed to display. Will not auto sense feed URL.
- * @param int $num_items Optional. Number of items to display, default is all.
- */
-function wp_rss( $url, $num_items = -1 ) {
-	if ( $rss = fetch_rss( $url ) ) {
-		echo '<ul>';
-
-		if ( $num_items !== -1 ) {
-			$rss->items = array_slice( $rss->items, 0, $num_items );
-		}
-
-		foreach ( (array) $rss->items as $item ) {
-			printf(
-				'<li><a href="%1$s" title="%2$s">%3$s</a></li>',
-				esc_url( $item['link'] ),
-				esc_attr( strip_tags( $item['description'] ) ),
-				esc_html( $item['title'] )
-			);
-		}
-
-		echo '</ul>';
-	} else {
-		_e( 'An error has occurred, which probably means the feed is down. Try again later.' );
+if ( ! function_exists( 'wp_rss' ) ) {
+	function wp_rss() {
+		echo '';
 	}
 }
-endif;
 
-if ( !function_exists('get_rss') ) :
-/**
- * Display RSS items in HTML list items.
- *
- * You have to specify which HTML list you want, either ordered or unordered
- * before using the function. You also have to specify how many items you wish
- * to display. You can't display all of them like you can with wp_rss()
- * function.
- *
- * @since 1.5.0
- * @package External
- * @subpackage MagpieRSS
- *
- * @param string $url URL of feed to display. Will not auto sense feed URL.
- * @param int $num_items Optional. Number of items to display, default is all.
- * @return bool False on failure.
- */
-function get_rss ($url, $num_items = 5) { // Like get posts, but for RSS
-	$rss = fetch_rss($url);
-	if ( $rss ) {
-		$rss->items = array_slice($rss->items, 0, $num_items);
-		foreach ( (array) $rss->items as $item ) {
-			echo "<li>\n";
-			echo "<a href='$item[link]' title='$item[description]'>";
-			echo esc_html($item['title']);
-			echo "</a><br />\n";
-			echo "</li>\n";
-		}
-	} else {
+if ( ! function_exists( 'get_rss' ) ) {
+	function get_rss() {
 		return false;
 	}
 }
-endif;
