bf5e15cd2807c14391c28449bd106587e5c7e175
Dennis Snell <dennis.snell@automattic.com>
2024-04-11 16:45:06 +0200

    Nest blocks and track stack of open blocks.

--- src/wp-includes/html-api/class-wp-unified-block-parser.php
+++ src/wp-includes/html-api/class-wp-unified-block-parser.php
@@ -54,15 +54,168 @@ class WP_Lazy_JSON_Object {
 	}
 }
 
+class WP_Lazy_Parsed_Block {
+	/**
+	 * The block's namespace.
+	 *
+	 * @var string
+	 */
+	public $namespace;
+
+	/**
+	 * The block's name.
+	 *
+	 * @var string
+	 */
+	public $block_name;
+
+	/**
+	 * Whether the block is void.
+	 *
+	 * @var bool
+	 */
+	public $is_void;
+
+	/**
+	 * The block's JSON attributes, if available, lazily evaluated.
+	 *
+	 * @var ?WP_Lazy_JSON_Object
+	 */
+	public $attributes;
+
+	/**
+	 * The block's inner blocks.
+	 *
+	 * @var WP_Lazy_Parsed_Block[].
+	 */
+	public $inner_blocks = array();
+
+	/**
+	 * Bookmark for block opener.
+	 *
+	 * @var string
+	 */
+	private $block_opener_at;
+
+	/**
+	 * Bookmark for block closer.
+	 *
+	 * @var string
+	 */
+	private $block_closer_at;
+
+	public function __construct( $namespace, $block_name, $attributes, $opener_at, $is_void ) {
+		$this->namespace       = $namespace;
+		$this->block_name      = $block_name;
+		$this->attributes      = $attributes;
+		$this->block_opener_at = $opener_at;
+		$this->is_void         = $is_void;
+		if ( $is_void ) {
+			$this->block_closer_at = $opener_at;
+		}
+	}
+
+	public function end_at( $bookmark_name ) {
+		$this->block_closer_at = $bookmark_name;
+	}
+}
+
+class WP_Parsed_Block_Comment {
+	/**
+	 * The block's namespace.
+	 *
+	 * @var string
+	 */
+	public $namespace;
+
+	/**
+	 * The block's name.
+	 *
+	 * @var string
+	 */
+	public $block_name;
+
+	/**
+	 * The block's attributes, if an opener.
+	 *
+	 * @var ?WP_Lazy_JSON_Object
+	 */
+	public $attributes;
+
+	/**
+	 * What kind of comment delimiter this is.
+	 *
+	 * @var string One of "opener" "closer" or "void".
+	 */
+	public $type;
+
+	public function __construct( $type, $namespace, $block_name, $attributes ) {
+		$this->type       = $type;
+		$this->namespace  = $namespace;
+		$this->block_name = $block_name;
+		if ( self::CLOSER !== $type ) {
+			$this->attributes = $attributes;
+		}
+	}
+
+	const OPENER = 'opener';
+
+	const CLOSER = 'closer';
+
+	const VOID = 'void';
+}
+
 class WP_Unified_Block_Parser {
 	/** @var WP_HTML_Processor */
 	private $processor;
 
-	/** @var array[] */
+	/** @var WP_Lazy_Parsed_Block[] */
 	private $blocks = array();
+
+	private $block_count = 0;
 
 	public function __construct( $html ) {
 		$this->processor = WP_HTML_Processor::create_fragment( $html );
+	}
+
+	/**
+	 * @param WP_Lazy_Parsed_Block $block
+	 */
+	private function open_block( $block ) {
+		if ( $block->is_void ) {
+			echo "\e[90mFound a \e[32mvoid\e[90m block of type \e[34m{$block->namespace}\e[90m/\e[34m{$block->block_name}\e[90m with ";
+			if ( isset( $block->attributes ) ) {
+				echo "\e[3;35m{$block->attributes->source()}\e[m\n";
+			} else {
+				echo "\e[3mno attributes\e[m\n";
+			}
+
+			return;
+		}
+
+		echo "\e[90mOpening a block of type \e[34m{$block->namespace}\e[90m/\e[34m{$block->block_name}\e[90m with ";
+		if ( isset( $block->attributes ) ) {
+			echo "\e[3;35m{$block->attributes->source()}\e[m\n";
+		} else {
+			echo "\e[3mno attributes\e[m\n";
+		}
+	}
+
+	/**
+	 * @param WP_Lazy_Parsed_Block $block
+	 */
+	private function close_block( $block ) {
+		echo "\e[90mClosing block of type \e[34m{$block->namespace}\e[90m/\e[34m{$block->block_name}\e[m\n";
+		if ( count( $block->inner_blocks ) > 0 ) {
+			echo "  \e[90mit contained inner blocks:\n";
+			foreach ( $block->inner_blocks as $inner_block ) {
+				echo "\e[90m    - \e[34m{$inner_block->namespace}\e[90m/\e[34m{$inner_block->block_name}";
+				if ( isset( $inner_block->inner_blocks ) && count( $inner_block->inner_blocks ) > 0 ) {
+					echo "\e[90m which itself contained \e[33m" . count( $inner_block->inner_blocks ) . "\e[90m inner blocks";
+				}
+				echo "\e[m\n";
+			}
+		}
 	}
 
 	public function step() {
@@ -71,18 +224,70 @@ class WP_Unified_Block_Parser {
 		}
 
 		if ( WP_HTML_Tag_Processor::COMMENT_AS_HTML_COMMENT === $this->processor->get_comment_type() ) {
-			$comment_text = $this->processor->get_modifiable_text();
-			echo "\e[90mFound a \e[32mcomment\e[90m: \e[34m{$comment_text}\e[m\n";
-			$block = self::parse_block_comment_text( $comment_text );
-			if ( false !== $block ) {
-				echo "  \e[90mand it was a block!\e[m\n";
-				$json = isset( $block[3] ) ? $block[3]->source() : '(no attributes)';
-				echo "  \e[90m  \e[31m{$block[0]} for \e[33m{$block[1]}\e[90m/\e[33m{$block[2]}\e[90m: \e[3;35m{$json}\e[m\n";
-				return $block;
+			$comment_text  = $this->processor->get_modifiable_text();
+			$block_comment = self::parse_block_comment_text( $comment_text );
+
+			if ( isset( $block_comment ) ) {
+				switch ( $block_comment->type ) {
+					case WP_Parsed_Block_Comment::OPENER:
+					case WP_Parsed_Block_Comment::VOID:
+						++$this->block_count;
+						$bookmark = "block-{$this->block_count}";
+						$this->processor->set_bookmark( $bookmark );
+
+						$block = new WP_Lazy_Parsed_Block(
+							$block_comment->namespace,
+							$block_comment->block_name,
+							$block_comment->attributes,
+							$bookmark,
+							WP_Parsed_Block_Comment::VOID === $block_comment->type
+						);
+
+						$open_block = end( $this->blocks );
+						if ( false !== $open_block ) {
+							$open_block->inner_blocks[] = $block;
+						}
+
+						$this->blocks[] = $block;
+						$this->open_block( $block );
+						break;
+
+					case WP_Parsed_Block_Comment::CLOSER:
+						// Ignore closers if there are no openers.
+						if ( 0 === count( $this->blocks ) ) {
+							break;
+						}
+
+						// Ignore also if it's not the associated closer for the most-recently opened block.
+						$opener = end( $this->blocks );
+						if ( $opener->namespace !== $block_comment->namespace || $opener->block_name !== $block_comment->block_name ) {
+							break;
+						}
+
+						++$this->block_count;
+						$bookmark = "block-{$this->block_count}";
+						$this->processor->set_bookmark( $bookmark );
+						$opener->end_at( $bookmark );
+
+						array_pop( $this->blocks );
+						$this->close_block( $opener );
+
+						break;
+				}
+				return $block_comment;
 			}
 		}
 	}
 
+	/**
+	 * Parses a comment's modifiable text to determine if it represents
+	 * a valid block comment delimiter, and if so, returns the block meta.
+	 *
+	 * @since {WP_VERSION}
+	 *
+	 * @param string $text Modifiable text for an HTML comment to parse.
+	 * @return WP_Parsed_Block_Comment|null Parsed block comment delimiter, if possible, otherwise null.
+	 */
 	public static function parse_block_comment_text( $text ) {
 		$at     = 0;
 		$length = strlen( $text );
@@ -96,7 +301,7 @@ class WP_Unified_Block_Parser {
 		 *         └────┘ 6 characters.
 		 */
 		if ( $length < 6 ) {
-			return false;
+			return null;
 		}
 
 		/*
@@ -109,7 +314,7 @@ class WP_Unified_Block_Parser {
 		 */
 		$at += strspn( $text, " \t\r\n\f", $at );
 		if ( $at >= $length ) {
-			return false;
+			return null;
 		}
 
 		/*
@@ -154,7 +359,7 @@ class WP_Unified_Block_Parser {
 		 *          └─┘
 		 */
 		if ( 0 !== substr_compare( $text, 'wp:', $at, 3 ) ) {
-			return false;
+			return null;
 		}
 
 		/*
@@ -167,7 +372,7 @@ class WP_Unified_Block_Parser {
 		 */
 		$name_length = strcspn( $text, " \t\r\n\f", $at );
 		if ( 0 === $name_length ) {
-			return false;
+			return null;
 		}
 
 		/*
@@ -181,7 +386,7 @@ class WP_Unified_Block_Parser {
 		 */
 		$slash_offset = strcspn( $text, '/', $at );
 		if ( 0 === $slash_offset || $name_length === $slash_offset ) {
-			return false;
+			return null;
 		}
 
 		$has_namespace = $slash_offset === $name_length;
@@ -212,7 +417,7 @@ class WP_Unified_Block_Parser {
 			1 !== preg_match( $name_pattern, $namespace ) ||
 			1 !== preg_match( $name_pattern, $block_name )
 		) {
-			return false;
+			return null;
 		}
 
 		/*
@@ -231,7 +436,7 @@ class WP_Unified_Block_Parser {
 
 		// If this ends the comment, then there are no attributes.
 		if ( $at >= $length ) {
-			return array( $delimiter_type, $namespace, $block_name, null );
+			return new WP_Parsed_Block_Comment( $delimiter_type, $namespace, $block_name, null );
 		}
 
 		/*
@@ -244,7 +449,7 @@ class WP_Unified_Block_Parser {
 		 *                       ^
 		 */
 		if ( '{' !== $text[ $at ] ) {
-			return false;
+			return null;
 		}
 
 		/*
@@ -252,14 +457,14 @@ class WP_Unified_Block_Parser {
 		 * This could appear at the end, or if it's a void tag, immediately before it.
 		 */
 		if ( ! str_contains( " \t\r\n\f", $text[ $length - ( $is_void ? 2 : 1 ) ] ) ) {
-			return false;
+			return null;
 		}
 
 		$json_region = substr( $text, $at, $length - $at - ( $is_void ? 1 : 0 ) );
 		$json_region = trim( $json_region, " \t\r\n\f" );
 
 		if ( '}' !== $json_region[ strlen( $json_region ) - 1 ] ) {
-			return false;
+			return null;
 		}
 
 		/*
@@ -268,6 +473,6 @@ class WP_Unified_Block_Parser {
 		 *       the parse for now it can defer the parsing costs until they are read.
 		 */
 		$attributes = $is_closer ? null : new WP_Lazy_JSON_Object( $json_region );
-		return array( $delimiter_type, $namespace, $block_name, $attributes );
+		return new WP_Parsed_Block_Comment( $delimiter_type, $namespace, $block_name, $attributes );
 	}
 }
