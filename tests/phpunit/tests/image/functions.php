0bbc7136ff2d344ccbfdee835c1acd888151ca79
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2024-04-16 23:58:04 +0000

    Tests: Use an image on WordPress.org CDN in external HTTP tests.
    Due to some changes on the WP.com side to compress the requested images on the fly, the exact image size in the response could be different between platforms.
    
    This commit aims to make the affected tests more reliable.
    
    Follow-up to [139/tests], [31258], [34568], [47142], [57903], [57904], [57924].
    
    Merges [57931] to the 4.1 branch.
    
    Props peterwilsoncc, jorbin.
    See #60865.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/4.1@58016 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/image/functions.php
+++ tests/phpunit/tests/image/functions.php
@@ -296,9 +296,9 @@ class Tests_Image_Functions extends WP_UnitTestCase {
 			$this->markTestSkipped( 'Tests_Image_Functions::test_wp_crop_image_url() requires openssl.' );
 		}
 
-		$file = wp_crop_image( 'https://asdftestblog1.files.wordpress.com/2008/04/canola.jpg',
+		$file = wp_crop_image( 'https://s.w.org/screenshots/3.9/dashboard.png',
 							  0, 0, 100, 100, 100, 100, false,
-							  DIR_TESTDATA . '/images/' . rand_str() . '.jpg' );
+							  DIR_TESTDATA . '/images/' . rand_str() . '.png' );
 		$this->assertNotInstanceOf( 'WP_Error', $file );
 		$this->assertFileExists( $file );
 		$image = wp_get_image_editor( $file );
@@ -320,7 +320,7 @@ class Tests_Image_Functions extends WP_UnitTestCase {
 			$this->markTestSkipped( 'Tests_Image_Functions::test_wp_crop_image_url_not_exist() requires openssl.' );
 		}
 
-		$file = wp_crop_image( 'https://asdftestblog1.files.wordpress.com/2008/04/canoladoesnotexist.jpg',
+		$file = wp_crop_image( 'https://wordpress.org/screenshots/3.9/canoladoesnotexist.jpg',
 							  0, 0, 100, 100, 100, 100 );
 		$this->assertInstanceOf( 'WP_Error', $file );
 	}
