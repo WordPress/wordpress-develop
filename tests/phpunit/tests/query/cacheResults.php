694b385559acad0dfa746102c0b28f377e2a2d64
Jonny Harris <jonathan.harris@news.co.uk>
2024-04-17 08:56:18 +0100

    Fix lint error

--- tests/phpunit/tests/query/cacheResults.php
+++ tests/phpunit/tests/query/cacheResults.php
@@ -265,9 +265,9 @@ class Test_Query_CacheResults extends WP_UnitTestCase {
 				'query_vars1' => array( 'post_type' => 'post' ),
 				'query_vars2' => array( 'post_type' => 'post' ),
 			),
-			'different order post type'                 => array(
+			'different order post type' => array(
 				'query_vars1' => array( 'post_type' => array( 'post', 'page' ) ),
-				'query_vars2' => array( 'post_type' =>  array( 'page', 'post' ) ),
+				'query_vars2' => array( 'post_type' => array( 'page', 'post' ) ),
 			),
 			'cache parameters'          => array(
 				'query_vars1' => array(
