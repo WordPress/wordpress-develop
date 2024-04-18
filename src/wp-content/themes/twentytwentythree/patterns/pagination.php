7c7176cb40f32e58bac70b6b148bb045dcf85540
dennis <dennis@yoast.com>
2024-04-18 14:33:59 +0200

    Twenty Twenty-Three: Extract pagination into pattern
    This adds translation support to the pagination-previous and pagination-next labels.

---
+++ src/wp-content/themes/twentytwentythree/patterns/pagination.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Pagination
+ * Slug: twentytwentythree/pagination
+ * Categories: featured
+ * Keywords: Pagination
+ */
+?>
+<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<!-- wp:query-pagination-previous {"label":"<?php esc_html_e( 'Newer posts', 'twentytwentythree' ); ?>"} /-->
+	<!-- wp:query-pagination-next {"label":"<?php esc_html_e( 'Older posts', 'twentytwentythree' ); ?>"} /-->
+<!-- /wp:query-pagination -->
