7f103f147463dfe48cb020cb10ea92267bf5c31e
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2024-04-18 15:57:13 +0300

    Update WP_Test_REST_TestCase::assertErrorResponse() to allow custom failure messages

--- tests/phpunit/includes/testcase-rest-api.php
+++ tests/phpunit/includes/testcase-rest-api.php
@@ -1,19 +1,30 @@
 <?php
 
 abstract class WP_Test_REST_TestCase extends WP_UnitTestCase {
-	protected function assertErrorResponse( $code, $response, $status = null ) {
+	/**
+	 * Asserts that the REST API response has specified error.
+	 *
+	 * @since 6.6.0 Added the `$message` parameter.
+	 *
+	 * @param string|int  						$code 		Expected error code.
+	 * @param true|WP_Error|WP_REST_Response   	$response  	REST API response.
+	 * @param int  								$status 	Optional. status code.
+	 * @param string 							$message 	Optional. Message to display when the assertion fails.
+	 *
+	 */
+	protected function assertErrorResponse( $code, $response, $status = null, $message = '' ) {
 
 		if ( $response instanceof WP_REST_Response ) {
 			$response = $response->as_error();
 		}
 
-		$this->assertWPError( $response );
-		$this->assertSame( $code, $response->get_error_code() );
+		$this->assertWPError( $response, $message );
+		$this->assertSame( $code, $response->get_error_code(), $message );
 
 		if ( null !== $status ) {
 			$data = $response->get_error_data();
-			$this->assertArrayHasKey( 'status', $data );
-			$this->assertSame( $status, $data['status'] );
+			$this->assertArrayHasKey( 'status', $data, $message );
+			$this->assertSame( $status, $data['status'], $message );
 		}
 	}
 }
