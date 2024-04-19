e7a66e5a4f26601f67a578badf4b73688f91ecf8
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2024-04-19 16:26:03 +0300

    Update WP_Test_REST_TestCase::assertErrorResponse() to allow custom failure messages Fix linting issues.

--- tests/phpunit/includes/testcase-rest-api.php
+++ tests/phpunit/includes/testcase-rest-api.php
@@ -6,10 +6,10 @@ abstract class WP_Test_REST_TestCase extends WP_UnitTestCase {
 	 *
 	 * @since 6.6.0 Added the `$message` parameter.
 	 *
-	 * @param string|int  						$code 		Expected error code.
-	 * @param true|WP_Error|WP_REST_Response   	$response  	REST API response.
-	 * @param int  								$status 	Optional. status code.
-	 * @param string 							$message 	Optional. Message to display when the assertion fails.
+	 * @param string|int                        $code       Expected error code.
+	 * @param true|WP_Error|WP_REST_Response    $response   REST API response.
+	 * @param int                               $status     Optional. status code.
+	 * @param string                            $message    Optional. Message to display when the assertion fails.
 	 *
 	 */
 	protected function assertErrorResponse( $code, $response, $status = null, $message = '' ) {
