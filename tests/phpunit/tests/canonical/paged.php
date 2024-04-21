3cd787e93266ea8cef4535c931eca3e551ad800e
rajinsharwar2 <rajinsharwar@gmail.com>
2024-04-20 20:00:14 -0400

    Adding as a new method

--- tests/phpunit/tests/canonical/paged.php
+++ tests/phpunit/tests/canonical/paged.php
@@ -26,4 +26,23 @@ class Tests_Canonical_Paged extends WP_Canonical_UnitTestCase {
 		// Non-existing page should redirect to the permalink.
 		$this->assertCanonical( $link . '4/', $link );
 	}
+
+	public function test_redirect_missing_front_page_pagination_canonical() {
+		
+		update_option( 'show_on_front', 'page' );
+		
+		$post_id = self::factory()->post->create(
+			array(
+				'post_title'   => 'front-page-1',
+				'post_type'    => 'page',
+				'post_content' => "Front Page 1\n<!--nextpage-->\nPage 2",
+			)
+		);
+
+		update_option( 'page_on_front', $post_id );
+
+		$link = parse_url( get_permalink( $post_id ), PHP_URL_PATH );
+
+		$this->assertCanonical( $link . '3/', $link );
+	}
 }
