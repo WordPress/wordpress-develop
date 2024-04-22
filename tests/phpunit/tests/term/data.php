b57121566c0d11a5b92cfda9f10f1f7faae826d1
Anton Vlasenko <vlasenko.anton@gmail.com>
2024-04-22 16:22:58 +0200

    Implement unit test for WP_Term->data and magic methods.

--- tests/phpunit/tests/term/data.php
+++ tests/phpunit/tests/term/data.php
@@ -9,16 +9,57 @@ class Tests_Term_Data extends WP_UnitTestCase {
 	 * @var WP_Term
 	 */
 	private static $term;
-	public static function wpSetUpBeforeClass() {
+
+	public function set_up() {
+		parent::set_up();
+
 		register_taxonomy( 'wptests_tax', 'post' );
 
-		static::$term = self::factory()->term->create(
+		static::$term = self::factory()->term->create_and_get(
 			array(
 				'taxonomy' => 'wptests_tax',
 			)
 		);
 	}
-	public function test_throws_error_on_accessing_dynamic_property() {
 
+	public function test_accessing_dynamic_property_should_work_correctly() {
+		$this->expect_notice( 'WP_Term::__get(): Getting the dynamic property "foo" on WP_Term is deprecated.' );
+		static::$term->foo;
+	}
+
+	public function test_checking_class_properties_should_work_correctly() {
+		$this->assertFalse( isset( static::$term->foo ) );
+		$this->assertFalse( isset( static::$term->data ) );
+		static::$term->data; // Activates __get().
+		$this->assertTrue( isset( static::$term->data ) );
+	}
+
+	public function test_setting_class_properties_should_work_correctly() {
+		static::$term->data = new stdClass();
+		$this->expect_notice( 'WP_Term::__set(): Setting the dynamic property "foo" on WP_Term is deprecated.' );
+		static::$term->foo = 'foo';
+	}
+
+	public function test_unsetting_class_properties_should_work_correctly() {
+		unset ( static::$term->data );
+		$this->expect_notice( 'WP_Term::__unset(): Unsetting the dynamic property "foo" on WP_Term is deprecated.' );
+		unset ( static::$term->foo );
+	}
+
+	protected function expect_notice( $message ) {
+		$this->expectException( Exception::class );
+		$this->expectExceptionMessage( $message );
+		set_error_handler(
+			static function ( $errno, $errstr ) {
+				restore_error_handler();
+				throw new Exception( $errstr, $errno );
+			},
+			E_USER_DEPRECATED
+		);
+	}
+
+	public function tear_down() {
+		unregister_taxonomy( 'wptests_tax' );
+		parent::tear_down();
 	}
 }
