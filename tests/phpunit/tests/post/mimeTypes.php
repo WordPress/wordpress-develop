0657e30dab84eb48a77c0c2cf7144dbf4c96b0dd
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2024-04-24 15:25:40 -0700

    Add unit tests for get_available_post_mime_types

---
+++ tests/phpunit/tests/post/mimeTypes.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Unit tests covering post mime types.
+ *
+ * @ticket 59195
+ *
+ * @group post
+ *
+ * @covers ::get_available_post_mime_types
+ */
+
+class Tests_Get_Available_Post_Mime_Types extends WP_UnitTestCase {
+	/**
+	 * Test get_available_post_mime_types.
+	 */
+	public function test_get_available_post_mime_types() {
+		$mime_types = get_available_post_mime_types();
+
+		$this->assertIsArray( $mime_types );
+		$this->assertNotEmpty( $mime_types );
+	}
+
+	/**
+	 * Test handling of nulls in get_available_post_mime_types.
+	 */
+	public function test_get_available_post_mime_types_with_null() {
+		// Add filter to inject null into the mime types array.
+		add_filter( 'pre_get_available_post_mime_types', array( $this, 'filter_add_null_to_post_mime_types' ) );
+
+		$mime_types = get_available_post_mime_types();
+		$this->assertIsArray( $mime_types );
+		$this->assertEqualsCanonicalizing( array( 'image/jpeg', 'image/png' ), $mime_types );
+
+		// Remove filter.
+		remove_filter( 'pre_get_available_post_mime_types', array( $this, 'filter_add_null_to_post_mime_types' ) );
+	}
+
+	/**
+	 * Filter to inject null into the mime types array.
+	 *
+	 * @param string $type Post type.
+	 * @return array
+	 */
+	public function filter_add_null_to_post_mime_types( $type ) {
+		return array( 'image/jpeg', null, 'image/png' );
+	}
+}
