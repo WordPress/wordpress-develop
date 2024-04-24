3a2ae1058b10bc9fb9a166bc810526bca8d4ebdb
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2024-04-24 16:44:56 -0700

    Add uploads and compare with expected types

--- tests/phpunit/tests/post/mimeTypes.php
+++ tests/phpunit/tests/post/mimeTypes.php
@@ -14,10 +14,24 @@ class Tests_Get_Available_Post_Mime_Types extends WP_UnitTestCase {
 	 * Test get_available_post_mime_types.
 	 */
 	public function test_get_available_post_mime_types() {
+		// Upload a JPEG image.
+		$filename = DIR_TESTDATA . '/images/test-image.jpg';
+		$contents = file_get_contents( $filename );
+		$upload   = wp_upload_bits( wp_basename( $filename ), null, $contents );
+		$this->assertEmpty( $upload['error'] );
+		$this->_make_attachment( $upload );
+
+		// Upload a PDF file.
+		$filename = DIR_TESTDATA . '/images/test-alpha.pdf';
+		$contents = file_get_contents( $filename );
+		$upload   = wp_upload_bits( wp_basename( $filename ), null, $contents );
+		$this->assertEmpty( $upload['error'] );
+		$this->_make_attachment( $upload );
+
 		$mime_types = get_available_post_mime_types();
 
 		$this->assertIsArray( $mime_types );
-		$this->assertNotEmpty( $mime_types );
+		$this->assertSame( array( 'image/jpeg', 'application/pdf' ), $mime_types );
 	}
 
 	/**
