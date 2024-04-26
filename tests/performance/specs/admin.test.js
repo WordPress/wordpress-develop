1dc70bd7ea7c539aae539c76e077522071ed67c8
Pascal Birchler <pascalb@google.com>
2024-04-26 16:51:30 +0200

    Move back some logic to JS

--- tests/performance/specs/admin.test.js
+++ tests/performance/specs/admin.test.js
@@ -12,31 +12,59 @@ const results = {
 	timeToFirstByte: [],
 };
 
+const locales = [ 'en_US', 'de_DE' ];
+
 test.describe( 'Admin', () => {
-	test.afterAll( async ( {}, testInfo ) => {
-		await testInfo.attach( 'results', {
-			body: JSON.stringify( results, null, 2 ),
-			contentType: 'application/json',
-		} );
-	} );
+	for ( const locale of locales ) {
+		test.describe( `Locale: ${ locale }`, () => {
+			test.beforeAll( async ( { requestUtils } ) => {
+				await requestUtils.activateTheme( 'twentytwentyone' );
+				await requestUtils.updateSiteSettings( {
+					language: 'en_US' === locale ? '' : locale,
+				} );
+			} );
 
-	const iterations = Number( process.env.TEST_RUNS );
-	for ( let i = 1; i <= iterations; i++ ) {
-		test( `Measure load time metrics (${ i } of ${ iterations })`, async ( {
-			admin,
-			metrics,
-		} ) => {
-			await admin.visitAdminPage( '/' );
+			test.afterAll( async ( { requestUtils }, testInfo ) => {
+				await testInfo.attach( 'results', {
+					body: JSON.stringify( results, null, 2 ),
+					contentType: 'application/json',
+				} );
 
-			const serverTiming = await metrics.getServerTiming();
+				await requestUtils.updateSiteSettings( {
+					language: '',
+				} );
 
-			for ( const [ key, value ] of Object.entries( serverTiming ) ) {
-				results[ camelCaseDashes( key ) ] ??= [];
-				results[ camelCaseDashes( key ) ].push( value );
+				results.timeToFirstByte = [];
+			} );
+
+			test.afterAll( async ( {}, testInfo ) => {
+				await testInfo.attach( 'results', {
+					body: JSON.stringify( results, null, 2 ),
+					contentType: 'application/json',
+				} );
+			} );
+
+			const iterations = Number( process.env.TEST_RUNS );
+			for ( let i = 1; i <= iterations; i++ ) {
+				test( `Measure load time metrics (${ i } of ${ iterations })`, async ( {
+					admin,
+					metrics,
+				} ) => {
+					await admin.visitAdminPage( '/' );
+
+					const serverTiming = await metrics.getServerTiming();
+
+					for ( const [ key, value ] of Object.entries(
+						serverTiming
+					) ) {
+						results[ camelCaseDashes( key ) ] ??= [];
+						results[ camelCaseDashes( key ) ].push( value );
+					}
+
+					const ttfb = await metrics.getTimeToFirstByte();
+					results.timeToFirstByte.push( ttfb );
+				} );
 			}
-
-			const ttfb = await metrics.getTimeToFirstByte();
-			results.timeToFirstByte.push( ttfb );
 		} );
 	}
 } );
--- tests/performance/specs/home.test.js
+++ tests/performance/specs/home.test.js
@@ -14,39 +14,66 @@ const results = {
 	lcpMinusTtfb: [],
 };
 
+const themes = [ 'twentytwentyone', 'twentytwentythree', 'twentytwentyfour' ];
+
+const locales = [ 'en_US', 'de_DE' ];
+
 test.describe( 'Front End', () => {
 	test.use( {
 		storageState: {}, // User will be logged out.
 	} );
 
-	test.afterAll( async ( { requestUtils }, testInfo ) => {
-		await testInfo.attach( 'results', {
-			body: JSON.stringify( results, null, 2 ),
-			contentType: 'application/json',
-		} );
-	} );
+	for ( const theme of themes ) {
+		for ( const locale of locales ) {
+			test.describe( `Theme: ${ theme }, Locale: ${ locale }`, () => {
+				test.beforeAll( async ( { requestUtils } ) => {
+					await requestUtils.activateTheme( theme );
+					await requestUtils.updateSiteSettings( {
+						language: 'en_US' === locale ? '' : locale,
+					} );
+				} );
 
-	const iterations = Number( process.env.TEST_RUNS );
-	for ( let i = 1; i <= iterations; i++ ) {
-		test( `Measure load time metrics (${ i } of ${ iterations })`, async ( {
-			page,
-			metrics,
-		} ) => {
-			await page.goto( '/' );
+				test.afterAll( async ( { requestUtils }, testInfo ) => {
+					await testInfo.attach( 'results', {
+						body: JSON.stringify( results, null, 2 ),
+						contentType: 'application/json',
+					} );
 
-			const serverTiming = await metrics.getServerTiming();
+					await requestUtils.updateSiteSettings( {
+						language: '',
+					} );
 
-			for ( const [ key, value ] of Object.entries( serverTiming ) ) {
-				results[ camelCaseDashes( key ) ] ??= [];
-				results[ camelCaseDashes( key ) ].push( value );
-			}
+					results.largestContentfulPaint = [];
+					results.timeToFirstByte = [];
+					results.lcpMinusTtfb = [];
+				} );
 
-			const ttfb = await metrics.getTimeToFirstByte();
-			const lcp = await metrics.getLargestContentfulPaint();
+				const iterations = Number( process.env.TEST_RUNS );
+				for ( let i = 1; i <= iterations; i++ ) {
+					test( `Measure load time metrics (${ i } of ${ iterations })`, async ( {
+						page,
+						metrics,
+					} ) => {
+						await page.goto( '/' );
 
-			results.largestContentfulPaint.push( lcp );
-			results.timeToFirstByte.push( ttfb );
-			results.lcpMinusTtfb.push( lcp - ttfb );
-		} );
+						const serverTiming = await metrics.getServerTiming();
+
+						for ( const [ key, value ] of Object.entries(
+							serverTiming
+						) ) {
+							results[ camelCaseDashes( key ) ] ??= [];
+							results[ camelCaseDashes( key ) ].push( value );
+						}
+
+						const ttfb = await metrics.getTimeToFirstByte();
+						const lcp = await metrics.getLargestContentfulPaint();
+
+						results.largestContentfulPaint.push( lcp );
+						results.timeToFirstByte.push( ttfb );
+						results.lcpMinusTtfb.push( lcp - ttfb );
+					} );
+				}
+			} );
+		}
 	}
 } );
