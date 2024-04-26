1caca8dcdb82bb54d508396994b391941c30eeae
Aslam Doctor <aslam.doctor@gmail.com>
2024-04-26 19:53:48 +0530

    Added unit test for localize script
    Trac ticket: 60862

---
+++ tests/phpunit/tests/dependencies/localizeScript.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @ticket 60862
+ * @group dependencies
+ * @group scripts
+ * @covers ::wp_localize_script
+ */
+class Test_Dependencies_LocalizeScript extends WP_UnitTestCase {
+	/**
+	 * Test if wp_localize_script() works.
+	 */
+	function test_wp_localize_script_works_before_enqueue_script() {
+		$this->assertTrue(
+			wp_localize_script(
+				'wp-util',
+				'salcodeExample',
+				[
+					'answerToTheUltimateQuestionOfLifeTheUniverseAndEverything' => 42,
+				]
+			)
+		);
+	}
+}
