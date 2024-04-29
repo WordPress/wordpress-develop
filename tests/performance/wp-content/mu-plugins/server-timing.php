9a23f401921438f77e5f837c1f1b31877757b91e
Pascal Birchler <pascalb@google.com>
2024-04-29 12:10:33 +0200

    Server-Timing in admin

--- tests/performance/wp-content/mu-plugins/server-timing.php
+++ tests/performance/wp-content/mu-plugins/server-timing.php
@@ -49,3 +49,45 @@ add_filter(
 	},
 	PHP_INT_MAX
 );
+
+add_action(
+	'admin_init',
+	static function () {
+		global $timestart, $wpdb;
+
+		ob_start();
+
+		add_action(
+			'shutdown',
+			static function () use ( $wpdb, $timestart ) {
+				$output = ob_get_clean();
+
+				$server_timing_values = array();
+
+				$server_timing_values['total'] = microtime( true ) - $timestart;
+
+				/*
+				 * While values passed via Server-Timing are intended to be durations,
+				 * any numeric value can actually be passed.
+				 * This is a nice little trick as it allows to easily get this information in JS.
+				 */
+				$server_timing_values['memory-usage']  = memory_get_usage();
+				$server_timing_values['db-queries']    = $wpdb->num_queries;
+				$server_timing_values['ext-obj-cache'] = wp_using_ext_object_cache() ? 1 : 0;
+
+				$header_values = array();
+				foreach ( $server_timing_values as $slug => $value ) {
+					if ( is_float( $value ) ) {
+						$value = round( $value * 1000.0, 2 );
+					}
+					$header_values[] = sprintf( 'wp-%1$s;dur=%2$s', $slug, $value );
+				}
+				header( 'Server-Timing: ' . implode( ', ', $header_values ) );
+
+				echo $output;
+			},
+			PHP_INT_MIN
+		);
+	},
+	PHP_INT_MAX
+);
