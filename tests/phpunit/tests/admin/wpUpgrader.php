0daa8a9bf2da4b520d323335b6b252142d5d33ae
Carolina Nymark <hi@themesbycarolina.com>
2024-05-02 10:35:12 +0200

    Test/Admin/WpUpgrader: Create a new TestCase
    Move the set up and tear down functions from `Tests_Admin_WpUpgrader` to a new abstract TestCase class, `Admin_WpUpgrader_TestCase` so that they can be re-used in multiple tests.

--- tests/phpunit/tests/admin/wpUpgrader.php
+++ tests/phpunit/tests/admin/wpUpgrader.php
@@ -1,79 +1,14 @@
 <?php
+
+require_once __DIR__ . '/Admin_WpUpgrader_TestCase.php';
+
 /**
  * Tests the `WP_Upgrader` class.
  *
  * @group admin
  * @group upgrade
  */
-class Tests_Admin_WpUpgrader extends WP_UnitTestCase {
-
-	/**
-	 * An instance of the WP_Upgrader class being tested.
-	 *
-	 * @var WP_Upgrader
-	 */
-	private static $instance;
-
-	/**
-	 * @var WP_Upgrader_Skin&PHPUnit\Framework\MockObject\MockObject
-	 */
-	private static $upgrader_skin_mock;
-
-	/**
-	 * Filesystem mock.
-	 *
-	 * @var WP_Filesystem_Base&PHPUnit\Framework\MockObject\MockObject
-	 */
-	private static $wp_filesystem_mock;
-
-	/**
-	 * A backup of the existing 'wp_filesystem' global.
-	 *
-	 * @var mixed|null
-	 */
-	private static $wp_filesystem_backup = null;
-
-	/**
-	 * Loads the class to be tested.
-	 */
-	public static function set_up_before_class() {
-		parent::set_up_before_class();
-
-		require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-		require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php';
-	}
-
-	/**
-	 * Sets up the class instance and mocks needed for each test.
-	 */
-	public function set_up() {
-		parent::set_up();
-
-		self::$upgrader_skin_mock = $this->getMockBuilder( 'WP_Upgrader_Skin' )->getMock();
-
-		self::$instance = new WP_Upgrader( self::$upgrader_skin_mock );
-
-		self::$wp_filesystem_mock = $this->getMockBuilder( 'WP_Filesystem_Base' )->getMock();
-
-		if ( array_key_exists( 'wp_filesystem', $GLOBALS ) ) {
-			self::$wp_filesystem_backup = $GLOBALS['wp_filesystem'];
-		}
-
-		$GLOBALS['wp_filesystem'] = self::$wp_filesystem_mock;
-	}
-
-	/**
-	 * Cleans up after each test.
-	 */
-	public function tear_down() {
-		if ( null !== self::$wp_filesystem_backup ) {
-			$GLOBALS['wp_filesystem'] = self::$wp_filesystem_backup;
-		} else {
-			unset( $GLOBALS['wp_filesystem'] );
-		}
-
-		parent::tear_down();
-	}
+class Tests_Admin_WpUpgrader extends Admin_WpUpgrader_TestCase {
 
 	/**
 	 * Tests that `WP_Upgrader::__construct()` creates a skin when one is not
