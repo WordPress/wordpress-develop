e308ed29e6f23da1631d2036f8cedd1238a6c793
Carolina Nymark <hi@themesbycarolina.com>
2024-05-02 10:52:03 +0200

    Move the test for `WP_Upgrader::__construct()` to a separate test class
    Move the test for `WP_Upgrader::__construct()` from `Tests_Admin_WpUpgrader` to a new test class, `Admin_WpUpgrader_Construct_Test`.
    Moves `covers` to the class level.
    This is done to make the test class compatible with PHPUnit 11.

--- tests/phpunit/tests/admin/wpUpgrader.php
+++ tests/phpunit/tests/admin/wpUpgrader.php
@@ -11,20 +11,6 @@ require_once __DIR__ . '/Admin_WpUpgrader_TestCase.php';
 class Tests_Admin_WpUpgrader extends Admin_WpUpgrader_TestCase {
 
 	/**
-	 * Tests that `WP_Upgrader::__construct()` creates a skin when one is not
-	 * passed to the constructor.
-	 *
-	 * @ticket 54245
-	 *
-	 * @covers WP_Upgrader::__construct
-	 */
-	public function test_constructor_should_create_skin_when_one_is_not_provided() {
-		$instance = new WP_Upgrader();
-
-		$this->assertInstanceOf( WP_Upgrader_Skin::class, $instance->skin );
-	}
-
-	/**
 	 * Tests that `WP_Upgrader::init()` calls `WP_Upgrader::set_upgrader()`.
 	 *
 	 * @ticket 54245
