3fb13c0276332bdaaf8d53cd9826ac996895a00a
ramon <ramonjd@gmail.com>
2024-05-03 13:44:57 +1000

    Synching https://github.com/WordPress/gutenberg/pull/42084 First commit, no tests

--- src/wp-includes/class-wp-duotone.php
+++ src/wp-includes/class-wp-duotone.php
@@ -814,7 +814,7 @@ class WP_Duotone {
 	 * @return string The CSS for global styles.
 	 */
 	private static function get_global_styles_presets( $sources ) {
-		$css = 'body{';
+		$css = WP_Theme_JSON::ROOT_CSS_PROPERTIES_SELECTOR . '{';
 		foreach ( $sources as $filter_id => $filter_data ) {
 			$slug              = $filter_data['slug'];
 			$colors            = $filter_data['colors'];
