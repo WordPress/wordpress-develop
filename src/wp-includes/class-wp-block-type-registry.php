4e56ab53fc6e6223e7b7f22660f289571946fc07
Tom Cafferkey <tjcafferkey@gmail.com>
2024-05-08 11:34:48 +0100

    Update variable name

--- src/wp-includes/class-wp-block-type-registry.php
+++ src/wp-includes/class-wp-block-type-registry.php
@@ -110,7 +110,7 @@ final class WP_Block_Type_Registry {
 				if ( ! isset( $this->$registered_block_type_aliases[ $alias_name ] ) ) {
 					$new_args                             = array_merge( $args, $alias_args );
 					$new_args['alias_of']                 = $name;
-					$alias_args['attributes']['metadata'] = array( 'alias' => $name );
+					$new_args['attributes']['metadata'] = array( 'alias' => $name );
 
 					// Register the alias block.
 					$this->registered_block_type_aliases[ $alias_name ] = new WP_Block_Type( $alias_name, array_merge( $args, $alias_args ) );
