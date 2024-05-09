b9cd850a5a9940700ac549e280c320ba21515c19
Jon Surrell <sirreal@users.noreply.github.com>
2024-05-09 13:07:47 +0200

    Add more details to comments

--- src/wp-includes/interactivity-api/class-wp-interactivity-api.php
+++ src/wp-includes/interactivity-api/class-wp-interactivity-api.php
@@ -169,27 +169,44 @@ final class WP_Interactivity_API {
 		if ( ! empty( $interactivity_data ) ) {
 
 			/*
-			 * This data will be printed as JSON inside a script tag:
+			 * This data will be printed as JSON inside a script tag like this:
 			 *   <script type="application/json"></script>
-			 * A script tag must be closed by a sequence beginning with `</`. We ensure that
-			 * `<` is escaped and `/` can remain unescaped. `</script>` will be
-			 * printed as `\u003C/script\u00E3`.
+			 *
+			 * A script tag must be closed by a sequence beginning with `</`. It's impossible to
+			 * close a script tag without using `<`. We ensure that `<` is escaped and `/` can
+			 * remain unescaped, so `</script>` will be printed as `\u003C/script\u00E3`.
 			 *
 			 *   - JSON_HEX_TAG: All < and > are converted to \u003C and \u003E.
 			 *   - JSON_UNESCAPED_SLASHES: Don't escape /.
 			 *
-			 * @see https://www.php.net/manual/en/json.constants.php
+			 * @see https://www.php.net/manual/en/json.constants.php for details on these constants.
+			 * @see https://html.spec.whatwg.org/#script-data-state for details on script
+			 *      tag parsing.
 			 */
 			$json_encode_flags = JSON_HEX_TAG | JSON_UNESCAPED_SLASHES;
 			if ( 'UTF-8' === get_option( 'blog_charset' ) ) {
 				/*
-				 * If the page uses the UTF-8 charset unicode does not need to be escaped.
+				 * If the page will use UTF-8 encoding, it's safe to print unescaped unicode in
+				 * JSON. Set the following flags:
 				 *
-				 *   - JSON_UNESCAPED_UNICODE: Encode multibyte Unicode characters literally
-				 *     (default is to escape as \uXXXX).
-				 *   - JSON_UNESCAPED_LINE_TERMINATORS: The line terminators are kept unescaped when
-				 *     JSON_UNESCAPED_UNICODE is supplied. It uses the same behaviour as it was
-				 *     before PHP 7.1 without this constant. Available as of PHP 7.1.0.
+				 * - JSON_UNESCAPED_UNICODE: Encode multibyte Unicode characters literally
+				 *   (default is to escape as \uXXXX).
+				 * - JSON_UNESCAPED_LINE_TERMINATORS: The line terminators are kept unescaped when
+				 *   JSON_UNESCAPED_UNICODE is supplied. It uses the same behaviour as it was
+				 *   before PHP 7.1 without this constant. Available as of PHP 7.1.0.
+				 *
+				 * The JSON specification does not specify a character encoding, RFC-8259
+				 * suggests that UTF-8 be used everywhere. It's risky to print unicode if the page
+				 * uses any other encoding.
+				 *
+				 * > JSON text exchanged between systems that are not part of a closed ecosystem
+				 * > MUST be encoded using UTF-8. Previous specifications of JSON have not required
+				 * > the use of UTF-8 when transmitting JSON text.  However, the vast majority of
+				 * > JSON- based software implementations have chosen to use the UTF-8 encoding,
+				 * > to the extent that it is the only encoding that achieves interoperability.
+				 *
+				 * @see https://www.rfc-editor.org/rfc/rfc8259.html
+				 *
 				 */
 				$json_encode_flags |= JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_LINE_TERMINATORS;
 			}
