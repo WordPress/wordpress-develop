bf71ec1ba25f1bf82e3ada98089d927cb33382d3
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2024-05-11 10:04:02 +1000

    CS Fix.

--- src/wp-includes/option.php
+++ src/wp-includes/option.php
@@ -1331,7 +1331,7 @@ function get_transient( $transient ) {
 			if ( ! isset( $alloptions[ $transient_option ] ) ) {
 				$transient_timeout = '_transient_timeout_' . $transient;
 				wp_prime_option_caches( array( $transient_option, $transient_timeout ) );
-				$timeout           = get_option( $transient_timeout );
+				$timeout = get_option( $transient_timeout );
 				if ( false !== $timeout && $timeout < time() ) {
 					delete_option( $transient_option );
 					delete_option( $transient_timeout );
