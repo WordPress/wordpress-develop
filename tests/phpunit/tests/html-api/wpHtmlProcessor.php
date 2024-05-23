f95bd051d431b4b01e012ccc6455679a7fd40df8
Dennis Snell <dennis.snell@automattic.com>
2024-05-23 15:08:15 -0700

    Revert test to use `next_tag( 'class_name' )`

--- tests/phpunit/tests/html-api/wpHtmlProcessor.php
+++ tests/phpunit/tests/html-api/wpHtmlProcessor.php
@@ -348,15 +348,8 @@ class Tests_HtmlApi_WpHtmlProcessor extends WP_UnitTestCase {
 	public function test_reports_proper_element_depth_in_body( $html_with_target_element, $depth_at_element ) {
 		$processor = WP_HTML_Processor::create_fragment( $html_with_target_element );
 
-		while ( $processor->next_tag() ) {
-			if ( $processor->has_class( 'target' ) ) {
-				break;
-			}
-		}
-
-		$this->assertSame(
-			'#tag',
-			$processor->get_token_type(),
+		$this->assertTrue(
+			$processor->next_tag( array( 'class_name' => 'target' ) ),
 			'Failed to find target element: check test data provider.'
 		);
 
