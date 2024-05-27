014cf5d7322c4a9ccf58d399f3cb8fe39d855732
paul bearne <paul>
2024-05-27 13:08:56 -0400

    Add transient validation methods and corresponding tests
    A new function named 'valid_transient' has been introduced to verify if a transient and its value are valid, also by checking if they have not expired. Sites with transient data can use the 'valid_site_transient' function to perform similar checks. Along with these functions, PHPUnit test cases have been added to ensure these functions perform as expected.

---
+++ tests/phpunit/tests/option/ValidTransient.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @group option
+ * @group transient
+ *
+ * @covers ::valid_transient
+ */
+class Tests_Option_ValidTransient extends WP_UnitTestCase {
+
+	/**
+	 * @ticket 37040
+	 */
+	public function test_valid_transient_with_expired_timeout() {
+		$transient_name = 'valid_transient_with_expired_timeout';
+		$transient_value = 'transient_value';
+
+		set_transient( $transient_name, $transient_value, 10 );
+
+		$this->assertTrue( valid_transient( $transient_name ) );
+
+		update_option( '_transient_timeout_' . $transient_name, time() - 10 );
+
+		$this->assertFalse( valid_transient( $transient_name ) );
+	}
+
+	/**
+	 * @ticket 37040
+	 */
+	public function test_valid_transient_with_no_timeout() {
+		$transient_name = 'valid_transient_with_no_timeout';
+		$transient_value = 'transient_value';
+
+		set_transient( $transient_name, $transient_value );
+		$this->assertTrue( valid_transient( $transient_name ) );
+	}
+
+	/**
+	 * @ticket 37040
+	 */
+	public function test_valid_transient_with_no_transient() {
+		$transient_name = 'valid_transient_with_no_transient';
+
+		$this->assertFalse( valid_transient( $transient_name ) );
+	}
+}
