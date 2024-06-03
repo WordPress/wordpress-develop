1e93db47e2e636ce50f6b907038933a690907452
Alex Lende <alex+github.com@lende.xyz>
2024-06-03 17:36:57 -0500

    Added 6.6.0 doc comments

--- src/wp-includes/block-editor.php
+++ src/wp-includes/block-editor.php
@@ -814,6 +814,7 @@ function get_block_editor_theme_styles() {
  * Returns the classic theme supports settings for block editor.
  *
  * @since 6.2.0
+ * @since 6.6.0 Add support for 'editor-spacing-sizes' theme support.
  *
  * @return array The classic theme supports settings.
  */
