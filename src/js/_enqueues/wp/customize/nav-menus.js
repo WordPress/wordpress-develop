70dad66b76926c53cc58f7ee7e0f0fbc1c848407
Rcreators <rishi@rcreators.com>
2024-06-03 18:52:48 +0530

    Updated Customizer Nav-menu js as per comment - 2

--- src/js/_enqueues/wp/customize/nav-menus.js
+++ src/js/_enqueues/wp/customize/nav-menus.js
@@ -1627,8 +1627,7 @@
 			if ( isPrimaryMenuItem ) {
 				primaryItems = $( '.menu-item-depth-0' ),
 				itemPosition = primaryItems.index( menuItem ) + 1,
-				totalMenuItems = primaryItems.length;
-
+				totalMenuItems = primaryItems.length,
 				// String together help text for primary menu items.
 				title = menus.menuFocus.replace( '%1$s', itemName ).replace( '%2$s', menuItemType ).replace( '%3$d', itemPosition ).replace( '%4$d', totalMenuItems );
 			} else {
@@ -1636,18 +1635,18 @@
 				parentItemId = parentItem.find( '.menu-item-data-db-id' ).val(),
 				parentItemName = parentItem.find( '.menu-item-title' ).text(),
 				subItems = $( '.menu-item .menu-item-data-parent-id[value="' + parentItemId + '"]' ),
-				totalSubItems = subItems.length;
+				totalSubItems = subItems.length,
 				itemPosition = $( subItems.parents('.menu-item').get().reverse() ).index( menuItem ) + 1;
 
 				// String together help text for sub menu items.
-				if( depth < 2 ) {
+				if ( depth < 2 ) {
 					title = menus.subMenuFocus.replace( '%1$s', itemName ).replace( '%2$s', menuItemType ).replace( '%3$d', itemPosition ).replace( '%4$d', totalSubItems ).replace( '%5$s', parentItemName );
 				} else {
 					title = menus.subMenuMoreDepthFocus.replace( '%1$s', itemName ).replace( '%2$s', menuItemType ).replace( '%3$d', itemPosition ).replace( '%4$d', totalSubItems ).replace( '%5$s', parentItemName ).replace( '%6$d', depth );
 				}
 			}
 
-			$this.find('.screen-reader-text').text( title );
+			$this.find( '.screen-reader-text' ).text( title );
 
 			// Mark this item's accessibility as refreshed.
 			$this.data( 'needs_accessibility_refresh', false );
