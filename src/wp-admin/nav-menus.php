3a260d767afafe26dee4555846dd572331d55f4e
Rcreators <rishi@rcreators.com>
2024-06-03 18:56:02 +0530

    Update nav-menus.php as per comments

--- src/wp-admin/nav-menus.php
+++ src/wp-admin/nav-menus.php
@@ -580,11 +580,11 @@ $nav_menus_l10n = array(
 	'under'                   => __( 'Under %s' ),
 	/* translators: %s: Previous item name. */
 	'outFrom'                 => __( 'Out from under %s' ),
-	/* translators: 1: Item name, 2: Item type, 3: Item index, 4: Total Items */
+	/* translators: 1: Item name, 2: Item type, 3: Item index, 4: Total items. */
 	'menuFocus'               => __( 'Edit %1$s (%2$s, %3$d of %4$d)' ),
-	/* translators: 1: Item name, 2: Item type, 3: Item index, 4: Total Items, 5: Item Parent. */
+	/* translators: 1: Item name, 2: Item type, 3: Item index, 4: Total items, 5: Item parent. */
 	'subMenuFocus'            => __( 'Edit %1$s (%2$s, sub-item %3$d of %4$d under %5$s)' ),
-	/* translators: 1: Item name, 2: Item type, 3: Item index, 4: Total Items, 5: Item Parent, 6: Item Depth */
+	/* translators: 1: Item name, 2: Item type, 3: Item index, 4: Total items, 5: Item parent, 6: Item depth */
 	'subMenuMoreDepthFocus'   => __( 'Edit %1$s (%2$s, sub-item %3$d of %4$d under %5$s, level %6$d)' ),
 	/* translators: %s: Item name. */
 	'menuItemDeletion'        => __( 'item %s' ),
