1dd1014d186250b4efbc55a3846ee675002b195b
paul bearne <paul>
2024-06-05 14:00:41 -0400

    Add `nicename_exists` function and associated tests
    The `nicename_exists` function is implemented to determine if a given user_nicename exists. The function is also integrated into the user nickname checking mechanism within the user class. Additionally, various test cases for the new function have been added, which cover scenarios with existing, non-existing, and nicknames associated with different user logins.

--- tests/phpunit/tests/user/nicenameExisits.php
+++ tests/phpunit/tests/user/nicenameExisits.php
@@ -37,7 +37,7 @@ class Tests_User_NicenameExists extends WP_UnitTestCase {
 	public function test_nicename_exists_with_different_user_login() {
 		$user_id_1 = $this->factory()->user->create( array( 'user_nicename' => 'test-nicename' ) );
 		$user_id_2 = $this->factory()->user->create();
-		$user_2 = get_user_by('id', $user_id_2 );
+		$user_2    = get_user_by( 'id', $user_id_2 );
 
 		$this->assertSame( $user_id_1, nicename_exists( 'test-nicename', $user_2->user_login ) );
 	}
@@ -49,7 +49,7 @@ class Tests_User_NicenameExists extends WP_UnitTestCase {
 	 */
 	public function test_nicename_exists_with_same_user_login() {
 		$user_id_1 = $this->factory()->user->create( array( 'user_nicename' => 'test-nicename' ) );
-		$user_1 = get_user_by('id', $user_id_1 );
+		$user_1    = get_user_by( 'id', $user_id_1 );
 
 		$this->assertFalse( nicename_exists( 'test-nicename', $user_1->user_login ) );
 	}
