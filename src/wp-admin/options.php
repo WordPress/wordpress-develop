ac15583a91bc0615212c5af8b8fda66add474413
narenin <narendra.sisodiya10@gmail.com>
2024-06-05 10:39:09 +0530

    Implemented suggestions

--- src/wp-admin/options.php
+++ src/wp-admin/options.php
@@ -191,7 +191,6 @@ if ( ! is_multisite() ) {
 
 	$allowed_options['general'][] = 'users_can_register';
 	$allowed_options['general'][] = 'default_role';
-	
 	$allowed_options['writing'][] = 'ping_sites';
 
 	$allowed_options['media'][] = 'uploads_use_yearmonth_folders';
