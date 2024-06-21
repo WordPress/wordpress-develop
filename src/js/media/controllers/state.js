3ae04799e16e3192d4fa1bd90f5ca74b523cd38d
Joe Dolson <design@joedolson.com>
2024-06-21 15:50:30 -0500

    Remove action menu if fewer than 2 items present
    Tests 60666

--- src/js/media/controllers/state.js
+++ src/js/media/controllers/state.js
@@ -167,15 +167,15 @@ var State = Backbone.Model.extend(/** @lends wp.media.controller.State.prototype
 			mode = this.get('menu'),
 			actionMenuItems,
 			actionMenuLength,
-			view;
+			view,
+            actionMenuItems = this.frame.menu.get('views'),
+			actionMenuLength = actionMenuItems ? actionMenuItems.views.get().length : 0;
 
 		if ( this.frame.menu ) {
-			actionMenuItems = this.frame.menu.get('views'),
-			actionMenuLength = actionMenuItems ? actionMenuItems.views.get().length : 0,
 			// Show action menu only if it is active and has more than one default element.
 			this.frame.$el.toggleClass( 'hide-menu', ! mode || actionMenuLength < 2 );
 		}
-		if ( ! mode ) {
+		if ( ! mode || actionMenuLength < 2) {
 			return;
 		}
 
