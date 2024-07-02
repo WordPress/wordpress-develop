33c83c07960b72820f86f08661fabbd7259b97ec
Jonathan Desrosiers <desrosj@git.wordpress.org>
2024-07-02 19:02:55 +0000

    Build/Test Tools: Make use of new reusable workflows for 5.3.
    This updates the 5.3 branch to utilize the new reusable workflows in trunk introduced in [58165].
    
    This also includes backports for a some additional improvements and bug fixes that are necessary for the local development environment to continue working long term:
    
    - The image and platform properties for the mysql container have been updated to always prefer `amd64` containers (#60822).
    - `macos-13` is now pinned for MacOS jobs instead of `macos-latest` (#61340).
    - Migrating to Docker Compose V2 (#60901).
    - Removing the version property from docker-compose.yml (#59416).
    - Improvements to how artifacts and comments for Playground testing are generated.
    - Removing SVN related commands causing failures (#61216).
    - Updating the `actions/github-scripts` action to the latest version.
    - Cache the results of `PHP_CodeSniffer` runs (#49783).
    - Move the Memcached container into the Docker Compose config (#55700).
    - Improvements to the `healthcheck` command for the `mysql` container (#58867).
    - Skip some tests when not in the primary branch (#50401).
    
    Merges [49264], [51673], [52179], [53552], [53895], [56464], [57918], [58157], [57124], [57125], [57249] to the 5.3 branch.
    
    Props johnbillion, joemcgill, swissspidy, thelovekesh, narenin, mukesh27, JeffPaul, peterwilsoncc, zieladam, ockham, SergeyBiryukov, jorbin, Clorith, afragen, jrf.
    See #49783, #58867, #61340, #60822, #61216, #60901, #61101, #59416, #59805, #61213.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/5.3@58624 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/includes/abstract-testcase.php
+++ tests/phpunit/includes/abstract-testcase.php
@@ -180,22 +180,23 @@ abstract class WP_UnitTestCase_Base extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * Allow tests to be skipped on some automated runs.
+	 * Allows tests to be skipped on some automated runs.
 	 *
-	 * For test runs on Travis for something other than trunk/master
-	 * we want to skip tests that only need to run for master.
+	 * For test runs on GitHub Actions for something other than trunk,
+	 * we want to skip tests that only need to run for trunk.
 	 */
 	public function skipOnAutomatedBranches() {
-		// https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
-		$travis_branch       = getenv( 'TRAVIS_BRANCH' );
-		$travis_pull_request = getenv( 'TRAVIS_PULL_REQUEST' );
+		// https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+		$github_event_name = getenv( 'GITHUB_EVENT_NAME' );
+		$github_ref        = getenv( 'GITHUB_REF' );
 
-		if ( ! $travis_branch || ! $travis_pull_request ) {
-			return;
-		}
+		if ( $github_event_name ) {
+			// We're on GitHub Actions.
+			$skipped = array( 'pull_request', 'pull_request_target' );
 
-		if ( 'master' !== $travis_branch || 'false' !== $travis_pull_request ) {
-			$this->markTestSkipped( 'For automated test runs, this test is only run on trunk/master' );
+			if ( in_array( $github_event_name, $skipped, true ) || 'refs/heads/trunk' !== $github_ref ) {
+				$this->markTestSkipped( 'For automated test runs, this test is only run on trunk' );
+			}
 		}
 	}
 
