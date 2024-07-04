59aed17a94ec137eccaf2fb15899eaa719790577
Dennis Snell <dennis.snell@automattic.com>
2024-07-04 14:08:01 -0700

    Expand comment warning not to move the query arg.

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -2224,7 +2224,14 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) :
 		/* translators: %s: User login. */
 		$message  = sprintf( __( 'Username: %s' ), $user->user_login ) . "\r\n\r\n";
 		$message .= __( 'To set your password, visit the following address:' ) . "\r\n\r\n";
-		// Note, the order of arguments is significant here. See https://core.trac.wordpress.org/ticket/42957
+
+		/*
+		 * Since some user login names end in a period, this could produce ambiguous URLs that
+		 * end in a period. To avoid the ambiguity, ensure that the login is not the last query
+		 * arg in the URL. If moving it to the end, a trailing period will need to be escaped.
+		 *
+		 * @see https://core.trac.wordpress.org/tickets/42957
+		 */
 		$message .= network_site_url( 'wp-login.php?login=' . rawurlencode( $user->user_login ) . "&key=$key&action=rp", 'login' ) . "\r\n\r\n";
 
 		$message .= wp_login_url() . "\r\n";
