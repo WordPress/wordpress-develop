4eaf773f1c4dc6dc86be1f607054a3a9f66a0d2a
Ankur Vishwakarma <ankurvishwakarma54@yahoo.com>
2024-07-05 19:32:54 +0530

    Update theme.js
    Ticket https://core.trac.wordpress.org/ticket/61578

--- src/js/_enqueues/wp/theme.js
+++ src/js/_enqueues/wp/theme.js
@@ -96,7 +96,8 @@ themes.view.Appearance = wp.Backbone.View.extend({
 	},
 
 	// Defines search element container.
-	searchContainer: $( '.search-form .search-box' ),
+	//searchContainer: $( '.search-form .search-box' ),//Bug => //Submit only works with Form and not with <p> tag
+	searchContainer: $( '.search-form' ),//Fix
 
 	// Search input and view
 	// for current theme collection.
@@ -118,9 +119,11 @@ themes.view.Appearance = wp.Backbone.View.extend({
 		// Render and append after screen title.
 		view.render();
 		this.searchContainer
+			.find('.search-box')
 			.append( $.parseHTML( '<label for="wp-filter-search-input">' + l10n.search + '</label>' ) )
-			.append( view.el )
-			.on( 'submit', function( event ) {
+			.append( view.el );
+
+		this.searchContainer.on( 'submit', function( event ) {
 				event.preventDefault();
 			});
 	},
