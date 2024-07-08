98d9dea693c8bd543ba3f93d5a9fa32df962ba5d
ramon <ramonjd@gmail.com>
2024-07-08 13:23:26 +1000

    Tabs. No spaces.

--- src/wp-includes/class-wp-theme-json.php
+++ src/wp-includes/class-wp-theme-json.php
@@ -237,7 +237,7 @@ class WP_Theme_JSON {
 		'background-position'               => array( 'background', 'backgroundPosition' ),
 		'background-repeat'                 => array( 'background', 'backgroundRepeat' ),
 		'background-size'                   => array( 'background', 'backgroundSize' ),
-        'background-attachment'             => array( 'background', 'backgroundAttachment' ),
+		'background-attachment'             => array( 'background', 'backgroundAttachment' ),
 		'border-radius'                     => array( 'border', 'radius' ),
 		'border-top-left-radius'            => array( 'border', 'radius', 'topLeft' ),
 		'border-top-right-radius'           => array( 'border', 'radius', 'topRight' ),
