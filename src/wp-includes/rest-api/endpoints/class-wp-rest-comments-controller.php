2d06fb1712d3656bc142a45d4ee861777efae63a
Anton Vlasenko <vlasenko.anton@gmail.com>
2024-07-11 17:43:53 +0200

    Draft version of the code for the comments controller.

--- src/wp-includes/rest-api/endpoints/class-wp-rest-comments-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-comments-controller.php
@@ -262,6 +262,20 @@ class WP_REST_Comments_Controller extends WP_REST_Controller {
 			$prepared_args['offset'] = $prepared_args['number'] * ( absint( $request['page'] ) - 1 );
 		}
 
+		// Don't overwrite the "update_post_meta_cache" value if it is already defined.
+		if ( ! isset( $prepared_args['update_post_meta_cache'] ) ) {
+			$object_subtype         = isset( $prepared_args['post_type'] ) ? $prepared_args['post_type'] : '';
+			$should_prime_meta_keys = ! empty( get_registered_meta_keys( 'comment', $object_subtype ) );
+			/**
+			 * Performance optimization. If there are no registered meta keys,
+			 * set "update_post_meta_cache" to false to avoid unnecessary priming of meta data.
+			 */
+			if ( ! $should_prime_meta_keys ) {
+				$prepared_args['update_comment_meta_cache']    = false;
+				$prepared_args['update_comment_post_cache'] = false;
+			}
+		}
+
 		/**
 		 * Filters WP_Comment_Query arguments when querying comments via the REST API.
 		 *
