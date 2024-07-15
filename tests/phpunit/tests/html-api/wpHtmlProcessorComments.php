067597ee56e12d117566cf2d6afd3f9832c07f0a
Jon Surrell <sirreal@users.noreply.github.com>
2024-07-15 17:48:13 +0200

    Split into simplified funky/regular comment tests
    Use two different tests with much simpler function signatures.

--- tests/phpunit/tests/html-api/wpHtmlProcessorComments.php
+++ tests/phpunit/tests/html-api/wpHtmlProcessorComments.php
@@ -19,11 +19,11 @@ class Tests_HtmlApi_WpHtmlProcessorComments extends WP_UnitTestCase {
 	 *
 	 * @dataProvider data_comments
 	 */
-	public function test_comment_processing( string $html, string $expected_token_name, string $expected_modifiable_text = '', string $expected_comment_type = null, string $expected_tag = null ) {
+	public function test_comment_processing( string $html, string $expected_comment_type, string $expected_modifiable_text, string $expected_tag = null ) {
 		$processor = WP_HTML_Processor::create_fragment( $html );
 		$processor->next_token();
 
-		$this->assertSame( $expected_token_name, $processor->get_token_name() );
+		$this->assertSame( '#comment', $processor->get_token_name() );
 		$this->assertSame( $expected_comment_type, $processor->get_comment_type() );
 		$this->assertSame( $expected_modifiable_text, $processor->get_modifiable_text() );
 		$this->assertSame( $expected_tag, $processor->get_tag() );
@@ -36,16 +36,42 @@ class Tests_HtmlApi_WpHtmlProcessorComments extends WP_UnitTestCase {
 	 */
 	public static function data_comments() {
 		return array(
-			'Normative comment'              => array( '<!-- A comment. -->', '#comment', ' A comment. ', WP_HTML_Processor::COMMENT_AS_HTML_COMMENT ),
-			'Abruptly closed comment'        => array( '<!-->', '#comment', '', WP_HTML_Processor::COMMENT_AS_ABRUPTLY_CLOSED_COMMENT ),
-			'Invalid HTML comment !'         => array( '<! Bang opener >', '#comment', ' Bang opener ', WP_HTML_Processor::COMMENT_AS_INVALID_HTML ),
-			'Invalid HTML comment ?'         => array( '<? Question opener >', '#comment', ' Question opener ', WP_HTML_Processor::COMMENT_AS_INVALID_HTML ),
-			'Funky comment # (empty)'        => array( '</#>', '#funky-comment', '#' ),
-			'Funky comment #'                => array( '</# foo>', '#funky-comment', '# foo' ),
-			'Funky comment •'                => array( '</• bar>', '#funky-comment', '• bar' ),
-			'Processing instriction comment' => array( '<?pi-target Instruction body. ?>', '#comment', ' Instruction body. ', WP_HTML_Processor::COMMENT_AS_PI_NODE_LOOKALIKE, 'pi-target' ),
-			'Processing instriction php'     => array( '<?php const HTML_COMMENT = true; ?>', '#comment', ' const HTML_COMMENT = true; ', WP_HTML_Processor::COMMENT_AS_PI_NODE_LOOKALIKE, 'php' ),
-			'CDATA comment'                  => array( '<![CDATA[ cdata body ]]>', '#comment', ' cdata body ', WP_HTML_Processor::COMMENT_AS_CDATA_LOOKALIKE ),
+			'Normative comment'              => array( '<!-- A comment. -->', WP_HTML_Processor::COMMENT_AS_HTML_COMMENT, ' A comment. ' ),
+			'Abruptly closed comment'        => array( '<!-->', WP_HTML_Processor::COMMENT_AS_ABRUPTLY_CLOSED_COMMENT, '' ),
+			'Invalid HTML comment !'         => array( '<! Bang opener >', WP_HTML_Processor::COMMENT_AS_INVALID_HTML, ' Bang opener ' ),
+			'Invalid HTML comment ?'         => array( '<? Question opener >', WP_HTML_Processor::COMMENT_AS_INVALID_HTML, ' Question opener ' ),
+			'CDATA comment'                  => array( '<![CDATA[ cdata body ]]>', WP_HTML_Processor::COMMENT_AS_CDATA_LOOKALIKE, ' cdata body ' ),
+			'Processing instriction comment' => array( '<?pi-target Instruction body. ?>', WP_HTML_Processor::COMMENT_AS_PI_NODE_LOOKALIKE, ' Instruction body. ', 'pi-target' ),
+			'Processing instriction php'     => array( '<?php const HTML_COMMENT = true; ?>', WP_HTML_Processor::COMMENT_AS_PI_NODE_LOOKALIKE, ' const HTML_COMMENT = true; ', 'php' ),
+		);
+	}
+
+	/**
+	 * Ensures that different types of comments are processed correctly.
+	 *
+	 * @ticket 61530
+	 *
+	 * @dataProvider data_funky_comments
+	 */
+	public function test_funky_comment( string $html, string $expected_modifiable_text ) {
+		$processor = WP_HTML_Processor::create_fragment( $html );
+		$processor->next_token();
+
+		$this->assertSame( '#funky-comment', $processor->get_token_name() );
+		$this->assertSame( $expected_modifiable_text, $processor->get_modifiable_text() );
+		$this->assertSame( $expected_tag, $processor->get_tag() );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array[]
+	 */
+	public static function data_funky_comments() {
+		return array(
+			'Funky comment # (empty)' => array( '</#>', '#' ),
+			'Funky comment #'         => array( '</# foo>', '# foo' ),
+			'Funky comment •'         => array( '</• bar>', '• bar' ),
 		);
 	}
 }
