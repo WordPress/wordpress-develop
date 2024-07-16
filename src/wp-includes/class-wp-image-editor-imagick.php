db8ee370fcb6b6f58292eab8ea139d334153bdd1
Adam Silverstein <adamjs@google.com>
2024-07-15 22:41:16 -0600

    remove additional 61614 changes

--- src/wp-includes/class-wp-image-editor-imagick.php
+++ src/wp-includes/class-wp-image-editor-imagick.php
@@ -214,7 +214,6 @@ class WP_Image_Editor_Imagick extends WP_Image_Editor {
 					if ( 'lossless' === $webp_info['type'] ) {
 						// Use WebP lossless settings.
 						$this->image->setImageCompressionQuality( 100 );
-						$this->image->setCompressionQuality( 100 );
 						$this->image->setOption( 'webp:lossless', 'true' );
 					} else {
 						$this->image->setImageCompressionQuality( $quality );
