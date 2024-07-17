8eca544a81b2da5bb55ec66c0819893de7110f38
Jon Surrell <sirreal@users.noreply.github.com>
2024-07-17 10:34:16 +0200

    Add argument type annotation to contains method

--- src/wp-includes/html-api/class-wp-html-open-elements.php
+++ src/wp-includes/html-api/class-wp-html-open-elements.php
@@ -136,7 +136,7 @@ class WP_HTML_Open_Elements {
 	 * @param string $node_name Name of node for which to check.
 	 * @return bool Whether a node of the given name is in the stack of open elements.
 	 */
-	public function contains( $node_name ): bool {
+	public function contains( string $node_name ): bool {
 		foreach ( $this->walk_up() as $item ) {
 			if ( $node_name === $item->node_name ) {
 				return true;
