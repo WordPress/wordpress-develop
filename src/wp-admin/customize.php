8f92773d41129fd42e79ce0a0a7f6a53b0249c2c
Tonya Mork <hellofromtonya@gmail.com>
2024-07-18 12:49:13 -0500

    Decrease indents for readability.

--- src/wp-admin/customize.php
+++ src/wp-admin/customize.php
@@ -87,8 +87,8 @@ if ( $wp_customize->changeset_post_id() ) {
 $url    = ! empty( $_REQUEST['url'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['url'] ) ) : '';
 $return = ! empty( $_REQUEST['return'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['return'] ) ) : '';
 $autofocus = ! empty( $_REQUEST['autofocus'] ) && is_array( $_REQUEST['autofocus'] )
-				? array_map( 'sanitize_text_field', wp_unslash( $_REQUEST['autofocus'] ) )
-				: array(); // Since `$_REQUEST['autofocus']` is an array.
+	? array_map( 'sanitize_text_field', wp_unslash( $_REQUEST['autofocus'] ) )
+	: array(); // Since `$_REQUEST['autofocus']` is an array.
 
 if ( ! empty( $url ) ) {
 	$wp_customize->set_preview_url( $url );
