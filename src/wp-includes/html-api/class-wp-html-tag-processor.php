c1034103cd2231f7efcc77884b5c9e275009eafd
Jon Surrell <sirreal@users.noreply.github.com>
2024-07-19 16:36:06 +0200

    Remove lower-casing behavior of class_list
    This is necessary for has_class to work properly.
    This could be put into a protected method or the class sensitivity could
    be a parameter if desired.

--- src/wp-includes/html-api/class-wp-html-tag-processor.php
+++ src/wp-includes/html-api/class-wp-html-tag-processor.php
@@ -1126,6 +1126,7 @@ class WP_HTML_Tag_Processor {
 	 *     // Outputs: "free <egg> lang-en "
 	 *
 	 * @since 6.4.0
+	 * @since 6.7.0 Class names are no longer force lower-cased.
 	 */
 	public function class_list() {
 		if ( self::STATE_MATCHED_TAG !== $this->parser_state ) {
@@ -1155,12 +1156,7 @@ class WP_HTML_Tag_Processor {
 				return;
 			}
 
-			/*
-			 * CSS class names are case-insensitive in the ASCII range.
-			 *
-			 * @see https://www.w3.org/TR/CSS2/syndata.html#x1
-			 */
-			$name = strtolower( substr( $class, $at, $length ) );
+			$name = substr( $class, $at, $length );
 			$at  += $length;
 
 			/*
