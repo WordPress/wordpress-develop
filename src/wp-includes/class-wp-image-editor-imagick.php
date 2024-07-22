3305dfe5b3214f12c3d4d9aa6bdb4cef9f97a353
Adam Silverstein <adamjs@google.com>
2024-07-22 08:18:54 -0600

    Set 'heic:speed' to 7 (default is 5)

--- src/wp-includes/class-wp-image-editor-imagick.php
+++ src/wp-includes/class-wp-image-editor-imagick.php
@@ -220,6 +220,7 @@ class WP_Image_Editor_Imagick extends WP_Image_Editor {
 					}
 					break;
 				case 'image/avif':
+					$this->image->setOption( 'heic:speed', 7 );
 				default:
 					$this->image->setImageCompressionQuality( $quality );
 			}
