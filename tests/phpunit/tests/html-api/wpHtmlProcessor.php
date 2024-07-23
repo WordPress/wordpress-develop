3f780fcb6ab60208d940f2e3a40f5b93062b1746
Jon Surrell <sirreal@users.noreply.github.com>
2024-07-23 10:47:50 +0200

    Revert "Remove "COL" from void tags test"
    This reverts commit d14eaf318a3e51428b41cb8c8dee179ce6082d15.

--- tests/phpunit/tests/html-api/wpHtmlProcessor.php
+++ tests/phpunit/tests/html-api/wpHtmlProcessor.php
@@ -331,6 +331,7 @@ class Tests_HtmlApi_WpHtmlProcessor extends WP_UnitTestCase {
 			'AREA'   => array( 'AREA' ),
 			'BASE'   => array( 'BASE' ),
 			'BR'     => array( 'BR' ),
+			'COL'    => array( 'COL' ),
 			'EMBED'  => array( 'EMBED' ),
 			'HR'     => array( 'HR' ),
 			'IMG'    => array( 'IMG' ),
