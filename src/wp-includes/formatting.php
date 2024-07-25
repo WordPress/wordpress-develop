04024b4ca046d7d437dd73b8d6f93ca2f5df57c7
Scott Reilly <scott@coffee2code.com>
2024-07-25 16:00:16 -0600

    Docs: Improve documentation for `wp_strip_all_tags()`
    Explicitly express return value of the example is "an empty string" and add a period to indicate the end of the sentence.
    
    Fixes https://core.trac.wordpress.org/ticket/61759

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -5503,7 +5503,7 @@ function normalize_whitespace( $str ) {
  *
  * This differs from strip_tags() because it removes the contents of
  * the `<script>` and `<style>` tags. E.g. `strip_tags( '<script>something</script>' )`
- * will return 'something'. wp_strip_all_tags will return ''
+ * will return 'something'. wp_strip_all_tags will return an empty string.
  *
  * @since 2.9.0
  *
