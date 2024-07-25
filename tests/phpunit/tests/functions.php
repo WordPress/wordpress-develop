366c8e52069fe8eaa04c87d9faca9a4a3c802e87
Aaron Jorbin <jorbin@git.wordpress.org>
2024-07-25 16:33:50 +0000

    Filesystem: Normalize allowed_files so comparison is apples to apples.
    In [58470] a change was made to normalize the filename in validate_file, however this leads to instances where the list of files that are allowed aren't normalized such as in the theme editor. By normalizing the array, the comparison is apples to apples.
    
    Reviewed by hellofromTonya.
    Merges 58570 to the 6.5 branch.
    
    Fixes #61488.
    Props jorbin, hellofromtonya, swissspidy, misulicus, script2see, Presskopp, audrasjb, peterwilsoncc, siliconforks, littler.chicken, paulkevan,
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.5@58808 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/functions.php
+++ tests/phpunit/tests/functions.php
@@ -1793,6 +1793,7 @@ class Tests_Functions extends WP_UnitTestCase {
 	 * Test file path validation
 	 *
 	 * @ticket 42016
+	 * @ticket 61488
 	 * @dataProvider data_validate_file
 	 *
 	 * @param string $file          File path.
@@ -1911,6 +1912,13 @@ class Tests_Functions extends WP_UnitTestCase {
 				'C:/WINDOWS/system32',
 				array( 'C:/WINDOWS/system32' ),
 				2,
+			),
+
+			// Windows Path with allowed file
+			array(
+				'Apache24\htdocs\wordpress/wp-content/themes/twentyten/style.css',
+				array( 'Apache24\htdocs\wordpress/wp-content/themes/twentyten/style.css' ),
+				0,
 			),
 
 			// Disallowed files:
