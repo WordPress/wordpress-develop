9d3ed0a6a2edbec0cef487e7b8944bd9609ad27c
Ari Stathopoulos <aristath@gmail.com>
2024-07-31 09:58:44 +0300

    Test that `class-*` files exist in the classMap. Props @peterwilsoncc

--- tests/phpunit/tests/autoloader-classmap.php
+++ tests/phpunit/tests/autoloader-classmap.php
@@ -91,4 +91,55 @@ class Tests_Autoloader_Classmap extends WP_UnitTestCase {
 
 		return $data;
 	}
+
+	/**
+	 * @group pwcc
+	 */
+	public function test_autoloader_class_files_exist_in_classmap() {
+		$expected_classmap = $this->get_all_wp_class_files();
+		$actual_classmap   = WP_Autoload::CLASSES_PATHS;
+		foreach ( $expected_classmap as $class_name => $file_path ) {
+			$this->assertArrayHasKey( $class_name, $actual_classmap, "Class '$class_name' is missing from the classmap." );
+			$this->assertSame( $file_path, $actual_classmap[ $class_name ], "Class '$class_name' is in the wrong file." );
+		}
+		// $this->assertEqualSetsWithIndex( $expected_classmap, $actual_classmap );
+	}
+
+	public function get_all_wp_class_files() {
+		$files        = array();
+		$directory    = new RecursiveDirectoryIterator( ABSPATH . WPINC );
+		$iterator     = new RecursiveIteratorIterator( $directory );
+		$regex        = new RegexIterator( $iterator, '/^.+\/class\-[a-z-]+\.php$/i', RecursiveRegexIterator::GET_MATCH );
+		$ltrim_length = strlen( trailingslashit( ABSPATH ) );
+
+		$package_paths_to_ignore = array(
+			'wp-includes/class-requests.php',  // 3rd-party library.
+			'wp-includes/Requests/',           // 3rd-party library.
+			'wp-includes/sodium_compat/',      // 3rd-party library.
+			'wp-includes/class-avif-info.php', // 3rd-party library.
+			'wp-includes/class-simplepie.php', // 3rd-party library.
+			'wp-includes/class-snoopy.php',    // Deprecated.
+		);
+
+		foreach ( $regex as $file ) {
+			$class_file    = $file[0];
+			$relative_file = substr( $class_file, $ltrim_length );
+			foreach ( $package_paths_to_ignore as $package_path ) {
+				if ( str_contains( $relative_file, $package_path ) !== false ) {
+					continue 2;
+				}
+			}
+
+			$file_contents = file_get_contents( $class_file );
+			// Extract the class name from the file.
+			preg_match( '/^class\s+([a-zA-Z0-9_]+)/m', $file_contents, $matches );
+			if ( empty( $matches ) ) {
+				continue;
+			}
+			$class_name           = strtolower( $matches[1] );
+			$files[ $class_name ] = $relative_file;
+		}
+
+		return $files;
+	}
 }
