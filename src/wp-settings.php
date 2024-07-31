5168218d304f3531c49e4383bd3fb9c06c42c178
Ari Stathopoulos <aristath@gmail.com>
2024-07-31 10:21:24 +0300

    Nitpick

--- src/wp-settings.php
+++ src/wp-settings.php
@@ -17,7 +17,8 @@ if ( ! defined( 'WPINC' ) ) {
 	define( 'WPINC', 'wp-includes' );
 }
 
-require_once ABSPATH . 'wp-includes/class-wp-autoload.php';
+// Require the autoloader.
+require_once ABSPATH . WPINC . '/class-wp-autoload.php';
 
 /**
  * Version information for the current WordPress release.
