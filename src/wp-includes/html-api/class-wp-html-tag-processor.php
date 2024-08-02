53d9f068fce7c4274a5f48358837b69bf9c363a1
Dennis Snell <dennis.snell@automattic.com>
2024-08-02 15:17:10 -0700

    Add comment explaining why the code returns.

--- src/wp-includes/html-api/class-wp-html-tag-processor.php
+++ src/wp-includes/html-api/class-wp-html-tag-processor.php
@@ -3198,6 +3198,7 @@ class WP_HTML_Tag_Processor {
 			 */
 			$escaped_new_value = in_array( $comparable_name, wp_kses_uri_attributes() ) ? esc_url( $value ) : esc_attr( $value );
 
+			// If the escaping functions wiped out the update, reject it and indicate it was rejected.
 			if ( '' === $escaped_new_value && '' !== $value ) {
 				return false;
 			}
