418b207da6a5ad7c4161ad555b528fa14615bb9b
Sneha Patil <90276347+snehapatil2001@users.noreply.github.com>
2024-08-06 11:05:27 +0530

    comment change
    Co-authored-by: Stephen A. Bernhardt <sabernhardt@yahoo.com>

--- src/wp-includes/comment-template.php
+++ src/wp-includes/comment-template.php
@@ -1739,8 +1739,9 @@ function comments_popup_link( $zero = false, $one = false, $more = false, $css_c
  *     @type string $respond_id The selector identifying the responding comment. Passed as the third parameter
  *                              to addComment.moveForm(), and appended to the link URL as a hash value.
  *                              Default 'respond'.
- *     @type string $reply_text The text of the Reply link. Default 'Reply'.
- *     @type string $reply_to_text The text template for "Reply to %s". Default 'Reply to %s'.
+ *     @type string $reply_text        The visible text of the Reply link. Default 'Reply'.
+ *     @type string $reply_to_text     The accessible name of the Reply link, using `%s` as a placeholder
+ *                                     for the comment author's name. Default 'Reply to %s'.
  *     @type bool   $use_reply_to_text Whether to use reply_to_text as visible link text. Default false.
  *     @type string $login_text The text of the link to reply if logged out. Default 'Log in to Reply'.
  *     @type int    $max_depth  The max depth of the comment tree. Default 0.
