87727dc2705508e161e03cb2b2a54d9954b73498
Erik <11491369+kebbet@users.noreply.github.com>
2024-08-06 09:57:27 +0200

    Changes requested.

--- src/wp-admin/options-general.php
+++ src/wp-admin/options-general.php
@@ -124,7 +124,6 @@ $tagline_description = sprintf(
 	$app_icon_alt_value     = '';
 	$browser_icon_alt_value = '';
 
-	// Store icon URL as variables for use in multiple places.
 	$site_icon_url = get_site_icon_url();
 
 	if ( $site_icon_id ) {
@@ -171,7 +170,7 @@ $tagline_description = sprintf(
 				<svg role="img" aria-hidden="true" fill="none" xmlns="http://www.w3.org/2000/svg" class="browser-buttons"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 20a6 6 0 1 1 12 0 6 6 0 0 1-12 0Zm18 0a6 6 0 1 1 12 0 6 6 0 0 1-12 0Zm24-6a6 6 0 1 0 0 12 6 6 0 0 0 0-12Z" /></svg>
 				<div class="site-icon-preview-tab">
 					<img id="browser-icon-preview" src="<?php echo esc_url( $site_icon_url ); ?>" class="browser-icon-preview" alt="<?php echo esc_attr( $browser_icon_alt_value ); ?>" />
-					<div class="site-icon-preview-site-title" id="site-icon-preview-site-title" aria-hidden="true"><?php echo get_bloginfo( 'name' ); ?></div>
+					<div class="site-icon-preview-site-title" id="site-icon-preview-site-title" aria-hidden="true"><?php bloginfo( 'name' ); ?></div>
 						<svg role="img" aria-hidden="true" fill="none" xmlns="http://www.w3.org/2000/svg" class="close-button">
 							<path d="M12 13.0607L15.7123 16.773L16.773 15.7123L13.0607 12L16.773 8.28772L15.7123 7.22706L12 10.9394L8.28771 7.22705L7.22705 8.28771L10.9394 12L7.22706 15.7123L8.28772 16.773L12 13.0607Z" />
 						</svg>
