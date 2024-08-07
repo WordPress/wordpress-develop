ce861646be584e811c2622d24bc66dbeb640c9b9
Jon Surrell <sirreal@users.noreply.github.com>
2024-08-07 17:04:24 +0200

    Do not change the namespace to the integration node type

--- src/wp-includes/html-api/class-wp-html-processor.php
+++ src/wp-includes/html-api/class-wp-html-processor.php
@@ -393,7 +393,7 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 				$provenance            = ( ! $same_node || $is_virtual ) ? 'virtual' : 'real';
 				$this->element_queue[] = new WP_HTML_Stack_Event( $token, WP_HTML_Stack_Event::PUSH, $provenance );
 
-				$this->change_parsing_namespace( $token->integration_node_type ?? $token->namespace );
+				$this->change_parsing_namespace( $token->namespace );
 			}
 		);
 
@@ -406,7 +406,7 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 				$adjusted_current_node = $this->get_adjusted_current_node();
 				$this->change_parsing_namespace(
 					$adjusted_current_node
-						? ( $adjusted_current_node->integration_node_type ?? $adjusted_current_node->namespace )
+						? $adjusted_current_node->namespace
 						: 'html'
 				);
 			}
