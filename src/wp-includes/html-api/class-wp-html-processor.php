e29b20701eec9d216987ae4452fbd27e27de0005
Dennis Snell <dennis.snell@automattic.com>
2024-08-08 09:37:42 -0700

    Avoid separating the self-closing rules, remove the goto.

--- src/wp-includes/html-api/class-wp-html-processor.php
+++ src/wp-includes/html-api/class-wp-html-processor.php
@@ -4239,17 +4239,22 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 			/*
 			 * > If the token has its self-closing flag set, then run
 			 * > the appropriate steps from the following list:
+			 * >
+			 * >   ↪ the token's tag name is "script", and the new current node is in the SVG namespace
+			 * >         Acknowledge the token's self-closing flag, and then act as
+			 * >         described in the steps for a "script" end tag below.
+			 * >
+			 * >   ↪ Otherwise
+			 * >         Pop the current node off the stack of open elements and
+			 * >         acknowledge the token's self-closing flag.
+			 *
+			 * Since the rules for SCRIPT below indicate to pop the element off of the stack of
+			 * open elements, which is the same for the Otherwise condition, there's no need to
+			 * separate these checks. The difference comes when a parser operates with the scripting
+			 * flag enabled, and executes the script, which this parser does not support.
 			 */
 			if ( $this->state->current_token->has_self_closing_flag ) {
-				if ( 'SCRIPT' === $this->state->current_token->node_name && 'svg' === $this->state->current_token->namespace ) {
-					/*
-					 * > Acknowledge the token's self-closing flag, and then act as
-					 * > described in the steps for a "script" end tag below.
-					 */
-					goto in_foreign_content_svg_script_close_tag;
-				} else {
-					$this->state->stack_of_open_elements->pop();
-				}
+				$this->state->stack_of_open_elements->pop();
 			}
 			return true;
 		}
@@ -4258,7 +4263,6 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 		 * > An end tag whose name is "script", if the current node is an SVG script element.
 		 */
 		if ( $this->is_tag_closer() && 'SCRIPT' === $this->state->current_token->node_name && 'svg' === $this->state->current_token->namespace ) {
-			in_foreign_content_svg_script_close_tag:
 			$this->state->stack_of_open_elements->pop();
 			return true;
 		}
