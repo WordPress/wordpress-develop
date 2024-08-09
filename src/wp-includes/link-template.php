231f2479d763562ee3164e261922a0b24bc391fa
Felix Arntz <flixos90@gmail.com>
2024-08-09 10:31:15 -0700

    Enhance documentation.

--- src/wp-includes/link-template.php
+++ src/wp-includes/link-template.php
@@ -1595,9 +1595,11 @@ function get_delete_post_link( $post = 0, $deprecated = '', $force_delete = fals
  * Retrieves the edit comment link.
  *
  * @since 2.3.0
+ * @since 6.7.0 The $context parameter was added.
  *
  * @param int|WP_Comment $comment_id Optional. Comment ID or WP_Comment object.
- * @param string         $context    Optional. How to output the '&' character. Default '&amp;'.
+ * @param string         $context    Optional. Context in which the URL should be used. Either 'display',
+ *                                   to include HTML entities, or 'url'. Default 'display'.
  * @return string|void The edit comment link URL for the given comment id. Null if the comment id does not exist
  *                     or does not allow an editing UI.
  */
