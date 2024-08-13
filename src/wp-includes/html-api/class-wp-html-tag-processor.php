a9e924d6e3733eee517953f4d49393cc6ba22211
Jon Surrell <sirreal@users.noreply.github.com>
2024-08-13 17:02:21 +0200

    Revert "Replace null bytes in class_list class names"
    This reverts commit 4f1836818ca863f0611409fce5658acbe3fffe12.

--- src/wp-includes/html-api/class-wp-html-tag-processor.php
+++ src/wp-includes/html-api/class-wp-html-tag-processor.php
@@ -1127,7 +1127,6 @@ class WP_HTML_Tag_Processor {
 	 *
 	 * @since 6.4.0
 	 * @since 6.7.0 Class names are no longer force lower-cased.
-	 * @since 6.7.0 Null bytes are replaced with the replacement character (U+FFFD).
 	 */
 	public function class_list() {
 		if ( self::STATE_MATCHED_TAG !== $this->parser_state ) {
@@ -1157,7 +1156,7 @@ class WP_HTML_Tag_Processor {
 				return;
 			}
 
-			$name = str_replace( "\x00", "\u{FFFD}", substr( $class, $at, $length ) );
+			$name = substr( $class, $at, $length );
 			$at  += $length;
 
 			/*
