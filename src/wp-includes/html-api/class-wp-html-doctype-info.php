edba9e15cb53c8cf2f25d52393c17e598a087bb4
Jon Surrell <sirreal@users.noreply.github.com>
2024-08-16 18:26:13 +0200

    Move quirks detect to get_compatibility_mode method

--- src/wp-includes/html-api/class-wp-html-doctype-info.php
+++ src/wp-includes/html-api/class-wp-html-doctype-info.php
@@ -118,6 +118,152 @@ class WP_HTML_Doctype_Info {
 	 * @return string A string indicating the resulting quirks mode. One of "quirks",
 	 *                "limited-quirks", or "no-quirks".
 	 */
-	public function get_quirks_mode(): string {
+	public function get_compatibility_mode(): string {
+		/*
+		 * > A system identifier whose value is the empty string is not considered missing for the
+		 * > purposes of the conditions above.
+		 */
+		$system_identifier_is_missing = null === $this->system_identifier;
+
+		/*
+		 * > The system identifier and public identifier strings must be compared to the values
+		 * > given in the lists above in an ASCII case-insensitive manner. A system identifier whose
+		 * > value is the empty string is not considered missing for the purposes of the conditions above.
+		 */
+		$public_identifier = null === $this->public_identifier ? '' : strtolower( $this->public_identifier );
+		$system_identifier = null === $this->system_identifier ? '' : strtolower( $this->system_identifier );
+
+		/*
+		 * > [If] the DOCTYPE token matches one of the conditions in the following list, then set
+		 * > the Document to quirks mode:
+		 */
+
+		// > The force-quirks flag is set to on.
+		if ( $this->force_quirks_flag ) {
+			return 'quirks';
+		}
+
+		// > The name is not "html".
+		if ( 'html' !== $this->name ) {
+			return 'quirks';
+		}
+
+		// > The public identifier is set to…
+		if (
+			'-//w3o//dtd w3 html strict 3.0//en//' === $public_identifier ||
+			'-/w3c/dtd html 4.0 transitional/en' === $public_identifier ||
+			'html' === $public_identifier
+		) {
+			return 'quirks';
+		}
+
+		// > The system identifier is set to…
+		if ( 'http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd' === $system_identifier ) {
+			return 'quirks';
+		}
+
+		/*
+		 * All of the following conditions depend on matching the public identifier.
+		 * If the public identifier is falsy, none of the following conditions will match.
+		 */
+		if ( '' === $public_identifier ) {
+			return 'no-quirks';
+		}
+
+		// > The public identifier starts with…
+		if (
+			str_starts_with( $public_identifier, '+//silmaril//dtd html pro v0r11 19970101//' ) ||
+			str_starts_with( $public_identifier, '-//as//dtd html 3.0 aswedit + extensions//' ) ||
+			str_starts_with( $public_identifier, '-//advasoft ltd//dtd html 3.0 aswedit + extensions//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 2.0 level 1//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 2.0 level 2//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 2.0 strict level 1//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 2.0 strict level 2//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 2.0 strict//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 2.0//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 2.1e//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 3.0//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 3.2 final//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 3.2//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html 3//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html level 0//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html level 1//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html level 2//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html level 3//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html strict level 0//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html strict level 1//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html strict level 2//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html strict level 3//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html strict//' ) ||
+			str_starts_with( $public_identifier, '-//ietf//dtd html//' ) ||
+			str_starts_with( $public_identifier, '-//metrius//dtd metrius presentational//' ) ||
+			str_starts_with( $public_identifier, '-//microsoft//dtd internet explorer 2.0 html strict//' ) ||
+			str_starts_with( $public_identifier, '-//microsoft//dtd internet explorer 2.0 html//' ) ||
+			str_starts_with( $public_identifier, '-//microsoft//dtd internet explorer 2.0 tables//' ) ||
+			str_starts_with( $public_identifier, '-//microsoft//dtd internet explorer 3.0 html strict//' ) ||
+			str_starts_with( $public_identifier, '-//microsoft//dtd internet explorer 3.0 html//' ) ||
+			str_starts_with( $public_identifier, '-//microsoft//dtd internet explorer 3.0 tables//' ) ||
+			str_starts_with( $public_identifier, '-//netscape comm. corp.//dtd html//' ) ||
+			str_starts_with( $public_identifier, '-//netscape comm. corp.//dtd strict html//' ) ||
+			str_starts_with( $public_identifier, "-//o'reilly and associates//dtd html 2.0//" ) ||
+			str_starts_with( $public_identifier, "-//o'reilly and associates//dtd html extended 1.0//" ) ||
+			str_starts_with( $public_identifier, "-//o'reilly and associates//dtd html extended relaxed 1.0//" ) ||
+			str_starts_with( $public_identifier, '-//sq//dtd html 2.0 hotmetal + extensions//' ) ||
+			str_starts_with( $public_identifier, '-//softquad software//dtd hotmetal pro 6.0::19990601::extensions to html 4.0//' ) ||
+			str_starts_with( $public_identifier, '-//softquad//dtd hotmetal pro 4.0::19971010::extensions to html 4.0//' ) ||
+			str_starts_with( $public_identifier, '-//spyglass//dtd html 2.0 extended//' ) ||
+			str_starts_with( $public_identifier, '-//sun microsystems corp.//dtd hotjava html//' ) ||
+			str_starts_with( $public_identifier, '-//sun microsystems corp.//dtd hotjava strict html//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html 3 1995-03-24//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html 3.2 draft//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html 3.2 final//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html 3.2//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html 3.2s draft//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html 4.0 frameset//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html 4.0 transitional//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html experimental 19960712//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd html experimental 970421//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd w3 html//' ) ||
+			str_starts_with( $public_identifier, '-//w3o//dtd w3 html 3.0//' ) ||
+			str_starts_with( $public_identifier, '-//webtechs//dtd mozilla html 2.0//' ) ||
+			str_starts_with( $public_identifier, '-//webtechs//dtd mozilla html//' )
+		) {
+			return 'quirks';
+		}
+
+		// > The system identifier is missing and the public identifier starts with…
+		if (
+			$system_identifier_is_missing && (
+				str_starts_with( $public_identifier, '-//w3c//dtd html 4.01 frameset//' ) ||
+				str_starts_with( $public_identifier, '-//w3c//dtd html 4.01 transitional//' )
+			)
+		) {
+			return 'quirks';
+		}
+
+		/*
+		 * > Otherwise, [if] the DOCTYPE token matches one of the conditions in
+		 * > the following list, then set the Document to limited-quirks mode.
+		 */
+
+		// > The public identifier starts with…
+		if (
+			str_starts_with( $public_identifier, '-//w3c//dtd xhtml 1.0 frameset//' ) ||
+			str_starts_with( $public_identifier, '-//w3c//dtd xhtml 1.0 transitional//' )
+		) {
+			return 'limited-quirks';
+		}
+
+		// > The system identifier is not missing and the public identifier starts with…
+		if (
+			! $system_identifier_is_missing && (
+				str_starts_with( $public_identifier, '-//w3c//dtd html 4.01 frameset//' ) ||
+				str_starts_with( $public_identifier, '-//w3c//dtd html 4.01 transitional//' )
+			)
+		) {
+			return 'limited-quirks';
+		}
+
+		return 'no-quirks';
 	}
 }
