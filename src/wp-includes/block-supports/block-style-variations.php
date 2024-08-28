41ea40968e84ee2a78cfa599be1f3a9c9dae3f0a
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2024-08-28 12:34:19 +0700

    Move deprecation function to correct location

--- src/wp-includes/block-supports/block-style-variations.php
+++ src/wp-includes/block-supports/block-style-variations.php
@@ -8,24 +8,6 @@
  */
 
 /**
- * Generate block style variation instance name.
- *
- * @since 6.6.0
- * @deprecated 6.7.0 Use `wp_unique_id( $variation . '--' )` instead.
- *
- * @access private
- *
- * @param array  $block     Block object.
- * @param string $variation Slug for the block style variation.
- *
- * @return string The unique variation name.
- */
-function wp_create_block_style_variation_instance_name( $block, $variation ) {
-	_deprecated_function( __FUNCTION__, '6.7.0', 'wp_unique_id' );
-	return $variation . '--' . md5( serialize( $block ) );
-}
-
-/**
  * Determines the block style variation names within a CSS class string.
  *
  * @since 6.6.0
