b2134a35ca3fe3d856d6001e67fe603461ed2ebd
iflairwebtechnologies <jinal@iflair.com>
2024-08-29 15:03:03 +0530

    Fixed: TT3 and TT4: make template heading text translatable

---
+++ src/wp-content/themes/twentytwentythree/patterns/home-heading.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Home Heading
+ * Slug: twentytwentythree/home-heading
+ * Inserter: no
+ */
+?>
+<!-- wp:heading {"level":1,"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}}} -->
+<h1 class="alignwide" style="margin-bottom:var(--wp--preset--spacing--60)"><?php echo esc_html_x( 'Mindblown: a blog about philosophy.', 'Home Heading', 'twentytwentythree' ); ?></h1>
+<!-- /wp:heading -->
