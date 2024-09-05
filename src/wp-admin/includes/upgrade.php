ee872b2d97277ba364293783e248994e85e58451
mujuonly <muju.only@gmail.com>
2024-09-05 12:49:18 +0530

    Update upgrade.php
    Strict comparison

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -1449,7 +1449,7 @@ function upgrade_230() {
 		$links = $wpdb->get_results( "SELECT link_id, link_category FROM $wpdb->links" );
 		if ( ! empty( $links ) ) {
 			foreach ( $links as $link ) {
-				if ( 0 == $link->link_category ) {
+				if ( 0 === $link->link_category ) {
 					continue;
 				}
 				if ( ! isset( $link_cat_id_map[ $link->link_category ] ) ) {
