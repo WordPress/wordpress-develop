82eebc7d7c4d39980e61b5e99696722400f573a4
Bernie Reiter <ockham@raz.or.at>
2024-09-09 17:04:28 +0200

    Make basic redirect work

--- src/wp-includes/class-wp-taxonomy.php
+++ src/wp-includes/class-wp-taxonomy.php
@@ -512,6 +512,10 @@ final class WP_Taxonomy {
 
 			add_rewrite_tag( "%$this->name%", $tag, $this->query_var ? "{$this->query_var}=" : "taxonomy=$this->name&term=" );
 			add_permastruct( $this->name, "{$this->rewrite['slug']}/%$this->name%", $this->rewrite );
+
+			// TODO: If rewrite tag exists, append taxonomy name to regex.
+			add_rewrite_tag( '%taxonomy%', "(?:$this->name)", $this->query_var ? "{$this->query_var}=" : "taxonomy=$this->name&term=" );
+			add_permastruct( 'taxonomy', '%taxonomy%', $this->rewrite );
 		}
 	}
 
