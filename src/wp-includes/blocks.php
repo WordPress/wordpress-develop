7b34ec959e31cd586310178d6f050236ca885c9f
Carolina Nymark <hi@themesbycarolina.com>
2024-09-09 13:22:08 +0200

    Add support for the `format` property in query
    Adds handling the format property ($request['format'] ) in the class WP_REST_Posts_Controller.
    
    Adds handling the format property ($block->context['query']['format'])  in the function build_query_vars_from_query_block.

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -2330,6 +2330,7 @@ function wp_migrate_old_typography_shape( $metadata ) {
  *
  * @since 5.8.0
  * @since 6.1.0 Added `query_loop_block_query_vars` filter and `parents` support in query.
+ * @since 6.7.0 Added support for the `format` property in query.
  *
  * @param WP_Block $block Block instance.
  * @param int      $page  Current query's page.
@@ -2418,6 +2419,52 @@ function build_query_vars_from_query_block( $block, $page ) {
 						'include_children' => false,
 					);
 				}
+			}
+		}
+		if ( ! empty( $block->context['query']['format'] ) ) {
+			$query['format'] = $block->context['query']['format'];
+			$tax_query = array( 'relation' => 'OR' );
+
+			// The default post format, 'standard', is not stored in the database.
+			// If 'standard' is part of the request, the query needs to exclude all post items that
+			// have a format assigned.
+			if ( in_array( 'standard', $formats, true ) ) {
+				$tax_query[] = array(
+					'taxonomy' => 'post_format',
+					'field'    => 'slug',
+					'terms'    => array(),
+					'operator' => 'NOT EXISTS',
+				);
+				// Remove the standard format, since it cannot be queried.
+				unset( $formats[ array_search( 'standard', $formats, true ) ] );
+
+				// Add any remaining formats to the tax query.
+				if ( ! empty( $formats ) ) {
+					// Add the post-format- prefix.
+					$terms = array_map(
+						static function ( $format ) {
+							return 'post-format-' . $format;
+						},
+						$formats
+					);
+
+					$tax_query[] = array(
+						'taxonomy' => 'post_format',
+						'field'    => 'slug',
+						'terms'    => $terms,
+						'operator' => 'IN',
+					);
+				}
+			}
+
+			if ( ! isset( $query['tax_query'] ) ) {
+				$query['tax_query'] = array();
+			}
+
+			// This condition is intended to prevent $tax_query from being added to $query
+			// if it only contains the relation.
+			if ( count( $tax_query ) > 1 ) {
+				$query['tax_query'][] = $tax_query;
 			}
 		}
 		if (
--- src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php
@@ -339,6 +339,56 @@ class WP_REST_Posts_Controller extends WP_REST_Controller {
 
 		$args = $this->prepare_tax_query( $args, $request );
 
+		if ( ! empty( $request['format'] ) ) {
+			// If format is not an array, convert it to an array so that the
+			// required prefix can be added to all items.
+			$formats = is_array( $request['format'] ) ? $request['format'] : array( $request['format'] );
+
+			$tax_query = array( 'relation' => 'OR' );
+
+			// The default post format, 'standard', is not stored in the database.
+			// If 'standard' is part of the request, the query needs to exclude all post items that
+			// have a format assigned.
+			if ( in_array( 'standard', $formats, true ) ) {
+				$tax_query[] = array(
+					'taxonomy' => 'post_format',
+					'field'    => 'slug',
+					'terms'    => array(),
+					'operator' => 'NOT EXISTS',
+				);
+				// Remove the standard format, since it cannot be queried.
+				unset( $formats[ array_search( 'standard', $formats, true ) ] );
+			}
+
+			// Add any remaining formats to the tax query.
+			if ( ! empty( $formats ) ) {
+				// Add the post-format- prefix.
+				$terms = array_map(
+					static function ( $format ) {
+						return 'post-format-' . $format;
+					},
+					$formats
+				);
+
+				$tax_query[] = array(
+					'taxonomy' => 'post_format',
+					'field'    => 'slug',
+					'terms'    => $terms,
+					'operator' => 'IN',
+				);
+			}
+
+			// Enable filtering by both post formats and other taxonomies by combining them with AND.
+			if ( isset( $args['tax_query'] ) ) {
+				$args['tax_query'][] = array(
+					'relation' => 'AND',
+					$tax_query,
+				);
+			} else {
+				$args['tax_query'] = $tax_query;
+			}
+		}
+
 		// Force the post_type argument, since it's not a user input variable.
 		$args['post_type'] = $this->post_type;
 
@@ -2976,6 +3026,17 @@ class WP_REST_Posts_Controller extends WP_REST_Controller {
 			$query_params['sticky'] = array(
 				'description' => __( 'Limit result set to items that are sticky.' ),
 				'type'        => 'boolean',
+			);
+		}
+
+		if ( post_type_supports( $this->post_type, 'post-formats' ) ) {
+			$query_params['format'] = array(
+				'description' => __( 'Limit result set to items assigned one or more given formats.' ),
+				'type'        => 'array',
+				'items'       => array(
+					'enum' => array_values( get_post_format_slugs() ),
+					'type' => 'string',
+				),
 			);
 		}
 
