09d39d240b9a930192993a811ae49b1ddbe3e326
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-09-10 13:04:28 +0200

    Fix another wrong docblock comment

--- src/wp-includes/block-template.php
+++ src/wp-includes/block-template.php
@@ -385,7 +385,8 @@ function wp_register_block_template( $template_name, $args = array() ) {
  * Unregister a block template.
  *
  * @param string $template_name Template name in the form of `plugin_uri//template_name`.
- * @return WP_Block_Template|WP_Error True on success, WP_Error on failure or if the template doesn't exist.
+ * @return WP_Block_Template|WP_Error The unregistered template object on success, WP_Error object on failure or if the
+ *                                    template doesn't exist.
  *
  * @since 6.7.0
  */
