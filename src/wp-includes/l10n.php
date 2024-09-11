327e65dcc8472198987ba8624a65b14bad123a15
Louis Wolmarans <louiswol94@gmail.com>
2024-09-11 11:52:07 +0200

    Code review

--- src/wp-includes/l10n.php
+++ src/wp-includes/l10n.php
@@ -1991,10 +1991,12 @@ function wp_get_word_count_type() {
 
 /**
  * Returns a boolean to indicate whether a translation exists for a given string with optional text domain and locale.
+ * 
+ * @since 6.7.0
  *
- * @param string $singular  Singular translation to check.
- * @param string $textdomain  Optional. Text domain. Default 'default'.
- * @param ?string $locale  Optional. Locale. Default current locale.
+ * @param string  $singular   Singular translation to check.
+ * @param string  $textdomain Optional. Text domain. Default 'default'.
+ * @param ?string $locale     Optional. Locale. Default current locale.
  * @return bool  True if the translation exists, false otherwise.
  */
 function has_translation( string $singular, string $textdomain = 'default', ?string $locale = null ): bool {
--- src/wp-includes/l10n/class-wp-translation-controller.php
+++ src/wp-includes/l10n/class-wp-translation-controller.php
@@ -437,10 +437,12 @@ final class WP_Translation_Controller {
 
 	/**
 	 * Returns a boolean to indicate whether a translation exists for a given string with optional text domain and locale.
+	 * 
+	 * @since 6.7.0
 	 *
-	 * @param string $singular  Singular translation to check.
-	 * @param string $textdomain  Optional. Text domain. Default 'default'.
-	 * @param ?string $locale  Optional. Locale. Default current locale.
+	 * @param string  $singular   Singular translation to check.
+	 * @param string  $textdomain Optional. Text domain. Default 'default'.
+	 * @param ?string $locale     Optional. Locale. Default current locale.
 	 * @return bool  True if the translation exists, false otherwise.
 	 */
 	public function has_translation( string $singular, string $textdomain = 'default', ?string $locale = null ): bool {
