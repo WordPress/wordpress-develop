d8ac3610365caf54b86ccb75cf9b3390c0e49c2f
Jon Surrell <sirreal@users.noreply.github.com>
2024-09-12 17:53:03 +0200

    Add spawn_fragment_parser method

--- src/wp-includes/html-api/class-wp-html-processor.php
+++ src/wp-includes/html-api/class-wp-html-processor.php
@@ -425,6 +425,55 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 	}
 
 	/**
+	 * Creates a fragment processor with the current node as its context element.
+	 *
+	 * @see https://html.spec.whatwg.org/multipage/parsing.html#html-fragment-parsing-algorithm
+	 *
+	 * @param string $html     Input HTML fragment to process.
+	 * @return static|null     The created processor if successful, otherwise null.
+	 */
+	private function spawn_fragment_parser( string $html ): ?self {
+		if ( $this->get_token_type() !== '#tag' ) {
+			return null;
+		}
+
+		/*
+		 * Prevent creating fragments at "self-contained" nodes.
+		 *
+		 * @see https://github.com/WordPress/wordpress-develop/pull/7141
+		 * @see https://github.com/WordPress/wordpress-develop/pull/7198
+		 */
+		if (
+			'html' === $this->get_namespace() &&
+			in_array( $this->get_tag(), array( 'IFRAME', 'NOEMBED', 'NOFRAMES', 'SCRIPT', 'STYLE', 'TEXTAREA', 'TITLE', 'XMP' ), true )
+		) {
+			return null;
+		}
+
+		$fragment_processor              = self::create_fragment( $html );
+		$fragment_processor->compat_mode = $this->compat_mode;
+
+		// @todo The context element probably needs a namespace{
+		$context_element = array( $this->get_tag(), array() );
+		foreach ( $this->get_attribute_names_with_prefix( '' ) as $name => $value ) {
+			$context_element[1][ $name ] = $value;
+		}
+		$fragment_processor->state->context_node = $context_element;
+
+		if ( 'TEMPLATE' === $context_element[0] ) {
+			$fragment_processor->state->stack_of_template_insertion_modes[] = WP_HTML_Processor_State::INSERTION_MODE_IN_TEMPLATE;
+		}
+
+		$fragment_processor->reset_insertion_mode_appropriately();
+
+		// @todo Set the parser's form element pointer.
+
+		$fragment_processor->state->encoding_confidence = 'irrelevant';
+
+		return $fragment_processor;
+	}
+
+	/**
 	 * Stops the parser and terminates its execution when encountering unsupported markup.
 	 *
 	 * @throws WP_HTML_Unsupported_Exception Halts execution of the parser.
