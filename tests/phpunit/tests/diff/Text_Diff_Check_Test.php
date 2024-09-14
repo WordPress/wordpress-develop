f4804bcdeeb0498a2f6ce2ffa07600d41fba23c5
jrfnl <jrfnl@users.noreply.github.com>
2024-09-14 15:35:01 +0200

    PHP 8.4 | Text_Diff::_check(): fix trigger_error() with E_USER_ERROR is deprecated
    PHP 8.4 deprecates the use of `trigger_errror()` with `E_USER_ERROR` as the error level, as there are a number of gotchas to this way of creating a `Fatal Error` (`finally` blocks not executing, destructors not executing).
    The recommended replacements are either to use exceptions or to do a hard `exit`.
    
    Considering this is an unmaintained external dependency, I'm propose we fix this in the WP specific copy of the dependency.
    
    Now, there were basically three options:
    * Silence the deprecation until PHP 9.0 and delay properly solving this until then.
        This would lead to an awkward solution, as prior to PHP 8.0, error silencing would apply to all errors, while, as of PHP 8.0, it will no longer apply to fatal errors.
        It also would only buy us some time and wouldn't actually solve anything.
    * Use `exit($status)`.
        This would make the code untestable and would disable handling of these errors via custom error handlers, which makes this an undesirable solution.
    * Throw an exception.
        This makes for the most elegant solution with the least BC-breaking impact.
    
    This commit implements the third option. It introduces a new `Text_Exception` class and starts using that in the `Text_Diff::_check()` method in all applicable places.
    
    It also adds tests for the first two error conditions.
    
    Unfortunately, I have not been able to get a test set up to test the other three exceptions, as without hacking the object under test, i.e. manually editing the `$_edits` property between the instantiating the object and running the `_check()`, I have not found a way to trigger those exceptions.
    
    Ref:
    * https://wiki.php.net/rfc/deprecations_php_8_4#deprecate_passing_e_user_error_to_trigger_error
    * https://www.php.net/manual/en/migration80.incompatible.php

--- tests/phpunit/tests/diff/Text_Diff_Check_Test.php
+++ tests/phpunit/tests/diff/Text_Diff_Check_Test.php
@@ -23,6 +23,7 @@ final class Text_Diff_Check_Test extends WP_UnitTestCase {
 
 	public static function set_up_before_class() {
 		require_once ABSPATH . 'wp-includes/Text/Diff.php';
+		require_once ABSPATH . 'wp-includes/Text/Exception.php';
 	}
 
 	/**
@@ -33,5 +34,21 @@ final class Text_Diff_Check_Test extends WP_UnitTestCase {
 	public function test_check_passes_when_passed_same_input() {
 		$diff = new Text_Diff( 'auto', array( self::FILE_A, self::FILE_B ) );
 		$this->assertTrue( $diff->_check( self::FILE_A, self::FILE_B ) );
+	}
+
+	public function test_check_throws_exception_when_from_is_not_same_as_original() {
+		$this->expectException( Text_Exception::class );
+		$this->expectExceptionMessage( 'Reconstructed original does not match' );
+
+		$diff = new Text_Diff( 'auto', array( self::FILE_A, self::FILE_B ) );
+		$diff->_check( self::FILE_B, self::FILE_B );
+	}
+
+	public function test_check_throws_exception_when_to_is_not_same_as_final() {
+		$this->expectException( Text_Exception::class );
+		$this->expectExceptionMessage( 'Reconstructed final does not match' );
+
+		$diff = new Text_Diff( 'auto', array( self::FILE_A, self::FILE_B ) );
+		$diff->_check( self::FILE_A, self::FILE_A );
 	}
 }
