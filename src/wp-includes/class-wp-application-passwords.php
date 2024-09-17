2baeb1990dfc0447a2e424688e497fa9862bc11d
John Blackbourn <johnbillion@gmail.com>
2024-09-17 13:19:45 -0700

    Allow either the name or password to change in order to allow an application password to get updated.

--- src/wp-includes/class-wp-application-passwords.php
+++ src/wp-includes/class-wp-application-passwords.php
@@ -279,17 +279,27 @@ class WP_Application_Passwords {
 			}
 
 			if ( ! empty( $update['name'] ) ) {
-				$item['name'] = sanitize_text_field( $update['name'] );
+				$update['name'] = sanitize_text_field( $update['name'] );
 			}
 
-			if ( ! empty( $update['password'] ) ) {
+			$save = false;
+
+			if ( ! empty( $update['name'] ) && $item['name'] !== $update['name'] ) {
+				$item['name'] = $update['name'];
+				$save         = true;
+			}
+
+			if ( ! empty( $update['password'] ) && $item['password'] !== $update['password'] ) {
 				$item['password'] = $update['password'];
+				$save             = true;
 			}
 
-			$saved = static::set_user_application_passwords( $user_id, $passwords );
+			if ( $save ) {
+				$saved = static::set_user_application_passwords( $user_id, $passwords );
 
-			if ( ! $saved ) {
-				return new WP_Error( 'db_error', __( 'Could not save application password.' ) );
+				if ( ! $saved ) {
+					return new WP_Error( 'db_error', __( 'Could not save application password.' ) );
+				}
 			}
 
 			/**
