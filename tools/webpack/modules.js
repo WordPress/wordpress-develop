d6de7d0ac5c743d15f90a5d7ce561291633c66d3
Jon Surrell <sirreal@users.noreply.github.com>
2024-09-17 18:48:13 +0200

    Prepare build for more script modules

--- tools/webpack/modules.js
+++ tools/webpack/modules.js
@@ -1,4 +1,10 @@
 /**
+ * External dependencies
+ */
+const { createRequire } = require( 'node:module' );
+const { dirname } = require( 'node:path' );
+
+/**
  * WordPress dependencies
  */
 const DependencyExtractionPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
@@ -13,6 +19,53 @@ const {
 	MODULES,
 	WORDPRESS_NAMESPACE,
 } = require( './shared' );
+
+/** @type {Map<string, string>} */
+const scriptModules = new Map();
+for ( const packageName of MODULES ) {
+	const packageRequire = createRequire(
+		`${dirname( require.resolve( `${ packageName }/package.json` ) )}/`
+	);
+
+	const depPackageJson = packageRequire( './package.json' );
+	if ( ! Object.hasOwn( depPackageJson, 'wpScriptModuleExports' ) ) {
+		continue;
+	}
+
+	const moduleName = packageName.substring( WORDPRESS_NAMESPACE.length );
+	let { wpScriptModuleExports } = depPackageJson;
+
+	// Special handling for { "wpScriptModuleExports": "./build-module/index.js" }.
+	if ( typeof wpScriptModuleExports === 'string' ) {
+		wpScriptModuleExports = { '.': wpScriptModuleExports };
+	}
+
+	if ( Object.getPrototypeOf( wpScriptModuleExports ) !== Object.prototype ) {
+		throw new Error( 'wpScriptModuleExports must be an object' );
+	}
+
+	for ( const [ exportName, exportPath ] of Object.entries(
+		wpScriptModuleExports
+	) ) {
+		if ( typeof exportPath !== 'string' ) {
+			throw new Error( 'wpScriptModuleExports paths must be strings' );
+		}
+
+		if ( ! exportPath.startsWith( './' ) ) {
+			throw new Error(
+				'wpScriptModuleExports paths must start with "./"'
+			);
+		}
+
+		const name =
+			exportName === '.' ? 'index' : exportName.replace( /^\.\/?/, '' );
+
+		scriptModules.set(
+			`${ moduleName }/${ name }`,
+			packageRequire.resolve( exportPath )
+		);
+	}
+}
 
 module.exports = function (
 	env = { environment: 'production', watch: false, buildTarget: false }
@@ -29,45 +82,26 @@ module.exports = function (
 	const baseConfig = getBaseConfig( env );
 	const config = {
 		...baseConfig,
-		entry: MODULES.map( ( packageName ) =>
-			packageName.replace( WORDPRESS_NAMESPACE, '' )
-		).reduce( ( memo, packageName ) => {
-			const path =
-				'development' === mode && 'interactivity' === packageName
-					? 'interactivity/build-module/debug'
-					: packageName;
-			memo[ packageName ] = {
-				import: normalizeJoin(
-					baseDir,
-					`node_modules/@wordpress/${ path }`
-				),
-			};
-
-			return memo;
-		}, {} ),
+		entry: Object.fromEntries( scriptModules.entries()),
 		experiments: {
 			outputModule: true,
 		},
 		output: {
 			devtoolNamespace: 'wp',
 			filename: `[name]${ suffix }.js`,
-			path: normalizeJoin( baseDir, `${ buildTarget }/js/dist` ),
+			path: normalizeJoin( baseDir, `${ buildTarget }/js/dist/script-modules` ),
 			library: {
 				type: 'module',
 			},
 			environment: { module: true },
-		},
-		externalsType: 'module',
-		externals: {
-			'@wordpress/interactivity': '@wordpress/interactivity',
-			'@wordpress/interactivity-router':
-				'import @wordpress/interactivity-router',
+			module: true,
+			chunkFormat: 'module',
+			asyncChunks: false,
 		},
 		plugins: [
 			...baseConfig.plugins,
 			new DependencyExtractionPlugin( {
 				injectPolyfill: false,
-				useDefaults: false,
 			} ),
 		],
 	};
