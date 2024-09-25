5266258d001a54dbb326433ab005628d92193b1a
Debarghya Banerjee <debabanerjee31@gmail.com>
2024-09-25 20:21:11 +0530

    Fix unit test and sprintf placeholders

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -2564,7 +2564,7 @@ if ( ! function_exists( 'wp_hash' ) ) :
 		if ( ! in_array( $algo, hash_hmac_algos(), true ) ) {
 			throw new InvalidArgumentException(
 				sprintf(
-					'Unsupported hashing algorithm: %s. Supported algorithms are: %s',
+					'Unsupported hashing algorithm: %1$s. Supported algorithms are: %2$s',
 					$algo,
 					implode( ', ', hash_hmac_algos() )
 				)
