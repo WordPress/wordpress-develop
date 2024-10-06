f108b0bb344cdb900c0c107a0b45577df19d8eea
Brad Veryard <veryard@users.noreply.github.com>
2024-10-06 22:58:29 +0800

    chore: Add back empty line

--- src/wp-admin/customize.php
+++ src/wp-admin/customize.php
@@ -83,6 +83,7 @@ if ( $wp_customize->changeset_post_id() ) {
 		);
 	}
 }
+
 $url       = ! empty( $_REQUEST['url'] ) ? esc_url_raw( wp_unslash( $_REQUEST['url'] ) ) : '';
 $return    = ! empty( $_REQUEST['return'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['return'] ) ) : '';
 $autofocus = ! empty( $_REQUEST['autofocus'] ) && is_array( $_REQUEST['autofocus'] )
