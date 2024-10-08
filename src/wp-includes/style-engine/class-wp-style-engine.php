8eb5431bf74dd28ae9a5b5260942a463e75744f9
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2024-10-08 12:28:56 +1100

    Add writingMode to the style engine, to support dynamic blocks

--- src/wp-includes/style-engine/class-wp-style-engine.php
+++ src/wp-includes/style-engine/class-wp-style-engine.php
@@ -305,6 +305,12 @@ final class WP_Style_Engine {
 				),
 				'path'          => array( 'typography', 'letterSpacing' ),
 			),
+			'writingMode'    => array(
+				'property_keys' => array(
+					'default' => 'writing-mode',
+				),
+				'path'          => array( 'typography', 'writingMode' ),
+			),
 		),
 	);
 
