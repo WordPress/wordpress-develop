430090d86b76d307326fdcf823eac2021e702d3a
Debarghya Banerjee <debabanerjee31@gmail.com>
2024-10-08 21:00:55 +0530

    Add  as a class field

--- src/wp-admin/includes/class-ftp.php
+++ src/wp-admin/includes/class-ftp.php
@@ -120,6 +120,8 @@ class ftp_base {
 	var $_eol_code;
 	var $AutoAsciiExt;
 
+	public $features;
+
 	/* Constructor */
 	function __construct($port_mode=FALSE, $verb=FALSE, $le=FALSE) {
 		$this->LocalEcho=$le;
