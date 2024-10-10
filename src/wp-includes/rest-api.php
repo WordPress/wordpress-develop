c052e3612380532f18b7e6f6856ffb96a02a65db
Pascal Birchler <pascalb@google.com>
2024-10-10 13:42:33 +0200

    Remove errant space

--- src/wp-includes/rest-api.php
+++ src/wp-includes/rest-api.php
@@ -2920,7 +2920,7 @@ function rest_preload_api_request( $memo, $path ) {
 	if ( isset( $path_parts['path'] ) && '/' !== $path_parts['path'] ) {
 		// Remove trailing slashes from the "path" part of the REST API path.
 		$path_parts['path'] = untrailingslashit( $path_parts['path'] );
-		$path               = str_contains ( $path, '?' ) ?
+		$path               = str_contains( $path, '?' ) ?
 			$path_parts['path'] . '?' . $path_parts['query'] :
 			$path_parts['path'];
 	}
