ec04a46a1ce41b134ee1dc43f69a458c6b31f802
Debarghya Banerjee <debabanerjee31@gmail.com>
2024-10-21 22:38:43 +0530

    Fix the DOC block comments

--- src/wp-includes/https-detection.php
+++ src/wp-includes/https-detection.php
@@ -63,6 +63,11 @@ function wp_is_site_url_using_https() {
 /**
  * Checks whether HTTPS is supported for the server and domain.
  *
+ * This function makes an HTTP request through `wp_get_https_detection_errors()`
+ * to check for HTTPS support. As this process can be resource-intensive,
+ * it should be used cautiously, especially in performance-sensitive environments,
+ * to avoid potential latency issues.
+ *
  * @since 5.7.0
  *
  * @return bool True if HTTPS is supported, false otherwise.
@@ -77,10 +82,14 @@ function wp_is_https_supported() {
 /**
  * Runs a remote HTTPS request to detect whether HTTPS supported, and stores potential errors.
  *
- * This internal function is called by a regular Cron hook to ensure HTTPS support is detected and maintained.
+ * This function checks for HTTPS support by making an HTTP request. As this process can be resource-intensive,
+ * it should be used cautiously, especially in performance-sensitive environments.
+ * It is called when HTTPS support needs to be validated.
  *
  * @since 6.4.0
  * @access private
+ *
+ * @return array An array containing potential detection errors related to HTTPS, or an empty array if no errors are found.
  */
 function wp_get_https_detection_errors() {
 	/**
