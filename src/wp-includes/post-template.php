d6c7837e8e72abaa8e69cae3467893cd539b739e
John Blackbourn <johnbillion@gmail.com>
2024-10-22 23:09:01 +0100

    Add tests for post password handling.

--- src/wp-includes/post-template.php
+++ src/wp-includes/post-template.php
@@ -865,7 +865,6 @@ function get_body_class( $css_class = '' ) {
  * Determines whether the post requires password and whether a correct password has been provided.
  *
  * @since 2.7.0
- * @TODO test coverage
  *
  * @param int|WP_Post|null $post An optional post. Global $post used if not provided.
  * @return bool false if a password is not required or the correct password cookie is present, true otherwise.
