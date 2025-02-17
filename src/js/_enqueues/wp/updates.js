40c45c2a0c2b5a12751ab773953a10ae7c2ccf1d
shail-mehta <shailmehta25@gmail.com>
2025-02-17 21:38:43 +0530

    Bulk Update no Longer Working When 1 Plugin Failed Update

--- src/js/_enqueues/wp/updates.js
+++ src/js/_enqueues/wp/updates.js
@@ -629,6 +629,7 @@
 	wp.updates.updatePluginError = function( response ) {
 		var $pluginRow, $card, $message, errorMessage, buttonText, ariaLabel,
 			$adminBarUpdates = $( '#wp-admin-bar-updates' );
+			isPluginInstall = 'plugin-install' === pagenow || 'plugin-install-network' === pagenow;
 
 		if ( ! wp.updates.isValidResponse( response, 'update' ) ) {
 			return;
@@ -667,7 +668,7 @@
 			} else {
 				$message.find( 'p' ).removeAttr( 'aria-label' );
 			}
-		} else if ( 'plugin-install' === pagenow || 'plugin-install-network' === pagenow ) {
+		} else if ( isPluginInstall ) {
 			buttonText = __( 'Update failed.' );
 
 			$card = $( '.plugin-card-' + response.slug + ', #plugin-information-footer' )
@@ -716,7 +717,7 @@
 
 		wp.a11y.speak( errorMessage, 'assertive' );
 
-		if ( 'plugin-information-footer' === $card.attr('id' ) ) {
+		if ( isPluginInstall && 'plugin-information-footer' === $card.attr( 'id' ) ) {
 			wp.updates.setCardButtonStatus(
 				{
 					status: 'plugin-update-failed',
