b914e6a32167145ec9434486a10a4a1b48015755
himanshupathak95 <himanshu.pathak@rtcamp.com>
2025-02-24 21:13:17 +0530

    Nav Menu: Replace tab index with type button

--- src/wp-includes/class-wp-customize-nav-menus.php
+++ src/wp-includes/class-wp-customize-nav-menus.php
@@ -1145,7 +1145,7 @@ final class WP_Customize_Nav_Menus {
 		?>
 		<div id="available-menu-items" class="accordion-container">
 			<div class="customize-section-title">
-				<button type="button" class="customize-section-back" tabindex="-1">
+				<button type="button" class="customize-section-back" type="button">
 					<span class="screen-reader-text">
 						<?php
 						/* translators: Hidden accessibility text. */
