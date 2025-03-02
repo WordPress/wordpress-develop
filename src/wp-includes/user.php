b3147521ebf54f2827977e58d777b5dd93df12a9
Peter Wilson <git-commit-by@peterwilson.cc>
2025-03-03 08:12:30 +1100

    Use absint in case wp_parse_id_list changes.

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -682,8 +682,7 @@ function count_many_users_posts( $users, $post_type = 'post', $public_only = fal
 		return $pre;
 	}
 
-	$users    = wp_parse_id_list( $users );
-	$userlist = implode( ',', $users );
+	$userlist = implode( ',', array_map( 'absint', $users ) );
 	$where    = get_posts_by_author_sql( $post_type, true, null, $public_only );
 
 	$result = $wpdb->get_results( "SELECT post_author, COUNT(*) FROM $wpdb->posts $where AND post_author IN ($userlist) GROUP BY post_author", ARRAY_N );
