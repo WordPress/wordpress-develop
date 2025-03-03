3080ae76a7cbd10664f7d5240cc3b871c6731ca4
Joe Dolson <design@joedolson.com>
2025-03-03 15:41:18 -0600

    Match comment voice.

--- src/wp-content/themes/twentynineteen/inc/template-functions.php
+++ src/wp-content/themes/twentynineteen/inc/template-functions.php
@@ -181,7 +181,7 @@ add_filter( 'wp_nav_menu', 'twentynineteen_add_ellipses_to_nav', 10, 2 );
  */
 function twentynineteen_nav_menu_link_attributes( $atts, $item, $args ) {
 
-	// Checks if this is the primary menu.
+	// Check that this is the primary menu.
 	if ( isset( $args->theme_location ) && 'menu-1' === $args->theme_location ) {
 		// Add [aria-haspopup] and [aria-expanded] to menu items that have children.
 		$item_has_children = in_array( 'menu-item-has-children', $item->classes, true );
