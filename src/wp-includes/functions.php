093d9aaacd90c5e540e6b7685bf911395daa85e9
Christoph Daum <christoph.daum@coding-pioneers.com>
2025-03-03 08:36:25 +0100

    docs: Fixed wrong typehint for param $data in is_serialized()

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -674,8 +674,8 @@ function maybe_unserialize( $data ) {
  * @since 2.0.5
  * @since 6.1.0 Added Enum support.
  *
- * @param string $data   Value to check to see if was serialized.
- * @param bool   $strict Optional. Whether to be strict about the end of the string. Default true.
+ * @param mixed $data   Value to check to see if was serialized.
+ * @param bool  $strict Optional. Whether to be strict about the end of the string. Default true.
  * @return bool False if not serialized and true if it was.
  */
 function is_serialized( $data, $strict = true ) {
