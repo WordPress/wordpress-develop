f1046d238c47a0efc6b9e48b837edaa6f2a6a3be
Peter Wilson <git-commit-by@peterwilson.cc>
2025-03-04 09:11:03 +1100

    Revert "Widgets: Fix `get_calendar()` related PHPUnit tests."
    This reverts commit d55d8dcffa7ffb4370015096dfa6dc08e11d0300.

--- tests/phpunit/tests/general/getCalendar.php
+++ tests/phpunit/tests/general/getCalendar.php
@@ -69,6 +69,8 @@ class Tests_General_GetCalendar extends WP_UnitTestCase {
 		remove_all_filters( 'get_calendar_args' );
 
 		$this->assertStringContainsString( '<table id="wp-calendar"', $calendar_html );
+		$this->assertStringContainsString( 'Posts published on February 3, 2025', $calendar_html );
+		$this->assertStringContainsString( 'February 2025', $calendar_html );
 	}
 
 	/**
