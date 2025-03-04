a65f4cee7e0a3a58b41b41827d76d29f13bb6e2b
Rohan Jha <nickeyjha2@gmail.com>
2025-03-04 17:11:32 +0530

    Refactor  tests to use dataProvider

--- tests/phpunit/tests/formatting/isEmail.php
+++ tests/phpunit/tests/formatting/isEmail.php
@@ -6,31 +6,50 @@
  * @covers ::is_email
  */
 class Tests_Formatting_IsEmail extends WP_UnitTestCase {
-	public function test_returns_the_email_address_if_it_is_valid() {
-		$data = array(
-			'bob@example.com',
-			'phil@example.info',
-			'ace@204.32.222.14',
-			'kevin@many.subdomains.make.a.happy.man.edu',
-			'a@b.co',
-			'bill+ted@example.com',
+
+	/**
+	 * Data provider for valid email addresses.
+	 *
+	 * @return array
+	 */
+	public function valid_email_provider() {
+		return array(
+			'valid email 1' => array( 'bob@example.com' ),
+			'valid email 2' => array( 'phil@example.info' ),
+			'valid email 3' => array( 'ace@204.32.222.14' ),
+			'valid email 4' => array( 'kevin@many.subdomains.make.a.happy.man.edu' ),
+			'valid email 5' => array( 'a@b.co' ),
+			'valid email 6' => array( 'bill+ted@example.com' ),
 		);
-		foreach ( $data as $datum ) {
-			$this->assertSame( $datum, is_email( $datum ), $datum );
-		}
 	}
 
-	public function test_returns_false_if_given_an_invalid_email_address() {
-		$data = array(
-			'khaaaaaaaaaaaaaaan!',
-			'http://bob.example.com/',
-			"sif i'd give u it, spamer!1",
-			'com.exampleNOSPAMbob',
-			'bob@your mom',
-			'a@b.c',
+	/**
+	 * Data provider for invalid email addresses.
+	 *
+	 * @return array
+	 */
+	public function invalid_email_provider() {
+		return array(
+			'invalid email 1' => array( 'khaaaaaaaaaaaaaaan!' ),
+			'invalid email 2' => array( 'http://bob.example.com/' ),
+			'invalid email 3' => array( "sif i'd give u it, spamer!1" ),
+			'invalid email 4' => array( 'com.exampleNOSPAMbob' ),
+			'invalid email 5' => array( 'bob@your mom' ),
+			'invalid email 6' => array( 'a@b.c' ),
 		);
-		foreach ( $data as $datum ) {
-			$this->assertFalse( is_email( $datum ), $datum );
-		}
+	}
+
+	/**
+	 * @dataProvider valid_email_provider
+	 */
+	public function test_returns_the_email_address_if_it_is_valid( $email ) {
+		$this->assertSame( $email, is_email( $email ), $email );
+	}
+
+	/**
+	 * @dataProvider invalid_email_provider
+	 */
+	public function test_returns_false_if_given_an_invalid_email_address( $email ) {
+		$this->assertFalse( is_email( $email ), $email );
 	}
 }
