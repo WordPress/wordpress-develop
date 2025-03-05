bc4ffaea733545cc333ca8cc8cd616421b20faa5
Joe McGill <joe.mcgill@10up.com>
2025-03-05 15:18:00 -0600

    Revert "Editor: Set new default rendering mode for Pages."
    This reverts commit 83b90808e9b7e0d910c5237bda6b52d863702074.

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -69,11 +69,6 @@ function create_initial_post_types() {
 		)
 	);
 
-	// Enhance page editor for block themes by rendering template and content blocks.
-	if ( wp_is_block_theme() && current_theme_supports( 'block-templates' ) ) {
-		add_post_type_support( 'page', 'editor', array( 'default-mode' => 'template-locked' ) );
-	}
-
 	register_post_type(
 		'attachment',
 		array(
