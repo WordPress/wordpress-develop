92ba5de440ad1fd5f761968ca1bc1fe8d7ba55ac
Joe Dolson <design@joedolson.com>
2025-03-06 16:35:04 -0600

    Match the URL validation from customizer

--- src/js/_enqueues/lib/nav-menu.js
+++ src/js/_enqueues/lib/nav-menu.js
@@ -1122,7 +1122,20 @@
 				errorMessage.hide();
 				urlWrap.removeClass( 'has-error' );
 
-				if ( '' === url || 'https://' === url || 'http://' === url ) {
+				/*
+				 * Allow URLs including:
+				 * - http://example.com/
+				 * - //example.com
+				 * - /directory/
+				 * - ?query-param
+				 * - #target
+				 * - mailto:foo@example.com
+				 *
+				 * Any further validation will be handled on the server when the setting is attempted to be saved,
+				 * so this pattern does not need to be complete.
+				 */
+				urlRegex = /^((\w+:)?\/\/\w.*|\w+:(?!\/\/$)|\/|\?|#)/;
+				if ( ! urlRegex.test( url ) ) {
 					e.preventDefault();
 					urlInput.addClass( 'form-invalid' )
 						.attr( 'aria-invalid', 'true' )
