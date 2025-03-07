f5af84f8c83c0379a090aed8d4c700989ac0d2d5
Peter Wilson <git-commit-by@peterwilson.cc>
2025-03-08 08:08:00 +1100

    Remove commented out code, add comments.

--- tests/e2e/specs/media-upload.test.js
+++ tests/e2e/specs/media-upload.test.js
@@ -11,25 +11,23 @@ import path from 'path';
 test( 'Test dismissing failed upload works correctly', async ({ page, admin }) => {
 	await admin.visitAdminPage( '/media-new.php' );
 
+	// It takes a moment for the multi-file uploader to become available.
 	await page.waitForLoadState('load');
 
 	const testImagePath = path.join(__dirname, '../assets/sample.svg');
 	console.log( 'testImagePath', testImagePath );
 
+	// Upload a file that will fail.
 	const input = page.locator( '#plupload-upload-ui input[type="file"]' );
 	await input.setInputFiles( testImagePath );
 
-	// await page.waitForEvent('networkidle');
-
-	// await dragAndDropFile(page, "#plupload-upload-ui", testImagePath, "sample.svg");
-
+	// Ensure the error message is visible.
 	await expect(
 		page.getByText('“sample.svg” has failed to upload.')
 	).toBeVisible();
 
-
+	// Ensure the error message is dismissed.
 	await page.getByRole('button', { name: 'Dismiss' }).click();
-
 	await expect(
 		page.getByText('“sample.svg” has failed to upload.')
 	).not.toBeVisible();
