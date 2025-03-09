f82fe884417b82c87ae131e851cfeb582ceca259
Debarghya Banerjee <debabanerjee31@gmail.com>
2025-03-10 00:31:53 +0530

    Update test and send static value based on if dst

--- tests/phpunit/tests/functions/wpTimezoneOverrideOffset.php
+++ tests/phpunit/tests/functions/wpTimezoneOverrideOffset.php
@@ -33,22 +33,28 @@ class Tests_Functions_wpTimezoneOverrideOffset extends WP_UnitTestCase {
 			'bad option set'                => array( 'BAD_TIME_ZONE', false ),
 			'UTC option set'                => array( 'UTC', 0.0 ),
 			'EST option set'                => array( 'EST', -5.0 ),
-			'NST option set'                => array( 'America/St_Johns', $this->get_expected_offset( 'America/St_Johns' ) ),
+			'NST option set'                => array( 'America/St_Johns', $this->get_timezone_offset_based_on_dst( 'America/St_Johns' ) ),
 		);
 	}
 
 	/**
-	 * Gets the expected timezone offset in hours for a given timezone.
+	 * Determines the current timezone offset based on daylight saving time (DST).
 	 *
-	 * This function determines the current offset (including daylight saving time adjustments)
-	 * for the provided timezone string.
+	 * This function checks if the provided timezone is currently observing DST
+	 * and returns the corresponding offset in hours.
 	 *
 	 * @param string $timezone_string The timezone identifier (e.g., 'America/St_Johns').
-	 * @return float The current timezone offset in hours.
+	 * @return float The timezone offset in hours (-3.5 for standard time, -2.5 for DST).
 	 */
-	private function get_expected_offset( $timezone_string ) {
+	private function get_timezone_offset_based_on_dst( $timezone_string ) {
 		$timezone = new DateTimeZone( $timezone_string );
-		$datetime = new DateTime( 'now', $timezone );
-		return $timezone->getOffset( $datetime ) / 3600;
+		$timestamp = time();
+		$transitions = $timezone->getTransitions( $timestamp, $timestamp );
+
+		if ( false === $transitions || ! is_array( $transitions ) || ! isset( $transitions[0]['isdst'] ) ) {
+			return -3.5;
+		}
+
+		return $transitions[0]['isdst'] ? -2.5 : -3.5;
 	}
 }
