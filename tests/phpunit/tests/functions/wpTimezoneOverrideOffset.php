3aa03e69d774a8270d420661c8df6632b18c73e4
Debarghya Banerjee <debabanerjee31@gmail.com>
2025-03-09 20:51:49 +0530

    Fix failing test_wp_timezone_override_offset

--- tests/phpunit/tests/functions/wpTimezoneOverrideOffset.php
+++ tests/phpunit/tests/functions/wpTimezoneOverrideOffset.php
@@ -33,7 +33,22 @@ class Tests_Functions_wpTimezoneOverrideOffset extends WP_UnitTestCase {
 			'bad option set'                => array( 'BAD_TIME_ZONE', false ),
 			'UTC option set'                => array( 'UTC', 0.0 ),
 			'EST option set'                => array( 'EST', -5.0 ),
-			'NST option set'                => array( 'America/St_Johns', -3.5 ),
+			'NST option set'                => array( 'America/St_Johns', $this->get_expected_offset( 'America/St_Johns' ) ),
 		);
+	}
+
+	/**
+	 * Gets the expected timezone offset in hours for a given timezone.
+	 *
+	 * This function determines the current offset (including daylight saving time adjustments)
+	 * for the provided timezone string.
+	 *
+	 * @param string $timezone_string The timezone identifier (e.g., 'America/St_Johns').
+	 * @return float The current timezone offset in hours.
+	 */
+	private function get_expected_offset( $timezone_string ) {
+		$timezone = new DateTimeZone( $timezone_string );
+		$datetime = new DateTime( 'now', $timezone );
+		return $timezone->getOffset( $datetime ) / 3600;
 	}
 }
