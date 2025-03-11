422ab9fde6b5c31f94060a57905a7aee57f2eae4
Sneha <snehapatildeepali@gmail.com>
2025-03-11 19:32:28 +0530

    Fix: Don't apply canonical redirects to URLs with commas in query parameters.

--- src/wp-includes/canonical.php
+++ src/wp-includes/canonical.php
@@ -65,9 +65,8 @@ function redirect_canonical( $requested_url = null, $do_redirect = true ) {
 		return;
 	}
 
-	// Don't redirect URLs with commas in query parameters (Bug #62920)
 	if ( isset( $_SERVER['QUERY_STRING'] ) && strpos( $_SERVER['QUERY_STRING'], ',' ) !== false ) {
-		return false;
+		return;
 	}
 
 	if ( ! $requested_url && isset( $_SERVER['HTTP_HOST'] ) ) {
