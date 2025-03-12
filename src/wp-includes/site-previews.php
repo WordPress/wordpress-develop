cc6a210ffe09808852e5ebbdf40691bd61094562
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-03-12 11:29:57 +0900

    Use IFRAME_REQUEST constant instead of show_admin_bar hook

--- src/wp-includes/site-previews.php
+++ src/wp-includes/site-previews.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Add filter to hide the admin bar.
+ * Initialize site preview.
  *
- * This filter is used to hide the admin bar in classic theme site previews in the site editor.
+ * This function sets IFRAME_REQUEST to true if the site preview parameter is set.
  *
  * @since 6.8.0
  */
 function wp_initialize_site_preview_hooks() {
-	if ( isset( $_GET['wp_site_preview'] ) && 1 === (int) $_GET['wp_site_preview'] ) {
-		add_filter( 'show_admin_bar', '__return_false' );
+	if ( ! defined( 'IFRAME_REQUEST' ) && isset( $_GET['wp_site_preview'] ) && 1 === (int) $_GET['wp_site_preview'] ) {
+		define( 'IFRAME_REQUEST', true );
 	}
 }
