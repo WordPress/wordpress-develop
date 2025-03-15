65be613efeddd19352a0ea39c8416ab532e9fded
shail-mehta <shailmehta25@gmail.com>
2025-03-15 11:09:52 +0530

    Twenty Twelve: functions.php, readme.txt, style.css

--- src/wp-content/themes/twentytwelve/functions.php
+++ src/wp-content/themes/twentytwelve/functions.php
@@ -212,7 +212,7 @@ function twentytwelve_scripts_styles() {
 	}
 
 	// Loads our main stylesheet.
-	wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri(), array(), '20241112' );
+	wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri(), array(), '20250415' );
 
 	// Theme block stylesheet.
 	wp_enqueue_style( 'twentytwelve-block-style', get_template_directory_uri() . '/css/blocks.css', array( 'twentytwelve-style' ), '20240812' );
