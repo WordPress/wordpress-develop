630374658c1951b3fdd5db768388a848a83c002a
Robert Payne <rpayne7264@yahoo.com>
2025-03-16 00:50:37 -0400

    Update comment related to checking if the request is for a file

--- src/wp-includes/template-loader.php
+++ src/wp-includes/template-loader.php
@@ -1,8 +1,10 @@
 <?php
 
-// Check if the request is for a file
+// Check if the request is for a file, and if so, stop further processing
+// This short-circuits WordPress processing for unnecessary file requests, 
+// preventing it from loading the full WordPress environment when only a file is being requested.
 if (wp_is_file_request()) {
-	exit;
+    exit;
 }
 
 /**
