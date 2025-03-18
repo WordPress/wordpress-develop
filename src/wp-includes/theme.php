2c647d67081cf4246b2032a317e34ebfb83580c9
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2025-03-18 14:11:11 +1100

    Check for defintion of theme directories.
    Co-authored-by: Joe McGill <801097+joemcgill@users.noreply.github.com>

--- src/wp-includes/theme.php
+++ src/wp-includes/theme.php
@@ -4352,7 +4352,7 @@ function create_initial_theme_features() {
  * @return bool Whether the active theme is a block-based theme or not.
  */
 function wp_is_block_theme() {
-	if ( ! did_action( 'setup_theme' ) ) {
+	if ( empty( $GLOBALS['wp_theme_directories'] ) ) {
 		_doing_it_wrong( __FUNCTION__, __( 'This function should not be called before themes are set up.' ), '6.8.0' );
 		return false;
 	}
