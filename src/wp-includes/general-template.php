8da7ac7da8c5712e9e19e1263309e6613b0f298f
im3dabasia <eshaan.dabasiya@rtcamp.com>
2025-03-19 13:53:55 +0530

    fix: Fix slash in paginated links while searching

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -4746,7 +4746,7 @@ function paginate_links( $args = '' ) {
 					$link = add_query_arg( $add_args, $link );
 				}
 				$link .= $args['add_fragment'];
-				$link  = get_option( 'permalink_structure' ) ? user_trailingslashit( $link, 'paged' ) : $link;
+				$link  = get_option( 'permalink_structure' ) && ! is_search() ? user_trailingslashit( $link, 'paged' ) : $link;
 
 				$page_links[] = sprintf(
 					'<a class="page-numbers" href="%s">%s</a>',
