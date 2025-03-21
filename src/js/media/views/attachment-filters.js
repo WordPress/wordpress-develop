607f8ef0114f63256401e29728fc5edddddd1a97
Adam Silverstein <adamjs@google.com>
2025-03-21 12:59:47 -0600

    Show icons in media grid filter

--- src/js/media/views/attachment-filters.js
+++ src/js/media/views/attachment-filters.js
@@ -28,8 +28,13 @@ AttachmentFilters = wp.media.View.extend(/** @lends wp.media.view.AttachmentFilt
 
 		// Build `<option>` elements.
 		this.$el.html( _.chain( this.filters ).map( function( filter, value ) {
+			// Potentially add an icon to the option.
+			var filterText = filter.text;
+			if ( filter.props.icon ) {
+				filterText = '<span class="dashicons dashicons-' + filter.props.icon + '"></span> ' + filter.text;
+			}
 			return {
-				el: $( '<option></option>' ).val( value ).html( filter.text )[0],
+				el: $( '<option></option>' ).val( value ).html( filterText )[0],
 				priority: filter.priority || 50
 			};
 		}, this ).sortBy('priority').pluck('el').value() );
