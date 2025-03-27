ec51e4c860d9b6fddcac959981913bac86ad5e2d
Debarghya Banerjee <debabanerjee31@gmail.com>
2025-03-27 11:36:35 +0530

    Add prefix field

--- tests/phpunit/tests/functions/wpUniqueIdFromValues.php
+++ tests/phpunit/tests/functions/wpUniqueIdFromValues.php
@@ -13,6 +13,13 @@
 class Tests_Functions_WpUniqueIdFromValues extends WP_UnitTestCase {
 
 	/**
+	 * Prefix used for testing.
+	 *
+	 * @var string
+	 */
+	private $prefix = 'my-prefix-';
+
+	/**
 	 * Test that the function returns consistent ids for the passed params.
 	 *
 	 * @ticket 62985
@@ -21,17 +28,17 @@ class Tests_Functions_WpUniqueIdFromValues extends WP_UnitTestCase {
 	 *
 	 * @since 6.8.0
 	 */
-	public function test_wp_unique_id_from_values( $data, $prefix ) {
+	public function test_wp_unique_id_from_values( $data ) {
 		// Generate IDs
-		$output1 = wp_unique_id_from_values( $data );
-		$output2 = wp_unique_id_from_values( $data, $prefix );
+		$unique_id_original = wp_unique_id_from_values( $data );
+		$unique_id_prefixed = wp_unique_id_from_values( $data, $this->prefix );
 
 		// Ensure that the same input produces the same ID.
-		$this->assertSame( $output1, wp_unique_id_from_values( $data ) );
-		$this->assertSame( $output2, wp_unique_id_from_values( $data, $prefix ) );
+		$this->assertSame( $unique_id_original, wp_unique_id_from_values( $data ) );
+		$this->assertSame( $unique_id_prefixed, wp_unique_id_from_values( $data, $this->prefix ) );
 
 		// Ensure that the prefixed ID is the prefix + the original ID
-		$this->assertSame( $prefix . $output1, $output2 );
+		$this->assertSame( $this->prefix . $unique_id_original, $unique_id_prefixed );
 	}
 
 	/**
@@ -43,22 +50,22 @@ class Tests_Functions_WpUniqueIdFromValues extends WP_UnitTestCase {
 	 *
 	 * @since 6.8.0
 	 */
-	public function test_wp_unique_id_from_values_uniqueness( $data, $prefix ) {
+	public function test_wp_unique_id_from_values_uniqueness( $data ) {
 		// Generate IDs
-		$output1 = wp_unique_id_from_values( $data );
-		$output2 = wp_unique_id_from_values( $data, $prefix );
+		$unique_id_original = wp_unique_id_from_values( $data );
+		$unique_id_prefixed = wp_unique_id_from_values( $data, $this->prefix );
 
 		// Modify the data slightly to generate a different ID.
 		$data_modified          = $data;
 		$data_modified['value'] = 'modified';
 
 		// Generate new IDs with the modified data
-		$output3 = wp_unique_id_from_values( $data_modified );
-		$output4 = wp_unique_id_from_values( $data_modified, $prefix );
+		$unique_id_modified = wp_unique_id_from_values( $data_modified );
+		$unique_id_prefixed_modified = wp_unique_id_from_values( $data_modified, $this->prefix );
 
 		// Assert that the IDs for different data are distinct
-		$this->assertNotSame( $output1, $output3 );
-		$this->assertNotSame( $output2, $output4 );
+		$this->assertNotSame( $unique_id_original, $unique_id_modified );
+		$this->assertNotSame( $unique_id_prefixed, $unique_id_prefixed_modified );
 	}
 
 	/**
@@ -68,56 +75,32 @@ class Tests_Functions_WpUniqueIdFromValues extends WP_UnitTestCase {
 	 */
 	public function data_wp_unique_id_from_values() {
 		return array(
-			'string'          => array(
-				'data'   => array( 'value' => 'text' ),
-				'prefix' => 'my-prefix-',
-			),
-			'integer'         => array(
-				'data'   => array( 'value' => 123 ),
-				'prefix' => 'my-prefix-',
-			),
-			'float'           => array(
-				'data'   => array( 'value' => 1.23 ),
-				'prefix' => 'my-prefix-',
-			),
-			'boolean'         => array(
-				'data'   => array( 'value' => true ),
-				'prefix' => 'my-prefix-',
-			),
-			'object'          => array(
-				'data'   => array( 'value' => new StdClass() ),
-				'prefix' => 'my-prefix-',
-			),
-			'null'            => array(
-				'data'   => array( 'value' => null ),
-				'prefix' => 'my-prefix-',
-			),
-			'multiple values' => array(
-				'data'   => array(
+			'string'          => array( array( 'value' => 'text' ) ),
+			'integer'         => array( array( 'value' => 123 ) ),
+			'float'           => array( array( 'value' => 1.23 ) ),
+			'boolean'         => array( array( 'value' => true ) ),
+			'object'          => array( array( 'value' => new StdClass() ) ),
+			'null'            => array( array( 'value' => null ) ),
+			'multiple values' => array( array(
+				'value1' => 'text',
+				'value2' => 123,
+				'value3' => 1.23,
+				'value4' => true,
+				'value5' => new StdClass(),
+				'value6' => null,
+			) ),
+			'nested arrays'   => array( array(
+				'list1' => array(
 					'value1' => 'text',
 					'value2' => 123,
 					'value3' => 1.23,
+				),
+				'list2' => array(
 					'value4' => true,
 					'value5' => new StdClass(),
 					'value6' => null,
 				),
-				'prefix' => 'my-prefix-',
-			),
-			'nested arrays'   => array(
-				'data'   => array(
-					'list1' => array(
-						'value1' => 'text',
-						'value2' => 123,
-						'value3' => 1.23,
-					),
-					'list2' => array(
-						'value4' => true,
-						'value5' => new StdClass(),
-						'value6' => null,
-					),
-				),
-				'prefix' => 'my-prefix-',
-			),
+			) ),
 		);
 	}
 
@@ -131,7 +114,7 @@ class Tests_Functions_WpUniqueIdFromValues extends WP_UnitTestCase {
 	 * @since 6.8.0
 	 */
 	public function test_wp_unique_id_from_values_empty_array() {
-		wp_unique_id_from_values( array(), 'my-prefix-' );
+		wp_unique_id_from_values( array(), $this->prefix );
 	}
 
 	/**
@@ -143,10 +126,10 @@ class Tests_Functions_WpUniqueIdFromValues extends WP_UnitTestCase {
 	 *
 	 * @since 6.8.0
 	 */
-	public function test_wp_unique_id_from_values_invalid_data( $data, $prefix ) {
+	public function test_wp_unique_id_from_values_invalid_data( $data ) {
 		$this->expectException( TypeError::class );
 
-		wp_unique_id_from_values( $data, $prefix );
+		wp_unique_id_from_values( $data, $this->prefix );
 	}
 
 	/**
@@ -156,30 +139,13 @@ class Tests_Functions_WpUniqueIdFromValues extends WP_UnitTestCase {
 	 */
 	public function data_wp_unique_id_from_values_invalid_data() {
 		return array(
-			'string'  => array(
-				'data'   => 'text',
-				'prefix' => '',
-			),
-			'integer' => array(
-				'data'   => 123,
-				'prefix' => '',
-			),
-			'float'   => array(
-				'data'   => 1.23,
-				'prefix' => '',
-			),
-			'boolean' => array(
-				'data'   => true,
-				'prefix' => '',
-			),
-			'object'  => array(
-				'data'   => new StdClass(),
-				'prefix' => '',
-			),
-			'null'    => array(
-				'data'   => null,
-				'prefix' => '',
-			),
+			'string'  => array( 'text' ),
+			'integer' => array( 123 ),
+			'float'   => array( 1.23 ),
+			'boolean' => array( true ),
+			'object'  => array( new StdClass() ),
+			'null'    => array( null ),
 		);
 	}
 }
+
