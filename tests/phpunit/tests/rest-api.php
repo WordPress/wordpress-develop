0427dabd004161423a33065ee7501f5e0d88a397
himanshupathak95 <himanshu.pathak@rtcamp.com>
2025-03-29 01:17:53 +0530

    Making dummy changes to trigger the jobs

--- tests/phpunit/tests/rest-api.php
+++ tests/phpunit/tests/rest-api.php
@@ -2580,6 +2580,7 @@ class Tests_REST_API extends WP_UnitTestCase {
 				'The REST route parameter must be a string.',
 				$e->getMessage()
 			);
+			
 			throw $e; // Re-throw to satisfy expectException
 		}
 	}
