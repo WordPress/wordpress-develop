4f697c5c4ad3816c660bd671b4229f5615723bc9
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2025-04-02 21:01:54 +0200

    Fix 63222

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -91,6 +91,8 @@ function get_block_asset_url( $path ) {
 	if ( empty( $path ) ) {
 		return false;
 	}
+	
+	$path = realpath( $path );
 
 	// Path needs to be normalized to work in Windows env.
 	static $wpinc_path_norm = '';
