baa07e0b518c8cfb76b087c03fcaf717195ea6bb
Jonny Harrus <jon@spacedmonkey.co.uk>
2025-04-04 14:31:33 -0400

    Refactor tests to use shared user setup and teardown.
    Replaces inline test user creation with shared class-level setup and cleanup methods for consistency and reduced redundancy. Introduces `wpSetUpBeforeClass` and `wpTearDownAfterClass` for initializing and removing test-specific roles and users.

--- tests/phpunit/tests/rest-api/restUserCanEditPost.php
+++ tests/phpunit/tests/rest-api/restUserCanEditPost.php
@@ -8,12 +8,32 @@
  */
 class Tests_Rest_User_Can_Edit_Post extends WP_UnitTestCase {
 
+	protected static $editor_id;
+	protected static $sub_id;
+	protected static $private_role;
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		self::$editor_id = $factory->user->create( array( 'role' => 'editor' ) );
+		self::$sub_id    = $factory->user->create( array( 'role' => 'subscriber' ) );
+
+		add_role( 'private_reader', 'Private Reader' );
+		$role = get_role( 'private_reader' );
+		$role->add_cap( 'edit_post_baz' );
+		self::$private_role = $factory->user->create( array( 'role' => 'private_reader' ) );
+	}
+
+	public static function wpTearDownAfterClass() {
+		remove_role( 'private_reader' );
+
+		self::delete_user( self::$editor_id );
+		self::delete_user( self::$sub_id );
+		self::delete_user( self::$private_role );
+	}
+
 	/**
 	 * Tests that `rest_user_can_edit_post()` returns true for a user with edit_posts capability.
 	 */
 	public function test_rest_user_can_edit_post() {
-		$user_id = $this->factory()->user->create( array( 'role' => 'editor' ) );
-		wp_set_current_user( $user_id );
+		wp_set_current_user( self::$editor_id );
 
 		$this->assertTrue( rest_user_can_edit_post() );
 	}
@@ -22,8 +42,7 @@ class Tests_Rest_User_Can_Edit_Post extends WP_UnitTestCase {
 	 * Tests that `rest_user_can_edit_post()` returns false for a user without edit_posts capability.
 	 */
 	public function test_rest_user_cannot_edit_post() {
-		$user_id = $this->factory()->user->create( array( 'role' => 'subscriber' ) );
-		wp_set_current_user( $user_id );
+		wp_set_current_user( self::$sub_id );
 
 		$this->assertFalse( rest_user_can_edit_post() );
 	}
@@ -38,10 +57,7 @@ class Tests_Rest_User_Can_Edit_Post extends WP_UnitTestCase {
 	}
 
 	public function test_rest_user_can_edit_custom_type() {
-		$user_id  = $this->factory()->user->create( array( 'role' => 'subscriber' ) );
-		$sub_role = get_role( 'subscriber' );
-		$sub_role->add_cap( 'edit_post_baz' );
-		wp_set_current_user( $user_id );
+		wp_set_current_user( self::$private_role );
 
 		// Create a custom post type and check if the user can edit it.
 		register_post_type(
