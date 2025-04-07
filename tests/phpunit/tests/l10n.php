c090c23a5610a7c59d23579ef86386a207671bc1
Peter Wilson <git-commit-by@peterwilson.cc>
2025-04-07 11:54:47 +1000

    Some shared user fixtures.

--- tests/phpunit/tests/l10n.php
+++ tests/phpunit/tests/l10n.php
@@ -15,7 +15,6 @@ class Tests_L10n extends WP_UnitTestCase {
 	 */
 	private $long_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-
 	/**
 	 * Editor user ID.
 	 *
