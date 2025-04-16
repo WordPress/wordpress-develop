77111430379dd2880ee922609654f22a75a28ab2
Bernie Reiter <ockham@raz.or.at>
2025-04-16 15:52:24 +0200

    Block Hooks: Suppress insertion of hooked blocks in before/after position into post object content

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -1129,9 +1129,25 @@ function apply_block_hooks_to_content( $content, $context = null, $callback = 'i
 	 * We also need to cover the case where the hooked block is not present in
 	 * `$content` at first and we're allowed to insert it once -- but not again.
 	 */
-	$suppress_single_instance_blocks = static function ( $hooked_block_types ) use ( &$block_allows_multiple_instances, $content ) {
+	$suppress_single_instance_blocks = static function ( $hooked_block_types, $relative_position, $anchor_block_type ) use ( &$block_allows_multiple_instances, $content, $context ) {
 		static $single_instance_blocks_present_in_content = array();
 		foreach ( $hooked_block_types as $index => $hooked_block_type ) {
+			if ( $context instanceof WP_Post ) {
+				$wrapper_block_type = 'core/post-content';
+				if ( 'wp_navigation' === $context->post_type ) {
+					$wrapper_block_type = 'core/navigation';
+				} elseif ( 'wp_block' === $context->post_type ) {
+					$wrapper_block_type = 'core/block';
+				}
+
+				if (
+					$wrapper_block_type === $anchor_block_type &&
+					in_array( $relative_position, array( 'before', 'after' ), true )
+				) {
+					$hooked_block_types = array();
+				}
+			}
+
 			if ( ! isset( $block_allows_multiple_instances[ $hooked_block_type ] ) ) {
 				$hooked_block_type_definition =
 					WP_Block_Type_Registry::get_instance()->get_registered( $hooked_block_type );
@@ -1157,7 +1173,7 @@ function apply_block_hooks_to_content( $content, $context = null, $callback = 'i
 		}
 		return $hooked_block_types;
 	};
-	add_filter( 'hooked_block_types', $suppress_single_instance_blocks, PHP_INT_MAX );
+	add_filter( 'hooked_block_types', $suppress_single_instance_blocks, PHP_INT_MAX, 3 );
 	$content = traverse_and_serialize_blocks(
 		parse_blocks( $content ),
 		$before_block_visitor,
