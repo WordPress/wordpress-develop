946c629688ba18ba4693fcce4d4c38fa60cf1973
Jonny Harrus <jon@spacedmonkey.co.uk>
2025-04-29 20:56:34 +0100

    Ensure width and height attributes are sanitized for images
    Added validation to sanitize 'width' and 'height' attributes before use and updated the default attributes accordingly. This improves security and prevents potential issues with invalid values in generated image markup.

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -1070,7 +1070,6 @@ function wp_get_attachment_image( $attachment_id, $size = 'thumbnail', $icon = f
 		list( $src, $width, $height ) = $image;
 
 		$attachment = get_post( $attachment_id );
-		$hwstring   = image_hwstring( $width, $height );
 		$size_class = $size;
 
 		if ( is_array( $size_class ) ) {
@@ -1078,9 +1077,11 @@ function wp_get_attachment_image( $attachment_id, $size = 'thumbnail', $icon = f
 		}
 
 		$default_attr = array(
-			'src'   => $src,
-			'class' => "attachment-$size_class size-$size_class",
-			'alt'   => trim( strip_tags( get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) ) ),
+			'src'    => $src,
+			'class'  => "attachment-$size_class size-$size_class",
+			'alt'    => trim( strip_tags( get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) ) ),
+			'width'  => $width,
+			'height' => $height,
 		);
 
 		/**
@@ -1169,8 +1170,17 @@ function wp_get_attachment_image( $attachment_id, $size = 'thumbnail', $icon = f
 		 */
 		$attr = apply_filters( 'wp_get_attachment_image_attributes', $attr, $attachment, $size );
 
-		$attr = array_map( 'esc_attr', $attr );
-		$html = rtrim( "<img $hwstring" );
+		if ( isset( $attr['height'] ) ) {
+			$attr['height'] = absint( $attr['height'] );
+			unset( $attr['height'] );
+		}
+		if ( isset( $attr['width'] ) ) {
+			$attr['width'] = absint( $attr['width'] );
+			unset( $attr['width'] );
+		}
+		$attr     = array_map( 'esc_attr', $attr );
+		$hwstring = image_hwstring( $width, $height );
+		$html     = rtrim( "<img $hwstring" );
 
 		foreach ( $attr as $name => $value ) {
 			$html .= " $name=" . '"' . $value . '"';
