a6d0e01e7b2ab1e2aa5b5d738aa2c281a259c8d2
Shane McCurdy <shanemac10@gmail.com>
2025-05-01 11:40:47 -0500

    Update formatting.php untrailingslashit rtrim call cast value to string
    Fixes errors like the following:
    Deprecated: rtrim(): Passing null to parameter 1 ($string) of type string is deprecated in /wp-includes/formatting.php on line 2819

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -2816,7 +2816,7 @@ function trailingslashit( $value ) {
  * @return string String without the trailing slashes.
  */
 function untrailingslashit( $value ) {
-	return rtrim( $value, '/\\' );
+	return rtrim( strval( $value ), '/\\' );
 }
 
 /**
