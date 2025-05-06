fd401539bfdce490e32e2a0a38aa438d787921af
Peter Wilson <git-commit-by@peterwilson.cc>
2025-05-06 11:52:28 +1000

    Update Emoji CDN to 16.0.1 endpoint.
    See https://meta.trac.wordpress.org/ticket/7970#comment:1

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -5905,7 +5905,7 @@ function _print_emoji_detection_script() {
 		 *
 		 * @param string $url The emoji base URL for png images.
 		 */
-		'baseUrl' => apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/15.1.0/72x72/' ),
+		'baseUrl' => apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/16.0.1/72x72/' ),
 
 		/**
 		 * Filters the extension of the emoji png files.
@@ -5923,7 +5923,7 @@ function _print_emoji_detection_script() {
 		 *
 		 * @param string $url The emoji base URL for svg images.
 		 */
-		'svgUrl'  => apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/15.1.0/svg/' ),
+		'svgUrl'  => apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/16.0.1/svg/' ),
 
 		/**
 		 * Filters the extension of the emoji SVG files.
@@ -6018,7 +6018,7 @@ function wp_staticize_emoji( $text ) {
 	}
 
 	/** This filter is documented in wp-includes/formatting.php */
-	$cdn_url = apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/15.1.0/72x72/' );
+	$cdn_url = apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/16.0.1/72x72/' );
 
 	/** This filter is documented in wp-includes/formatting.php */
 	$ext = apply_filters( 'emoji_ext', '.png' );
