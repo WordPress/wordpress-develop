4394019af3b6d45aebe8aa086cedaa3a114ed1dd
Nikunj Hatkar <nikunj8866@gmail.com>
2025-05-20 18:55:38 +0530

    Fixed PHPCS error

--- src/wp-admin/edit-tag-form.php
+++ src/wp-admin/edit-tag-form.php
@@ -71,7 +71,7 @@ do_action( "{$taxonomy}_pre_edit_form", $tag, $taxonomy ); ?>
 <div class="wrap">
 <h1>
 	<?php echo $tax->labels->edit_item; ?>
-	<?php 
+	<?php
 	if ( is_term_publicly_viewable( $tag ) ) {
 		$view_svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true"
 			focusable="false">
