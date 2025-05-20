64b9a5407d59300a75bba42a788bf97d316fc546
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-05-20 07:56:32 -0600

    phpcbf

--- src/wp-includes/rest-api/endpoints/class-wp-rest-autosaves-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-autosaves-controller.php
@@ -311,14 +311,20 @@ class WP_REST_Autosaves_Controller extends WP_REST_Revisions_Controller {
 		}
 
 		$posts_per_page = 100;
-		$per_page = $request['per_page'];
+		$per_page       = $request['per_page'];
 		if ( ! empty( $per_page ) ) {
 			$posts_per_page = $per_page;
 		}
 
 		$response  = array();
 		$parent_id = $parent->ID;
-		$revisions = wp_get_post_revisions( $parent_id, array( 'check_enabled' => false, 'posts_per_page' => $posts_per_page ) );
+		$revisions = wp_get_post_revisions(
+			$parent_id,
+			array(
+				'check_enabled'  => false,
+				'posts_per_page' => $posts_per_page,
+			)
+		);
 
 		foreach ( $revisions as $revision ) {
 			if ( str_contains( $revision->post_name, "{$parent_id}-autosave" ) ) {
@@ -513,7 +519,7 @@ class WP_REST_Autosaves_Controller extends WP_REST_Revisions_Controller {
 	 */
 	public function get_collection_params() {
 		return array(
-			'context' => $this->get_context_param( array( 'default' => 'view' ) ),
+			'context'  => $this->get_context_param( array( 'default' => 'view' ) ),
 			'per_page' => array(
 				'description'       => __( 'Maximum number of autosaves to return.' ),
 				'type'              => 'integer',
