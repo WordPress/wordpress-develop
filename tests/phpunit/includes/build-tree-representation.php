2b33187ab12d36077b3f7cd04e99f0c9fa452c70
Bernie Reiter <ockham@raz.or.at>
2025-06-02 16:49:34 +0200

    Remove strict_types declaration

--- tests/phpunit/includes/build-tree-representation.php
+++ tests/phpunit/includes/build-tree-representation.php
@@ -1,4 +1,4 @@
-<?php declare( strict_types = 1 );
+<?php
 
 /* phpcs:disable WordPress.Security.EscapeOutput.ExceptionNotEscaped */
 
--- tests/phpunit/tests/build-tree-representation.php
+++ tests/phpunit/tests/build-tree-representation.php
@@ -1,4 +1,4 @@
-<?php declare( strict_types = 1 );
+<?php
 
 /**
  * Tests for build_tree_representation().
