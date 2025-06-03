b762ded2cf02d0479102d3958b6e7c8b81fde919
Sukhendu Sekhar Guria <sukhendu.guria@rtcamp.com>
2025-06-03 10:37:15 +0530

    Prevent username-email conflicts

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -2248,8 +2248,21 @@ function wp_insert_user( $userdata ) {
 		return new WP_Error( 'user_login_too_long', __( 'Username may not be longer than 60 characters.' ) );
 	}
 
-	if ( ! $update && username_exists( $user_login ) ) {
-		return new WP_Error( 'existing_user_login', __( 'Sorry, that username already exists!' ) );
+	if ( ! $update ) {
+		if ( username_exists( $user_login ) ) {
+			return new WP_Error( 'existing_user_login', __( 'Sorry, that username already exists!' ) );
+		}
+
+		if ( email_exists( $user_login ) ) {
+			return new WP_Error( 'existing_user_email_as_login', __( 'Sorry, that username is not available.' ) );
+		}
+	} else {
+		if ( isset( $userdata['user_email'] ) && isset( $userdata['ID'] ) ) {
+			$username_exists_id = username_exists( $userdata['user_email'] );
+			if ( $username_exists_id && $username_exists_id !== $userdata['ID'] ) {
+				return new WP_Error( 'email_as_username', __( 'Sorry, that email address is not available as it matches an existing username.' ) );
+			}
+		}
 	}
 
 	/**
@@ -3481,6 +3494,8 @@ function register_new_user( $user_login, $user_email ) {
 		$sanitized_user_login = '';
 	} elseif ( username_exists( $sanitized_user_login ) ) {
 		$errors->add( 'username_exists', __( '<strong>Error:</strong> This username is already registered. Please choose another one.' ) );
+	} elseif ( email_exists( $sanitized_user_login ) ) {
+		$errors->add( 'username_exists_as_email', __( '<strong>Error:</strong> This username is not available. Please choose another one.' ) );
 	} else {
 		/** This filter is documented in wp-includes/user.php */
 		$illegal_user_logins = (array) apply_filters( 'illegal_user_logins', array() );
