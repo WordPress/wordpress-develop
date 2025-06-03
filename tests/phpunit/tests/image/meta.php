b7222cac09478672f57e59e9cb2e331cf974b79a
Paul Bearne <pbearne@git.wordpress.org>
2025-06-03 12:37:28 -0400

    syntax errors

--- tests/phpunit/tests/image/meta.php
+++ tests/phpunit/tests/image/meta.php
@@ -217,10 +217,10 @@ class Tests_Image_Meta extends WP_UnitTestCase {
 					'title'             => 'IPTC Headline',
 					'orientation'       => '0',
 					'keywords'          => array(),
-					'created' => '2004-07-22T17:14:35+00:00',
+					'created'           => '2004-07-22T17:14:35+00:00',
 				),
 			),
-			'Exif from a DMC-LX2 camera with keywords' => array(
+			'Exif from a DMC-LX2 camera with keywords'         => array(
 				'file'     => 'testimagemeta://wp_read_image_metadata/image3.jpg',
 				'metadata' => array(
 					'aperture'          => '8',
@@ -235,7 +235,7 @@ class Tests_Image_Meta extends WP_UnitTestCase {
 					'title'             => 'Photoshop Document Ttitle',
 					'orientation'       => '1',
 					'keywords'          => array( 'beach', 'baywatch', 'LA', 'sunset' ),
-					'created' => '2011-05-25T09:22:07+00:00',
+					'created'           => '2011-05-25T09:22:07+00:00',
 				),
 			),
 		);
