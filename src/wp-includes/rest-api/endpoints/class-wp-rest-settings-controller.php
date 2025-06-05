73c586c1aa6f160424bf5893128be8515f6f16c4
Vedanshmini26 <97348795+Vedanshmini26@users.noreply.github.com>
2025-06-05 16:57:32 +0530

    Removed whitespace

--- src/wp-includes/rest-api/endpoints/class-wp-rest-settings-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-settings-controller.php
@@ -147,10 +147,10 @@ class WP_REST_Settings_Controller extends WP_REST_Controller {
 		$params = $request->get_params();
 
 		if ( empty( $params ) || ! empty( array_diff_key( $params, $options ) ) ) {
-			$message = empty( $params ) 
+			$message = empty( $params )
 				? __( 'Request body cannot be empty.' )
 				: __( 'Invalid parameter(s) provided.' );
-			
+
 			return new WP_Error(
 				'rest_invalid_param',
 				$message,
