9dcc63f0f6b89e6dc8f31264ec0c07fa463630cd
Weston Ruter <weston@ruter.net>
2025-06-06 15:56:52 -0700

    Write wp-config.php to repo root from start instead of ABSPATH and then moving up

--- tools/local-env/scripts/install.js
+++ tools/local-env/scripts/install.js
@@ -2,7 +2,7 @@ const dotenv       = require( 'dotenv' );
 const dotenvExpand = require( 'dotenv-expand' );
 const wait_on = require( 'wait-on' );
 const { execSync } = require( 'child_process' );
-const { renameSync, readFileSync, writeFileSync } = require( 'fs' );
+const { readFileSync, writeFileSync } = require( 'fs' );
 const local_env_utils = require( './utils' );
 
 dotenvExpand.expand( dotenv.config() );
@@ -11,7 +11,7 @@ dotenvExpand.expand( dotenv.config() );
 local_env_utils.determine_auth_option();
 
 // Create wp-config.php.
-wp_cli( 'config create --dbname=wordpress_develop --dbuser=root --dbpass=password --dbhost=mysql --force' );
+wp_cli( `config create --dbname=wordpress_develop --dbuser=root --dbpass=password --dbhost=mysql --force --config-file=${process.env.LOCAL_DIR}/../wp-config.php` );
 
 // Add the debug settings to wp-config.php.
 // Windows requires this to be done as an additional step, rather than using the --extra-php option in the previous step.
@@ -21,9 +21,6 @@ wp_cli( `config set WP_DEBUG_DISPLAY ${process.env.LOCAL_WP_DEBUG_DISPLAY} --raw
 wp_cli( `config set SCRIPT_DEBUG ${process.env.LOCAL_SCRIPT_DEBUG} --raw --type=constant` );
 wp_cli( `config set WP_ENVIRONMENT_TYPE ${process.env.LOCAL_WP_ENVIRONMENT_TYPE} --type=constant` );
 wp_cli( `config set WP_DEVELOPMENT_MODE ${process.env.LOCAL_WP_DEVELOPMENT_MODE} --type=constant` );
-
-// Move wp-config.php to the base directory, so it doesn't get mixed up in the src or build directories.
-renameSync( `${process.env.LOCAL_DIR}/wp-config.php`, 'wp-config.php' );
 
 // Read in wp-tests-config-sample.php, edit it to work with our config, then write it to wp-tests-config.php.
 const testConfig = readFileSync( 'wp-tests-config-sample.php', 'utf8' )
