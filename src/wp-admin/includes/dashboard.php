471eee4306c927d0770e728191a6b730a01c9c87
SirLouen <sir.louen@gmail.com>
2025-06-08 16:11:58 +0200

    Default Post Format in Quick Press Issue

--- src/wp-admin/includes/dashboard.php
+++ src/wp-admin/includes/dashboard.php
@@ -603,6 +603,11 @@ function wp_dashboard_quick_press( $error_msg = false ) {
 			<input type="hidden" name="action" id="quickpost-action" value="post-quickdraft-save" />
 			<input type="hidden" name="post_ID" value="<?php echo $post_ID; ?>" />
 			<input type="hidden" name="post_type" value="post" />
+			<?php
+			if ( current_theme_supports( 'post-formats' ) && post_type_supports( 'post', 'post-formats' ) && get_option( 'default_post_format' ) ) {
+				echo '<input type="hidden" name="post_format" value="' . esc_attr( get_option( 'default_post_format' ) ) . '" />';
+			}
+			?>
 			<?php wp_nonce_field( 'add-post' ); ?>
 			<?php submit_button( __( 'Save Draft' ), 'primary', 'save', false, array( 'id' => 'save-post' ) ); ?>
 			<br class="clear" />
