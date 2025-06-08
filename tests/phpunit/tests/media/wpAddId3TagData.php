36d0b40a43b2606b4bf5d26c452512df49eeab4c
Ankit Kumar Shah <ankit.shah@rtcamp.com>
2025-06-08 13:29:59 +0530

    Tests: Add unit tests for `wp_add_id3_tag_data()` function

---
+++ tests/phpunit/tests/media/wpAddId3TagData.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Tests for `wp_add_id3_tag_data()` function
+ *
+ * @group media
+ * @covers ::wp_add_id3_tag_data
+ */
+class Tests_Media_WpAddId3TagData extends WP_UnitTestCase {
+
+	public function set_up() {
+		parent::set_up();
+	}
+
+	public function tear_down() {
+		parent::tear_down();
+	}
+}
