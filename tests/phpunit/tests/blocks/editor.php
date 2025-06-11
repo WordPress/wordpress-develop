7e5d18a5e5875cd38c6acc1a6e6d55c39f6b8009
Jon Surrell <sirreal@users.noreply.github.com>
2025-06-11 15:44:45 +0200

    Clean up test function name and documentation

--- tests/phpunit/tests/blocks/editor.php
+++ tests/phpunit/tests/blocks/editor.php
@@ -725,16 +725,19 @@ class Tests_Blocks_Editor extends WP_UnitTestCase {
 	 * @ticket 62797
 	 *
 	 * @covers ::block_editor_rest_api_preload
+	 *
+	 * Some valid JSON-encoded data is dangerous to embed in HTML without appropriate
+	 * escaping. This test includes prints an example of such data that would prevent
+	 * the enclosing `<script>` from closing on its apparent closer and remain open.
 	 */
-	public function test_preload_closes_script_tag() {
+	public function test_ensure_preload_data_script_tag_closes() {
 		add_theme_support( 'html5', array( 'script' ) );
-
 		register_rest_route(
 			'test/v0',
 			'test-62797',
 			array(
 				'methods'             => 'GET',
-				'callback'            => function ( WP_REST_Request $request ) {
+				'callback'            => function () {
 					return '<!-- unclosed comment and a script tag <script></script>';
 				},
 				'permission_callback' => '__return_true',
