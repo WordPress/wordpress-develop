5bf2ebfe30cb617c20e9792f4a373730aab34ba1
Jon Surrell <sirreal@users.noreply.github.com>
2025-06-11 18:26:10 +0200

    Fix issue with other site urls in tests

--- tests/phpunit/tests/blocks/editor.php
+++ tests/phpunit/tests/blocks/editor.php
@@ -755,9 +755,11 @@ class Tests_Blocks_Editor extends WP_UnitTestCase {
 
 		ob_start();
 		wp_scripts()->do_item( 'wp-api-fetch' );
-		$output   = ob_get_clean();
+		$output = ob_get_clean();
+
+		$baseurl  = site_url();
 		$expected = <<<HTML
-<script src="http://example.org/wp-includes/js/dist/api-fetch.min.js?ver=test" id="wp-api-fetch-js"></script>
+<script src="{$baseurl}/wp-includes/js/dist/api-fetch.min.js?ver=test" id="wp-api-fetch-js"></script>
 <script id="wp-api-fetch-js-after">
 wp.apiFetch.use( wp.apiFetch.createPreloadingMiddleware( {"/test/v0/test-62797":{"body":["\\u003C!-- unclosed comment and a script tag \\u003Cscript\\u003E\\u003C/script\\u003E"],"headers":{"Allow":"GET"}}} ) );
 </script>
