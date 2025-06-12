41d68bf77e5b2251fd562dfde045459ef9cec8fc
Weston Ruter <weston@ruter.net>
2025-06-12 12:07:43 -0700

    Revert "Do enqueueing in existing function"
    This reverts commit e7144a3652f8888b1690db58acf361e4d0093912.

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -2066,13 +2066,12 @@ function wp_sizes_attribute_includes_valid_auto( string $sizes_attr ): bool {
  * This rule overrides the similar rule in the default user agent stylesheet, to avoid images that use e.g.
  * `width: auto` or `width: fit-content` to appear smaller.
  *
- * @since 6.7.1
- * @since 6.9.0 The style is now enqueued as opposed to being printed.
+ * @since 6.9.0
  *
- * @link https://html.spec.whatwg.org/multipage/rendering.html#img-contain-size
- * @link https://core.trac.wordpress.org/ticket/62413
+ * @see https://html.spec.whatwg.org/multipage/rendering.html#img-contain-size
+ * @see https://core.trac.wordpress.org/ticket/62413
  */
-function wp_print_auto_sizes_contain_css_fix() {
+function wp_enqueue_img_auto_sizes_contain_css_fix(): void {
 	/** This filter is documented in wp-includes/media.php */
 	$add_auto_sizes = apply_filters( 'wp_img_tag_add_auto_sizes', true );
 	if ( ! $add_auto_sizes ) {
