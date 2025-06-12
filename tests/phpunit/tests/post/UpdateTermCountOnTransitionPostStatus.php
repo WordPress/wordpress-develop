43eb3ca02723a13299dc254d7d117f8e6f67057a
John Blackbourn <johnbillion@gmail.com>
2025-06-12 18:47:21 +0100

    These tests don't touch the post status transition handling.

--- tests/phpunit/tests/post/UpdateTermCountOnTransitionPostStatus.php
+++ tests/phpunit/tests/post/UpdateTermCountOnTransitionPostStatus.php
@@ -132,62 +132,6 @@ class Tests_Taxonomy_UpdateTermCountOnTransitionPostStatus extends WP_UnitTestCa
 	}
 
 	/**
-	 * Test that the term count is updated when a post is removed from a term.
-	 *
-	 * @ticket 42522
-	 */
-	public function test_update_term_count_on_remove_term() {
-		$this->assertTermCount( 1, self::$term_id );
-
-		// Remove post from term.
-		wp_set_object_terms( self::$post_id, array(), self::$taxonomy );
-
-		$this->assertTermCount( 0, self::$term_id );
-	}
-
-	/**
-	 * Test that the term count is updated when a post is added to a term.
-	 *
-	 * @ticket 42522
-	 */
-	public function test_update_term_count_on_add_term() {
-		$this->assertTermCount( 1, self::$term_id );
-
-		// Add post to another term.
-		$term_id2 = self::factory()->term->create(
-			array(
-				'taxonomy' => self::$taxonomy,
-				'name'     => 'Test Category 2',
-			)
-		);
-
-		wp_set_object_terms( self::$post_id, array( self::$term_id, $term_id2 ), self::$taxonomy );
-
-		$this->assertTermCount( 1, self::$term_id );
-		$this->assertTermCount( 1, $term_id2 );
-	}
-
-	/**
-	 * Test that the term count is updated when a post is added to a term.
-	 *
-	 * @ticket 42522
-	 */
-	public function test_update_term_count_on_add_new_post_with_term() {
-		$this->assertTermCount( 1, self::$term_id );
-
-		$post_id = self::factory()->post->create(
-			array(
-				'post_type'   => self::$post_type,
-				'post_status' => 'publish',
-			)
-		);
-
-		wp_set_object_terms( $post_id, self::$term_id, self::$taxonomy );
-
-		$this->assertTermCount( 2, self::$term_id );
-	}
-
-	/**
 	 * Assert that the term count is correct.
 	 *
 	 * @param int $expected_count Expected term count.
