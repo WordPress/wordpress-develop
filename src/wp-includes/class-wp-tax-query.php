3c2432e77dc005668047652fac2082d7246046c3
SirLouen <sir.louen@gmail.com>
2025-06-15 22:14:51 +0200

    Sorting #20604

--- src/wp-includes/class-wp-tax-query.php
+++ src/wp-includes/class-wp-tax-query.php
@@ -605,7 +605,13 @@ class WP_Tax_Query {
 		$resulting_field = sanitize_key( $resulting_field );
 
 		// Empty terms always results in a null transformation but terms like 0 are not empty. See #46350.
-		$terms = array_filter( $query['terms'], 'strlen' );
+		$terms = array_filter(
+			$query['terms'],
+			function ( $value ) {
+				return $value !== 0 || is_string( $value );
+			}
+		);
+
 		if ( empty( $terms ) ) {
 			$query['terms'] = array();
 			$query['field'] = $resulting_field;
