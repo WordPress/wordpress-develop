09b0f0844fcc7e7b61bf5591f61043ded0fefc86
SirLouen <sir.louen@gmail.com>
2025-06-24 07:07:02 +0200

    Refreshing 39370.1.diff

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -2293,7 +2293,8 @@ function wp_insert_user( $userdata ) {
 		return new WP_Error( 'user_nicename_too_long', __( 'Nicename may not be longer than 50 characters.' ) );
 	}
 
-	$user_nicename_check = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE user_nicename = %s AND user_login != %s LIMIT 1", $user_nicename, $user_login ) );
+	$user_id             = ! empty( $ID ) ? (int) $ID : 0;
+	$user_nicename_check = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE user_nicename = %s AND ID != %d LIMIT 1", $user_nicename, $user_id ) );
 
 	if ( $user_nicename_check ) {
 		$suffix = 2;
@@ -2301,7 +2302,7 @@ function wp_insert_user( $userdata ) {
 			// user_nicename allows 50 chars. Subtract one for a hyphen, plus the length of the suffix.
 			$base_length         = 49 - mb_strlen( $suffix );
 			$alt_user_nicename   = mb_substr( $user_nicename, 0, $base_length ) . "-$suffix";
-			$user_nicename_check = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE user_nicename = %s AND user_login != %s LIMIT 1", $alt_user_nicename, $user_login ) );
+			$user_nicename_check = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE user_nicename = %s AND ID != %d LIMIT 1", $alt_user_nicename, $user_id ) );
 			++$suffix;
 		}
 		$user_nicename = $alt_user_nicename;
