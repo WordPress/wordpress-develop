8dc76916e758a37955f89365942d97bd46fa5a25
Vedanshmini26 <97348795+Vedanshmini26@users.noreply.github.com>
2025-06-24 12:51:39 +0530

    Removed get_url_params() function call

--- src/wp-includes/rest-api/endpoints/class-wp-rest-settings-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-settings-controller.php
@@ -145,15 +145,14 @@ class WP_REST_Settings_Controller extends WP_REST_Controller {
 	public function update_item( $request ) {
 		$options = $this->get_registered_options();
 		$params = $request->get_params();
-		$url_params = $request->get_url_params();
 		$query_params = $request->get_query_params();
-		$params = array_diff_key( $params, $url_params, $query_params );
+		$params = array_diff_key( $params, $query_params );
 
 		if ( empty( $params ) || ! empty( array_diff_key( $params, $options ) ) ) {
-			$message = empty( $params )
+			$message = empty( $params ) 
 				? __( 'Request body cannot be empty.' )
 				: __( 'Invalid parameter(s) provided.' );
-
+			
 			return new WP_Error(
 				'rest_invalid_param',
 				$message,
