fb5546c0d398b03d081fa2755ce9294507b3482d
Jon Surrell <sirreal@users.noreply.github.com>
2025-06-27 14:13:10 +0200

    Revert "Remove padding numeric entities on normalization"
    This reverts commit 40ecfc315954d9a783fa091271c8cf473b03fb44.

--- src/wp-includes/kses.php
+++ src/wp-includes/kses.php
@@ -2075,7 +2075,7 @@ function wp_kses_normalize_entities2( $matches ) {
 	$i = $matches[1];
 
 	if ( valid_unicode( $i ) ) {
-		$i = ltrim( $i, '0' );
+		$i = str_pad( ltrim( $i, '0' ), 3, '0', STR_PAD_LEFT );
 		$i = "&#$i;";
 	} else {
 		$i = "&amp;#$i;";
