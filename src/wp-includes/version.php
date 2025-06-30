f99bab8eaec2feb9fe8d1e235f57b64ca1c2a9ef
Peter Wilson <git-commit-by@peterwilson.cc>
2025-07-01 09:42:37 +1000

    Bump version to 6.0.10

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.9-src';
+$wp_version = '6.0.10-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
