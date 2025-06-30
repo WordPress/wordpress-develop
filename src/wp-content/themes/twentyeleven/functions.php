08f69a891957a8b22cb4ccef109f8827633a1a8c
Shreya Shrivastava <shreya2202shri@gmail.com>
2025-06-30 16:44:36 +0530

    fix: switch nav labels according to order

--- src/wp-content/themes/twentyeleven/functions.php
+++ src/wp-content/themes/twentyeleven/functions.php
@@ -642,11 +642,16 @@ if ( ! function_exists( 'twentyeleven_content_nav' ) ) :
 		global $wp_query;
 
 		if ( $wp_query->max_num_pages > 1 ) :
+			$order   = get_query_var( 'order', 'DESC' );
+			$is_desc = ( 'DESC' === $order );
+
+			$new_posts_text = __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' );
+			$old_posts_text = __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyeleven' );
 			?>
 			<nav id="<?php echo esc_attr( $html_id ); ?>">
 				<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
-				<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyeleven' ) ); ?></div>
-				<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></div>
+				<div class="<?php echo esc_attr( $is_desc ? 'nav-previous' : 'nav-next' ); ?>"><?php next_posts_link( $is_desc ? $old_posts_text : $new_posts_text ); ?></div>
+				<div class="<?php echo esc_attr( $is_desc ? 'nav-next' : 'nav-previous' ); ?>"><?php previous_posts_link( $is_desc ? $new_posts_text : $old_posts_text ); ?></div>
 			</nav><!-- #nav-above -->
 			<?php
 	endif;
