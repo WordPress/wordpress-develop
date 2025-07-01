17b993756507b6e75f683a0cad2d542375978d64
Prashant Baldha <pmbaldha@gmail.com>
2025-07-01 17:33:23 +0530

    Add a PHPDoc block for the twentyfourteen_credits action hook in the Twenty Fourteen theme.

--- src/wp-content/themes/twentyfourteen/footer.php
+++ src/wp-content/themes/twentyfourteen/footer.php
@@ -18,7 +18,14 @@
 			<?php get_sidebar( 'footer' ); ?>
 
 			<div class="site-info">
-				<?php do_action( 'twentyfourteen_credits' ); ?>
+				<?php
+				/**
+				 * Fires before the Twenty Fourteen credits in the footer.
+				 *
+				 * @since Twenty Fourteen 1.0
+				 */
+				do_action( 'twentyfourteen_credits' );
+				?>
 				<?php
 				if ( function_exists( 'the_privacy_policy_link' ) ) {
 					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
