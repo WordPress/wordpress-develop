a64b13af45284f624026e5c18256762239c9801a
Shreya Shrivastava <shreya2202shri@gmail.com>
2025-07-02 18:39:16 +0530

    refactor: switch nav labels while preserving DOM order

--- src/wp-content/themes/twentyeleven/functions.php
+++ src/wp-content/themes/twentyeleven/functions.php
@@ -647,11 +647,29 @@ if ( ! function_exists( 'twentyeleven_content_nav' ) ) :
 
 			$new_posts_text = __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' );
 			$old_posts_text = __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyeleven' );
+
+			$prev_link = $is_desc ? get_next_posts_link( $old_posts_text ) : get_previous_posts_link( $old_posts_text );
+			$next_link = $is_desc ? get_previous_posts_link( $new_posts_text ) : get_next_posts_link( $new_posts_text );
+
+			$allowed_html_tags = array(
+				'a'    => array(
+					'href'  => array(),
+					'title' => array(),
+				),
+				'span' => array(
+					'class' => array(),
+				),
+			)
 			?>
 			<nav id="<?php echo esc_attr( $html_id ); ?>">
 				<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
-				<div class="<?php echo esc_attr( $is_desc ? 'nav-previous' : 'nav-next' ); ?>"><?php next_posts_link( $is_desc ? $old_posts_text : $new_posts_text ); ?></div>
-				<div class="<?php echo esc_attr( $is_desc ? 'nav-next' : 'nav-previous' ); ?>"><?php previous_posts_link( $is_desc ? $new_posts_text : $old_posts_text ); ?></div>
+				<?php if ( $prev_link ) : ?>
+					<div class="nav-previous"><?php echo wp_kses( $prev_link, $allowed_html_tags ); ?></div>
+				<?php endif; ?>
+
+				<?php if ( $next_link ) : ?>
+					<div class="nav-next"><?php echo wp_kses( $next_link, $allowed_html_tags ); ?></div>
+				<?php endif; ?>
 			</nav><!-- #nav-above -->
 			<?php
 	endif;
