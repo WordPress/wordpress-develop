5d4377889b3250ed6a04bee2d7a842d160df9121
Prashant Baldha <pmbaldha@gmail.com>
2025-07-02 10:48:10 +0530

    Update src/wp-content/themes/twentynineteen/inc/helper-functions.php
    Co-authored-by: Stephen A. Bernhardt <sabernhardt@yahoo.com>

--- src/wp-content/themes/twentynineteen/inc/helper-functions.php
+++ src/wp-content/themes/twentynineteen/inc/helper-functions.php
@@ -12,6 +12,7 @@
  */
 function twentynineteen_can_show_post_thumbnail() {
 	$show_post_thumbnail = ( ! post_password_required() && ! is_attachment() && has_post_thumbnail() );
+
 	/**
 	 * Filters whether to show post thumbnail.
 	 *
