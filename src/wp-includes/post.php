2b547fce7119b5f49070985726f056f62b8ad5dc
Vipul Gupta <vipulgup04@gmail.com>
2025-07-04 10:52:39 +0530

    fix: PHPCS errors.

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -6167,7 +6167,7 @@ function get_page_by_path( $page_path, $output = OBJECT, $post_type = 'page' ) {
 				&& $p->post_name === $revparts[ $count ]
 			) {
 				$found_id = $page->ID;
-				if ( is_array($post_type) ? in_array($page->post_type, $post_type, true) : $page->post_type === $post_type ) {
+				if ( is_array( $post_type ) ? in_array( $page->post_type, $post_type, true ) : $page->post_type === $post_type ) {
 					break;
 				}
 			}
