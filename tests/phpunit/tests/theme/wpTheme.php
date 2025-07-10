684aa74694a3a8dbaf11376e883850360e66c5f4
Pedro Duarte <xipasduarte@users.noreply.github.com>
2025-07-10 19:23:25 +0100

    Fix linter suggestions

--- tests/phpunit/tests/theme/wpTheme.php
+++ tests/phpunit/tests/theme/wpTheme.php
@@ -408,9 +408,12 @@ class Tests_Theme_wpTheme extends WP_UnitTestCase {
 		$clean_admin_url = admin_url( 'customize.php' );
 
 		// Ensure the existing query arg is present in the URL.
-		add_filter( 'admin_url', function( $url ) {
-			return add_query_arg( 'existing_arg', 'value', $url );
-		} );
+		add_filter(
+			'admin_url',
+			function ( $url ) {
+				return add_query_arg( 'existing_arg', 'value', $url );
+			}
+		);
 		$this->assertSame( esc_url( $clean_admin_url . '?existing_arg=value&theme=foo' ), wp_customize_url( 'foo' ) );
 	}
 
@@ -425,9 +428,12 @@ class Tests_Theme_wpTheme extends WP_UnitTestCase {
 		$clean_admin_url = admin_url( 'customize.php' );
 
 		// Ensure the theme query arg is replaced with the new value.
-		add_filter( 'admin_url', function( $url ) {
-			return add_query_arg( 'theme', 'to-be-replaced', $url );
-		} );
+		add_filter(
+			'admin_url',
+			function ( $url ) {
+				return add_query_arg( 'theme', 'to-be-replaced', $url );
+			}
+		);
 		$this->assertSame( esc_url( $clean_admin_url . '?theme=foo' ), wp_customize_url( 'foo' ) );
 	}
 
@@ -442,9 +448,12 @@ class Tests_Theme_wpTheme extends WP_UnitTestCase {
 		$clean_admin_url = admin_url( 'customize.php' );
 
 		// Ensure the theme query arg is replaced with the new value.
-		add_filter( 'admin_url', function( $url ) {
-			return add_query_arg( [ 'theme' => [ 'to-be-replaced-1', 'to-be-replaced-2' ] ], $url );
-		} );
+		add_filter(
+			'admin_url',
+			function ( $url ) {
+				return add_query_arg( [ 'theme' => [ 'to-be-replaced-1', 'to-be-replaced-2' ] ], $url );
+			}
+		);
 		$this->assertSame( esc_url( $clean_admin_url . '?theme=foo' ), wp_customize_url( 'foo' ) );
 	}
 
