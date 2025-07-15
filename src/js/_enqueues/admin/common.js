b366373f9257aa32ba92f26575794a99ab1e28c1
Dominik Schilling <dominikschilling+git@gmail.com>
2025-07-15 11:33:37 +0200

    Disable button if nothing is selected

--- src/js/_enqueues/admin/common.js
+++ src/js/_enqueues/admin/common.js
@@ -2354,3 +2354,15 @@ $( function( $ ) {
 	// Expose public methods.
 	return pub;
 })();
+
+
+// Disable Confirm Deletion button until delete or reassignemnt is selected.
+( function( $ ) {
+	var $deleteButton = $( '#confirm-users-deletion' );
+
+	if ( ! $deleteButton.length ) {
+		return;
+	}
+
+	$deleteButton.prop( 'disabled', true );
+}( jQuery ));
