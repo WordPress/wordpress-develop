c2b7fbd0b56be2f01d1bdee27fafeffac057c6ff
Dominik Schilling <dominikschilling+git@gmail.com>
2025-07-15 11:33:23 +0200

    Add TODO

--- src/wp-admin/includes/ms.php
+++ src/wp-admin/includes/ms.php
@@ -966,6 +966,7 @@ function confirm_delete_users( $users ) {
 								printf( __( 'Site: %s' ), $user_site );
 								?>
 							</li>
+							<?php // TODO: Fix duplicate IDs ?>
 							<li><label><input type="radio" id="delete_option0" name="delete[<?php echo $details->userblog_id . '][' . $delete_user->ID; ?>]" value="delete" />
 							<?php _e( 'Delete all content.' ); ?></label></li>
 							<li><label><input type="radio" id="delete_option1" name="delete[<?php echo $details->userblog_id . '][' . $delete_user->ID; ?>]" value="reassign" />
