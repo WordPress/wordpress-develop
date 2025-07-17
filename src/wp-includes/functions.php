042579fa26010569f0bd7f5d72070164e2bd7925
Timo Tijhof <krinkle@fastmail.com>
2025-07-16 21:24:47 -0700

    External Libraries: Adopt standard get_temp_dir() in Text_Diff
    This is placing files in a potentially different place than the
    rest of WordPress, and its potential false return value was not
    checked by the only caller in Text_Diff_Engine_shell.
    
    I considered changing our get_temp_dir implementations to check
    the additional directories that Text_Diff::_getTempDir considered,
    but decided against it. The Text_Diff code predates the introduction
    of sys_get_temp_dir in PHP 5.2, and so is checking these to make up
    for that. The latest version of the code that this was based on no
    longer does this and calls sys_get_temp_dir instead. Specifically, it
    removed _getTempDir() in favor of tempnam(null) [1], and later
    switched to horde/util, [2] which uses sys_get_temp_dir. [3]
    
    [1]: https://github.com/horde/Text_Diff/commit/323c4783b9
    [2]: https://github.com/horde/Text_Diff/commit/152f6aacf8
    [3]: https://github.com/horde/Util/blob/v2.5.12/lib/Horde/Util.php#L200

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -2209,10 +2209,12 @@ function wp_normalize_path( $path ) {
  *
  * Function's preference is the return value of sys_get_temp_dir(),
  * followed by your PHP temporary upload directory, followed by WP_CONTENT_DIR,
- * before finally defaulting to /tmp/
+ * before finally defaulting to /tmp/.
+ *
+ * Note that sys_get_temp_dir honors the TMPDIR environment variable.
  *
  * In the event that this function does not find a writable location,
- * It may be overridden by the WP_TEMP_DIR constant in your wp-config.php file.
+ * it may be overridden by the WP_TEMP_DIR constant in your wp-config.php file.
  *
  * @since 2.5.0
  *
