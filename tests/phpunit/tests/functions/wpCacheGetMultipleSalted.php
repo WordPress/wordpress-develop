42b3dee23b2818604252f451475f6a263cf8af6b
Jonny Harrus <jon@spacedmonkey.co.uk>
2025-07-22 22:17:20 +0100

    Refactor PHPUnit tests for query cache to use `salted` naming convention and update test cases for consistency.

--- tests/phpunit/tests/functions/wpCacheGetMultipleSalted.php
+++ tests/phpunit/tests/functions/wpCacheGetMultipleSalted.php
@@ -1,21 +1,21 @@
 <?php
 
 /**
- * Tests for the behavior of `wp_cache_get_multiple_query_data()`
+ * Tests for the behavior of `wp_cache_get_multiple_salted()`
  *
  * @group functions
  * @group cache
  *
  * @covers ::wp_cache_get_multiple_salted
  */
-class Tests_Functions_wpCacheGetMultipleQueryData extends WP_UnitTestCase {
+class Tests_Functions_wpCacheGetMultipleSalted extends WP_UnitTestCase {
 
 	/**
-	 * Test that wp_cache_get_multiple_query_data returns the cached data.
+	 * Test that wp_cache_get_multiple_salted returns the cached data.
 	 *
 	 * @ticket 59592
 	 */
-	public function test_wp_cache_get_multiple_query_data_return_data() {
+	public function test_wp_cache_get_multiple_salted_return_data() {
 		$last_changed = wp_cache_get_last_changed( 'query_data' );
 		$cache_value  = array(
 			'salt' => $last_changed,
@@ -32,11 +32,36 @@ class Tests_Functions_wpCacheGetMultipleQueryData extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test that wp_cache_get_multiple_query_data returns an empty array when no data is cached.
+	 * Test that wp_cache_get_multiple_salted returns the cached data with a salt.
 	 *
 	 * @ticket 59592
 	 */
-	public function test_wp_cache_get_multiple_query_data_return_false() {
+	public function test_wp_cache_get_multiple_salted_return_data_array_salt() {
+		$last_changed        = array(
+			wp_cache_get_last_changed( 'query_data_1' ),
+			wp_cache_get_last_changed( 'query_data_2' ),
+		);
+		$last_changed_string = implode( ':', $last_changed );
+		$cache_value         = array(
+			'salt' => $last_changed_string,
+			'data' => array(
+				'key1' => 'value1',
+				'key2' => 'value2',
+			),
+		);
+		wp_cache_set( 'cache_key', $cache_value, 'query_data' );
+
+		$result = wp_cache_get_multiple_salted( array( 'cache_key' ), 'query_data', $last_changed );
+
+		$this->assertSameSets( $cache_value['data'], $result['cache_key'] );
+	}
+
+	/**
+	 * Test that wp_cache_get_multiple_salted returns an empty array when no data is cached.
+	 *
+	 * @ticket 59592
+	 */
+	public function test_wp_cache_get_multiple_salted_return_false() {
 		wp_cache_set( 'cache_key', false, 'query_data' );
 		wp_cache_set( 'another_key', null, 'query_data' );
 
@@ -54,11 +79,11 @@ class Tests_Functions_wpCacheGetMultipleQueryData extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test that wp_cache_get_multiple_query_data returns the cached data for multiple keys.
+	 * Test that wp_cache_get_multiple_salted returns the cached data for multiple keys.
 	 *
 	 * @ticket 59592
 	 */
-	public function test_wp_cache_get_multiple_query_data_with_some_false() {
+	public function test_wp_cache_get_multiple_salted_with_some_false() {
 		$last_changed = wp_cache_get_last_changed( 'query_data' );
 		wp_cache_set(
 			'cache_key',
