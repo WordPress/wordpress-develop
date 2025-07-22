1db62a3c66b501a3fae4c5b49babf117b8a25e9f
Jon Surrell <sirreal@users.noreply.github.com>
2025-07-22 15:05:56 +0200

    Implement subclass without reflection

--- tests/phpunit/tests/html-api/wpHtmlProcessorMetaTag.php
+++ tests/phpunit/tests/html-api/wpHtmlProcessorMetaTag.php
@@ -34,39 +34,14 @@ class Tests_HtmlApi_WpHtmlProcessorMetaTag extends WP_UnitTestCase {
 	 * @dataProvider data_supported_meta_tags
 	 */
 	public function test_supported_meta_tag( string $html ) {
-		$html  = '<!DOCTYPE html>' . $html;
-		$class = new class('') extends WP_HTML_Processor {
+		$html      = '<!DOCTYPE html>' . $html;
+		$processor = new class($html) extends WP_HTML_Processor {
 			public function __construct( $html ) {
 				parent::__construct( $html, parent::CONSTRUCTOR_UNLOCK_CODE );
 			}
-
-			public static function create( string $html ): self {
-				$instance = self::create_full_parser( $html );
-				// Reset encoding so META tags can handle it.
-				$reflection = new ReflectionClass( $instance );
-				$property   = $reflection->getParentClass()->getProperty( 'state' );
-				$property->setAccessible( true );
-				$state                      = $property->getValue( $instance );
-				$state->encoding            = null;
-				$state->encoding_confidence = 'tentative';
-				return $instance;
-			}
-
-			public function get_state_encoding(): ?string {
-				$reflection = new ReflectionClass( $this );
-				return $reflection->getParentClass()->getProperty( 'state' )->getValue( $this )->encoding;
-			}
-
-			public function get_state_encoding_confidence(): string {
-				$reflection = new ReflectionClass( $this );
-				return $reflection->getParentClass()->getProperty( 'state' )->getValue( $this )->encoding_confidence;
-			}
 		};
 
-		$processor = $class::create( $html );
 		$this->assertTrue( $processor->next_tag( 'META' ) );
-		$this->assertSame( 'tentative', $processor->get_state_encoding_confidence() );
-		$this->assertNull( $processor->get_state_encoding() );
 	}
 
 	/**
@@ -89,27 +64,14 @@ class Tests_HtmlApi_WpHtmlProcessorMetaTag extends WP_UnitTestCase {
 	 * @dataProvider data_unsupported_meta_tags
 	 */
 	public function test_unsupported_meta_tags( string $html, string $unsupported_message ) {
-		$html  = '<!DOCTYPE html>' . $html;
-		$class = new class('') extends WP_HTML_Processor {
+		$html      = '<!DOCTYPE html>' . $html;
+		$processor = new class($html) extends WP_HTML_Processor {
 			public function __construct( $html ) {
 				parent::__construct( $html, parent::CONSTRUCTOR_UNLOCK_CODE );
 			}
-
-			public static function create( string $html ): self {
-				$instance = self::create_full_parser( $html );
-				// Reset encoding so META tags can handle it.
-				$reflection = new ReflectionClass( $instance );
-				$property   = $reflection->getParentClass()->getProperty( 'state' );
-				$property->setAccessible( true );
-				$state                      = $property->getValue( $instance );
-				$state->encoding            = null;
-				$state->encoding_confidence = 'tentative';
-				return $instance;
-			}
 		};
 
-		$processor = $class::create( $html );
 		$this->assertFalse( $processor->next_tag( 'META' ) );
-		$this->assertSame( $unsupported_message, $processor->get_unsupported_exception()->getMessage() );
+		$this->assertInstanceOf( WP_HTML_Unsupported_Exception::class, $processor->get_unsupported_exception() );
 	}
 }
