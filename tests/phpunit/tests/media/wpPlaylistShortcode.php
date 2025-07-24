80b9976cce459b2816f36ad1fc3b3f3e176d7678
Aditya Singh <adityasinghboss1234@gmail.com>
2025-07-24 07:51:03 +0530

    Tests: Update playlist shortcode test to verify scripts load exactly once for invalid playlist

--- tests/phpunit/tests/media/wpPlaylistShortcode.php
+++ tests/phpunit/tests/media/wpPlaylistShortcode.php
@@ -8,13 +8,13 @@ class Tests_Media_Wp_Playlist_Shortcode extends WP_UnitTestCase {
 	/**
 	 * @ticket 63583
 	 */
-	public function test_should_load_scripts_when_first_playlist_is_invalid() {
-		$scripts_loaded = false;
+	public function test_should_load_scripts_exactly_once_when_first_playlist_is_invalid() {
+		$script_load_count = 0;
 
 		add_action(
 			'wp_playlist_scripts',
-			function () use ( &$scripts_loaded ) {
-				$scripts_loaded = true;
+			function () use ( &$script_load_count ) {
+				$script_load_count++;
 			}
 		);
 
@@ -23,9 +23,10 @@ class Tests_Media_Wp_Playlist_Shortcode extends WP_UnitTestCase {
 		);
 
 		wp_playlist_shortcode( array( 'ids' => '999999' ) );
-		wp_playlist_shortcode( array( 'ids' => $attachment_id ) );
+		wp_playlist_shortcode( array( 'ids' => (string)$attachment_id ) );
+		wp_playlist_shortcode( array( 'ids' => (string)$attachment_id ) );
 
-		$this->assertTrue( $scripts_loaded, 'Scripts should load even when first playlist has invalid ID' );
+		$this->assertSame( 1, $script_load_count, 'The playlist scripts should be loaded exactly once.' );
 
 		remove_all_actions( 'wp_playlist_scripts' );
 	}
