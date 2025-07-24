010a9a46c5d00190bdb37a5d7623804f82f27697
Aditya Singh <adityasinghboss1234@gmail.com>
2025-07-24 16:05:52 +0530

    Tests: Refactor script load count verification in wp_playlist_shortcode test

--- tests/phpunit/tests/media/wpPlaylistShortcode.php
+++ tests/phpunit/tests/media/wpPlaylistShortcode.php
@@ -9,14 +9,7 @@ class Tests_Media_Wp_Playlist_Shortcode extends WP_UnitTestCase {
 	 * @ticket 63583
 	 */
 	public function test_should_load_scripts_exactly_once_when_first_playlist_is_invalid() {
-		$script_load_count = 0;
-
-		add_action(
-			'wp_playlist_scripts',
-			function () use ( &$script_load_count ) {
-				$script_load_count++;
-			}
-		);
+		global $wp_scripts;
 
 		$attachment_id = self::factory()->attachment->create_upload_object(
 			DIR_TESTDATA . '/uploads/small-audio.mp3'
@@ -26,8 +19,9 @@ class Tests_Media_Wp_Playlist_Shortcode extends WP_UnitTestCase {
 		wp_playlist_shortcode( array( 'ids' => (string) $attachment_id ) );
 		wp_playlist_shortcode( array( 'ids' => (string) $attachment_id ) );
 
-		$this->assertSame( 1, $script_load_count, 'The playlist scripts should be loaded exactly once.' );
+		$queue_count = array_count_values( $wp_scripts->queue );
 
-		remove_all_actions( 'wp_playlist_scripts' );
+		$this->assertArrayHasKey( 'wp-playlist', $queue_count, 'wp-playlist handle should be in the queue.' );
+		$this->assertSame( 1, $queue_count['wp-playlist'], 'The wp-playlist script handle should appear exactly once in the queue.' );
 	}
 }
