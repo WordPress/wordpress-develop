0c736fa372386ba578369cdc4adfdade02920e8e
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2025-08-06 06:19:13 -0500

    Use variables for strings, where possible

--- src/wp-admin/options-general.php
+++ src/wp-admin/options-general.php
@@ -291,9 +291,10 @@ if ( $new_admin_email && get_option( 'admin_email' ) !== $new_admin_email ) {
 
 <?php if ( ! is_multisite() ) { ?>
 
+<?php $membership_title = __( 'Membership' ); ?>
 <tr>
-<th scope="row"><?php _e( 'Membership' ); ?></th>
-<td> <fieldset><legend class="screen-reader-text"><span><?php _e( 'Membership' ); ?></span></legend><label for="users_can_register">
+<th scope="row"><?php echo $membership_title; ?></th>
+<td> <fieldset><legend class="screen-reader-text"><span><?php echo $membership_title; ?></span></legend><label for="users_can_register">
 <input name="users_can_register" type="checkbox" id="users_can_register" value="1" <?php checked( '1', get_option( 'users_can_register' ) ); ?> />
 	<?php _e( 'Anyone can register' ); ?></label>
 </fieldset></td>
@@ -454,12 +455,13 @@ if ( empty( $tzstring ) ) { // Create a UTC+- zone if no timezone string exists.
 </p>
 <?php endif; ?>
 </td>
-
 </tr>
+
+<?php $date_format_title = __( 'Date Format' ); ?>
 <tr>
-<th scope="row"><?php _e( 'Date Format' ); ?></th>
+<th scope="row"><?php echo $date_format_title; ?></th>
 <td>
-	<fieldset><legend class="screen-reader-text"><span><?php _e( 'Date Format' ); ?></span></legend>
+	<fieldset><legend class="screen-reader-text"><span><?php echo $date_format_title; ?></span></legend>
 <?php
 	/**
 	 * Filters the default date formats.
@@ -501,10 +503,12 @@ foreach ( $date_formats as $format ) {
 	</fieldset>
 </td>
 </tr>
+
+<?php $time_format_title = __( 'Time Format' ); ?>
 <tr>
-<th scope="row"><?php _e( 'Time Format' ); ?></th>
+<th scope="row"><?php echo $time_format_title; ?></th>
 <td>
-	<fieldset><legend class="screen-reader-text"><span><?php _e( 'Time Format' ); ?></span></legend>
+	<fieldset><legend class="screen-reader-text"><span><?php echo $time_format_title; ?></span></legend>
 <?php
 	/**
 	 * Filters the default time formats.
