0534f6f886e456caa01d26d0031cd08d317e1f23
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2025-08-08 16:55:37 +1000

    Add border radius preset settings to theme json and style engine

--- src/wp-includes/class-wp-theme-json.php
+++ src/wp-includes/class-wp-theme-json.php
@@ -205,6 +205,15 @@ class WP_Theme_JSON {
 			'classes'           => array(),
 			'properties'        => array( 'box-shadow' ),
 		),
+		array(
+			'path'              => array( 'border', 'radiusSizes' ),
+			'prevent_override'  => false,
+			'use_default_names' => false,
+			'value_key'         => 'size',
+			'css_vars'          => '--wp--preset--border-radius--$slug',
+			'classes'           => array(),
+			'properties'        => array( 'border-radius' ),
+		),
 	);
 
 	/**
