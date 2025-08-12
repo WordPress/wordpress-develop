94bf366dde550dcdb4b5b280fc8bfa75b6ee1cce
Dennis Snell <dennis.snell@automattic.com>
2025-08-12 00:12:44 -0500

    Oops: those two bytes are important.

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -1005,7 +1005,7 @@ function wp_is_valid_utf8( string $bytes ): bool {
 			$bytes,
 			"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" .
 			"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f" .
-			"!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+			" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f",
 			$i
 		);
 		if ( $i >= $end ) {
