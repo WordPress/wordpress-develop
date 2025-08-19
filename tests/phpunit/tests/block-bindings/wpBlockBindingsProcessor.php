527c5d8c41b5701d505052f02e491788e6f01973
Bernie Reiter <ockham@raz.or.at>
2025-08-19 16:56:18 +0200

    Increase test coverage

--- tests/phpunit/tests/block-bindings/wpBlockBindingsProcessor.php
+++ tests/phpunit/tests/block-bindings/wpBlockBindingsProcessor.php
@@ -10,6 +10,27 @@
  * @group block-bindings
  */
 class Tests_Blocks_wpBlockBindingsProcessor extends WP_UnitTestCase {
+	/**
+	 * @ticket 63840
+	 */
+	public function test_replace_rich_text() {
+		$button_wrapper_opener = '<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">';
+		$button_wrapper_closer = '</a></div>';
+		$processor             = WP_Block_Bindings_Processor::create_fragment(
+			$button_wrapper_opener . 'This should not appear' . $button_wrapper_closer
+		);
+		$processor->next_tag( array( 'tag_name' => 'a' ) );
+
+		$this->assertTrue( $processor->replace_rich_text( 'The hardest button to button' ) );
+		$this->assertEquals(
+			$button_wrapper_opener . 'The hardest button to button' . $button_wrapper_closer,
+			$processor->build()
+		);
+	}
+
+	/**
+	 * @ticket 63840
+	 */
 	public function test_set_attribute_and_replace_rich_text() {
 		$figure_opener = '<figure class="wp-block-image">';
 		$img           = '<img src="breakfast.jpg" alt="" class="wp-image-1"/>';
