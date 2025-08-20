86e836dd67ce2e0bc1fb6f9a656a3daaccb2635c
Bernie Reiter <ockham@raz.or.at>
2025-08-20 11:13:26 +0200

    Increase test coverage

--- tests/phpunit/tests/block-bindings/wpBlockBindingsProcessor.php
+++ tests/phpunit/tests/block-bindings/wpBlockBindingsProcessor.php
@@ -56,4 +56,36 @@ class Tests_Blocks_wpBlockBindingsProcessor extends WP_UnitTestCase {
 			$processor->build()
 		);
 	}
+
+	/**
+	 * @ticket 63840
+	 */
+	public function test_replace_rich_text_and_seek() {
+		$figure_opener = '<figure class="wp-block-image">';
+		$img           = '<img src="breakfast.jpg" alt="" class="wp-image-1"/>';
+		$figure_closer = '</figure>';
+		$processor     = WP_Block_Bindings_Processor::create_fragment(
+			$figure_opener .
+			$img .
+			'<figcaption class="wp-element-caption">Breakfast at a <em>café</em> in Berlin</figcaption>' .
+			$figure_closer
+		);
+
+		$processor->next_tag( array( 'tag_name' => 'img' ) );
+		$processor->set_bookmark( 'image' );
+
+		$processor->next_tag( array( 'tag_name' => 'figcaption' ) );
+
+		$this->assertTrue( $processor->replace_rich_text( '<strong>New</strong> image caption' ) );
+
+		$processor->seek( 'image' );
+
+		$this->assertEquals(
+			$figure_opener .
+			$img .
+			'<figcaption class="wp-element-caption"><strong>New</strong> image caption</figcaption>' .
+			$figure_closer,
+			$processor->build()
+		);
+	}
 }
