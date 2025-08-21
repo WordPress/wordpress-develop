66fef38310d7c3076e9460c937a08079696d6883
Jon Surrell <sirreal@users.noreply.github.com>
2025-08-21 19:36:46 +0200

    Make the hidden class instance static

--- src/wp-includes/class-wp-block.php
+++ src/wp-includes/class-wp-block.php
@@ -462,49 +462,52 @@ class WP_Block {
 	}
 
 	private static function get_block_bindings_processor( string $block_content ) {
-		$internal_processor_class = new class('', WP_HTML_Processor::CONSTRUCTOR_UNLOCK_CODE) extends WP_HTML_Processor {
-			private $output         = '';
-			private $end_of_flushed = 0;
+		static $internal_processor_class = null;
+		if ( null === $internal_processor_class ) {
+			$internal_processor_class = new class('', WP_HTML_Processor::CONSTRUCTOR_UNLOCK_CODE) extends WP_HTML_Processor {
+				private $output         = '';
+				private $end_of_flushed = 0;
 
-			public function build() {
-				return $this->output . substr( $this->get_updated_html(), $this->end_of_flushed );
-			}
-
-			/**
-			 * Replace the rich text content between a tag opener and matching closer.
-			 *
-			 * When stopped on a tag opener, replace the content enclosed by it and its
-			 * matching closer with the provided rich text.
-			 *
-			 * @param string $rich_text The rich text to replace the original content with.
-			 * @return bool True on success.
-			 */
-			public function replace_rich_text( $rich_text ) {
-				if ( $this->is_tag_closer() ) {
-					return false;
+				public function build() {
+					return $this->output . substr( $this->get_updated_html(), $this->end_of_flushed );
 				}
 
-				$depth = $this->get_current_depth();
+				/**
+				 * Replace the rich text content between a tag opener and matching closer.
+				 *
+				 * When stopped on a tag opener, replace the content enclosed by it and its
+				 * matching closer with the provided rich text.
+				 *
+				 * @param string $rich_text The rich text to replace the original content with.
+				 * @return bool True on success.
+				 */
+				public function replace_rich_text( $rich_text ) {
+					if ( $this->is_tag_closer() ) {
+						return false;
+					}
 
-				$this->set_bookmark( '_wp_block_bindings_tag_opener' );
-				// The bookmark names are prefixed with `_` so the key below has an extra `_`.
-				$bm            = $this->bookmarks['__wp_block_bindings_tag_opener'];
-				$this->output .= substr( $this->get_updated_html(), $this->end_of_flushed, $bm->start + $bm->length );
-				$this->output .= $rich_text;
-				$this->release_bookmark( '_wp_block_bindings_tag_opener' );
+					$depth = $this->get_current_depth();
 
-				// Find matching tag closer.
-				while ( $this->next_token() && $this->get_current_depth() >= $depth ) {
+					$this->set_bookmark( '_wp_block_bindings_tag_opener' );
+					// The bookmark names are prefixed with `_` so the key below has an extra `_`.
+					$bm            = $this->bookmarks['__wp_block_bindings_tag_opener'];
+					$this->output .= substr( $this->get_updated_html(), $this->end_of_flushed, $bm->start + $bm->length );
+					$this->output .= $rich_text;
+					$this->release_bookmark( '_wp_block_bindings_tag_opener' );
+
+					// Find matching tag closer.
+					while ( $this->next_token() && $this->get_current_depth() >= $depth ) {
+					}
+
+					$this->set_bookmark( '_wp_block_bindings_tag_closer' );
+					$bm                   = $this->bookmarks['__wp_block_bindings_tag_closer'];
+					$this->end_of_flushed = $bm->start;
+					$this->release_bookmark( '_wp_block_bindings_tag_closer' );
+
+					return true;
 				}
-
-				$this->set_bookmark( '_wp_block_bindings_tag_closer' );
-				$bm                   = $this->bookmarks['__wp_block_bindings_tag_closer'];
-				$this->end_of_flushed = $bm->start;
-				$this->release_bookmark( '_wp_block_bindings_tag_closer' );
-
-				return true;
-			}
-		};
+			};
+		}
 
 		return $internal_processor_class::create_fragment( $block_content );
 	}
