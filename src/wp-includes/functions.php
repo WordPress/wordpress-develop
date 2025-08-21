eaa8eaa69316b141d6eb2d1e6cf7889d324d4a86
Joseph Scott <joseph@josephscott.org>
2025-08-21 16:09:38 -0600

    send_frame_options_header: new header and headers sent check
    https://core.trac.wordpress.org/ticket/29429
    
    My original concern was the lack of a `headers_sent()` in the
    `send_frame_options_header()` function, but after having found 29429 I think
    it makes sense to do both at the same time.

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -7142,7 +7142,10 @@ function wp_find_hierarchy_loop_tortoise_hare( $callback, $start, $override = ar
  * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
  */
 function send_frame_options_header() {
-	header( 'X-Frame-Options: SAMEORIGIN' );
+	if ( ! headers_sent() ) {
+		header( 'X-Frame-Options: SAMEORIGIN' );
+		header( "Content-Security-Policy: frame-ancestors 'self';" );
+	}
 }
 
 /**
