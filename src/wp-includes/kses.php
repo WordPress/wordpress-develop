881191dc74f105d8efaf60426d15ae416f4de46b
Jon Surrell <sirreal@users.noreply.github.com>
2025-08-21 16:48:18 +0200

    Deprecate it.

--- src/wp-includes/kses.php
+++ src/wp-includes/kses.php
@@ -1818,10 +1818,13 @@ function wp_kses_no_null( $content, $options = null ) {
  *
  * @since 1.0.0
  *
+ * @deprecated 6.9.0 Not necessary since the `e` PCRE modifier was removed in PHP 7.0.
+ *
  * @param string $content String to strip slashes from.
  * @return string Fixed string with quoted slashes.
  */
 function wp_kses_stripslashes( $content ) {
+	_deprecated_function( __FUNCTION__, '6.9.0' );
 	return preg_replace( '%\\\\"%', '"', $content );
 }
 
