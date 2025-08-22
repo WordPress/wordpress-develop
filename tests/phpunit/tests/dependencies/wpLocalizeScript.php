115e376cfce4d40b522b330e9f40306ab687fd6a
Devasheesh Kaul <devasheesh0909@gmail.com>
2025-08-22 15:39:27 +0530

    test: add test case for checking wp_localize_script for safe JSON output

--- tests/phpunit/tests/dependencies/wpLocalizeScript.php
+++ tests/phpunit/tests/dependencies/wpLocalizeScript.php
@@ -3,7 +3,7 @@
  * @group dependencies
  * @group scripts
  */
-class Tests_Dependencies_LocalizeScript extends WP_UnitTestCase {
+class Tests_Dependencies_wpLocalizeScript extends WP_UnitTestCase {
 	/**
 	 * @var WP_Scripts
 	 */
@@ -37,5 +37,31 @@ class Tests_Dependencies_LocalizeScript extends WP_UnitTestCase {
 				)
 			)
 		);
+	}
+
+	/**
+	 * Verifies that wp_localize_script() outputs safe JSON whe harmful data is provided.
+	 *
+	 * @ticket 63851
+	 * @covers ::wp_localize_script
+	 */
+	public function test_wp_localize_script_outputs_safe_json() {
+		$unsafe_data     = array( '<!--' => '<script>' );
+		$expected_unsafe = '{"\\u003C!--":"\\u003Cscript\\u003E"}';
+
+		$path     = '/test.js';
+		$base_url = site_url( $path );
+
+		wp_register_script( 'test-script', $path, array(), null );
+		wp_localize_script( 'test-script', 'testData', $unsafe_data );
+
+		ob_start();
+		wp_print_scripts( array( 'test-script' ) );
+		$output = ob_get_clean();
+
+		$expected = "<script type=\"text/javascript\" id=\"test-script-js-extra\">\n/* <![CDATA[ */\nvar testData = {$expected_unsafe};\n/* ]]> */\n</script>\n";
+		$expected .= "<script type=\"text/javascript\" src=\"{$base_url}\" id=\"test-script-js\"></script>\n";
+
+		$this->assertEqualHTML( $expected, $output );
 	}
 }
