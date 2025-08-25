968ecfd964f038677abbe634bbde10a2db094406
himanshupathak95 <himanshu.pathak@rtcamp.com>
2025-08-25 12:42:45 +0530

    Tests: Add get calendar test suite class

---
+++ tests/phpunit/tests/general-template/get-calendar-test.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Class responsible for testing the functionality of the get_calendar() template function
+ * in various scenarios, including input validation for invalid parameters.
+ *
+ * @covers get_calendar()
+ */
+class Tests_General_Template_GetCalendar extends WP_UnitTestCase {
+}
