e06ecb1b66c978313256f62090d66158490b05d0
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-08-26 16:55:09 -0600

    remove autosave preload entirely

--- src/wp-admin/edit-form-blocks.php
+++ src/wp-admin/edit-form-blocks.php
@@ -68,7 +68,6 @@ $preload_paths = array(
 	array( rest_get_route_for_post_type_items( 'page' ), 'OPTIONS' ),
 	array( rest_get_route_for_post_type_items( 'wp_block' ), 'OPTIONS' ),
 	array( rest_get_route_for_post_type_items( 'wp_template' ), 'OPTIONS' ),
-	sprintf( '%s/autosaves?context=edit&per_page=1', $rest_path ),
 	'/wp/v2/settings',
 	array( '/wp/v2/settings', 'OPTIONS' ),
 	'/wp/v2/global-styles/themes/' . $active_theme . '?context=view',
