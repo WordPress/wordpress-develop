42b83db4e1bd17d88a1c257b34492c60607b01e3
karthikeya-io <karthikeya.bethu@rtcamp.com>
2025-09-03 02:15:27 +0530

    Fix: Incorrect post status if user is before site time-zone

--- src/js/_enqueues/admin/inline-edit-post.js
+++ src/js/_enqueues/admin/inline-edit-post.js
@@ -434,7 +434,14 @@ window.wp = window.wp || {};
 		post_date_string += ' ' + $(':input[name="hh"]').val() + ':' + $(':input[name="mn"]').val() + ':' + $(':input[name="ss"]').val();
 		var post_date = new Date( post_date_string );
 		status = $('._status', rowData).text();
-		if ( 'future' !== status && Date.now() > post_date ) {
+		// Convert browser Date into site timezone before comparing.
+		var browserDate = new Date();
+		var browserOffset = browserDate.getTimezoneOffset();
+		var siteOffset = (window.inlineEditPostConfig && inlineEditPostConfig.siteTzOffset)
+			? parseInt(inlineEditPostConfig.siteTzOffset, 10)
+			: browserOffset;
+		var siteDate = new Date( browserDate.getTime() + (browserOffset - siteOffset) * 60000 );
+		if ( 'future' !== status && siteDate > post_date ) {
 			$('select[name="_status"] option[value="future"]', editRow).remove();
 		} else {
 			$('select[name="_status"] option[value="publish"]', editRow).remove();
