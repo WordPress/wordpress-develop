cde5e6007418e9708545fdef7689335d9f81eabe
Matthew Reishus <mreishus@users.noreply.github.com>
2025-09-03 19:21:26 +0000

    style-engine: Add string check to prevent fatal errors from malformed block attributes

--- src/wp-includes/style-engine/class-wp-style-engine-css-declarations.php
+++ src/wp-includes/style-engine/class-wp-style-engine-css-declarations.php
@@ -58,6 +58,10 @@ class WP_Style_Engine_CSS_Declarations {
 		if ( empty( $property ) ) {
 			return $this;
 		}
+		// Bails early if the value is not a string.
+		if ( ! is_string( $value ) ) {
+			return $this;
+		}
 
 		// Trims the value. If empty, bail early.
 		$value = trim( $value );
