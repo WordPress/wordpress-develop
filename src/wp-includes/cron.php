d7eac5136d751d1e95eb267fb9df73f5aa4c238e
Henrik Urlund <henrik@urlund.com>
2025-09-05 21:09:13 +0200

    Remove extra space in get_option default array
    Cleaned up the default array parameter in get_option by removing an unnecessary space for consistency and code style.

--- src/wp-includes/cron.php
+++ src/wp-includes/cron.php
@@ -459,7 +459,7 @@ function wp_schedule_bulk_events( int $timestamp, string $hook, array $args, boo
 	}
 
 	// get current cron events
-	$bulk_value = $orig_value = get_option( 'cron', array( ) );
+	$bulk_value = $orig_value = get_option( 'cron', array() );
 
 	// when updating the cron option, keep a copy of the original value (to skip update for now)
 	$tmp_update_option = function ( $new_value, $old_value ) use ( &$bulk_value ) {
