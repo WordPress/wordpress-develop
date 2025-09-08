42d0e56f32db472127c1b52b65c6c71922401922
Jonny Harrus <jon@spacedmonkey.co.uk>
2025-09-08 13:39:32 +0100

    Add cache invalidation for post count queries using `last_changed`.

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -3386,7 +3386,7 @@ function wp_count_posts( $type = 'post', $perm = '' ) {
 	}
 
 	$cache_key    = _count_posts_cache_key( $type, $perm );
-	$last_changed = wp_cache_get_last_changed( 'posts' );
+	$last_changed = wp_cache_get_last_changed( $type . '_count' );
 
 	$counts = wp_cache_get_salted( $cache_key, 'post-queries', $last_changed );
 	if ( false !== $counts ) {
@@ -3462,7 +3462,7 @@ function wp_count_attachments( $mime_type = '' ) {
 		'attachments%s',
 		! empty( $mime_type ) ? ':' . str_replace( '/', '_', implode( '-', (array) $mime_type ) ) : ''
 	);
-	$last_changed = wp_cache_get_last_changed( 'posts' );
+	$last_changed = wp_cache_get_last_changed( 'attachment_count' );
 
 	$counts = wp_cache_get_salted( $cache_key, 'post-queries', $last_changed );
 
@@ -3890,6 +3890,8 @@ function wp_delete_post( $post_id = 0, $force_delete = false ) {
 	do_action( 'deleted_post', $post_id, $post );
 
 	clean_post_cache( $post );
+
+	wp_cache_set_post_count_last_changed( $post->post_type );
 
 	if ( is_post_type_hierarchical( $post->post_type ) && $children ) {
 		foreach ( $children as $child ) {
@@ -6723,6 +6725,7 @@ function wp_delete_attachment( $post_id, $force_delete = false ) {
 	wp_delete_attachment_files( $post_id, $meta, $backup_sizes, $file );
 
 	clean_post_cache( $post );
+	wp_cache_set_post_count_last_changed( $post->post_type );
 
 	return $post;
 }
@@ -7910,8 +7913,7 @@ function _transition_post_status( $new_status, $old_status, $post ) {
 	}
 
 	if ( $new_status !== $old_status ) {
-		wp_cache_delete( _count_posts_cache_key( $post->post_type ), 'post-queries' );
-		wp_cache_delete( _count_posts_cache_key( $post->post_type, 'readable' ), 'post-queries' );
+		wp_cache_set_post_count_last_changed( $post->post_type );
 	}
 
 	// Always clears the hook in case the post status bounced from future to draft.
@@ -8352,6 +8354,15 @@ function wp_add_trashed_suffix_to_post_name_for_post( $post ) {
 	$wpdb->update( $wpdb->posts, array( 'post_name' => $post_name ), array( 'ID' => $post->ID ) );
 	clean_post_cache( $post->ID );
 	return $post_name;
+}
+
+/**
+ * Sets the last changed time for the 'posts' cache group.
+ *
+ * @since 6.9.0
+ */
+function wp_cache_set_post_count_last_changed( $post_type ) {
+	wp_cache_set_last_changed( $post_type . '_count' );
 }
 
 /**
