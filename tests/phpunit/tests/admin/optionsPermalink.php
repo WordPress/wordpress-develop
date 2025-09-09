2b2a6c7108cc7d7774353c34a147ec805275c0d3
Himanshu Pathak <himanshu.pathak@rtcamp.com>
2025-09-09 14:47:49 +0530

    Tests: Setup the provider function

--- tests/phpunit/tests/admin/optionsPermalink.php
+++ tests/phpunit/tests/admin/optionsPermalink.php
@@ -7,4 +7,17 @@
  * @group rewrite
  */
 class Tests_Admin_OptionsPermalink extends WP_UnitTestCase {
+	/**
+	 * Data provider for base sanitization tests.
+	 */
+	public function data_base_sanitization() {
+		return [
+			[ 'Foo Bar', '/foo-bar' ],
+			[ 'Foo & Bar!', '/foo-bar' ],
+			[ 'Foo Bar/Baz Qux', '/foo-bar/baz-qux' ],
+			[ '', '' ],
+			[ '/Foo Bar', '/foo-bar' ],
+			[ 'Multiple/Slashes', '/multiple/slashes' ],
+		];
+	}
 }
