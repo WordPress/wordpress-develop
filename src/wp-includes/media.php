41f6098f6571dbed9c4645309f09f505b5cf3a17
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2025-09-10 11:11:28 +0530

    Refactor comments for thumbnail size fallback logic

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -825,10 +825,10 @@ function image_get_intermediate_size( $post_id, $size = 'thumbnail' ) {
 
 			$data = array_shift( $candidates );
 			/*
-			* When the size requested is smaller than the thumbnail dimensions, we
-			* fall back to the thumbnail size to maintain backward compatibility with
-			* pre 4.6 versions of WordPress.
-			*/
+			 * When the size requested is smaller than the thumbnail dimensions, we
+			 * fall back to the thumbnail size to maintain backward compatibility with
+			 * pre 4.6 versions of WordPress.
+			 */
 		} elseif ( ! empty( $imagedata['sizes']['thumbnail'] ) && $imagedata['sizes']['thumbnail']['width'] >= $size[0] && $imagedata['sizes']['thumbnail']['width'] >= $size[1] ) {
 			$data = $imagedata['sizes']['thumbnail'];
 		} else {
