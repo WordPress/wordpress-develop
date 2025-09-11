78e5483c707d509e3f3dc566188e370e9e5ed9f8
Pascal Birchler <pascalb@google.com>
2025-09-11 20:42:33 +0200

    More instances

--- src/wp-includes/class-wp-hook.php
+++ src/wp-includes/class-wp-hook.php
@@ -191,9 +191,13 @@ final class WP_Hook implements Iterator, ArrayAccess {
 	 * @return bool Whether the callback existed before it was removed.
 	 */
 	public function remove_filter( $hook_name, $callback, $priority ) {
+		if ( null === $priority ) {
+			$priority = 0;
+		}
+
 		$function_key = _wp_filter_build_unique_id( $hook_name, $callback, $priority );
 
-		$exists = isset( $this->callbacks[ $priority ][ $function_key ] );
+		$exists = isset( $function_key, $this->callbacks[ $priority ][ $function_key ] );
 
 		if ( $exists ) {
 			unset( $this->callbacks[ $priority ][ $function_key ] );
