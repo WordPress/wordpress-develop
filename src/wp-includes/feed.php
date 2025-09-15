91f3a3e737b76faebddf82dad74f81dbab584eac
Pascal Birchler <pascalb@google.com>
2025-09-15 14:47:07 +0200

    Use `get_registry()` instead of `set_sanitize_class`

--- src/wp-includes/feed.php
+++ src/wp-includes/feed.php
@@ -812,7 +812,8 @@ function fetch_feed( $url ) {
 
 	$feed = new SimplePie\SimplePie();
 
-	$feed->set_sanitize_class( 'WP_SimplePie_Sanitize_KSES' );
+	$feed->get_registry()->register(SimplePie\Sanitize::class, 'WP_SimplePie_Sanitize_KSES', true);
+
 	/*
 	 * We must manually overwrite $feed->sanitize because SimplePie's constructor
 	 * sets it before we have a chance to set the sanitization class.
