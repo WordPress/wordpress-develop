1ff250fe71563798c0be70deb6bbc3b7a6ff32ed
Joe Dolson <design@joedolson.com>
2025-09-15 17:21:50 -0500

    Update src/wp-includes/widgets/class-wp-widget-archives.php
    Co-authored-by: Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>

--- src/wp-includes/widgets/class-wp-widget-archives.php
+++ src/wp-includes/widgets/class-wp-widget-archives.php
@@ -131,7 +131,7 @@ class WP_Widget_Archives extends WP_Widget {
 	});
 	dropdown.addEventListener( 'click', function() {
 		dropdown.removeAttribute( 'data-lastkey' );
-    });
+	});
 	dropdown.onchange = onSelectChange;
 })();
 </script>
