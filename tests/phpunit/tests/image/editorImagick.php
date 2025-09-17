b949f8495c320561772b725fa8a6d8a140ca9d2a
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2025-09-16 21:01:54 -0400

    Re-add the PHP version constraint

--- tests/phpunit/tests/image/editorImagick.php
+++ tests/phpunit/tests/image/editorImagick.php
@@ -940,6 +940,10 @@ class Tests_Image_Editor_Imagick extends WP_Image_UnitTestCase {
 	 * Test AVIF quality filters.
 	 *
 	 * @ticket 61614
+	 *
+	 * Temporarily disabled until we can figure out why it fails on the Trixie based PHP container.
+	 * See https://core.trac.wordpress.org/ticket/63932.
+	 * @requires PHP < 8.3
 	 */
 	public function test_quality_with_avif_conversion_file_sizes() {
 		$temp_dir = get_temp_dir();
