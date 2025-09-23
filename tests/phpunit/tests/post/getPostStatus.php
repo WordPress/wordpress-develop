c3e2f0fed0ca9e3751da40361b5a92842bf07008
Paul Bonneau <bonneau-paul@hotmail.fr>
2025-09-23 10:35:12 +0200

    Adding tests and a doc block for the new  filter

--- tests/phpunit/tests/post/getPostStatus.php
+++ tests/phpunit/tests/post/getPostStatus.php
@@ -164,4 +164,52 @@ class Tests_Post_GetPostStatus extends WP_UnitTestCase {
 			array( 'delete-attachment', 'publish', 'publish' ),
 		);
 	}
+
+	/**
+	 * Ensure the `post_states_string` filter works to modify post state output.
+	 *
+	 * @ticket 51403
+	 * @dataProvider data_filter_post_states_string_should_enable_post_state_html_output_modification
+	 *
+	 * @param string $post_state The post state to test.
+	 */
+	public function test_filter_post_states_string_should_enable_post_state_html_output_modification( $post_state ) {
+		$post = get_post( self::$post_ids[ $post_state ] );
+
+		$random_string   = wp_generate_password( 12 );
+		$original_output = _post_states( $post, false );
+		$text_to_append  = '<span class="post-state">, ' . $random_string . '</span>';
+
+		add_filter(
+			'post_states_string',
+			function ( $post_states_string, $post ) use ( $text_to_append ) {
+				return $post_states_string . $text_to_append;
+			},
+			10,
+			2
+		);
+
+		$output = _post_states( $post, false );
+
+		$this->assertStringContainsString( $text_to_append, $output );
+		$this->assertStringContainsString( $original_output, $output );
+	}
+
+	/**
+	 * Data provider for test_filter_post_states_string_should_enable_post_state_html_output_modification().
+	 *
+	 * @return array[] {
+	 *     @type string $post_state The post state to test.
+	 * }
+	 */
+	public function data_filter_post_states_string_should_enable_post_state_html_output_modification() {
+		return array(
+			array( 'publish' ),
+			array( 'future' ),
+			array( 'draft' ),
+			array( 'auto-draft' ),
+			array( 'trash' ),
+			array( 'private' ),
+		);
+	}
 }
