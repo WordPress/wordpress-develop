7ee878ab68d29c5720b620534989d48632d3314e
Callum Bridgford-Whittick <callumbw@gmail.com>
2025-09-25 09:28:31 +0100

    coding standards
    Co-authored-by: John Parris <public@johnparris.com>

--- src/wp-admin/options-general.php
+++ src/wp-admin/options-general.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/admin.php';
 require_once ABSPATH . 'wp-admin/includes/translation-install.php';
 
 if ( ! current_user_can( 'manage_options' ) ) {
-	wp_die( __( 'Sorry, you are not allowed to manage options for this site.' ) ,403 );
+	wp_die( __( 'Sorry, you are not allowed to manage options for this site.' ), 403 );
 }
 
 // Used in the HTML title tag.
