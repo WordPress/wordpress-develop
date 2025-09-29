96de3d9d50662f6a9165b846fd77872dd2761d03
K. Adam White <kadamwhite@users.noreply.github.com>
2025-09-29 16:44:49 -0400

    64049: Skip content-emptiness check if content key is not present in comment object
    Permits patch updates which modify non-content values without incorrectly failing a partial object for having "empty" content

--- src/wp-includes/rest-api/endpoints/class-wp-rest-comments-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-comments-controller.php
@@ -1902,6 +1902,10 @@ class WP_REST_Comments_Controller extends WP_REST_Controller {
 	 * @return bool True if the content is allowed, false otherwise.
 	 */
 	protected function check_is_comment_content_allowed( $prepared_comment ) {
+		if ( ! isset( $prepared_comment['comment_content'] ) ) {
+			return true;
+		}
+
 		$check = wp_parse_args(
 			$prepared_comment,
 			array(
