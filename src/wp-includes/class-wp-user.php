2a3db8e7a730055b884a9dfd61d96f26cbe0c718
Jonny Harris <spacedmonkey@users.noreply.github.com>
2025-10-02 23:41:41 +0100

    Apply suggestions from code review

--- src/wp-includes/class-wp-user.php
+++ src/wp-includes/class-wp-user.php
@@ -37,7 +37,7 @@
  * @property string $rich_editing
  * @property string $syntax_highlighting
  * @property string $use_ssl
- * @property array<string, bool>|null $caps
+ * @property array<string, bool> $caps
  * @property string[] $roles
  * @property array<string, bool> $allcaps
  */
