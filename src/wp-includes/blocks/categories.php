8ef058f90e434ba0d68a4bc7bd43aa5acb62bf56
Weston Ruter <weston@ruter.net>
2025-10-03 21:25:35 -0700

    Revert change to block which will come from Gutenberg

--- src/wp-includes/blocks/categories.php
+++ src/wp-includes/blocks/categories.php
@@ -110,7 +110,6 @@ function build_dropdown_script_block_core_categories( $dropdown_id ) {
 		}
 		dropdown.onchange = onCatChange;
 	})();
-	//# sourceURL=<?php echo rawurlencode( __FUNCTION__ ); ?>
 	</script>
 	<?php
 	return wp_get_inline_script_tag( str_replace( array( '<script>', '</script>' ), '', ob_get_clean() ) );
