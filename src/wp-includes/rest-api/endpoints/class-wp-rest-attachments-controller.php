eb6218b3e9fb0449fec13bb11a02ddf364b73a36
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2025-10-07 09:47:32 +1100

    Try renaming to attached-to

--- src/wp-includes/rest-api/endpoints/class-wp-rest-attachments-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-attachments-controller.php
@@ -985,7 +985,7 @@ class WP_REST_Attachments_Controller extends WP_REST_Posts_Controller {
 			$post = get_post( $post->post_parent );
 
 			if ( ! empty( $post ) ) {
-				$links['https://api.w.org/post'] = array(
+				$links['https://api.w.org/attached-to'] = array(
 					'href'       => rest_url( rest_get_route_for_post( $post ) ),
 					'embeddable' => true,
 					'post_type'  => $post->post_type,
