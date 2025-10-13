92c44f4c30fa0825d99c7c0d97c015232836f5f1
Weston Ruter <weston@ruter.net>
2025-10-13 16:37:14 -0700

    Avoid needless calls to array_unique() and array_merge() when there is nothing to merge

--- src/wp-includes/class-wp-block.php
+++ src/wp-includes/class-wp-block.php
@@ -676,23 +676,35 @@ class WP_Block {
 		wp_styles()->queue         = $before_styles_queue;
 		wp_scripts()->queue        = $before_scripts_queue;
 		wp_script_modules()->queue = $before_script_modules_queue;
+		$has_new_styles            = count( $new_styles_queue ) > 0;
+		$has_new_scripts           = count( $new_scripts_queue ) > 0;
+		$has_new_script_modules    = count( $new_script_modules_queue ) > 0;
 
 		// Merge the newly enqueued assets with the existing assets if the rendered block is not empty.
 		if (
-			trim( $block_content ) !== '' ||
-			/**
-			 * Filters whether to enqueue assets for a block which has no rendered content.
-			 *
-			 * @since 6.9.0
-			 *
-			 * @param bool   $enqueue    Whether to enqueue assets.
-			 * @param string $block_name Block name.
-			 */
-			(bool) apply_filters( 'enqueue_empty_block_content_assets', false, $this->name )
+			( $has_new_styles || $has_new_scripts || $has_new_script_modules ) &&
+			(
+				trim( $block_content ) !== '' ||
+				/**
+				 * Filters whether to enqueue assets for a block which has no rendered content.
+				 *
+				 * @since 6.9.0
+				 *
+				 * @param bool   $enqueue    Whether to enqueue assets.
+				 * @param string $block_name Block name.
+				 */
+				(bool) apply_filters( 'enqueue_empty_block_content_assets', false, $this->name )
+			)
 		) {
-			wp_styles()->queue         = array_unique( array_merge( wp_styles()->queue, $new_styles_queue ) );
-			wp_scripts()->queue        = array_unique( array_merge( wp_scripts()->queue, $new_scripts_queue ) );
-			wp_script_modules()->queue = array_unique( array_merge( wp_script_modules()->queue, $new_script_modules_queue ) );
+			if ( $has_new_styles ) {
+				wp_styles()->queue = array_unique( array_merge( wp_styles()->queue, $new_styles_queue ) );
+			}
+			if ( $has_new_scripts ) {
+				wp_scripts()->queue = array_unique( array_merge( wp_scripts()->queue, $new_scripts_queue ) );
+			}
+			if ( $has_new_script_modules ) {
+				wp_script_modules()->queue = array_unique( array_merge( wp_script_modules()->queue, $new_script_modules_queue ) );
+			}
 		}
 
 		return $block_content;
