0118303c8da97b99469f8763d68f15d7ecf30b6f
Peter Wilson <git-commit-by@peterwilson.cc>
2025-10-15 11:04:57 +1100

    Split the tests.

--- tests/phpunit/tests/taxonomy/cleanTaxonomyCache.php
+++ tests/phpunit/tests/taxonomy/cleanTaxonomyCache.php
@@ -12,9 +12,8 @@ class Tests_Taxonomy_CleanTaxonomyCache extends WP_UnitTestCase {
 	 * @ticket 64090
 	 */
 	public function test_hierarchy_cache_not_cleared_for_non_hierarchical_taxonomy() {
-		// Prime the tag and category caches by inserting terms.
+		// Prime the tag cache by inserting terms.
 		wp_insert_term( 'foo', 'post_tag' );
-		wp_insert_term( 'foo', 'category' );
 
 		/*
 		 * Determine if delete_option( "{$taxonomy}_children" ) is called.
@@ -35,6 +34,27 @@ class Tests_Taxonomy_CleanTaxonomyCache extends WP_UnitTestCase {
 			}
 		);
 
+		clean_taxonomy_cache( 'post_tag' );
+
+		$this->assertFalse( $delete_post_tag_children_called, 'An attempt to clear the post_tag_children option should not be made.' );
+	}
+
+	/**
+	 * Ensure clearing the cache of a hierarchical taxonomy does attempt to delete the hierarchy cache.
+	 *
+	 * @ticket 64090
+	 */
+	public function test_hierarchy_cache_cleared_for_hierarchical_taxonomy() {
+		// Prime the category cache by inserting terms.
+		wp_insert_term( 'foo', 'category' );
+
+		/*
+		 * Determine if delete_option( "{$taxonomy}_children" ) is called.
+		 *
+		 * None of the actions in delete_option() or _get_term_hierarchy() fire for
+		 * non-hierarchical taxonomies, so the query filter needs to be used to determine
+		 * if an attempt to delete the option was made.
+		 */
 		$delete_category_children_called = false;
 		add_filter(
 			'query',
@@ -47,11 +67,8 @@ class Tests_Taxonomy_CleanTaxonomyCache extends WP_UnitTestCase {
 			}
 		);
 
-		// Clear the tag cache and category cache.
-		clean_taxonomy_cache( 'post_tag' );
 		clean_taxonomy_cache( 'category' );
 
 		$this->assertTrue( $delete_category_children_called, 'An attempt to clear the category_children option should be made.' );
-		$this->assertFalse( $delete_post_tag_children_called, 'An attempt to clear the post_tag_children option should not be made.' );
 	}
 }
