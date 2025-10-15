8b63fc71f83ab05c0066ed2275358bf1b257d7ec
Samuel Paget <sjapaget@gmail.com>
2025-10-15 22:01:24 +0200

    fixes from review

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -3467,3 +3467,79 @@ function wp_remove_surrounding_empty_script_tags( $contents ) {
 		);
 	}
 }
+
+/**
+ * Adds filters to ensure that block styles are loaded on demand in classic themes.
+ *
+ * @since 6.9.0
+ */
+function wp_always_load_block_styles_on_demand_init() {
+	if ( wp_is_block_theme() ) {
+		return;
+	}
+	/*
+	 * Make sure that wp_should_output_buffer_template_for_enhancement() returns true even if there aren't any
+	 * `wp_template_enhancement_output_buffer` filters added, but do so at priority zero so that applications which
+	 * wish to stream responses can more easily turn this off.
+	 */
+	add_filter( 'wp_should_output_buffer_template_for_enhancement', '__return_true', 0 );
+
+	if ( ! wp_should_output_buffer_template_for_enhancement() ) {
+		return;
+	}
+
+	// Load separate block styles so that the large block-library stylesheet is not enqueued unconditionally,
+	// and so that block-specific styles will only be enqueued when they are used on the page.
+	add_filter( 'should_load_separate_core_block_assets', '__return_true' );
+
+	// Also ensure that block assets are loaded on demand (although the default value is should_load_separate_core_block_assets).
+	add_filter( 'should_load_block_assets_on_demand', '__return_true' );
+
+	// Add hooks which require the presence of the output buffer. Ideally the above two filters could be added here, but they run too early.
+	add_action( 'wp_template_enhancement_output_buffer_started', 'wp_use_placeholder_for_delayed_css' );
+}
+
+/**
+ * Adds the hooks needed for CSS output to be delayed until after the content of the page has been established.
+ *
+ * @since 6.9.0
+ */
+function wp_use_placeholder_for_delayed_css(): void {
+
+	// While normally late styles are printed, there is a filter to disable late styles, so this makes sure they are printed.
+	add_filter( 'print_late_styles', '__return_true', 100 );
+
+	// Print a placeholder comment to inject late styles right after the head styles are printed.
+	$placeholder = sprintf( '<!--%s:%s-->', 'late_styles', wp_generate_uuid4() );
+	remove_action( 'wp_head', 'wp_print_styles', 8 );
+	add_action(
+		'wp_head',
+		static function () use ( $placeholder ) {
+			wp_print_styles();
+			echo $placeholder;
+		},
+		8
+	);
+
+	// Replace logic that prints scripts and styles in the footer.
+	$late_styles = '';
+	remove_action( 'wp_print_footer_scripts', '_wp_footer_scripts' );
+	add_action(
+		'wp_print_footer_scripts',
+		static function () use ( &$late_styles ) {
+			ob_start();
+			print_late_styles();
+			$late_styles = ob_get_clean();
+
+			print_footer_scripts();
+		}
+	);
+
+	// Replace placeholder with the captured late styles.
+	add_filter(
+		'wp_template_enhancement_output_buffer',
+		static function ( $buffer ) use ( $placeholder, &$late_styles ) {
+			return str_replace( $placeholder, $late_styles, $buffer );
+		}
+	);
+}
