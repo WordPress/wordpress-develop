f49563c7a20f3fb1d3e6115cb20cdc2268cc6b40
Weston Ruter <weston@ruter.net>
2025-10-15 07:35:19 -0700

    Remove whitespace at end of line

--- src/wp-includes/template.php
+++ src/wp-includes/template.php
@@ -907,7 +907,7 @@ function wp_start_template_enhancement_output_buffer(): bool {
  *
  * Checks to see if the output buffer is complete and contains HTML. If so, runs the content through
  * the `wp_template_enhancement_output_buffer` filter.  If not, the original content is returned.
- * 
+ *
  * @since 6.9.0
  *
  * @see wp_start_template_enhancement_output_buffer()
