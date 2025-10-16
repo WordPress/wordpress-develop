0aa3c0fb73c230ec870ede5886e4d8b3832a0ad5
Sam Seay <samueljseay@gmail.com>
2025-10-16 17:53:03 +1300

    Update src/wp-includes/class-wp-script-modules.php
    Co-authored-by: David Arenas <darensr@gmail.com>

--- src/wp-includes/class-wp-script-modules.php
+++ src/wp-includes/class-wp-script-modules.php
@@ -220,8 +220,9 @@ class WP_Script_Modules {
 	 * @param array             $args     {
 	 *     Optional. An array of additional args. Default empty array.
 	 *
-	 *     @type 'auto'|'low'|'high' $fetchpriority Fetch priority. Default 'auto'. Optional.
-	 *     @type bool $load_on_client_navigation Whether the script module should be loaded on client navigation. Default false. Optional.
+	 *     @type 'auto'|'low'|'high' $fetchpriority             Fetch priority. Default 'auto'. Optional.
+	 *     @type bool                $load_on_client_navigation Whether the script module should be loaded on client
+	 *                                                          navigation. Default false. Optional.
 	 * }
 	 */
 	public function enqueue( string $id, string $src = '', array $deps = array(), $version = false, array $args = array() ) {
