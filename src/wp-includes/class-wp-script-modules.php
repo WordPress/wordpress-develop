de83bd2342c765500076f3897e1cddccbe847af3
Weston Ruter <weston@ruter.net>
2025-10-20 10:28:31 -0700

    Restore $position variable containing action name
    Co-authored-by: John Parris <mindctrl@git.wordpress.org>

--- src/wp-includes/class-wp-script-modules.php
+++ src/wp-includes/class-wp-script-modules.php
@@ -336,7 +336,8 @@ class WP_Script_Modules {
 	 */
 	public function add_hooks() {
 		$is_block_theme = wp_is_block_theme();
-		add_action( $is_block_theme ? 'wp_head' : 'wp_footer', array( $this, 'print_import_map' ) );
+		$position       = $is_block_theme ? 'wp_head' : 'wp_footer';
+		add_action( $position, array( $this, 'print_import_map' ) );
 		if ( $is_block_theme ) {
 			// Modules can only be printed in the head for block themes because only with
 			// block themes will import map be fully populated by modules discovered by
@@ -346,7 +347,7 @@ class WP_Script_Modules {
 			add_action( 'wp_head', array( $this, 'print_head_enqueued_script_modules' ) );
 		}
 		add_action( 'wp_footer', array( $this, 'print_enqueued_script_modules' ) );
-		add_action( $is_block_theme ? 'wp_head' : 'wp_footer', array( $this, 'print_script_module_preloads' ) );
+		add_action( $position, array( $this, 'print_script_module_preloads' ) );
 
 		add_action( 'admin_print_footer_scripts', array( $this, 'print_import_map' ) );
 		add_action( 'admin_print_footer_scripts', array( $this, 'print_enqueued_script_modules' ) );
