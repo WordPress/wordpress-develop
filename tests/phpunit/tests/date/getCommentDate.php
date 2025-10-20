05d09e10864a9b8a4902b451487057c98706556c
Paul Bearne <pbearne@gmail.com>
2025-10-20 14:48:54 -0400

    Update tests/phpunit/tests/date/getCommentDate.php
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- tests/phpunit/tests/date/getCommentDate.php
+++ tests/phpunit/tests/date/getCommentDate.php
@@ -34,7 +34,6 @@ class Tests_Date_GetCommentDate extends WP_UnitTestCase {
 	public function test_get_comment_time_returns_correct_time() {
 		$c = self::factory()->comment->create( array( 'comment_date' => '2020-08-29 01:51:00' ) );
 
-		// $GLOBALS['comment'] = get_comment( $c );
 		$this->assertSame( '1:51 am', get_comment_time( 'g:i a', 'local', false, $c ) );
 	}
 
