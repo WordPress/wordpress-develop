d538d617c2f74da44fa332f948d7a42d2b7591b3
Aaron Jorbin <622599+aaronjorbin@users.noreply.github.com>
2025-10-21 01:49:35 +0200

    try reverts again

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -886,10 +886,6 @@ function upgrade_all() {
 		upgrade_682();
 	}
 
-	if ( $wp_current_db_version < 60717 ) {
-		upgrade_690();
-	}
-
 	maybe_disable_link_manager();
 
 	maybe_disable_automattic_widgets();
@@ -2418,7 +2414,6 @@ function upgrade_650() {
 		wp_set_option_autoload_values( $autoload );
 	}
 }
-
 /**
  * Executes changes made in WordPress 6.7.0.
  *
@@ -2482,31 +2477,6 @@ function upgrade_682() {
 		$ping_sites_value = array_filter( $ping_sites_value );
 		$ping_sites_value = implode( "\n", $ping_sites_value );
 		update_option( 'ping_sites', $ping_sites_value );
-	}
-}
-
-/**
- * Executes changes made in WordPress 6.9.0.
- *
- * @ignore
- * @since 6.9.0
- *
- * @global int $wp_current_db_version The old (current) database version.
- */
-function upgrade_690() {
-	global $wp_current_db_version;
-
-	if ( $wp_current_db_version < 60717 ) {
-		// Switch Hello Dolly from file to directory format. See #53323
-		$active_plugins = (array) get_option( 'active_plugins', array() );
-		$old_plugin     = 'hello.php';
-		$new_plugin     = 'hello-dolly/hello.php';
-		$key            = array_search( $old_plugin, $active_plugins, true );
-
-		if ( $key ) {
-			$active_plugins[ $key ] = $new_plugin;
-			update_option( 'active_plugins', $active_plugins );
-		}
 	}
 }
 
@@ -2962,10 +2932,8 @@ function dbDelta( $queries = '', $execute = true ) { // phpcs:ignore WordPress.N
 	// Create a tablename index for an array ($cqueries) of recognized query types.
 	foreach ( $queries as $qry ) {
 		if ( preg_match( '|CREATE TABLE ([^ ]*)|', $qry, $matches ) ) {
-			$table_name = trim( $matches[1], '`' );
-
-			$cqueries[ $table_name ]   = $qry;
-			$for_update[ $table_name ] = 'Created table ' . $matches[1];
+			$cqueries[ trim( $matches[1], '`' ) ] = $qry;
+			$for_update[ $matches[1] ]            = 'Created table ' . $matches[1];
 			continue;
 		}
 
@@ -3203,7 +3171,7 @@ function dbDelta( $queries = '', $execute = true ) { // phpcs:ignore WordPress.N
 				$fieldtype_base = strtok( $fieldtype_without_parentheses, ' ' );
 
 				// Is actual field type different from the field type in query?
-				if ( $tablefield->Type !== $fieldtype_lowercased ) {
+				if ( $tablefield->Type !== $fieldtype ) {
 					$do_change = true;
 					if ( in_array( $fieldtype_lowercased, $text_fields, true ) && in_array( $tablefield_type_lowercased, $text_fields, true ) ) {
 						if ( array_search( $fieldtype_lowercased, $text_fields, true ) < array_search( $tablefield_type_lowercased, $text_fields, true ) ) {
@@ -3282,7 +3250,7 @@ function dbDelta( $queries = '', $execute = true ) { // phpcs:ignore WordPress.N
 					'fieldname' => $tableindex->Column_name,
 					'subpart'   => $tableindex->Sub_part,
 				);
-				$index_ary[ $keyname ]['unique']     = ( '0' === (string) $tableindex->Non_unique ) ? true : false;
+				$index_ary[ $keyname ]['unique']     = ( '0' === $tableindex->Non_unique ) ? true : false;
 				$index_ary[ $keyname ]['index_type'] = $tableindex->Index_type;
 			}
 
