93fa4f87771fa4c74a723215e8debcba46e959c1
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2025-10-24 10:34:21 +0530

    Remove test_feed_links_extra_handles_custom_post_type
    Removed test for custom post type handling in feed links extra.

--- tests/phpunit/tests/general/feedLinksExtra.php
+++ tests/phpunit/tests/general/feedLinksExtra.php
@@ -645,20 +645,4 @@ class Tests_General_FeedLinksExtra extends WP_UnitTestCase {
 
 		$this->assertNotEmpty( get_echo( 'feed_links_extra' ) );
 	}
-
-	/**
-	 * @ticket 63263
-	 */
-	public function test_feed_links_extra_handles_custom_post_type() {
-		$post_id = self::factory()->post->create(
-			array(
-				'post_type'  => self::$post_with_cpt_id,
-				'post_title' => 'Test post for custom post type feed links extra',
-			)
-		);
-
-		$this->go_to( get_permalink( $post_id ) );
-		$GLOBALS['post'] = null;
-		$this->assertNotEmpty( get_echo( 'feed_links_extra' ) );
-	}
 }
