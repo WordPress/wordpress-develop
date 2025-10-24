cb8cdbee5096aaad9b1b4f2ca66d2b8aed9135e8
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-10-23 21:59:17 -0600

    reorder @since

--- src/wp-includes/class-wp-comment-query.php
+++ src/wp-includes/class-wp-comment-query.php
@@ -536,10 +536,9 @@ class WP_Comment_Query {
 	 * Used internally to get a list of comment IDs matching the query vars.
 	 *
 	 * @since 4.4.0
+	 * @since 6.9.0 Excludes the 'note' comment type, unless 'all' or the 'note' types are requested.
 	 *
 	 * @global wpdb $wpdb WordPress database abstraction object.
-	 *
-	 * @since 6.9.0 Excludes the 'note' comment type, unless 'all' or the 'note'types are requested.
 	 *
 	 * @return int|array A single count of comment IDs if a count query. An array of comment IDs if a full query.
 	 */
