6308437cbdf0f157e370281492b522c1ee4b1b10
Weston Ruter <weston@ruter.net>
2025-10-28 21:25:54 -0700

    Remove PHPCompatibility.FunctionDeclarations.NewClosure.ThisFoundOutsideClass suppression for PHPCS
    See https://github.com/WordPress/wordpress-develop/pull/10288#discussion_r2469686284

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -3685,9 +3685,8 @@ function wp_hoist_late_printed_styles() {
 			// Anonymous subclass of WP_HTML_Tag_Processor which exposes underlying bookmark spans.
 			$processor = new class( $buffer ) extends WP_HTML_Tag_Processor {
 				public function get_span(): WP_HTML_Span {
-					$instance = $this; // phpcs:ignore PHPCompatibility.FunctionDeclarations.NewClosure.ThisFoundOutsideClass -- It is inside an anonymous class. This can be removed once <https://github.com/PHPCompatibility/PHPCompatibility/issues/1481> is released in v10.
-					$instance->set_bookmark( 'here' );
-					return $instance->bookmarks['here'];
+					$this->set_bookmark( 'here' );
+					return $this->bookmarks['here'];
 				}
 			};
 
