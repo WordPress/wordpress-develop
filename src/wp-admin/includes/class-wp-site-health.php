5bb378f0d1ca2b602c2728ecbc6ec5f38ebc7708
Weston Ruter <weston@ruter.net>
2025-10-31 19:48:07 -0700

    Fix indentation

--- src/wp-admin/includes/class-wp-site-health.php
+++ src/wp-admin/includes/class-wp-site-health.php
@@ -770,11 +770,11 @@ class WP_Site_Health {
 			return $result;
 		}
 
-			$result['description'] .= '<p>' . sprintf(
-				/* translators: %s: The minimum recommended PHP version. */
-				__( 'The minimum recommended version of PHP is %s.' ),
-				$response['recommended_version']
-			) . '</p>';
+		$result['description'] .= '<p>' . sprintf(
+			/* translators: %s: The minimum recommended PHP version. */
+			__( 'The minimum recommended version of PHP is %s.' ),
+			$response['recommended_version']
+		) . '</p>';
 
 		// PHP is up to date.
 		if ( version_compare( PHP_VERSION, $response['recommended_version'], '>=' ) ) {
