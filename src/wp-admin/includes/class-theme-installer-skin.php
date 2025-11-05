51e0effa31446c6fe316110a6f596c0a36964444
Nino Mihovilic <nino.mihovilic@automattic.com>
2025-11-05 11:24:34 +0100

    Themes: Add is block theme check installer.
    Add the is block theme check in installer check.
    
    See #64159.

--- src/wp-admin/includes/class-theme-installer-skin.php
+++ src/wp-admin/includes/class-theme-installer-skin.php
@@ -121,7 +121,7 @@ class Theme_Installer_Skin extends WP_Upgrader_Skin {
 
 		$install_actions = array();
 
-		if ( current_user_can( 'edit_theme_options' ) && current_user_can( 'customize' ) ) {
+		if ( current_user_can( 'edit_theme_options' ) && ( $theme_info->is_block_theme() || current_user_can( 'customize' ) ) ) {
 			if ( $theme_info->is_block_theme() ) {
 				$customize_url = add_query_arg(
 					array(
