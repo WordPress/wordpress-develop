8ab218a000ad5b4f4dcc46cafa0e16f334b9b10b
Paul Bearne <pbearne@git.wordpress.org>
2025-11-05 14:15:46 -0500

    changed to use datasets

--- tests/phpunit/tests/functions/wpTimezoneChoice.php
+++ tests/phpunit/tests/functions/wpTimezoneChoice.php
@@ -9,78 +9,77 @@
  */
 class Tests_Functions_wpTimezoneChoice extends WP_UnitTestCase {
 
+	public function tear_down() {
+
+		restore_current_locale();
+	}
 	/**
 	 * Default values.
 	 *
 	 * @ticket 59941
+	 *
+	 * @dataProvider data_wp_timezone_choice
 	 */
-	public function test_wp_timezone_choice() {
-		// Test selecting a timezone from the list.
-		$timezone_list            = wp_timezone_choice( '' );
-		$selected_timezone_option = '<option selected="selected" value="">Select a city</option>';
-		$this->assertStringContainsString( $selected_timezone_option, $timezone_list );
+	public function test_wp_timezone_choice( $expected ) {
+		$timezone_list = wp_timezone_choice( '' );
+		$this->assertStringContainsString( $expected, $timezone_list );
+	}
 
-		$selected_timezone_option = '<option value="America/Los_Angeles">Los Angeles</option>';
-		$this->assertStringContainsString( $selected_timezone_option, $timezone_list );
-
-		// Test selecting a deprecated timezone.
-		$deprecated_timezone_option = '<option value="Pacific/Honolulu">Honolulu</option>';
-		$this->assertStringContainsString( $deprecated_timezone_option, $timezone_list );
-
-		// Test selecting a manual offset.
-		$manual_offset_option = '<option value="UTC-8">UTC-8</option>';
-		$this->assertStringContainsString( $manual_offset_option, $timezone_list );
-
-		// Test selecting UTC.
-		$utc_option = '<option value="UTC">UTC</option>';
-		$this->assertStringContainsString( $utc_option, $timezone_list );
-
-		// Test selecting a timezone from a continent.
-		$timezone_from_continent_option = '<option value="Africa/Johannesburg">Johannesburg</option>';
-		$this->assertStringContainsString( $timezone_from_continent_option, $timezone_list );
-
-		// Test selecting a timezone from a specific city.
-		$timezone_from_city_option = '<option value="Asia/Kuala_Lumpur">Kuala Lumpur</option>';
-		$this->assertStringContainsString( $timezone_from_city_option, $timezone_list );
-
-		// Test selecting a timezone from a specific city and sub-city.
-		$timezone_from_city_and_subcity_option = '<option value="America/Argentina/Buenos_Aires">Argentina - Buenos Aires</option>';
-		$this->assertStringContainsString( $timezone_from_city_and_subcity_option, $timezone_list );
-
-		// Test a translated city.
-		$timezone_from_city_es = '<option value="Pacific/Port_Moresby">Port Moresby</option>';
-		$this->assertStringContainsString( $timezone_from_city_es, $timezone_list );
+	/**
+	 * Datasets for test_wp_timezone_choice.
+	 *
+	 * @return array
+	 */
+	public function data_wp_timezone_choice() {
+		return array(
+			'placeholder option' => array('<option selected="selected" value="">Select a city</option>'),
+			'city in Americas' => array('<option value="America/Los_Angeles">Los Angeles</option>'),
+			'deprecated timezone' => array('<option value="Pacific/Honolulu">Honolulu</option>'),
+			'manual offset example' => array('<option value="UTC-8">UTC-8</option>'),
+			'UTC option' => array('<option value="UTC">UTC</option>'),
+			'continent example' => array('<option value="Africa/Johannesburg">Johannesburg</option>'),
+			'city example' => array('<option value="Asia/Kuala_Lumpur">Kuala Lumpur</option>'),
+			'city with sub-city' => array('<option value="America/Argentina/Buenos_Aires">Argentina - Buenos Aires</option>'),
+			'translated city name appears' => array('<option value="Pacific/Port_Moresby">Port Moresby</option>'),
+		);
 	}
 
 	/**
 	 * Zones are selected from the list.
 	 *
 	 * @ticket 59941
+	 *
+	 * @dataProvider data_wp_timezone_choice_selected
 	 */
-	public function test_wp_timezone_choice_selected() {
-		// Test selecting a city from the list.
-		$selected_zone = 'America/Los_Angeles';
-		$expected      = '<option selected="selected" value="America/Los_Angeles">Los Angeles</option>';
-		$actual        = wp_timezone_choice( $selected_zone );
+	public function test_wp_timezone_choice_selected( $selected_zone, $expected ) {
+		$actual = wp_timezone_choice( $selected_zone );
 		$this->assertStringContainsString( $expected, $actual );
+	}
 
-		// Test selecting a deprecated, but valid, timezone string.
-		$selected_zone = 'Pacific/Auckland';
-		$expected      = '<option selected="selected" value="Pacific/Auckland">Auckland</option>';
-		$actual        = wp_timezone_choice( $selected_zone );
-		$this->assertStringContainsString( $expected, $actual );
-
-		// Test selecting UTC.
-		$selected_zone = 'UTC';
-		$expected      = '<option selected="selected" value="UTC">UTC</option>';
-		$actual        = wp_timezone_choice( $selected_zone );
-		$this->assertStringContainsString( $expected, $actual );
-
-		// Test selecting a manual UTC offset.
-		$selected_zone = 'UTC+10';
-		$expected      = '<option selected="selected" value="UTC+10">UTC+10</option>';
-		$actual        = wp_timezone_choice( $selected_zone );
-		$this->assertStringContainsString( $expected, $actual );
+	/**
+	 * Datasets for test_wp_timezone_choice_selected.
+	 *
+	 * @return array
+	 */
+	public function data_wp_timezone_choice_selected() {
+		return array(
+			'city from the list' => array(
+				'America/Los_Angeles',
+				'<option selected="selected" value="America/Los_Angeles">Los Angeles</option>',
+			),
+			'deprecated but valid timezone string' => array(
+				'Pacific/Auckland',
+				'<option selected="selected" value="Pacific/Auckland">Auckland</option>',
+			),
+			'UTC' => array(
+				'UTC',
+				'<option selected="selected" value="UTC">UTC</option>',
+			),
+			'manual UTC offset' => array(
+				'UTC+10',
+				'<option selected="selected" value="UTC+10">UTC+10</option>',
+			),
+		);
 	}
 
 	/**
@@ -88,42 +87,56 @@ class Tests_Functions_wpTimezoneChoice extends WP_UnitTestCase {
 	 *
 	 * @ticket 59941
 	 */
-	public function test_wp_timezone_choice_es() {
-		// Test selecting a timezone from the list.
-		$timezone_list            = wp_timezone_choice( '', 'es_ES' );
-		$selected_timezone_option = '<option selected="selected" value="">Select a city</option>';
-		$this->assertStringContainsString( $selected_timezone_option, $timezone_list );
+	/**
+	 * Test passing the locale in.
+	 *
+	 * @ticket 59941
+	 *
+	 * @dataProvider data_wp_timezone_choice_es
+	 */
+	public function test_wp_timezone_choice_es( $expected ) {
+		$timezone_list = wp_timezone_choice( '', 'es_ES' );
+		$this->assertStringContainsString( $expected, $timezone_list );
+	}
 
-		$timezone_from_city_es = '<option value="Pacific/Port_Moresby">Puerto Moresby</option>';
-		$this->assertStringContainsString( $timezone_from_city_es, $timezone_list );
-
-		$timezone_from_city_es = '<optgroup label="Ártico">';
-		$this->assertStringContainsString( $timezone_from_city_es, $timezone_list );
-
-		$timezone_from_city_es = '<optgroup label="Manual Offsets">';
-		$this->assertStringContainsString( $timezone_from_city_es, $timezone_list );
+	/**
+	 * Datasets for test_wp_timezone_choice_es.
+	 *
+	 * @return array
+	 */
+	public function data_wp_timezone_choice_es() {
+		return array(
+			'placeholder remains in English (no translation override passed)' => array('<option selected="selected" value="">Select a city</option>'),
+			'spanish city translation' => array('<option value="Pacific/Port_Moresby">Puerto Moresby</option>'),
+			'spanish optgroup Arctic' => array('<optgroup label="Ártico">'),
+			'spanish optgroup Manual Offsets untranslated' => array('<optgroup label="Manual Offsets">'),
+		);
 	}
 
 	/**
 	 * Set the locale globally.
 	 *
 	 * @ticket 59941
+	 *
+	 * @dataProvider data_wp_timezone_choice_es_set
 	 */
-	public function test_wp_timezone_choice_es_set() {
-
+	public function test_wp_timezone_choice_es_set( $expected ) {
 		switch_to_locale( 'es_ES' );
-		// Test selecting a timezone from the list.
-		$timezone_list            = wp_timezone_choice( '' );
-		$selected_timezone_option = '<option selected="selected" value="">Elige una ciudad</option>';
-		$this->assertStringContainsString( $selected_timezone_option, $timezone_list );
+		$timezone_list = wp_timezone_choice( '' );
+		$this->assertStringContainsString( $expected, $timezone_list );
+	}
 
-		$timezone_from_city_es = '<option value="Pacific/Port_Moresby">Puerto Moresby</option>';
-		$this->assertStringContainsString( $timezone_from_city_es, $timezone_list );
-
-		$timezone_from_city_es = '<optgroup label="Ártico">';
-		$this->assertStringContainsString( $timezone_from_city_es, $timezone_list );
-
-		$timezone_from_city_es = '<optgroup label="Compensaciones manuales">';
-		$this->assertStringContainsString( $timezone_from_city_es, $timezone_list );
+	/**
+	 * Datasets for test_wp_timezone_choice_es_set.
+	 *
+	 * @return array
+	 */
+	public function data_wp_timezone_choice_es_set() {
+		return array(
+			'placeholder in Spanish' => array('<option selected="selected" value="">Elige una ciudad</option>'),
+			'spanish city translation' => array('<option value="Pacific/Port_Moresby">Puerto Moresby</option>'),
+			'spanish optgroup Arctic' => array('<optgroup label="Ártico">'),
+			'spanish optgroup Manual Offsets' => array('<optgroup label="Compensaciones manuales">'),
+		);
 	}
 }
