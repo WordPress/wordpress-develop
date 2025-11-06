34f68ab31a06cc6adfe550dfe141f073d5d5eed4
Asadister <asadister@gmail.com>
2025-11-06 16:19:22 +0330

    Add unit tests for LTR CSS support including metadata and output rendering. Props @asadister. See #64193

---
+++ tests/phpunit/tests/dependencies/stylesLTRSupport.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @group dependencies
+ * @group i18n
+ */
+class Tests_Dependencies_StylesLtrSupport extends WP_UnitTestCase {
+
+	public function set_up(): void {
+		parent::set_up();
+		switch_to_locale( 'fa_IR' ); // RTL language
+		wp_styles()->registered = [];
+	}
+
+	public function tear_down(): void {
+		restore_previous_locale();
+		parent::tear_down();
+	}
+
+	public function test_ltr_css_replace_data_is_set() {
+		$handle = 'sample-style';
+		wp_register_style( $handle, 'https://example.com/style.css' );
+		wp_style_add_data( $handle, 'ltr', 'replace' );
+
+		$styles = wp_styles();
+		$this->assertArrayHasKey( 'ltr', $styles->registered[ $handle ]->extra );
+		$this->assertSame( 'replace', $styles->registered[ $handle ]->extra['ltr'] );
+	}
+
+	public function test_ltr_css_suffix_data_is_set() {
+		$handle = 'sample-style-suffix';
+		wp_register_style( $handle, 'https://example.com/style.min.css' );
+		wp_style_add_data( $handle, 'ltr', 'suffix' );
+
+		$styles = wp_styles();
+		$this->assertArrayHasKey( 'ltr', $styles->registered[ $handle ]->extra );
+		$this->assertSame( 'suffix', $styles->registered[ $handle ]->extra['ltr'] );
+	}
+
+	public function test_no_ltr_data_for_ltr_locale() {
+		restore_previous_locale();
+		switch_to_locale( 'en_US' ); // LTR language
+
+		$handle = 'sample-style-ltr';
+		wp_register_style( $handle, 'https://example.com/style.css' );
+		wp_style_add_data( $handle, 'ltr', 'replace' );
+
+		$styles = wp_styles();
+		$this->assertArrayHasKey( 'ltr', $styles->registered[ $handle ]->extra );
+		$this->assertSame( 'replace', $styles->registered[ $handle ]->extra['ltr'] );
+	}
+}
