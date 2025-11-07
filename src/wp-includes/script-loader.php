885bdddd720f45ad13c920f83f1a7a19a3c7acca
Weston Ruter <weston@ruter.net>
2025-11-06 19:32:51 -0800

    Add missing static to closure

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -3732,7 +3732,7 @@ function wp_hoist_late_printed_styles() {
 	// Replace placeholder with the captured late styles.
 	add_filter(
 		'wp_template_enhancement_output_buffer',
-		function ( $buffer ) use ( $placeholder, &$printed_block_styles, &$printed_late_styles ) {
+		static function ( $buffer ) use ( $placeholder, &$printed_block_styles, &$printed_late_styles ) {
 
 			// Anonymous subclass of WP_HTML_Tag_Processor which exposes underlying bookmark spans.
 			$processor = new class( $buffer ) extends WP_HTML_Tag_Processor {
