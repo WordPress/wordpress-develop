5f1ea9a705c6abeb368852b20a68817c9020d581
Shail Mehta <shailmehta25@gmail.com>
2025-11-08 10:51:39 +0530

    Fixed Version Issue

--- src/wp-content/themes/twentytwelve/functions.php
+++ src/wp-content/themes/twentytwelve/functions.php
@@ -215,7 +215,7 @@ function twentytwelve_scripts_styles() {
 	wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri(), array(), '20251202' );
 
 	// Theme block stylesheet.
-	wp_enqueue_style( 'twentytwelve-block-style', get_template_directory_uri() . '/css/blocks.css', array( 'twentytwelve-style' ), '20251101' );
+	wp_enqueue_style( 'twentytwelve-block-style', get_template_directory_uri() . '/css/blocks.css', array( 'twentytwelve-style' ), '20251031' );
 
 	// Register the Internet Explorer specific stylesheet.
 	wp_register_style( 'twentytwelve-ie', false, array( 'twentytwelve-style' ) );
