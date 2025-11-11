9d9c9037c2d60924d9fd5a104a5095cfe72308c5
Jon Surrell <sirreal@users.noreply.github.com>
2025-11-11 19:57:36 +0100

    Fix @see tag and comment text

--- src/wp-includes/class-wp-scripts.php
+++ src/wp-includes/class-wp-scripts.php
@@ -351,8 +351,8 @@ class WP_Scripts extends WP_Dependencies {
 		$translations = $this->print_translations( $handle, false );
 		if ( $translations ) {
 			/**
-			 * The sourceURL comment is not included by @see WP_Scripts::print_translations()
-			 * when `$display == false` to prevent issues where the script tag contents are used
+			 * The sourceURL comment is not included by {@see WP_Scripts::print_translations()}
+			 * when `$display` is `false` to prevent issues where the script tag contents are used
 			 * by extenders for other purposes, for example concatenated with other script content.
 			 *
 			 * Include the sourceURL comment here as it would be when printed directly.
