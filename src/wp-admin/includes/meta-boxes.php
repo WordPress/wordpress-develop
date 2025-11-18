223bed702150f44956e6a02f0544760bc1501eec
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-11-18 10:09:46 +0000

    Update hook documentation for add_meta_boxes_{}

--- src/wp-admin/includes/meta-boxes.php
+++ src/wp-admin/includes/meta-boxes.php
@@ -1687,17 +1687,21 @@ function register_and_do_post_meta_boxes( $post ) {
 	/**
 	 * Fires after all built-in meta boxes have been added, contextually for the given post type.
 	 *
-	 * The dynamic portion of the hook name, `$post_type`, refers to the post type of the post.
+	 * The dynamic portion of the hook name, `$post_type`, refers to the post type of the post,
+	 * or the object type (comment, link).
 	 *
 	 * Possible hook names include:
 	 *
 	 *  - `add_meta_boxes_post`
 	 *  - `add_meta_boxes_page`
 	 *  - `add_meta_boxes_attachment`
+	 *  - `add_meta_boxes_comment`
+	 *  - `add_meta_boxes_link`
 	 *
 	 * @since 3.0.0
 	 *
-	 * @param WP_Post $post Post object.
+	 * @param WP_Post|WP_Comment|object $post The post, comment, or link object. Type varies depending on
+	 *                                        the hook name.
 	 */
 	do_action( "add_meta_boxes_{$post_type}", $post );
 
