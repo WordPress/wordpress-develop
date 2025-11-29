76e47ac41182717252b7ad34b27b739533b33942
Phuc Nguyen <manhphucofficial@gmail.com>
2025-11-29 12:46:51 +0700

    Update script-loader.php
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -3409,7 +3409,7 @@ function wp_enqueue_block_style( $block_name, $args ) {
 }
 
 /**
- * Loads classic theme styles on classic themes.
+ * Loads classic theme styles in themes which lack a theme.json file.
  *
  * This is used for backwards compatibility for Button and File blocks specifically.
  *
