cd879f83ab138f74511b19e7b3e294e3d637eff3
AKSHAT2802 <akshat.kakkad2802@gmail.com>
2025-12-05 02:10:31 +0530

    phpcs fix

--- src/wp-admin/includes/class-wp-site-health.php
+++ src/wp-admin/includes/class-wp-site-health.php
@@ -3414,14 +3414,14 @@ class WP_Site_Health {
 			'x-srcache-fetch-status' => $cache_hit_callback,
 
 			// Generic caching proxies (Nginx, Varnish, etc.)
-			'x-cache' => $cache_hit_callback,
+			'x-cache'        => $cache_hit_callback,
 			'x-cache-status' => $cache_hit_callback,
 			
 			// Cloudflare
 			'cf-cache-status' => $cache_hit_callback,
 			
 			// Apache mod_cache
-			'x-cache-detail' => $cache_hit_callback,			
+			'x-cache-detail' => $cache_hit_callback,
 		);
 
 		/**
