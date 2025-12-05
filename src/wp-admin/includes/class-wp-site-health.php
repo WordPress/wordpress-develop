6c189b73f46c7930f17274f84f95f6147eb32a67
AKSHAT2802 <akshat.kakkad2802@gmail.com>
2025-12-06 00:30:50 +0530

    feat: Add common cache headers for improved caching diagnostics

--- src/wp-admin/includes/class-wp-site-health.php
+++ src/wp-admin/includes/class-wp-site-health.php
@@ -3414,8 +3414,11 @@ class WP_Site_Health {
 			'x-srcache-fetch-status' => $cache_hit_callback,
 
 			// Generic caching proxies (Nginx, Varnish, etc.)
-			'x-cache'        => $cache_hit_callback,
-			'x-cache-status' => $cache_hit_callback,
+			'x-cache'           => $cache_hit_callback,
+			'x-cache-status'    => $cache_hit_callback,
+			'x-litespeed-cache' => $cache_hit_callback,
+			'x-proxy-cache'     => $cache_hit_callback,
+			'via'               => '',
 
 			// Cloudflare
 			'cf-cache-status' => $cache_hit_callback,
