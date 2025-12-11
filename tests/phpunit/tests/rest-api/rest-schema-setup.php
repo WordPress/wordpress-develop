12ebc34bec885352041ddb526e4dd473f1389563
Jake Spurlock <whyisjake@gmail.com>
2025-12-10 17:18:14 -0800

    Tests: Add content-types REST routes to schema test
    Updates the expected routes list in the REST schema test to include the new /wp/v2/content-types endpoints.

--- tests/phpunit/tests/rest-api/rest-schema-setup.php
+++ tests/phpunit/tests/rest-api/rest-schema-setup.php
@@ -133,6 +133,8 @@ class WP_Test_REST_Schema_Initialization extends WP_Test_REST_TestCase {
 			'/wp/v2/users/(?P<user_id>(?:[\\d]+|me))/application-passwords/(?P<uuid>[\\w\\-]+)',
 			'/wp/v2/comments',
 			'/wp/v2/comments/(?P<id>[\\d]+)',
+			'/wp/v2/content-types',
+			'/wp/v2/content-types/(?P<content_type>[a-z0-9_-]+)',
 			'/wp/v2/global-styles/(?P<id>[\/\d+]+)',
 			'/wp/v2/global-styles/(?P<parent>[\d]+)/revisions',
 			'/wp/v2/global-styles/(?P<parent>[\d]+)/revisions/(?P<id>[\d]+)',
