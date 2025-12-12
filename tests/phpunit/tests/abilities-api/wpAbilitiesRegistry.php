d8a71e5e8b093e359fde031d9e1ad1ff7c2eb9c7
artpi <artur.piszek@gmail.com>
2025-12-12 15:06:42 +0100

    Move Tests_Custom_Ability_Class to a seperate file

--- tests/phpunit/tests/abilities-api/wpAbilitiesRegistry.php
+++ tests/phpunit/tests/abilities-api/wpAbilitiesRegistry.php
@@ -23,6 +23,8 @@ class Tests_Abilities_API_WpAbilitiesRegistry extends WP_UnitTestCase {
 	 * Set up each test method.
 	 */
 	public function set_up(): void {
+		require_once DIR_TESTDATA . '/../includes/class-tests-custom-ability-class.php';
+
 		parent::set_up();
 
 		$this->registry = new WP_Abilities_Registry();
@@ -678,34 +680,5 @@ class Tests_Abilities_API_WpAbilitiesRegistry extends WP_UnitTestCase {
 
 		$unfiltered_ability = $this->registry->register( 'test/another-ability', self::$test_ability_args );
 		$this->assertNotSame( $filtered_ability->get_label(), $unfiltered_ability->get_label(), 'The filter incorrectly modified the args for an ability it should not have.' );
-	}
-}
-
-/**
- * Test custom ability class that extends WP_Ability.
- *
- * This class overrides do_execute() and check_permissions() directly,
- * allowing registration without execute_callback or permission_callback.
- */
-class Tests_Custom_Ability_Class extends WP_Ability {
-
-	/**
-	 * Custom execute implementation that multiplies instead of adds.
-	 *
-	 * @param mixed $input The input data.
-	 * @return int The result of multiplying a and b.
-	 */
-	protected function do_execute( $input = null ) {
-		return $input['a'] * $input['b'];
-	}
-
-	/**
-	 * Custom permission check that always returns true.
-	 *
-	 * @param mixed $input The input data.
-	 * @return bool Always true.
-	 */
-	public function check_permissions( $input = null ) {
-		return true;
 	}
 }
