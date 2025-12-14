eab6402cf44de55af8149c789fa491df777b8102
Weston Ruter <weston@ruter.net>
2025-12-13 23:00:17 -0800

    Tweak comment

--- src/wp-includes/taxonomy.php
+++ src/wp-includes/taxonomy.php
@@ -2000,7 +2000,7 @@ function wp_delete_object_term_relationships( $object_id, $taxonomies ) {
 	foreach ( (array) $taxonomies as $taxonomy ) {
 		$term_ids = wp_get_object_terms( $object_id, $taxonomy, array( 'fields' => 'ids' ) );
 		if ( ! is_array( $term_ids ) ) {
-			// Skip return value in the case of an error or a 'wp_get_object_terms' filter returning an invalid value.
+			// Skip return value in the case of an error or the 'wp_get_object_terms' filter returning an invalid value.
 			continue;
 		}
 		$term_ids = array_map( 'intval', $term_ids );
