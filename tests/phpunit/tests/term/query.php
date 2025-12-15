a49b48752cae1db21b855f6117d5d9e403915b25
Himanshu Pathak <himanshu.pathak@rtcamp.com>
2025-12-15 21:55:51 +0530

    Taxonomy: Add tests for handling null and zero object_ids in WP_Term_Query

--- tests/phpunit/tests/term/query.php
+++ tests/phpunit/tests/term/query.php
@@ -452,6 +452,48 @@ class Tests_Term_Query extends WP_UnitTestCase {
 	}
 
 	/**
+	 *
+	 * @ticket 63256
+	 */
+	public function test_object_ids_null_should_return_all_terms() {
+		register_taxonomy( 'wptests_tax_1', 'post' );
+
+		$terms = self::factory()->term->create_many( 3, array( 'taxonomy' => 'wptests_tax_1' ) );
+
+		$query = new WP_Term_Query(
+			array(
+				'taxonomy'   => 'wptests_tax_1',
+				'object_ids' => null,
+				'hide_empty' => false,
+				'fields'     => 'ids',
+			)
+		);
+
+		$this->assertSameSets( $terms, $query->terms );
+	}
+
+	/**
+	 *
+	 * @ticket 63256
+	 */
+	public function test_object_ids_zero_should_be_treated_as_numeric() {
+		register_taxonomy( 'wptests_tax_1', 'post' );
+
+		$term = self::factory()->term->create( array( 'taxonomy' => 'wptests_tax_1' ) );
+
+		$query = new WP_Term_Query(
+			array(
+				'taxonomy'   => 'wptests_tax_1',
+				'object_ids' => 0,
+				'hide_empty' => false,
+				'fields'     => 'ids',
+			)
+		);
+
+		$this->assertIsArray( $query->terms );
+	}
+
+	/**
 	 * @ticket 38295
 	 * @group cache
 	 */
