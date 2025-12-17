511284e1d7565983167a693ff10bc87dd0f70ba5
Jon Surrell <sirreal@users.noreply.github.com>
2025-12-17 12:33:39 +0100

    Apply suggestions from code review
    Strictly speaking, a `STYLE` tag will be closed by a `STYLE` close tag. However, the CSS contents are concatenated together and the strict check for a STYLE close tag may later be invalidated by concatenation.

--- src/wp-includes/customize/class-wp-customize-custom-css-setting.php
+++ src/wp-includes/customize/class-wp-customize-custom-css-setting.php
@@ -171,7 +171,7 @@ final class WP_Customize_Custom_CSS_Setting extends WP_Customize_Setting {
 		 *
 		 * @see https://html.spec.whatwg.org/multipage/parsing.html#generic-raw-text-element-parsing-algorithm
 		 */
-		if ( preg_match( '#</style[ \\t\\f\\n\\r/>]#', $css ) ) {
+		if ( stripos( $css, '</style' ) ) {
 			$validity->add( 'illegal_markup', __( 'Markup is not allowed in CSS.' ) );
 		}
 
