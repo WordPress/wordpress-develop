d6bfdca5f937ec91f5d5ab25fc9159a85a55d4c9
Jon Surrell <sirreal@users.noreply.github.com>
2025-12-19 20:45:33 +0100

    Remove debugging assertions

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -3030,10 +3030,10 @@ function wp_get_inline_script_tag( $data, $attributes = array() ) {
 	$processor->next_tag();
 	foreach ( $attributes as $name => $value ) {
 		if ( is_string( $value ) || true === $value ) {
-			assert( $processor->set_attribute( $name, $value ) );
+			$processor->set_attribute( $name, $value );
 		}
 	}
-	assert( $processor->set_modifiable_text( $data ) );
+	$processor->set_modifiable_text( $data );
 	return $processor->get_updated_html();
 }
 
