eb9678630f0d340f85e20325d6150424400af51a
Jon Surrell <sirreal@users.noreply.github.com>
2025-12-23 17:08:24 +0100

    Remove non-HTML5 behavior from wpSanitizeScriptAttributes

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -2871,8 +2871,7 @@ function wp_enqueue_editor_format_library_assets() {
  * @return string String made of sanitized `<script>` tag attributes.
  */
 function wp_sanitize_script_attributes( $attributes ) {
-	$html5_script_support = is_admin() || current_theme_supports( 'html5', 'script' );
-	$attributes_string    = '';
+	$attributes_string = '';
 
 	/*
 	 * If HTML5 script tag is supported, only the attribute name is added
@@ -2881,7 +2880,7 @@ function wp_sanitize_script_attributes( $attributes ) {
 	foreach ( $attributes as $attribute_name => $attribute_value ) {
 		if ( is_bool( $attribute_value ) ) {
 			if ( $attribute_value ) {
-				$attributes_string .= $html5_script_support ? ' ' . esc_attr( $attribute_name ) : sprintf( ' %1$s="%2$s"', esc_attr( $attribute_name ), esc_attr( $attribute_name ) );
+				$attributes_string .= ' ' . esc_attr( $attribute_name );
 			}
 		} else {
 			$attributes_string .= sprintf( ' %1$s="%2$s"', esc_attr( $attribute_name ), esc_attr( $attribute_value ) );
