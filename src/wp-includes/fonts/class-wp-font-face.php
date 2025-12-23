c92472fd7678287c3afe38e401223be13d11538a
Jon Surrell <sirreal@users.noreply.github.com>
2025-12-23 14:57:21 +0100

    Remove text/css type from class-wp-font-face

--- src/wp-includes/fonts/class-wp-font-face.php
+++ src/wp-includes/fonts/class-wp-font-face.php
@@ -73,21 +73,6 @@ class WP_Font_Face {
 	private $style_tag_attrs = array();
 
 	/**
-	 * Creates and initializes an instance of WP_Font_Face.
-	 *
-	 * @since 6.4.0
-	 */
-	public function __construct() {
-		if (
-			function_exists( 'is_admin' ) && ! is_admin()
-			&&
-			function_exists( 'current_theme_supports' ) && ! current_theme_supports( 'html5', 'style' )
-		) {
-			$this->style_tag_attrs = array( 'type' => 'text/css' );
-		}
-	}
-
-	/**
 	 * Generates and prints the `@font-face` styles for the given fonts.
 	 *
 	 * @since 6.4.0
