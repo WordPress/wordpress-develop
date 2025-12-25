4419b5ccf831a7229afe7653f17bafc1bf1bd64f
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-12-25 19:56:55 +0900

    get_block_wrapper_attributes(): Preserve zero value

--- src/wp-includes/class-wp-block-supports.php
+++ src/wp-includes/class-wp-block-supports.php
@@ -121,7 +121,7 @@ class WP_Block_Supports {
 
 			if ( ! empty( $new_attributes ) ) {
 				foreach ( $new_attributes as $attribute_name => $attribute_value ) {
-					if ( empty( $output[ $attribute_name ] ) ) {
+					if ( ! array_key_exists( $attribute_name, $output ) || '' === (string) $output[ $attribute_name ] ) {
 						$output[ $attribute_name ] = $attribute_value;
 					} else {
 						$output[ $attribute_name ] .= " $attribute_value";
@@ -161,60 +161,4 @@ class WP_Block_Supports {
 			}
 		}
 	}
-}
-
-/**
- * Generates a string of attributes by applying to the current block being
- * rendered all of the features that the block supports.
- *
- * @since 5.6.0
- *
- * @param string[] $extra_attributes Optional. Array of extra attributes to render on the block wrapper.
- * @return string String of HTML attributes.
- */
-function get_block_wrapper_attributes( $extra_attributes = array() ) {
-	$new_attributes = WP_Block_Supports::get_instance()->apply_block_supports();
-
-	if ( empty( $new_attributes ) && empty( $extra_attributes ) ) {
-		return '';
-	}
-
-	// This is hardcoded on purpose.
-	// We only support a fixed list of attributes.
-	$attributes_to_merge = array( 'style', 'class', 'id', 'aria-label' );
-	$attributes          = array();
-	foreach ( $attributes_to_merge as $attribute_name ) {
-		if ( empty( $new_attributes[ $attribute_name ] ) && empty( $extra_attributes[ $attribute_name ] ) ) {
-			continue;
-		}
-
-		if ( empty( $new_attributes[ $attribute_name ] ) ) {
-			$attributes[ $attribute_name ] = $extra_attributes[ $attribute_name ];
-			continue;
-		}
-
-		if ( empty( $extra_attributes[ $attribute_name ] ) ) {
-			$attributes[ $attribute_name ] = $new_attributes[ $attribute_name ];
-			continue;
-		}
-
-		$attributes[ $attribute_name ] = $extra_attributes[ $attribute_name ] . ' ' . $new_attributes[ $attribute_name ];
-	}
-
-	foreach ( $extra_attributes as $attribute_name => $value ) {
-		if ( ! in_array( $attribute_name, $attributes_to_merge, true ) ) {
-			$attributes[ $attribute_name ] = $value;
-		}
-	}
-
-	if ( empty( $attributes ) ) {
-		return '';
-	}
-
-	$normalized_attributes = array();
-	foreach ( $attributes as $key => $value ) {
-		$normalized_attributes[] = $key . '="' . esc_attr( $value ) . '"';
-	}
-
-	return implode( ' ', $normalized_attributes );
 }
