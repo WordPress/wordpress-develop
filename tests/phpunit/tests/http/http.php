aa5990c17e7625a8498f35d4d04b7e135fad6625
Phuc Nguyen <manhphucofficial@gmail.com>
2025-12-30 15:22:14 +0700

    Tests: fix array alignment for PHPCS

--- tests/phpunit/tests/http/http.php
+++ tests/phpunit/tests/http/http.php
@@ -566,7 +566,7 @@ class Tests_HTTP_HTTP extends WP_UnitTestCase {
 				'url'           => 'https://example.com:81/caniload.php',
 				'cb_safe_ports' => 'callback_remove_safe_ports',
 			),
-			'underscore_in_hostname' => array(
+			'underscore_in_hostname'                       => array(
 				'url' => 'https://foo_bar.example.com/',
 			),
 		);
