6c7eccb4851544fc6c11a5c6dcc17b8256461777
Phuc Nguyen <manhphucofficial@gmail.com>
2025-12-30 14:56:01 +0700

    Tests: fix inline alignment to satisfy PHPCS

--- tests/phpunit/tests/http/http.php
+++ tests/phpunit/tests/http/http.php
@@ -566,11 +566,11 @@ class Tests_HTTP_HTTP extends WP_UnitTestCase {
 				'url'           => 'https://example.com:81/caniload.php',
 				'cb_safe_ports' => 'callback_remove_safe_ports',
 			),
-			'underscore_in_hostname'						=> array(
-				'url'			=> 'https://foo_bar.example.com/',
+			'underscore_in_hostname'                        => array(
+				'url'           => 'https://foo_bar.example.com/',
 			),
-			'valid_ip_host'									=> array(
-				'url'			=> 'https://1.1.1.1/',
+			'valid_ip_host'                                 => array(
+				'url'           => 'https://1.1.1.1/',
 			),
 		);
 	}
