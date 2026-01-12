95e26d9b9f5d3453c9f0620dd864535f7d02a9ff
Sören Wünsch <soeren.wrede@parshipgroup.com>
2026-01-12 11:47:30 +0100

    Code Modernization: Replace manual comparison logic with the spaceship operator <=>

--- src/wp-includes/script-loader.php
+++ src/wp-includes/script-loader.php
@@ -3001,7 +3001,7 @@ function wp_maybe_inline_styles() {
 		usort(
 			$styles,
 			static function ( $a, $b ) {
-				return ( $a['size'] <= $b['size'] ) ? -1 : 1;
+				return $a['size'] <=> $b['size'];
 			}
 		);
 
