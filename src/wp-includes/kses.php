4c492c3595a26b0bca55ecf951901b0c703553f1
Jon Surrell <sirreal@users.noreply.github.com>
2026-01-13 23:30:33 +0100

    Add trailing parens to function name in PHPdoc
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- src/wp-includes/kses.php
+++ src/wp-includes/kses.php
@@ -2390,7 +2390,7 @@ function wp_filter_global_styles_post( $data ) {
 		 * JSON encode the data stored in post content.
 		 * Escape characters that are likely to be mangled by HTML filters: "<>&".
 		 *
-		 * This matches the escaping in {@see WP_REST_Global_Styles_Controller::prepare_item_for_database}.
+		 * This matches the escaping in {@see WP_REST_Global_Styles_Controller::prepare_item_for_database()}.
 		 */
 		return wp_slash( wp_json_encode( $data_to_encode, JSON_UNESCAPED_SLASHES | JSON_HEX_TAG | JSON_HEX_AMP ) );
 	}
