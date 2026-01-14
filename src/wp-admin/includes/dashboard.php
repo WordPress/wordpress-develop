2aed48c8d59b79b6ce0941fc5d3f38369da31ef9
Weston Ruter <weston@ruter.net>
2026-01-14 13:02:31 -0800

    Revert "Use full date as fallback"
    This reverts commit b42c718062655c9427160bf11379255cbabdb65c.

--- src/wp-admin/includes/dashboard.php
+++ src/wp-admin/includes/dashboard.php
@@ -1015,8 +1015,7 @@ function wp_dashboard_recent_posts( $args ) {
 			$time = get_the_time( 'U' );
 
 			if ( ! is_int( $time ) ) {
-				/* translators: Date and time format for recent posts on the dashboard, from a different calendar year, see https://www.php.net/manual/datetime.format.php */
-				$relative = date_i18n( __( 'M jS Y' ), $time );
+				$relative = '';
 			} elseif ( gmdate( 'Y-m-d', $time ) === $today ) {
 				$relative = __( 'Today' );
 			} elseif ( gmdate( 'Y-m-d', $time ) === $tomorrow ) {
@@ -1036,7 +1035,7 @@ function wp_dashboard_recent_posts( $args ) {
 			printf(
 				'<li><span>%1$s</span> <a href="%2$s" aria-label="%3$s">%4$s</a></li>',
 				/* translators: 1: Relative date, 2: Time. */
-				sprintf( _x( '%1$s, %2$s', 'dashboard' ), $relative, get_the_time() ),
+				$relative ? sprintf( _x( '%1$s, %2$s', 'dashboard' ), $relative, get_the_time() ) : get_the_time(),
 				$recent_post_link,
 				/* translators: %s: Post title. */
 				esc_attr( sprintf( __( 'Edit &#8220;%s&#8221;' ), $draft_or_post_title ) ),
