5c1f47a8cd999003db053df2060a49b312566881
Fabian Kägy <mail@fabian-kaegy.de>
2026-01-23 22:03:39 +0100

    Admin: Rename 'Default' admin color scheme to 'Classic'.

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -4885,7 +4885,7 @@ function register_admin_color_schemes() {
 
 	wp_admin_css_color(
 		'fresh',
-		_x( 'Default', 'admin color scheme' ),
+		_x( 'Classic', 'admin color scheme' ),
 		false,
 		array( '#1d2327', '#2c3338', '#2271b1', '#72aee6' ),
 		array(
