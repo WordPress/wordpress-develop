1b15d2cb7da0eee8479e7278d958ea31e4acab0f
Weston Ruter <weston@ruter.net>
2026-01-25 14:39:01 -0800

    Add typing and encapsulate in IIFE

--- src/js/_enqueues/vendor/codemirror/fakejshint.js
+++ src/js/_enqueues/vendor/codemirror/fakejshint.js
@@ -1,39 +1,66 @@
 // JSHINT has some GPL Compatability issues, so we are faking it out and using espree for validation
 // Based on https://github.com/jquery/esprima/blob/gh-pages/demo/validate.js which is MIT licensed
 
-var fakeJSHINT = function() {
-	var syntax;
-	var that = this;
-	this.data = [];
-	this.convertError = function( error ){
+( () => {
+	/**
+	 * @typedef {Object} JSHINTError
+	 * @property {number} line - Line number.
+	 * @property {number} character - Column number.
+	 * @property {string} reason - Error message.
+	 * @property {string} code - Error code.
+	 */
+
+	/**
+	 * Fake JSHINT.
+	 */
+	const fakeJSHINT = {
+		/**
+		 * Collected error(s) during parsing.
+		 *
+		 * @type {JSHINTError[]}
+		 */
+		data: [],
+
+		/**
+		 * Converts a SyntaxError to a JSHINT error.
+		 *
+		 * @param {SyntaxError} error - SyntaxError to convert.
+		 * @returns {JSHINTError}
+		 */
+		convertError( error ) {
+			return {
+				line: error.lineNumber,
+				character: error.column,
+				reason: error.message,
+				code: 'E',
+			};
+		},
+
+		/**
+		 * Parses JS code to find errors.
+		 *
+		 * @param {string} code - JS code to parse.
+		 */
+		parse( code ) {
+			try {
+				window.espree.parse( code, {
+					ecmaVersion: 'latest',
+					loc: true,
+					sourceType: 'module',
+				} );
+				this.data = [];
+			} catch ( error ) {
+				this.data.push( this.convertError( error ) );
+			}
+		},
+	};
+
+	window.JSHINT = ( text ) => {
+		fakeJSHINT.parse( text );
+	};
+	window.JSHINT.data = () => {
 		return {
-			line: error.lineNumber,
-			character: error.column,
-			reason: error.description,
-			code: 'E'
+			errors: fakeJSHINT.data,
 		};
 	};
-	this.parse = function( code ){
-		try {
-			syntax = window.espree.parse(code, {
-				ecmaVersion: 'latest',
-				loc: true,
-				sourceType: 'module'
-			});
-			that.data = [];
-		} catch (e) {
-			that.data.push( that.convertError( e ) );
-		}
-	};
-};
-
-window.JSHINT = function( text ){
-	fakeJSHINT.parse( text );
-};
-window.JSHINT.data = function(){
-	return {
-		errors: fakeJSHINT.data
-	};
-};
-
-
+} )();
