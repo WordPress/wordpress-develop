bb37a4017eaf15622a01a4585a013c0f9f56aafd
Rodrigo Butzke <buutqn@gmail.com>
2026-01-27 15:51:55 -0300

    Use wp_check_filetype to determine file extension
    Fix WP_Customize_Media_Control::to_json() extension validation.

--- src/wp-includes/customize/class-wp-customize-media-control.php
+++ src/wp-includes/customize/class-wp-customize-media-control.php
@@ -92,7 +92,9 @@ class WP_Customize_Media_Control extends WP_Customize_Control {
 				 * Fake an attachment model - needs all fields used by template.
 				 * Note that the default value must be a URL, NOT an attachment ID.
 				 */
-				$ext  = substr( $this->setting->default, -3 );
+				$wp_filetype = wp_check_filetype( $this->setting->default );
+				$ext  = $wp_filetype['ext'];
+				
 				$type = in_array( $ext, array( 'jpg', 'png', 'gif', 'bmp', 'webp', 'avif' ), true ) ? 'image' : 'document';
 
 				$default_attachment = array(
