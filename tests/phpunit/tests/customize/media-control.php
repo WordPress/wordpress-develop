8b4741cc0672b5727f390349e03355c138f89d19
Weston Ruter <weston@ruter.net>
2026-02-01 21:37:40 -0800

    Remove needless global override in test

--- tests/phpunit/tests/customize/media-control.php
+++ tests/phpunit/tests/customize/media-control.php
@@ -11,18 +11,11 @@
 class Test_WP_Customize_Media_Control extends WP_UnitTestCase {
 
 	/**
-	 * Manager.
-	 */
-	public ?WP_Customize_Manager $wp_customize;
-
-	/**
 	 * Set up.
 	 */
 	public function set_up() {
 		parent::set_up();
 		require_once ABSPATH . WPINC . '/class-wp-customize-manager.php';
-		$GLOBALS['wp_customize'] = new WP_Customize_Manager();
-		$this->wp_customize      = $GLOBALS['wp_customize'];
 	}
 
 	/**
@@ -31,30 +24,32 @@ class Test_WP_Customize_Media_Control extends WP_UnitTestCase {
 	 * @covers ::to_json
 	 */
 	public function test_to_json() {
-		$this->wp_customize->add_setting(
+		$manager = new WP_Customize_Manager();
+
+		$manager->add_setting(
 			'some_jpg',
 			array(
 				'default' => 'https://example.com/image.jpg',
 			)
 		);
-		$this->wp_customize->add_setting(
+		$manager->add_setting(
 			'some_avif',
 			array(
 				'default' => 'https://example.com/image.avif',
 			)
 		);
-		$this->wp_customize->add_setting(
+		$manager->add_setting(
 			'some_pdf',
 			array(
 				'default' => 'https://example.com/image.pdf',
 			)
 		);
-		$this->wp_customize->add_setting( 'no_default' );
+		$manager->add_setting( 'no_default' );
 
-		$some_jpg_control   = $this->wp_customize->add_control( new WP_Customize_Media_Control( $this->wp_customize, 'some_jpg' ) );
-		$some_avif_control  = $this->wp_customize->add_control( new WP_Customize_Media_Control( $this->wp_customize, 'some_avif' ) );
-		$some_pdf_control   = $this->wp_customize->add_control( new WP_Customize_Media_Control( $this->wp_customize, 'some_pdf' ) );
-		$no_default_control = $this->wp_customize->add_control( new WP_Customize_Media_Control( $this->wp_customize, 'no_default' ) );
+		$some_jpg_control   = $manager->add_control( new WP_Customize_Media_Control( $manager, 'some_jpg' ) );
+		$some_avif_control  = $manager->add_control( new WP_Customize_Media_Control( $manager, 'some_avif' ) );
+		$some_pdf_control   = $manager->add_control( new WP_Customize_Media_Control( $manager, 'some_pdf' ) );
+		$no_default_control = $manager->add_control( new WP_Customize_Media_Control( $manager, 'no_default' ) );
 
 		$some_jpg_control_json  = $some_jpg_control->json();
 		$some_avif_control_json = $some_avif_control->json();
@@ -64,14 +59,5 @@ class Test_WP_Customize_Media_Control extends WP_UnitTestCase {
 		$this->assertSame( 'image', $some_avif_control_json['defaultAttachment']['type'] );
 		$this->assertSame( 'document', $some_pdf_control_json['defaultAttachment']['type'] );
 		$this->assertArrayNotHasKey( 'defaultAttachment', $no_default_control->json() );
-	}
-
-	/**
-	 * Tear down.
-	 */
-	public function tear_down() {
-		$this->wp_customize = null;
-		unset( $GLOBALS['wp_customize'] );
-		parent::tear_down();
 	}
 }
