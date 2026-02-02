b39c5d7f3f55c118e819fd43cd0111d1c67f7ae2
hbhalodia <hit.bhalodia@rtcamp.com>
2026-02-02 13:52:25 +0530

    CoreTrac-64568 Add wp_clean_plugins_cache to remove stale data after plugin uninstall

--- src/wp-admin/includes/plugin.php
+++ src/wp-admin/includes/plugin.php
@@ -1054,6 +1054,9 @@ function delete_plugins( $plugins, $deprecated = '' ) {
 		return new WP_Error( 'could_not_remove_plugin', sprintf( $message, implode( ', ', $errors ) ) );
 	}
 
+	// Clean up plugin cache to remove stale data after plugin deletion.
+	wp_clean_plugins_cache();
+
 	return true;
 }
 
