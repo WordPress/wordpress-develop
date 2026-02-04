c7124bad24ea6ce5adb3155bfecedd4676328536
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2026-02-04 17:55:37 -0600

    Move `$page` and `$paged` earlier in T11 `header.php`

--- src/wp-content/themes/twentyeleven/header.php
+++ src/wp-content/themes/twentyeleven/header.php
@@ -8,6 +8,13 @@
  * @subpackage Twenty_Eleven
  * @since Twenty Eleven 1.0
  */
+
+/**
+ * @global int $page  WordPress paginated post page count.
+ * @global int $paged WordPress archive pagination page count.
+ */
+global $page, $paged;
+
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -15,14 +22,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>
 <?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 *
-	 * @global int $page  WordPress paginated post page count.
-	 * @global int $paged WordPress archive pagination page count.
-	 */
-	global $page, $paged;
-
+	// Print the <title> tag based on what is being viewed.
 	wp_title( '|', true, 'right' );
 
 	// Add the site name.
