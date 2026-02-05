d130c92041cb79146d4eeb0097141b553a786595
Jon Surrell <sirreal@users.noreply.github.com>
2026-02-05 12:54:33 +0100

    Fix newlines disappearing inside PRE,TEXTAREA elements
    Add serialize + normalize tests
    
    Add more PRE tests
    
    Fix newlines disappearing inside PRE,TEXTAREA elements
    
    Handline PRE,LISTING
    
    lints
    
    Simpler fix
    
    Remove overly-specific tests
    
    Add LISTING tests
    
    Add explanatory comment
    
    lints
    
    Remove accidental import

--- tests/phpunit/tests/html-api/wpHtmlProcessor-serialize.php
+++ tests/phpunit/tests/html-api/wpHtmlProcessor-serialize.php
@@ -321,4 +321,61 @@ class Tests_HtmlApi_WpHtmlProcessor_Serialize extends WP_UnitTestCase {
 			'Double quotes in system ID' => array( '<!DOCTYPE html SYSTEM \'"quoted"\'\>', '<!DOCTYPE html SYSTEM \'"quoted"\'>' ),
 		);
 	}
+
+	/**
+	 * @ticket TBD
+	 *
+	 * @dataProvider data_provider_normalize_special_leading_newline_cases
+	 */
+	public function test_normalize_special_leading_newline_handling( string $input, string $expected ) {
+		$normalized = WP_HTML_Processor::normalize( $input );
+		$this->assertEqualHTML( $expected, $normalized );
+		$normalized_twice = WP_HTML_Processor::normalize( $normalized );
+		$this->assertEqualHTML( $expected, $normalized_twice );
+	}
+
+	public static function data_provider_normalize_special_leading_newline_cases() {
+		return array(
+			'Leading newline in PRE'             => array(
+				"<pre>\nline 1\nline 2</pre>",
+				"<pre>line 1\nline 2</pre>",
+			),
+			'Double leading newline in PRE'      => array(
+				"<pre>\n\nline 2\nline 3</pre>",
+				"<pre>\n\nline 2\nline 3</pre>",
+			),
+			'Multiple text nodes inside PRE'     => array(
+				"<pre>\nline 1<!--comment--> still line 1</pre>",
+				'<pre>line 1<!--comment--> still line 1</pre>',
+			),
+			'Multiple text nodes inside PRE with leading newlines' => array(
+				"<pre>\n\nline 2<!--comment--> still line 2</pre>",
+				"<pre>\n\nline 2<!--comment--> still line 2</pre>",
+			),
+			'Leading newline in LISTING'         => array(
+				"<listing>\nline 1\nline 2</listing>",
+				"<listing>line 1\nline 2</listing>",
+			),
+			'Double leading newline in LISTING'  => array(
+				"<listing>\n\nline 2\nline 3</listing>",
+				"<listing>\n\nline 2\nline 3</listing>",
+			),
+			'Multiple text nodes inside LISTING' => array(
+				"<listing>\nline 1<!--comment--> still line 1</listing>",
+				'<listing>line 1<!--comment--> still line 1</listing>',
+			),
+			'Multiple text nodes inside LISTING with leading newlines' => array(
+				"<listing>\n\nline 2<!--comment--> still line 2</listing>",
+				"<listing>\n\nline 2<!--comment--> still line 2</listing>",
+			),
+			'Leading newline in TEXTAREA'        => array(
+				"<textarea>\nline 1\nline 2</textarea>",
+				"<textarea>line 1\nline 2</textarea>",
+			),
+			'Double leading newline in TEXTAREA' => array(
+				"<textarea>\n\nline 2\nline 3</textarea>",
+				"<textarea>\n\nline 2\nline 3</textarea>",
+			),
+		);
+	}
 }
