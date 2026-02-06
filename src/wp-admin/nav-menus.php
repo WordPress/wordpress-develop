d07b0b35b872079d9898d2613efa09a877ca9590
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2026-02-06 11:52:47 -0600

    Move `aria-describedby` escaping later

--- src/wp-admin/nav-menus.php
+++ src/wp-admin/nav-menus.php
@@ -1200,8 +1200,8 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 										<legend class="menu-settings-group-name howto"><?php _e( 'Display location' ); ?></legend>
 										<?php
 										foreach ( $locations as $location => $description ) :
-											$checked      = false;
-											$location_set = '';
+											$checked          = false;
+											$aria_describedby = '';
 
 											if ( isset( $menu_locations[ $location ] )
 												&& 0 !== $nav_menu_selected_id
@@ -1213,13 +1213,20 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 											if ( ! empty( $menu_locations[ $location ] )
 												&& $menu_locations[ $location ] !== $nav_menu_selected_id
 											) {
-													$location_set = ' aria-describedby="theme-location-set-' . esc_attr( $location ) . '"';
+													$aria_describedby = "theme-location-set-$location";
 											}
 											?>
 											<div class="menu-settings-input checkbox-input">
-												<input type="checkbox"<?php checked( $checked ); ?> name="menu-locations[<?php echo esc_attr( $location ); ?>]" id="locations-<?php echo esc_attr( $location ); ?>" value="<?php echo esc_attr( $nav_menu_selected_id ); ?>"<?php echo $location_set; ?> />
+												<input type="checkbox"<?php checked( $checked ); ?>
+													name="menu-locations[<?php echo esc_attr( $location ); ?>]"
+													id="locations-<?php echo esc_attr( $location ); ?>"
+													value="<?php echo esc_attr( $nav_menu_selected_id ); ?>"
+													<?php if ( '' !== $aria_describedby ) : ?>
+														aria-describedby="<?php echo esc_attr( $aria_describedby ); ?>"
+													<?php endif; ?>
+												/>
 												<label for="locations-<?php echo esc_attr( $location ); ?>"><?php echo $description; ?></label>
-												<?php if ( '' !== $location_set ) : ?>
+												<?php if ( '' !== $aria_describedby ) : ?>
 													<span class="theme-location-set" id="theme-location-set-<?php echo esc_attr( $location ); ?>">
 													<?php
 														printf(
