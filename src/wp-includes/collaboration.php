79c7f35387777e769115a22ed125e78893af9499
chriszarate <chris.zarate@automattic.com>
2026-02-09 23:05:11 -0500

    Register collaboration post meta

--- src/wp-includes/collaboration.php
+++ src/wp-includes/collaboration.php
@@ -7,6 +7,41 @@
  */
 
 /**
+ * Registers collaboration-related post meta.
+ *
+ * @since 7.0.0
+ *
+ * @access private
+ */
+function wp_collaboration_register_meta() {
+	$persisted_crdt_post_meta_key = '_crdt_document';
+
+	register_meta(
+		'post',
+		$persisted_crdt_post_meta_key,
+		array(
+			'auth_callback'     => function ( bool $_allowed, string $_meta_key, int $object_id, int $user_id ): bool {
+				return user_can( $user_id, 'edit_post', $object_id );
+			},
+			/*
+			 * Revisions must be disabled because we always want to preserve
+			 * the latest persisted CRDT document, even when a revision is restored.
+			 * This ensures that we can continue to apply updates to a shared document
+			 * and peers can simply merge the restored revision like any other incoming
+			 * update.
+			 *
+			 * If we want to persist CRDT documents alongside revisions in the
+			 * future, we should do so in a separate meta key.
+			 */
+			'revisions_enabled' => false,
+			'show_in_rest'      => true,
+			'single'            => true,
+			'type'              => 'string',
+		)
+	);
+}
+
+/**
  * Injects the real-time collaboration setting into a global variable.
  *
  * @since 6.8.0
--- src/wp-includes/default-filters.php
+++ src/wp-includes/default-filters.php
@@ -125,6 +125,7 @@ foreach ( array( 'user_register', 'deleted_user' ) as $action ) {
 add_action( 'added_post_meta', 'wp_cache_set_posts_last_changed' );
 add_action( 'updated_post_meta', 'wp_cache_set_posts_last_changed' );
 add_action( 'deleted_post_meta', 'wp_cache_set_posts_last_changed' );
+add_action( 'init', 'wp_collaboration_register_meta' );
 
 // User meta.
 add_action( 'added_user_meta', 'wp_cache_set_users_last_changed' );
