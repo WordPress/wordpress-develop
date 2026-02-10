7415cec0e3425f4b9f042f72451f5a6110a72501
Anand Rajaram <anandrajaram21@gmail.com>
2026-02-10 10:41:09 +0530

    feat: change 'true' to true

--- src/wp-includes/canonical.php
+++ src/wp-includes/canonical.php
@@ -979,7 +979,7 @@ function redirect_guess_404_permalink() {
 			'update_post_term_cache' => false,
 			'fields'                 => 'ids',
 			'orderby'                => 'none',
-			'suppress_filters'       => 'true',
+			'suppress_filters'       => true,
 		);
 
 		if ( $strict_guess ) {
