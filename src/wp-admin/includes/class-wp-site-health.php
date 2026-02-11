8eaebf3f873778b5c4d3ebfa45f9bc7b5b98b940
Weston Ruter <weston@ruter.net>
2026-02-11 10:58:28 -0800

    Add missing return type hint

--- src/wp-admin/includes/class-wp-site-health.php
+++ src/wp-admin/includes/class-wp-site-health.php
@@ -2762,7 +2762,7 @@ class WP_Site_Health {
 	 *
 	 * @return array<string, string|array<string, string>> The test result.
 	 */
-	public function get_test_opcode_cache() {
+	public function get_test_opcode_cache(): array {
 		$opcode_cache_enabled = false;
 		if ( function_exists( 'opcache_get_status' ) ) {
 			$status = @opcache_get_status( false ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged -- Warning emitted in failure case.
