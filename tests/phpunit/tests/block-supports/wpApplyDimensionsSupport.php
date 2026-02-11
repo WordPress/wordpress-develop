df6792bde309337e90a9feeb0bce01c1cd9f3270
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2026-02-11 22:52:49 +0100

    Add width apply dimension support styles test

--- tests/phpunit/tests/block-supports/wpApplyDimensionsSupport.php
+++ tests/phpunit/tests/block-supports/wpApplyDimensionsSupport.php
@@ -100,4 +100,75 @@ class Tests_Block_Supports_WpApplyDimensionsSupport extends WP_UnitTestCase {
 			),
 		);
 	}
+
+	/**
+	 * Tests that width block support works as expected.
+	 *
+	 * @ticket 64200
+	 *
+	 * @covers ::wp_apply_dimensions_support
+	 *
+	 * @dataProvider data_width_block_support
+	 *
+	 * @param string $block_name The test block name to register.
+	 * @param mixed  $dimensions The dimensions block support settings.
+	 * @param mixed  $expected   The expected results.
+	 */
+	public function test_width_block_support( $block_name, $dimensions, $expected ) {
+		$this->test_block_name = $block_name;
+		register_block_type(
+			$this->test_block_name,
+			array(
+				'api_version' => 2,
+				'attributes'  => array(
+					'style' => array(
+						'type' => 'object',
+					),
+				),
+				'supports'    => array(
+					'dimensions' => $dimensions,
+				),
+			)
+		);
+		$registry    = WP_Block_Type_Registry::get_instance();
+		$block_type  = $registry->get_registered( $this->test_block_name );
+		$block_attrs = array(
+			'style' => array(
+				'dimensions' => array(
+					'width' => '300px',
+				),
+			),
+		);
+
+		$actual = wp_apply_dimensions_support( $block_type, $block_attrs );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_width_block_support() {
+		return array(
+			'style is applied' => array(
+				'block_name' => 'test/width-style-is-applied',
+				'dimensions' => array(
+					'width' => true,
+				),
+				'expected'   => array(
+					'style' => 'width:300px;',
+				),
+			),
+			'style output is skipped when individual feature serialization is skipped' => array(
+				'block_name' => 'test/width-with-individual-skipped-serialization-block-supports',
+				'dimensions' => array(
+					'width'                           => true,
+					'__experimentalSkipSerialization' => array( 'width' ),
+				),
+				'expected'   => array(),
+			),
+		);
+	}
 }
