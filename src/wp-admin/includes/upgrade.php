ae7a76797ffb13bacb4c2f48308f247b7b39e50b
Fabian Kägy <mail@fabian-kaegy.de>
2026-02-15 08:52:33 +0100

    update db version to commit number

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -886,7 +886,7 @@ function upgrade_all() {
 		upgrade_682();
 	}
 
-	if ( $wp_current_db_version < 60718 ) {
+	if ( $wp_current_db_version < 61644 ) {
 		upgrade_700();
 	}
 
@@ -2498,7 +2498,7 @@ function upgrade_700() {
 	global $wp_current_db_version, $wpdb;
 
 	// Migrate users with 'fresh' admin color to 'modern'.
-	if ( $wp_current_db_version < 60718 ) {
+	if ( $wp_current_db_version < 61644 ) {
 		$wpdb->update(
 			$wpdb->usermeta,
 			array( 'meta_value' => 'modern' ),
