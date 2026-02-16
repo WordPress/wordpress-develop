2413b12952b0fb9d96bac6b294f943d317e4f347
Chris Rosser <chris.rosser@automattic.com>
2026-02-16 17:26:29 +1300

    Incorporate WP_HTTP_IXR_Client fixes from #40784

--- src/wp-includes/class-wp-http-ixr-client.php
+++ src/wp-includes/class-wp-http-ixr-client.php
@@ -23,8 +23,8 @@ class WP_HTTP_IXR_Client extends IXR_Client {
 		if ( ! $path ) {
 			// Assume we have been given a URL instead.
 			$bits         = parse_url( $server );
-			$this->scheme = $bits['scheme'];
-			$this->server = $bits['host'];
+			$this->scheme = $bits['scheme'] ?? '';
+			$this->server = $bits['host'] ?? '';
 			$this->port   = $bits['port'] ?? $port;
 			$this->path   = ! empty( $bits['path'] ) ? $bits['path'] : '/';
 
