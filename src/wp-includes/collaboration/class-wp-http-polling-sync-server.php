0226f6a50fd19d1b0639d36d11286183b37d34b3
chriszarate <chris.zarate@automattic.com>
2026-02-17 09:15:13 -0500

    Account for empty awareness array

--- src/wp-includes/collaboration/class-wp-http-polling-sync-server.php
+++ src/wp-includes/collaboration/class-wp-http-polling-sync-server.php
@@ -227,7 +227,10 @@ class WP_HTTP_Polling_Sync_Server {
 			}
 
 			// The lowest client ID is nominated to perform compaction when needed.
-			$is_compactor = min( array_keys( $merged_awareness ) ) === $client_id;
+			$is_compactor = false;
+			if ( count( $merged_awareness ) > 0 ) {
+				$is_compactor = min( array_keys( $merged_awareness ) ) === $client_id;
+			}
 
 			// Process each update according to its type.
 			foreach ( $room_request['updates'] as $update ) {
