117213d220e5b77e1585c908116bd9720403f620
Presskopp <cherrmann@gmx.de>
2026-02-19 11:38:26 +0100

    Bulk Actions: Differentiate notice when no bulk action is selected.
    Improve the bulk action validation notice by distinguishing between:
    
    - No bulk action selected.
    - No items selected.
    
    This prevents misleading feedback when clicking Apply without choosing a bulk action.
    
    Fixes #64659.

--- src/js/_enqueues/admin/common.js
+++ src/js/_enqueues/admin/common.js
@@ -1350,11 +1350,12 @@ $( function() {
 		event.stopPropagation();
 		$( 'html, body' ).animate( { scrollTop: 0 } );
 
-		var errorMessage = __( 'Please select at least one item to perform this action on.' );
 		addAdminNotice( {
-			id: 'no-items-selected',
+			id: value !== '-1' ? 'no-items-selected' : 'no-bulk-action-selected',
 			type: 'error',
-			message: errorMessage,
+			message: value !== '-1'
+				? __( 'Please select at least one item to perform this action on.' )
+				: __( 'Please select a bulk action before clicking Apply.' ),
 			dismissible: true,
 		} );
 
