83246de503fe6d93b278388101e046085753d403
Weston Ruter <westonruter@gmail.com>
2026-02-19 10:45:14 -0800

    Fix return description for wp_die()
    Co-authored-by: John Blackbourn <john@johnblackbourn.com>

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -3765,7 +3765,7 @@ function wp_nonce_ays( $action ) {
  *                                  is a WP_Error.
  *     @type bool   $exit           Whether to exit the process after completion. Default true.
  * }
- * @return never|void Returns false if `$args['exit']` is false, otherwise exits.
+ * @return never|void Returns void if `$args['exit']` is false, otherwise exits.
  *
  * @phpstan-return ( $args['exit'] is false ? void : never )
  */
