fffe2f6b8700af8cba2dc03e013a04b0399e4b9e
Christoph Daum <c.daum@me.com>
2026-02-25 08:14:38 +0100

    docs: Refine phpdoc of dead_db()
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- src/wp-includes/class-wpdb.php
+++ src/wp-includes/class-wpdb.php
@@ -2117,7 +2117,9 @@ class wpdb {
 	 * @since 3.9.0
 	 *
 	 * @param bool $allow_bail Optional. Allows the function to bail. Default true.
-	 * @return bool True if the connection is up, false otherwise.
+	 * @return bool|never Whether the connection is up.
+	 *
+	 * @phpstan-return ( $allow_bail is true ? bool|never : bool )
 	 */
 	public function check_connection( $allow_bail = true ) {
 		// Check if the connection is alive.
