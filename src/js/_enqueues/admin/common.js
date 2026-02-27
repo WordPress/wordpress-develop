e17e300c19c13612172eaf27f0c40e96425c1695
Pratik Nawkar <pratik.nawkar@xecurify.com>
2026-02-27 17:14:31 +0530

    Keep errorMessage assignment for use with wp.a11y.speak()

--- src/js/_enqueues/admin/common.js
+++ src/js/_enqueues/admin/common.js
@@ -1352,9 +1352,11 @@ $( function() {
 
 		var errorMessage = __( 'Please select at least one item to perform this action on.' );
 		addAdminNotice( {
-			id: 'no-items-selected',
+			id: value !== '-1' ? 'no-items-selected' : 'no-bulk-action-selected',
 			type: 'error',
-			message: errorMessage,
+			message: value !== '-1'
+				? __( 'Please select at least one item to perform this action on.' )
+				: __( 'Please select a bulk action before clicking Apply.' ),
 			dismissible: true,
 		} );
 
