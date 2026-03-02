9c4ece0b57c19b42a5ef4f4e917a9df78659eefd
GautamMKGarg <GautamMKgarg@gmail.com>
2026-03-03 00:55:00 +0530

    Formatting fixed
    Formatting fixed.

--- src/wp-includes/class-wp-list-util.php
+++ src/wp-includes/class-wp-list-util.php
@@ -165,7 +165,7 @@ class WP_List_Util {
 			 */
 			foreach ( $this->output as $key => $value ) {
 				if ( is_object( $value ) ) {
-					if ( property_exists( $value, $field ) ){
+					if ( property_exists( $value, $field ) ) {
 						$newlist[ $key ] = $value->$field;
 					}
 				} elseif ( is_array( $value ) ) {
