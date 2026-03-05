5ed5a6b48cf5f514807cdfc64def5a9c28c2cb88
Joe Fusco <hello@josephfus.co>
2026-03-05 07:23:23 -0500

    Collaboration: Remove wp_sync_storage filter for v1.
    Removes premature filter surface for the sync storage backend.
    Can be reintroduced in a future release if needed.

--- src/wp-includes/rest-api.php
+++ src/wp-includes/rest-api.php
@@ -432,17 +432,7 @@ function create_initial_rest_routes() {
 	// Collaboration.
 	if ( wp_is_collaboration_enabled() ) {
 		$sync_storage = new WP_Sync_Table_Storage();
-
-		/**
-		 * Filters the sync storage backend used for real-time collaboration.
-		 *
-		 * @since 7.0.0
-		 *
-		 * @param WP_Sync_Storage $sync_storage Storage backend instance.
-		 */
-		$sync_storage = apply_filters( 'wp_sync_storage', $sync_storage );
-
-		$sync_server = new WP_HTTP_Polling_Sync_Server( $sync_storage );
+		$sync_server  = new WP_HTTP_Polling_Sync_Server( $sync_storage );
 		$sync_server->register_routes();
 	}
 }
