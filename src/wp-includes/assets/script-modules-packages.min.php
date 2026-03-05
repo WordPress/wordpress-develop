401377ffd05a5140575fc62ceadb0f2bac4f8919
Ella <ellatrix@git.wordpress.org>
2026-03-05 05:55:44 -0600

    Restore: Sync Gutenberg @ e499abfb843a43ac88455ca319220c5f181e1cf3
    See changelog in e7d40e7b994b9f0d8d448599b729bbde65c00015
    
    > Gutenberg ref update.
    > CI run: https://github.com/WordPress/wordpress-develop/pull/11167.
    >
    > See #64595.

--- src/wp-includes/assets/script-modules-packages.min.php
+++ src/wp-includes/assets/script-modules-packages.min.php
@@ -164,7 +164,7 @@
         'import' => 'static'
       )
     ),
-    'version' => '4c5f43f6950193300099'
+    'version' => '77514d54b76613289de5'
   ),
   'connectors/index.min.js' => array(
     'dependencies' => array(
@@ -175,7 +175,7 @@
       'wp-i18n',
       'wp-private-apis'
     ),
-    'version' => '00a04c7280e7ad762a10'
+    'version' => 'fb1bf5511eb12e411ec6'
   ),
   'core-abilities/index.min.js' => array(
     'dependencies' => array(
@@ -203,13 +203,13 @@
         'import' => 'static'
       )
     ),
-    'version' => 'c636e2e534fd52f7c77c'
+    'version' => 'e57f44d1a9f69e75d2d9'
   ),
   'interactivity/index.min.js' => array(
     'dependencies' => array(
       
     ),
-    'version' => '4d9d9330d767da3813cc'
+    'version' => 'f5a180d17cd8a38658aa'
   ),
   'interactivity-router/full-page.min.js' => array(
     'dependencies' => array(
@@ -237,7 +237,7 @@
         'import' => 'static'
       )
     ),
-    'version' => '493dd3de12e3112b3130'
+    'version' => '71aa17bac91628a0f874'
   ),
   'latex-to-mathml/index.min.js' => array(
     'dependencies' => array(
@@ -271,7 +271,7 @@
       'wp-private-apis',
       'wp-style-engine'
     ),
-    'version' => '9750cba8a806aa170cf4'
+    'version' => '30ab62f45bfe9f971ea0'
   ),
   'route/index.min.js' => array(
     'dependencies' => array(
@@ -298,7 +298,7 @@
     'dependencies' => array(
       
     ),
-    'version' => '52add8dbf585e013d5a6'
+    'version' => '4064876f4a80da877c25'
   ),
   'workflow/index.min.js' => array(
     'dependencies' => array(
@@ -319,6 +319,6 @@
         'import' => 'static'
       )
     ),
-    'version' => 'fcc73952f871fd953358'
+    'version' => '13556bc597bbf2a8d620'
   )
 );
