27619d3298488fd820de50e3073565b01934fa52
Christoph Daum <christoph.daum@coding-pioneers.com>
2026-03-08 14:47:00 +0100

    fix(hooks): use add_action for admin_print_styles
    Change add_filter() to add_action() for the
    admin_print_styles hook which is fired via do_action().
    
    Related to https://core.trac.wordpress.org/ticket/64828

--- src/wp-admin/includes/admin-filters.php
+++ src/wp-admin/includes/admin-filters.php
@@ -53,7 +53,7 @@ add_filter( 'nav_menu_meta_box_object', '_wp_nav_menu_meta_box_object' );
 
 // Prerendering.
 if ( ! is_customize_preview() ) {
-	add_filter( 'admin_print_styles', 'wp_resource_hints', 1 );
+	add_action( 'admin_print_styles', 'wp_resource_hints', 1 );
 }
 
 add_action( 'admin_print_scripts', 'print_emoji_detection_script' );
