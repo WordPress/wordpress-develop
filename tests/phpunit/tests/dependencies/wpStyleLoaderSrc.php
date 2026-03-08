066516845bc4be0ba17277d952005f14d2d9e27f
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2026-03-07 23:49:34 -0600

    `assertStringContainsString`

--- tests/phpunit/tests/dependencies/wpStyleLoaderSrc.php
+++ tests/phpunit/tests/dependencies/wpStyleLoaderSrc.php
@@ -25,7 +25,7 @@ class Tests_Dependencies_wpStyleLoaderSrc extends WP_UnitTestCase {
 		if ( is_admin() ) {
 			$this->assertFalse( wp_style_loader_src( '', 'colors' ) );
 		} else {
-			$this->assertContains( '/colors.css', wp_style_loader_src( '', 'colors' ) );
+			$this->assertStringContainsString( '/colors.css', wp_style_loader_src( '', 'colors' ) );
 		}
 	}
 }
