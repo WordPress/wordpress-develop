29c05083abd8ff052c3cbcda26bbc858145356d0
Huzaifa Al Mesbah <huzaifah4m@gmail.com>
2026-03-08 23:52:37 +0600

    Update src/wp-admin/admin-header.php
    Co-authored-by: Christoph Daum <c.daum@me.com>

--- src/wp-admin/admin-header.php
+++ src/wp-admin/admin-header.php
@@ -191,7 +191,7 @@ if ( $current_screen->taxonomy ) {
 	$admin_body_class .= ' taxonomy-' . $current_screen->taxonomy;
 }
 
-$admin_body_class .= ' branch-' . str_replace( array( '.', ',' ), '-', (string) get_bloginfo( 'version' ) );
+$admin_body_class .= ' branch-' . str_replace( array( '.', ',' ), '-', (string) ( (float)  get_bloginfo( 'version' ) ) );
 $admin_body_class .= ' version-' . str_replace( '.', '-', preg_replace( '/^([.0-9]+).*/', '$1', get_bloginfo( 'version' ) ) );
 $admin_body_class .= ' admin-color-' . sanitize_html_class( get_user_option( 'admin_color' ), 'modern' );
 $admin_body_class .= ' locale-' . sanitize_html_class( strtolower( str_replace( '_', '-', get_user_locale() ) ) );
