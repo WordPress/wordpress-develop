e6bbbfc3fff9acca64d812ff2bc70ae57df140de
Huzaifa Al Mesbah <huzaifah4m@gmail.com>
2026-03-09 10:18:54 +0600

    Apply alternative suggestion
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- src/wp-includes/ms-settings.php
+++ src/wp-includes/ms-settings.php
@@ -99,13 +99,11 @@ if ( ! isset( $current_site ) || ! isset( $current_blog ) ) {
 	wp_load_core_site_options( $site_id );
 }
 
-if ( isset( $wpdb ) ) {
-	if ( isset( $table_prefix ) ) {
-		$wpdb->set_prefix( $table_prefix, false ); // $table_prefix can be set in sunrise.php.
-	}
-	$wpdb->set_blog_id( $current_blog->blog_id, $current_blog->site_id );
-	$table_prefix = $wpdb->get_blog_prefix();
-}
+assert( isset( $wpdb ) );
+assert( isset( $table_prefix ) );
+$wpdb->set_prefix( $table_prefix, false ); // $table_prefix can be set in sunrise.php.
+$wpdb->set_blog_id( $current_blog->blog_id, $current_blog->site_id );
+$table_prefix       = $wpdb->get_blog_prefix();
 $_wp_switched_stack = array();
 $switched           = false;
 
