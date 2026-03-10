2bc5d70a8dbd24b92a1169bda70ec72470196165
Jonathan Desrosiers <desrosj@git.wordpress.org>
2026-03-10 03:10:39 +0000

    Build/Test Tools: Explicitly enable pretty permalinks on the local development environment.
    The `wp_install_maybe_enable_pretty_permalinks()` function usually enables pretty permalinks during installation, but it relies on a loopback request that may not work on all development environments.
    
    In temporary lieu of fixing the underlying loopback request on all the container images, this switches to explictly enabling permalinks and removes redundant permalink configuration from GitHub Actions workflow files.
    
    Merges [61733] to the 6.8 branch.
    
    Props johnbillion, jorbin.
    See #64227.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.8@61872 602fd350-edb4-49c9-b593-d223f7449a82

--- tools/local-env/scripts/install.js
+++ tools/local-env/scripts/install.js
@@ -53,6 +53,7 @@ wait_on( {
 		wp_cli( 'db reset --yes --defaults' );
 		const installCommand = process.env.LOCAL_MULTISITE === 'true'  ? 'multisite-install' : 'install';
 		wp_cli( `core ${ installCommand } --title="WordPress Develop" --admin_user=admin --admin_password=password --admin_email=test@test.com --skip-email --url=http://localhost:${process.env.LOCAL_PORT}` );
+		wp_cli( `rewrite structure '/%year%/%monthnum%/%postname%/'` );
 	} )
 	.catch( err => {
 		console.error( `Error: Unable to reset DB and install WordPress. Message: ${ err.message }` );
