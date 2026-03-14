ec8f62769641b681128f865836b96e4e7ed98ce2
Pablo Estevez <pestevez@fb.com>
2026-03-13 20:15:17 -0400

    Add Threads as an oEmbed provider

---
+++ tests/phpunit/tests/oembed/wpOembedThreadsProvider.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * Tests for the Threads oEmbed provider.
+ *
+ * @group oembed
+ *
+ * @covers WP_oEmbed::get_provider
+ */
+class Tests_oEmbed_wpOembedThreadsProvider extends WP_UnitTestCase {
+
+	/**
+	 * @var WP_oEmbed
+	 */
+	protected $oembed;
+
+	public function set_up() {
+		parent::set_up();
+
+		require_once ABSPATH . WPINC . '/class-wp-oembed.php';
+		$this->oembed = _wp_oembed_get_object();
+	}
+
+	/**
+	 * @dataProvider data_threads_provider_urls
+	 *
+	 * @param string $url      The URL to test.
+	 * @param string $expected The expected oEmbed provider URL.
+	 */
+	public function test_threads_provider_url( $url, $expected ) {
+		$provider = $this->oembed->get_provider( $url, array( 'discover' => false ) );
+		$this->assertSame( $expected, $provider );
+	}
+
+	/**
+	 * Data provider for valid Threads URLs.
+	 *
+	 * @return array[]
+	 */
+	public function data_threads_provider_urls() {
+		return array(
+			// threads.com post URLs.
+			'threads.com post URL'          => array(
+				'https://www.threads.com/@zuck/post/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+			'threads.com post URL no www'   => array(
+				'https://threads.com/@zuck/post/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+			'threads.com post URL http'     => array(
+				'http://www.threads.com/@zuck/post/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+
+			// threads.com short URLs.
+			'threads.com short URL'         => array(
+				'https://www.threads.com/t/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+			'threads.com short URL no www'  => array(
+				'https://threads.com/t/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+			'threads.com short URL http'    => array(
+				'http://www.threads.com/t/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+
+			// threads.net post URLs.
+			'threads.net post URL'          => array(
+				'https://www.threads.net/@zuck/post/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+			'threads.net post URL no www'   => array(
+				'https://threads.net/@zuck/post/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+
+			// threads.net short URLs.
+			'threads.net short URL'         => array(
+				'https://www.threads.net/t/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+			'threads.net short URL no www'  => array(
+				'https://threads.net/t/C1234567890',
+				'https://graph.threads.com/oembed',
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider data_threads_non_matching_urls
+	 *
+	 * @param string $url The URL to test.
+	 */
+	public function test_threads_provider_does_not_match_non_post_urls( $url ) {
+		$provider = $this->oembed->get_provider( $url, array( 'discover' => false ) );
+		$this->assertFalse( $provider );
+	}
+
+	/**
+	 * Data provider for URLs that should not match the Threads provider.
+	 *
+	 * @return array[]
+	 */
+	public function data_threads_non_matching_urls() {
+		return array(
+			'threads.com profile URL'       => array(
+				'https://www.threads.com/@zuck',
+			),
+			'threads.com homepage'          => array(
+				'https://www.threads.com/',
+			),
+			'threads.com search'            => array(
+				'https://www.threads.com/search',
+			),
+			'non-threads URL with threads in path' => array(
+				'https://example.com/threads/post/123',
+			),
+		);
+	}
+}
