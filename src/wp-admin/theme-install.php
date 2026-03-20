ac997ec4337be854af080f31df02b3a9cc5426e8
Guido07111975 <gjvdleest@yahoo.com>
2026-03-20 11:03:43 +0100

    Update theme-install.php
    Fix

--- src/wp-admin/theme-install.php
+++ src/wp-admin/theme-install.php
@@ -596,7 +596,7 @@ if ( $tab ) {
 						<div class="theme-description">{{{ data.description }}}</div>
 					</div>
 
-					<a class="theme-url" href="<?php echo esc_url( __( 'https://wordpress.org/themes/' ) ); ?>/{{ data.id }}/" target="_blank">
+					<a class="theme-url" href="<?php echo esc_url( __( 'https://wordpress.org/themes/' ) ); ?>{{ data.id }}/" target="_blank">
 						<?php _e( 'WordPress.org Theme Page &#187;' ); ?>
 					</a>
 				</div>
