4a39f9a02d5a79c6b8c161d72e4428acb62a5471
Peter Wilson <git-commit-by@peterwilson.cc>
2026-03-30 10:42:46 +1100

    Revert "Use json encoded data to avoid serialization issues."
    This reverts commit 470fa9092f5d926ccb37b7415113387cce167991.

--- src/wp-includes/collaboration/class-wp-sync-post-meta-storage.php
+++ src/wp-includes/collaboration/class-wp-sync-post-meta-storage.php
@@ -79,8 +79,7 @@ class WP_Sync_Post_Meta_Storage implements WP_Sync_Storage {
 			return false;
 		}
 
-		$update  = wp_json_encode( $update );
-		$meta_id = add_post_meta( $post_id, wp_slash( self::SYNC_UPDATE_META_KEY ), wp_slash( $update ), false );
+		$meta_id = add_post_meta( $post_id, self::SYNC_UPDATE_META_KEY, $update, false );
 
 		return (bool) $meta_id;
 	}
@@ -267,7 +266,7 @@ class WP_Sync_Post_Meta_Storage implements WP_Sync_Storage {
 
 		$updates = array();
 		foreach ( $rows as $row ) {
-			$updates[] = json_decode( $row->meta_value, true );
+			$updates[] = maybe_unserialize( $row->meta_value );
 		}
 
 		return $updates;
