1925f0245cc43f2233180da562bf8aa7297421ff
Weston Ruter <weston@ruter.net>
2026-03-29 20:11:59 -0700

    Fix conditional return type for wp_die()

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -3775,9 +3775,9 @@ function wp_nonce_ays( $action ) {
  *                                  is a WP_Error.
  *     @type bool   $exit           Whether to exit the process after completion. Default true.
  * }
- * @return never|void Returns void if `$args['exit']` is false, otherwise exits.
+ * @return void Never returns if `$args['exit']` is true (the default), otherwise returns void.
  *
- * @phpstan-return ( $args['exit'] is false ? void : never )
+ * @phpstan-return ( $args is array{ exit: false } ? void : never )
  */
 function wp_die( $message = '', $title = '', $args = array() ) {
 	global $wp_query;
