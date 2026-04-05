869faa0eeea4e80db2eb5835fa3db6838138fba7
Brian <brian@brianhaas.li>
2026-04-05 21:25:11 +0200

    add _reset_privacy_policy_page_for_post function

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -4028,6 +4028,26 @@ function _reset_front_page_settings_for_post( $post_id ) {
 }
 
 /**
+ * Resets the Privacy Policy page ID option when the Privacy Policy page
+ * is deleted or trashed, to prevent uncached database queries for a
+ * non-existent page.
+ *
+ * @since 7.1.0
+ * @access private
+ *
+ * @param int $post_id The ID of the post being deleted or trashed.
+ */
+function _reset_privacy_policy_page_for_post( $post_id ) {
+	$post = get_post( $post_id );
+
+	if ( $post && 'page' === $post->post_type ) {
+		if ( (int) get_option( 'wp_page_for_privacy_policy' ) === (int) $post_id ) {
+			update_option( 'wp_page_for_privacy_policy', 0 );
+		}
+	}
+}
+
+/**
  * Moves a post or page to the Trash
  *
  * If Trash is disabled, the post or page is permanently deleted.
