4119555852e3ccb5a20b3edc6c5c18b381b72dc7
Sainath Poojary <spoojary614@gmail.com>
2026-04-06 14:03:41 +0530

    fix: PHPCS Linting issues

--- tests/phpunit/tests/query/postsPerPage.php
+++ tests/phpunit/tests/query/postsPerPage.php
@@ -30,9 +30,11 @@ class Tests_Query_PostsPerPage extends WP_UnitTestCase {
 	}
 
 	public function test_posts_per_page_integer_positive() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => 2,
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => 2,
+			)
+		);
 
 		$this->assertSame( 2, $count );
 	}
@@ -41,33 +43,41 @@ class Tests_Query_PostsPerPage extends WP_UnitTestCase {
 	 * @ticket 24142
 	 */
 	public function test_posts_per_page_integer_zero() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => 0,
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => 0,
+			)
+		);
 
 		$this->assertSame( 0, $count );
 	}
 
 	public function test_posts_per_page_string_numeric() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => '2',
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => '2',
+			)
+		);
 
 		$this->assertSame( 2, $count );
 	}
 
 	public function test_posts_per_page_string_non_numeric() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => 'foo',
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => 'foo',
+			)
+		);
 
 		$this->assertSame( 10, $count );
 	}
 
 	public function test_posts_per_page_boolean_true() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => true,
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => true,
+			)
+		);
 
 		$this->assertSame( 1, $count );
 	}
@@ -76,49 +86,61 @@ class Tests_Query_PostsPerPage extends WP_UnitTestCase {
 	 * @ticket 24142
 	 */
 	public function test_posts_per_page_boolean_false() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => false,
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => false,
+			)
+		);
 
 		$this->assertSame( 0, $count );
 	}
 
 	public function test_posts_per_page_null() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => null,
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => null,
+			)
+		);
 
 		$this->assertSame( 10, $count );
 	}
 
 	public function test_posts_per_page_empty_string() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => '',
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => '',
+			)
+		);
 
 		$this->assertSame( 10, $count );
 	}
 
 	public function test_posts_per_page_array() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => array(),
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => array(),
+			)
+		);
 
 		$this->assertSame( 10, $count );
 	}
 
 	public function test_posts_per_page_negative() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => -2,
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => -2,
+			)
+		);
 
 		$this->assertSame( 2, $count );
 	}
 
 	public function test_posts_per_page_all() {
-		$count = $this->_get_post_count( array(
-			'posts_per_page' => -1,
-		) );
+		$count = $this->_get_post_count(
+			array(
+				'posts_per_page' => -1,
+			)
+		);
 
 		$this->assertSame( 11, $count );
 	}
