b8a380f8e551baae7c42a35ec0014d09525d3d24
Himanshu Pathak <himanshu.pathak@rtcamp.com>
2026-04-07 20:05:13 +0530

    Tests: Add documentation

--- tests/phpunit/tests/formatting/stripHtmlNewlines.php
+++ tests/phpunit/tests/formatting/stripHtmlNewlines.php
@@ -1,5 +1,11 @@
 <?php
 
-
+/**
+ * Tests for strip_html_newlines().
+ *
+ * @group formatting
+ *
+ * @covers ::strip_html_newlines
+ */
 class Tests_Formatting_StripHtmlNewlines extends WP_UnitTestCase {
 }
