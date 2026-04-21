8175e94daf70b905b793e9da5dd64e5b93f423fb
Infinite-Null <ankit.kum.sha9933@gmail.com>
2026-04-21 21:59:51 +0530

    Fix: Remove accessibility modification for cache property in non-serializable object test

--- tests/phpunit/tests/cache.php
+++ tests/phpunit/tests/cache.php
@@ -539,7 +539,6 @@ class Tests_Cache extends WP_UnitTestCase {
 		// any object-clone logic in set(), simulating a real-world scenario where
 		// a non-serializable object ends up in the cache.
 		$cache_property = new ReflectionProperty( $this->cache, 'cache' );
-		$cache_property->setAccessible( true );
 		$cache_data                       = $cache_property->getValue( $this->cache );
 		$cache_data['xml-group']['item1'] = $xml_object;
 		$cache_property->setValue( $this->cache, $cache_data );
