090ee6121f35900ae2919d41cb61ced6ec7be481
Peter Wilson <git-commit-by@peterwilson.cc>
2026-04-24 11:58:38 +1000

    Slightly neater.

--- src/wp-includes/admin-bar.php
+++ src/wp-includes/admin-bar.php
@@ -975,10 +975,9 @@ function wp_admin_bar_command_palette_menu( WP_Admin_Bar $wp_admin_bar ): void {
 
 	$script  = <<<JS
 		(( shortCutLabels ) => {
-			let userAgent = '';
 			// Assigning agent may error if the HTTP header is blocked at the browser level.
 			try {
-				userAgent = navigator.userAgent;
+				const userAgent = navigator.userAgent;
 			} catch (error) {
 				// Make no change to the default shortcut label.
 				return;
