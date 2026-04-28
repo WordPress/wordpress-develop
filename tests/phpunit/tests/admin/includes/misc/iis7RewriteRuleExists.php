29d33fd0f61b7b79860f40733b68a6e1cff15ca5
Paul Bearne <pbearne@git.wordpress.org>
2026-04-28 18:20:12 -0400

    Reorder and update test data array in data_iis7_rewrite_rule_exists() for consistency

--- tests/phpunit/tests/admin/includes/misc/iis7RewriteRuleExists.php
+++ tests/phpunit/tests/admin/includes/misc/iis7RewriteRuleExists.php
@@ -61,6 +61,10 @@ class Tests_iis7_rewrite_rule_exists extends WP_UnitTestCase {
 	 */
 	public function data_iis7_rewrite_rule_exists(): array {
 		return array(
+			'Rule with name starting with "wordpress" exists' => array(
+				'expected' => true,
+				'content'  => '<configuration><system.webServer><rewrite><rules><rule name="wordpress-rules" /></rules></rewrite></system.webServer></configuration>',
+			),
 			'Rule with name "wordpress" exists'               => array(
 				'expected' => true,
 				'content'  => '<configuration><system.webServer><rewrite><rules><rule name="wordpress" /></rules></rewrite></system.webServer></configuration>',
@@ -68,10 +72,6 @@ class Tests_iis7_rewrite_rule_exists extends WP_UnitTestCase {
 			'Rule with name "WordPress" exists'               => array(
 				'expected' => true,
 				'content'  => '<configuration><system.webServer><rewrite><rules><rule name="WordPress" /></rules></rewrite></system.webServer></configuration>',
-			),
-			'Rule with name starting with "wordpress" exists' => array(
-				'expected' => true,
-				'content'  => '<configuration><system.webServer><rewrite><rules><rule name="wordpress-rules" /></rules></rewrite></system.webServer></configuration>',
 			),
 			'Rule does not exist'                             => array(
 				'expected' => false,
