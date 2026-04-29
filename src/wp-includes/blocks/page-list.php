6f79b93b9726a20246bb030b4abe96e0a6e739af
Shreyas Ikhar <shreyasikhar26@gmail.com>
2026-04-29 17:47:11 +0530

    Fix failing checks

--- src/wp-includes/blocks/page-list.php
+++ src/wp-includes/blocks/page-list.php
@@ -166,14 +166,14 @@ function block_core_page_list_build_css_font_sizes( $context ) {
  *
  * @since 5.8.0
  *
- * @param bool  $submenu_visibility Whether to open submenus on click instead of hover.
- * @param bool  $show_submenu_icons Whether to show submenu indicator icons.
- * @param bool  $is_navigation_child If block is a child of Navigation block.
- * @param array $nested_pages The array of nested pages.
- * @param bool  $is_nested Whether the submenu is nested or not.
- * @param array $active_page_ancestor_ids An array of ancestor ids for active page.
- * @param array $colors Color information for overlay styles.
- * @param int   $depth The nesting depth.
+ * @param boolean $open_submenus_on_click Whether to open submenus on click instead of hover.
+ * @param boolean $show_submenu_icons Whether to show submenu indicator icons.
+ * @param boolean $is_navigation_child If block is a child of Navigation block.
+ * @param array   $nested_pages The array of nested pages.
+ * @param boolean $is_nested Whether the submenu is nested or not.
+ * @param array   $active_page_ancestor_ids An array of ancestor ids for active page.
+ * @param array   $colors Color information for overlay styles.
+ * @param integer $depth The nesting depth.
  *
  * @return string List markup.
  */
