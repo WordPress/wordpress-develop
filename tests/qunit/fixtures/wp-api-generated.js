92e376ac2f8ec8c1588ffc52e32c23bf477ca16c
Lakshyajeet Singh Goyal <lakshyajeet.goyal@rtcamp.com>
2026-04-30 14:01:07 +0530

    fix: update qunit fixtures

--- tests/qunit/fixtures/wp-api-generated.js
+++ tests/qunit/fixtures/wp-api-generated.js
@@ -10200,6 +10200,15 @@ mockedApiResponse.Schema = {
                             "minLength": 1,
                             "pattern": ".*\\S.*",
                             "required": true
+                        },
+                        "expires": {
+                            "description": "The GMT date the application password expires.",
+                            "type": [
+                                "string",
+                                "null"
+                            ],
+                            "format": "date-time",
+                            "required": false
                         }
                     }
                 },
@@ -10294,6 +10303,15 @@ mockedApiResponse.Schema = {
                             "type": "string",
                             "minLength": 1,
                             "pattern": ".*\\S.*",
+                            "required": false
+                        },
+                        "expires": {
+                            "description": "The GMT date the application password expires.",
+                            "type": [
+                                "string",
+                                "null"
+                            ],
+                            "format": "date-time",
                             "required": false
                         }
                     }
