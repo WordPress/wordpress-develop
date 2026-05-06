f7a2911cd79a395006a779ad56f81b9b67f328c7
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2026-05-06 13:29:03 +0200

    Abilities API: Set REST status for normalization errors.

--- src/wp-includes/rest-api/endpoints/class-wp-rest-abilities-v1-run-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-abilities-v1-run-controller.php
@@ -161,6 +161,11 @@ class WP_REST_Abilities_V1_Run_Controller extends WP_REST_Controller {
 		$input = $this->get_input_from_request( $request );
 		$input = $ability->normalize_input( $input );
 		if ( is_wp_error( $input ) ) {
+			$error_data = $input->get_error_data();
+			if ( ! is_array( $error_data ) || ! isset( $error_data['status'] ) ) {
+				$input->add_data( array( 'status' => 400 ) );
+			}
+
 			return $input;
 		}
 
