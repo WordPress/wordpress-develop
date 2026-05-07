48a9321e4a9de1dc9ef27c73fec39834fc398f06
Paul Bearne <pbearne@git.wordpress.org>
2026-05-07 11:50:46 -0400

    Add unit tests for wp_page_reload_on_back_button_js() in wp-admin/includes/misc.php

---
+++ tests/phpunit/tests/admin/includes/misc/wpPageReloadOnBackButtonJs.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Test wp_page_reload_on_back_button_js().
+ *
+ * @group admin
+ * @group misc
+ */
+class Tests_Admin_Includes_Misc_WpPageReloadOnBackButtonJs extends WP_UnitTestCase {
+
+	/**
+	 * Tests wp_page_reload_on_back_button_js().
+	 */
+	public function test_wp_page_reload_on_back_button_js() {
+		ob_start();
+		wp_page_reload_on_back_button_js();
+		$output = ob_get_clean();
+
+		$this->assertStringContainsString( '<script>', $output );
+		$this->assertStringContainsString( 'performance.navigation.type === 2', $output );
+		$this->assertStringContainsString( 'document.location.reload( true )', $output );
+		$this->assertStringContainsString( '</script>', $output );
+	}
+}
