448cb2eef873702ae3ee664392486445f1e38d60
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2026-05-08 23:36:32 +0900

    Simplifying context
    Co-authored-by: Aaron Jorbin <aaronjorbin@users.noreply.github.com>

--- src/wp-admin/includes/class-custom-image-header.php
+++ src/wp-admin/includes/class-custom-image-header.php
@@ -550,7 +550,7 @@ class Custom_Image_Header {
 
 		<?php if ( get_custom_header() || display_header_text() ) : ?>
 <tr>
-<th scope="row"><?php echo esc_html_x( 'Preview', 'noun, table row label for the header image preview' ); ?></th>
+<th scope="row"><?php echo esc_html_x( 'Preview', 'noun' ); ?></th>
 <td>
 			<?php
 			if ( $this->admin_image_div_callback ) {
