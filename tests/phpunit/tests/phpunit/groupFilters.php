bb1a9c9eb96c491f688e387b1ee812b7948cd880
Arkaprabha Chowdhury <arkaprabha.chowdhury@rtcamp.com>
2026-05-12 15:15:29 +0530

    Tests: Preserve default excluded groups when using --exclude-group.

---
+++ tests/phpunit/tests/phpunit/groupFilters.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @group phpunit
+ */
+class Tests_PHPUnit_GroupFilters extends WP_UnitTestCase {
+
+	public function tear_down() {
+		WP_PHPUnit_Util_Getopt::reset();
+		parent::tear_down();
+	}
+
+	public function test_single_site_default_excluded_groups_are_reported() {
+		$this->assertSame(
+			array( 'ajax', 'external-http' ),
+			WP_UnitTestCase_Base::get_default_excluded_groups_for_test( array( 'ajax', 'external-http', 'query' ) )
+		);
+	}
+
+	public function test_requested_group_is_not_reported_as_default_excluded() {
+		ob_start();
+		new WP_PHPUnit_Util_Getopt(
+			array(
+				'phpunit',
+				'--group',
+				'ajax',
+			)
+		);
+		ob_end_clean();
+
+		$this->assertSame(
+			array(),
+			WP_UnitTestCase_Base::get_default_excluded_groups_for_test( array( 'ajax' ) )
+		);
+	}
+
+	public function test_exclude_group_does_not_clear_default_excluded_groups() {
+		ob_start();
+		new WP_PHPUnit_Util_Getopt(
+			array(
+				'phpunit',
+				'--exclude-group',
+				'import',
+			)
+		);
+		ob_end_clean();
+
+		$this->assertSame(
+			array( 'ajax', 'ms-required' ),
+			WP_UnitTestCase_Base::get_default_excluded_groups_for_test( array( 'ajax', 'ms-required' ) )
+		);
+	}
+}
