7f29a32b1b291b07348bd1daec0fb17e8f4fd62f
Deepak rohilla <deepakrohilla.deepakrohilla@gmail.com>
2026-05-18 14:11:41 +0530

    Update blocks.php

--- src/wp-includes/blocks.php
+++ src/wp-includes/blocks.php
@@ -128,8 +128,8 @@ function get_block_asset_url( $path ) {
 }
 
 /**
- * Finds a script module ID for the selected block metadata field. Detects
- * when a path to file was provided and optionally finds a corresponding asset
+ * Finds a script module ID for the selected block metadata field.
+ * Detects when a path to file was provided and optionally finds a corresponding asset
  * file with details necessary to register the script module under with an
  * automatically generated module ID. It returns unprocessed script module
  * ID otherwise.
