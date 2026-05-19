1afcd3099d9198daedb31350f64671b8b3abd62c
Boro Sitnikovski <buritomath@gmail.com>
2026-05-20 00:36:26 +0200

    Plugins: Fix inverted is_callable() condition in _wp_filter_build_unique_id().
    The guard should return null for non-callable arrays, not for valid ones.

--- src/wp-includes/plugin.php
+++ src/wp-includes/plugin.php
@@ -1007,7 +1007,7 @@ function _wp_filter_build_unique_id( $hook_name, $callback, $priority ): ?string
 	}
 
 	$callback = (array) $callback;
-	if ( is_callable( $callback, true ) ) {
+	if ( ! is_callable( $callback, true ) ) {
 		return null;
 	}
 
