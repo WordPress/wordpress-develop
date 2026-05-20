b71c362368e4c14493d22cb0288c7460dad63c9a
Khokan Sardar <itzmekhokan@gmail.com>
2026-05-21 00:26:08 +0530

    General Settings: Reject empty custom date and time formats.
    Validate custom date and time format fields on the General Settings screen
    before saving. When either custom field is empty, restore the previous value,
    show an admin error, and reject empty values in sanitize_option() as well.
    
    Fixes #65281.

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -4957,6 +4957,10 @@ function sanitize_option( $option, $value ) {
 			} else {
 				$value = strip_tags( $value );
 				$value = wp_kses_data( $value );
+
+				if ( ( 'date_format' === $option || 'time_format' === $option ) && '' === $value ) {
+					$error = __( 'Please configure a valid date and time format.' );
+				}
 			}
 			break;
 
