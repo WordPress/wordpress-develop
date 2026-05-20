e6c4fff880e0689c4cc72c09c62e20c9323abc75
Paul Bearne <pbearne@gmail.com>
2026-05-20 09:40:47 -0400

    Update getTagcloud.php

--- tests/phpunit/tests/admin/includes/ajax-actions/getTagcloud.php
+++ tests/phpunit/tests/admin/includes/ajax-actions/getTagcloud.php
@@ -166,10 +166,10 @@ class Tests_wp_ajax_get_tagcloud extends WP_Ajax_UnitTestCase {
 
 		// Ensure no tags exist for the taxonomy.
 		$tags = get_terms(
-			array(
-				'taxonomy' => 'post_tag',
-				'hide_empty' => false,
-			)
+		array(
+			'taxonomy' => 'post_tag',
+			'hide_empty' => false,
+		    )
 		);
 		foreach ( $tags as $tag ) {
 			wp_delete_term( $tag->term_id, 'post_tag' );
