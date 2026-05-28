e81153f045350b471f8bba82aabfd0b2ff7f4511
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2026-05-28 09:32:01 -0400

    Update src/wp-includes/general-template.php
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -351,9 +351,7 @@ function get_search_form( $args = array() ): ?string {
 				 * from the form to avoid nesting two identical landmarks. Any aria-label
 				 * names the <search> landmark instead of the form.
 				 */
-				$search_label = $args['aria_label'] ? ' aria-label="' . esc_attr( $args['aria_label'] ) . '"' : '';
-
-				$form = '<search' . $search_label . '><form method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">' . $form_inner . '</form></search>';
+				$form = '<search ' . $aria_label . '><form method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">' . $form_inner . '</form></search>';
 			} else {
 				$form = '<form role="search" ' . $aria_label . 'method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">' . $form_inner . '</form>';
 			}
