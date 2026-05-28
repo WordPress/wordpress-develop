f728cfb6921645d03afef7e3b4544d3a63fd57d7
Weston Ruter <weston@ruter.net>
2026-05-28 10:52:04 -0700

    Use non-deprecated factory

--- tests/phpunit/tests/media/wpDeleteAttachmentHeicCompanionFile.php
+++ tests/phpunit/tests/media/wpDeleteAttachmentHeicCompanionFile.php
@@ -18,7 +18,7 @@ class Tests_Media_wpDeleteAttachmentHeicCompanionFile extends WP_UnitTestCase {
 	 * @ticket 64915
 	 */
 	public function test_deletes_heic_file_recorded_in_metadata_original(): void {
-		$attachment_id = $this->factory->attachment->create_upload_object( DIR_TESTDATA . '/images/canola.jpg' );
+		$attachment_id = self::factory()->attachment->create_upload_object( DIR_TESTDATA . '/images/canola.jpg' );
 
 		$attached_file = get_attached_file( $attachment_id, true );
 		$dir           = dirname( $attached_file );
@@ -43,7 +43,7 @@ class Tests_Media_wpDeleteAttachmentHeicCompanionFile extends WP_UnitTestCase {
 	 * @ticket 64915
 	 */
 	public function test_noop_when_metadata_original_is_missing(): void {
-		$attachment_id = $this->factory->attachment->create_upload_object( DIR_TESTDATA . '/images/canola.jpg' );
+		$attachment_id = self::factory()->attachment->create_upload_object( DIR_TESTDATA . '/images/canola.jpg' );
 
 		// Sanity: no 'original' key on freshly-created metadata.
 		$metadata = wp_get_attachment_metadata( $attachment_id, true );
@@ -62,7 +62,7 @@ class Tests_Media_wpDeleteAttachmentHeicCompanionFile extends WP_UnitTestCase {
 	 * @ticket 64915
 	 */
 	public function test_noop_when_metadata_original_is_not_a_string(): void {
-		$attachment_id = $this->factory->attachment->create_upload_object( DIR_TESTDATA . '/images/canola.jpg' );
+		$attachment_id = self::factory()->attachment->create_upload_object( DIR_TESTDATA . '/images/canola.jpg' );
 		$attached_file = get_attached_file( $attachment_id, true );
 
 		$metadata             = wp_get_attachment_metadata( $attachment_id, true );
