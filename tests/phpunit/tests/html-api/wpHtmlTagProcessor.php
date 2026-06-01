96d81fe0027fd9fe8b5f3d04b31b728448995eba
Jon Surrell <sirreal@users.noreply.github.com>
2026-06-01 12:57:29 +0200

    Tests: HTML API: Fix typo "subesequent" in remove_class assertion messages.
    Corrects two occurrences of "subesequent" to "subsequent" in the assertion messages of the remove_class() test.

--- tests/phpunit/tests/html-api/wpHtmlTagProcessor.php
+++ tests/phpunit/tests/html-api/wpHtmlTagProcessor.php
@@ -1523,11 +1523,11 @@ class Tests_HtmlApi_WpHtmlTagProcessor extends WP_UnitTestCase {
 		$this->assertSame(
 			'<div  id="first"><span class="not-main bold with-border" id="second">Text</span></div>',
 			$processor->get_updated_html(),
-			'Updated HTML does not reflect class attribute removed via subesequent remove_class() calls'
+			'Updated HTML does not reflect class attribute removed via subsequent remove_class() calls'
 		);
 		$this->assertNull(
 			$processor->get_attribute( 'class' ),
-			"get_attribute( 'class' ) did not return null for class attribute removed via subesequent remove_class() calls"
+			"get_attribute( 'class' ) did not return null for class attribute removed via subsequent remove_class() calls"
 		);
 	}
 
