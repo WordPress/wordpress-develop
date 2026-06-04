95c277634efb3d9fae59582abb174e347cf8bd6f
Sören Wünsch <soeren.wrede@parshipgroup.com>
2026-06-04 15:06:01 +0200

    Add WP_Filesystem_ftpsockets

--- src/wp-admin/includes/class-wp-filesystem-ftpsockets.php
+++ src/wp-admin/includes/class-wp-filesystem-ftpsockets.php
@@ -26,9 +26,16 @@ class WP_Filesystem_ftpsockets extends WP_Filesystem_Base {
 	 *
 	 * @since 2.5.0
 	 *
-	 * @param array $opt
+	 * @param array $opt {
+	 * *     Optional. Array of connection options.
+	 * *
+	 * *     @type string $hostname Required. FTP server hostname.
+	 * *     @type string $username Required. FTP username.
+	 * *     @type string $password Required. FTP password.
+	 * *     @type int $port Optional. FTP server port. Default 21.
+	 * * }
 	 */
-	public function __construct( $opt = '' ) {
+	public function __construct( $opt = array() ) {
 		$this->method = 'ftpsockets';
 		$this->errors = new WP_Error();
 
