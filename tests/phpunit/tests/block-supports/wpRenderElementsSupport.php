4265f5f1561895287e41b7774457b212a26e6b6c
Weston Ruter <weston@ruter.net>
2026-06-08 17:17:06 -0700

    Fix assertion message

--- tests/phpunit/tests/block-supports/wpRenderElementsSupport.php
+++ tests/phpunit/tests/block-supports/wpRenderElementsSupport.php
@@ -155,7 +155,7 @@ class Tests_Block_Supports_WpRenderElementsSupport extends WP_UnitTestCase {
 		$this->assertSame(
 			$block_content,
 			wp_render_elements_class_name( $block_content, $block ),
-			'Block content should be returned unchanged when className is not a string'
+			'Block content should be returned unchanged when className lacks a class with the expected prefix'
 		);
 	}
 
