bd476f62d997bf2c2d2be2294c83c766cf6c3c4e
Jonathan Desrosiers <359867+desrosj@users.noreply.github.com>
2026-06-11 12:24:27 -0400

    Enforce one rule per line.

--- src/wp-content/themes/twentynineteen/postcss.config.js
+++ src/wp-content/themes/twentynineteen/postcss.config.js
@@ -6,10 +6,25 @@ module.exports = {
     }
 };
 
+var oneSelectorPerLine = function () {
+    return {
+        postcssPlugin: 'one-selector-per-line',
+        Rule: function (rule) {
+            if (rule.selector.indexOf(',') !== -1) {
+                var before = rule.raws.before || '';
+                var indent = before.substring(before.lastIndexOf('\n') + 1);
+                rule.selector = rule.selector.split(/,\s*/).join(',\n' + indent);
+            }
+        }
+    };
+};
+oneSelectorPerLine.postcss = true;
+
 module.exports = {
     plugins: [
         postcssFocusWithin({
             disablePolyfillReadyClass: true
-        })
+        }),
+        oneSelectorPerLine
     ]
 };
