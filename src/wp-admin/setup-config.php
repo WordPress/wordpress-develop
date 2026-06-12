164872ac91a367b0c3faa0c54266a39e082ae84f
Andrea Fercia <a.fercia@gmail.com>
2026-06-12 14:37:31 +0200

    Fix lang attribute on the setup-config page.

--- src/wp-admin/setup-config.php
+++ src/wp-admin/setup-config.php
@@ -95,16 +95,15 @@ function setup_config_display_header( $body_classes = array() ) {
 	$body_classes   = (array) $body_classes;
 	$body_classes[] = 'wp-core-ui';
 	$body_classes[] = 'admin-color-modern';
-	$dir_attr       = '';
+
 	if ( is_rtl() ) {
 		$body_classes[] = 'rtl';
-		$dir_attr       = ' dir="rtl"';
 	}
 
 	header( 'Content-Type: text/html; charset=utf-8' );
 	?>
 <!DOCTYPE html>
-<html<?php echo $dir_attr; ?>>
+<html <?php language_attributes(); ?>>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
