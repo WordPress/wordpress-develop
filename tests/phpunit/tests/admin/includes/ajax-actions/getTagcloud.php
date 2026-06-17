1fea39dcecd3d63a065f3bca2ad441307ecc7e56
Paul Bearne <pbearne@gmail.com>
2026-06-17 18:31:49 -0400

    Fix formatting of taxonomy key in getTagcloud.php

--- tests/phpunit/tests/admin/includes/ajax-actions/getTagcloud.php
+++ tests/phpunit/tests/admin/includes/ajax-actions/getTagcloud.php
@@ -167,7 +167,7 @@ class Tests_wp_ajax_get_tagcloud extends WP_Ajax_UnitTestCase {
 		// Ensure no tags exist for the taxonomy.
 		$tags = get_terms(
 			array(
-				'taxonomy' => 'post_tag',
+				'taxonomy'   => 'post_tag',
 				'hide_empty' => false,
 			)
 		);
