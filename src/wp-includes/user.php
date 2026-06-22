f88dafc5d8d7e20ac04f6bb0e6514bdf3c5e8a46
Mohd Salman <47385962+salmanshafiq8630@users.noreply.github.com>
2026-06-22 13:23:38 +0530

    Update user.php Change Since Version 7.1

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -5234,7 +5234,7 @@ function wp_is_password_reset_allowed_for_user( $user ) {
 /**
  * Retrieves the roles assigned to a user.
  *
- * @since 6.x.x
+ * @since 7.1
  *
  * @param int $user_id User ID.
  * @return string[] Array of role slugs. Returns an empty array if the user does not exist or has no assigned roles.
