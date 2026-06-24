c2dc85c35e91246d20459002e15bd6899170e763
Sainath Poojary <spoojary614@gmail.com>
2026-06-25 00:10:00 +0530

    style: remove trailing whitespace in blogger_getUsersBlogs test file

--- tests/phpunit/tests/xmlrpc/blogger/getUsersBlogs.php
+++ tests/phpunit/tests/xmlrpc/blogger/getUsersBlogs.php
@@ -11,13 +11,13 @@ class Tests_XMLRPC_blogger_getUsersBlogs extends WP_XMLRPC_UnitTestCase {
 	 */
 	public function test_multisite_argument_parsing() {
 		$subscriber_id = $this->make_user_by_role( 'subscriber' );
-		
+
 		$result = $this->myxmlrpcserver->blogger_getUsersBlogs( array( 1, 'subscriber', 'subscriber' ) );
 
 		$this->assertNotIXRError( $result );
 		$this->assertIsArray( $result );
 		$this->assertNotEmpty( $result );
-		
+
 		$blog = $result[0];
 		$this->assertArrayHasKey( 'url', $blog );
 		$this->assertArrayHasKey( 'blogid', $blog );
