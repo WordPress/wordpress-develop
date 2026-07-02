9cd6b6e27f9eab71e40f8174e1b8833df9b86673
ntsekouras <ntsekouras@outlook.com>
2026-07-02 13:14:36 +0300

    View Config: Add versioning handling

--- src/wp-includes/default-filters.php
+++ src/wp-includes/default-filters.php
@@ -826,9 +826,7 @@ add_filter( 'rest_pre_insert_wp_template_part', 'inject_ignored_hooked_blocks_me
 foreach ( array( 'page', 'wp_block', 'wp_template_part', 'wp_template' ) as $post_type ) {
 	add_filter(
 		"get_entity_view_config_postType_{$post_type}",
-		"_wp_get_entity_view_config_post_type_{$post_type}",
-		10,
-		1
+		"_wp_get_entity_view_config_post_type_{$post_type}"
 	);
 }
 
