22138b31f170835da91c0289f1e4c2744c03b632
ArkaPrabhaChowdhury <arkopra@gmail.com>
2026-07-03 15:28:12 +0530

    Tests: Isolate admin menu URL test state

--- tests/phpunit/tests/admin/menuHeader.php
+++ tests/phpunit/tests/admin/menuHeader.php
@@ -5,26 +5,10 @@
 class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 
 	/**
-	 * Admin user ID.
-	 *
-	 * @var int
-	 */
-	public static $admin_id;
-
-	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
-		self::$admin_id = $factory->user->create( array( 'role' => 'administrator' ) );
-	}
-
-	public static function wpTearDownAfterClass() {
-		self::delete_user( self::$admin_id );
-	}
-
-	/**
 	 * @ticket 56302
 	 */
 	public function test_wp_menu_output_uses_absolute_top_level_url() {
-		wp_set_current_user( self::$admin_id );
-		update_option( 'siteurl', 'http://example.com' );
+		wp_set_current_user( self::factory()->user->create( array( 'role' => 'administrator' ) ) );
 
 		$menu = array(
 			array( 'Dashboard', 'read', 'index.php', 'Dashboard', 'menu-top', 'menu-dashboard', 'dashicons-dashboard' ),
@@ -32,15 +16,14 @@ class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 
 		$actual = $this->render_admin_menu( $menu, array() );
 
-		$this->assertStringContainsString( "href='http://example.com/wp-admin/index.php'", $actual );
+		$this->assertStringContainsString( "href='" . admin_url( 'index.php' ) . "'", $actual );
 	}
 
 	/**
 	 * @ticket 56302
 	 */
 	public function test_wp_menu_output_uses_absolute_submenu_parent_url() {
-		wp_set_current_user( self::$admin_id );
-		update_option( 'siteurl', 'http://example.com' );
+		wp_set_current_user( self::factory()->user->create( array( 'role' => 'administrator' ) ) );
 
 		$menu    = array(
 			array( 'Tools', 'read', 'tools.php', 'Tools', 'menu-top', 'menu-tools', 'dashicons-admin-tools' ),
@@ -53,15 +36,14 @@ class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 
 		$actual = $this->render_admin_menu( $menu, $submenu );
 
-		$this->assertStringContainsString( "href='http://example.com/wp-admin/tools.php'", $actual );
+		$this->assertStringContainsString( "href='" . admin_url( 'tools.php' ) . "'", $actual );
 	}
 
 	/**
 	 * @ticket 56302
 	 */
 	public function test_wp_menu_output_uses_absolute_submenu_urls() {
-		wp_set_current_user( self::$admin_id );
-		update_option( 'siteurl', 'http://example.com' );
+		wp_set_current_user( self::factory()->user->create( array( 'role' => 'administrator' ) ) );
 
 		$menu    = array(
 			array( 'Posts', 'read', 'edit.php', 'Posts', 'menu-top', 'menu-posts', 'dashicons-admin-post' ),
@@ -75,16 +57,15 @@ class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 
 		$actual = $this->render_admin_menu( $menu, $submenu );
 
-		$this->assertStringContainsString( "href='http://example.com/wp-admin/edit.php'", $actual );
-		$this->assertStringContainsString( "href='http://example.com/wp-admin/post-new.php?post_type=post'", $actual );
+		$this->assertStringContainsString( "href='" . admin_url( 'edit.php' ) . "'", $actual );
+		$this->assertStringContainsString( "href='" . admin_url( 'post-new.php?post_type=post' ) . "'", $actual );
 	}
 
 	/**
 	 * @ticket 56302
 	 */
 	public function test_wp_menu_output_uses_absolute_plugin_page_url() {
-		wp_set_current_user( self::$admin_id );
-		update_option( 'siteurl', 'http://example.com' );
+		wp_set_current_user( self::factory()->user->create( array( 'role' => 'administrator' ) ) );
 
 		add_menu_page( 'Test Page', 'Test Page', 'read', 'test-page', '__return_null' );
 
@@ -92,15 +73,14 @@ class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 
 		$actual = $this->render_admin_menu( $menu, array(), false );
 
-		$this->assertStringContainsString( "href='http://example.com/wp-admin/admin.php?page=test-page'", $actual );
+		$this->assertStringContainsString( "href='" . admin_url( 'admin.php?page=test-page' ) . "'", $actual );
 	}
 
 	/**
 	 * @ticket 56302
 	 */
 	public function test_wp_menu_output_preserves_external_urls() {
-		wp_set_current_user( self::$admin_id );
-		update_option( 'siteurl', 'http://example.com' );
+		wp_set_current_user( self::factory()->user->create( array( 'role' => 'administrator' ) ) );
 
 		$menu = array(
 			array( 'External', 'read', 'https://wordpress.org/news/', 'External', 'menu-top', 'menu-external', 'dashicons-admin-site' ),
@@ -116,6 +96,8 @@ class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 
 		global $self, $parent_file, $submenu_file, $plugin_page, $typenow;
 
+		$globals = $this->backup_globals( array( 'self', 'parent_file', 'submenu_file', 'plugin_page', 'typenow' ) );
+
 		$self         = 'index.php';
 		$parent_file  = '';
 		$submenu_file = '';
@@ -123,8 +105,12 @@ class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 		$typenow      = '';
 
 		ob_start();
-		_wp_menu_output( $menu, $submenu, $submenu_as_parent );
-		return ob_get_clean();
+		try {
+			_wp_menu_output( $menu, $submenu, $submenu_as_parent );
+			return ob_get_clean();
+		} finally {
+			$this->restore_globals( $globals );
+		}
 	}
 
 	private function require_menu_header() {
@@ -134,6 +120,9 @@ class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 
 		global $menu, $submenu, $parent_file, $submenu_file;
 
+		$globals  = $this->backup_globals( array( 'menu', 'submenu', 'parent_file', 'submenu_file' ) );
+		$php_self = isset( $_SERVER['PHP_SELF'] ) ? $_SERVER['PHP_SELF'] : null;
+
 		$menu         = array();
 		$submenu      = array();
 		$parent_file  = '';
@@ -142,7 +131,40 @@ class Tests_Admin_MenuHeader extends WP_UnitTestCase {
 		$_SERVER['PHP_SELF'] = '/wp-admin/index.php';
 
 		ob_start();
-		require ABSPATH . 'wp-admin/menu-header.php';
-		ob_end_clean();
+		try {
+			require ABSPATH . 'wp-admin/menu-header.php';
+			ob_end_clean();
+		} finally {
+			$this->restore_globals( $globals );
+
+			if ( null === $php_self ) {
+				unset( $_SERVER['PHP_SELF'] );
+			} else {
+				$_SERVER['PHP_SELF'] = $php_self;
+			}
+		}
+	}
+
+	private function backup_globals( $names ) {
+		$globals = array();
+
+		foreach ( $names as $name ) {
+			$globals[ $name ] = array(
+				'exists' => array_key_exists( $name, $GLOBALS ),
+				'value'  => array_key_exists( $name, $GLOBALS ) ? $GLOBALS[ $name ] : null,
+			);
+		}
+
+		return $globals;
+	}
+
+	private function restore_globals( $globals ) {
+		foreach ( $globals as $name => $global ) {
+			if ( $global['exists'] ) {
+				$GLOBALS[ $name ] = $global['value'];
+			} else {
+				unset( $GLOBALS[ $name ] );
+			}
+		}
 	}
 }
