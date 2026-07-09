a2a3e02422c2eba4d6442cffbc992db914955218
Weston Ruter <weston@ruter.net>
2026-07-09 13:28:04 -0700

    Add type definition for $args on wp_delete_term()

--- src/wp-includes/taxonomy.php
+++ src/wp-includes/taxonomy.php
@@ -2048,6 +2048,10 @@ function wp_delete_object_term_relationships( $object_id, $taxonomies ) {
  * @return bool|int|WP_Error True on success, false if term does not exist. Zero on attempted
  *                           deletion of default Category. WP_Error if the taxonomy does not exist.
  * @phpstan-param non-empty-string $taxonomy
+ * @phpstan-param string|array{
+ *     default?: int|numeric-string,
+ *     force_default?: bool,
+ * } $args
  * @phpstan-return bool|WP_Error|0
  */
 function wp_delete_term( $term, $taxonomy, $args = array() ) {
