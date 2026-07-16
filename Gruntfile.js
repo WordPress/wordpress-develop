7b8926a46e3f8846590c784d7d442313fd73ac0e
Jarda Snajdr <jsnajdr@gmail.com>
2026-07-16 12:25:56 +0200

    Move Gutenberg vendor copying to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,8 +51,6 @@ module.exports = function(grunt) {
 			 */
 			'!wp-includes/js/dist',
 			'wp-includes/js/dist/vendor/*.js',
-			// Managed by the Gutenberg-related tasks (react, react-dom, react-jsx-runtime).
-			'!wp-includes/js/dist/vendor/react*',
 		],
 
 		// Files sourced from the Gutenberg repository built asset that are ignored by version control.
@@ -61,7 +59,6 @@ module.exports = function(grunt) {
 			SOURCE_DIR + 'wp-includes/css/dist',
 			SOURCE_DIR + 'wp-includes/js/dist/*.js',
 			SOURCE_DIR + 'wp-includes/js/dist/script-modules',
-			SOURCE_DIR + 'wp-includes/js/dist/vendor/react*',
 		],
 
 		// Files sourced from the Gutenberg repository built asset that are managed through version control.
@@ -454,6 +451,15 @@ module.exports = function(grunt) {
 					{
 						[ WORKING_DIR + 'wp-includes/js/dist/vendor/regenerator-runtime.js' ]: [ './node_modules/regenerator-runtime/runtime.js' ],
 						[ WORKING_DIR + 'wp-includes/js/dist/vendor/regenerator-runtime.min.js' ]: [ './node_modules/regenerator-runtime/runtime.js' ],
+					},
+					// React libraries: react, react-dom, react-jsx-runtime (sourced from the Gutenberg build).
+					{
+						[ WORKING_DIR + 'wp-includes/js/dist/vendor/react.js' ]: [ './gutenberg/build/scripts/vendors/react.js' ],
+						[ WORKING_DIR + 'wp-includes/js/dist/vendor/react.min.js' ]: [ './gutenberg/build/scripts/vendors/react.min.js' ],
+						[ WORKING_DIR + 'wp-includes/js/dist/vendor/react-dom.js' ]: [ './gutenberg/build/scripts/vendors/react-dom.js' ],
+						[ WORKING_DIR + 'wp-includes/js/dist/vendor/react-dom.min.js' ]: [ './gutenberg/build/scripts/vendors/react-dom.min.js' ],
+						[ WORKING_DIR + 'wp-includes/js/dist/vendor/react-jsx-runtime.js' ]: [ './gutenberg/build/scripts/vendors/react-jsx-runtime.js' ],
+						[ WORKING_DIR + 'wp-includes/js/dist/vendor/react-jsx-runtime.min.js' ]: [ './gutenberg/build/scripts/vendors/react-jsx-runtime.min.js' ],
 					},
 					// Polyfills
 					{
