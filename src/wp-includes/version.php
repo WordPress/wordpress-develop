f6a86c3f756c212fa0fbf83755616da7fb3c5451
Aaron Jorbin <jorbin@git.wordpress.org>
2026-07-17 19:34:18 +0000

    Post WordPress 7.0.2 version bump.
    The 7.0 branch is now 7.0.3 alpha.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/7.0@62793 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0.2-src';
+$wp_version = '7.0.3-alpha-62792-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
