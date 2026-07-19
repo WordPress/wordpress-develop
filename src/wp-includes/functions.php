756bf6a0893d366f0302a76bbe6c699a447da871
Weston Ruter <weston@ruter.net>
2026-07-18 18:10:53 -0700

    Fix phpstan complaint about under-typed array param

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -5042,7 +5042,7 @@ function wp_parse_list( $input_list ) {
  * @since 3.0.0
  * @since 5.1.0 Refactored to use wp_parse_list().
  *
- * @param array|string $input_list List of IDs.
+ * @param mixed[]|string $input_list List of IDs.
  * @return int[] Sanitized array of IDs.
  * @phpstan-return list<non-negative-int>
  */
