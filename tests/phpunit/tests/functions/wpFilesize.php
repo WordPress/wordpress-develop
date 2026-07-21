32585a8b4ef1e65bfa48d2dd99e6a0f6f2636370
Weston Ruter <weston@ruter.net>
2026-07-21 12:44:34 -0700

    Add tests

--- tests/phpunit/tests/functions/wpFilesize.php
+++ tests/phpunit/tests/functions/wpFilesize.php
@@ -9,46 +9,75 @@
  */
 class Tests_Functions_wpFilesize extends WP_UnitTestCase {
 
+	const TEST_FILE = DIR_TESTDATA . '/images/test-image-upside-down.jpg';
+
 	/**
 	 * @ticket 49412
 	 */
-	public function test_wp_filesize() {
-		$file = DIR_TESTDATA . '/images/test-image-upside-down.jpg';
-
-		$this->assertSame( filesize( $file ), wp_filesize( $file ) );
+	public function test_wp_filesize(): void {
+		$this->assertSame( filesize( self::TEST_FILE ), wp_filesize( self::TEST_FILE ) );
 	}
 
 	/**
 	 * @ticket 49412
 	 */
-	public function test_wp_filesize_filters() {
-		$file = DIR_TESTDATA . '/images/test-image-upside-down.jpg';
+	public function test_wp_filesize_filters(): void {
+		add_filter( 'wp_filesize', static fn () => 999 );
 
-		add_filter(
-			'wp_filesize',
-			static function () {
-				return 999;
-			}
-		);
+		$this->assertSame( 999, wp_filesize( self::TEST_FILE ) );
 
-		$this->assertSame( 999, wp_filesize( $file ) );
+		add_filter( 'pre_wp_filesize', static fn () => 111 );
 
-		add_filter(
-			'pre_wp_filesize',
-			static function () {
-				return 111;
-			}
-		);
-
-		$this->assertSame( 111, wp_filesize( $file ) );
+		$this->assertSame( 111, wp_filesize( self::TEST_FILE ) );
 	}
 
 	/**
 	 * @ticket 49412
 	 */
-	public function test_wp_filesize_with_nonexistent_file() {
-		$file = 'nonexistent/file.jpg';
+	public function test_wp_filesize_with_nonexistent_file(): void {
+		$this->assertSame( 0, wp_filesize( 'nonexistent/file.jpg' ) );
+	}
 
-		$this->assertSame( 0, wp_filesize( $file ) );
+	/**
+	 * @ticket 65670
+	 */
+	public function test_wp_filesize_pre_wp_filesize_filter_null(): void {
+		add_filter( 'pre_wp_filesize', '__return_null' );
+
+		$this->assertSame( filesize( self::TEST_FILE ), wp_filesize( self::TEST_FILE ) );
+	}
+
+	/**
+	 * @ticket 65670
+	 */
+	public function test_wp_filesize_pre_wp_filesize_filter_negative(): void {
+		add_filter( 'pre_wp_filesize', static fn() => -1 );
+
+		$this->assertSame( filesize( self::TEST_FILE ), wp_filesize( self::TEST_FILE ) );
+	}
+
+	/**
+	 * @ticket 65670
+	 *
+	 * @dataProvider data_provider_test_wp_filesize_filter_invalid_value
+	 *
+	 * @param mixed $value
+	 */
+	public function test_wp_filesize_wp_filesize_filter_invalid_value( $value ): void {
+		add_filter( 'wp_filesize', static fn() => $value );
+		$this->assertSame( 0, wp_filesize( self::TEST_FILE ) );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array<string, array{ 0: mixed }>
+	 */
+	public function data_provider_test_wp_filesize_filter_invalid_value(): array {
+		return array(
+			'negative' => array( -1 ),
+			'null'     => array( null ),
+			'array'    => array( array( 'bad', 'array' ) ),
+		);
 	}
 }
