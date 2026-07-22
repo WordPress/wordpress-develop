73fbff2d02da90408692acc182a93ef998313df6
Sainath Poojary <spoojary614@gmail.com>
2026-07-22 18:28:03 +0530

    fix: use document.getElementById for panel selection

--- src/js/_enqueues/admin/common.js
+++ src/js/_enqueues/admin/common.js
@@ -695,7 +695,7 @@ $('.contextual-help-tabs').on( 'click', 'a', function(e) {
 	$('.contextual-help-tabs .active').removeClass('active');
 	link.parent('li').addClass('active');
 
-	panel = $( '#' + link.attr( 'aria-controls' ) );
+	panel = $( document.getElementById( link.attr( 'aria-controls' ) ) );
 
 	// Panels.
 	$('.help-tab-content').not( panel ).removeClass('active').hide();
