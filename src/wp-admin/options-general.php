a399399eb199bb029516eda1ecab7e889d45a0d7
Himanshu Pathak <himanshu.pathak@rtcamp.com>
2026-07-27 13:25:20 +0530

    Add aria-req attribute

--- src/wp-admin/options-general.php
+++ src/wp-admin/options-general.php
@@ -74,7 +74,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 
 <tr class="form-field form-required">
 <th scope="row"><label for="blogname"><?php _e( 'Site Title' ); ?></label></th>
-<td><input name="blogname" type="text" id="blogname" value="<?php form_option( 'blogname' ); ?>" class="regular-text" required /></td>
+<td><input name="blogname" type="text" id="blogname" value="<?php form_option( 'blogname' ); ?>" class="regular-text" required aria-required="true" /></td>
 </tr>
 
 <?php
