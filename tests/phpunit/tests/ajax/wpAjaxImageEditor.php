feaef53fb5c4606b4b88548660571ff347643a32
Joseph Scott <joseph@josephscott.org>
2026-07-29 16:41:47 -0600

    Update tests/phpunit/tests/ajax/wpAjaxImageEditor.php
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- tests/phpunit/tests/ajax/wpAjaxImageEditor.php
+++ tests/phpunit/tests/ajax/wpAjaxImageEditor.php
@@ -212,7 +212,7 @@ class Tests_Ajax_wpAjaxImageEditor extends WP_Ajax_UnitTestCase {
 	 *
 	 * @covers ::wp_save_image
 	 */
-	public function test_save_image_with_unusable_sizes_metadata( $meta ) {
+	public function test_save_image_with_unusable_sizes_metadata( array $meta ) {
 		require_once ABSPATH . 'wp-admin/includes/image-edit.php';
 
 		$filename = DIR_TESTDATA . '/images/canola.jpg';
