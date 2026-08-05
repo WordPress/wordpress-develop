23625b16d8830f3c03d64c9c2e7bc44dcb734c57
Weston Ruter <weston@ruter.net>
2026-08-04 18:10:42 -0700

    Add missing never return

--- src/wp-admin/includes/ajax-actions.php
+++ src/wp-admin/includes/ajax-actions.php
@@ -287,6 +287,8 @@ function wp_ajax_oembed_cache() {
  * @since 3.4.0
  * @since 7.1.0 The search term is now sanitized, and a missing, non-string,
  *              or empty term results in a `0` response instead of an empty array.
+ *
+ * @return never
  */
 function wp_ajax_autocomplete_user() {
 	if ( ! is_multisite() || ! current_user_can( 'promote_users' ) || wp_is_large_network( 'users' ) ) {
