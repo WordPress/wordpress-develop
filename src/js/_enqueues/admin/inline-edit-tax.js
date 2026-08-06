09bbfae80a01b13dbc5aa036b50832a96e4cc345
ArkaPrabhaChowdhury <arkopra@gmail.com>
2026-08-06 13:26:31 +0530

    Prevent duplicate Quick Edit saves

--- src/js/_enqueues/admin/inline-edit-tax.js
+++ src/js/_enqueues/admin/inline-edit-tax.js
@@ -20,6 +20,12 @@ window.wp = window.wp || {};
 ( function( $, wp ) {
 
 window.inlineEditTax = {
+	/**
+	 * Whether a save request is active.
+	 *
+	 * @type {boolean}
+	 */
+	saving: false,
 
 	/**
 	 * Initializes the inline taxonomy editor by adding event handlers to be able to
@@ -167,6 +173,11 @@ window.inlineEditTax = {
 	save : function(id) {
 		var params, fields, tax = $('input[name="taxonomy"]').val() || '';
 
+		if ( this.saving ) {
+			return false;
+		}
+		this.saving = true;
+
 		// Makes sure we can pass an HTMLElement as the ID.
 		if( typeof(id) === 'object' ) {
 			id = this.getId(id);
@@ -242,7 +253,9 @@ window.inlineEditTax = {
 					wp.a11y.speak( wp.i18n.__( 'Error while saving the changes.' ) );
 				}
 			}
-		);
+		).always( function() {
+			inlineEditTax.saving = false;
+		} );
 
 		// Prevent submitting the form when pressing Enter on a focused field.
 		return false;
