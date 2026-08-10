cd87447b96c4ee949d125490be6e1d836f2a359a
Sören Wünsch <soeren.wrede@parshipgroup.com>
2026-08-10 10:54:22 +0200

    Fix: Update default site_admins to an empty array instead of containing "admin"

--- src/wp-includes/capabilities.php
+++ src/wp-includes/capabilities.php
@@ -1224,7 +1224,7 @@ function grant_super_admin( $user_id ) {
 	do_action( 'grant_super_admin', $user_id );
 
 	// Directly fetch site_admins instead of using get_super_admins().
-	$super_admins = get_site_option( 'site_admins', array( 'admin' ) );
+	$super_admins = get_site_option( 'site_admins', array() );
 
 	$user = get_userdata( $user_id );
 	if ( $user && ! in_array( $user->user_login, $super_admins, true ) ) {
@@ -1272,7 +1272,7 @@ function revoke_super_admin( $user_id ) {
 	do_action( 'revoke_super_admin', $user_id );
 
 	// Directly fetch site_admins instead of using get_super_admins().
-	$super_admins = get_site_option( 'site_admins', array( 'admin' ) );
+	$super_admins = get_site_option( 'site_admins', array() );
 
 	$user = get_userdata( $user_id );
 	if ( $user ) {
