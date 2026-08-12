7b4985482809c81ed5b5bb75a5ed5f0f075dc316
Jonathan Desrosiers <desrosj@git.wordpress.org>
2026-08-12 16:57:46 +0000

    WordPress 5.5.20
    git-svn-id: https://develop.svn.wordpress.org/branches/5.5@63265 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.19-src';
+$wp_version = '5.5.20-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
