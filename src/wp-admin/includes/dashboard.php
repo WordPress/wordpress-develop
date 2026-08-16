ee505311ca418494558aeb636f58700b2c10bd99
Anca Mosoiu <anca@techliminal.com>
2026-08-16 11:39:51 -0700

    formatting for notes in dashboard

--- src/wp-admin/includes/dashboard.php
+++ src/wp-admin/includes/dashboard.php
@@ -933,10 +933,10 @@ function _wp_dashboard_recent_comments_row( &$comment, $show_date = true ) {
 }
 
 /**
- * Outputs a row for the Recent Comments widget.
+ * Outputs a row for the Recent Notes widget.  Notes are implemented as a type of comment.
  *
  * @access private
- * @since 2.7.0
+ * @since 7.2.0
  *
  * @global WP_Comment $comment Global comment object.
  *
@@ -948,130 +948,10 @@ function _wp_dashboard_recent_notes_row( &$comment, $show_date = true ) {
 
 	if ( $comment->comment_post_ID > 0 ) {
 		$comment_post_title = _draft_or_post_title( $comment->comment_post_ID );
-		$comment_post_url   = get_the_permalink( $comment->comment_post_ID );
+		$comment_post_url   = get_edit_post_link( $comment->comment_post_ID );
 		$comment_post_link  = '<a href="' . esc_url( $comment_post_url ) . '">' . $comment_post_title . '</a>';
 	} else {
 		$comment_post_link = '';
-	}
-
-	$actions_string = '';
-	if ( current_user_can( 'edit_comment', $comment->comment_ID ) ) {
-		// Pre-order it: Approve | Reply | Edit | Spam | Trash.
-		$actions = array(
-			'approve'   => '',
-			'unapprove' => '',
-			'reply'     => '',
-			'edit'      => '',
-			'spam'      => '',
-			'trash'     => '',
-			'delete'    => '',
-			'view'      => '',
-		);
-
-		$approve_nonce = esc_html( '_wpnonce=' . wp_create_nonce( 'approve-comment_' . $comment->comment_ID ) );
-		$del_nonce     = esc_html( '_wpnonce=' . wp_create_nonce( 'delete-comment_' . $comment->comment_ID ) );
-
-		$action_string = 'comment.php?action=%s&p=' . $comment->comment_post_ID . '&c=' . $comment->comment_ID . '&%s';
-
-		$approve_url   = sprintf( $action_string, 'approvecomment', $approve_nonce );
-		$unapprove_url = sprintf( $action_string, 'unapprovecomment', $approve_nonce );
-		$spam_url      = sprintf( $action_string, 'spamcomment', $del_nonce );
-		$trash_url     = sprintf( $action_string, 'trashcomment', $del_nonce );
-		$delete_url    = sprintf( $action_string, 'deletecomment', $del_nonce );
-
-		$actions['approve'] = sprintf(
-			'<a href="%s" data-wp-lists="%s" class="vim-a aria-button-if-js" aria-label="%s">%s</a>',
-			esc_url( $approve_url ),
-			"dim:the-comment-list:comment-{$comment->comment_ID}:unapproved:e7e7d3:e7e7d3:new=approved",
-			esc_attr__( 'Approve this comment' ),
-			__( 'Approve' )
-		);
-
-		$actions['unapprove'] = sprintf(
-			'<a href="%s" data-wp-lists="%s" class="vim-u aria-button-if-js" aria-label="%s">%s</a>',
-			esc_url( $unapprove_url ),
-			"dim:the-comment-list:comment-{$comment->comment_ID}:unapproved:e7e7d3:e7e7d3:new=unapproved",
-			esc_attr__( 'Unapprove this comment' ),
-			__( 'Unapprove' )
-		);
-
-		$actions['edit'] = sprintf(
-			'<a href="%s" aria-label="%s">%s</a>',
-			"comment.php?action=editcomment&amp;c={$comment->comment_ID}",
-			esc_attr__( 'Edit this comment' ),
-			__( 'Edit' )
-		);
-
-		$actions['reply'] = sprintf(
-			'<button type="button" onclick="window.commentReply && commentReply.open(\'%s\',\'%s\');" class="vim-r button-link hide-if-no-js" aria-label="%s">%s</button>',
-			$comment->comment_ID,
-			$comment->comment_post_ID,
-			esc_attr__( 'Reply to this comment' ),
-			/* translators: Comment reply button text. */
-			_x( 'Reply', 'verb' )
-		);
-
-		$actions['spam'] = sprintf(
-			'<a href="%s" data-wp-lists="%s" class="vim-s vim-destructive aria-button-if-js" aria-label="%s">%s</a>',
-			esc_url( $spam_url ),
-			"delete:the-comment-list:comment-{$comment->comment_ID}::spam=1",
-			esc_attr__( 'Mark this comment as spam' ),
-			/* translators: "Mark as spam" link. */
-			_x( 'Spam', 'verb' )
-		);
-
-		if ( ! EMPTY_TRASH_DAYS ) {
-			$actions['delete'] = sprintf(
-				'<a href="%s" data-wp-lists="%s" class="delete vim-d vim-destructive aria-button-if-js" aria-label="%s">%s</a>',
-				esc_url( $delete_url ),
-				"delete:the-comment-list:comment-{$comment->comment_ID}::trash=1",
-				esc_attr__( 'Delete this comment permanently' ),
-				__( 'Delete Permanently' )
-			);
-		} else {
-			$actions['trash'] = sprintf(
-				'<a href="%s" data-wp-lists="%s" class="delete vim-d vim-destructive aria-button-if-js" aria-label="%s">%s</a>',
-				esc_url( $trash_url ),
-				"delete:the-comment-list:comment-{$comment->comment_ID}::trash=1",
-				esc_attr__( 'Move this comment to the Trash' ),
-				_x( 'Trash', 'verb' )
-			);
-		}
-
-		$actions['view'] = sprintf(
-			'<a class="comment-link" href="%s" aria-label="%s">%s</a>',
-			esc_url( get_comment_link( $comment ) ),
-			esc_attr__( 'View this comment' ),
-			__( 'View' )
-		);
-
-		/** This filter is documented in wp-admin/includes/class-wp-comments-list-table.php */
-		$actions = apply_filters( 'comment_row_actions', array_filter( $actions ), $comment );
-
-		$i = 0;
-
-		foreach ( $actions as $action => $link ) {
-			++$i;
-
-			if ( ( ( 'approve' === $action || 'unapprove' === $action ) && 2 === $i )
-				|| 1 === $i
-			) {
-				$separator = '';
-			} else {
-				$separator = ' | ';
-			}
-
-			// Reply and quickedit need a hide-if-no-js span.
-			if ( 'reply' === $action || 'quickedit' === $action ) {
-				$action .= ' hide-if-no-js';
-			}
-
-			if ( 'view' === $action && '1' !== $comment->comment_approved ) {
-				$action .= ' hidden';
-			}
-
-			$actions_string .= "<span class='$action'>{$separator}{$link}</span>";
-		}
 	}
 	?>
 
@@ -1079,11 +959,6 @@ function _wp_dashboard_recent_notes_row( &$comment, $show_date = true ) {
 
 			<?php
 			$comment_row_class = '';
-
-			if ( get_option( 'show_avatars' ) ) {
-				echo get_avatar( $comment, 50, 'mystery' );
-				$comment_row_class .= ' has-avatar';
-			}
 			?>
 
 			<?php if ( ! $comment->comment_type || 'comment' === $comment->comment_type ) : ?>
@@ -1091,20 +966,21 @@ function _wp_dashboard_recent_notes_row( &$comment, $show_date = true ) {
 			<div class="dashboard-comment-wrap has-row-actions <?php echo $comment_row_class; ?>">
 			<p class="comment-meta">
 				<?php
-				// Comments might not have a post they relate to, e.g. programmatically created ones.
+				//echo get_comment_date( $comment );
+				// notes always need a post to be attached to.  
 				if ( $comment_post_link ) {
 					printf(
-						/* translators: 1: Comment author, 2: Post link, 3: Notification if the comment is pending. */
-						__( 'From %1$s on %2$s %3$s' ),
-						'<cite class="comment-author">' . get_comment_author_link( $comment ) . '</cite>',
-						$comment_post_link,
-						'<span class="approve">' . __( '[Pending]' ) . '</span>'
+						/* translators: 1: Comment author, 2: Post link */
+						__( '%1$s - From %2$s on %3$s' ),
+						'' , //get_comment_date( $comment ),
+						'<cite class="comment-author">' . get_comment_author( $comment ) . '</cite>',
+						$comment_post_link
 					);
 				} else {
 					printf(
 						/* translators: 1: Comment author, 2: Notification if the comment is pending. */
 						__( 'From %1$s %2$s' ),
-						'<cite class="comment-author">' . get_comment_author_link( $comment ) . '</cite>',
+						'<cite class="comment-author">' . get_comment_author( $comment ) . '</cite>',
 						'<span class="approve">' . __( '[Pending]' ) . '</span>'
 					);
 				}
@@ -1151,9 +1027,6 @@ function _wp_dashboard_recent_notes_row( &$comment, $show_date = true ) {
 
 			<?php endif; // comment_type ?>
 			<blockquote><p><?php comment_excerpt( $comment ); ?></p></blockquote>
-			<?php if ( $actions_string ) : ?>
-			<p class="row-actions"><?php echo $actions_string; ?></p>
-			<?php endif; ?>
 			</div>
 		</li>
 	<?php
