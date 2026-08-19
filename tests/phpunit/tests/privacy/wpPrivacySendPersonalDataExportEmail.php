e82c6486bf949d059fd10d2805c3cecc13bf67e1
Jon Surrell <sirreal@users.noreply.github.com>
2026-08-19 15:44:02 +0400

    Tests: Reset locale fixtures after email tests

--- tests/phpunit/tests/privacy/wpPrivacySendPersonalDataExportEmail.php
+++ tests/phpunit/tests/privacy/wpPrivacySendPersonalDataExportEmail.php
@@ -80,6 +80,10 @@ class Tests_Privacy_wpPrivacySendPersonalDataExportEmail extends WP_UnitTestCase
 	 */
 	public function set_up() {
 		parent::set_up();
+
+		unset( $GLOBALS['locale'] );
+		get_locale();
+
 		reset_phpmailer_instance();
 	}
 
@@ -92,6 +96,8 @@ class Tests_Privacy_wpPrivacySendPersonalDataExportEmail extends WP_UnitTestCase
 		reset_phpmailer_instance();
 		restore_previous_locale();
 		parent::tear_down();
+
+		unset( $GLOBALS['locale'] );
 	}
 
 	/**
