162c09d1cc9e41b6a48eb7b2b35b3554be19ba8a
Jon Surrell <sirreal@users.noreply.github.com>
2026-08-20 16:48:56 +0400

    HTML API: Clarify parse_next_attribute() return value

--- src/wp-includes/html-api/class-wp-html-tag-processor.php
+++ src/wp-includes/html-api/class-wp-html-tag-processor.php
@@ -2208,7 +2208,8 @@ class WP_HTML_Tag_Processor {
 	 * @since 6.2.0
 	 * @ignore
 	 *
-	 * @return bool Whether an attribute was found before the end of the document.
+	 * @return bool Whether an attribute was found. Returns `false` upon reaching the end
+	 *              of the tag (`>` or `/>`) or the end of the document.
 	 */
 	private function parse_next_attribute(): bool {
 		$doc_length = strlen( $this->html );
