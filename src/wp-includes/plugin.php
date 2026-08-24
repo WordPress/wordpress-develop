2905a7d172244d8410e101f5bb1e9949652669bf
John Blackbourn <john@johnblackbourn.com>
2026-08-24 03:32:04 +0100

    Use the `Maybe_Callable` alias for the `$callback` parameter of `_wp_filter_build_unique_id()`.

--- src/wp-includes/plugin.php
+++ src/wp-includes/plugin.php
@@ -1005,7 +1005,7 @@ function _wp_call_all_hook( $args ) {
  * @param int                   $priority  Unused. The order in which the functions
  *                                         associated with a particular action are executed.
  * @return string|null Unique function ID for usage as array key, or null if it couldn't be determined.
- * @phpstan-param callable|string|array{ 0: string|object, 1: string, ... } $callback
+ * @phpstan-param Maybe_Callable $callback
  * @phpstan-return non-decimal-int-string|null
  */
 function _wp_filter_build_unique_id( $hook_name, $callback, $priority ): ?string {
