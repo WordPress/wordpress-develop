9ce51c95faeb675d1bc236eae85361364fcfcc75
Andrea Fercia <a.fercia@gmail.com>
2026-08-29 12:46:36 +0200

    Fix return notation of the sync function in wp.media.model.Attachment.

--- src/js/media/models/attachment.js
+++ src/js/media/models/attachment.js
@@ -18,7 +18,7 @@ Attachment = Backbone.Model.extend(/** @lends wp.media.model.Attachment.prototyp
 	 * @param {wp.media.model.Attachment} model
 	 * @param {Object} [options={}]
 	 *
-	 * @return {Promise} A jQuery Promise that is resolved or rejected based on the success of the sync operation.
+	 * @return {jQuery.Promise} A jQuery Promise that is resolved or rejected based on thesuccess of the sync operation. Returns a rejected Promise when `this.id` is undefined or when the `update` nonce is missing.
 	 */
 	sync: function( method, model, options ) {
 		// If the attachment does not yet have an `id`, return an instantly
