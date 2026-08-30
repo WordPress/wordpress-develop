d2e3f4c86d1c3e68affe4e5fcda987707b687b7f
Weston Ruter <weston@ruter.net>
2026-08-29 22:59:22 -0700

    Add missing type information for $wp_post_types global

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -1917,7 +1917,7 @@ function register_post_type( $post_type, $args = array() ) {
  *
  * @since 4.5.0
  *
- * @global array $wp_post_types List of post types.
+ * @global array<string, WP_Post_Type> $wp_post_types List of post types.
  *
  * @param string $post_type Post type to unregister.
  * @return true|WP_Error True on success, WP_Error on failure or if the post type doesn't exist.
