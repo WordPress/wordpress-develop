763c1edbe81826317b0282dc2faf454a6cc9fe5e
Sainath Poojary <spoojary614@gmail.com>
2026-08-31 23:57:03 +0530

    feat: add register_site_meta() and unregister_site_meta() functions

--- src/wp-includes/meta.php
+++ src/wp-includes/meta.php
@@ -1789,7 +1789,7 @@ function _wp_register_meta_args_allowed_list( $args, $default_args ) {
  * Returns the object subtype for a given object ID of a specific type.
  *
  * @since 4.9.8
- * @since 6.X.0 Added support for 'blog' object type in multisite.
+ * @since 7.2.0 Added support for 'blog' object type in multisite.
  *
  * @param string $object_type Type of object metadata is for. Accepts 'blog', 'post', 'comment', 'term',
  *                            'user', or any other object type with an associated meta table.
