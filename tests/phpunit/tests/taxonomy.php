56ce558b680d1669056d6aa9d079eddae1e1dcc6
Lance Willett <lancewillett@git.wordpress.org>
2026-09-02 18:33:43 +0000

    Build/Test Tools: Improve bcrypt-related unit test performance.
    Merges [60253] and [60298] to the 6.8 branch.
    
    Move repeated user creation into shared fixtures and lower the bcrypt cost during tests. This reduces suite runtime and prevents CI timeouts, especially on PHP 8.4.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13018
    
    Reviewed by: desrosj.
    Props peterwilsoncc, johnbillion, lucatume, adrianmoldovanwp.
    Fixes #65861.
    See #63026.
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.8@63439 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/taxonomy.php
+++ tests/phpunit/tests/taxonomy.php
@@ -4,6 +4,18 @@
  * @group taxonomy
  */
 class Tests_Taxonomy extends WP_UnitTestCase {
+
+	/**
+	 * Editor user ID.
+	 *
+	 * @var int $editor_id
+	 */
+	public static $editor_id;
+
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		self::$editor_id = $factory->user->create( array( 'role' => 'editor' ) );
+	}
+
 	public function test_get_post_taxonomies() {
 		$this->assertSame( array( 'category', 'post_tag', 'post_format' ), get_object_taxonomies( 'post' ) );
 	}
@@ -1002,7 +1014,7 @@ class Tests_Taxonomy extends WP_UnitTestCase {
 			)
 		);
 
-		wp_set_current_user( self::factory()->user->create( array( 'role' => 'editor' ) ) );
+		wp_set_current_user( self::$editor_id );
 		$updated_post_id = edit_post(
 			array(
 				'post_ID'   => $post->ID,
@@ -1028,7 +1040,7 @@ class Tests_Taxonomy extends WP_UnitTestCase {
 	 */
 	public function test_default_term_for_custom_taxonomy() {
 
-		wp_set_current_user( self::factory()->user->create( array( 'role' => 'editor' ) ) );
+		wp_set_current_user( self::$editor_id );
 
 		$tax = 'custom-tax';
 
