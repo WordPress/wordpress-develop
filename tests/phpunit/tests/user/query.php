8c81776f497109b06975263d8048b35175662622
Lance Willett <lance.willett@automattic.com>
2026-09-03 12:43:23 -0700

    Tests: Preserve super admin capability fixture

--- tests/phpunit/tests/user/query.php
+++ tests/phpunit/tests/user/query.php
@@ -1656,7 +1656,7 @@ class Tests_User_Query extends WP_UnitTestCase {
 	 */
 	public function test_search_by_display_name_only() {
 
-		$new_user1 = self::factory()->user->create(
+		$new_user1  = self::factory()->user->create(
 			array(
 				'user_login'   => 'name1',
 				'display_name' => 'Sophia Andresen',
@@ -1684,7 +1684,7 @@ class Tests_User_Query extends WP_UnitTestCase {
 	 */
 	public function test_search_by_display_name_only_ignore_others() {
 
-		$new_user1 = self::factory()->user->create(
+		$new_user1  = self::factory()->user->create(
 			array(
 				'user_login'   => 'Sophia Andresen',
 				'display_name' => 'name1',
