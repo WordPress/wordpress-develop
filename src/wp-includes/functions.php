f799969716a2b429f901cdb7d4609090c1809091
Weston Ruter <westonruter@git.wordpress.org>
2026-09-07 00:10:14 +0000

    Build/Test Tools: Update PHPStan to 2.2.13.
    Recent PHPStan releases bring substantial performance improvements. The baselines are regenerated for the new version.
    
    The docblock of `_wp_die_process_input()` is also corrected to admit the integer message that `wp_die()` accepts for the legacy Ajax responses, which the helper passes through untouched. This removes a baseline entry for a guard in `_default_wp_die_handler()` that the incomplete type made look unreachable.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/13410.
    Follow-up to r44666, r53144, r62703, r62798.
    
    Props markusstaab, westonruter.
    See #65817.
    Fixes #66051.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@63510 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -4360,15 +4360,15 @@ function _scalar_wp_die_handler( $message = '', $title = '', $args = array() ) {
  * @since 5.1.0
  * @access private
  *
- * @param string|WP_Error $message Error message or WP_Error object.
- * @param string          $title   Optional. Error title. Default empty string.
- * @param string|array    $args    Optional. Arguments to control behavior. Default empty array.
+ * @param string|WP_Error|int $message Error message, WP_Error object, or integer response.
+ * @param string              $title   Optional. Error title. Default empty string.
+ * @param string|array        $args    Optional. Arguments to control behavior. Default empty array.
  * @return array {
  *     Processed arguments.
  *
- *     @type string $0 Error message.
- *     @type string $1 Error title.
- *     @type array  $2 Arguments to control behavior.
+ *     @type string|int $0 Error message, or integer response.
+ *     @type string     $1 Error title.
+ *     @type array      $2 Arguments to control behavior.
  * }
  */
 function _wp_die_process_input( $message, $title = '', $args = array() ) {
