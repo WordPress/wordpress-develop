336be9aab03f8daa21477d4854fc92ed8a412cbf
Dinesh Yadav <dineshinau@gmail.com>
2026-09-11 01:51:43 +0530

    Update src/wp-includes/class-wpdb.php
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- src/wp-includes/class-wpdb.php
+++ src/wp-includes/class-wpdb.php
@@ -1959,7 +1959,7 @@ class wpdb {
 	 * @since 3.0.0
 	 * @since 3.9.0 $allow_bail parameter added.
 	 * @since 7.2.0 A 500 status header and no-cache headers are now sent
- 	 *              before loading a custom db-error.php drop-in.
+	 *              before loading a custom db-error.php drop-in.
 	 *
 	 * @param bool $allow_bail Optional. Allows the function to bail. Default true.
 	 * @return bool True with a successful connection, false on failure.
