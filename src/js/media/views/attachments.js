2822b0a0b8b12b859dec675e43b6bb83040a7831
Andrea Fercia <a.fercia@gmail.com>
2026-09-10 12:18:45 +0200

    Add remaining missing param descriptions.

--- src/js/media/views/attachments.js
+++ src/js/media/views/attachments.js
@@ -365,7 +365,7 @@ Attachments = View.extend(/** @lends wp.media.view.Attachments.prototype */{
 	 *
 	 * @since 3.5.0
 	 *
-	 * @param {wp.media.model.Attachment} attachment
+	 * @param {wp.media.model.Attachment} attachment The attachment model for which to create a view.
 	 *
 	 * @return {wp.media.View} The created view.
 	 */
