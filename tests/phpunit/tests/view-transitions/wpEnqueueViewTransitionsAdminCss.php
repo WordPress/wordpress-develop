d566a1990db09e662701c571b7f3c9e17110929b
Lance Willett <lance.willett@automattic.com>
2026-09-15 09:22:55 -0700

    Tests: Include embeds and view transitions in the assets group.

--- tests/phpunit/tests/view-transitions/wpEnqueueViewTransitionsAdminCss.php
+++ tests/phpunit/tests/view-transitions/wpEnqueueViewTransitionsAdminCss.php
@@ -57,6 +57,8 @@ class Tests_View_Transitions_wpEnqueueViewTransitionsAdminCss extends WP_UnitTes
 	/**
 	 * Tests enqueuing admin view transitions CSS.
 	 *
+	 * @group assets
+	 *
 	 * @ticket 64470
 	 */
 	public function test_wp_enqueue_view_transitions_admin_css() {
