68f0557bbc1e0204ce53609c471e79e731ba6dfa
Paul Bearne <pbearne@git.wordpress.org>
2026-09-18 14:12:50 -0400

    Refactor and rename deprecated function tests for improved clarity and consistency

--- tests/phpunit/tests/wp-includes/functions/DeprecatedFunctionTest.php
+++ tests/phpunit/tests/wp-includes/functions/DeprecatedFunctionTest.php
@@ -1,41 +1,37 @@
 <?php
 
+namespace WordPress\Tests\WP_Includes\Functions;
+
+use MockAction;
+use WP_UnitTestCase;
+
 /**
- * Tests for the _deprecated_function function.
+ * Tests for the `_deprecated_function()` function.
  *
- * @group Functions
+ * @group functions
  *
- * @covers ::__deprecated_function
+ * @covers ::_deprecated_function
  */
-class Tests_Functions_deprecatedFunction extends WP_UnitTestCase {
+class DeprecatedFunctionTest extends WP_UnitTestCase {
 
 	/**
 	 * Sets up the test case.
 	 *
 	 * This method is responsible for setting up the test case before each test method is executed.
-	 * It removes certain actions related to deprecated and _deprecated_functionfunctions.
-	 *
-	 * @return void
+	 * It removes certain actions related to deprecated and _deprecated_function functions.
 	 */
 	public function set_up() {
 		parent::set_up();
 
-		// remove ethe spherical handling for _deprecated_functionin the PHPUnint setup so we can test it
+		// Remove the special handling for _deprecated_function in the PHPUnit setup so we can test it.
 		remove_action( 'deprecated_function_run', array( $this, 'deprecated_function_run' ), 10, 4 );
 		remove_action( 'deprecated_function_trigger_error', '__return_false' );
 	}
 
-
 	/**
+	 * Tests the action being called when _deprecated_function() is invoked.
+	 *
 	 * @ticket 60116
-	 *
-	 * test_deprecated_function_action_called() method tests the action being called when _deprecated_function()
-	 *
-	 * It creates a MockAction object and registers it as a filter for the '_deprecated_function_run' action.
-	 * It then calls __deprecated_function() function with the given 'function_name', 'message', and 1 as arguments.
-	 * Finally, it asserts that the call count of the filter object is equal to 1.
-	 *
-	 * @return void
 	 */
 	public function test_deprecated_function_action_called() {
 		$this->expectDeprecation();
@@ -50,18 +46,9 @@ class Tests_Functions_deprecatedFunction extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Tests if the '_deprecated_function_trigger_error' filter is called when _deprecated_function() is invoked.
+	 *
 	 * @ticket 60116
-	 *
-	 * This method tests if the '_deprecated_function_trigger_error' filter is called
-	 * when the _deprecated_function() function is invoked.
-	 *
-	 * It creates a mock action object and adds a filter to the '_deprecated_function_trigger_error'
-	 * hook, using the mock action as the callback. Then, it calls the __deprecated_function()
-	 * function with the specified function name, message, and error level.
-	 * Finally, it asserts that the filter callback is called exactly once by checking
-	 * the call count of the mock action object.
-	 *
-	 * @return void
 	 */
 	public function test_deprecated_function_filter_called() {
 		$this->expectDeprecation();
@@ -76,10 +63,9 @@ class Tests_Functions_deprecatedFunction extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Tests deprecation notice when no replacement function is specified.
+	 *
 	 * @ticket 60116
-	 *
-	 *
-	 * @return void
 	 */
 	public function test_deprecated_function_no_replacement() {
 		$this->expectDeprecation();
