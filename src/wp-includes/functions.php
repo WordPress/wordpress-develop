29b9e556c09d65e9a39c98db0346889341d876db
Paul Bearne <pbearne@git.wordpress.org>
2026-09-18 13:50:54 -0400

    Suppress warnings in `wp_mkdir_p` and enhance `WpMkdirPTest` teardown process: - Added `@` to `mkdir` in `wp_mkdir_p` to suppress PHP warnings. - Enhanced `WpMkdirPTest` with a robust `tear_down` method for cleaning up test directories.

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -2103,7 +2103,7 @@ function wp_mkdir_p( $target ) {
 		$dir_perms = 0777;
 	}
 
-	if ( mkdir( $target, $dir_perms, true ) || is_dir( $target ) ) {
+	if ( @mkdir( $target, $dir_perms, true ) || is_dir( $target ) ) {
 
 		/*
 		 * If a umask is set that modifies $dir_perms, we'll have to re-set
--- tests/phpunit/tests/wp-includes/functions/WpMkdirPTest.php
+++ tests/phpunit/tests/wp-includes/functions/WpMkdirPTest.php
@@ -13,28 +13,43 @@ use WP_UnitTestCase;
  */
 class WpMkdirPTest extends WP_UnitTestCase {
 
-	public function test_folder_is_made() {
+	/**
+	 * Temporary directories created during testing.
+	 *
+	 * @var string[]
+	 */
+	protected $created_dirs = array();
 
-		$upload_dir = wp_upload_dir();
-		$target     = $upload_dir['basedir'] . '/test';
+	public function tear_down() {
+		foreach ( array_reverse( $this->created_dirs ) as $dir ) {
+			if ( is_dir( $dir ) ) {
+				rmdir( $dir );
+			}
+		}
+
+		parent::tear_down();
+	}
+
+	public function test_folder_is_made() {
+		$upload_dir           = wp_upload_dir();
+		$target               = $upload_dir['basedir'] . '/test';
+		$this->created_dirs[] = $target;
+
 		$this->assertTrue( wp_mkdir_p( $target ) );
 		$this->assertDirectoryExists( $target );
 	}
 
 	public function test_if_file_with_name_exists() {
-
 		$this->assertFalse( wp_mkdir_p( ABSPATH . 'wp-admin/index.php' ) );
 	}
 
 	public function test_if_folder_with_name_exists() {
-
 		$this->assertTrue( wp_mkdir_p( ABSPATH . 'wp-admin' ) );
 	}
 
 	// should return false ../ in path
 	// Do not allow path traversals.
 	public function test_if_up_tree_in_target() {
-
 		$this->assertFalse( wp_mkdir_p( '../wp-admin' ) );
 		$this->assertFalse( wp_mkdir_p( '../../test' ) );
 		$this->assertFalse( wp_mkdir_p( '..' . DIRECTORY_SEPARATOR . 'test' ) );
@@ -45,10 +60,11 @@ class WpMkdirPTest extends WP_UnitTestCase {
 	}
 
 	public function test_permissions_are_set() {
+		$upload_dir           = wp_upload_dir();
+		$target               = $upload_dir['basedir'] . '/permission_test';
+		$this->created_dirs[] = $target;
+		$parent_stat          = stat( $upload_dir['basedir'] );
 
-		$upload_dir  = wp_upload_dir();
-		$target      = $upload_dir['basedir'] . 'permission_test';
-		$parent_stat = stat( $upload_dir['basedir'] );
 		$this->assertTrue( wp_mkdir_p( $target ) );
 		$target_stat = stat( $target );
 		$this->assertSame( $parent_stat['mode'], $target_stat['mode'] );
