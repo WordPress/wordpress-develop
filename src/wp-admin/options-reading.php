06284a6c36502d4a2662e32bb4ac431fb59ec759
Andrea Fercia <a.fercia@gmail.com>
2026-09-20 13:04:16 +0200

    Use explicit labeling and consistent markup.

--- src/wp-admin/options-reading.php
+++ src/wp-admin/options-reading.php
@@ -192,10 +192,10 @@ else :
 <th scope="row"><?php echo $rss_use_excerpt_title; ?> </th>
 <td><fieldset aria-describedby="rss-use-excerpt-description">
 	<legend class="screen-reader-text"><?php echo $rss_use_excerpt_title; ?></legend>
-	<p>
-		<label><input name="rss_use_excerpt" type="radio" value="0" <?php checked( 0, get_option( 'rss_use_excerpt' ) ); ?>	/> <?php _e( 'Full text' ); ?></label><br />
-		<label><input name="rss_use_excerpt" type="radio" value="1" <?php checked( 1, get_option( 'rss_use_excerpt' ) ); ?> /> <?php _e( 'Excerpt' ); ?></label>
-	</p>
+	<input id="rss-use-full-text" name="rss_use_excerpt" type="radio" value="0" <?php checked( 0, get_option( 'rss_use_excerpt' ) ); ?> />
+	<label for="rss-use-full-text"><?php _e( 'Full text' ); ?></label><br />
+	<input id="rss-use-excerpt" name="rss_use_excerpt" type="radio" value="1" <?php checked( 1, get_option( 'rss_use_excerpt' ) ); ?> />
+	<label for="rss-use-excerpt"><?php _e( 'Excerpt' ); ?></label>
 	<p class="description" id="rss-use-excerpt-description">
 		<?php
 		printf(
