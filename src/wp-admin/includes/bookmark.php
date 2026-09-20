c348216d01518354a1e6b4bc014780b391648b4d
Weston Ruter <weston@ruter.net>
2026-09-20 16:41:24 -0700

    Correct return type of wp_get_link_cats() to account for WP_Error

--- src/wp-admin/includes/bookmark.php
+++ src/wp-admin/includes/bookmark.php
@@ -121,12 +121,13 @@ function wp_delete_link( $link_id ) {
  * @since 2.1.0
  *
  * @param int $link_id Link ID to look up.
- * @return int[] The IDs of the requested link's categories.
- *
- * @phpstan-return ( $link_id is empty ? array{} : array<int, int<1, max>> )
+ * @return int[]|WP_Error The IDs of the requested link's categories, or else a WP_Error if the `link_category` taxonomy was unregistered.
  */
 function wp_get_link_cats( $link_id = 0 ) {
 	$cats = wp_get_object_terms( $link_id, 'link_category', array( 'fields' => 'ids' ) );
+	if ( is_wp_error( $cats ) ) {
+		return $cats;
+	}
 	return array_unique( $cats );
 }
 
