dba0a229b03bdf0932ade32bffce18071621e075
Lance Willett <lance.willett@automattic.com>
2026-09-22 08:58:05 -0700

    Build/Test Tools: Assert that the parent image copy succeeds in the site icon test.
    Follow-up per review: a `copy()` returning `false` now fails the test with a clear message instead of an unrelated failure further down.
    
    See #66159.

--- tests/phpunit/tests/image/siteIcon.php
+++ tests/phpunit/tests/image/siteIcon.php
@@ -138,7 +138,7 @@ class Tests_WP_Site_Icon extends WP_UnitTestCase {
 		$attachment_id = $this->insert_attachment();
 		$parent_file   = get_attached_file( $attachment_id );
 		$cropped       = str_replace( wp_basename( $parent_file ), 'cropped-test-image.jpg', $parent_file );
-		copy( $parent_file, $cropped );
+		$this->assertTrue( copy( $parent_file, $cropped ), 'Failed to copy the image for the cropped attachment.' );
 
 		$object     = wp_copy_parent_attachment_properties( $cropped, $attachment_id, 'site-icon' );
 		$cropped_id = $this->wp_site_icon->insert_attachment( $object, $cropped );
