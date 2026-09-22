3f62d5d476bfccaa4508ff56ce537cedbda4f452
haritpanchal <panchalharit007@gmail.com>
2026-09-22 20:21:01 +0530

    Build/Test Tools: Use strict assertions in the post tests.
    Trac ticket: https://core.trac.wordpress.org/ticket/64895

--- tests/phpunit/tests/post/types.php
+++ tests/phpunit/tests/post/types.php
@@ -91,7 +91,7 @@ class Tests_Post_Types extends WP_UnitTestCase {
 		 */
 		$args = register_post_type( $this->post_type, array( 'public' => $public = false ) );
 
-		$this->assertNotEquals( $public, $args->exclude_from_search );
+		$this->assertNotSame( $public, $args->exclude_from_search );
 	}
 
 	/**
