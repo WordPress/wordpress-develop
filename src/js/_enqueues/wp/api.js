1fb723fe9f6fd51117256112b4d60af063b93af4
Andrea Fercia <a.fercia@gmail.com>
2026-09-22 09:01:33 +0200

    Improve documentation of sync functions in wp api.js file.

--- src/js/_enqueues/wp/api.js
+++ src/js/_enqueues/wp/api.js
@@ -866,14 +866,14 @@
 			},
 
 			/**
-			 * Set nonce header before every Backbone sync.
+			 * Sets a nonce header before every Backbone sync.
 			 *
 			 * @param {string}         method               The CRUD method ("create", "read", "update", or "delete") to be performed.
 			 * @param {Backbone.Model} model                The model to be synced.
 			 * @param {Object}         options              Additional options for the sync.
 			 * @param {Function}       [options.beforeSend] A function to be called before sending the request.
 			 * @param {Function}       [options.complete]   A function to be called when the request completes.
-			 * @return {*}.
+			 * @return {JQuery.Promise} A promise that resolves when the sync operation completes.
 			 */
 			sync: function( method, model, options ) {
 				var beforeSend;
@@ -1029,9 +1029,9 @@
 			},
 
 			/**
-			 * Extend Backbone.Collection.sync to add nonce and pagination support.
+			 * Extends Backbone.Collection.sync to add nonce and pagination support.
 			 *
-			 * Set nonce header before every Backbone sync.
+			 * Sets a nonce header before every Backbone sync.
 			 *
 			 * @param {string}         method               The CRUD method ("create", "read", "update", or "delete") to be performed.
 			 * @param {Backbone.Model} model                The model to be synced.
@@ -1040,7 +1040,7 @@
 			 * @param {Function}       [options.success]    A function to be called when the request succeeds.
 			 * @param {Function}       [options.complete]   A function to be called when the request completes, regardless of success or failure.
 			 * @param {Object}         [options.data]       Data to be sent with the request.
-			 * @return {*}.
+			 * @return {JQuery.Promise} A promise that resolves when the sync operation completes.
 			 */
 			sync: function( method, model, options ) {
 				var beforeSend, success,
