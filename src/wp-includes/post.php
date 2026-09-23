69d9b727bbc854c5dcf405aea98c2712e325fddc
Adam Zieliński <adam@adamziel.com>
2026-09-23 16:24:01 +0200

    Query: Stop at the first page-path match for post type arrays

--- src/wp-includes/post.php
+++ src/wp-includes/post.php
@@ -6448,7 +6448,7 @@ function get_page_by_path( $page_path, $output = OBJECT, $post_type = 'page' ) {
 				&& $p->post_name === $revparts[ $count ]
 			) {
 				$found_id = $page->ID;
-				if ( $page->post_type === $post_type ) {
+				if ( in_array( $page->post_type, (array) $post_type, true ) ) {
 					break;
 				}
 			}
