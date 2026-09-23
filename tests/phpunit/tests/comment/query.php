2725f43af552d2d02d677e0ff9dc9b3adde8cb24
Weston Ruter <weston@ruter.net>
2026-09-22 18:36:18 -0700

    Remove trailing whitespace

--- tests/phpunit/tests/comment/query.php
+++ tests/phpunit/tests/comment/query.php
@@ -4523,7 +4523,7 @@ class Tests_Comment_Query extends WP_UnitTestCase {
 	 * @dataProvider data_hierarchical_modes
 	 *
 	 * @param 'threaded'|'flat' $hierarchical Value of the 'hierarchical' query var.
-	 * 
+	 *
 	 * @global wpdb $wpdb WordPress database abstraction object.
 	 */
 	public function test_fill_descendants_should_skip_cached_child_ids_that_no_longer_exist( string $hierarchical ): void {
