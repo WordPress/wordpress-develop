d20da9a1383eb9390e434d7fd0d7cb224087473f
Lance Willett <lancewillett@git.wordpress.org>
2026-09-23 17:16:22 +0000

    Tests: Use strict assertions in REST API tests.
    Use type-strict assertions for REST response values and metadata. Match expected types to raw database values and REST schema sanitization, and explicitly check for null when JSON parameters are absent.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13683
    
    Props haritpanchal.
    See #64895.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@63896 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/rest-api/rest-post-meta-fields.php
+++ tests/phpunit/tests/rest-api/rest-post-meta-fields.php
@@ -854,12 +854,12 @@ class WP_Test_REST_Post_Meta_Fields extends WP_Test_REST_TestCase {
 		$meta = get_post_meta( self::$post_id, 'test_custom_schema', false );
 		$this->assertNotEmpty( $meta );
 		$this->assertCount( 1, $meta );
-		$this->assertEquals( 3, $meta[0] );
+		$this->assertSame( '3', $meta[0] );
 
 		$data = $response->get_data();
 		$meta = (array) $data['meta'];
 		$this->assertArrayHasKey( 'test_custom_schema', $meta );
-		$this->assertEquals( 3, $meta['test_custom_schema'] );
+		$this->assertSame( 3.0, $meta['test_custom_schema'] );
 	}
 
 	public function test_set_value_multiple_custom_schema() {
@@ -883,7 +883,7 @@ class WP_Test_REST_Post_Meta_Fields extends WP_Test_REST_TestCase {
 		$meta = get_post_meta( self::$post_id, 'test_custom_schema_multi', false );
 		$this->assertNotEmpty( $meta );
 		$this->assertCount( 1, $meta );
-		$this->assertEquals( 2, $meta[0] );
+		$this->assertSame( '2', $meta[0] );
 
 		// Add another value.
 		$data = array(
