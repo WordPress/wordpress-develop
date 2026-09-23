ba1b0bac5049278dc1eea0040658ae1e01aebb79
arshidkv12 <arshidkv12@gmail.com>
2026-09-23 21:29:26 +0530

    Code Quality: Remove unnecessary null coalescing

--- src/wp-includes/class-wp-http-cookie.php
+++ src/wp-includes/class-wp-http-cookie.php
@@ -99,7 +99,7 @@ class WP_Http_Cookie {
 	 *     @type string          $name      Cookie name.
 	 *     @type mixed           $value     Value. Should NOT already be urlencoded.
 	 *     @type string|int|null $expires   Optional. Unix timestamp or formatted date. Default null.
-	 *     @type string          $path      Optional. Path. Default '/'.
+	 *     @type string|null     $path      Optional. Path. Default '/'.
 	 *     @type string          $domain    Optional. Domain. Default host of parsed $requested_url.
 	 *     @type int|string      $port      Optional. Port or comma-separated list of ports. Default null.
 	 *     @type bool            $host_only Optional. host-only storage flag. Default true.
