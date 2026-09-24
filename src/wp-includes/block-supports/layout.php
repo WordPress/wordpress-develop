59bb6c636b4275887c838a43739184a1c686a552
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2026-09-24 17:43:41 +0530

    Block Supports: Add additional flex justification options

--- src/wp-includes/block-supports/layout.php
+++ src/wp-includes/block-supports/layout.php
@@ -733,7 +733,11 @@ function wp_get_layout_style( $selector, $layout, $has_block_gap_support = false
 		);
 
 		if ( 'horizontal' === $layout_orientation ) {
-			$justify_content_options    += array( 'space-between' => 'space-between' );
+			$justify_content_options    += array(
+				'space-between' => 'space-between',
+				'space-around'  => 'space-around',
+				'space-evenly'  => 'space-evenly',
+			);
 			$vertical_alignment_options += array( 'stretch' => 'stretch' );
 		} else {
 			$justify_content_options    += array( 'stretch' => 'stretch' );
