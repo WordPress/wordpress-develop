529bf0cb47d51d2e0dbd158484ccd422317e0e70
Shail Mehta <shailmehta25@gmail.com>
2026-09-24 21:50:50 +0530

    Apply Feedback Changes

--- tests/phpunit/tests/icons/wpRestIconsController.php
+++ tests/phpunit/tests/icons/wpRestIconsController.php
@@ -135,11 +135,17 @@ class Tests_REST_WpRestIconsController extends WP_Test_REST_Controller_Testcase 
 	}
 
 	/**
-	 * @ticket 64651
 	 * @ticket 40538
+	 * @ticket 64651
 	 */
 	public function test_context_param() {
 		$request  = new WP_REST_Request( 'OPTIONS', '/wp/v2/icons' );
+		$response = rest_get_server()->dispatch( $request );
+		$data     = $response->get_data();
+		$this->assertSame( 'view', $data['endpoints'][0]['args']['context']['default'] );
+		$this->assertSame( array( 'view', 'embed', 'edit' ), $data['endpoints'][0]['args']['context']['enum'] );
+
+		$request  = new WP_REST_Request( 'OPTIONS', '/wp/v2/icons/core' );
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
 		$this->assertSame( 'view', $data['endpoints'][0]['args']['context']['default'] );
@@ -241,8 +247,8 @@ class Tests_REST_WpRestIconsController extends WP_Test_REST_Controller_Testcase 
 	}
 
 	/**
-	 * @ticket 64651
 	 * @ticket 40538
+	 * @ticket 64651
 	 *
 	 * @covers ::get_item_schema
 	 */
